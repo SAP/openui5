a35cfc3212809db1861bb3ba0439a6b90785d79b
Jocelyn Gu <jocelyn.gu@sap.com>
2013-09-17 17:15:18 +0800

    [SAPUI5-AutoTest] Add TextView test case and refine test case. 1. Change QTP to visual in url.
    Change-Id: I81dc5748b48bea9b99c004382e3e4315668d15d8

---
+++ test/selenium-visual-tests/src/test/java/com/sap/ui5/modules/librarytests/commons/pages/TextViewPO.java
@@ -0,0 +1,16 @@
+package com.sap.ui5.modules.librarytests.commons.pages;
+
+public class TextViewPO {
+
+	public String textFieldId = "textField";
+
+	public String textView1Id = "textView1";
+
+	public String textView3Id = "textView3";
+
+	public String textView10Id = "textView10";
+
+	public String textView11Id = "textView11";
+
+	public int millisecond = 1000;
+}
---
+++ test/selenium-visual-tests/src/test/java/com/sap/ui5/modules/librarytests/commons/tests/TextViewTest.java
@@ -0,0 +1,56 @@
+package com.sap.ui5.modules.librarytests.commons.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.ui5.modules.librarytests.commons.pages.TextViewPO;
+import com.sap.ui5.selenium.common.TestBase;
+
+public class TextViewTest extends TestBase {
+
+	private TextViewPO page;
+
+	private String targetUrl = "/test-resources/sap/ui/commons/visual/TextView.html";
+
+	@Before
+	public void setUp() {
+		page = PageFactory.initElements(driver, TextViewPO.class);
+		driver.get(getFullUrl(targetUrl));
+		userAction.mouseClickStartPoint(driver);
+	}
+
+	@Test
+	public void testAllElements() {
+		waitForReady(2000);
+		verifyFullPageUI("full-initial");
+	}
+
+	@Test
+	public void testTextViewTooltip() {
+		String elementId = page.textView1Id;
+
+		showToolTip(elementId, page.millisecond);
+		verifyBrowserViewBox("Tooltip-" + elementId);
+	}
+
+	@Test
+	public void testDoubleClickAction() {
+		String elementId = page.textView3Id;
+
+		// ------------ Selected text on mouse double-click --------------
+		userAction.mouseDoubleClick(driver, page.textView3Id);
+		userAction.mouseMoveToStartPoint(driver);
+		verifyElementUI(elementId, "DoubleClick-" + elementId);
+	}
+
+	@Test
+	public void testChangeTextEvent() {
+		verifyElementUI(page.textView10Id, "beforeChanging-" + page.textView10Id);
+		driver.findElement(By.id(page.textFieldId)).sendKeys("Changed text for TextView10");
+
+		verifyElementUI(page.textView10Id, "afterChanging-" + page.textView10Id);
+	}
+
+}
