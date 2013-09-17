0d73437cbbe5b11b79458287a8438bdacbc047ab
Owen Hou <owen.hou@sap.com>
2013-09-17 15:48:26 +0800

    [SAPUI5-AutoTest] Refine Tree test case. 1. Remove showTooltip method in TreePO. 2. Remove waitForTreeSubNodes method in TreePO.
    Change-Id: I692009727db73e4d1f9c7308aa2b1967ad57837e

---
+++ test/selenium-visual-tests/src/test/java/com/sap/ui5/modules/librarytests/commons/tests/TreeTest.java
@@ -0,0 +1,92 @@
+package com.sap.ui5.modules.librarytests.commons.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.ui5.modules.librarytests.commons.pages.TreePO;
+import com.sap.ui5.selenium.common.TestBase;
+
+public class TreeTest extends TestBase {
+
+	private TreePO page;
+
+	private String targetUrl = "/test-resources/sap/ui/commons/visual/Tree.html";
+
+	private int waitMilliseconds = 1000;
+
+	private long timeOutSeconds = 10;
+
+	@Before
+	public void setUp() {
+		page = PageFactory.initElements(driver, TreePO.class);
+		driver.get(getFullUrl(targetUrl));
+		userAction.mouseClickStartPoint(driver);
+	}
+
+	@Test
+	public void testAllElements() {
+		verifyFullPageUI("full-initial");
+	}
+
+	@Test
+	public void testClickAction() {
+		// Test click on tree1
+		// Click node11. Avoid twinkle of tree node on IE9.
+		userAction.mouseClick(driver, page.tree1Node11.getAttribute("id"));
+		userAction.mouseMoveToStartPoint(driver);
+		verifyNotificationArea("-Click-Node");
+
+		// Click node111. Avoid twinkle of tree node on IE9.
+		userAction.mouseClick(driver, page.tree1Node111.getAttribute("id"));
+		userAction.mouseMoveToStartPoint(driver);
+		waitForReady(waitMilliseconds);
+		verifyNotificationArea("-Click-NonSelectableNode");
+
+		// Click node1
+		page.clickNodeIcon(driver, userAction, page.tree1Node1, isRtlTrue());
+		waitForElement(driver, false, page.tree1Node1.getAttribute("id") + "-children", timeOutSeconds);
+		verifyNotificationArea("-CollapseNode");
+
+		// Click node2
+		page.clickNodeIcon(driver, userAction, page.tree1Node2, isRtlTrue());
+		waitForElement(driver, true, page.tree1Node2.getAttribute("id") + "-children", timeOutSeconds);
+		verifyNotificationArea("-ExpandNode");
+
+		// Click node211. Avoid twinkle of tree node on IE9.
+		userAction.mouseClick(driver, page.tree1Node211.getAttribute("id"));
+		userAction.mouseMoveToStartPoint(driver);
+		verifyElementUI(page.tree1Id, page.tree1Id + "-Click-NonSelectableNonExpandableNode");
+	}
+
+	@Test
+	public void testExpandOrCollapseAllNodes() {
+		// Click collaspeAll button in tree1
+		page.tree1CollapseAll.click();
+		waitForElement(driver, false, page.tree1Node1.getAttribute("id") + "-children", timeOutSeconds);
+		verifyElementUI(page.tree1ContentId, page.tree1Id + "-CollapseAll");
+		verifyElementUI(page.notificationAreaId, "notificationArea-" + page.tree1Id + "-CollapseAll");
+
+		// Click expandAll button in tree1
+		page.tree1ExpandAll.click();
+		waitForElement(driver, true, page.tree1Node1.getAttribute("id") + "-children", timeOutSeconds);
+		verifyNotificationArea("-ExpandAll");
+	}
+
+	@Test
+	public void testTooltip() {
+		// Test toolTip on unselected Node
+		showToolTip(page.tree1Node1.getAttribute("id"), waitMilliseconds);
+		verifyBrowserViewBox(page.tree1Node1.getAttribute("id") + "-tooltip");
+
+		// Test toolTip on selected Node
+		showToolTip(page.tree1Node121Id, waitMilliseconds);
+		verifyBrowserViewBox(page.tree1Node121Id + "-tooltip");
+	}
+
+	private void verifyNotificationArea(String imageName) {
+		verifyElementUI(page.tree1Id, page.tree1Id + imageName);
+		verifyElementUI(page.notificationAreaId, "notificationArea-" + page.tree1Id + imageName);
+	}
+
+}
