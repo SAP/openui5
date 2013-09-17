643cd189091a0db7e0370a64a8eef9026beea6dc
Jocelyn Gu <jocelyn.gu@sap.com>
2013-09-17 17:09:08 +0800

    [SAPUI5-AutoTest] Add Slider test case and refine test case. 1. Change QTP to visual in url.
    Change-Id: I7070b34951487c9e2a33921601d83aa86cfd588c

---
+++ test/selenium-visual-tests/src/test/java/com/sap/ui5/modules/librarytests/commons/pages/SliderPO.java
@@ -0,0 +1,25 @@
+package com.sap.ui5.modules.librarytests.commons.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SliderPO {
+	@FindBy(className = "sapUiSli")
+	public List<WebElement> sliders;
+
+	public String slider1Id = "sli1";
+
+	public String slider6Id = "sli6";
+
+	public String gripSuffix = "-grip";
+
+	public String hiliSuffix = "-hili";
+
+	public String targetPrefix = "target-";
+
+	public String outputEventId = "sliVal";
+
+	public int millisecond = 1000;
+}
