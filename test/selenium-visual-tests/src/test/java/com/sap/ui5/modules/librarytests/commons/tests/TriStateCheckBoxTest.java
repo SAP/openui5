5746189406ec76f3867129603956ea2764f9ce6a
Owen Hou <owen.hou@sap.com>
2013-09-18 14:59:44 +0800

    [SAPUI5-AutoTest] Refine TriStateCheckBox test case. 1. Change QTP to visual in url.
    Change-Id: Iec482f28eb5019022a2ad04939cf26102369d720

---
+++ test/selenium-visual-tests/src/test/java/com/sap/ui5/modules/librarytests/commons/tests/TriStateCheckBoxTest.java
@@ -0,0 +1,55 @@
+package com.sap.ui5.modules.librarytests.commons.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+
+import com.sap.ui5.modules.librarytests.commons.pages.TriStateCheckBoxPO;
+import com.sap.ui5.selenium.common.TestBase;
+
+public class TriStateCheckBoxTest extends TestBase {
+
+	private TriStateCheckBoxPO page;
+
+	private String targetUrl = "/test-resources/sap/ui/commons/visual/TriStateCheckBox.html";
+
+	@Before
+	public void setUp() {
+		page = PageFactory.initElements(driver, TriStateCheckBoxPO.class);
+		driver.get(getFullUrl(targetUrl));
+		userAction.mouseClickStartPoint(driver);
+	}
+
+	@Test
+	public void testAllElements() {
+		verifyFullPageUI("full-initial");
+	}
+
+	@Test
+	public void testClickAction() {
+		// Avoid generating no dashed on FIREFOX
+		new Actions(driver).sendKeys(Keys.TAB).perform();
+
+		page.clickElement(driver, userAction, page.checkBoxPcb1Id);
+		verifyElementUI(page.uiAreaId, "Selected-All-first");
+
+		page.clickElement(driver, userAction, page.checkBoxCcb1Id);
+		verifyElementUI(page.uiAreaId, "Deselected-Partly");
+
+		page.clickElement(driver, userAction, page.checkBoxPcb1Id);
+		verifyElementUI(page.uiAreaId, "Selected-All-second");
+
+		page.clickElement(driver, userAction, page.checkBoxPcb1Id);
+		verifyElementUI(page.uiAreaId, "Deselected-All");
+
+		page.clickElement(driver, userAction, page.checkBoxCcb2Id);
+		verifyElementUI(page.uiAreaId, "Selected-Partly");
+
+		page.clickElement(driver, userAction, page.checkBoxCcb1Id);
+		verifyElementUI(page.uiAreaId, "Selected-All-CheckBox");
+
+	}
+
+}
