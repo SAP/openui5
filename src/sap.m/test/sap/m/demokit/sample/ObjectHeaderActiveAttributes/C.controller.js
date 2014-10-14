884cdd7e269bb82a0bca522e3be8c39c7a078048
Borislav Yovchev <borislav.yovchev@sap.com>
2014-10-14 09:44:28 +0300

    [FIX] sap.m.ObjectAttribute: Keyboard handling
    If property active is set to true, the ObjectAttribute will react on
    Space and Enter keys which trigger the press event. Added qUnit tests to
    cover the fix and explored sample to demonstrate the use-case.
    
    Change-Id: Iaa7ba06f5cb811d35ec32f1376ac973de1098b8a
    CSS: 0120031469 0000671382 2014

---
+++ src/sap.m/test/sap/m/demokit/sample/ObjectHeaderActiveAttributes/C.controller.js
@@ -0,0 +1,60 @@
+sap.ui.controller("sap.m.sample.ObjectHeaderActiveAttributes.C", {
+
+	onInit: function(evt) {
+		// set explored app's demo model on this sample
+		var oModel = new sap.ui.model.json.JSONModel("test-resources/sap/ui/demokit/explored/products.json");
+		this.getView().setModel(oModel);
+	},
+
+	handleSAPLinkPressed: function(oEvent) {
+		sap.m.URLHelper.redirect("http://www.sap.com", true);
+	},
+
+	handleFeedbacklinkPressed: function(oEvent) {
+		var oDialog = new sap.m.Dialog({
+			title: "Provide feedback",
+			content: [
+				new sap.m.VBox({
+					fitContainer: true,
+					items: [
+						new sap.m.RatingIndicator({
+							maxValue: 5
+						}), new sap.m.TextArea({
+							placeholder: "What do you think about this item?",
+							rows: 5,
+							cols: 30,
+						})
+					]
+				})
+			],
+			beginButton: new sap.m.Button({
+				text: "Submit",
+				type: sap.m.ButtonType.Accept,
+				press: function(oEvent) {
+					oDialog.setBusyIndicatorDelay(0);
+					oDialog.setBusy(true);
+					setTimeout(function() {
+						sap.m.MessageToast.show("Feedback sent.", {
+							duration: 2000,
+							my: sap.ui.core.Popup.Dock.CenterCenter,
+							at: sap.ui.core.Popup.Dock.CenterCenter,
+							of: oDialog,
+							onClose: function() {
+								oDialog.close();
+								oDialog.setBusy(false);
+							}
+						});
+					}, 2000);
+				}
+			}),
+			endButton: new sap.m.Button({
+				text: "Cancel",
+				type: sap.m.ButtonType.Reject,
+				press: function(oEvent) {
+					oDialog.close();
+				}
+			})
+		});
+		oDialog.open();
+	}
+});
