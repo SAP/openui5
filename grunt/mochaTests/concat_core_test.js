dfe5954fd4c52a5fd103acec2002edad6de6737b
Tobias Oetzel <tobias.oetzel@sap.com>
2014-10-15 12:45:26 +0200

    [INTERNAL] sap-ui-core.js gets concatinated when building
    - this enables to load the sap-ui-core.js async when referenced out of the target folder
    - also added sap-ui-core-noJQuery and sap-ui-debug.js
    
    Change-Id: Id0d8382d59aa29f464df11e9e9cefdfb00ff3db7

---
+++ grunt/mochaTests/concat_core_test.js
@@ -0,0 +1,34 @@
+/*eslint-env mocha */
+'use strict';
+
+var assert = require('assert');
+var grunt = require('grunt');
+
+describe('openui5_concat_core', function() {
+
+	it('should create the concatinated sap-ui-core.js', function() {
+
+		var coreJsContent = grunt.file.read('target/openui5/resources/sap-ui-core.js');
+		assert.ok(coreJsContent.indexOf('sap.ui.Device') !== -1, 'Did contain the Device api');
+		assert.ok(coreJsContent.indexOf('return jQuery;') !== -1, 'Did contain jQuery');
+		assert.ok(coreJsContent.indexOf('jQuery.sap.require("sap.ui.core.Core"); sap.ui.getCore().boot && sap.ui.getCore().boot();') !== -1, 'Did contain the bootstrap for core');
+
+	});
+
+	it('should create the concatinated sap-ui-coreNoJQuery.js', function() {
+
+		var coreNoQueryJsContent = grunt.file.read('target/openui5/resources/sap-ui-core-noJQuery.js');
+		assert.ok(coreNoQueryJsContent.indexOf('sap.ui.Device') !== -1, 'Did contain the Device api');
+		assert.ok(coreNoQueryJsContent.indexOf('return jQuery;') === -1, 'Did not contain jQuery');
+		assert.ok(coreNoQueryJsContent.indexOf('jQuery.sap.require("sap.ui.core.Core"); sap.ui.getCore().boot && sap.ui.getCore().boot();') !== -1, 'Did contain the bootstrap for core');
+
+	});
+
+	it('should create the sap-ui-debug.js', function() {
+
+		var sapUiDebugContent = grunt.file.read('target/openui5/resources/sap-ui-debug.js');
+		assert.ok(sapUiDebugContent.indexOf("sap.ui.define('sap/ui/debug/ControlTree'") !== -1, 'Did contain the Control Tree');
+		assert.ok(sapUiDebugContent.indexOf('jQuery.sap.require("sap.ui.core.Core"); sap.ui.getCore().boot && sap.ui.getCore().boot();') === -1, 'Did not contain the bootstrap for core');
+
+	});
+});
