5ed852d75596c539f36eb1af6de503bd96ae2078
Jiawei Cao <jiawei.cao@sap.com>
2014-10-17 16:37:46 +0200

    [Fix] sap.m.Input: Input suggestion shouldn't invalidate the popup when insert suggestion item on phone
    - When popup is invalidated, the currently focused input blurs and may
      close the opened on screen keyboard
    
    CSS: 0120031469 0000762005 2014
    
    Change-Id: I0d6d30ae2e94b259b776d3ee1731da61c9a76626

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -1154,7 +1154,20 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 			}
 	
 			if (oInput._oSuggestionPopup) {
-				oInput._oSuggestionPopup.addContent(oInput._oList);
+				if (sap.ui.Device.system.phone) {
+					// oInput._oList needs to be manually rendered otherwise it triggers a rerendering of the whole
+					// dialog and may close the opened on screen keyboard
+					oInput._oSuggestionPopup.addAggregation("content", oInput._oList, true);
+					var oRenderTarget = oInput._oSuggestionPopup.$("scrollCont")[0];
+					if (oRenderTarget) {
+						var rm = sap.ui.getCore().createRenderManager();
+						rm.renderControl(oInput._oList);
+						rm.flush(oRenderTarget);
+						rm.destroy();
+					}
+				} else {
+					oInput._oSuggestionPopup.addContent(oInput._oList);
+				}
 			}
 		}
 	
