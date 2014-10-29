a727fde21c145cbaaba9216f5e8c59881bfa8d8f
Krasimir Chariyski <krasimir.chariyski@sap.com>
2014-10-29 14:05:06 +0200

    [FIX] sap.m.Carousel: keyboard handling Change keyboard handling according latest spec
    Change-Id: I4a0d92160b0c55bda9c1307d0234035753f8de81

--- src/sap.m/src/sap/m/CarouselRenderer.js
+++ src/sap.m/src/sap/m/CarouselRenderer.js
@@ -1,22 +1,22 @@
 /*!
  * ${copyright}
  */
- 
+
 sap.ui.define(['jquery.sap.global'],
 	function(jQuery) {
 	"use strict";
 
 
 	/**
-	 * @class Carousel renderer. 
+	 * @class Carousel renderer.
 	 * @static
 	 */
 	var CarouselRenderer = {
 	};
-	
+
 	/**
 	 * Renders the Carousel's HTML, using the provided {@link sap.ui.core.RenderManager}.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager} oRm the RenderManager that can be used for writing to the render output buffer
 	 * @param {sap.ui.core.Control} oControl an object representation of the control that should be rendered
 	 */
@@ -25,48 +25,50 @@ sap.ui.define(['jquery.sap.global'],
 		if (!oCarousel.getVisible()) {
 			return;
 		}
-		
-	
+
+
 		//Outer carousel div
 		rm.write("<div");
 		rm.writeControlData(oCarousel);
-	
+		// custom F6 handling
+		rm.writeAttribute("data-sap-ui-customfastnavgroup", "true");
+
 		rm.addStyle("width", oCarousel.getWidth());
 		rm.addStyle("height", oCarousel.getHeight());
 		rm.writeStyles();
-		
+
 		rm.addClass("sapMCrsl");
 		//'sapMCrslFluid' is originally from mobify-carousel
 		rm.addClass("sapMCrslFluid");
-		
+
 		// add all classes (also custom classes) to carousel tag
 		rm.writeClasses();
-		
+
 		var sTooltip = oCarousel.getTooltip_AsString();
 		if (sTooltip) {
 			rm.writeAttributeEscaped("title", sTooltip);
 		}
-		
+
 		rm.writeAttributeEscaped("tabindex","0");
-		
+
 		rm.write(">");
-		
+
 		var aPages = oCarousel.getPages();
 		var iPageCount = aPages.length;
 		var sPageIndicatorPlacement = oCarousel.getShowPageIndicator() ?
 			oCarousel.getPageIndicatorPlacement() : null;
-		
-		
+
+
 		//visual indicator
 		if (sPageIndicatorPlacement === sap.m.PlacementType.Top) {
 			this._renderPageIndicator(rm, iPageCount);
 		}
-		
+
 		//inner carousel div
 		rm.write("<div class='sapMCrslInner'>");
 		//do housekeeping
 		oCarousel._cleanUpScrollContainer();
-		
+
 		var fnRenderPage = function(oPage, iIndex) {
 			//item div
 			rm.write("<div class='sapMCrslItem");
@@ -77,42 +79,42 @@ sap.ui.define(['jquery.sap.global'],
 				rm.renderControl(oCarousel._createScrollContainer(oPage, iIndex));
 			rm.write("</div>");
 		};
-		
+
 		//Render Pages
 		aPages.forEach(fnRenderPage);
-		
-		
+
+
 		rm.write("</div>");
 		//inner div ends
-		
-		
+
+
 		if (sap.ui.Device.system.desktop && iPageCount > 1) {
 			//heads up controls for desktop browsers
 			rm.write("<div class='sapMCrslControls sapMCrslHud'>");
 				rm.write("<a class='sapMCrslPrev' href='#' data-slide='prev' tabIndex='-1'><div class='sapMCrslHudInner'>");
 				rm.renderControl(oCarousel._getNavigationArrow('left'));
 				rm.write("</div></a>");
-				
+
 				rm.write("<a class='sapMCrslNext' href='#' data-slide='next' tabIndex='-1'><div class='sapMCrslHudInner'>");
 				rm.renderControl(oCarousel._getNavigationArrow('right'));
 				rm.write("</div></a>");
 			rm.write("</div>");
 		}
-		
-		
+
+
 		//visual indicator
 		if (sPageIndicatorPlacement === sap.m.PlacementType.Bottom) {
 			this._renderPageIndicator(rm, iPageCount, true);
 		}
 		rm.write("</div>");
-		
+
 		//page-wrap ends
 	};
-	
-	
+
+
 	/**
 	 * Renders the page indicator, using the provided {@link sap.ui.core.RenderManager}.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager} oRm the RenderManager that can be used for writing to the render output buffer
 	 * @param aPages array of controls to be rendered
 	 * @private
