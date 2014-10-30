d800510f5af9a464ca29d144c29a613ba7e64b71
Sebastian Ried <sebastian.ried@sap.com>
2014-10-30 09:39:23 +0100

    [FIX] jQuery: Performance of regular expressions in IE
    - Due to an issue in the IE regex engine the execution of of regex.test() takes very long for expressions where  patterns outside a group need to be combined with those inside a group
    
    Change-Id: I3ef145a3d4be0b43394d570649f7fb4097d13231

--- src/sap.ui.core/src/sap/ui/thirdparty/jquery/jquery-1.8.1.js
+++ src/sap.ui.core/src/sap/ui/thirdparty/jquery/jquery-1.8.1.js
@@ -5517,7 +5517,11 @@ var nodeNames = "abbr|article|aside|audio|bdi|canvas|data|datalist|details|figca
 	rtagName = /<([\w:]+)/,
 	rtbody = /<tbody/i,
 	rhtml = /<|&#?\w+;/,
-	rnoInnerhtml = /<(?:script|style|link)/i,
+	// ##### BEGIN: MODIFIED BY SAP
+	// This fix solves performance issues of the IE regex engine
+	// rnoInnerhtml = /<(?:script|style|link)/i,
+	rnoInnerhtml = /(?:<script|<style|<link)/i,
+	// ##### END: MODIFIED BY SAP
 	rnocache = /<(?:script|object|embed|option|style)/i,
 	rnoshimcache = new RegExp("<(?:" + nodeNames + ")[\\s/>]", "i"),
 	rcheckableType = /^(?:checkbox|radio)$/,
