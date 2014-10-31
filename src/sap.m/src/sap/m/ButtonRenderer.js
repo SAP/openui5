06afd4c09d0e93af4be796e5436125914126f208
Ralph Marshall <Ralph.Marshall@sas.com>
2014-10-31 14:19:58 -0400

    [FEATURE] sap.m.Button: add ARIA/screenreader support
    Added the ariaDescribedBy and ariaLabelledBy associations to the
    sap.m.Button control. Also updated the explored page for buttons to use
    the new associations. I added a new disabled button to the explored sample
    to ensure that bringing over the code from the desktop version of Button,
    which used a slightly different handling of the disabled property, still
    worked correctly.
    
    Fixed indentation and other whitespace
    
    Standardized all JSDoc to use @return instead of @returns
    
    Fixes https://github.com/SAP/openui5/issues/207

--- src/sap.m/src/sap/m/ButtonRenderer.js
+++ src/sap.m/src/sap/m/ButtonRenderer.js
@@ -3,112 +3,121 @@
  */
 
 sap.ui.define(['jquery.sap.global'],
+
 	function(jQuery) {
 	"use strict";
-
 
 	/**
 	 * @class Button renderer.
 	 * @static
 	 */
 	var ButtonRenderer = {};
-	
+
 	/**
 	 * Renders the HTML for the given control, using the provided
 	 * {@link sap.ui.core.RenderManager}.
 	 *
-	 * @param {sap.ui.core.RenderManager}
-	 *            oRenderManager the RenderManager that can be used for writing to
+	 * @param {sap.ui.core.RenderManager} oRm
+	 *            the RenderManager that can be used for writing to
 	 *            the Render-Output-Buffer
-	 * @param {sap.ui.core.Control}
-	 *            oControl an object representation of the control that should be
-	 *            rendered
+	 * @param {sap.ui.core.Control} oButton
+	 *            the button to be rendered
 	 */
 	ButtonRenderer.render = function(oRm, oButton) {
-	
+
 		// return immediately if control is invisible
 		if (!oButton.getVisible()) {
 			return;
 		}
-	
+
 		// get control properties
 		var sType = oButton.getType();
 		var bEnabled = oButton.getEnabled();
 		var sWidth = oButton.getWidth();
 		var sTooltip = oButton.getTooltip_AsString();
-	
+
 		// get icon from icon pool
 		var sBackURI = sap.ui.core.IconPool.getIconURI("nav-back");
-	
+
 		// start button tag
 		oRm.write("<button type=\"button\"");
 		oRm.writeControlData(oButton);
-	
+
 		// button container style class
 		if (!oButton._isUnstyled()) {
 			oRm.addClass("sapMBtn");
-	
+
 			// extend  minimum button size if icon is set without text for button types back and up
 			if ((sType === sap.m.ButtonType.Back || sType === sap.m.ButtonType.Up) && oButton.getIcon() && !oButton.getText()) {
 				oRm.addClass("sapMBtnBack");
 			}
 		}
-	
+
+		//ARIA attributes
+		oRm.writeAccessibilityState(oButton, {
+			role: 'button',
+			disabled: !oButton.getEnabled()
+		});
+
 		// check if the button is disabled
 		if (!bEnabled) {
+			oRm.writeAttribute("tabIndex", -1);
 			if (!oButton._isUnstyled()) {
 				oRm.addClass("sapMBtnDisabled");
 			}
-			oRm.writeAttribute("disabled", "disabled");
 		} else {
+			oRm.writeAttribute("tabIndex", 0);
 			switch (sType) {
-				case sap.m.ButtonType.Accept:
-				case sap.m.ButtonType.Reject:
-				case sap.m.ButtonType.Emphasized:
-					oRm.addClass("sapMBtnInverted");
+			case sap.m.ButtonType.Accept:
+			case sap.m.ButtonType.Reject:
+			case sap.m.ButtonType.Emphasized:
+				oRm.addClass("sapMBtnInverted");
+				break;
+			default: // No need to do anything for other button types
+				break;
 			}
 		}
-	
+
 		// add tooltip if available
 		if (sTooltip) {
 			oRm.writeAttributeEscaped("title", sTooltip);
 		}
-	
+
 		oRm.writeClasses();
-	
+
 		// set user defined width
 		if (sWidth != "" || sWidth.toLowerCase() === "auto") {
 			oRm.addStyle("width", sWidth);
 			oRm.writeStyles();
 		}
-	
+
 		// close button tag
 		oRm.write(">");
-	
+
 		// start inner button tag
 		oRm.write("<div");
 		oRm.writeAttribute("id", oButton.getId() + "-inner");
-	
+
 		// button style class
 		if (!oButton._isUnstyled()) {
 			oRm.addClass("sapMBtnInner");
 		}
-	
+
 		// check if button is hoverable
 		if (oButton._isHoverable()) {
 			oRm.addClass("sapMBtnHoverable");
 		}
-	
+
 		// check if button is focusable (not disabled)
 		if (bEnabled) {
 			oRm.addClass("sapMFocusable");
 		}
-	
+
 		//get render attributes of depended buttons (e.g. ToggleButton) 
 		if (this.renderButtonAttributes) {
 			this.renderButtonAttributes(oRm, oButton);
 		}
-	
+
 		// set padding depending on icons left or right or none
 		if (!oButton._isUnstyled()) {
 			if (!oButton.getIcon()) {
@@ -129,29 +138,29 @@ sap.ui.define(['jquery.sap.global'],
 				}
 			}
 		}
-	
+
 		// set button specific styles
 		if (!oButton._isUnstyled() && sType !== "") {
 			// set button specific styles
 			oRm.addClass("sapMBtn" + jQuery.sap.escapeHTML(sType));
 		}
-	
+
 		// add all classes to inner button tag
 		oRm.writeClasses();
-	
+
 		// close inner button tag
 		oRm.write(">");
-	
+
 		// set image for internal image control (back)
 		if (sType === sap.m.ButtonType.Back || sType === sap.m.ButtonType.Up) {
 			this.writeInternalIconPoolHtml(oRm, oButton, sBackURI);
 		}
-	
+
 		// write icon
 		if (oButton.getIcon()) {
 			this.writeImgHtml(oRm, oButton);
 		}
-	
+
 		// write button text
 		if (oButton.getText()) {
 			oRm.write("<span");
@@ -170,10 +179,8 @@ sap.ui.define(['jquery.sap.global'],
 					}
 					oRm.addClass("sapMBtnContentLeft");
 				}
-			} else {
-				if (sType === sap.m.ButtonType.Back || sType === sap.m.ButtonType.Up) {
-					oRm.addClass("sapMBtnContentRight");
-				}
+			} else if (sType === sap.m.ButtonType.Back || sType === sap.m.ButtonType.Up) {
+				oRm.addClass("sapMBtnContentRight");
 			}
 			oRm.writeClasses();
 			oRm.writeAttribute("id", oButton.getId() + "-content");
@@ -181,32 +188,41 @@ sap.ui.define(['jquery.sap.global'],
 			oRm.writeEscaped(oButton.getText());
 			oRm.write("</span>");
 		}
-	
+
 		// end inner button tag
 		oRm.write("</div>");
-	
+
 		// end button tag
 		oRm.write("</button>");
 	};
-	
-	
+
 	/**
 	 * HTML for image
 	 *
+	 * @param {sap.ui.core.RenderManager} oRm
+	 *            the RenderManager that can be used for writing to
+	 *            the Render-Output-Buffer
+	 * @param {sap.ui.core.Control} oButton
+	 *            the button to be rendered
 	 * @private
 	 */
 	ButtonRenderer.writeImgHtml = function(oRm, oButton) {
 		oRm.renderControl(oButton._getImage((oButton.getId() + "-img"), oButton.getIcon(), oButton.getActiveIcon(), oButton.getIconDensityAware()));
 	};
-	
-	
+
 	/**
+	 * @param {sap.ui.core.RenderManager} oRm
+	 *	      the RenderManager that can be used for writing to
+	 *	      the Render-Output-Buffer
+	 * @param {sap.ui.core.Control} oButton
+	 *	      the button to be rendered
+	 * @param {sap.ui.core.URI} sURI
+	 *            URI of the icon to be written
 	 * HTML for internal image (icon pool)
 	 */
 	ButtonRenderer.writeInternalIconPoolHtml = function(oRm, oButton, sURI) {
 		oRm.renderControl(oButton._getInternalIconBtn((oButton.getId() + "-iconBtn"), sURI));
 	};
-	
 
 	return ButtonRenderer;
 
