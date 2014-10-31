d8124a34d20c0545cdecc9b7671137acbafe5e33
Christiane Kurz <christiane.kurz@sap.com>
2014-10-31 10:08:00 +0100

    [FIX] ODataModel - correct error handling for metadata requests
    When a new model tries to access shared metadata from the metadata
    cache, and finds metadata which is broken because of a previously failed
    request, it will now automatically trigger a refresh. When this happens,
    and the request fails again, the subsequently created model will hence
    also receive a metadataFailed event and can react accordingly.
    
    Change-Id: Ide27d27d85c55ba27637fdf9dd8037fd9d87510f

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -174,6 +174,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 				this.oMetadata.attachFailed(this.fireMetadataFailed, this);
 			}
 
+			if (this.oMetadata.isFailed()){
+				this.refreshMetadata();
+			}
+
 			if (this.sAnnotationURI) {
 				this.oAnnotations = new sap.ui.model.odata.ODataAnnotations(this.sAnnotationURI, this.oMetadata, { async: this.bLoadMetadataAsync });
 				this.oAnnotations.attachFailed(this.fireAnnotationsFailed, this);
@@ -212,7 +216,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 		},
 		metadata : {
 			publicMethods : ["read", "create", "update", "remove", "submitChanges", "getServiceMetadata",
-			                 "hasPendingChanges", "refresh", "resetChanges", "setDefaultCountMode",
+			                 "hasPendingChanges", "refresh", "refreshMetadata", "resetChanges", "setDefaultCountMode",
 			                 "setDefaultBindingMode", "getDefaultBindingMode", "getDefaultCountMode",
 			                 "setProperty", "getSecurityToken", "refreshSecurityToken", "setHeaders",
 			                 "getHeaders", "setUseBatch", "setDeferredBatchGroups", "getDeferredBatchGroups",
@@ -320,6 +324,19 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 			// In case of synchronous or asynchronous non-joined loading, or if no annotations are
 			// loaded at all, the events are fired individually
 			doFire(this.bLoadMetadataAsync, bDelayEvent);
+		}
+	};
+
+	/**
+	 * refreshes the metadata for model, e.g. in case the first request for metadata has failed 
+	 *
+	 * @public
+	 * @name sap.ui.model.odata.ODataModel#refreshMetadata
+	 * @function
+	 */
+	ODataModel.prototype.refreshMetadata = function(){
+		if (this.oMetadata && this.oMetadata.refresh){
+			this.oMetadata.refresh();
 		}
 	};
 
