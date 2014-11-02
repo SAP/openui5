a818d7bbee168206d6e9d3ce552c3a3e0b555b8f
Jens Himmelrath <jens.himmelrath@sap.com>
2014-11-02 14:39:45 +0100

    [FEATURE] m/ProgressIndicator: Visible property moved to Control
    Depends on: 89eb1f29ccafb57e48257aadc192181aa09bae13
    
    Change-Id: Id0793faf43a8fac5dcf7ff40dd46229ea08fb537

--- src/sap.m/src/sap/m/ProgressIndicator.js
+++ src/sap.m/src/sap/m/ProgressIndicator.js
@@ -33,12 +33,6 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	
 		library : "sap.m",
 		properties : {
-	
-			/**
-			 * Invisible controls are not rendered
-			 */
-			visible : {type : "boolean", group : "Behavior", defaultValue : true},
-	
 			/**
 			 * Switches enabled state of the control. Disabled fields have different colors, and can not be focused.
 			 */
