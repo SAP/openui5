3c09aa2bba17e8630a3062de8530547ef7496b1c
Patric Ksinsik <p.ksinsik@sap.com>
2014-11-03 15:50:30 +0100

    [INTERNAL] Templating sample app, part2
    - <repeat> over Identification fields
    - entity type selection
    - use element binding instead of createBindingContext for data model
    - use v2 of merged service metadata from
    http://vecocomst.dhcp.wdf.sap.corp:1080/coco/tools/MetannoJ/
    
    PS1: PP with KTH and PKS solo
    PS2: Comment KTH
    PS3: Rebase
    PS4: Rebase again onto encoding fix for ODataHelper
    (https://git.wdf.sap.corp:8080/681421/)
    
    Change-Id: I618e197416c7706a920a327e731b22e978ed1214
    JIRA: FLPUISERVICES2014-844

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/ViewTemplate/scenario/Main.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/ViewTemplate/scenario/Main.controller.js
@@ -3,30 +3,81 @@
  */
 
 sap.ui.controller("sap.ui.core.sample.ViewTemplate.scenario.Main", {
+	onInit: function () {
+		var oEntityTypes = {
+				selectedKey: "Product",
+				types: [
+					{
+						key: "BusinessPartner",
+						target: "GWSAMPLE_BASIC.BusinessPartner",
+						instance: "/BusinessPartnerSet('0100000001')"
+					},
+					{
+						key: "Contact",
+						target: "GWSAMPLE_BASIC.Contact",
+						instance: "/ContactSet(guid'0050568D-393C-1EE4-9882-CEC33E1510CD')"
+					},
+					{
+						key: "Product",
+						target: "GWSAMPLE_BASIC.Product",
+						instance: "/ProductSet('HT-1000')"
+					},
+					{
+						key: "SalesOrder",
+						target: "GWSAMPLE_BASIC.SalesOrder",
+						instance: "/SalesOrderSet('0500000000')"
+					},
+					{
+						key: "SalesOrderLineItem",
+						target: "GWSAMPLE_BASIC.SalesOrderLineItem",
+						instance: "/SalesOrderSet('0500000000')"
+					}
+				]
+			};
+
+		this.getView().setModel(new sap.ui.model.json.JSONModel(oEntityTypes), "ui");
+	},
+
 	//TODO clarify with UI5 Core: why can view models not be accessed in onInit?
 	onBeforeRendering: function () {
-		var oModel = this.getView().getModel(),
-			that = this;
+		this._showDetails();
+	},
 
-		oModel.createBindingContext("/ProductSet('HT-1000')", null, null, function (oContext) {
-			var sANNO_PATH = "/schemas/GWSAMPLE_BASIC/annotations",
-				oMetaContext,
-				oMetaModel = that.getView().getModel("meta"),
-				aAnnotations = oMetaModel.getObject(sANNO_PATH),
-				i;
+	onChangeType: function (oEvent) {
+		this._showDetails();
+	},
 
-			for (i = 0; i < aAnnotations.length; i += 1) {
-				if (aAnnotations[i].target === "GWSAMPLE_BASIC.Product") {
-					oMetaContext = oMetaModel.createBindingContext(sANNO_PATH + "/" + i);
-					break;
-				}
+	_showDetails: function () {
+		var sANNO_PATH = "/schemas/GWSAMPLE_BASIC/annotations",
+			oDetailView,
+			oView = this.getView(),
+			oMetaContext,
+			oMetaModel = oView.getModel("meta"),
+			aAnnotations = oMetaModel.getObject(sANNO_PATH),
+			oModel = oView.getModel(),
+			aTypes = oView.getModel("ui").getProperty("/types"),
+			oType,
+			i;
+
+		for (i = 0; i < aTypes.length; i += 1) {
+			if (aTypes[i].key === oView.getModel("ui").getProperty("/selectedKey")) {
+				oType = aTypes[i];
+				break;
 			}
-			that.getView().byId("detail").destroyItems().addItem(sap.ui.view({
-				type: sap.ui.core.mvc.ViewType.XML,
-				viewName: "sap.ui.core.sample.ViewTemplate.scenario.Detail",
-				bindingContexts: {undefined: oContext, meta: oMetaContext},
-				models: {undefined: oModel, meta: oMetaModel}
-			}));
+		}
+		for (i = 0; i < aAnnotations.length; i += 1) {
+			if (aAnnotations[i].target === oType.target) {
+				oMetaContext = oMetaModel.createBindingContext(sANNO_PATH + "/" + i);
+				break;
+			}
+		}
+		oDetailView = sap.ui.view({
+			type: sap.ui.core.mvc.ViewType.XML,
+			viewName: "sap.ui.core.sample.ViewTemplate.scenario.Detail",
+			bindingContexts: {meta: oMetaContext},
+			models: {meta: oMetaModel}
 		});
+		oDetailView.bindElement(oType.instance, {models: oModel});
+		oView.byId("detail").destroyItems().addItem(oDetailView);
 	}
 });
