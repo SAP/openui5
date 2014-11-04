fa33b2ad5d851c5076ed97a695d2ccd9d079238c
Siarhei Ulasenka <siarhei.ulasenka@sap.com>
2014-11-04 13:27:00 +0100

    [FIX] sap.m.SearchField: wrong tooltip is shown over the search icon
    - tooltip of the refresh button is shown over the search icon
      on a focused search field
    
    Change-Id: Iddacd70456374f7ca07521d0d7bdc9ad3752e255

--- src/sap.m/src/sap/m/SearchField.js
+++ src/sap.m/src/sap/m/SearchField.js
@@ -381,6 +381,10 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	
 		this.$().toggleClass("sapMFocus", true);
 	
+		// clear tooltip of the refresh button
+		if (this.getShowRefreshButton()) {
+			this.$("search").removeAttr("title");
+		} 
 		// Some applications do re-render during the liveSearch event.
 		// The input is focused and most browsers select the input text for copy.
 		// Any following key press deletes the whole selection.
@@ -397,7 +401,17 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	 * @private
 	 */
 	SearchField.prototype.onBlur = function(oEvent) {
+		var tooltip;
+		
 		this.$().toggleClass("sapMFocus", false);
+		
+		// restore toltip of the refresh button
+		if (this.getShowRefreshButton()) {
+			tooltip = this.getRefreshButtonTooltip();
+			if (tooltip) {
+				this.$("search").attr("title", tooltip);
+			}
+		} 
 	};
 	
 	SearchField.prototype.setValue = function(value){
