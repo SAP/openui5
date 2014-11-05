2735e5b570c6555be355a9a527f7d8d5022e304a
Axel Warner <axel.warner@sap.com>
2014-11-05 11:30:56 +0100

    [INTERNAL] P13nColumnsPanel: change internal logic
    - new aggregation: columnsItems
    - change logic for creation of table items
    - free of ESLint errors
    
    Change-Id: I04e7c287d9eda54ee2e093fbfc6e748a91b02ebd

---
+++ src/sap.m/src/sap/m/P13nColumnsItem.js
@@ -0,0 +1,58 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides control sap.m.P13nColumnsItem.
+sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Item'],
+	function(jQuery, library, Item) {
+	"use strict";
+
+
+	
+	/**
+	 * Constructor for a new P13nColumnsItem.
+	 *
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
+	 * @param {object} [mSettings] initial settings for the new control
+	 *
+	 * @class
+	 * tbd
+	 * @extends sap.ui.core.Item
+	 * @version ${version}
+	 *
+	 * @constructor
+	 * @public
+	 * @name sap.m.P13nColumnsItem
+	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
+	 */
+	var P13nColumnsItem = Item.extend("sap.m.P13nColumnsItem", /** @lends sap.m.P13nColumnsItem.prototype */ { metadata : {
+	
+		library : "sap.m",
+		properties : {
+
+			/**
+			 *  This property contains the unique table column key
+			 */
+			columnKey : {type : "string", group : "Misc"}, //don't set a default value
+			
+			/**
+			 * This property contains the index of a table column
+			 */
+			index : {type : "int", group : "Appearance"}, //don't set a default value
+			
+			/**
+			 * This property decides whether a P13nColumnsItem is visible
+			 */
+			visible : {type : "boolean", group : "Appearance"},  //don't set a default value
+
+			/**
+			 *  This property contains the with of a table column.
+			 */
+			width : {type : "string", group : "Misc"} //don't set a default value
+
+		}
+	}});
+	
+	return P13nColumnsItem;
+
+}, /* bExport= */ true);
