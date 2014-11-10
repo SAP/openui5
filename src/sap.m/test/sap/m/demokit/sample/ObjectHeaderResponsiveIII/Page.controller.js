d89822ec1c3f4f9423cdf3d82f359b5b0bab7bf7
Gergana Marcheva <gergana.marcheva@sap.com>
2014-11-10 10:11:39 +0200

    [FEATURE] sap.m.ObjectHeader: Implementation of the new design specification of Responsive Object Header
    Change-Id: Iab692e9752c7ccb3b4af3e4b10486be830543067

--- src/sap.m/test/sap/m/demokit/sample/ObjectHeaderResponsiveIII/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/ObjectHeaderResponsiveIII/Page.controller.js
@@ -4,9 +4,22 @@ sap.ui.controller("sap.m.sample.ObjectHeaderResponsiveIII.Page", {
 		var oModel = new sap.ui.model.json.JSONModel("test-resources/sap/ui/demokit/explored/products.json");
 		this.getView().setModel(oModel);
 	},
-
-	onPress: function (evt) {
-		jQuery.sap.require("sap.m.MessageBox");
-		sap.m.MessageBox.alert("Link was clicked!");
+	
+	onExit : function () {
+		if (this._oPopover) {
+			this._oPopover.destroy();
+		}
 	},
+
+	_getPopover : function () {
+		if (!this._oPopover) {
+			this._oPopover = sap.ui.xmlfragment("sap.m.sample.ObjectHeaderResponsiveIII.Popover", this);
+		}
+		return this._oPopover;
+	},
+
+	handleTitlePress : function (oEvent) {
+		var domRef = oEvent.getParameter("domRef");
+		this._getPopover().openBy(domRef);
+	}
 });
