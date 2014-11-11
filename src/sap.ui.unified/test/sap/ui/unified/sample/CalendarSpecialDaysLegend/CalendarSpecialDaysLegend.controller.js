106a3c693637a97b80cce01671af36e48c69e72c
Mihail Yordanov <mihail.yordanov@sap.com>
2014-11-11 15:40:12 +0300

    [INTERNAL][FIX] sap.m.DatePicker, sap.ui.unified.Calendar
    -Fixes in DatePicker and Calendar
    -Added CalendarLegend HCB styles
    -Modified CalendarSpecialDays sample
    
    Change-Id: I80c360b72e805bf5769b787f2d4a8e6695fa4884

--- src/sap.ui.unified/test/sap/ui/unified/sample/CalendarSpecialDaysLegend/CalendarSpecialDaysLegend.controller.js
+++ src/sap.ui.unified/test/sap/ui/unified/sample/CalendarSpecialDaysLegend/CalendarSpecialDaysLegend.controller.js
@@ -1,8 +1,10 @@
 sap.ui.controller("sap.ui.unified.sample.CalendarSpecialDaysLegend.CalendarSpecialDaysLegend", {
 
 	handleShowSpecialDays: function(oEvent) {	
-		var oCal = this.getView().byId("calendar");
-		var oLeg = this.getView().byId("legend");
+		var oCal1 = this.getView().byId("calendar1");
+		var oLeg1 = this.getView().byId("legend1");
+		var oCal2 = this.getView().byId("calendar2");
+		var oLeg2 = this.getView().byId("legend2");
 		var bPressed = oEvent.getParameter("pressed");
 		if (bPressed) {
 			var oRefDate = new Date();
@@ -14,18 +16,28 @@ sap.ui.controller("sap.ui.unified.sample.CalendarSpecialDaysLegend.CalendarSpeci
 				} else {
 					sType = "Type" + i;
 				}
-				oCal.addSpecialDate(new sap.ui.unified.DateTypeRange({
+				oCal1.addSpecialDate(new sap.ui.unified.DateTypeRange({
 					startDate : new Date(oRefDate),
 					type : sType,
 					tooltip : "Placeholder " + i
 				}));
-				oLeg.addItem(new sap.ui.unified.CalendarLegendItem({
+				oCal2.addSpecialDate(new sap.ui.unified.DateTypeRange({
+					startDate : new Date(oRefDate),
+					type : sType,
+					tooltip : "Placeholder " + i
+				}));
+				oLeg1.addItem(new sap.ui.unified.CalendarLegendItem({
+					text : "Placeholder " + i
+				}));
+				oLeg2.addItem(new sap.ui.unified.CalendarLegendItem({
 					text : "Placeholder " + i
 				}));
 			}
 		} else {
-			oCal.destroySpecialDates();
-			oLeg.destroyItems();
+			oCal1.destroySpecialDates();
+			oCal2.destroySpecialDates();
+			oLeg1.destroyItems();
+			oLeg2.destroyItems();
 		}
 	}
 });
