893950ec6be19c18b95c48c56e48a6b2d88e335b
Adrian Bobev <adrian.bobev@sap.com>
2014-11-12 15:29:29 +0200

    [INTERNAL] sap.m.Panel: Remove whitespaces
    Change-Id: I25c30f71c068318d333c78fc6b340e45208ff33d

--- src/sap.m/src/sap/m/PanelRenderer.js
+++ src/sap.m/src/sap/m/PanelRenderer.js
@@ -5,16 +5,15 @@ sap.ui.define(['jquery.sap.global'],
 	function(jQuery) {
 	"use strict";
 
-
 	/**
 	 * @class Panel renderer
 	 * @static
 	 */
 	var PanelRenderer = {};
-	
+
 	/**
 	 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager}
 	 *          oRm the RenderManager that can be used for writing to the render output buffer
 	 * @param {sap.ui.core.Control}
@@ -25,7 +24,7 @@ sap.ui.define(['jquery.sap.global'],
 		if (!oControl.getVisible()) {
 			return;
 		}
-	
+
 		// start Panel
 		oRm.write("<section");
 		oRm.writeControlData(oControl);
@@ -35,15 +34,15 @@ sap.ui.define(['jquery.sap.global'],
 		oRm.writeClasses();
 		oRm.writeStyles();
 		oRm.write(">");
-	
+
 		var bIsExpandable = oControl.getExpandable();
 		var oHeaderTBar = oControl.getHeaderToolbar();
-	
+
 		if (bIsExpandable) {
-	
-			// we need a wrapping div around icon and header since otherwise the border needed for both do not exact align 
+
+			// we need a wrapping div around icon and header since otherwise the border needed for both do not exact align
 			oRm.write("<div");
-			
+
 			if (oHeaderTBar) {
 			// we are in the toolbar case - since
 			oRm.addClass("sapMPanelWrappingDivTb");
@@ -52,7 +51,7 @@ sap.ui.define(['jquery.sap.global'],
 			}
 			oRm.writeClasses();
 			oRm.write(">");
-	
+
 			var oIcon = oControl._getIcon();
 			if (oControl.getExpanded()) {
 				oIcon.addStyleClass("sapMPanelExpandableIconExpanded");
@@ -61,20 +60,20 @@ sap.ui.define(['jquery.sap.global'],
 			}
 			oRm.renderControl(oIcon);
 		}
-	
+
 		// render header
 		var sHeaderText = oControl.getHeaderText();
-	
-	
+
+
 		if (oHeaderTBar) {
 			oHeaderTBar.setDesign(sap.m.ToolbarDesign.Transparent, true);
-	
+
 			if (oControl.getExpandable()) {
 				// use this class as marker class - to ease selection later in onAfterRendering
 				oHeaderTBar.addStyleClass("sapMPanelHdrExpandable");
 			}
 			oRm.renderControl(oHeaderTBar);
-	
+
 		} else if (sHeaderText) {
 			oRm.write("<div");
 			oRm.addClass("sapMPanelHdr");
@@ -82,40 +81,40 @@ sap.ui.define(['jquery.sap.global'],
 				// use this class as marker class - to ease selection later in onAfterRendering
 				oRm.addClass("sapMPanelHdrExpandable");
 			}
-	
+
 			oRm.writeClasses();
 			oRm.write(">");
-	
+
 			oRm.writeEscaped(sHeaderText);
 			oRm.write("</div>");
 		}
-	
+
 		if (bIsExpandable) {
 			oRm.write("</div>");
 		}
-	
+
 		var oInfoTBar = oControl.getInfoToolbar();
 		if (oInfoTBar && oControl.getExpandable()) {
 			// use this class as marker class - to ease selection later in onAfterRendering
 			oInfoTBar.addStyleClass("sapMPanelExpandablePart");
 		}
-	
+
 		// render infoBar
 		if (oInfoTBar) {
 			oInfoTBar.setDesign(sap.m.ToolbarDesign.Info, true);
 			oRm.renderControl(oInfoTBar);
 		}
-	
+
 		// render content
 		oRm.write("<div");
 		oRm.addClass("sapMPanelContent");
 		oRm.addClass("sapMPanelBG");
-	
+
 		if (oControl.getExpandable()) {
 			// use this class as marker class - to ease selection later in onAfterRendering
 			oRm.addClass("sapMPanelExpandablePart");
 		}
-	
+
 		oRm.writeClasses();
 		oRm.write(">");
 		var aChildren = oControl.getContent();
@@ -124,7 +123,7 @@ sap.ui.define(['jquery.sap.global'],
 			oRm.renderControl(aChildren[i]);
 		}
 		oRm.write("</div>");
-	
+
 		oRm.write("</section>");
 	};
 
