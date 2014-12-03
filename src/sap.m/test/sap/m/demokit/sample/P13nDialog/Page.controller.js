38c84d6b6cfd412f629d992ddbb96012f1652025
Eugen Resch <eugen.resch@sap.com>
2014-12-03 13:14:37 +0100

    [FIX] sap.m.P13nDialog - explored example fixed
    Change-Id: Ic93f53a18083374f952bdbb3b066e07af9760c33
    CSS: 1482017941

--- src/sap.m/test/sap/m/demokit/sample/P13nDialog/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/P13nDialog/Page.controller.js
@@ -1,5 +1,7 @@
 sap.ui.controller("sap.m.sample.P13nDialog.Page", {
 
+	oPersonalizationDialog : null,
+
 	onInit : function() {
 		// set explored app's demo model on this sample
 		this.getView().setModel(
@@ -7,14 +9,33 @@ sap.ui.controller("sap.m.sample.P13nDialog.Page", {
 	},
 
 	handleClose : function(oEvent) {
-		sap.m.MessageToast.show("Close button has been clicked", {
-			width : "auto"
-		});
+		oPersonalizationDialog.close();
 	},
 
 	handleReset : function(oEvent) {
 		sap.m.MessageToast.show("Reset button has been clicked", {
 			width : "auto"
 		});
+	},
+
+	openDialog : function() {
+		// associate controller with the fragment
+		oPersonalizationDialog = sap.ui.xmlfragment("sap.m.sample.P13nDialog.PersonalizationDialog", this);
+		this.getView().addDependent(oPersonalizationDialog);
+
+		// toggle compact style
+		jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), oPersonalizationDialog);
+		oPersonalizationDialog.open();
+	},
+	
+	onPersonalizationDialogPress : function(oEvent) {
+		this.openDialog();
+	},
+	
+	onAddColumnsItem : function(oEvent){
+		sap.m.MessageToast.show("Event 'addColumnsItem' fired in order to move the selected column item", {
+			width : "auto"
+		});
 	}
+	
 });
