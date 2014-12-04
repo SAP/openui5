7e98f14b9401b2c89656b4e6592f11ac7551817c
Sebastian Allmann <sebastian.allmann@sap.com>
2014-12-04 13:50:44 +0100

    [FIX] DatePicker / DateRangeSelection - several issues
    - possible to select dates < 0001-01-01 or > 9999-12-31 what brings
    syntax errors
    - errors if such dates are provided by setters
    - if invalid date was types in and then the last one is choosen via
    calendar the value is not updated
    - on mobile devices calendar opens after DatePicker was rerendered.
    
    Change-Id: I5a584a66d14d0561a35bd0b6e082c88b7acac412
    BCP: 0020283765 0000111804 2014
    BCP: 1472003463
    BCP: 1482027948
    BCP: 1482024076

--- src/sap.ui.unified/src/sap/ui/unified/DateRange.js
+++ src/sap.ui.unified/src/sap/ui/unified/DateRange.js
@@ -2,13 +2,13 @@
  * ${copyright}
  */
 
-// Provides control sap.ui.unified.DateRange.
+//Provides control sap.ui.unified.DateRange.
 sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
-	function(jQuery, Element, library) {
+		function(jQuery, Element, library) {
 	"use strict";
 
 
-	
+
 	/**
 	 * Constructor for a new DateRange.
 	 *
@@ -27,40 +27,48 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', './library'],
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var DateRange = Element.extend("sap.ui.unified.DateRange", /** @lends sap.ui.unified.DateRange.prototype */ { metadata : {
-	
+
 		library : "sap.ui.unified",
 		properties : {
-	
+
 			/**
 			 * Start date for a date range. This must be a JavaScript date object.
 			 */
 			startDate : {type : "object", group : "Misc", defaultValue : null},
-	
+
 			/**
 			 * Start date for a date range. If empty only a single date is presented by this DateRange element. This must be a JavaScript date object.
 			 */
 			endDate : {type : "object", group : "Misc", defaultValue : null}
 		}
 	}});
-	
+
 	///**
 	// * This file defines behavior for the control,
 	// */
-	
+
 	DateRange.prototype.setStartDate = function(oDate){
-	
+
 		jQuery.sap.assert(!oDate || oDate instanceof Date, "Date must be a JavaScript date object");
-	
+		if (oDate) {
+			var iYear = oDate.getFullYear();
+			jQuery.sap.assert(iYear <= 9999 && iYear >= 1, "Date must not be in valid range (between 0001-01-01 and 9999-12-31)");
+		}
+
 		this.setProperty("startDate", oDate);
-	
+
 	};
-	
+
 	DateRange.prototype.setEndDate = function(oDate){
-	
+
 		jQuery.sap.assert(!oDate || oDate instanceof Date, "Date must be a JavaScript date object");
-	
+		if (oDate) {
+			var iYear = oDate.getFullYear();
+			jQuery.sap.assert(iYear <= 9999 && iYear >= 1, "Date must not be in valid range (between 0001-01-01 and 9999-12-31)");
+		}
+
 		this.setProperty("endDate", oDate);
-	
+
 	};
 
 	return DateRange;
