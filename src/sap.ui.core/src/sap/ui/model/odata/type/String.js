4ac06c864424a7e2e340325e031142ed97eee8a6
Knut Thies <knut.thies@sap.com>
2014-12-04 16:54:43 +0100

    [INTERNAL] sap.ui.model.odata.type.String supports nullable
    PS1: PP Thomas, Knut
    PS2: fix AnnotationHelper test; comments by TCH
    PS3: Remove @function tag
    PS4: Change @return to @returns
    
    Change-Id: I51efc7b413d8f62e8b123542ccf75a4b6407b30e
    JIRA: FLPUISERVICES2014-1053

--- src/sap.ui.core/src/sap/ui/model/odata/type/String.js
+++ src/sap.ui.core/src/sap/ui/model/odata/type/String.js
@@ -46,7 +46,7 @@ sap.ui.define(['sap/ui/model/FormatException',
 	 *   the value to be formatted
 	 * @param {string} sTargetType
 	 *   the target type
-	 * @return {string|number|boolean}
+	 * @returns {string|number|boolean}
 	 *   the formatted output value in the target type; <code>undefined</code> or <code>null</code>
 	 *   will be formatted to <code>null</code>
 	 * @throws sap.ui.model.FormatException
@@ -59,30 +59,38 @@ sap.ui.define(['sap/ui/model/FormatException',
 	/**
 	 * Parse the given value which is expected to be of the given type to a string.
 	 *
-	 * @function
 	 * @param {string|number|boolean} vValue
-	 *   the value to be parsed
+	 *   the value to be parsed, maps <code>""</code> to <code>null</code>
 	 * @param {string} sSourceType
 	 *   the source type (the expected type of <code>sValue</code>)
-	 * @return {string}
+	 * @returns {string}
 	 *   the parsed value
 	 * @throws sap.ui.model.ParseException
 	 *   if <code>sSourceType</code> is unsupported
 	 * @public
 	 */
-	EdmString.prototype.parseValue = StringType.prototype.parseValue;
+	EdmString.prototype.parseValue = function (vValue, sSourceType) {
+		return vValue === "" ? null : StringType.prototype.parseValue.apply(this, arguments);
+	};
 
 	/**
 	 * Validate whether the given value in model representation is valid and meets the
 	 * defined constraints (if any).
 	 *
-	 * @function
 	 * @param {string} sValue
 	 *   the value to be validated
 	 * @throws sap.ui.model.ValidateException if the value is not valid
 	 * @public
 	 */
-	EdmString.prototype.validateValue = StringType.prototype.validateValue;
+	EdmString.prototype.validateValue = function (sValue) {
+		if (sValue === null) {
+			if (this.oConstraints.nullable === false) {
+				throw new ValidateException("Illegal " + this.sName + " value: " + sValue);
+			}
+		} else {
+			StringType.prototype.validateValue.apply(this, arguments);
+		}
+	};
 
 	/**
 	 * Set the constraints.
@@ -91,19 +99,31 @@ sap.ui.define(['sap/ui/model/FormatException',
 	 * 	 constraints
 	 * @param {int} [oConstraints.maxLength]
 	 *   the maximal allowed length of the string; unlimited if not defined
+	 * @param {boolean} [oConstraints.nullable=true]
+	 *   if <code>true</code>, the value <code>null</code> will be accepted; note that
+	 *   {@link #parseValue} maps <code>""</code> to <code>null</code>
 	 * @public
 	 */
 	EdmString.prototype.setConstraints = function(oConstraints) {
 		var iMaxLength;
 
 		this.oConstraints = {};
-		if (oConstraints && oConstraints.hasOwnProperty("maxLength")) {
+		if (oConstraints) {
 			iMaxLength = oConstraints.maxLength;
 			if (typeof iMaxLength === "number" && iMaxLength > 0) {
 				this.oConstraints.maxLength = iMaxLength;
-			} else {
+			} else if (iMaxLength !== undefined) {
 				jQuery.sap.log.warning("Illegal maxLength: " + iMaxLength,
 					null, "sap.ui.model.odata.type.String");
+			}
+
+			if (oConstraints.nullable === false) {
+				this.oConstraints.nullable = false;
+			} else if (oConstraints.nullable !== undefined
+					&& typeof oConstraints.nullable !== "boolean") {
+				// Note: setConstraints is called by the super constructor w/ wrong this.sName
+				jQuery.sap.log.warning("Illegal nullable: " + oConstraints.nullable, null,
+					"sap.ui.model.odata.type.String");
 			}
 		}
 	};
