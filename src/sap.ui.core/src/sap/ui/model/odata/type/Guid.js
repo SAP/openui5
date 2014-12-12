adfb64bac72565422719156b7b132c5aa449694f
Sven Bernhard <sven.bernhard@sap.com>
2014-12-12 15:02:07 +0100

    [INTERNAL] sap.ui.model.odata.type.Guid: Implementation
    PS1: PP HMU and SBE
    PS2: Comments from TCH and URE, rebase
    
    Change-Id: I02c7a41264b61980c899d68b222971f9da1c5f7e
    JIRA: FLPUISERVICES2014-984

---
+++ src/sap.ui.core/src/sap/ui/model/odata/type/Guid.js
@@ -0,0 +1,167 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define(['sap/ui/model/FormatException', 'sap/ui/model/ParseException',
+		'sap/ui/model/SimpleType', 'sap/ui/model/ValidateException'],
+	function(FormatException, ParseException, SimpleType, ValidateException) {
+	"use strict";
+
+	var rGuid = /^[A-F0-9]{8}-([A-F0-9]{4}-){3}[A-F0-9]{12}$/i;
+
+	/**
+	 * Returns the locale-dependent error message.
+	 *
+	 * @returns {string}
+	 *   the locale-dependent error message.
+	 * @private
+	 */
+	function getErrorMessage() {
+		return sap.ui.getCore().getLibraryResourceBundle().getText("EnterGuid");
+	}
+
+	/**
+	 * Constructor for an OData primitive type <code>Edm.Guid</code>.
+	 *
+	 * @class This class represents the OData primitive type <a
+	 * href="http://www.odata.org/documentation/odata-version-2-0/overview#AbstractTypeSystem">
+	 * <code>Edm.Guid</code></a>.
+	 *
+	 * @extends sap.ui.model.SimpleType
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @alias sap.ui.model.odata.type.Guid
+	 * @param {object} [oFormatOptions]
+	 *   format options; no format options supported
+	 * @param {object} [oConstraints]
+	 *   constraints, see {@link #setConstraints}
+	 * @public
+	 * @since 1.27.0
+	 */
+	var EdmGuid = SimpleType.extend("sap.ui.model.odata.type.Guid",
+			/** @lends sap.ui.model.odata.type.Guid.prototype */
+			{
+				constructor : function () {
+					SimpleType.apply(this, arguments);
+			}
+		});
+
+	/**
+	 * Formats the given value to the given target type
+	 *
+	 * @param {string} sValue
+	 *   the value to be formatted
+	 * @param {string} sTargetType
+	 *   the target type
+	 * @returns {string}
+	 *   the formatted output value in the target type; <code>undefined</code> or <code>null</code>
+	 *   is formatted to <code>null</code>
+	 * @throws sap.ui.model.FormatException
+	 *   if <code>sTargetType</code> is unsupported
+	 * @public
+	 */
+	EdmGuid.prototype.formatValue = function(sValue, sTargetType) {
+		if (sValue == undefined || sValue == null) {
+			return null;
+		}
+		if (sTargetType === "string" || sTargetType === "any") {
+			return sValue;
+		}
+		throw new FormatException("Don't know how to format " + this.getName() + " to "
+			+ sTargetType);
+	};
+
+	/**
+	 * Returns the type's name.
+	 *
+	 * @returns {String}
+	 *   the type's name
+	 * @public
+	 */
+	EdmGuid.prototype.getName = function () {
+		return "sap.ui.model.odata.type.Guid";
+	};
+
+	/**
+	 * Parses the given value, which is expected to be of the given type, to a GUID.
+	 *
+	 * @param {string} sValue
+	 *   the value to be parsed, maps <code>""</code> to <code>null</code>
+	 * @param {string} sSourceType
+	 *   the source type (the expected type of <code>sValue</code>)
+	 * @returns {string}
+	 *   the parsed value
+	 * @throws sap.ui.model.ParseException
+	 *   if <code>sSourceType</code> is unsupported
+	 * @public
+	 */
+	EdmGuid.prototype.parseValue = function (sValue, sSourceType) {
+		var sResult;
+		if (sValue === "" || sValue === null) {
+			return null;
+		}
+		if (sSourceType !== "string") {
+			throw new ParseException("Don't know how to parse " + this.getName() + " from "
+				+ sSourceType);
+		}
+		// remove all whitespaces and separators
+		sResult = sValue.replace(/[-\s]/g, '');
+		if (sResult.length != 32) {
+			// don't try to add separators to invalid value
+			return sValue;
+		}
+		sResult = sResult.slice(0, 8) + '-' + sResult.slice(8, 12) + '-' + sResult.slice(12, 16)
+			+ '-' + sResult.slice(16, 20) + '-' + sResult.slice(20);
+		return sResult.toUpperCase();
+	};
+
+	/**
+	 * Sets the constraints.
+	 *
+	 * @param {object} [oConstraints]
+	 *   constraints
+	 * @param {boolean|string} [oConstraints.nullable=true]
+	 *   if <code>true</code>, the value <code>null</code> is accepted; note that
+	 *   {@link #parseValue} maps <code>""</code> to <code>null</code>
+	 * @public
+	 */
+	EdmGuid.prototype.setConstraints = function(oConstraints) {
+		var vNullable = oConstraints && oConstraints.nullable;
+
+		this.oConstraints = undefined;
+		if (vNullable === false || vNullable === "false") {
+			this.oConstraints = {nullable: false};
+		} else if (vNullable !== undefined && vNullable !== true && vNullable !== "true") {
+			jQuery.sap.log.warning("Illegal nullable: " + vNullable, null, this.getName());
+		}
+	};
+
+	/**
+	 * Validates whether the given value in model representation is valid and meets the
+	 * defined constraints (if any).
+	 *
+	 * @param {string} sValue
+	 *   the value to be validated
+	 * @throws sap.ui.model.ValidateException if the value is not valid
+	 * @public
+	 */
+	EdmGuid.prototype.validateValue = function (sValue) {
+		if (sValue === null) {
+			if (this.oConstraints && this.oConstraints.nullable === false) {
+				throw new ValidateException(getErrorMessage());
+			}
+			return;
+		}
+		if (typeof sValue !== "string") {
+			// This is a "technical" error by calling validate w/o parse
+			throw new ValidateException("Illegal " + this.getName() + " value: " + sValue);
+		}
+		if (!rGuid.test(sValue)) {
+			throw new ValidateException(getErrorMessage());
+		}
+	};
+
+	return EdmGuid;
+});
---
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/Guid.qunit.js
@@ -0,0 +1,177 @@
+/*!
+ * ${copyright}
+ */
+(function () {
+	/*global asyncTest, deepEqual, equal, expect, module, notDeepEqual,
+	notEqual, notStrictEqual, ok, raises, sinon, start, strictEqual, stop, test,
+	*/
+	"use strict";
+
+	var sDefaultLanguage = sap.ui.getCore().getConfiguration().getLanguage();
+
+	//*********************************************************************************************
+	module("sap.ui.model.odata.type.Guid", {
+		setup: function () {
+			sap.ui.getCore().getConfiguration().setLanguage("en-US");
+		},
+		teardown: function () {
+			sap.ui.getCore().getConfiguration().setLanguage(sDefaultLanguage);
+		}
+	});
+
+	//*********************************************************************************************
+	test("basics", function () {
+		var oType = new sap.ui.model.odata.type.Guid();
+
+		ok(oType instanceof sap.ui.model.odata.type.Guid, "is a Guid");
+		ok(oType instanceof sap.ui.model.SimpleType, "is a SimpleType");
+		strictEqual(oType.getName(), "sap.ui.model.odata.type.Guid", "type name");
+		deepEqual(oType.oFormatOptions, {}, "no format options");
+		deepEqual(oType.oConstraints, undefined, "default constraints");
+
+		oType.setConstraints();
+		deepEqual(oType.oConstraints, undefined, "default constraints");
+	});
+
+	//*********************************************************************************************
+	jQuery.each(["false", false, "true", true, undefined], function (i, vNullable) {
+		test("with nullable=" + vNullable + " (type: " + typeof vNullable + ")",
+			sinon.test(function () {
+				var oType;
+
+				this.mock(jQuery.sap.log).expects("warning").never();
+
+				oType = new sap.ui.model.odata.type.Guid({}, {
+					foo: "a",
+					nullable: vNullable
+				});
+				deepEqual(oType.oConstraints, i >= 2 ? undefined : {nullable: false});
+			})
+		);
+	});
+
+	//*********************************************************************************************
+	test("default nullable is true", sinon.test(function () {
+		var oType = new sap.ui.model.odata.type.Guid({}, {nullable: false});
+
+		this.mock(jQuery.sap.log).expects("warning").once()
+			.withExactArgs("Illegal nullable: foo", null, "sap.ui.model.odata.type.Guid");
+
+		oType = new sap.ui.model.odata.type.Guid(null, {nullable: "foo"});
+		deepEqual(oType.oConstraints, undefined, "illegal nullable -> default to true");
+	}));
+
+	//*********************************************************************************************
+	test("format success", function () {
+		var oType = new sap.ui.model.odata.type.Guid();
+
+		strictEqual(oType.formatValue(undefined, "foo"), null, "undefined");
+		strictEqual(oType.formatValue(null, "foo"), null, "null");
+		strictEqual(oType.formatValue("0050568D-393C-1ED4-9D97-E65F0F3FCC23", "string"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "target type string");
+		strictEqual(oType.formatValue("0050568D-393C-1ED4-9D97-E65F0F3FCC23", "any"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "target type any");
+	});
+
+	//*********************************************************************************************
+	jQuery.each(["int", "boolean", "float", "foo"], function (i, sTargetType) {
+		test("format fail for target type " + sTargetType, function () {
+			var oType = new sap.ui.model.odata.type.Guid();
+
+			try {
+				oType.formatValue("0050568D-393C-1ED4-9D97-E65F0F3FCC23", sTargetType);
+				ok(false);
+			} catch (e) {
+				ok(e instanceof sap.ui.model.FormatException);
+				strictEqual(e.message, "Don't know how to format sap.ui.model.odata.type.Guid to "
+					+ sTargetType);
+			}
+		});
+	});
+
+
+	//*********************************************************************************************
+	test("parse success", function () {
+		var oType = new sap.ui.model.odata.type.Guid();
+
+		strictEqual(oType.parseValue(null, "string"), null, "null");
+		strictEqual(oType.parseValue("", "string"), null, "empty string is converted to null");
+		strictEqual(oType.parseValue("0050568D-393C-1ED4-9D97-E65F0F3FCC23", "string"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "real GUID");
+		strictEqual(oType.parseValue("0050568D393C1ED49D97E65F0F3FCC23", "string"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "real GUID without '-'");
+		strictEqual(oType.parseValue("-005--05-68D3-93C1E-D49D97E65F0F3FCC23-", "string"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "GUID with separators at wrong places");
+		strictEqual(oType.parseValue("0050568d-393c-1ed4-9d97-e65f0f3fcc23", "string"),
+			"0050568D-393C-1ED4-9D97-E65F0F3FCC23", "real GUID lower case");
+		strictEqual(oType.parseValue("  0050\u180E568D 393C\t1ED49D97-E65F0F3FCC23 \n",
+			"string"), "0050568D-393C-1ED4-9D97-E65F0F3FCC23", "real GUID");
+		strictEqual(oType.parseValue("0050568D-393C-1", "string"),
+			"0050568D-393C-1", "parse invalid GUID");
+		strictEqual(oType.parseValue("005X568D-393C-1ED4-9D97-E65F0F3FCC23", "string"),
+			"005X568D-393C-1ED4-9D97-E65F0F3FCC23", "invalid character X");
+	});
+
+	//*********************************************************************************************
+	jQuery.each([[123, "int"], [true, "boolean"], [1.23, "float"], ["foo", "bar"]],
+		function (i, aFixture) {
+			test("parse fail for source type " + aFixture[1], function () {
+				var oType = new sap.ui.model.odata.type.Guid();
+
+				try {
+					oType.parseValue(aFixture[0], aFixture[1]);
+					ok(false);
+				} catch (e) {
+					ok(e instanceof sap.ui.model.ParseException);
+					strictEqual(e.message, "Don't know how to parse sap.ui.model.odata.type.Guid "
+						+ "from " + aFixture[1]);
+				}
+			});
+		}
+	);
+
+	//*********************************************************************************************
+	test("validate success", sinon.test(function () {
+		var oSpy = this.spy(sap.ui.getCore().getLibraryResourceBundle(), "getText"),
+			oType = new sap.ui.model.odata.type.Guid();
+
+		jQuery.each(["0050568D-393C-1ED4-9D97-E65F0F3FCC23", null],
+			function (i, sValue) {
+				oType.validateValue(sValue);
+			}
+		);
+
+		oType.setConstraints({nullable: false});
+		try {
+			oType.validateValue(null);
+			ok(false);
+		} catch (e) {
+			ok(e instanceof sap.ui.model.ValidateException);
+			ok(e.message === "Enter a GUID (like 12345678-90AB-CDEF-1234-567890ABCDEF).");
+			ok(oSpy.calledOnce);
+			ok(oSpy.calledWith("EnterGuid"), "get text for 'EnterGuid'");
+		}
+	}));
+
+	//*********************************************************************************************
+	jQuery.each([
+		{value: 1234, message: "Illegal sap.ui.model.odata.type.Guid value: 1234"},
+		{value: "123", message: "Enter a GUID (like 12345678-90AB-CDEF-1234-567890ABCDEF)."},
+		{value: "0050568D-393C-1ED4-9D97-E65F0F3FCC23-2",
+			message: "Enter a GUID (like 12345678-90AB-CDEF-1234-567890ABCDEF)."},
+		{value: "G050568D-393C-1ED4-9D97-E65F0F3FCC23",
+			message: "Enter a GUID (like 12345678-90AB-CDEF-1234-567890ABCDEF)."}
+	], function (i, oFixture) {
+		test("validate exception for value " + oFixture.value, function () {
+			var oType = new sap.ui.model.odata.type.Guid();
+
+			try {
+				oType.validateValue(oFixture.value);
+				ok(false);
+			} catch (e) {
+				ok(e instanceof sap.ui.model.ValidateException);
+				strictEqual(e.message, oFixture.message);
+			}
+		});
+	});
+} ());
