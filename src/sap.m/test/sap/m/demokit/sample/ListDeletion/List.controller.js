2603deb68b0cd260d6718d041829035c4c5819b2
Cahit Guerguec <cahit.guerguec@sap.com>
2014-12-22 10:57:12 +0100

    [FIX] ExploredApp: List deletion should retain the focus.
    Change-Id: Id27ddf9d2aaf9b4c778876139719787ef0d2978e
    (cherry picked from commit 7757d039389db85db3250ba2b2c4e18303691bfa)
    
    Change-Id: Ib32f055f9dbb9a4ff6894d34429c43ecba7967f6
    CR-Id: 012006153200000034732015

--- src/sap.m/test/sap/m/demokit/sample/ListDeletion/List.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/ListDeletion/List.controller.js
@@ -1,13 +1,37 @@
+// require mock server implementation
+jQuery.sap.require("sap.m.sample.ListDeletion.MockServer");
+
 sap.ui.controller("sap.m.sample.ListDeletion.List", {
+	
+	onInit: function(oEvent) {
 
-	onInit : function (evt) {
-		// set explored app's demo model on this sample
-		var oModel = new sap.ui.model.json.JSONModel("test-resources/sap/ui/demokit/explored/products.json");
-		this.getView().setModel(oModel);
+		// NOTE TO DEVELOPERS: You do not need to reproduce this following section
+		// It is just so we can simulate 1000ms delay from the fictional OData service
+		sap.m.sample.ListDeletion.MockServer.start();
+
+		// create and set ODATA Model
+		this.oProductModel = new sap.ui.model.odata.ODataModel("/mockserver/products", true);
+		this.getView().setModel(this.oProductModel);
 	},
-
-	handleDelete: function(evt) {
-		evt.getSource().removeItem(evt.getParameter("listItem"));
-	}
-
+	
+	onExit : function() {
+		// NOTE TO DEVELOPERS: You do not need to reproduce this following section
+		// It stops the fictional OData service generated onInit
+		sap.m.sample.ListDeletion.MockServer.stop();
+		
+		// destroy the model and clear the model data
+		this.oProductModel.destroy();
+	},
+	
+	handleDelete: function(oEvent) {
+		var oList = oEvent.getSource(),
+			oItem = oEvent.getParameter("listItem"),
+			sPath = oItem.getBindingContext().getPath();
+		
+		// after deletion put the focus back to the list
+		oList.attachEventOnce("updateFinished", oList.focus, oList);
+		
+		// send a delete request to the odata service
+		this.oProductModel.remove(sPath);
+	},
 });
