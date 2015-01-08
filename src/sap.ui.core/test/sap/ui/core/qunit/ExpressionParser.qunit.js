4038ca0135f6607c12bd7d385eee650d5886c28f
Patric Ksinsik <p.ksinsik@sap.com>
2015-01-08 09:47:31 +0100

    [INTERNAL] Expression binding part1: Enhance BindingParser
    Support {=expression} syntax in BindingParser, ExpressionParser.parse
    interface.
    Fix wrong lastIndex if previous call to BindingParser.complexParser
    failed with an error.
    
    Verify:
    /testsuite/explored.html#/sample/sap.ui.core.sample.ViewTemplate.scenario/preview
    Icons for Contact are shown based on expression binding.
    
    PS1: (Draft) PP PBU, PKS
    PS2: (Draft) PP HMÜ, PKS
    PS3: PP HMÜ, PKS
    PS4: Trailing whitespace, typo
    PS5: Comments TCH, reset _keepBindingString in unit test
    PS6: Whitespace
    PS7: Comment TCH, fix "at" returned by ExpressionParser
    PS8: Improve error message
    PS9: Add ExpressionParser.qunit.html to testsuite
    PS10: Rebase
    PS11: Fix jsdoc (comment FWE), fix format + source refs for scenario app
    PS12: Add lost parts for integration testing expression binding again
    
    Change-Id: I295543e059f5793d010d298a8762e6f1f9dd1d84
    JIRA: FLPUISERVICES2014-1137

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/ExpressionParser.qunit.js
@@ -0,0 +1,16 @@
+/*!
+ * ${copyright}
+ */
+(function () {
+	"use strict";
+
+	jQuery.sap.require("sap.ui.base.ExpressionParser");
+
+	//*********************************************************************************************
+	module("sap.ui.base.ExpressionParser");
+
+	//*********************************************************************************************
+	test("todo", function () {
+		ok(true, "TODO: test expression parser");
+	});
+} ());
