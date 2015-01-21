b1090767358dc78edf20b11dd62b85cea4f1ff79
Deyan Yanakiev <deyan.yanakiev@sap.com>
2015-01-21 13:07:07 +0200

    [INTERNAL] sap.m.ProgressIndicator: Inner text is now correctly displayed
    -Change the default value for 'displayValue' since
     wrong value was displayed when not set explicitly
    -Simplify percent value checking
    -Add qunit for displayValue
    
    Change-Id: I82e922adba6ef33bcb68a2c4b20ddd783ea72369
    Signed-off-by: Deyan Yanakiev <deyan.yanakiev@sap.com>

--- src/sap.m/src/sap/m/ProgressIndicator.js
+++ src/sap.m/src/sap/m/ProgressIndicator.js
@@ -8,11 +8,11 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	"use strict";
 
 
-	
+
 	/**
 	 * Constructor for a new ProgressIndicator.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given
 	 * @param {object} [mSettings] initial settings for the new control
 	 *
 	 * @class
@@ -30,39 +30,39 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var ProgressIndicator = Control.extend("sap.m.ProgressIndicator", /** @lends sap.m.ProgressIndicator.prototype */ { metadata : {
-	
+
 		library : "sap.m",
 		properties : {
 			/**
 			 * Switches enabled state of the control. Disabled fields have different colors, and can not be focused.
 			 */
 			enabled : {type : "boolean", group : "Behavior", defaultValue : true},
-	
+
 			/**
 			 * The state of the bar. Enumeration sap.ui.core.ValueState provides Error (red), Warning (yellow), Success (green), None (blue) (default value).
 			 */
 			state : {type : "sap.ui.core.ValueState", group : "Appearance", defaultValue : sap.ui.core.ValueState.None},
-	
+
 			/**
 			 * The text value to be displayed in the bar.
 			 */
-			displayValue : {type : "string", group : "Appearance", defaultValue : '0%'},
-	
+			displayValue : {type : "string", group : "Appearance", defaultValue : null},
+
 			/**
 			 * The numerical value for the displayed length of the progress bar.
 			 */
 			percentValue : {type : "float", group : "Data", defaultValue : 0},
-	
+
 			/**
 			 * Specifies whether the current value shall be rendered inside the bar.
 			 */
 			showValue : {type : "boolean", group : "Appearance", defaultValue : true},
-	
+
 			/**
 			 * The width of the control.
 			 */
 			width : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : '100%'},
-	
+
 			/**
 			 * The height of the control. The default value depends on the theme. Suggested size for normal use is 2.5rem (40px). Suggested size for small size (like for use in ObjectHeader) is 1.375rem (22px).
 			 * @since 1.15.0
@@ -70,14 +70,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			height : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : null}
 		}
 	}});
-	
-	///**
-	// * This file defines behavior for the control,
-	// */
-	//sap.m.ProgressIndicator.prototype.init = function(){
-	//   // do something for initialization...
-	//};
-	
+
 	ProgressIndicator.prototype.onAfterRendering = function() {
 		//if the user sets a height, this wins against everything else, therefore the styles have to be calculated and set here
 		if (!!this.getHeight()) {
@@ -86,24 +79,21 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			this.$("textLeft").css("line-height", lineHeightText + "px");
 		}
 	};
-	
+
 	ProgressIndicator.prototype.setPercentValue = function(fPercentValue) {
-	
 		var that = this;
-	
+
 		// validation of fPercentValue
-		if (typeof (fPercentValue) == "number") {
-			if (!((fPercentValue >= 0) && (fPercentValue <= 100))) {
-				jQuery.sap.log.error(this + ": percentValue (" + fPercentValue + ") is not correct! It has to be between 0-100.");
-				return this;
-			}
+		if (typeof (fPercentValue) !== "number" || fPercentValue < 0 || fPercentValue > 100) {
+			jQuery.sap.log.error(this + ": percentValue (" + fPercentValue + ") is not correct! It has to be a number between 0-100.");
+			return this;
 		}
-	
-		if (that.getPercentValue() != fPercentValue) {
+
+		if (this.getPercentValue() !== fPercentValue) {
 			// animation without rerendering
+			this.setProperty("percentValue", fPercentValue, true);
 			this.$().addClass("sapMPIAnimate");
 			var time = Math.abs(that.getPercentValue() - fPercentValue) * 20;
-			this.setProperty("percentValue", fPercentValue, true);
 			var $Bar = this.$("bar");
 			$Bar.animate({
 				width : fPercentValue + "%"
@@ -112,26 +102,26 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 				that.$().removeClass("sapMPIAnimate");
 			});
 		}
-	
+
 		return this;
 	};
-	
+
 	ProgressIndicator.prototype._setText = function() {
-	
+
 		this.$().toggleClass("sapMPIValueGreaterHalf", this.getPercentValue() > 50);
-	
+
 		return this;
 	};
-	
+
 	ProgressIndicator.prototype.setDisplayValue = function(sDisplayValue) {
-	
+
 		// change of value without rerendering
 		this.setProperty("displayValue", sDisplayValue, true);
 		var $textLeft = this.$("textLeft");
 		var $textRight = this.$("textRight");
 		$textLeft.text(sDisplayValue);
 		$textRight.text(sDisplayValue);
-	
+
 		return this;
 	};
 
