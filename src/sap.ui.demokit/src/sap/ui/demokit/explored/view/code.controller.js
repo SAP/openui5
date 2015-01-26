2a4d2273c90fdcbe9b78ef7179dc5c902a31d761
Denise Nepraunig <denise.nepraunig@sap.com>
2015-01-26 14:52:51 +0100

    [FEATURE] Self contained examples
    download products, supplier & img.json files
    correct path of images from "test-resources" to openui5
    
    Change-Id: I1ca27b43cdb1678c2bfa57ec29eed0d034611c4c

--- src/sap.ui.demokit/src/sap/ui/demokit/explored/view/code.controller.js
+++ src/sap.ui.demokit/src/sap/ui/demokit/explored/view/code.controller.js
@@ -6,6 +6,8 @@
 
 sap.ui.controller("sap.ui.demokit.explored.view.code", {
 
+	_aMockFiles : ["products.json", "supplier.json", "img.json"],
+
 	onInit : function () {
 		this.router = sap.ui.core.UIComponent.getRouterFor(this);
 		this.router.attachRoutePatternMatched(this.onRouteMatched, this);
@@ -44,37 +46,25 @@ sap.ui.controller("sap.ui.demokit.explored.view.code", {
 		var oMetadata = oComp.getMetadata();
 		var oConfig = (oMetadata) ? oMetadata.getConfig() : null;
 		var oData = {
+			id : oSample.id,
 			title : "Code: " + oSample.name,
+			name : oSample.name,
+			stretch : oConfig.sample ? oConfig.sample.stretch : false,
 			files : []
 		};
 
 		// retrieve files
 		// (via the 'Orcish maneuver': Use XHR to retrieve and cache code)
-		var that = this;
-		var fnSuccess = function (result) {
-			that._viewData.component.codeCache[sUrl] = result;
-		};
-		var fnError = function (result) {
-			that._viewData.component.codeCache[sUrl] = "not found: '" + sUrl + "'";
-		};
 		if (oConfig && oConfig.sample && oConfig.sample.files) {
 			var sRef = jQuery.sap.getModulePath(oSample.id);
 			for (var i = 0 ; i < oConfig.sample.files.length ; i++) {
 				var sFile = oConfig.sample.files[i];
-				var sUrl = sRef + "/" + sFile;
-				if (!(sUrl in this._viewData.component.codeCache)) {
-					this._viewData.component.codeCache[sUrl] = "";
-					jQuery.ajax(sUrl, {
-						async: false,
-						dataType: "text",
-						success: fnSuccess,
-						error: fnError
-					});
-				}
+				var sContent = this.fetchSourceFile(sRef, sFile);
+				
 				oData.files.push({
 					name : sFile,
-					raw : that._viewData.component.codeCache[sUrl],
-					code : this._convertCodeToHtml(that._viewData.component.codeCache[sUrl])
+					raw : sContent,
+					code : this._convertCodeToHtml(sContent)
 				});
 			}
 		}
@@ -85,6 +75,30 @@ sap.ui.controller("sap.ui.demokit.explored.view.code", {
 		// scroll to top of page
 		var page = this.getView().byId("page");
 		page.scrollTo(0);
+	},
+	
+	fetchSourceFile : function (sRef, sFile) {
+		var that = this;
+		var sUrl = sRef + "/" + sFile;
+		
+		var fnSuccess = function (result) {
+			that._viewData.component.codeCache[sUrl] = result;
+		};
+		var fnError = function (result) {
+			that._viewData.component.codeCache[sUrl] = "not found: '" + sUrl + "'";
+		};
+
+		if (!(sUrl in this._viewData.component.codeCache)) {
+			this._viewData.component.codeCache[sUrl] = "";
+			jQuery.ajax(sUrl, {
+				async: false,
+				dataType: "text",
+				success: fnSuccess,
+				error: fnError
+			});
+		}
+
+		return that._viewData.component.codeCache[sUrl];
 	},
 
 	onDownload : function (evt) {
@@ -97,10 +111,58 @@ sap.ui.controller("sap.ui.demokit.explored.view.code", {
 		for (var i = 0 ; i < data.files.length ; i++) {
 			var oFile = data.files[i];
 			ozipFile.file(oFile.name, oFile.raw);
+
+			// mock files
+			for (var j = 0; j < this._aMockFiles.length; j++) {
+				var sMockFile = this._aMockFiles[j]; 
+				if (oFile.raw.indexOf(sMockFile) > -1){
+					ozipFile.file("mockdata/" + sMockFile, this.downloadMockFile(sMockFile));
+				}
+			}
 		}
+
+		var sRef = jQuery.sap.getModulePath(this._sId);
+		ozipFile.file("Component.js", this.fetchSourceFile(sRef, "Component.js"));	
+		ozipFile.file("index.html", this.createIndexFile(data));	
+		
 		var oContent = ozipFile.generate();
 
 		location.href = "data:application/zip;base64," + oContent;
+	},
+
+	createIndexFile : function(oData) {
+
+		var sHeight,
+			bScrolling;
+
+		var sRef = jQuery.sap.getModulePath("sap.ui.demokit.explored.tmpl");
+		var sIndexFile = this.fetchSourceFile(sRef, "index.html.tmpl");
+
+		sIndexFile = sIndexFile.replace(/{{TITLE}}/g, oData.name);
+		sIndexFile = sIndexFile.replace(/{{SAMPLE_ID}}/g, oData.id);
+
+		sHeight = oData.stretch ? 'height : "100%", ' : "";
+		sIndexFile = sIndexFile.replace(/{{HEIGHT}}/g, sHeight);
+
+		bScrolling = !oData.stretch;
+		sIndexFile = sIndexFile.replace(/{{SCROLLING}}/g, bScrolling);		
+		
+		return sIndexFile;
+	},
+
+	downloadMockFile : function(sFile) {
+
+		var sRef = jQuery.sap.getModulePath("sap.ui.demo.mock");
+		var sWrongPath = "test-resources/sap/ui/demokit/explored/img/";
+		var sCorrectPath = "https://openui5.hana.ondemand.com/test-resources/sap/ui/demokit/explored/img/";
+		var oRegExp = new RegExp(sWrongPath,"g");
+		var sMockData = this.fetchSourceFile(sRef, sFile);
+
+		if (sMockData) {
+			sMockData = sMockData.replace(oRegExp, sCorrectPath);
+		}
+
+		return sMockData;
 	},
 
 	onNavBack : function () {
