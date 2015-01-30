998eb204b8ea01461bc47eafee9afb4f0b166397
Monika Juhasz <monika.juhasz@sap.com>
2015-01-30 15:39:38 +0100

    [INTERNAL] moving icon explorer from sap.m. to demokit lib: step 1: copy; step 2: make it work without index.html
    Change-Id: I458ecac60f79a091ed5b12725e9f1691874ac6ed

---
+++ src/sap.ui.demokit/src/sap/ui/demokit/icex/model/Config.js
@@ -0,0 +1,15 @@
+/*!
+ * @copyright@
+ */
+
+jQuery.sap.declare("sap.ui.demokit.icex.model.Config");
+
+sap.ui.demokit.icex.model.Config = {};
+
+sap.ui.demokit.icex.model.Config.getPageSize = function() {
+
+	// Lacking the capability to detect the rendering performance
+	// of the device we assume that "desktop devices"
+	// are 5 times faster than "mobile" devices.
+	return (sap.ui.Device.system.desktop) ? 250 : 50;
+};
