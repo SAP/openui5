c7a6b788c2735c5a9fd624294830f704a4180048
Tobias Oetzel <tobias.oetzel@sap.com>
2015-01-30 13:13:25 +0100

    [INTERNAL] sap.ui.demo.mdskeleton - removed unused arrangements
    Change-Id: I2e6f16a43ba3221591aa02bbffe273df3d5df212

--- src/sap.m/test/sap/m/demokit/md-skeleton/test/opa/arrangement/NavigationArrangement.js
+++ src/sap.m/test/sap/m/demokit/md-skeleton/test/opa/arrangement/NavigationArrangement.js
@@ -3,48 +3,5 @@ sap.ui.define(['sap/ui/test/Opa5'],
 		"use strict";
 
 		return Opa5.extend("sap.ui.demo.mdskeleton.test.opa.arrangement.NavigationArrangement", {
-			iAmOnTheObject1Page : function () {
-				var oObject1Item = null;
-
-				return this.waitFor({
-					id : "list",
-					viewName : "Master",
-					check : function (oMasterList) {
-						return oMasterList.getItems().some(function (oItem) {
-							if (oItem.getTitle() === "Object 1") {
-								oObject1Item = oItem;
-								return true;
-							}
-
-							return false;
-						});
-					},
-					success : function (oMasterList) {
-						oObject1Item.$().trigger("tap");
-						ok(oMasterList, "Pressed the Object 1 item");
-						var oLineItem1 = null;
-						this.waitFor({
-							id : "lineItemsList",
-							viewName : "Detail",
-							check : function (oLineItemList) {
-								return oLineItemList.getItems().some(function (oItem) {
-									if (oItem.getText() === "LineItemID_1") {
-										oLineItem1 = oItem;
-										return true;
-									}
-
-									return false;
-								});
-							},
-							success : function (oLineItemList) {
-								oLineItem1.$().trigger("tap");
-								ok(oLineItemList, "Pressed the Object 1 item");
-							},
-							errorMessage : "the list did not contain Object 1"
-						});
-					},
-					errorMessage : "the list did not contain Object 1"
-				});
-			}
 		});
 	});
