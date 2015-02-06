1a425019c52ab7ab3a8def034f08e91890d2d24f
Matthias Osswald <mat.osswald@sap.com>
2015-02-06 10:36:10 +0100

    [INTERNAL] update eslint to 0.13.0
    Change-Id: I852c1f4599f3fd668189721c604b3a144ecb82f9

--- grunt/config/eslint.js
+++ grunt/config/eslint.js
@@ -1,9 +1,16 @@
 // JavaScript validation using eslint
 module.exports = function(grunt, config) {
 
+	// read out CLI option for quiet (default true)
+	// use --no-quiet / --quiet=false to set it to false
+	var quiet = grunt.option('quiet');
+	if (quiet !== false) {
+		quiet = true;
+	}
+
 	var eslint = {
 		options: {
-			quiet: true
+			quiet: quiet
 		}
 	};
 
