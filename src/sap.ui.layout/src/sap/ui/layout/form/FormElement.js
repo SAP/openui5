fe396f217d2c3b1595cb55efe76e05444819f630
Sebastian Allmann <sebastian.allmann@sap.com>
2015-02-06 09:36:15 +0100

    [INTERNAL] sap.ui.layout.form.Form: syntax error on label assignment fixed
    Change-Id: I4bac104290cca993a70ac84edee4db803b77f8bb

--- src/sap.ui.layout/src/sap/ui/layout/form/FormElement.js
+++ src/sap.ui.layout/src/sap/ui/layout/form/FormElement.js
@@ -7,8 +7,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 	function(jQuery, Element, EnabledPropagator, library) {
 	"use strict";
 
-
-	
 	/**
 	 * Constructor for a new form/FormElement.
 	 *
@@ -29,10 +27,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var FormElement = Element.extend("sap.ui.layout.form.FormElement", /** @lends sap.ui.layout.form.FormElement.prototype */ { metadata : {
-	
+
 		library : "sap.ui.layout",
 		properties : {
-	
+
 			/**
 			 * Invisible FormElements are not rendered.
 			 */
@@ -40,63 +38,58 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 		},
 		defaultAggregation : "fields",
 		aggregations : {
-	
+
 			/**
 			 * Label of the fields. Can either be a Label object, or a simple string.
 			 */
 			label : {type : "sap.ui.core.Label", altTypes : ["string"], multiple : false}, 
-	
+
 			/**
 			 * Formular controls.
 			 */
 			fields : {type : "sap.ui.core.Control", multiple : true, singularName : "field"}
 		}
 	}});
-	
-	
-	
-	
+
 	/**
 	 * This file defines behavior for the control,
 	 */
-	
-	
+
 	// TODO deactivated until Element/Control has been clarified: sap.ui.core.EnabledPropagator.call(sap.ui.layout.form.FormElement.prototype);
-	
+
 	(function() {
-	
+
 		FormElement.prototype.init = function(){
-	
+
 			this._oFieldDelegate = {oElement: this, onAfterRendering: _fieldOnAfterRendering};
-	
+
 		};
-	
+
 		FormElement.prototype.exit = function(){
-	
+
 			if (this._oLabel) {
 				this._oLabel.destroy();
 				delete this._oLabel;
 			}
-	
+
 			this._oFieldDelegate = undefined;
-	
+
 		};
 
-	
 	/*
 	 * sets the label for the FormElement. If it's only a string an internal label is created.
 	 * overwrite the isRequired and the getLabelForRendering functions with Form specific ones.
 	 */
 		FormElement.prototype.setLabel = function(vAny) {
-	
+
 			if (!this._oLabel) {
 				var oOldLabel = this.getLabel();
 				if (oOldLabel && oOldLabel.isRequired) {
-						oOldLabel.isRequired = oOldLabel._sapui_isRequired;
-						oOldLabel._sapui_isRequired = undefined;
+					oOldLabel.isRequired = oOldLabel._sapuiIsRequired;
+					oOldLabel._sapuiIsRequired = undefined;
 				}
 			}
-	
+
 			this.setAggregation("label", vAny);
 			var oLabel = vAny;
 			if (typeof oLabel === "string") {
@@ -115,130 +108,131 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 					delete this._oLabel;
 				}
 				if (oLabel && oLabel.isRequired) {
-					oLabel._sapui_isRequired = oLabel.isRequired;
+					oLabel._sapuiIsRequired = oLabel.isRequired;
 					oLabel.isRequired = _labelIsRequired;
 				}
 			}
-			
+
 			_updateLabelFor(this); 
-	
+
 			return this;
-	
+
 		};
-	
 
 		/**
 		 * Returns the Label Control, even if the Label is entered as Text.
 		 *
-		 * @type sap.ui.core.Label
+		 * @returns {sap.ui.core.Label}  <code>this</code> to allow method chaining
 		 * @public
 		 * @ui5-metamodel This method also will be described in the UI5 (legacy) designtime metamodel
-	 */
+		 */
 		FormElement.prototype.getLabelControl = function() {
-	
+
 			if (this._oLabel) {
 				return this._oLabel;
 			} else {
 				return this.getLabel();
 			}
-	
+
 		};
-	
+
 		FormElement.prototype.addField = function(oField) {
-			
+
 			this.addAggregation("fields", oField);
 			oField.addDelegate(this._oFieldDelegate);
 			_updateLabelFor(this);
-	
+
 			return this;
-	
+
 		};
-	
+
 		FormElement.prototype.insertField = function(oField, iIndex) {
-	
+
 			this.insertAggregation("fields", oField, iIndex);
 			oField.addDelegate(this._oFieldDelegate);
 			_updateLabelFor(this);
-	
+
 			return this;
-	
+
 		};
-	
+
 		FormElement.prototype.removeField = function(oField) {
-	
+
 			var oRemovedField = this.removeAggregation("fields", oField);
 			oRemovedField.removeDelegate(this._oFieldDelegate);
 			_updateLabelFor(this);
-	
+
 			return oRemovedField;
-	
+
 		};
-	
+
 		FormElement.prototype.removeAllFields = function() {
-	
+
 			var aRemovedFields = this.removeAllAggregation("fields");
-	
+
 			for ( var i = 0; i < aRemovedFields.length; i++) {
 				var oRemovedField = aRemovedFields[i];
 				oRemovedField.removeDelegate(this._oFieldDelegate);
 			}
 			_updateLabelFor(this);
-	
+
 			return aRemovedFields;
-	
+
 		};
-	
+
 		FormElement.prototype.destroyFields = function() {
-	
+
 			var aFields = this.getFields();
-	
+
 			for ( var i = 0; i < aFields.length; i++) {
 				var oField = aFields[i];
 				oField.removeDelegate(this._oFieldDelegate);
 			}
-	
+
 			this.destroyAggregation("fields");
-			
+
 			_updateLabelFor(this);
-	
+
 			return this;
-	
+
 		};
-	
+
 		FormElement.prototype.updateFields = function() {
-	
+
 			var aFields = this.getFields();
-	
-			for ( var i = 0; i < aFields.length; i++) {
-				var oField = aFields[i];
+			var oField;
+			var i = 0;
+
+			for (i = 0; i < aFields.length; i++) {
+				oField = aFields[i];
 				oField.removeDelegate(this._oFieldDelegate);
 			}
-	
+
 			this.updateAggregation("fields");
-	
+
 			aFields = this.getFields();
-	
-			for ( var i = 0; i < aFields.length; i++) {
-				var oField = aFields[i];
+
+			for (i = 0; i < aFields.length; i++) {
+				oField = aFields[i];
 				oField.addDelegate(this._oFieldDelegate);
 			}
-			
+
 			_updateLabelFor(this);
-	
+
 			return this;
-	
+
 		};
-	
+
 		/*
 		 * Enhance Aria properties of fields to set aria-labelledby to FormElements label if not set otherwise
 		 * Set aria-describedby to the title of the container, but only for the first field in the container
 		 * This function is called during rendering.
 		 */
 		FormElement.prototype.enhanceAccessibilityState = function(oElement, mAriaProps) {
-	
+
 			var oLabel = this.getLabelControl();
 			if (oLabel && oLabel != oElement) {
-				
+
 				var sLabelledBy = mAriaProps["labelledby"];
 				if (!sLabelledBy) {
 					sLabelledBy = oLabel.getId();
@@ -277,53 +271,53 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 					}
 				}
 			}
-	
+
 			return mAriaProps;
-	
+
 		};
-	
+
 		/*
 		 * If LayoutData changed on control this may need changes on the layout. So bubble to the form
 		 */
 		FormElement.prototype.onLayoutDataChange = function(oEvent){
-	
+
 			// call function of parent (if assigned)
 			var oParent = this.getParent();
 			if (oParent && oParent.onLayoutDataChange) {
 				oParent.onLayoutDataChange(oEvent);
 			}
-	
+
 		};
-	
+
 		// *** Private helper functions ***
-	
+
 		/*
 		 * overwrite Labels isRequired function to check if one of the fields in the element is required,
 		 * not only the one directly assigned.
 		 */
-		var _labelIsRequired = function(){
-	
+		function _labelIsRequired(){
+
 			var oFormElement = this.getParent();
 			var aFields = oFormElement.getFields();
-	
+
 			for ( var i = 0; i < aFields.length; i++) {
 				var oField = aFields[i];
 				if (oField.getRequired && oField.getRequired() === true) {
 					return true;
 				}
 			}
-	
+
 			return false;
-	
-		};
-		
+
+		}
+
 		/*
 		 * Update the for association of the related label
 		 */
-		var _updateLabelFor = function(oFormElement){
+		function _updateLabelFor(oFormElement){
 			var aFields = oFormElement.getFields();
-			var oField = aFields.length > 0 ? aFields[0] : 0;
-			
+			var oField = aFields.length > 0 ? aFields[0] : null;
+
 			var oLabel = oFormElement._oLabel;
 			if (oLabel) {
 				oLabel.setAlternativeLabelFor(oField);
@@ -332,21 +326,21 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/core/EnabledP
 			if (oLabel instanceof sap.ui.core.Control /*might also be a string*/) {
 				oLabel.setAlternativeLabelFor(oField);
 			}
-		};
-	
+		}
+
 		/*
 		 * If onAfterRendering of a field is processed the Form (layout) might need to change it.
 		 */
-		var _fieldOnAfterRendering = function(oEvent){
-	
+		function _fieldOnAfterRendering(oEvent){
+
 			// call function of parent (if assigned)
 			var oParent = this.oElement.getParent();
 			if (oParent && oParent.contentOnAfterRendering) {
 				oParent.contentOnAfterRendering( this.oElement, oEvent.srcControl);
 			}
-	
-		};
-	
+
+		}
+
 	}());
 
 	return FormElement;
