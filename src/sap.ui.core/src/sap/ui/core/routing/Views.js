a09048ad939721f16885bc795d557b333c5aea3b
Tobias Oetzel <tobias.oetzel@sap.com>
2015-02-18 16:11:16 +0100

    [INTERNAL][FEATURE] sap.ui.core.routing.Router added the bypassed event + parameter
    Change-Id: I11adec634a91a1c5749bcee0c4f7a3cc90728d69

--- src/sap.ui.core/src/sap/ui/core/routing/Views.js
+++ src/sap.ui.core/src/sap/ui/core/routing/Views.js
@@ -102,7 +102,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/EventProvider', 'sap/ui/core/UI
 			 * @param {sap.ui.base.EventProvider} oEvent.getSource
 			 * @param {object} oEvent.getParameters
 			 * @param {sap.ui.core.mvc.View} oEvent.getParameters.view the instance of the created view.
-			 * @param {string} oEvent.getParameters.viewOptions The view options passed to {@link sap.ui.view}
+			 * @param {object} oEvent.getParameters.viewOptions The view options passed to {@link sap.ui.view}
 			 */
 
 			/**
