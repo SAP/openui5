327e4cf3962d4a053322f052a2247a91aa538e44
Peter Buchholz <peter.buchholz@sap.com>
2015-02-19 10:20:59 +0100

    [INTERNAL] Fix JSDoc
    PS1: PP URE/PBU Decimal & Int64
    PS2: PP SBE/PBU Int64
    
    Change-Id: I6a8729ffc82bb7d5ca709c9f1600a2e490890ee1
    JIRA: FLPUISERVICES2014-1250

--- src/sap.ui.core/src/sap/ui/model/odata/type/Int64.js
+++ src/sap.ui.core/src/sap/ui/model/odata/type/Int64.js
@@ -153,13 +153,6 @@ sap.ui.define(['sap/ui/model/odata/type/ODataType', 'sap/ui/model/FormatExceptio
 	/**
 	 * Formats the given value to the given target type.
 	 *
-	 * If the type uses certain format options, parsing from a <code>String</code> uses floating
-	 * point numbers internally. This may cause a loss of precision (e.g.
-	 * "1,234,567,890,123,456,789" will be parsed to "1234567890123456800"). The following options
-	 * do not cause this effect: decimals, decimalSeparator, groupingEnabled, groupingSeparator,
-	 * maxFractionDigits, maxIntegerDigits, minFractionDigits, minIntegerDigits, minusSign and
-	 * plusSign.
-	 *
 	 * @param {string} sValue
 	 *   the value to be formatted, which is represented as a string in the model
 	 * @param {string} sTargetType
@@ -220,6 +213,14 @@ sap.ui.define(['sap/ui/model/odata/type/ODataType', 'sap/ui/model/FormatExceptio
 	/**
 	 * Parses the given value, which is expected to be of the given type, to an Int64 in
 	 * <code>string</code> representation.
+	 *
+	 * If certain format options are defined and you parse a value with <code>sSourceType</code>
+	 * "string", floating point numbers will be used internally.
+	 * This may cause a loss of precision (e.g.
+	 * "1,234,567,890,123,456,789" will be parsed to "1234567890123456800"). The following options
+	 * do not cause this effect: decimals, decimalSeparator, groupingEnabled, groupingSeparator,
+	 * maxFractionDigits, maxIntegerDigits, minFractionDigits, minIntegerDigits, minusSign and
+	 * plusSign.
 	 *
 	 * @param {string|number} vValue
 	 *   the value to be parsed; the empty string and <code>null</code> will be parsed to
