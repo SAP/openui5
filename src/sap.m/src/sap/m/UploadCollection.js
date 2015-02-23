cbd0e1b9d013f807129a3f19a331b77914655e91
Claus Neuwirth <claus.neuwirth@sap.com>
2015-02-23 10:52:54 +0100

    [INTERNAL][FIX] sap.m.UploadCollection: Deletion of the default tooltip of the file Uploader and some adaptation of the testpage: Filenamelength to 55 letters; Unique document ID for the testpage examples and a long filename. Edit mode in SAP Fiori Client is now possible.
    Change-Id: If1ca401b059a4106c0f9dfdbfa9357443365e1f2

--- src/sap.m/src/sap/m/UploadCollection.js
+++ src/sap.m/src/sap/m/UploadCollection.js
@@ -485,7 +485,7 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './MessageToast', './library
 				var $oEditBox = jQuery.sap.byId(this.editModeItem + "-ta_editFileName-inner");
 				if ($oEditBox) {
 					var sId = this.editModeItem;
-					if (!(sap.ui.Device.os.ios && (sap.ui.Device.browser.chrome || sap.ui.Device.browser.safari))) {
+					if (!sap.ui.Device.os.ios) {
 						$oEditBox.focus(function() {
 							$oEditBox.selectText(0, $oEditBox.val().length);
 						});
@@ -1381,6 +1381,11 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './MessageToast', './library
 					that._onUploadProgress(oEvent);
 				}
 			});
+			var sTooltip = this._oFileUploader.getTooltip();
+			if (!sTooltip) {
+				// in case the tooltip is NOT overwritten, the default tooltip should NOT be chosen!
+				this._oFileUploader.setTooltip(" ");
+			}
 		}
 		return this._oFileUploader;
 	};
