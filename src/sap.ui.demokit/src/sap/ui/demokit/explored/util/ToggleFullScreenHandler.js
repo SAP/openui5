c08a989391a9ddaea9a24540130ac8da1eb7a1a3
Tobias Oetzel <tobias.oetzel@sap.com>
2015-02-25 13:26:43 +0100

    [INTERNAL] sap.ui.demokit.explored iframe samples may be opened in a new tab
    Change-Id: I7c4eaa5359e5af3b14d92667e54e29da39cb0dfe
    CR-Id: 012006153200001688582015

--- src/sap.ui.demokit/src/sap/ui/demokit/explored/util/ToggleFullScreenHandler.js
+++ src/sap.ui.demokit/src/sap/ui/demokit/explored/util/ToggleFullScreenHandler.js
@@ -9,8 +9,8 @@ sap.ui.define(['jquery.sap.global'],
 
 
 	var ToggleFullScreenHandler = {
-		
-		updateMode : function(oEvt, oView) {	
+
+		updateMode : function(oEvt, oView) {
 			if (!this._oShell) {
 				this._oShell = sap.ui.getCore().byId('Shell');
 			}
@@ -24,31 +24,29 @@ sap.ui.define(['jquery.sap.global'],
 			}
 			this.updateControl(oEvt.getSource(), oView, bSwitchToFullScreen);
 		},
-		
-		
+
+
 		_getSplitApp : function () {
 			if (!this._oSplitApp) {
 				this._oSplitApp = sap.ui.getCore().byId('splitApp');
 			}
 			return this._oSplitApp;
 		},
-	
+
 		updateControl : function (oButton, oView, bFullScreen) {
 			if (arguments.length === 2) {
 				bFullScreen = !(this._getSplitApp().getMode() === "ShowHideMode");
 			}
 			var i18nModel = oView.getModel('i18n');
 			if (!bFullScreen) {
-				oButton.setText(i18nModel.getProperty('sampleFullScreen'));
 				oButton.setTooltip(i18nModel.getProperty('sampleFullScreenTooltip'));
 				oButton.setIcon('sap-icon://full-screen');
 			} else {
-				oButton.setText(i18nModel.getProperty('sampleExitFullScreen'));
 				oButton.setTooltip(i18nModel.getProperty('sampleExitFullScreenTooltip'));
 				oButton.setIcon('sap-icon://exit-full-screen');
 			}
 		},
-		
+
 		cleanUp : function() {
 			this._oSplitApp = null;
 			this._oShell = null;
