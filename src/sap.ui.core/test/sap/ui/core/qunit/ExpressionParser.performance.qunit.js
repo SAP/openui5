fd34709aa70e8d4f194f03ff025d6eed25e2186e
Uwe Reeder <uwe.reeder@sap.com>
2015-02-26 13:02:52 +0100

    [INTERNAL] ExpressionParser: performance tests
    PS1: first version
    PS2: tests for direct call of expression formatter
    PS3: changed the message again
    
    Change-Id: I4471ed982af83698be6f00792273f3406f00d97f
    JIRA: FLPUISERVICES2014-1308

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/ExpressionParser.performance.qunit.js
@@ -0,0 +1,99 @@
+/*!
+ * ${copyright}
+ */
+(function () {
+	"use strict";
+
+	jQuery.sap.require("sap.ui.base.ExpressionParser");
+
+	var iCount = 10000,
+		oModel = new sap.ui.model.json.JSONModel({x: 2}),
+		TestControl = sap.ui.base.ManagedObject.extend("TestControl", {
+			metadata: {
+				properties: {
+					value: "int"
+				}
+			}
+		});
+
+	function toMicroSeconds(iMilliSeconds) {
+		return iMilliSeconds / iCount * 1000;
+	}
+
+	function repeatedTest(fnTest) {
+		var i, iStart = Date.now(), iDuration;
+
+		for (i = iCount; i; i -= 1) {
+			fnTest();
+		}
+		iDuration = Date.now() - iStart;
+		ok(true, iCount + " iterations took " + iDuration + " ms, that is " + iDuration / iCount
+			+ " ms per iteration");
+	}
+
+	window.formatters = {
+		trivial: function (iValue) {
+			return iValue;
+		},
+
+		moderate: function (iValue) {
+			return 3 + 2 * iValue;
+		},
+
+		complex: function (iValue) {
+			return 'foo'.charCodeAt(-17+2*Math.floor(null||(iValue===2?5+2*iValue:42)));
+		}
+	};
+
+	function bindTest(oBindingInfo) {
+		var oControl = new TestControl({
+				models: oModel,
+				bindingContexts: oModel.createBindingContext("/")
+			});
+
+		repeatedTest(function () {
+			oControl.bindProperty("value", oBindingInfo);
+		});
+	}
+
+	//*********************************************************************************************
+	module("sap.ui.base.ExpressionParser Performance");
+
+	//*********************************************************************************************
+	jQuery.each([
+		{name: "trivial", expression:"{=${x}}"},
+		{name: "moderate", expression: "{=3+2*${x}}"},
+		{name: "complex", expression: "{='foo'.charCodeAt(-17+2*Math.floor(null||(${x}===2?5+2*${x}:42)))}"}
+	], function(iUnused, oFixture) {
+
+		test("Parse expression binding: " + oFixture.name, function () {
+			repeatedTest(function () {
+				sap.ui.base.BindingParser.complexParser(oFixture.expression);
+			});
+		});
+
+		test("Evaluate expression binding: " + oFixture.name, function () {
+			var oBindingInfo = sap.ui.base.BindingParser.complexParser(oFixture.expression);
+			repeatedTest(function () {
+				oBindingInfo.formatter(2);
+			});
+		});
+
+		test("Formatter: " + oFixture.name, function () {
+			var fnFormatter = window.formatters[oFixture.name];
+
+			repeatedTest(function () {
+				fnFormatter(2);
+			});
+		});
+
+		test("Bind with expression: " + oFixture.name, function () {
+			bindTest(sap.ui.base.BindingParser.complexParser(oFixture.expression));
+		});
+
+		test("Bind with formatter: " + oFixture.name, function () {
+			bindTest(sap.ui.base.BindingParser.complexParser(
+				"{path: '/x', formatter: 'formatters." + oFixture.name + "'}"));
+		});
+	});
+} ());
