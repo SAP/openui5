bcbac9d24ffaf2444cf94bb9061fc708b9e9eb71
Tobias Oetzel <tobias.oetzel@sap.com>
2015-03-02 10:49:53 +0100

    [INTERNAL] sap.m.routing.Routing sample for fullscreenNavigation
    Change-Id: I643b8dbb09fad8069d2571c635b3f7c3e49f885b

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/RoutingFullscreen/routingApp/controller/View1.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define( ["sap/ui/core/mvc/Controller"], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sap.ui.core.sample.RoutingFullscreen.routingApp.controller.View1", {
+		onInit : function () {
+			var sUrl = "#" + this.getOwnerComponent().getRouter().getURL("page2");
+			this.byId("link").setHref(sUrl);
+		},
+
+		onToPage2 : function () {
+			this.getOwnerComponent().getRouter().navTo("page2");
+		}
+	});
+
+}, /* bExport= */ true);
---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/RoutingFullscreen/routingApp/controller/View2.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define( ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"], function (Controller, History) {
+	"use strict";
+
+	return Controller.extend("sap.ui.core.sample.RoutingFullscreen.routingApp.controller.View2", {
+		onInit : function () {
+			var sUrl = "#" + this.getOwnerComponent().getRouter().getURL("page1");
+			this.byId("link").setHref(sUrl);
+		},
+
+		onToPage1 : function () {
+			this.getOwnerComponent().getRouter().navTo("page1");
+		},
+
+		onBack : function () {
+			var sPreviousHash = History.getInstance().getPreviousHash();
+
+			//The history contains a previous entry
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				// There is no history!
+				// replace the current hash with page 1 (will not add an history entry)
+				this.getOwnerComponent().getRouter().navTo("page1", null, true);
+			}
+		}
+	});
+
+}, /* bExport= */ true);
