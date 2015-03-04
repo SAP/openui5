7394b496c0f147260dbe45b6ddf5265a0a2e909f
Frank Weigel <frank.weigel@sap.com>
2015-03-04 18:58:14 +0100

    [FIX] sap.ui.define: support 'export' flag for controllers and JSViews
    sap.ui.define failed to create the correct parent namespace for
    controllers and JSViews due to the dots in their base names (e.g.
    some/package/Controller.controller.js)
    
    By design, the 'export' flag can only support old-style modules without 
    a dot in their package names. But a dot in the base name should be 
    supported (as it doesn't contribute to the parent package).
    
    This change fixes the namespace creation within sap.ui.define.
    
    Additionally, some strict mode issues in sap.ui.core test code are 
    fixed (undeclared variables)
    
    Change-Id: I419eca9b17bf17e0f9e4211ef692cdbad4a86f5e

--- src/sap.ui.core/test/sap/ui/core/qunit/testdata/fragments/JSTestFragmentNoController.fragment.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/fragments/JSTestFragmentNoController.fragment.js
@@ -3,7 +3,7 @@ sap.ui.jsfragment("testdata.fragments.JSTestFragmentNoController", {
 	createContent: function(oController) {
 		var oLayout = new sap.ui.layout.HorizontalLayout();
 		
-		oButton = new sap.ui.commons.Button({
+		var oButton = new sap.ui.commons.Button({
 			text:"{/someText}"
 		});
 		oLayout.addContent(oButton);
