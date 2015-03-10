9b9047f2cac02dd37e4b0f6b4a752fa35265816c
Tobias Oetzel <tobias.oetzel@sap.com>
2015-03-10 09:42:35 +0100

    [INTERNAL] refactoring the BusyHandler - made it a controller
    -
    
    Change-Id: Iee33a5e0b2b1d5ffd8c5eb936ef8f6fb966ed371

--- src/sap.m/test/sap/m/demokit/worklist/test/integration/pages/App.js
+++ src/sap.m/test/sap/m/demokit/worklist/test/integration/pages/App.js
@@ -1,8 +1,9 @@
 sap.ui.require([
 		'sap/ui/test/Opa5',
+		'sap/ui/test/matchers/PropertyStrictEquals',
 		'sap/ui/demo/worklist/test/integration/pages/Common'
 	],
-	function(Opa5, Common) {
+	function(Opa5, PropertyStrictEquals, Common) {
 		"use strict";
 
 		Opa5.createPageObjects({
@@ -15,9 +16,10 @@ sap.ui.require([
 						return this.waitFor({
 							id : "app",
 							viewName : "App",
-							matchers : function (oAppControl) {
-								return oAppControl.getParent() && oAppControl.getParent().getBusy();
-							},
+							matchers : new PropertyStrictEquals({
+								name: "busy",
+								value: true
+							}),
 							success : function (oRootView) {
 								// we set the view busy, so we need to query the parent of the app
 								ok(true, "The rootview is busy");
