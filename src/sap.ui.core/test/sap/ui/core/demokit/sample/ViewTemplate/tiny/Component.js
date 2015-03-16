662321a67ce984ae0c98a58efb5a378926045eaa
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2015-03-16 09:43:10 +0100

    [INTERNAL] sap.ui.core.sample.ViewTemplate.tiny
    PS1: enhance description
    PS2: add message box in case loading fails
    PS3: team work on messages
    PS4: turn "!" into ".", move message to body
    PS5: rebased
    PS6: fix dependencies
    
    Change-Id: I1c1a32aa7c9db2c91373d13e9329f35f3596bea8

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/ViewTemplate/tiny/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/ViewTemplate/tiny/Component.js
@@ -8,12 +8,13 @@
  * @version @version@
  */
 sap.ui.define([
+		'jquery.sap.global',
 		'sap/m/VBox',
 		'sap/ui/core/UIComponent',
 		'sap/ui/core/mvc/View',
 		'sap/ui/model/odata/AnnotationHelper',
 		'sap/ui/model/odata/v2/ODataModel'
-	], function(VBox, UIComponent, View, AnnotationHelper, ODataModel) {
+	], function(jQuery, VBox, UIComponent, View, AnnotationHelper, ODataModel) {
 	"use strict";
 
 	var Component = UIComponent.extend("sap.ui.core.sample.ViewTemplate.tiny.Component", {
@@ -29,7 +30,13 @@ sap.ui.define([
 				}),
 				oMetaModel = oModel.getMetaModel(),
 				sPath = "/ProductSet('HT-1021')/ToSupplier",
-				oViewContainer = new VBox();
+				oViewContainer = new VBox({
+					items: [
+						new sap.m.Title({text: "This is meant to be a pure code sample. "
+							+ "(To run it, you would need a proxy which is configured properly.)",
+							titleStyle: sap.ui.core.TitleLevel.H3})
+					]
+				});
 
 			oMetaModel.loaded().then(function () {
 				var oTemplateView = sap.ui.view({
@@ -49,7 +56,13 @@ sap.ui.define([
 
 				oTemplateView.setModel(oModel);
 				oTemplateView.bindElement(sPath);
+				oViewContainer.destroyItems();
 				oViewContainer.addItem(oTemplateView);
+			}, function (oError) {
+				jQuery.sap.require("sap.m.MessageBox");
+				sap.m.MessageBox.alert(oError.message, {
+					icon: sap.m.MessageBox.Icon.ERROR,
+					title: "Missing Proxy?"});
 			});
 
 			// Note: synchronously return s.th. here and add content to it later on
