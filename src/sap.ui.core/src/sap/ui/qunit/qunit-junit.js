SAP/openui5

commit 76acf6580325ed4653a952ff7757d416652699d5

[INTERNAL][FIX] qunit-junit: cast module name to string
There should not be an error e.g. if no string gets passed to
QUnit.module as first parameter.

Change-Id: I470f3a7a8d86fcf9ba68e73299f427d538206385

src/sap.ui.core/src/sap/ui/qunit/qunit-junit.js
@@ -39,7 +39,7 @@
 
 		// avoid . in module names to avoid displaying issues in Jenkins results
 		var formatModuleName = function(sName) {
-			return sName.replace(/\./g, "_");
+			return String(sName).replace(/\./g, "_");
 		};
 
 		// HACK: insert our hook in front of QUnit's own hook so that we execute first

commit dc1f59da25b95aaa83b1d68878ec2b25df63b145

[INTERNAL][FIX] qunit-junit: add default module name
If no module was specified an empty string was used which resulted into
a dot at the end of the testsuite name in the JUnit report. This caused
issues when displaying the tests in Jenkins.
This change will make sure that the module name will not be empty.

Broken behavior was introduced with
76acf6580325ed4653a952ff7757d416652699d5

Change-Id: I3d240fa518d9ffd6ad2b297d81f2114e9559f659

src/sap.ui.core/src/sap/ui/qunit/qunit-junit.js
@@ -39,7 +39,7 @@
 
 		// avoid . in module names to avoid displaying issues in Jenkins results
 		var formatModuleName = function(sName) {
-			return String(sName).replace(/\./g, "_");
+			return String(sName || 'default').replace(/\./g, "_");
 		};
 
 		// HACK: insert our hook in front of QUnit's own hook so that we execute first
@@ -68,7 +68,7 @@
 			oData.name = sTestPageName + "." + formatModuleName(oData.name);
 		});
 		QUnit.testStart(function(oData) {
-			oData.module = sTestPageName + "." + formatModuleName(oData.module || 'default');
+			oData.module = sTestPageName + "." + formatModuleName(oData.module);
 			window.assert = QUnit.config.current.assert;
 		});
 		QUnit.testDone(function(assert) {