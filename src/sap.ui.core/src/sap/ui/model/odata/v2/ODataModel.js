4e78a3de28ada1afaadf142bc131a8993420db5c
Malte Wedel <malte.wedel@sap.com>
2015-03-26 18:57:40 +0100

    [FIX] v2.ODataModel: Either success or error handler must be called
    In the V2 ODataModel there were some cases, were neither the success nor
    the error handler of CRUD methods were called, e.g. when aborting
    requests or if batch requests failed. Aborted requests now have
    consistent event paramters.
    
    BCP: 1580014034
    Change-Id: Iefb0eb5963cda6ab9997639156be166ac18b54c2
    CR-Id: 012003146900001024792015

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -960,7 +960,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 				vResponse = vResponse.response;
 			}
 			//in case of aborted requests there is no further info
-			if (vResponse && vResponse.statusCode) {
+			if (vResponse && vResponse.statusCode != undefined) {
 				oEventInfo.response.headers = vResponse.headers;
 				oEventInfo.response.statusCode = vResponse.statusCode;
 				oEventInfo.response.statusText = vResponse.statusText;
@@ -1935,14 +1935,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 			that._processSuccess(oRequest, oResponse, fnSingleSuccess, mGetEntities, mChangeEntities, mEntityTypes);
 		};
 		var handleError = function(oError) {
-			if (fnError) {
-				fnError(that._handleError(oError));
-			}
-			oEventInfo = that._createEventInfo(oRequest, oError);
-
-			that.fireRequestCompleted(oEventInfo);
-			if (!oRequestHandle || !oRequestHandle.bAborted) {
-				that.fireRequestFailed(oEventInfo);
+			if (oError.message == "Request aborted") {
+				that._processAborted(oRequest, oError, fnError);
+			} else {
+				that._processError(oRequest, oError, fnError);
 			}
 		};
 		oRequestHandle =  this._submitRequest(oRequest, handleSuccess, handleError);
@@ -1987,7 +1983,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 								oRequestObject = aRequests[i][j];
 								
 								if (oRequestObject.request._aborted) {
-									that._processAborted(oRequestObject.request, oResponse);
+									that._processAborted(oRequestObject.request, oResponse, oRequestObject.fnError);
 								} else {
 									that._processError(oRequestObject.request, oResponse, oRequestObject.fnError);
 								}
@@ -2000,7 +1996,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 								oRequestObject = aRequests[i][j];
 								//check for error
 								if (oRequestObject.request._aborted) {
-									that._processAborted(oRequestObject.request, oChangeResponse);
+									that._processAborted(oRequestObject.request, oChangeResponse, oRequestObject.fnError);
 								} else if (oChangeResponse.message) {
 									that._processError(oRequestObject.request, oChangeResponse, oRequestObject.fnError);
 								} else {
@@ -2012,7 +2008,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 					} else {
 						oRequestObject = aRequests[i];
 						if (oRequestObject.request._aborted) {
-							that._processAborted(oRequestObject.request, oResponse);
+							that._processAborted(oRequestObject.request, oResponse, oRequestObject.fnError);
 						} else if (oResponse.message) {
 							that._processError(oRequestObject.request, oResponse, oRequestObject.fnError);
 						} else {
@@ -2031,13 +2027,35 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 		};
 
 		var handleError = function(oError) {
+			var oEventInfo,
+				bAborted = oRequestHandle && oRequestHandle.bAborted;
+			// Call procesError for all contained requests first
+			jQuery.each(aRequests, function(i, oRequest) {
+				if (jQuery.isArray(oRequest)) {
+					jQuery.each(oRequest, function(i, oRequest) {
+						if (bAborted) {
+							that._processAborted(oRequest.request, oError, oRequest.fnError);
+						} else {
+							that._processError(oRequest.request, oError, oRequest.fnError);
+						}
+					});
+				} else {
+					if (bAborted) {
+						that._processAborted(oRequest.request, oError, oRequest.fnError);
+					} else {
+						that._processError(oRequest.request, oError, oRequest.fnError);
+					}
+				}
+			});
+			// Call callback and fire events for the batch request
 			if (fnError) {
 				fnError(oError);
 			}
-			fireEvent("Completed", oBatchRequest, oError, aRequests);
+			oEventInfo = that._createEventInfo(oBatchRequest, oError, aRequests);
+			that.fireBatchRequestCompleted(oEventInfo);
 			// Don't fire RequestFailed for intentionally aborted requests; fire event if we have no (OData.read fails before handle creation)
-			if (!oRequestHandle || !oRequestHandle.bAborted) {
-				fireEvent("Failed", oBatchRequest, oError, aRequests);
+			if (!bAborted) {
+				that.fireBatchRequestFailed(oEventInfo);
 			}
 		};
 		
@@ -2239,7 +2257,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 							oChangeSet = {__changeRequests:[]};
 							for (var i = 0; i < aChangeSet.length; i++) {
 								//clear metadata.create
-								if (!aChangeSet[i].request._aborted) {
+								if (aChangeSet[i].request._aborted) {
+									that._processAborted(aChangeSet[i].request, null, aChangeSet[i].fnError);
+								} else {
 									if (aChangeSet[i].request.data && aChangeSet[i].request.data.__metadata) {
 										delete aChangeSet[i].request.data.__metadata.created;
 									}
@@ -2255,7 +2275,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 					if (oGroup.requests) {
 						var aRequests = oGroup.requests;
 						for (var i = 0; i < aRequests.length; i++) {
-							if (!aRequests[i].request._aborted) {
+							if (aRequests[i].request._aborted) {
+								that._processAborted(aRequests[i].request, null, aRequests[i].fnError);
+							} else {
 								aReadRequests.push(aRequests[i].request);
 								aBatchGroup.push(aRequests[i]);
 							}
@@ -2277,8 +2299,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 						jQuery.each(oGroup.changes, function(sChangeSetId, aChangeSet){
 							for (var i = 0; i < aChangeSet.length; i++) {
 								// store last request Handle. If no batch there will be only 1 and we cpould return it?
-								if (!aChangeSet[i].request._aborted) {
-									oRequestHandle.push(that._submitSingleRequest(aChangeSet[i].request, aChangeSet[i].fnSuccess, aChangeSet[i].fnError));
+								if (aChangeSet[i].request._aborted) {
+									that._processAborted(aChangeSet[i].request, null, aChangeSet[i].fnError);
+								} else {
+									aChangeSet[i].request._handle = that._submitSingleRequest(aChangeSet[i].request, aChangeSet[i].fnSuccess, aChangeSet[i].fnError);
+									oRequestHandle.push(aChangeSet[i].request._handle);
 								}
 							}
 						});
@@ -2287,8 +2312,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 						var aRequests = oGroup.requests;
 						for (var i = 0; i < aRequests.length; i++) {
 							// store last request Handle. If no batch there will be only 1 and we cpould return it?
-							if (!aRequests[i].request._aborted) {
-								oRequestHandle.push(that._submitSingleRequest(aRequests[i].request, aRequests[i].fnSuccess, aRequests[i].fnError));
+							if (aRequests[i].request._aborted) {
+								that._processAborted(aRequests[i].request, null, aRequests[i].fnError);
+							} else {
+								aRequests[i].request._handle = that._submitSingleRequest(aRequests[i].request, aRequests[i].fnSuccess, aRequests[i].fnError);
+								oRequestHandle.push(aRequests[i].request._handle);
 							}
 						}
 					}
@@ -2394,7 +2422,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 	};
 
 	/**
-	 * process request response for successful requests
+	 * process request response for failed requests
 	 *
 	 * @param {object} oRequest The request
 	 * @param {object} oResponse The response
@@ -2418,12 +2446,26 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 	 *
 	 * @param {object} oRequest The request
 	 * @param {object} oResponse The response
+	 * @param {function} fnError The error callback function
 	 * @private
 	 */
-	ODataModel.prototype._processAborted = function(oRequest, oResponse) {
-		var oEventInfo = this._createEventInfo(oRequest, oResponse);
-		oEventInfo.success = false;
-		this.fireRequestCompleted(oEventInfo);
+	ODataModel.prototype._processAborted = function(oRequest, oResponse, fnError) {
+		var oError = {
+			message: "Request aborted",
+			statusCode: 0,
+			statusText: "abort",
+			headers: {},
+			responseText: ""
+		};
+		if (fnError) {
+			fnError(oError);
+		}
+		// If no response is contained, request was never sent and completes event can be omitted
+		if (oResponse) {
+			var oEventInfo = this._createEventInfo(oRequest, oError);
+			oEventInfo.success = false;
+			this.fireRequestCompleted(oEventInfo);
+		}
 	};
 	
 	/**
@@ -2723,19 +2765,15 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 	 * @return {object} an object which has an <code>abort</code> function to abort the current request.
 	 */
 	ODataModel.prototype._processRequest = function(fnProcessRequest) {
-		var oRequestHandle, oRequestHandleInternal, oRequest,  
+		var oRequestHandle, oRequest,  
 			bAborted = false,
 			that = this;
 		
 		this.oMetadata.loaded().then(function() {			
 			oRequest = fnProcessRequest();
 			
-			if (that.bUseBatch) {
-				if (!that.oRequestTimer) {
-					that.oRequestTimer = jQuery.sap.delayedCall(0, that, that._processRequestQueue, [that.mRequests]);
-				}
-			} else {
-				oRequestHandleInternal = that._processRequestQueue(that.mRequests);
+			if (!that.oRequestTimer) {
+				that.oRequestTimer = jQuery.sap.delayedCall(0, that, that._processRequestQueue, [that.mRequests]);
 			}
 			
 			if (bAborted) {
@@ -2746,11 +2784,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 		oRequestHandle = {
 			abort: function() {
 				bAborted = true;
-				if (that.bUseBatch && oRequest) {
+				if (oRequest) {
 					oRequest._aborted = true;
-				} else if (oRequestHandleInternal) {
-					oRequestHandleInternal.abort();
-				}
+					if (oRequest._handle) {
+						oRequest._handle.abort();
+					}
+				} 
 			}
 		};
 
