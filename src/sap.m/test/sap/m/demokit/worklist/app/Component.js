1b3828883c1fce645254f4e5c8de73da43b4fcb1
Andreas Kunz <andreas.kunz@sap.com>
2015-03-27 17:27:02 +0100

    [INTERNAL] Template apps: add cozy mode and cleanup form factor settings
    Change-Id: I60ff6f435b2382ea039ff1d87b2d81c9a59ba58e

--- src/sap.m/test/sap/m/demokit/worklist/app/Component.js
+++ src/sap.m/test/sap/m/demokit/worklist/app/Component.js
@@ -64,12 +64,23 @@ sap.ui.define([
 
 			// call the base component's createContent function
 			var oRootView = UIComponent.prototype.createContent.apply(this, arguments);
-
-			if (!sap.ui.Device.support.touch) { // apply compact mode if touch is not supported; this could me made configurable on "combi" devices with touch AND mouse
-				oRootView.addStyleClass("sapUiSizeCompact");
-			}
-
+			oRootView.addStyleClass(this.getCompactCozyClass());
 			return oRootView;
+		},
+
+		/**
+		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy design mode class should be set, which influences the size appearance of some controls.
+		 * @public
+		 */
+		getCompactCozyClass : function() {
+			if (!this._sCompactCozyClass) {
+				if (!sap.ui.Device.support.touch) { // apply compact mode if touch is not supported; this could me made configurable for the user on "combi" devices with touch AND mouse
+					this._sCompactCozyClass = "sapUiSizeCompact";
+				} else {
+					this._sCompactCozyClass = "sapUiSizeCozy"; // needed for desktop-first controls like sap.ui.table.Table
+				}
+			}
+			return this._sCompactCozyClass;
 		},
 
 		/**
