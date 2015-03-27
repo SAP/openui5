9fb44a2d94b749d772d806a10c544538ed0182c6
Andreas Kunz <andreas.kunz@sap.com>
2015-03-27 10:30:11 +0100

    [INTERNAL] Template Apps: cleanup manifest and Component.js
    - correct descriptor i18n path
    - remove unneeded properties
    - use service config in manifest for i18n model
    
    Change-Id: I2c2e133e7cd5bfda680945fe10005add03943554

--- src/sap.m/test/sap/m/demokit/md-template/app/Component.js
+++ src/sap.m/test/sap/m/demokit/md-template/app/Component.js
@@ -14,14 +14,7 @@ sap.ui.define([
 	return UIComponent.extend("sap.ui.demo.mdtemplate.Component", {
 
 		metadata : {
-			name : "MD Template",
-			manifest: "json",
-
-			config : {
-				// always use absolute paths relative to our own component
-				// (relative paths will fail if running in the Fiori Launchpad)
-				rootPath: jQuery.sap.getModulePath("sap.ui.demo.mdtemplate")
-			}
+			manifest: "json"
 		},
 
 		/**
@@ -35,11 +28,6 @@ sap.ui.define([
 
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
-			// set the internationalization model
-			this.setModel(new ResourceModel({
-				bundleName : mConfig.i18nBundle
-			}), "i18n");
 
 			this.oListSelector = new ListSelector();
 
