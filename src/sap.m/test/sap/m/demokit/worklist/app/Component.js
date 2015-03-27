64362491572f6cbcab6367df0e7e720dc8fb6e80
Andreas Kunz <andreas.kunz@sap.com>
2015-03-27 15:11:02 +0100

    [INTERNAL] Template Apps: clean up manifest.json
    Change-Id: Ib7edd61e9d116f9b26e48820b39a77f1897dcc4a
    CR-Id: 012003146900000659592015

--- src/sap.m/test/sap/m/demokit/worklist/app/Component.js
+++ src/sap.m/test/sap/m/demokit/worklist/app/Component.js
@@ -10,14 +10,7 @@ sap.ui.define([
 	return UIComponent.extend("sap.ui.demo.worklist.Component", {
 
 		metadata : {
-			name : "Worklist Template",
-			manifest: "json",
-
-			"config": {
-				// always use absolute paths relative to our own component
-				// (relative paths will fail if running in the Fiori Launchpad)
-				rootPath: jQuery.sap.getModulePath("sap.ui.demo.worklist")
-			}
+			manifest: "json"
 		},
 
 		/**
