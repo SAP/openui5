02827cb83e6de998e156be188ce275218f151be3
Denise Nepraunig <denise.nepraunig@sap.com>
2015-04-10 16:52:36 +0200

    [INTERNAL] md-template turn on tests for phantomJS
    Some test journeys are not executed when phantomJS is used and
    pass the voter.
    In the future the the tests should run on all browsers and succeed -
    this is an open TODO.
    
    Change-Id: Ie6d89a1d79d4859ac83d66eef764bdaefb3831ef

--- src/sap.m/test/sap/m/demokit/md-template/test/integration/AllJourneys.js
+++ src/sap.m/test/sap/m/demokit/md-template/test/integration/AllJourneys.js
@@ -20,7 +20,7 @@ jQuery.sap.require("sap.ui.demo.mdtemplate.test.integration.MasterJourney");
 jQuery.sap.require("sap.ui.demo.mdtemplate.test.integration.NavigationJourney");
 //TODO Following test does not work on Firefox or IE. There seems to be a problem with OPA's back and forward in history simulation
 //Also, the URL parameter 'sap-ui-xx-fakeOS=ios' does not seem to work in this case, the screen still shows the app in desktop mode.
-if (sap.ui.Device.browser.chrome) {
+if (sap.ui.Device.browser.chrome || sap.ui.Device.browser.phantomJS) {
 	jQuery.sap.require("sap.ui.demo.mdtemplate.test.integration.NavigationJourneyPhone");
 	jQuery.sap.require("sap.ui.demo.mdtemplate.test.integration.NotFoundJourney");
 	jQuery.sap.require("sap.ui.demo.mdtemplate.test.integration.BusyJourney");
