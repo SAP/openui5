SAP/openui5

commit b1927835fc1e223ce49d3cb8fadb8592547d2707

[FIX] MatrixLayoutCell: alignment (Chrome)
Use CSS instead of align/valign attributes as those are deprecated:
http://www.w3.org/TR/html-markup/td.html#td-constraints

Fixes https://github.com/SAP/openui5/issues/367

BCP: 1570035703
Change-Id: Ib2e4366b748260ece1258d1c0b4153f422f0535b
(cherry picked from commit 0bd6f69977823b4b22f97db38a798f2535c1182f)
CR-Id: 012003146900000988352015

src/sap.ui.commons/src/sap/ui/commons/layout/MatrixLayoutRenderer.js
@@ -188,11 +188,11 @@ sap.ui.define(['jquery.sap.global'],
 	
 					var sHAlign = r.getHAlign(oMatrixLayoutCell.getHAlign(), bRTL);
 					if (sHAlign) {
-						rm.writeAttribute("align", sHAlign);
+						rm.addStyle("text-align", sHAlign);
 					}
 					var sVAlign = r.getVAlign(oMatrixLayoutCell.getVAlign());
-					if (sVAlign && sVAlign != "middle") { //middle is default, not needed
-						rm.writeAttribute("valign", sVAlign);
+					if (sVAlign) {
+						rm.addStyle("vertical-align", sVAlign);
 					}
 					if (oMatrixLayoutCell.getColSpan() > 1) {
 						rm.writeAttribute("colspan", oMatrixLayoutCell.getColSpan());

commit 743eae05583196577c213af99ea3ec8d922ba4a8

[FIX] MatrixLayoutCell: alignment
Fixes regression introduced with
b1927835fc1e223ce49d3cb8fadb8592547d2707

Change-Id: I12e105490cda8340b560602263cc8f504d840d15
CR-Id: 012003146900001084982015

src/sap.ui.commons/src/sap/ui/commons/layout/MatrixLayoutRenderer.js
@@ -186,9 +186,9 @@ sap.ui.define(['jquery.sap.global'],
 						//rm.addStyle("text-overflow", "ellipsis");
 					}
 	
-					var sHAlign = r.getHAlign(oMatrixLayoutCell.getHAlign(), bRTL);
+					var sHAlign = r.getHAlignClass(oMatrixLayoutCell.getHAlign(), bRTL);
 					if (sHAlign) {
-						rm.addStyle("text-align", sHAlign);
+						rm.addClass(sHAlign);
 					}
 					var sVAlign = r.getVAlign(oMatrixLayoutCell.getVAlign());
 					if (sVAlign) {
@@ -412,33 +412,38 @@ sap.ui.define(['jquery.sap.global'],
 	};
 	
 	/**
-	 * Returns the value for the HTML "align" attribute according to the given
-	 * horizontal alignment and RTL mode, or NULL if the HTML default is fine.
+	 * Returns the a classname according to the given
+	 * horizontal alignment and RTL mode or null if an invalid value.
+	 * was given.
 	 *
 	 * @param {sap.ui.commons.layout.HAlign} oHAlign
 	 * @param {boolean} bRTL
 	 * @type string
 	 */
-	MatrixLayoutRenderer.getHAlign = function(oHAlign, bRTL) {
-	  switch (oHAlign) {
+	MatrixLayoutRenderer.getHAlignClass = function(oHAlign, bRTL) {
+		var sClassPrefix = "sapUiMltCellHAlign";
+
+		switch (oHAlign) {
 		case sap.ui.commons.layout.HAlign.Begin:
-		  return null; // this is the HTML default
-	
+			return sClassPrefix + (bRTL ? "Right" : "Left");
+
 		case sap.ui.commons.layout.HAlign.Center:
-		  return "center";
-	
+			return sClassPrefix + "Center";
+
 		case sap.ui.commons.layout.HAlign.End:
-		  return bRTL ? "left" : "right";
-	
+			return sClassPrefix + (bRTL ? "Left" : "Right");
+
 		case sap.ui.commons.layout.HAlign.Left:
-		  return bRTL ? "left" : null;
-	
+			return sClassPrefix + "Left";
+
 		case sap.ui.commons.layout.HAlign.Right:
-		  return bRTL ? null : "right";
-	  }
-	
-	  jQuery.sap.assert(false, "MatrixLayoutRenderer.getHAlign: oHAlign must be a known value");
-	  return null;
+			return sClassPrefix + "Right";
+
+		default:
+			jQuery.sap.assert(false, "MatrixLayoutRenderer.getHAlign: oHAlign must be a known value");
+			return null;
+		}
+
 	};
 	
 	/**

src/sap.ui.commons/src/sap/ui/commons/themes/base/layout.less
@@ -16,6 +16,32 @@
 	box-sizing: border-box;
 }
 
+/* HAlign */
+.sapUiMltCellHAlignCenter {
+	text-align: center;
+}
+.sapUiMltCellHAlignLeft {
+	text-align: left;
+}
+.sapUiMltCellHAlignRight {
+	text-align: right;
+}
+
+.sapUiMltCellHAlignCenter > * {
+	margin-left: auto;
+	margin-right: auto;
+}
+.sapUiMltCellHAlignLeft > *,
+html[dir=rtl] .sapUiMltCellHAlignRight > * {
+	margin-right: auto;
+	margin-left: 0;
+}
+.sapUiMltCellHAlignRight > *,
+html[dir=rtl] .sapUiMltCellHAlignLeft > * {
+	margin-left: auto;
+	margin-right: 0;
+}
+
 /* BackGround design */
 .sapUiMltBgBorder   {background-color:@sapUiMediumBorder;}
 .sapUiMltBgFill1    {background-color:@sapUiHighlight;}