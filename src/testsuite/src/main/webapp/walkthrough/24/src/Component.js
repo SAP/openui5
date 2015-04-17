09a96b865ab19cbc10ff19f5ad0238bb71ea9a28
Michael Graf <mi.graf@sap.com>
2015-04-17 10:31:37 +0200

    [INTERNAL] walkthrough: corrections
    - added more fields to the app descriptor
    - cleaned up Component.js
    - minor stuff
    
    Change-Id: I37cab00263f5099057aec78754a7a01ca0f704f0

--- src/testsuite/src/main/webapp/walkthrough/24/src/Component.js
+++ src/testsuite/src/main/webapp/walkthrough/24/src/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel",
 	"sap/ui/demo/wt/controller/HelloDialog"
-], function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
+], function (UIComponent, JSONModel, HelloDialog) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
