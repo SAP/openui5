127d99f76b4e64ef4b5ead2af0ba59c09d699c9c
Wenqian.Wang <wenqian.wang@sap.com>
2015-04-22 10:32:36 +0200

    [FIX] sap.m.MultiInput: enhance layout for multiline multiinput in smart table
    Change-Id: I1d90579455b820fae2c95dbbb9ffbc4363d85d3b
    (cherry picked from commit 97bf46d3aa5d3e0345aaf5102a3edb4f6418c72d)
    CR-Id: 012003146900001409872015

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -429,9 +429,10 @@ sap.ui.define(['jquery.sap.global', './Input', './Token', './library', 'sap/ui/c
 			this._$input.parent().addClass("sapMMultiInputMultiModeInputContainer");
 		}
 
-		// necessary to display expanded MultiInput which is inside SimpleForm
-		if (this.$().parent('[class*="sapUiRespGridSpan"]')) {
-			this.$().parent('[class*="sapUiRespGridSpan"]').css("overflow", "visible");
+		// necessary to display expanded MultiInput which is inside layout
+		var $parent = this.$().parent('[class*="sapUiRespGridSpan"]') || this.$().parent('[class*="sapUiVlt"]');
+		if ($parent) {
+			$parent.css("overflow", "visible");
 		}
 		
 	};
@@ -448,10 +449,6 @@ sap.ui.define(['jquery.sap.global', './Input', './Token', './library', 'sap/ui/c
 				this._$input.parent().removeClass("sapMMultiInputMultiModeInputContainer");
 			}
 			
-			// set overflow of sapUiRespGridSpan back to hidden
-			if (this.$().parent('[class*="sapUiRespGridSpan"]')) {
-				this.$().parent('[class*="sapUiRespGridSpan"]').css("overflow", "hidden");
-			}
 	};
 
 	/**
