4a3b934adfbfdeefa93b06bb8ae904ffc74d4928
Sebastian Ried <sebastian.ried@sap.com>
2015-04-24 10:34:20 +0200

    [FIX] sap.ui.table.Table: MultiHeader Column Menu Opening DomRef
    - fixes that the column menu opens in a wrong place when the table uses MultiHeader
    
    Change-Id: Ib21e8fdeaf2c4c683b3e61661f1d90632f286574
    BCP: 1570155474

--- src/sap.ui.table/src/sap/ui/table/Table.js
+++ src/sap.ui.table/src/sap/ui/table/Table.js
@@ -2616,9 +2616,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 		var $Target = jQuery(oEvent.target);
 		var $Header = $Target.closest('.sapUiTableCol');
 		if ($Header.length > 0) {
-			var oColumn = $Header.control(0);
+			var oColumn = sap.ui.getCore().byId($Header.attr("data-sap-ui-colid"));
 			if (oColumn) {
-				oColumn._openMenu();
+				oColumn._openMenu($Header[0]);
 			}
 			oEvent.preventDefault();
 		} else {
@@ -2799,7 +2799,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 		if (this._bShowMenu && $col.length === 1) {
 			var iIndex = parseInt($col.attr("data-sap-ui-colindex"), 10);
 			var oColumn = this.getColumns()[iIndex];
-			this._onColumnSelect(oColumn);
+			this._onColumnSelect(oColumn, $col[0]);
 			return;
 		}
 
@@ -2928,7 +2928,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 	 * column select event => opens the column menu
 	 * @private
 	 */
-	Table.prototype._onColumnSelect = function(oColumn) {
+	Table.prototype._onColumnSelect = function(oColumn, oDomRef) {
 		// forward the event
 		var bExecuteDefault = this.fireColumnSelect({
 			column: oColumn
@@ -2937,7 +2937,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Interval
 		// if the default behavior should be prevented we suppress to open
 		// the column menu!
 		if (bExecuteDefault) {
-			oColumn._openMenu();
+			oColumn._openMenu(oDomRef);
 		}
 
 	};
