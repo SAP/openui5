10921f506fe967f6eb6d021e1fc86266970a6312
Ruben Tuider <ruben.tuider@sap.com>
2015-06-02 15:59:59 +0200

    [FIX] core.LibraryInfo: Added Check for Special Cases
    When calling "_getActualComponent" the special cases for the component
    info is fetched. 
    If there is no component info available for the provided module name
    fetching the special cases don't work and the app crashes.
    Now 
    
    Change-Id: I3a4dcffc86b372b8953c744e80b89b83d8a173b9

--- src/sap.ui.core/src/sap/ui/core/util/LibraryInfo.js
+++ src/sap.ui.core/src/sap/ui/core/util/LibraryInfo.js
@@ -24,28 +24,28 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 			BaseObject.apply(this);
 			this._oLibInfos = {};
 		},
-		
+
 		destroy : function() {
 			BaseObject.prototype.destroy.apply(this, arguments);
 			this._oLibInfos = {};
 		},
-		
+
 		getInterface : function() {
 			return this;
 		}
 	});
-	
-	
+
+
 	LibraryInfo.prototype._loadLibraryMetadata = function(sLibraryName, fnCallback) {
 		sLibraryName = sLibraryName.replace(/\//g, ".");
-		
+
 		if (this._oLibInfos[sLibraryName]) {
 			jQuery.sap.delayedCall(0, window, fnCallback, [this._oLibInfos[sLibraryName]]);
 			return;
 		}
-		
-		var that = this, 
-		    sUrl, 
+
+		var that = this,
+		    sUrl,
 		    sLibraryType,
 		    aParts = /themelib_(.*)/i.exec(sLibraryName);
 		if (!aParts) {
@@ -55,9 +55,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 		} else {
 			// theme library
 			sLibraryType = ".theme";
-			sUrl = jQuery.sap.getModulePath("sap.ui.core", '/themes/' + aParts[1] + "/");	
+			sUrl = jQuery.sap.getModulePath("sap.ui.core", '/themes/' + aParts[1] + "/");
 		}
-		
+
 		jQuery.ajax({
 			url : sUrl + sLibraryType,
 			dataType : "xml",
@@ -72,12 +72,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 			}
 		});
 	};
-	
-	
+
+
 	LibraryInfo.prototype._getLibraryInfo = function(sLibraryName, fnCallback) {
 		this._loadLibraryMetadata(sLibraryName, function(oData){
 			var result = {libs: [], library: oData.name, libraryUrl: oData.url};
-	
+
 			if (oData.data) {
 				var $data = jQuery(oData.data);
 				result.vendor = $data.find("vendor").text();
@@ -87,16 +87,16 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 				result.releasenotes = $data.find("releasenotes").attr("url"); // in the appdata section
 				result.componentInfo = LibraryInfo.prototype._getLibraryComponentInfo($data);
 			}
-			
+
 			fnCallback(result);
 		});
 	};
-	
-	
+
+
 	LibraryInfo.prototype._getThirdPartyInfo = function(sLibraryName, fnCallback) {
 		this._loadLibraryMetadata(sLibraryName, function(oData){
 			var result = {libs: [], library: oData.name, libraryUrl: oData.url};
-	
+
 			if (oData.data) {
 				var $Libs = jQuery(oData.data).find("appData").find("thirdparty").children();
 				$Libs.each(function(i, o){
@@ -115,35 +115,35 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 					}
 				});
 			}
-			
+
 			fnCallback(result);
 		});
 	};
-	
-	
+
+
 	LibraryInfo.prototype._getDocuIndex = function(sLibraryName, fnCallback) {
 		this._loadLibraryMetadata(sLibraryName, function(oData){
 			var lib = oData.name,
 				libUrl = oData.url,
 				result = {"docu": {}, library: lib, libraryUrl: libUrl};
-	
+
 			if (!oData.data) {
 				fnCallback(result);
 				return;
 			}
-				
+
 			var $Doc = jQuery(oData.data).find("appData").find("documentation");
 			var sUrl = $Doc.attr("indexUrl");
-			
+
 			if (!sUrl) {
 				fnCallback(result);
 				return;
 			}
-				
+
 			if ($Doc.attr("resolve") == "lib") {
 				sUrl = oData.url + sUrl;
 			}
-			
+
 			jQuery.ajax({
 				url : sUrl,
 				dataType : "json",
@@ -159,30 +159,30 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 			});
 		});
 	};
-	
+
 	LibraryInfo.prototype._getReleaseNotes = function(sLibraryName, sVersion, fnCallback) {
 		this._loadLibraryMetadata(sLibraryName, function(oData){
-	
+
 			if (!oData.data) {
 				fnCallback({});
 				return;
 			}
-			
+
 			var oVersion = jQuery.sap.Version(sVersion);
-			
+
 			var iMajor = oVersion.getMajor();
 			var iMinor = oVersion.getMinor();
 			var iPatch = oVersion.getPatch();
-			
+
 			var $Doc = jQuery(oData.data).find("appData").find("releasenotes");
 			var sUrl = $Doc.attr("url");
-			
+
 			if (!sUrl) {
 				jQuery.sap.log.warning("failed to load release notes for library " + sLibraryName );
 				fnCallback({});
 				return;
 			}
-			
+
 			// for SNAPSHOT versions we fallback to the next minor version, e.g.:
 			// 1.27.1-SNAPSHOT => 1.28.0
 			if (oVersion.getSuffix() === "-SNAPSHOT") {
@@ -192,18 +192,18 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 				}
 				sVersion = iMajor + "." + iMinor + "." + iPatch;
 			}
-			
+
 			// replace the placeholders for major, minor and patch
 			sUrl = sUrl.replace("{major}", iMajor);
 			sUrl = sUrl.replace("{minor}", iMinor);
 			sUrl = sUrl.replace("{patch}", iPatch);
-			
+
 			// if the URL should be resolved against the library the URL
 			// is relative to the library root path
 			if ($Doc.attr("resolve") == "lib") {
 				sUrl = oData.url + sUrl;
 			}
-			
+
 			// load the changelog/releasenotes
 			jQuery.ajax({
 				url : sUrl,
@@ -222,7 +222,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 					fnCallback(oData, sVersion);
 				}
 			});
-			
+
 		});
 	};
 
@@ -273,16 +273,23 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'jquery.sap.script'],
 	};
 
 	/**
-	*Return the control's component for Ownership app (TeamApp) & Explored app (Demokit)
-	*@param {array.<Object>} oComponentInfos object for each library with the default component and special cases
-	*@param {string} sModuleName control name, e.g. sap.m.Button
-	*@return {string} component
+	* Return the control's component for Ownership app (TeamApp) & Explored app (Demokit)
+	*
+	* @param {array.<Object>} oComponentInfos object for each library with the default component and special cases
+	* @param {string} sModuleName control name, e.g. sap.m.Button
+	* @return {string} component
+	* @private
 	*/
-
 	LibraryInfo.prototype._getActualComponent = function(oComponentInfos, sModuleName) {
 		if (sModuleName) {
 			for (var key in oComponentInfos) {
-				if (key && sModuleName.indexOf(key) === 0) {
+				if (!oComponentInfos[key]) {
+					// If no data wasn't found for the current component.
+					// This might be the case if the corresponding library info isn't deployed on the current server.
+					jQuery.sap.log.error("No library information deployed for " + key);
+				}
+
+				if (sModuleName.indexOf(key) === 0 && oComponentInfos[key]) {
 					//check for special modules
 					var bSpecialModule = false;
 					var oSpecCases = oComponentInfos[key].specialCases;
