c5e2eb6d7276aeab80831c0b18d4d2e9a1fa2498
Teodor Todorov <teodor.todorov@sap.com>
2015-06-03 17:55:53 +0300

    [FIX] sap.m.ObjectAttribute: Popover position corrected when opened from ObjectAttribute active text
    BCP: 1570462782
    Change-Id: I1bc5d9c59377e5c0629c6d7a9405eb6f4558a89f
    CR-Id: 012006153200002190912015

--- src/sap.m/src/sap/m/ObjectAttribute.js
+++ src/sap.m/src/sap/m/ObjectAttribute.js
@@ -8,11 +8,11 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	"use strict";
 
 
-	
+
 	/**
 	 * Constructor for a new ObjectAttribute.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given
 	 * @param {object} [mSettings] initial settings for the new control
 	 *
 	 * @class
@@ -27,7 +27,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var ObjectAttribute = Control.extend("sap.m.ObjectAttribute", /** @lends sap.m.ObjectAttribute.prototype */ { metadata : {
-	
+
 		library : "sap.m",
 		properties : {
 
@@ -35,12 +35,12 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			 * The object attribute title.
 			 */
 			title : {type : "string", group : "Misc", defaultValue : null},
-	
+
 			/**
 			 * The object attribute text.
 			 */
 			text : {type : "string", group : "Misc", defaultValue : null},
-	
+
 			/**
 			 * Indicates if the object attribute text is selectable by the user.
 			 */
@@ -53,20 +53,20 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			textDirection : {type : "sap.ui.core.TextDirection", group : "Appearance", defaultValue : sap.ui.core.TextDirection.Inherit}
 		},
 		aggregations : {
-	
+
 			/**
 			 * Text control to display title and text property
 			 */
 			_textControl : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"}
 		},
 		events : {
-	
+
 			/**
 			 * Event is fired when the user clicks active text
 			 */
 			press : {
 				parameters : {
-	
+
 					/**
 					 * Dom reference of the object attributes' text to be used for positioning.
 					 */
@@ -75,23 +75,23 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			}
 		}
 	}});
-	
+
 	///**
 	// * This file defines behavior for the control,
 	// */
-	
+
 	/**
 	 *  Initialize member variables
-	 * 
+	 *
 	 * @private
 	 */
 	ObjectAttribute.prototype.init = function() {
 		this.setAggregation('_textControl', new sap.m.Text());
 	};
-	
+
 	/**
 	 * Delivers text control with updated title, text and maxLines property
-	 * 
+	 *
 	 * @private
 	 */
 	ObjectAttribute.prototype._getUpdatedTextControl = function() {
@@ -120,7 +120,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 		oTextControl.setProperty('maxLines', iMaxLines, true);
 		return oTextControl;
 	};
-	
+
 	/**
 	 * @private
 	 */
@@ -132,7 +132,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			});
 		}
 	};
-	
+
 	/**
 	 * @private
 	 */
@@ -150,19 +150,19 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	sap.m.ObjectAttribute.prototype.onsapspace = function(oEvent) {
 		this.onsapenter(oEvent);
 	};
-	
+
 	/**
 	 * See 'return'.
-	 * 
+	 *
 	 * @private
 	 * @returns {boolean} true if attribute's text is empty or only consists of whitespaces.
 	 */
 	ObjectAttribute.prototype._isEmpty = function() {
 		return !(this.getText().trim() || this.getTitle().trim());
 	};
-	
+
 	/**
-	 * Function is called when the control is touched.  
+	 * Function is called when the control is touched.
 	 *
 	 * @private
 	 */
@@ -172,7 +172,17 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			oEvent.originalEvent._sapui_handledByControl = true;
 		}
 	};
-	
+
+	/**
+	 * Defines to which DOM reference the Popup should be docked
+	 *
+	 * @protected
+	 * @return {DomNode} the DOM reference that Popup should dock to
+	 */
+	ObjectAttribute.prototype.getPopupAnchorDomRef = function() {
+		return this.getDomRef("text");
+	};
+
 
 	return ObjectAttribute;
 
