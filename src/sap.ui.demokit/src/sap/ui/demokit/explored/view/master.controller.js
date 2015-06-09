cc6cca79ea8a41afd9550fb306cb65d22d80c2fb
Jens Pflueger <jens.pflueger@sap.com>
2015-06-09 18:13:37 +0200

    [INTERNAL] Explored App: Ensure to always set Density Class (either Cozy or Compact)
    The default of desktop firts controls like sap.ui.table.Table is
    different. The default "no class" = Cozy cannot be used here. Therefore
    the density class for cozy should explicitly added
    
    Change-Id: I1b80cb99d6b6afa081cbbf702d2372fbd51a98a7

--- src/sap.ui.demokit/src/sap/ui/demokit/explored/view/master.controller.js
+++ src/sap.ui.demokit/src/sap/ui/demokit/explored/view/master.controller.js
@@ -214,7 +214,7 @@ sap.ui.controller("sap.ui.demokit.explored.view.master", {
 			this._oVSDialog.setSelectedFilterKeys(aFilterKeys);
 			this._oVSDialog.setSelectedGroupItem(this._oViewSettings.groupProperty);
 			this._oVSDialog.setGroupDescending(this._oViewSettings.groupDescending);
-			jQuery('body').toggleClass("sapUiSizeCompact", this._oViewSettings.compactOn);
+			jQuery('body').toggleClass("sapUiSizeCompact", this._oViewSettings.compactOn).toggleClass("sapUiSizeCozy", !this._oViewSettings.compactOn);
 
 			// open
 			this._oVSDialog.open();
