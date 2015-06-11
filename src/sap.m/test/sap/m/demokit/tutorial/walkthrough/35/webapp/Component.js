d4d3bdfe2f9ec9d0673cfdcd0218701fe20aa233
Nabi Zamani <nabi.zamani@sap.com>
2015-06-11 15:09:35 +0200

    ContentDensity classes and Device API features added
    Change-Id: I7246c4a399b6f6d9e4e1de876fc11289ad21a55b

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/35/webapp/Component.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/35/webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/controller/HelloDialog"
-], function (UIComponent, JSONModel, HelloDialog) {
+	"sap/ui/demo/wt/controller/HelloDialog",
+	"sap/ui/Device"
+], function (UIComponent, JSONModel, HelloDialog, Device) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
@@ -25,12 +26,30 @@ sap.ui.define([
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 
+			// set device model
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
 			// set dialog
 			this.helloDialog = new HelloDialog();
 
 			// create the views based on the url/hash
 			this.getRouter().initialize();
+
+		},
+
+		getContentDensityClass : function() {
+			if (!this._sContentDensityClass) {
+				if (!sap.ui.Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy"; // needed for desktop-first controls like sap.ui.table.Table
+				}
+			}
+			return this._sContentDensityClass;
 		}
+
 	});
 
 });
