71424795bcf3dc605cec09f278ba52f3f22b30fe
Uwe Reeder <uwe.reeder@sap.com>
2015-06-19 13:01:52 +0200

    [INTERNAL] ODataModel.qunit.js: use Sinon fake server
    PS1: initial
    PS2: fake 404 response, too
    
    Change-Id: Ife94e81d3148c3af09976f512f231b262f579fb2
    JIRA: FLPUISERVICES2014-1489

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataModel.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataModel.qunit.js
@@ -12,22 +12,90 @@ sap.ui.require([
 	"use strict";
 
 	var TestControl = sap.ui.core.Element.extend("TestControl", {
-		metadata: {
-			properties: {
-				text: "string"
+			metadata: {
+				properties: {
+					text: "string"
+				}
 			}
-		}
-	});
+		}),
+		mFixture = {
+			"/foo/$metadata": 404,
+			"/Northwind/Northwind.svc/$metadata": "v2/metadata.xml",
+			"/Northwind/Northwind.svc/Products(ProductID=1)": "v2/Products(ProductID=1).json",
+			"/V4/Northwind/Northwind.svc/Products(ProductID=1)": "v4/Products(ProductID=1).json",
+			"/V4/Northwind/Northwind.svc/Products(ProductID=1)/ProductName": "v4/ProductName.json",
+		},
+		bRealOData = jQuery.sap.getUriParameters().get("realOData") === "true";
 
-	//FIX4MASTER: Use mock instead of real service calls to other servers in unit tests
-	//       (release build must not use other servers, stability requirements)
-	function getServiceUrl(sUrl) {
-		// if run locally, use proxy deployed with databinding web app on local Tomcat
-		if (jQuery.sap.startsWith(document.location.pathname, "/testsuite")) {
-			return "/databinding/proxy/http/services.odata.org/" + sUrl;
+	/**
+	 * Prepare Sinon fake server for the given fixture.
+	 *
+	 * @param {string} sBase
+	 *   The base URI for the response files.
+	 * @param {object} mFixture
+	 *   The fixture with the URLs to fake as keys and the URIs of the fake responses (relative to
+	 *   sBase) as values. The content type is determined from the response file's extension.
+	 */
+	function setupFakeServer(sBase, mFixture) {
+		var iCode,
+			mHeaders,
+			sMessage,
+			vResponse,
+			oServer,
+			sSource,
+			sUrl,
+			mUrls = {};
+
+		function contentType(sName) {
+			if (/\.xml$/.test(sName)) {
+				return "application/xml";
+			}
+			if (/\.json$/.test(sName)) {
+				return "application/json";
+			}
+			return "application/x-octet-stream";
 		}
-		// if run on central build server, use proxy deployed with qunit-tests
-		return "../../../../../../../proxy/http/services.odata.org/" + sUrl;
+
+		for (sUrl in mFixture) {
+			vResponse = mFixture[sUrl];
+			if (typeof vResponse === "number") {
+				iCode = vResponse;
+				sMessage = "";
+				mHeaders = {};
+			} else {
+				iCode = 200;
+				sSource = sBase + '/' + vResponse;
+				sMessage = jQuery.sap.syncGetText(sSource, "", null);
+				mHeaders = {"Content-Type": contentType(sSource)};
+			}
+			mUrls[sUrl] = [iCode, mHeaders, sMessage];
+		}
+
+		//TODO remove this workaround in IE9 for
+		// https://github.com/cjohansen/Sinon.JS/commit/e8de34b5ec92b622ef76267a6dce12674fee6a73
+		sinon.xhr.supportsCORS = true;
+
+		oServer = sinon.fakeServer.create(),
+		sinon.FakeXMLHttpRequest.useFilters = true;
+		sinon.FakeXMLHttpRequest.addFilter(function (sMethod, sUrl, bAsync) {
+			return !(sUrl in mFixture); // do not fake if URL is unknown
+		});
+
+		for (sUrl in mUrls) {
+			oServer.respondWith(sUrl, mUrls[sUrl]);
+		}
+		oServer.autoRespond = true;
+	}
+
+	if (!bRealOData) {
+		setupFakeServer("data", mFixture);
+	}
+
+	function getServiceUrl(sUrl) {
+		if (bRealOData) {
+			return "/databinding/proxy/http/services.odata.org" + sUrl;
+		}
+		return sUrl;
 	}
 
 	/**
@@ -36,9 +104,8 @@ sap.ui.require([
 	 * @param {string} sVersion - The OData version to use
 	 */
 	function createNorthwindModel(sVersion) {
-		//TODO
 		var sServiceUrl = getServiceUrl(
-				(sVersion === "V4" ? "V4/" : "") + "Northwind/Northwind.svc"),
+				(sVersion === "V4" ? "/V4" : "") + "/Northwind/Northwind.svc"),
 			fnConstructor = sVersion === "V4" ? ODataModel : V2ODataModel;
 
 		return new fnConstructor(sServiceUrl, {useBatch : false});
@@ -87,7 +154,7 @@ sap.ui.require([
 	].forEach(function (fnConstructor) {
 		test("empty " + fnConstructor.getMetadata().getName()
 				+ " model: property access from ManagedObject", function () {
-			var oModel = new fnConstructor("foo"),
+			var oModel = new fnConstructor("/foo"),
 				oControl = new TestControl();
 
 			oControl.setModel(oModel);
@@ -127,8 +194,7 @@ sap.ui.require([
 		});
 
 		ok(jQuery.sap.endsWith(odatajs.oData.read.args[0][0],
-				"/proxy/http/services.odata.org/V4/Northwind/Northwind.svc/Products(ProductID=1)",
-			"URL for read"));
+			"/V4/Northwind/Northwind.svc/Products(ProductID=1)", "URL for read"));
 		return oPromise;
 	});
 
@@ -141,7 +207,7 @@ sap.ui.require([
 				fnDone = oAssert.async();
 
 			oControl.setModel(oModel);
-			oControl.bindObject("/Products(ProductID=1)/");
+			oControl.bindObject("/Products(ProductID=1)");
 			oControl.bindProperty("text", "ProductName");
 			oControl.getBinding("text").attachChange(function () {
 				//v2 model sends several change events with only the last one setting the final
