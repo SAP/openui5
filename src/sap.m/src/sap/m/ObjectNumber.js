6c503a76d5bb4fc9f7918e3e863528cdc3be4a20
Teodor Todorov <teodor.todorov@sap.com>
2015-06-22 09:34:39 +0300

    [FIX] sap.m.ObjectNumber: Number unit position corrected
    Change-Id: I1145d7ae931621cc9eecbc197a16a88208cc8d81
    CR-Id: 012006153200002595212015
    BCP: 1570147815
    (cherry picked from commit 29eb927)

--- src/sap.m/src/sap/m/ObjectNumber.js
+++ src/sap.m/src/sap/m/ObjectNumber.js
@@ -3,20 +3,20 @@
  */
 
 // Provides control sap.m.ObjectNumber.
-sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
-	function(jQuery, library, Control) {
+sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/core/Renderer'],
+	function(jQuery, library, Control, Renderer) {
 	"use strict";
 
 
-	
 	/**
 	 * Constructor for a new ObjectNumber.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given
 	 * @param {object} [mSettings] initial settings for the new control
 	 *
 	 * @class
-	 * ObjectNumber displays number and number unit properties for an object. The number can be displayed using semantic colors to provide addition meaning about the object to the user.
+	 * ObjectNumber displays number and number unit properties for an object. The number can be displayed using semantic
+	 * colors to provide addition meaning about the object to the user.
 	 * @extends sap.ui.core.Control
 	 * @version ${version}
 	 *
@@ -27,33 +27,33 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var ObjectNumber = Control.extend("sap.m.ObjectNumber", /** @lends sap.m.ObjectNumber.prototype */ { metadata : {
-	
+
 		library : "sap.m",
 		properties : {
-	
+
 			/**
 			 * Number field of the object number
 			 */
 			number : {type : "string", group : "Misc", defaultValue : null},
-	
+
 			/**
 			 * Number units qualifier
-			 * @deprecated Since version 1.16.1. 
-			 * 
+			 * @deprecated Since version 1.16.1.
+			 *
 			 * Replaced by unit property due to the number before unit is redundant.
 			 */
 			numberUnit : {type : "string", group : "Misc", defaultValue : null, deprecated: true},
-	
+
 			/**
 			 * Indicates if the object number should appear emphasized
 			 */
 			emphasized : {type : "boolean", group : "Appearance", defaultValue : true},
-	
+
 			/**
 			 * The object number's value state. Setting this state will cause the number to be rendered in state-specific colors (only blue-crystal theme).
 			 */
 			state : {type : "sap.ui.core.ValueState", group : "Misc", defaultValue : sap.ui.core.ValueState.None},
-	
+
 			/**
 			 * Number units qualifier. If numberUnit and unit are both set, the unit value is used.
 			 * @since 1.16.1
@@ -71,55 +71,51 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 			textAlign : {type : "sap.ui.core.TextAlign", group : "Appearance", defaultValue : sap.ui.core.TextAlign.Begin}
 		}
 	}});
-	
-	///**
-	// * This file defines behavior for the control,
-	// */
-	
+
 	/**
 	 * String to prefix css class for number status to be used in
 	 * controler and renderer
-	 * @private 
+	 *
+	 * @private
 	 */
 	ObjectNumber.prototype._sCSSPrefixObjNumberStatus = 'sapMObjectNumberStatus';
-	
-	/*
+
+	/**
 	 * API method to set the object number's value state
+	 *
+	 * @override
+	 * @public
+	 * @param {sap.ui.core.ValueState} sState
+	 * @returns {ObjectNumber}
 	 */
 	ObjectNumber.prototype.setState = function(sState) {
 		//remove the current value state css class
 		this.$().removeClass(this._sCSSPrefixObjNumberStatus + this.getState());
-	
+
 		//do suppress rerendering
 		this.setProperty("state", sState, true);
-	
+
 		//now set the new css state class
 		this.$().addClass(this._sCSSPrefixObjNumberStatus + this.getState());
-	
+
 		return this;
 	};
-	
-	/*
+
+	/**
 	 * API method to set the text alignment of the control without rerendering the whole object number
+	 *
 	 * @override
 	 * @public
 	 * @param {sap.ui.core.TextAlign} sAlign the new value
-	 * @param suppressTextDirection if true the text direction doesn't influence to the text alignment
 	 */
 	ObjectNumber.prototype.setTextAlign = function(sAlign) {
-		var sAlignVal;
+		var sAlignVal = Renderer.getTextAlign(sAlign, this.getTextDirection());
 
-		//do suppress re-rendering
+		//do suppress rerendering
 		this.setProperty("textAlign", sAlign, true);
 
-		sAlignVal = this.getRenderer()._getTextAlignment(sAlign, this.getTextDirection());
-		if (sAlignVal) {
-			//change the inline style
-			this.$().css("text-align", sAlignVal);
-		} else {
-			// if empty set what was given by the user
-			this.$().css("text-align", sAlign);
-		}
+		sAlignVal = sAlignVal || sAlign;
+		this.$().css("text-align", sAlign);
 	};
 
 	return ObjectNumber;
