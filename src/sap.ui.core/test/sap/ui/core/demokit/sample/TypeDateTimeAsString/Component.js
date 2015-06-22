3c900738bee04fc512be645843573b275b41d9b5
Stefan Scherer <stefan.scherer@sap.com>
2015-06-22 12:16:30 +0200

    [FEATURE] Relative Time Format
    Change-Id: I2710923f14ead00db3447302a6055db5970faa7a

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeDateTimeAsString/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define(["sap/ui/core/UIComponent"],
+	function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.core.sample.TypeDateTimeAsString.Component", {
+
+		metadata: {
+			rootView: "sap.ui.core.sample.TypeDateTimeAsString.V",
+			dependencies: {
+				libs: [
+					"sap.m",
+					"sap.ui.layout"
+				]
+			},
+			config: {
+				sample: {
+					files: [
+						"V.view.xml",
+						"C.controller.js"
+					]
+				}
+			}
+		}
+	});
+});
---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeTimeAsTime/Component.js
@@ -0,0 +1,25 @@
+sap.ui.define(["sap/ui/core/UIComponent"],
+	function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.core.sample.TypeTimeAsTime.Component", {
+
+		metadata: {
+			rootView: "sap.ui.core.sample.TypeTimeAsTime.V",
+			dependencies: {
+				libs: [
+					"sap.m",
+					"sap.ui.layout"
+				]
+			},
+			config: {
+				sample: {
+					files: [
+						"V.view.xml",
+						"C.controller.js"
+					]
+				}
+			}
+		}
+	});
+});
