9cc0ea4611095fd80b7f81e32e402790c07af9b7
Jens Pflueger <jens.pflueger@sap.com>
2015-06-23 17:17:29 +0200

    [INTERNAL] sap.ui.table.Table: Extend OData Explored example with OperationMode showcase
    Change-Id: I1ccbd2fc8dbac8e153a465edc3b02cda3bae0cdf

--- src/sap.ui.table/test/sap/ui/table/demokit/sample/OData/Controller.controller.js
+++ src/sap.ui.table/test/sap/ui/table/demokit/sample/OData/Controller.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/table/sample/TableExampleUtils",
 	"sap/ui/core/util/MockServer",
-	"sap/ui/model/odata/v2/ODataModel"
-], function(Controller, TableExampleUtils, MockServer, ODataModel) {
+	"sap/ui/model/odata/v2/ODataModel",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/odata/OperationMode"
+], function(Controller, TableExampleUtils, MockServer, ODataModel, JSONModel, OperationMode) {
 	"use strict";
 	
 	var sServiceUrl = "http://my.test.service.com/";
@@ -26,21 +28,25 @@ sap.ui.define([
 			this.oMockServer.start();
 			
 			var oView = this.getView();
-			var oTable = oView.byId("table");
-			var oBusyIndicator = oTable.getNoData();
-			
+			this.oBusyIndicator = this.getTable().getNoData();
 			oView.setModel(new ODataModel(sServiceUrl));
 			
-			var oBinding = oTable.getBinding("rows");
-			oBinding.attachDataRequested(function(){
-				oTable.setNoData(oBusyIndicator);
-			});
-			oBinding.attachDataReceived(function(){
-				oTable.setNoData(null); //Use default again ("No Data" in case no data is available)
-			});
+			this.initBindingEventHandler();
+			
+			var oUiData = {
+				operationModes : [],
+				selectedOperationMode : OperationMode.Server
+			};
+			for (var mode in OperationMode) {
+				oUiData.operationModes.push({name: OperationMode[mode]});
+			}
+			oView.setModel(new JSONModel(oUiData), "ui");
 		},
 		
 		onExit : function () {
+			this.oBusyIndicator.destroy();
+			this.oBusyIndicator = null;
+			
 			this.oMockServer.destroy();
 			this.oMockServer = null;
 			MockServer.config({autoRespondAfter: 0});
@@ -53,8 +59,34 @@ sap.ui.define([
 			return null;
 		},
 		
-		refreshModel : function() {
-			this.getView().byId("table").getBinding().refresh(true);
+		getTable : function(){
+			return this.getView().byId("table");
+		},
+		
+		onModelRefresh : function() {
+			this.getTable().getBinding().refresh(true);
+		},
+		
+		onOperationModeChange : function(oEvent) {
+			this.getTable().bindRows({
+				path: "/ProductSet",
+				parameters: {operationMode: oEvent.getParameter("key")}
+			});
+			this.initBindingEventHandler();
+			this.onModelRefresh();
+		},
+		
+		initBindingEventHandler : function(){
+			var oBusyIndicator = this.oBusyIndicator;
+			var oTable = this.getTable();
+			var oBinding = oTable.getBinding("rows");
+			
+			oBinding.attachDataRequested(function(){
+				oTable.setNoData(oBusyIndicator);
+			});
+			oBinding.attachDataReceived(function(){
+				oTable.setNoData(null); //Use default again ("No Data" in case no data is available)
+			});
 		},
 		
 		showInfo : function(oEvent) {
