988a4698034b4efcc4dac327c29f24123acb05d1
Siarhei Ulasenka <siarhei.ulasenka@sap.com>
2015-07-01 11:19:25 +0200

    [FIX] sap.m.Input: value help for read-only inputs
    - fix: value help event is fired on read-only inputs by F4 key
    - fix: minimum suggestion value length is ignored on phones
    
    Change-Id: I7210d0670595de337e8a2a895606fc7d2fdbde2c
    CR-Id: 012006153200002865862015

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -798,7 +798,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 			if (this._oList instanceof Table) {
 				// CSN# 1421140/2014: hide the table for empty/initial results to not show the table columns
 				this._oList.addStyleClass("sapMInputSuggestionTableHidden");
-			} else {
+			} else if (this._oList) {
 				this._oList.destroyItems();
 			}
 		} else if (this._oSuggestionPopup && this._oSuggestionPopup.isOpen()) {
@@ -1129,7 +1129,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 					var sValue = oInput.getValue();
 
 					oInput._oPopupInput.setValue(sValue);
-					oInput.fireSuggest({suggestValue : sValue});
+					oInput._triggerSuggest(sValue);
 					refreshListItems(oInput);
 				}));
 
@@ -1429,7 +1429,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 	 * @private
 	 */
 	Input.prototype.onsapshow = function (oEvent) {
-		if (!this.getEnabled() || !this.getShowValueHelp()) {
+		if (!this.getEnabled() || !this.getEditable() || !this.getShowValueHelp()) {
 			return;
 		}
 
