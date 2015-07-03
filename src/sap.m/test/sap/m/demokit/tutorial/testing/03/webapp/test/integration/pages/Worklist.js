79c900139f255e8a50674cb6483a09fd5959e28b
Michael Graf <mi.graf@sap.com>
2015-07-03 14:47:57 +0200

    [INTERNAL] testing tutorial: initial commit (step 1-9)
    - finalized step 1-8
    - step 9 wip
    - step 10-XX still missing
    - removed temp data is still in the project, we have to delete it again later
    - marked tutorial as preview
    
    Change-Id: I61a63e2170d7f442152d6a634efec6dbea5ce89a

---
+++ src/sap.m/test/sap/m/demokit/tutorial/testing/03/webapp/test/integration/pages/Worklist.js
@@ -0,0 +1,109 @@
+sap.ui.require([
+		'sap/ui/test/Opa5',
+		'sap/ui/test/matchers/AggregationLengthEquals',
+		'sap/ui/test/matchers/AggregationFilled',
+		'sap/ui/test/matchers/PropertyStrictEquals',
+		'sap/ui/demo/bulletinboard/test/integration/pages/Common'
+	],
+	function (Opa5, AggregationLengthEquals, AggregationFilled, PropertyStrictEquals, Common) {
+		"use strict";
+
+		var sViewName = "Worklist",
+			sTableId = "table";
+
+
+		function createWaitForItemAtPosition(oOptions) {
+			var iPosition = oOptions.position;
+			return {
+				id: sTableId,
+				viewName: sViewName,
+				matchers: function (oTable) {
+					return oTable.getItems()[iPosition];
+				},
+				success: oOptions.success,
+				errorMessage: "Table in view '" + sViewName + "' does not contain an Item at position '" + iPosition + "'"
+			};
+		}
+
+		Opa5.createPageObjects({
+			onTheWorklistPage: {
+				baseClass: Common,
+				actions: {
+					iWaitUntilTheTableIsLoaded: function () {
+						return this.waitFor({
+							id: sTableId,
+							viewName: sViewName,
+							matchers: [new AggregationFilled({name: "items"})],
+							errorMessage: "The Table has not been loaded"
+						});
+					}
+
+				},
+				assertions: {
+
+					iShouldSeeTheTable: function () {
+						return this.waitFor({
+							id: sTableId,
+							viewName: sViewName,
+							success: function (oTable) {
+								ok(oTable, "Found the object Table");
+							},
+							errorMessage: "Can't see the master Table."
+						});
+					},
+
+					theTableShouldHaveAllEntries: function () {
+						return this.waitFor({
+							id: sTableId,
+							viewName: sViewName,
+							matchers: function (oTable) {
+								var bGrowing = oTable.getGrowing(),
+									iExpectedItems = (bGrowing ? oTable.getGrowingThreshold() : oTable.getItems().length);
+
+								return new AggregationLengthEquals({
+									name: "items",
+									length: iExpectedItems
+								}).isMatching(oTable);
+							},
+							success: function (oTable) {
+								var bGrowing = oTable.getGrowing(),
+									iExpectedItems = (bGrowing ? oTable.getGrowingThreshold() : oTable.getItems().length);
+
+								strictEqual(oTable.getItems().length, iExpectedItems, "The table has " + iExpectedItems + " items");
+							},
+							errorMessage: "Table does not have all entries."
+						});
+					},
+
+					theTitleShouldDisplayTheTotalAmountOfItems: function () {
+						return this.waitFor({
+							id: sTableId,
+							viewName: sViewName,
+							matchers: new AggregationFilled({name: "items"}),
+							success: function (oTable) {
+								var iObjectCount = oTable.getBinding("items").getLength();
+								this.waitFor({
+									id: "tableHeader",
+									viewName: sViewName,
+									matchers: function (oPage) {
+										var sExpectedText = oPage.getModel("i18n").getResourceBundle().getText("worklistTableTitleCount", [iObjectCount]);
+										return new PropertyStrictEquals({
+											name: "text",
+											value: sExpectedText
+										}).isMatching(oPage);
+									},
+									success: function () {
+										ok(true, "The Page has a title containing the number " + iObjectCount);
+									},
+									errorMessage: "The Page's header does not container the number of items " + iObjectCount
+								});
+							},
+							errorMessage: "The table has no items."
+						});
+					}
+
+				}
+			}
+		});
+
+	});
---
+++ src/sap.m/test/sap/m/demokit/tutorial/testing/06/webapp/test/integration/AllJourneys.js
@@ -0,0 +1,20 @@
+jQuery.sap.require("sap.ui.qunit.qunit-css");
+jQuery.sap.require("sap.ui.thirdparty.qunit");
+jQuery.sap.require("sap.ui.qunit.qunit-junit");
+jQuery.sap.require("sap.ui.test.opaQunit");
+jQuery.sap.require("sap.ui.test.Opa5");
+
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.pages.Common");
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.pages.Browser");
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.pages.Worklist");
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.pages.Object");
+
+sap.ui.test.Opa5.extendConfig({
+	arrangements: new sap.ui.demo.bulletinboard.test.integration.pages.Common(),
+	viewNamespace: "sap.ui.demo.bulletinboard.view."
+});
+
+// Start the tests
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.WorklistJourney");
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.ObjectJourney");
+jQuery.sap.require("sap.ui.demo.bulletinboard.test.integration.NavigationJourney");
