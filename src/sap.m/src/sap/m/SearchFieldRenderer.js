831b2d407b003164f3cc3a8e790ffdf84ac62b61
Siarhei Ulasenka <siarhei.ulasenka@sap.com>
2015-07-03 14:29:36 +0200

    [FIX] sap.m.searchField: incorrect button state in Firefox
    - inactive search field buttons sometimes
      have a blue (active) background in Firefox
    
    Change-Id: I40090d49433f1c3f2c9a827483d8296599e2cc38
    CR-Id: 012006153200002987892015
    BCP: 1580016637

--- src/sap.m/src/sap/m/SearchFieldRenderer.js
+++ src/sap.m/src/sap/m/SearchFieldRenderer.js
@@ -114,6 +114,9 @@ sap.ui.define(['jquery.sap.global'],
 				rm.writeAttribute("id", oSF.getId() + "-reset");
 				rm.addClass("sapMSFR"); // reset
 				rm.addClass("sapMSFB"); // button
+				if (sap.ui.Device.browser.firefox) {
+					rm.addClass("sapMSFBF"); // firefox, active state by peventDefault
+				}
 				if (!bShowSearchBtn) {
 					rm.addClass("sapMSFNS"); //no search button
 				}
@@ -126,6 +129,9 @@ sap.ui.define(['jquery.sap.global'],
 					rm.writeAttribute("id", oSF.getId() + "-search");
 					rm.addClass("sapMSFS"); // search
 					rm.addClass("sapMSFB"); // button
+					if (sap.ui.Device.browser.firefox) {
+						rm.addClass("sapMSFBF"); // firefox, active state by peventDefault
+					}
 					rm.writeClasses();
 					if (oSF.getRefreshButtonTooltip()) {
 						rm.writeAttributeEscaped("title", oSF.getRefreshButtonTooltip());
