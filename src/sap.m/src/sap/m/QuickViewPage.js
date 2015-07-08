2b3e49d661b285449f08d26d6a35440c59f7c8f4
Teodor Taushanov <teodor.taushanov@sap.com>
2015-07-08 16:18:00 +0300

    [FEATURE] sap.m.QuickView:  Icons screen reader support is improved
    Change-Id: I2e5263ee2867ec634de59b890966803b26072351

--- src/sap.m/src/sap/m/QuickViewPage.js
+++ src/sap.m/src/sap/m/QuickViewPage.js
@@ -255,11 +255,14 @@ sap.ui.define([
 				if (sIcon) {
 					if (this.getIcon().indexOf("sap-icon") == 0) {
 						oIcon = new Icon({
-							src: sIcon
+							src: sIcon,
+							useIconTooltip : false,
+							tooltip : sTitle
 						});
 					} else {
 						oIcon = new Image({
-							src: sIcon
+							src: sIcon,
+							tooltip : sTitle
 						}).addStyleClass("sapUiIcon");
 					}
 
@@ -354,6 +357,7 @@ sap.ui.define([
 					if (oCurrentGroupElement.getType() == QuickViewGroupElementType.mobile) {
 						var oSmsLink = new Icon({
 							src: IconPool.getIconURI("post"),
+							tooltip : this._oResourceBundle.getText("QUICKVIEW_SEND_SMS"),
 							decorative : false,
 							customData: [new CustomData({
 								key: "phoneNumber",
