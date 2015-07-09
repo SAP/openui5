87af2c99d33805ee67eab7536968558d66008834
Benedikt Schoelch <benedikt.schoelch@sap.com>
2015-07-09 15:32:07 +0200

    [INTERNAL] UnicodeNormalizer: update to Unicode 8.0
    New reference data for unorm.js, refactoring and more extensive unit
    tests for our polyfill.
    
    Change-Id: I9001871e7ad2552b5ac61a3321253d974dcf6bb2

--- src/sap.ui.core/src/sap/ui/core/util/UnicodeNormalizer.js
+++ src/sap.ui.core/src/sap/ui/core/util/UnicodeNormalizer.js
@@ -5,11 +5,11 @@
 // Provides a polyfill for the String.prototype.normalize function for older browsers
 sap.ui.define(['sap/ui/Device'], function(Device) {
 	"use strict";
-	
+
 	/*global UNorm *///declare unusual global vars for JSLint/SAPUI5 validation
 
 	// apply polyfill if needed and when not in a mobile browser
-	if (String.prototype.normalize != undefined || Device.browser.mobile == true) {
+	if (String.prototype.normalize || Device.browser.mobile) {
 		return;
 	} else {
 		jQuery.sap.require("sap.ui.thirdparty.unorm");
