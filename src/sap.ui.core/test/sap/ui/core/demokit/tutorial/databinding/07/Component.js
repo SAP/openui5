ef4bc1de1ef3af016153fba0c61435aa21dfbdbc
Michael Graf <mi.graf@sap.com>
2015-07-13 16:19:16 +0200

    [FEATURE] sap.ui.core.tutorial.databinding: initial content
    Step 1-15: reworked handover by Chris and adopted to Explored samples
    
    Change-Id: Ifcf3c800c47ac4fb0c2bcd3c33df1a2122745854
    CR-Id: 012003146900002568432015

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/databinding/07/Component.js
@@ -0,0 +1,26 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function (UIComponent) {
+		"use strict";
+
+		var Component = UIComponent.extend("sap.ui.core.tutorial.databinding.07.Component", {
+
+			metadata: {
+				config: {
+					sample: {
+						iframe: "webapp/index.html",
+						stretch: true,
+						files: [
+							"webapp/i18n/i18n.properties",
+							"webapp/i18n/i18n_de.properties",
+							"webapp/view/App.view.xml",
+							"webapp/index.html"
+						]
+					}
+				}
+			}
+
+		});
+
+		return Component;
+
+	});
