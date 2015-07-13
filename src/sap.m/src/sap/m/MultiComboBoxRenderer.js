7166d676cc9a1b63a399cc589e9fd80bce183dc7
Arley Triana Morin <arley.triana.morin@sap.com>
2015-07-13 14:03:13 +0200

    [INTERNAL] sap.m.MultiComboBox: add CSS class for the arrow button
    Change-Id: Ia9d35c89e4272beeba397a33d5286a5749c032c3

--- src/sap.m/src/sap/m/MultiComboBoxRenderer.js
+++ src/sap.m/src/sap/m/MultiComboBoxRenderer.js
@@ -13,21 +13,21 @@ sap.ui.define(['jquery.sap.global', './ComboBoxRenderer', 'sap/ui/core/Renderer'
 
 	/**
 	 * CSS class to be applied to the HTML root element of the MultiComboBox control.
-	 * 
+	 *
 	 * @type {string}
 	 */
 	MultiComboBoxRenderer.CSS_CLASS = "sapMMultiComboBox";
 
 	/**
 	 * CSS class to be applied to the HTML root element of the MultiComboBox control.
-	 * 
+	 *
 	 * @type {string}
 	 */
 	MultiComboBoxRenderer.DOT_CSS_CLASS = ".sapMMultiComboBox";
 
 	/**
 	 * Add classes to the MultiComboBox.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager}
 	 *          oRm The RenderManager that can be used for writing to the render output buffer.
 	 * @param {sap.ui.core.Control}
@@ -54,9 +54,21 @@ sap.ui.define(['jquery.sap.global', './ComboBoxRenderer', 'sap/ui/core/Renderer'
 		oRm.addClass(MultiComboBoxRenderer.CSS_CLASS + "InputInner");
 	};
 
+	/**
+	 * Add CSS classes to the combo box arrow button, using the provided {@link sap.ui.core.RenderManager}.
+	 * To be overwritten by subclasses.
+	 *
+	 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+	 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+	 */
+	MultiComboBoxRenderer.addButtonClasses = function(oRm, oControl) {
+		ComboBoxRenderer.addButtonClasses.apply(this, arguments);
+		oRm.addClass(MultiComboBoxRenderer.CSS_CLASS + "Arrow");
+	};
+
 	MultiComboBoxRenderer.openInputTag = function(oRm, oControl) {
 		oRm.write('<div class="sapMMultiComboBoxBorder"');
-		oRm.writeAttribute("id", oControl.getId() + "-border");  // UI5 core expect a DIV with ID	
+		oRm.writeAttribute("id", oControl.getId() + "-border");  // UI5 core expect a DIV with ID
 		oRm.write(">");
 
 		oRm.renderControl(oControl._oTokenizer);
