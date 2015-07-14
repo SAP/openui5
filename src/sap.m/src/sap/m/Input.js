4058bb92f643ecba0c001ebb00ed8156dbf27ece
Siarhei Ulasenka <siarhei.ulasenka@sap.com>
2015-07-14 15:27:45 +0200

    [FIX] sap.m.Input: custom suggestion value is ignored
    - Application may set a custom input value during the
      SuggestionItemSelected event. If this value equals the
      current value of the input, it is overwriten by the
      contents of the selected list item.
    
    Change-Id: I257e90a0b9836406f8cf9dd6348c66eb69c9f91e
    CR-Id: 012006153200003289782015
    BCP: 012002523100005507042015

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -276,6 +276,9 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 
 		// Show suggestions in a full screen dialog on phones:
 		this._bFullScreen = sap.ui.Device.system.phone;
+
+		// Counter for concurrent issues with setValue:
+		this._iSetCount = 0;
 	};
 
 	/**
@@ -1214,7 +1217,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 					rememberSelections : false,
 					selectionChange : function(oEvent) {
 						var oListItem = oEvent.getParameter("listItem"),
-							sOriginalValue = oInput.getValue(),
+							iCount = oInput._iSetCount,
 							sNewValue;
 
 						// fire suggestion item select event
@@ -1223,7 +1226,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 						});
 
 						// choose which field should be used for the value
-						if (sOriginalValue !== oInput.getValue()) {
+						if (iCount !== oInput._iSetCount) {
 							// if the event handler modified the input value we take this one as new value
 							sNewValue = oInput.getValue();
 						} else if (oListItem instanceof sap.m.DisplayListItem) {
@@ -1535,7 +1538,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 				rememberSelections : false,
 				selectionChange: function (oEvent) {
 					var oInput = that,
-						sOriginalValue = oInput.getValue(),
+						iCount = oInput._iSetCount,
 						oSelectedListItem = oEvent.getParameter("listItem"),
 						sNewValue;
 
@@ -1545,7 +1548,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 					});
 
 					// choose which field should be used for the value
-					if (sOriginalValue !== oInput.getValue()) {
+					if (iCount !== oInput._iSetCount) {
 						// if the event handler modified the input value we take this one as new value
 						sNewValue = oInput.getValue();
 					} else {
@@ -1689,6 +1692,20 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 	/*           end: forward aggregation methods to table         */
 	/* =========================================================== */
 
+	/**
+	 * Setter for property <code>value</code>.
+	 *
+	 * Default value is empty/<code>undefined</code>.
+	 *
+	 * @param {string} sValue New value for property <code>value</code>.
+	 * @return {sap.m.Input} <code>this</code> to allow method chaining.
+	 * @public
+	 */
+	Input.prototype.setValue = function(sValue) {
+		this._iSetCount++;
+		InputBase.prototype.setValue.call(this, sValue);
+		return this;
+	};
 
 	/**
 	 * Getter for property <code>valueStateText</code>.
