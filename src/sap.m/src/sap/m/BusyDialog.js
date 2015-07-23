72e160c68d94cf418e975562971ec3f2a9260905
Nikolay Aleksandrenko <nikolay.aleksandrenko@sap.com>
2015-07-23 10:47:52 +0300

    [INTERNAL] sap.m.BusyDialog: documentation
    Change-Id: I3b7a9c0316d06551a2d0ee0bf0e427d64a139482

--- src/sap.m/src/sap/m/BusyDialog.js
+++ src/sap.m/src/sap/m/BusyDialog.js
@@ -8,15 +8,15 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	"use strict";
 
 
-	
+
 	/**
 	 * Constructor for a new BusyDialog.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
-	 * @param {object} [mSettings] initial settings for the new control
+	 * @param {string} [sId] ID for the new control, generated automatically if no ID is given.
+	 * @param {object} [mSettings] Initial settings for the new control.
 	 *
 	 * @class
-	 * Busy Dialog is used to indicate that the system is busy with some task and the user has to wait. During this time the UI is blocked.
+	 * BusyDialog is used to indicate that the system is busy and the user has to wait.
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
@@ -28,91 +28,90 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var BusyDialog = Control.extend("sap.m.BusyDialog", /** @lends sap.m.BusyDialog.prototype */ { metadata : {
-	
+
 		library : "sap.m",
 		properties : {
-	
+
 			/**
-			 * Optional text shown inside the popup.
+			 * Optional text displayed inside the popup.
 			 */
 			text : {type : "string", group : "Appearance", defaultValue : null},
-	
+
 			/**
-			 * Sets a title to the busy dialog. Default is no title.
+			 * Sets the title of the BusyDialog. The default value is an empty string.
 			 */
 			title : {type : "string", group : "Appearance", defaultValue : null},
-	
+
 			/**
-			 * Icon that is displayed in the dialog header. This icon is invisible in iOS platform and it's density aware that you can use the density convention (@2, @1.5, etc.) to provide higher resolution image for higher density screen.
+			 * Icon displayed in the dialog header. This icon is invisible in iOS platform and it is density aware. You can use the density convention (@2, @1.5, etc.) to provide higher resolution image for higher density screens.
 			 */
 			customIcon : {type : "sap.ui.core.URI", group : "Appearance", defaultValue : null},
-	
+
 			/**
-			 * Defines the rotation speed of the given image. If a gif is used, the speed has to be set to 0. The unit is in ms.
+			 * Defines the rotation speed of the given image. If GIF file is used, the speed has to be set to 0. The value is in milliseconds.
 			 */
 			customIconRotationSpeed : {type : "int", group : "Appearance", defaultValue : 1000},
-	
+
 			/**
-			 * If this is set to false, the src image will be loaded directly without attempting to fetch the density perfect image for high density device.
-			 * 
-			 * By default, this is set to true but then one or more requests are sent trying to get the density perfect version of image if this version of image doesn't exist on the server.
-			 * 
-			 * If bandwidth is the key for the application, set this value to false.
+			 * If this is set to <code>false</code>, the source image will be loaded directly without attempting to fetch the density perfect image for high density devices.
+			 * By default, this is set to <code>true</code> but then one or more requests are sent trying to get the density perfect version of the image.
+			 *
+			 * If bandwidth is the key for the application, set this value to <code>false</code>.
 			 */
 			customIconDensityAware : {type : "boolean", defaultValue : true},
-	
+
 			/**
-			 * Width of the provided icon. By default 44px are used.
+			 * Width of the provided icon with default value "44px".
 			 */
 			customIconWidth : {type : "sap.ui.core.CSSSize", group : "Appearance", defaultValue : "30px"},
-	
+
 			/**
-			 * Height of the provided icon. By default 44px are used.
+			 * Height of the provided icon with default value "44px".
 			 */
 			customIconHeight : {type : "sap.ui.core.CSSSize", group : "Appearance", defaultValue : "44px"},
-	
+
 			/**
 			 * The text of the cancel button. The default text is "Cancel" (translated to the respective language).
 			 */
 			cancelButtonText : {type : "string", group : "Misc", defaultValue : null},
-	
+
 			/**
-			 * A cancel button will be rendered inside the busy dialog if this property is set to true.
+			 * Indicates if the cancel button will be rendered inside the busy dialog. The default value is set to <code>false</code>.
 			 */
 			showCancelButton : {type : "boolean", group : "Appearance", defaultValue : false}
 		},
 		aggregations : {
-	
+
 			/**
 			 * The hidden aggregation for internal maintained label.
 			 */
-			_busyLabel : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"}, 
-	
+			_busyLabel : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"},
+
 			/**
 			 * The hidden aggregation for internal maintained busyIndicator.
 			 */
-			_busyIndicator : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"}, 
-	
+			_busyIndicator : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"},
+
 			/**
 			 * The hidden aggregation for internal maintained toolbar which contains the cancel button.
 			 */
-			_toolbar : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"}, 
-	
+			_toolbar : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"},
+
 			/**
 			 * The hidden aggregation for internal maintained button.
 			 */
 			_cancelButton : {type : "sap.ui.core.Control", multiple : false, visibility : "hidden"}
 		},
 		events : {
-	
+
 			/**
-			 * This event will be fired when the busy dialog is closed.
+			 * Fires when the busy dialog is closed.
 			 */
 			close : {
 				parameters : {
-	
+
 					/**
-					 * this parameter is for an app to differ for a "close" event if it was fired because user pressed cancel button or because the operation was terminated.
+					 * Indicates if the close events are triggered by a user, pressing a cancel button or because the operation was terminated.
 					 * This parameter is set to true if the close event is fired by user interaction.
 					 */
 					cancelPressed : {type : "boolean"}
@@ -120,24 +119,24 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			}
 		}
 	}});
-	
-	
+
+
 	BusyDialog.prototype.init = function(){
 		var that = this;
 		this._$window = jQuery(window);
-	
+
 		this._busyIndicator = new sap.m.BusyIndicator(this.getId() + '-busyInd', {visible: false}).addStyleClass('sapMBsyInd');
 		this.setAggregation("_busyIndicator", this._busyIndicator, true);
-		
+
 		this.iOldWinHeight = 0;
 		this._oPopup = new Popup();
 		this._oPopup.setShadow(false);
 		this._oPopup.setModal(true, 'sapMDialogBLyInit');
 		this._oPopup.setAnimations(this.openAnimation, this.closeAnimation);
-	
+
 		//the orientationchange event listener
 		this._fOrientationChange = jQuery.proxy(this._reposition, this);
-		
+
 		this._oPopup._applyPosition = function(oPosition){
 			that._setDimensions();
 			Popup.prototype._applyPosition.call(this, oPosition);
@@ -157,7 +156,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 				Popup.prototype._hideBlockLayer.call(this);
 			/*});*/
 			/*$BlockRef.toggleClass('sapMDialogBLyShown', false);*/
-			
+
 		};
 		//keyboard support for desktop environments
 		if (sap.ui.Device.system.desktop) {
@@ -170,29 +169,41 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			this._oPopup.onsapescape = fnOnEscape;
 		}
 	};
-	
+
+	/**
+	 *
+	 * @param {Object} $Ref
+	 * @param {number} iRealDuration
+	 * @param fnOpened
+	 */
 	BusyDialog.prototype.openAnimation = function($Ref, iRealDuration, fnOpened) {
 		fnOpened();
 	};
-	
+
+	/**
+	 *
+	 * @param {Object} $Ref
+	 * @param {number} iRealDuration
+	 * @param fnClose
+	 */
 	BusyDialog.prototype.closeAnimation = function($Ref, iRealDuration, fnClose) {
 		fnClose();
 	};
-	
+
 	/**
-	 * Destroys the dialog control
+	 * Destroys the BusyDialog.
 	 * @private
 	 */
 	BusyDialog.prototype.exit = function(){
 		this._oPopup.close();
 		this._oPopup.destroy();
 		this._oPopup = null;
-		
+
 		this._$window.unbind("resize", this._fOrientationChange);
 	};
-	
+
 	/**
-	 * Opens the busy popup.
+	 * Opens the BusyDialog.
 	 *
 	 * @type sap.m.BusyDialog
 	 * @public
@@ -200,7 +211,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 	 */
 	BusyDialog.prototype.open = function(){
 		jQuery.sap.log.debug("sap.m.BusyDialog.open called at " + new Date().getTime());
-		
+
 		var oPopup = this._oPopup;
 
 		if (!oPopup) {
@@ -216,31 +227,34 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		oPopup.attachOpened(this._handleOpened, this);
 		oPopup.setPosition("center center", "center center", document, "0 0", "fit");
 		oPopup.setInitialFocusId(this.getShowCancelButton() ? this._oButton.getId() : this.getId());
-	
+
 		this._bOpenRequested = true;
 		this._openNowIfPossibleAndRequested();
-	
+
 		return this;
 	};
-	
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._openNowIfPossibleAndRequested = function(){
 		if (!this._bOpenRequested) {
 			return;
 		}
-		
+
 		// If body/Core are not available yet, give them some more time and open later if still required
 		if (!document.body || !sap.ui.getCore().isInitialized()) {
 			jQuery.sap.delayedCall(50, this, "_openNowIfPossibleAndRequested");
 			return;
 		}
-		
+
 		this._bOpenRequested = false; // opening request is handled
 		this._oPopup.open();
 	};
-	
+
 	/**
-	 * Close the busy popup.
+	 * Closes the BusyDialog.
 	 *
 	 * @type sap.m.BusyDialog
 	 * @public
@@ -269,7 +283,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		}
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setText = function(sText){
 		this.setProperty("text", sText, true);
 		if (!this._oLabel) {
@@ -279,37 +293,37 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		this._oLabel.setText(sText);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCustomIcon = function(oIcon){
 		this.setProperty("customIcon", oIcon, true);
 		this._busyIndicator.setCustomIcon(oIcon);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCustomIconRotationSpeed = function(iSpeed){
 		this.setProperty("customIconRotationSpeed", iSpeed, true);
 		this._busyIndicator.setCustomIconRotationSpeed(iSpeed);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCustomIconDensityAware = function(bAware){
 		this.setProperty("customIconDensityAware", bAware, true);
 		this._busyIndicator.setCustomIconDensityAware(bAware);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCustomIconWidth = function(sWidth){
 		this.setProperty("customIconWidth", sWidth, true);
 		this._busyIndicator.setCustomIconWidth(sWidth);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCustomIconHeight = function(sHeight){
 		this.setProperty("customIconHeight", sHeight, true);
 		this._busyIndicator.setCustomIconHeight(sHeight);
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setShowCancelButton = function(bShow){
 		this.setProperty("showCancelButton", bShow, false);
 		if (bShow) {
@@ -317,19 +331,23 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		}
 		return this;
 	};
-	
+
 	BusyDialog.prototype.setCancelButtonText = function(sText){
 		this.setProperty("cancelButtonText", sText, true);
 		this._createCancelButton();
 		this._oButton.setText(sText);
 		return this;
 	};
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._createCancelButton = function(){
 		if (!this._oButton) {
 			var that = this;
 			var sButtonText = (this.getCancelButtonText()) ? this.getCancelButtonText() : sap.ui.getCore().getLibraryResourceBundle("sap.m").getText("BUSYDIALOG_CANCELBUTTON_TEXT");
-	
+
 			this._oButton = new sap.m.Button(this.getId() + 'busyCancelBtn', {
 					text: sButtonText,
 					type: sap.m.ButtonType.Transparent,
@@ -337,7 +355,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 						that.close(true);
 					}
 			}).addStyleClass("sapMDialogBtn");
-	
+
 			if (sap.ui.Device.system.phone) {
 				this._oButton.addStyleClass("sapMDialogBtnPhone");
 				this.setAggregation("_cancelButton", this._oButton, true);
@@ -352,7 +370,11 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			}
 		}
 	};
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._reposition = function() {
 		if (!this._oPopup) {
 			return;
@@ -363,7 +385,11 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		}
 		this._oPopup._applyPosition(this._oPopup._oLastPosition);
 	};
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._handleOpened = function(){
 		this._oPopup.detachOpened(this._handleOpened, this);
 		// start busy indicator
@@ -373,12 +399,20 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		//  that's why the resize event is used here.
 		this._$window.bind("resize", this._fOrientationChange);
 	};
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._handleClosed = function(){
 		this._oPopup.detachClosed(this._handleClosed, this);
 		this._$window.unbind("resize", this._fOrientationChange);
 	};
-	
+
+	/**
+	 *
+	 * @private
+	 */
 	BusyDialog.prototype._setDimensions = function() {
 		// Derive width and height from viewport
 		var iWindowHeight =  this._$window.height();
@@ -404,7 +438,12 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		}
 		this.iOldWinHeight = this._$window.height();
 	};
-	
+
+	/**
+	 *
+	 * @param {number} iWindowHeight
+	 * @private
+	 */
 	BusyDialog.prototype._checkSize = function(iWindowHeight) {
 		if (iWindowHeight < this.$()[0].scrollHeight) {
 			this.$().toggleClass("sapMBsyDSmall", true);
@@ -413,7 +452,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			this.$().css("width", "18.75em");
 		}
 	};
-	
+
 
 	return BusyDialog;
 
