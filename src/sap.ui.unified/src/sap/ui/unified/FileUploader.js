8d167c623b96ec5cef24a07aa066f7632fd55c8c
Michael Sobczak <michael.sobczak@sap.com>
2015-07-28 16:13:26 +0200

    [FIX] sap.ui.unified.FileUploader: keyboard navigation in toolbar fixed
    Change-Id: I9fe37f4009e2dbee6670004f16041c2b53735a99
    BCP: 1570675342

--- src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
+++ src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
@@ -1127,7 +1127,15 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			}
 		} else if (iKeyCode != eKC.TAB &&
 					iKeyCode != eKC.SHIFT &&
-					iKeyCode != eKC.F6) {
+					iKeyCode != eKC.F6 &&
+					iKeyCode != eKC.PAGE_UP &&
+					iKeyCode != eKC.PAGE_DOWN &&
+					iKeyCode != eKC.END &&
+					iKeyCode != eKC.HOME &&
+					iKeyCode != eKC.ARROW_LEFT &&
+					iKeyCode != eKC.ARROW_UP &&
+					iKeyCode != eKC.ARROW_RIGHT &&
+					iKeyCode != eKC.ARROW_DOWN) {
 			oEvent.preventDefault();
 			oEvent.stopPropagation();
 		}
