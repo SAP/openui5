e9192ff12c4581a55f8ba4102703a868928fb2f2
Arley Triana Morin <arley.triana.morin@sap.com>
2015-07-31 16:27:16 +0200

    [INTERNAL][FIX] sap.m.MultiComboBox: adjust arrow button height for compact mode
    Change-Id: I4774dbc6a2c4a6c8c438f5cf9f291092bfde478e
    CR-Id: 012006153200003823882015

--- src/sap.m/src/sap/m/MultiComboBoxRenderer.js
+++ src/sap.m/src/sap/m/MultiComboBoxRenderer.js
@@ -1,34 +1,33 @@
 /*!
  * ${copyright}
  */
-sap.ui.define(['jquery.sap.global', './InputBaseRenderer', 'sap/ui/core/Renderer', 'sap/ui/core/ValueStateSupport'],
-	function(jQuery, InputBaseRenderer, Renderer, ValueStateSupport) {
+sap.ui.define(['jquery.sap.global', './ComboBoxBaseRenderer', 'sap/ui/core/Renderer', 'sap/ui/core/ValueStateSupport'],
+	function(jQuery, ComboBoxBaseRenderer, Renderer, ValueStateSupport) {
 	"use strict";
-
 
 	/**
 	 * MultiComboBox renderer.
 	 * @namespace
 	 */
 	var MultiComboBoxRenderer = Renderer.extend(sap.m.ComboBoxBaseRenderer);
-	
+
 	/**
 	 * CSS class to be applied to the HTML root element of the MultiComboBox control.
-	 * 
+	 *
 	 * @type {string}
 	 */
 	MultiComboBoxRenderer.CSS_CLASS = "sapMMultiComboBox";
-	
+
 	/**
 	 * CSS class to be applied to the HTML root element of the MultiComboBox control.
-	 * 
+	 *
 	 * @type {string}
 	 */
 	MultiComboBoxRenderer.DOT_CSS_CLASS = ".sapMMultiComboBox";
-	
+
 	/**
 	 * Add classes to the MultiComboBox.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager}
 	 *          oRm The RenderManager that can be used for writing to the render output buffer.
 	 * @param {sap.ui.core.Control}
@@ -41,38 +40,49 @@ sap.ui.define(['jquery.sap.global', './InputBaseRenderer', 'sap/ui/core/Renderer
 			oRm.addClass("sapMMultiComboBoxHasToken");
 		}
 	};
-	
+
 	/**
 	 * Add inner classes to the MultiMultiComboBox's input element.
-	 * 
+	 *
 	 * @param {sap.ui.core.RenderManager}
 	 *          oRm The RenderManager that can be used for writing to the render output buffer.
 	 * @param {sap.ui.core.Control}
 	 *          oControl An object representation of the control that should be rendered.
 	 */
 	MultiComboBoxRenderer.addInnerClasses = function(oRm, oControl) {
-		sap.m.ComboBoxBaseRenderer.addInnerClasses.apply(this, arguments);
+		ComboBoxBaseRenderer.addInnerClasses.apply(this, arguments);
 		oRm.addClass(MultiComboBoxRenderer.CSS_CLASS + "InputInner");
 	};
-	
+
+	/**
+	 * Add CSS classes to the combo box arrow button, using the provided {@link sap.ui.core.RenderManager}.
+	 * To be overwritten by subclasses.
+	 *
+	 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+	 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+	 */
+	MultiComboBoxRenderer.addButtonClasses = function(oRm, oControl) {
+		ComboBoxBaseRenderer.addButtonClasses.apply(this, arguments);
+		oRm.addClass(MultiComboBoxRenderer.CSS_CLASS + "Arrow");
+	};
+
 	MultiComboBoxRenderer.openInputTag = function(oRm, oControl) {
 		oRm.write('<div class="sapMMultiComboBoxBorder"');
-		oRm.writeAttribute("id", oControl.getId() + "-border");  // UI5 core expect a DIV with ID	
+		oRm.writeAttribute("id", oControl.getId() + "-border");  // UI5 core expect a DIV with ID
 		oRm.write(">");
-		
+
 		oRm.renderControl(oControl._oTokenizer);
-		
+
 		oRm.write("<div class=\"sapMMultiComboBoxInputContainer\">");
-		InputBaseRenderer.openInputTag.call(this, oRm, oControl);
+		ComboBoxBaseRenderer.openInputTag.call(this, oRm, oControl);
 	};
-	
+
 	MultiComboBoxRenderer.closeInputTag = function(oRm, oControl) {
-		InputBaseRenderer.closeInputTag.call(this, oRm, oControl);
+		ComboBoxBaseRenderer.closeInputTag.call(this, oRm, oControl);
 		oRm.write("</div>");
 		oRm.write("</div>");
 		oRm.write("<div class=\"sapMMultiComboBoxShadowDiv\"/>");
 	};
-	
 
 	return MultiComboBoxRenderer;
 
