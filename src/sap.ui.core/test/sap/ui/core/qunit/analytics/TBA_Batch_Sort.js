648a25568eada0ddfb0595daf5ff386978bd4034
Gerald Krause <gerald.krause@sap.com>
2015-08-10 14:58:57 +0200

    [INTERNAL][PERFORMANCE] AnalyticalBinding.js - Performance: Opt. length request
    Now that all relevant OData providers support $top=0, the request for
    the total size can be formulated efficiently with $top=0 such that it
    returns no entities and therefore the provider can recognize that they
    do not need to be determined.
    
    Change-Id: I5a399dec9c8c54abdb9e51f4a33abf090dbe7ce8
    CR-Id: 012003146900003407492015

--- src/sap.ui.core/test/sap/ui/core/qunit/analytics/TBA_Batch_Sort.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/analytics/TBA_Batch_Sort.js
@@ -6,7 +6,7 @@ o4aFakeService.addResponse({
 	uri: [
 			"ActualPlannedCosts(P_ControllingArea=\'US01\',P_CostCenter=\'100-1000\',P_CostCenterTo=\'999-9999\')/Results?$select=ActualCosts,Currency&$top=100&$inlinecount=allpages",
 			"ActualPlannedCosts(P_ControllingArea=\'US01\',P_CostCenter=\'100-1000\',P_CostCenterTo=\'999-9999\')/Results?$select=CostCenter,ActualCosts,Currency&$orderby=CostCenter%20asc&$top=120&$inlinecount=allpages",
-			"ActualPlannedCosts(P_ControllingArea=\'US01\',P_CostCenter=\'100-1000\',P_CostCenterTo=\'999-9999\')/Results?$select=CostCenter,CostElement,Currency&$top=1&$inlinecount=allpages"
+			"ActualPlannedCosts(P_ControllingArea=\'US01\',P_CostCenter=\'100-1000\',P_CostCenterTo=\'999-9999\')/Results?$select=CostCenter,CostElement,Currency&$top=0&$inlinecount=allpages"
 		],
 	header: o4aFakeService.headers.BATCH,
 	content: "--AAD136757C5CF75E21C04F59B8682CEA0\r\n" +
