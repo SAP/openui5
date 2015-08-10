c5a38509cd3c69a560d031bc4514e46208eb2b3d
Arley Triana Morin <arley.triana.morin@sap.com>
2015-08-10 14:28:12 +0200

    [FIX] sap.m.ComboBox: it should correctly set the selection if the items aggregation is bounded to an OData model
    Change-Id: Ic201f5fc6d3af1e0fb3b16cacfebc54ee88e7a6c
    BCP: 1570460580
    CR-Id: 012006153200004078592015

--- src/sap.m/src/sap/m/ComboBoxBase.js
+++ src/sap.m/src/sap/m/ComboBoxBase.js
@@ -720,7 +720,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './ComboBoxBaseRenderer', './Dialog
 		 * @public
 		 */
 		ComboBoxBase.prototype.addItem = function(oItem) {
-			this.addAggregation("items", oItem, true);
+			this.addAggregation("items", oItem);
 
 			if (oItem) {
 				oItem.attachEvent("_change", this.onItemChange, this);
