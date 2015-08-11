2fefc273e0321e66b781659892d5374266b92f7b
Sebastian Allmann <sebastian.allmann@sap.com>
2015-08-11 08:37:04 +0200

    [FIX] sap.m.DatePicker: error message shortly appears by closing
    - If the DatePicker has a Valuestate error or warning a message appears
    if it has the focus. Thats fine. But of now the calendar is opened and a
    value is choosen, the error state displays afer the calendar is
    closed, so the error message appears shortly.
    - fix some minor issues in Calendar
    
    Change-Id: I20f22ef036351ca243ec6d27c3c63a16d8e05cb0
    BCP: 1570786560

--- src/sap.m/src/sap/m/DatePicker.js
+++ src/sap.m/src/sap/m/DatePicker.js
@@ -731,10 +731,6 @@ sap.ui.define(['jquery.sap.global', './InputBase', 'sap/ui/model/type/Date', 'sa
 			var oDate;
 			var sValue = "";
 
-			this._oPopup.close();
-			this._bFocusNoPopup = true;
-			this.focus();
-
 			if (aSelectedDates.length > 0) {
 				oDate = aSelectedDates[0].getStartDate();
 			}
@@ -760,6 +756,11 @@ sap.ui.define(['jquery.sap.global', './InputBase', 'sap/ui/model/type/Date', 'sa
 					this.fireChangeEvent(sValue, {valid: true});
 				}
 			}
+
+			// close popup and focus input after change event to allow application to reset value state or similar things
+			this._oPopup.close();
+			this._bFocusNoPopup = true;
+			this.focus();
 
 		};
 
