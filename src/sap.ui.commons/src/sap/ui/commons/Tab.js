d4f4b3d26c4a19ca48410bc853c8be60124574c9
I312031 <teodor.taushanov@sap.com>
2015-08-13 11:15:47 +0300

    [FIX] sap.ui.commons.TabStrip: Tabs set to non-visible now disappear
    Fixes https://github.com/SAP/openui5/issues/554
    
    Change-Id: I6460c6f07e8d3dee1f5eacf92cceed5366698843
    CR-Id: 012006153200004204282015

--- src/sap.ui.commons/src/sap/ui/commons/Tab.js
+++ src/sap.ui.commons/src/sap/ui/commons/Tab.js
@@ -8,11 +8,11 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 	"use strict";
 
 
-	
+
 	/**
 	 * Constructor for a new Tab.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given 
+	 * @param {string} [sId] id for the new control, generated automatically if no id is given
 	 * @param {object} [mSettings] initial settings for the new control
 	 *
 	 * @class
@@ -28,34 +28,34 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var Tab = Panel.extend("sap.ui.commons.Tab", /** @lends sap.ui.commons.Tab.prototype */ { metadata : {
-	
+
 		library : "sap.ui.commons",
 		properties : {
-	
+
 			/**
 			 * Set the height property.
 			 */
 			verticalScrolling : {type : "sap.ui.core.Scrolling", group : "Behavior", defaultValue : sap.ui.core.Scrolling.None},
-	
+
 			/**
 			 * Set the width property.
 			 */
 			horizontalScrolling : {type : "sap.ui.core.Scrolling", group : "Behavior", defaultValue : sap.ui.core.Scrolling.None},
-	
+
 			/**
 			 * Defines whether the tab contains a close button.
 			 */
 			closable : {type : "boolean", group : "Misc", defaultValue : false},
-	
+
 			/**
 			 * Defines whether the tab is the active one.
-			 * @deprecated Since version 0.17.0. 
+			 * @deprecated Since version 0.17.0.
 			 * This property is not used. To indentify the selected tab in a TabStrip selectedIndex is used.
 			 */
 			selected : {type : "boolean", group : "Behavior", defaultValue : false, deprecated: true}
 		}
 	}});
-	
+
 	/*
 	 * Initialize the Tab
 	 * @private
@@ -63,26 +63,35 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 	Tab.prototype.init = function() {
 		// Setting this to role Tabpanel instead of its container basically worked.
 		// However, the role is set one level higher to get better output in screen reader.
-	
+
 		this.oScrollDomRef = null;      // Points to the content area
 	};
-	
+
 	Tab.prototype.rerender = function() {
-	
+
 		// as Tab has no own renderer call renderer of TabStrip
 		var oParent = this.getParent();
 		if (oParent) {
 			oParent.rerender();
 		}
-	
+
 	};
-	
+
+	Tab.prototype.invalidate = function() {
+
+		// as Tab has no own renderer call invalidate of TabStrip
+		var oParent = this.getParent();
+		if (oParent) {
+			oParent.invalidate();
+		}
+	};
+
 	/*
 	 * Called after rendering from the TabStrip control
 	 */
 	Tab.prototype.onAfterRendering = function () {
 		this.oScrollDomRef = this.getDomRef("panel");
-	
+
 		// Restore scroll positions
 		if (this.oScrollDomRef) {
 			var scrollTop = this.getProperty("scrollTop");
@@ -94,10 +103,10 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 				this.oScrollDomRef.scrollLeft = scrollLeft;
 			}
 		}
-	
+
 		// TODO: this must also be done for tabs where the contents are not rendered initially
 	};
-	
+
 	/*
 	 * Returns the scroll position of the tab in pixel from the left. Returns "0" if not rendered yet.
 	 * Also updates the control property internally.
@@ -111,10 +120,10 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 			scrollLeft = this.oScrollDomRef.scrollLeft;
 			this.setProperty("scrollLeft", scrollLeft, true);
 		}
-	
+
 		return scrollLeft;
 	};
-	
+
 	/*
 	 * Sets the scroll position of the tab in pixel from the left.
 	 * @param {int} iPosition The position to scroll to
@@ -129,7 +138,7 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 		}
 		return this;
 	};
-	
+
 	/*
 	 * Returns the scroll position of the tab in pixel from the top. Returns "0" if not rendered yet.
 	 * Also updates the control property internally.
@@ -143,10 +152,10 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 			scrollTop = this.oScrollDomRef.scrollTop;
 			this.setProperty("scrollTop", scrollTop, true);
 		}
-	
+
 		return scrollTop;
 	};
-	
+
 	/*
 	 * Sets the scroll position of the tab in pixel from the top.
 	 * @param {int} iPosition The position to scroll to
@@ -161,7 +170,7 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 		}
 		return this;
 	};
-	
+
 	/*
 	 * Property setter for the "enabled" state (overwrite method from panel)
 	 * Normally only classes are exchanged and no rerendering is needed.
@@ -175,14 +184,14 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 	 * @public
 	 */
 	Tab.prototype.setEnabled = function(bEnabled) {
-	
+
 		if (bEnabled == this.getEnabled()) {
 			return this;
 		}
-	
+
 		var oDomRef = this.getDomRef();
 		var oParent = this.getParent();
-	
+
 		if (!oDomRef || (!bEnabled && jQuery(this.getDomRef()).hasClass("sapUiTabSel")) ||
 			(bEnabled && oParent && oParent.getSelectedIndex && oParent.getSelectedIndex() < 0)) {
 			this.setProperty("enabled", bEnabled, false); // rendering needed
@@ -192,9 +201,9 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 			jQuery(this.getDomRef()).toggleClass("sapUiTab", bEnabled).toggleClass("sapUiTabDsbl", !bEnabled).attr("aria-disabled",!bEnabled);
 		}
 		return this;
-	
+
 	};
-	
+
 	/*
 	 * Setter of the width property. As it has no effect on a tab and is
 	 * only inherited from panel, noting to do. Just overwrite panel implementation
@@ -208,7 +217,7 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 		// do nothing
 		return this;
 	};
-	
+
 	/*
 	 * Property setter for the padding.As it has no effect on a tab and is
 	 * only inherited from panel, noting to do. Just overwrite panel implementation
@@ -222,7 +231,7 @@ sap.ui.define(['jquery.sap.global', './Panel', './library'],
 		// do nothing
 		return this;
 	};
-	
+
 	Tab.prototype._handleTrigger = function(oEvent) {
 		//see sap.ui.commons.Panel.prototype._handleTrigger -> Nothing to do here
 	};
