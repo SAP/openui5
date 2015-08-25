ed50e359eb6f1aab84ef8bcd1745f70754e588d3
Tobias Oetzel <tobias.oetzel@sap.com>
2015-08-25 08:50:17 +0200

    [INTERNAL] testing tutorial - step 3 rewriting
    - added a price formatter instead of the flagged formatter since its a one way binding usecase
    
    Change-Id: I77fe7249e50567d833c5779f5abb5342369a18d9

--- src/sap.m/test/sap/m/demokit/tutorial/testing/04/webapp/model/formatter.js
+++ src/sap.m/test/sap/m/demokit/tutorial/testing/04/webapp/model/formatter.js
@@ -17,6 +17,25 @@ sap.ui.define([
 			}
 
 			return parseFloat(sValue).toFixed(2);
+		},
+
+		/**
+		 * Defines a value state based on the price
+		 *
+		 * @public
+		 * @param {number} iPrice the price of a post
+		 * @returns {string} sValue the state for the price
+		 */
+		priceState: function (iPrice) {
+			if (iPrice < 50) {
+				return "Success";
+			} else if (iPrice >= 50 && iPrice < 250 ) {
+				return "None";
+			} else if (iPrice >= 250 && iPrice < 2000 ) {
+				return "Warning";
+			} else {
+				return "Error";
+			}
 		}
 
 	};
