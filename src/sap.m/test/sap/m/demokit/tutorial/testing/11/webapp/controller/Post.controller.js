5ad874524ec2ab6985ee545e045f12b40cdd6228
Tobias Oetzel <tobias.oetzel@sap.com>
2015-08-26 10:26:56 +0200

    [INTERNAL] test-tutorial: introducing step 11
    - copied step 10 and only adjusted the Component.js
    - no code changes compared with step 10 yet
    
    Change-Id: I1d0bed72863f8a1287bebfe7774dff9605cc1f97

---
+++ src/sap.m/test/sap/m/demokit/tutorial/testing/11/webapp/controller/Post.controller.js
@@ -0,0 +1,96 @@
+sap.ui.define([
+	'sap/ui/demo/bulletinboard/controller/BaseController',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/demo/bulletinboard/model/formatter'
+], function (BaseController, JSONModel, formatter) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.demo.bulletinboard.controller.Post", {
+
+		formatter: formatter,
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit: function () {
+			// Model used to manipulate control states. The chosen values make sure,
+			// detail page is busy indication immediately so there is no break in
+			// between the busy indication for loading the view's meta data
+			var oViewModel = new JSONModel({
+					shareSendEmailSubject: "",
+					shareSendEmailMessage: ""
+				});
+
+			this.getRouter().getRoute("post").attachPatternMatched(this._onPostMatched, this);
+			this.setModel(oViewModel, "postView");
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Navigates back to the worklist
+		 * @function
+		 */
+		onNavBack: function () {
+			this.myNavBack("worklist");
+		},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * Binds the view to the post path.
+		 *
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onPostMatched: function (oEvent) {
+			var oViewModel = this.getModel("postView"),
+				oDataModel = this.getModel();
+
+			this.getView().bindElement({
+				path: "/Posts('" + oEvent.getParameter("arguments").postId + "')",
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function () {
+						oDataModel.metadataLoaded().then(function () {
+							// Busy indicator on view should only be set if metadata is loaded,
+							// otherwise there may be two busy indications next to each other on the
+							// screen. This happens because route matched handler already calls '_bindView'
+							// while metadata is loaded.
+							oViewModel.setProperty("/busy", true);
+						});
+					},
+					dataReceived: function () {
+						oViewModel.setProperty("/busy", false);
+					}
+				}
+			});
+		},
+
+		_onBindingChange : function (oEvent) {
+			var oView = this.getView(),
+				oViewModel = this.getModel("postView"),
+				oResourceBundle = this.getResourceBundle(),
+				oPost = oView.getBindingContext().getObject(),
+				sPostId = oPost.PostID;
+
+			// Everything went fine.
+			oViewModel.setProperty("/shareSendEmailSubject",
+				oResourceBundle.getText("shareSendEmailObjectSubject", [sPostId]));
+			oViewModel.setProperty("/shareSendEmailMessage",
+				oResourceBundle.getText("shareSendEmailObjectMessage", [oPost.Title, sPostId, window.location.href]));
+		}
+
+	});
+
+});
