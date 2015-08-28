40ca4f3c88e076cea5e4a64145b562e498ed092c
D060268 <valeri.cernyh@sap.com>
2015-08-28 16:04:43 +0200

    [INTERNAL] Shopping Cart App Opa Test: added  Delete Product Journey
    - implemented Delete Product Journey
    - added page objects
    - added arrangement
    
    Change-Id: Id24a3dc7cc3c8d03f42c19a471c4ab8e4e931a9d

---
+++ src/sap.m/test/sap/m/demokit/cart/test/pageobjects/Home.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	'sap/ui/test/Opa5',
+	'sap/ui/test/matchers/BindingPath'
+], function (Opa5, BindingPath) {
+	Opa5.createPageObjects({
+		onHome: {
+
+			actions: {
+				iPressOnTheSecondCategory: function () {
+					return this.waitFor({
+						controlType: "sap.m.StandardListItem",
+						matchers: new BindingPath({path: "/ProductCategories('FS')"}),
+						success: function (aListItems) {
+							aListItems[0].$().trigger("tap");
+						},
+						errorMessage: "The category list does not contain required selection"
+					});
+				}
+			}
+
+		}
+	});
+});
