ced04ddac261069a4a065519c3c4f30d0218c105
Thilo Seidel <thilo.seidel@sap.com>
2015-09-01 16:16:41 +0200

    [FEATURE] Demo Application Added: Worklist
    basic worklist application without sapui5 dependency
    
    Change-Id: Ic898d23b07bb7cac14303b1d1e82a08f61059841
    (cherry picked from commit 23ce7f40a5f9414f1fc1603e80618e7b944773dd)
    CR-Id: 012003146900003013472015

---
+++ src/sap.m/test/sap/m/demokit/worklist/webapp/model/formatter.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	] , function () {
+		"use strict";
+
+		return {
+
+			/**
+			 * Rounds the number unit value to 2 digits
+			 * @public
+			 * @param {string} sValue the number string to be rounded
+			 * @returns {string} sValue with 2 digits rounded
+			 */
+			numberUnit : function (sValue) {
+				if (!sValue) {
+					return "";
+				}
+				return parseFloat(sValue).toFixed(2);
+			}
+
+		};
+
+	}
+);
