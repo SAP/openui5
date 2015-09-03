8f5360f66e802e2bd7fe7f849a3071ace1373c10
Deyan Yanakiev <deyan.yanakiev@sap.com>
2015-09-03 09:29:00 +0300

    [FIX] sap.m.MessagePopover: Correct heigh when initiallyExpanded false
    Change-Id: Ida6530e6ff93df80e01a6c32f50b274d97d2c927
    Signed-off-by: Deyan Yanakiev <deyan.yanakiev@sap.com>

--- src/sap.m/src/sap/m/MessagePopover.js
+++ src/sap.m/src/sap/m/MessagePopover.js
@@ -1010,14 +1010,14 @@ sap.ui.define(["jquery.sap.global", "./ResponsivePopover", "./Button", "./Toolba
 		};
 
 		/**
-		 * Shrinks the height of the MessagePopover to 48px so that only the header with
+		 * Sets the height of the MessagePopover to auto so that only the header with
 		 * the SegmentedButton is visible
 		 * @private
 		 */
 		MessagePopover.prototype._collapseMsgPopover = function () {
 			this._oPopover
 				.addStyleClass(CSS_CLASS + "-init")
-				.setContentHeight("48px");
+				.setContentHeight("auto");
 
 			this._oSegmentedButton.setSelectedButton("none");
 		};
