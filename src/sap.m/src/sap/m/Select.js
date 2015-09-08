05cc1c5092734e639e1b761454ef809ac4a61984
Arley Triana Morin <arley.triana.morin@sap.com>
2015-09-08 23:15:28 +0200

    [INTERNAL] sap.m.Select: suppress invalidation when the .removeAllItems() method is called
    Change-Id: Id9daa86fcf74c7a0d71abc0c79568ad9a0aad418
    CR-Id: 012003146900002895282015

--- src/sap.m/src/sap/m/Select.js
+++ src/sap.m/src/sap/m/Select.js
@@ -1837,12 +1837,9 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './Popov
 			var oList = this.getList(),
 				aItems = oList ? oList.removeAllItems() : [];
 
-			// clear the selection
 			this.clearSelection();
-
-			if (!this.isInvalidateSuppressed()) {
-				this.invalidate();
-			}
+			this.setValue("");
+			this.$("select").children().remove();
 
 			for (var i = 0; i < aItems.length; i++) {
 				aItems[i].detachEvent("_change", this.onItemChange, this);
