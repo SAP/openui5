54c32db0fe6cd84eb182e37be44917040497ad84
Krasimir Chariyski <krasimir.chariyski@sap.com>
2015-09-15 10:14:53 +0300

    [INTERNAL][FIX] sap.m.Carousel: fixed rerendering problem with AbsoluteLayout
    Change-Id: I21da41e4f8e4fa0b521fadbee1b39e06b2832faa
    BCP: 1580078315

--- src/sap.m/src/sap/m/Carousel.js
+++ src/sap.m/src/sap/m/Carousel.js
@@ -321,6 +321,14 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 				//mobify carousel is 1-based
 				this._oMobifyCarousel.move(iIndex + 1);
 				this._changePage(iIndex + 1);
+
+				// BCP: 1580078315
+				if (sap.ui.commons && this.getParent() instanceof sap.ui.commons.layout.PositionContainer) {
+					if (this._isCarouselUsedWithCommonsLayout === undefined){
+						jQuery.sap.delayedCall(0, this, "invalidate");
+						this._isCarouselUsedWithCommonsLayout = true;
+					}
+				}
 			}
 		}
 
