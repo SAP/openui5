666e600444c69a994b2812929e9d2abfabc9e4c3
Matthias Osswald <mat.osswald@sap.com>
2015-09-18 18:00:35 +0200

    [INTERNAL] QUnit Tests: add missing qunit scripts
    - qunit-junit: Creates a detailed test report as XML which
    will be read by the test-runner
    - qunit-coverage: (optional) enables client-side coverage report
    - QUnitUtils: Should be included after those scripts. Includes
    some workarounds for PhantomJS and initial QUnit settings
    
    Change-Id: I9e7d00cfeebc6370094515e5b42b448174d6e9fa

--- src/sap.m/test/sap/m/qunit/MessageStrip.qunit.js
+++ src/sap.m/test/sap/m/qunit/MessageStrip.qunit.js
@@ -2,8 +2,10 @@
 	"use strict";
 
 	jQuery.sap.require("sap.ui.qunit.qunit-css");
-	jQuery.sap.require("sap.ui.qunit.QUnitUtils");
 	jQuery.sap.require("sap.ui.thirdparty.qunit");
+	jQuery.sap.require("sap.ui.qunit.qunit-junit");
+	jQuery.sap.require("sap.ui.qunit.qunit-coverage");
+	jQuery.sap.require("sap.ui.qunit.QUnitUtils");
 	jQuery.sap.require("sap.ui.thirdparty.sinon");
 	jQuery.sap.require("sap.ui.thirdparty.sinon-qunit");
 	sinon.config.useFakeTimers = false;
