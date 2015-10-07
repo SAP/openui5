ef4f1bbbca71b689e6f6c6a0290bd720e47f9502
Arley Triana Morin <arley.triana.morin@sap.com>
2015-10-07 15:20:57 +0200

    [FIX] sap.m.Select: binding for property selectedKey doesn't work
    Change-Id: I8b2442ee15c951df82a6f3ea9c0cd34a47997dff
    BCP: 1580102355
    CR-Id: 012003146900003681612015

--- src/sap.m/src/sap/m/Select.js
+++ src/sap.m/src/sap/m/Select.js
@@ -1438,7 +1438,11 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './Popov
 						break;
 
 					case "key":
-						this.setSelectedKey(sNewValue);
+
+						if (!this.isBound("selectedKey")) {
+							this.setSelectedKey(sNewValue);
+						}
+
 						break;
 
 					// no default
