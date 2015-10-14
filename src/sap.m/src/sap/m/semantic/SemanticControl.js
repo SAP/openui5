327de73259c6af6734473e1236989ad21e97bbe7
Diana Pazheva <diana.pazheva@sap.com>
2015-10-14 15:37:27 +0300

    [INTERNAL] sap.m.semantic.MultiselectAction: Fixed "enabled" property data-binding issue
    Change-Id: Ic03b3fccfaf237548cfda94efd3428278a643074
    BCP: 1580105151

--- src/sap.m/src/sap/m/semantic/SemanticControl.js
+++ src/sap.m/src/sap/m/semantic/SemanticControl.js
@@ -59,10 +59,7 @@ sap.ui.define(["sap/m/semantic/SemanticConfiguration", "sap/ui/base/ManagedObjec
 
 	SemanticControl.prototype.setProperty = function (key, value, bSuppressInvalidate) {
 		ManagedObject.prototype.setProperty.call(this, key, value, true);
-		var sSetter = "set" + this._capitalize(key); //we call the setter, rather than setProperty on the control,
-													// to make sure we cover the case when the control has
-													// overwritten the setter with custom implementation
-		this._getControl()[sSetter](value, bSuppressInvalidate);
+		this._applyProperty(key, value, bSuppressInvalidate);
 
 		return this;
 	};
@@ -163,6 +160,17 @@ sap.ui.define(["sap/m/semantic/SemanticConfiguration", "sap/ui/base/ManagedObjec
 				this._getControl().applySettings(oSettings);
 			}
 		}
+	};
+
+	/**
+	 * Applies the property value according to semantic logic
+	 * @private
+	 */
+	SemanticControl.prototype._applyProperty = function(key, value, bSuppressInvalidate) {
+		var sSetter = "set" + this._capitalize(key); //we call the setter, rather than setProperty on the control,
+													// to make sure we cover the case when the control has
+													// overwritten the setter with custom implementation
+		this._getControl()[sSetter](value, bSuppressInvalidate);
 	};
 
 	SemanticControl.prototype._capitalize = function(sName) {
