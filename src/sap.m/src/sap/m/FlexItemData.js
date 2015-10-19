48d99e7243f94ed2baba48c0a763761c9aa83737
Thomas Koetter <thomas.koetter@sap.com>
2015-10-19 13:29:30 +0200

    [FEATURE] sap.m.FlexBox: New options for wrapping and aligning content
    Also, added properties for setting min/max height/width.
    Additionally, refactored the code to use style classes instead of inline
    styles.
    
    Change-Id: I888e200151b721b4c539ccb8f79ff4a5b26ca5d3

--- src/sap.m/src/sap/m/FlexItemData.js
+++ src/sap.m/src/sap/m/FlexItemData.js
@@ -2,21 +2,19 @@
  * ${copyright}
  */
 
-// Provides control sap.m.FlexItemData.
+// Provides element sap.m.FlexItemData
 sap.ui.define(['jquery.sap.global', './FlexBoxStylingHelper', './library', 'sap/ui/core/LayoutData'],
 	function(jQuery, FlexBoxStylingHelper, library, LayoutData) {
 	"use strict";
 
-
-
 	/**
 	 * Constructor for a new FlexItemData.
 	 *
-	 * @param {string} [sId] id for the new control, generated automatically if no id is given
-	 * @param {object} [mSettings] initial settings for the new control
+	 * @param {string} [sId] id for the new element, generated automatically if no id is given
+	 * @param {object} [mSettings] initial settings for the new element
 	 *
 	 * @class
-	 * Holds layout data for a FlexBox
+	 * Holds layout data for a FlexBox|HBox|VBox
 	 * @extends sap.ui.core.LayoutData
 	 * @version ${version}
 	 *
@@ -32,23 +30,29 @@ sap.ui.define(['jquery.sap.global', './FlexBoxStylingHelper', './library', 'sap/
 
 			/**
 			 * Determines cross-axis alignment of individual element (not currently supported in Internet Explorer)
+			 *
+			 * @see http://www.w3.org/TR/css-flexbox-1/#align-items-property
 			 */
 			alignSelf : {type : "sap.m.FlexAlignSelf", group : "Misc", defaultValue : sap.m.FlexAlignSelf.Auto},
 
 			/**
 			 * Determines the display order of flex items independent of their source code order.
+			 *
+			 * @see http://www.w3.org/TR/css-flexbox-1/#order-property
 			 */
 			order : {type : "int", group : "Misc", defaultValue : 0},
 
 			/**
 			 * Determines the flexibility of the flex item when allocatable space is remaining.
+			 *
+			 * @see http://www.w3.org/TR/css-flexbox-1/#flex-grow-property
 			 */
 			growFactor : {type : "float", group : "Misc", defaultValue : 0},
 
 			/**
 			 * The shrink factor determines how much the flex item will shrink relative to the rest of the flex items in the flex container when negative free space is distributed.
 			 *
-			 * @see http://www.w3.org/TR/css3-flexbox/#flex-shrink-factor
+			 * @see http://www.w3.org/TR/css-flexbox-1/#flex-shrink-property
 			 *
 			 * <b>Note:</b> This property is not supported in Internet Explorer 9, Android Native Browser/Webview <4.4, and Safari <7.
 			 * @since 1.24
@@ -58,52 +62,106 @@ sap.ui.define(['jquery.sap.global', './FlexBoxStylingHelper', './library', 'sap/
 			/**
 			 * The base size is the initial main size of the item for the flex algorithm. If set to "auto", this will be the computed size of the item.
 			 *
-			 * @see http://www.w3.org/TR/css3-flexbox/#flex-basis
+			 * @see http://www.w3.org/TR/css-flexbox-1/#flex-basis-property
 			 *
 			 * @since 1.32
 			 */
 			baseSize : {type : "sap.ui.core.CSSSize", group : "Misc", defaultValue : "auto"},
 
 			/**
-			 * The style class will be applied to the flex item and can be used for CSS selectors
-			 * @deprecated Since version 1.11.2.
-			 * Generic addStyleClass method is available on the control
+			 * The minimum height of the flex item.
+			 * @since 1.36
 			 */
-			styleClass : {type : "string", group : "Misc", defaultValue : '', deprecated: true}
+			minHeight : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : 'auto'},
+
+			/**
+			 * The maximum height of the flex item.
+			 * @since 1.36
+			 */
+			maxHeight : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : ''},
+
+			/**
+			 * The minimum height of the flex item.
+			 * @since 1.36
+			 */
+			minWidth : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : 'auto'},
+
+			/**
+			 * The maximum height of the flex item.
+			 * @since 1.36
+			 */
+			maxWidth : {type : "sap.ui.core.CSSSize", group : "Dimension", defaultValue : ''},
+
+			/**
+			 * The style class will be applied to the flex item and can be used for CSS selectors
+			 */
+			styleClass : {type : "string", group : "Misc", defaultValue : ''}
 		}
 	}});
 
-
 	FlexItemData.prototype.setAlignSelf = function(sValue) {
-		this.setProperty("alignSelf", sValue);
-		FlexBoxStylingHelper.setStyle(null, this, "align-self", sValue);
+		var sOldValue = this.getAlignSelf();
+		this.setProperty("alignSelf", sValue, true);
+		this.$().removeClass("sapMFlexItemAlign" + sOldValue).addClass("sapMFlexItemAlign" + this.getAlignSelf());
+
 		return this;
 	};
 
 	FlexItemData.prototype.setOrder = function(sValue) {
-		this.setProperty("order", sValue);
-		FlexBoxStylingHelper.setStyle(null, this, "order", sValue);
+		this.setProperty("order", sValue, true);
+		FlexBoxStylingHelper.setStyle(null, this, "order", this.getOrder());
+
 		return this;
 	};
 
 	FlexItemData.prototype.setGrowFactor = function(sValue) {
-		this.setProperty("growFactor", sValue);
-		FlexBoxStylingHelper.setStyle(null, this, "flex-grow", sValue);
+		this.setProperty("growFactor", sValue, true);
+		FlexBoxStylingHelper.setStyle(null, this, "flex-grow", this.getGrowFactor());
+
 		return this;
 	};
 
 	FlexItemData.prototype.setShrinkFactor = function(sValue) {
 		this.setProperty("shrinkFactor", sValue, true);
-		FlexBoxStylingHelper.setStyle(null, this, "flex-shrink", sValue);
+		FlexBoxStylingHelper.setStyle(null, this, "flex-shrink", this.getShrinkFactor());
+
 		return this;
 	};
 
 	FlexItemData.prototype.setBaseSize = function(sValue) {
 		this.setProperty("baseSize", sValue, true);
-		sap.m.FlexBoxStylingHelper.setStyle(null, this, "flex-basis", sValue);
+		FlexBoxStylingHelper.setStyle(null, this, "flex-basis", this.getBaseSize());
+
 		return this;
 	};
 
+	FlexItemData.prototype.setMinHeight = function(sValue) {
+		this.setProperty("minHeight", sValue, true);
+		this.$().css("min-height", this.getMinHeight());
+
+		return this;
+	};
+
+	FlexItemData.prototype.setMaxHeight = function(sValue) {
+		this.setProperty("maxHeight", sValue, true);
+		this.$().css("max-height", this.getMaxHeight());
+
+		return this;
+	};
+
+	FlexItemData.prototype.setMinWidth = function(sValue) {
+		this.setProperty("minWidth", sValue, true);
+		this.$().css("min-width", this.getMinWidth());
+
+		return this;
+	};
+
+	FlexItemData.prototype.setMaxWidth = function(sValue) {
+		this.setProperty("maxWidth", sValue, true);
+		this.$().css("max-width", this.getMaxWidth());
+
+		return this;
+	};
 
 	return FlexItemData;
 
