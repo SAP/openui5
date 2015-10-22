412f5a283e036c36624698bad6c5c67e58b50a1a
Uwe Reeder <uwe.reeder@sap.com>
2015-10-22 14:33:07 +0200

    [INTERNAL] sap.ui.model.odata.type.Date: avoid new Date(string)
    Make formatValue symmetrical to parseValue: parse with one formatter and
    then format with the other one
    
    PS2: ESLint
    
    Change-Id: Ib7bac9ff595d377cc6d999373267514656479d33

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/Date.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/Date.qunit.js
@@ -91,7 +91,8 @@ sap.ui.require([
 		{i: undefined, o: null},
 		{i: null, o: null},
 		{i: "foo", t: "any", o: "foo"},
-		{i: "2014-11-27", t: "string", o: "Nov 27, 2014"}
+		{i: "2014-11-27", t: "string", o: "Nov 27, 2014"},
+		{i: "2014-11-34", t: "string", o: "2014-11-34"}
 	].forEach(function (oFixture) {
 		QUnit.test("format value", function (assert) {
 			var oType = new DateType();
