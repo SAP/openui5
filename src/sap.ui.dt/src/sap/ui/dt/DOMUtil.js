5618136ecd1e479df88024c70539cd364c8f4e73
Mikhail Benderskiy <mikhail.benderskiy@sap.com>
2015-10-26 13:22:50 +0100

    [INTERNAL] sap.ui.dt Ghost fix for FF
    Change-Id: I0a290513b8a362fd6f7cdd3a5d54179f93d91284

--- src/sap.ui.dt/src/sap/ui/dt/DOMUtil.js
+++ src/sap.ui.dt/src/sap/ui/dt/DOMUtil.js
@@ -12,7 +12,7 @@ function(jQuery, ElementUtil) {
 
 	/**
 	 * Class for DOM Utils.
-	 * 
+	 *
 	 * @class
 	 * Utility functionality for DOM
 	 *
@@ -29,7 +29,7 @@ function(jQuery, ElementUtil) {
 	var DOMUtil = {};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getSize = function(oDomRef) {
 		return {
@@ -39,14 +39,14 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getOffsetFromParent = function(oPosition, mParentOffset, scrollTop, scrollLeft) {
 		var mOffset = {
 			left : oPosition.left,
 			top : oPosition.top
 		};
-		
+
 		if (mParentOffset) {
 			mOffset.left -= (mParentOffset.left - (scrollLeft ? scrollLeft : 0));
 			mOffset.top -= (mParentOffset.top - (scrollTop ? scrollTop : 0));
@@ -55,7 +55,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getZIndex = function(oDomRef) {
 		var zIndex;
@@ -67,7 +67,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getOverflows = function(oDomRef) {
 		var oOverflows;
@@ -81,7 +81,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getGeometry = function(oDomRef) {
 		if (oDomRef) {
@@ -94,13 +94,13 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.syncScroll = function(oSourceDom, oTargetDom) {
 		var $target = jQuery(oTargetDom);
 		var oTargetScrollTop = $target.scrollTop();
 		var oTargetScrollLeft = $target.scrollLeft();
-		
+
 		var $source = jQuery(oSourceDom);
 		var oSourceScrollTop = $source.scrollTop();
 		var oSourceScrollLeft = $source.scrollLeft();
@@ -114,7 +114,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getDomRefForCSSSelector = function(oDomRef, sCSSSelector) {
 		if (!sCSSSelector) {
@@ -132,14 +132,14 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.getEscapedString = function(sString) {
 		return sString.replace(/([;&,\.\+\*\~':"\!\^#$%@\[\]\(\)=>\|])/g, '\\$1');
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.setDraggable = function(oElement, bValue) {
 		oElement = jQuery(oElement);
@@ -148,7 +148,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.copyComputedStyle = function(oSrc, oDest) {
 		var fnCopyStylesTo = function(mStyles, oDest) {
@@ -182,7 +182,7 @@ function(jQuery, ElementUtil) {
 		// pseudo elements can't be inserted via js, so we should create a real elements, which copy pseudo styling
 		mStyles = window.getComputedStyle(oSrc, ":after");
 		var sContent = mStyles.getPropertyValue("content");
-		if (sContent) {
+		if (sContent && sContent !== "none") {
 			var oAfterElement = jQuery("<span></span>").appendTo(oDest);
 			oAfterElement.text(sContent.replace(/\"/g, ""));
 			fnCopyStylesTo(mStyles, oAfterElement.get(0));
@@ -191,7 +191,7 @@ function(jQuery, ElementUtil) {
 
 		mStyles = window.getComputedStyle(oSrc, ":before");
 		sContent = mStyles.getPropertyValue("content");
-		if (sContent) {
+		if (sContent && sContent !== "none") {
 			var oBeforeElement = jQuery("<span></span>").prependTo(oDest);
 			oBeforeElement.text(sContent.replace(/\"/g, ""));
 			fnCopyStylesTo(mStyles, oBeforeElement.get(0));
@@ -201,7 +201,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.copyComputedStylesForDOM = function(oSrc, oDest) {
 		oSrc = jQuery(oSrc).get(0);
@@ -215,7 +215,7 @@ function(jQuery, ElementUtil) {
 	};
 
 	/**
-	 * 
+	 *
 	 */
 	DOMUtil.cloneDOMAndStyles = function(oNode, oTarget) {
 		oNode = jQuery(oNode).get(0);
