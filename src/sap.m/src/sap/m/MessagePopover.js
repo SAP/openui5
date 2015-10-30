1477fdebceacd4e369ad54f13375a2dcca8bf746
Adrian Bobev <adrian.bobev@sap.com>
2015-10-30 15:28:34 +0200

    [FIX] MessagePopover/ResponsivePopover: add implementation for busy and visible methods
    Change-Id: I0efb4c67c499565970f006477eaa317acc7c0bea
    BCP: 1580197116

--- src/sap.m/src/sap/m/MessagePopover.js
+++ src/sap.m/src/sap/m/MessagePopover.js
@@ -1170,11 +1170,13 @@ sap.ui.define(["jquery.sap.global", "./ResponsivePopover", "./Button", "./Toolba
 			return this._oPopover && this._oPopover.getAggregation("_popup").getDomRef(sSuffix);
 		};
 
-		["addStyleClass", "removeStyleClass", "toggleStyleClass", "hasStyleClass"].forEach(function(sName){
+		["addStyleClass", "removeStyleClass", "toggleStyleClass", "hasStyleClass", "getBusyIndicatorDelay",
+			"setBusyIndicatorDelay", "getVisible", "setVisible", "getBusy", "setBusy"].forEach(function(sName){
 				MessagePopover.prototype[sName] = function() {
 					if (this._oPopover && this._oPopover[sName]) {
-						var res = this._oPopover[sName].apply(this._oPopover, arguments);
-						return res === this._oPopover ? this : res;
+						var oPopover = this._oPopover;
+						var res = oPopover[sName].apply(oPopover, arguments);
+						return res === oPopover ? this : res;
 					}
 				};
 			});
