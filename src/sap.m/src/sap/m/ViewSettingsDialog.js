ded1f72039cf88e7371b020ae97b1e471ad74194
Vladislav Iliev <vladislav.iliev@sap.com>
2015-11-10 17:59:22 +0200

    [FIX] sap.m.ViewSettingsDialog: cancelling chosen filter details ignores last selection.
    Change-Id: I3c6c14f4ac9c6167e296dc1c328fa10295e2fdc5
    BCP: 1580045867
    (cherry picked from commit 51cf80a)
    CR-Id: 012003146900004639752015

--- src/sap.m/src/sap/m/ViewSettingsDialog.js
+++ src/sap.m/src/sap/m/ViewSettingsDialog.js
@@ -1455,6 +1455,12 @@ function(jQuery, library, Control, IconPool) {
 
 			this._getSegmentedButton().setSelectedButton(sSelectedButtonId);
 			this._switchToPage(sPageId);
+
+			/* 1580045867 2015: make sure navContainer's current page is always page1,
+			because at this point we are always switching to sort,group,filter or custom tab.*/
+			if (this._getNavContainer().getCurrentPage() !== this._getPage1()) {
+				this._getNavContainer().to(this._getPage1().getId());
+			}
 		}
 	};
 
