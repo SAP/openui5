4bb66d23d673c2586141d33316c5d03a41772b2f
Vladislav Tasev <vladislav.tasev@sap.com>
2015-11-16 17:10:37 +0200

    [INTERNAL] sap.uxap - deleted the 3 examples
    Change-Id: Ib9d3c4643dcc3dddd2d2ab1d54182fa33d7e131b

--- src/sap.uxap/test/sap/uxap/demokit/sample/BlockInBlock/Block.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/uxap/BlockBase"], function (BlockBase) {
-	return BlockBase.extend("sap/uxap/sample/BlockInBlock/Block", {
-		metadata: {
-			/* no additional views provided */
-		}
-	});
-}, true);
--- src/sap.uxap/test/sap/uxap/demokit/sample/Eventing/EventingBlock.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(["sap/uxap/BlockBase"], function (BlockBase) {
-	return BlockBase.extend("sap.uxap.sample.Eventing.EventingBlock", {
-		metadata: {
-			events: {
-				"dummy": {}
-			}
-		}
-	});
-}, true);
