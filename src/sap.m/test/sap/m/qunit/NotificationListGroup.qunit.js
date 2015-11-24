9cdb45ab30e666d359265376e22c04f076d1891f
Nikolay Vankov <nikolay.vankov@sap.com>
2015-11-24 17:27:08 +0200

    [INTERNAL] sap.m.NotificationListGroup: Fixed unit tests
    Change-Id: I61408992739f4b538e0bb423ee480c462702227c

--- src/sap.m/test/sap/m/qunit/NotificationListGroup.qunit.js
+++ src/sap.m/test/sap/m/qunit/NotificationListGroup.qunit.js
@@ -10,7 +10,7 @@
 
     var classNameHeader = '.sapMNLG-Header';
     var classNameDatetime = '.sapMNLG-Datetime';
-    var classNameFooter = '.sapMNLG-Footer';
+    var classNameFooterToolbar = '.sapMTB';
     var classNameCloseButton = '.sapMNLG-CloseButton';
 
     var RENDER_LOCATION = 'qunit-fixture';
@@ -149,6 +149,8 @@
     QUnit.test('Render action buttons', function(assert) {
         // arrange
         var that = this;
+        var buttonsInFooter = 2;
+        
         this.NotificationListGroup.addAggregation('buttons',
             new sap.m.Button({
                 text: 'Accept',
@@ -167,8 +169,9 @@
         );
         sap.ui.getCore().applyChanges();
 
+
         // assert
-        assert.strictEqual(jQuery(classNameFooter).children('button').length, 3, 'Buttons should be rendered');
+        assert.strictEqual(jQuery(classNameFooterToolbar).children('button').length, buttonsInFooter, 'Buttons should be rendered');
     });
 
     QUnit.test('Changing the title', function(assert) {
