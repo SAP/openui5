876a836d5fd94cbc96abd45dfae85e0ac669aaa1
Benedikt Schoelch <benedikt.schoelch@sap.com>
2015-11-26 15:17:45 +0100

    [INTERNAL] StashedControlSupport: mixin for controls
    Change-Id: I38220f35cd90feb22ff5e196a097d98ce066b5e5

--- src/sap.ui.core/src/sap/ui/base/ManagedObject.js
+++ src/sap.ui.core/src/sap/ui/base/ManagedObject.js
@@ -724,6 +724,29 @@ sap.ui.define([
 	};
 
 	/**
+	 * Optional StashedControlSupport dependecy
+	 * @private
+	 */
+	var StashedControlSupport;
+
+	/**
+	 * Returns an array of stashed child elements or an empty array if there are none.
+	 *
+	 * @param {string} sId id of the object which should have stashed children
+	 * @return {sap.ui.core._StashedControl[]} array of stashed children
+	 * @private
+	 */
+	function getStashedControls(sId) {
+		if (!StashedControlSupport) {
+			StashedControlSupport = sap.ui.require("sap/ui/core/StashedControlSupport");
+		}
+		if (StashedControlSupport) {
+			return StashedControlSupport.getStashedControls(sId);
+		}
+		return [];
+	}
+
+	/**
 	 * A global preprocessor for the ID of a ManagedObject (used internally).
 	 * If set, this function will be called before the ID is applied to any ManagedObject.
 	 * If the original ID was empty, the hook will not be called (to be discussed).
@@ -1906,6 +1929,13 @@ sap.ui.define([
 		var aChildren = this.mAggregations[sAggregationName],
 			i, aChild;
 
+		// destroy surrogates in this aggregation
+		getStashedControls(this.getId()).forEach(function(c) {
+			if (c.sParentAggregationName === sAggregationName) {
+				c.destroy();
+			}
+		});
+
 		if (!aChildren) {
 			return this;
 		}
@@ -2157,6 +2187,11 @@ sap.ui.define([
 		for (var oAggr in this.mAggregations) {
 			this.destroyAggregation(oAggr, bSuppressInvalidate);
 		}
+
+		// destroy all inactive children
+		getStashedControls(this.getId()).forEach(function(c) {
+			c.destroy();
+		});
 
 		// Deregister, if available
 		if (this.deregister) {
@@ -3745,6 +3780,14 @@ sap.ui.define([
 						mSettings[sName] = oAggregation;
 					}
 				}
+			}
+
+			// clone inactive children
+			var aInactiveChildren = getStashedControls(this.getId());
+			for (var i = 0, l = aInactiveChildren.length; i < l; i++) {
+					var oClonedChild = aInactiveChildren[i].clone(sIdSuffix);
+					oClonedChild.sParentId = sId;
+					oClonedChild.sParentAggregationName = aInactiveChildren[i].sParentAggregationName;
 			}
 
 			// Clone associations
