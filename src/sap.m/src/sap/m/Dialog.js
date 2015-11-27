a325d45fc9c900ce6fb47568d29b903583be2942
Nikolay Aleksandrenko <nikolay.aleksandrenko@sap.com>
2015-11-27 17:46:17 +0200

    [FIX] sap.m.Dialog: tap on header (draggable) and rerender will no longer break the postion
    Change-Id: I01454d770adc6c560606f66b6930abb09ed3a63f
    BCP: 1570837379
    CR-Id: 012006153200004629922015

--- src/sap.m/src/sap/m/Dialog.js
+++ src/sap.m/src/sap/m/Dialog.js
@@ -1338,6 +1338,8 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Associative
 				};
 
 				if ((isHeaderClicked(e.target) && this.getDraggable()) || bResize) {
+					that._bDisableRepositioning = true;
+
 					that._$dialog.addClass('sapDialogDisableTransition');
 					//remove the transform translate
 					that._$dialog.addClass('sapMDialogTouched');
