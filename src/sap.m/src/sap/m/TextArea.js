f264d64d99839e97af668763a5edc3a97e703d96
D058983 <cahit.guerguec@sap.com>
2015-12-07 09:40:05 +0100

    [FIX] sap.m.TextArea: Default for wrapping property in constructor.
    Fixes: https://github.com/SAP/openui5/issues/695
    
    Change-Id: Id28af743d98a44dfb831c1b7daf1a2a2d9acdfc6

--- src/sap.m/src/sap/m/TextArea.js
+++ src/sap.m/src/sap/m/TextArea.js
@@ -58,7 +58,7 @@ sap.ui.define(['jquery.sap.global', './InputBase', './library'],
 			/**
 			 * Indicates how the control wraps the text, e.g. <code>Soft</code>, <code>Hard</code>, <code>Off</code>.
 			 */
-			wrapping : {type : "sap.ui.core.Wrapping", group : "Behavior", defaultValue : null},
+			wrapping : {type : "sap.ui.core.Wrapping", group : "Behavior", defaultValue : sap.ui.core.Wrapping.None},
 	
 			/**
 			 * Indicates when the <code>value</code> property gets updated with the user changes. Setting it to <code>true</code> updates the <code>value</code> property whenever the user has modified the text shown on the text area.
