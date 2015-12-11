b5f37c941b52f61833f6a56b8596bf74a9445a3d
Marin Parushev <marin.parushev@sap.com>
2015-12-11 09:54:03 +0200

    [INTERNAL] sap.m.NotificationListItem: Added visual tests
    - Also tests for sap.m.NotificationListGroup
    
    Change-Id: Ia69df46e0986aaf013533f00a09a9e4fca7d4ef9

---
+++ src/sap.m/test/sap/m/visual/NotificationListItem.spec.js
@@ -0,0 +1,27 @@
+describe('sap.m.NotificationListItem', function() {
+
+    it('should load test page', function () {
+        expect(takeScreenshot()).toLookAs('initial');
+    });
+
+    it('should test compact mode', function () {
+        element(by.id('toggleCompactModeButton')).click();
+        expect(takeScreenshot()).toLookAs('compact-mode');
+        element(by.id('toggleCompactModeButton')).click();
+    });
+
+    it('should fire "accept" button pressed event', function () {
+        element(by.id('notificationAcceptButton')).click();
+        expect(takeScreenshot()).toLookAs('accept-clicked');
+    });
+
+    it('should fire "cancel" button pressed event', function () {
+        element(by.id('notificationCancelButton')).click();
+        expect(takeScreenshot()).toLookAs('cancel-clicked');
+    });
+
+    it('should close the item', function () {
+        element(by.css('#firstNotification .sapMNLI-CloseButton')).click();
+        expect(takeScreenshot()).toLookAs('close-clicked');
+    });
+});
