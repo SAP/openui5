5c60501b4d63838ec8729e104a0595a52a99b808
Pavan <pavan.nayak@sap.com>
2015-12-11 13:40:36 +0100

    [INTERNAL][FIX] AnalyticalBinding: add parameter for dataReceived event
    AnalyticalBinding fires "dataReceived" event multiple times:
    add an internal "__simulateAsyncAnalyticalBinding"  parameter in the
    first/additional dataReceived event fired by the AnalyticalBinding; so
    that consumers can differentiate between actual dataReceived event and
    this simulation scenario.
    Also synchronize the behaviour of fireDataRecieved with ODataListBinding
    by returning data in the event parameter.
    
    BCP: 1570865004
    BCP: 1570876337
    Change-Id: I047434a1be83b0afadb7355c4428041d347545c9

--- src/sap.ui.core/src/sap/ui/model/analytics/AnalyticalBinding.js
+++ src/sap.ui.core/src/sap/ui/model/analytics/AnalyticalBinding.js
@@ -1997,7 +1997,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 		var aBatchQueryRequest = [], aExecutedRequestDetails = [];
 
 		function triggerDataReceived() {
-			that.fireDataReceived();
+			that.fireDataReceived({__simulateAsyncAnalyticalBinding: true});
 		}
 
 		// Batch Response Handling for ODataModel V2
@@ -2017,7 +2017,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 
 			if (oAnalyticalQueryRequest.getURIQueryOptionValue("$select") == null) {
 				// no dimensions and no measures requested, so create an artificial empty root context (synonym for the regular "/")
-				this.fireDataRequested(); // simulate the async behavior
+				this.fireDataRequested({__simulateAsyncAnalyticalBinding: true}); // simulate the async behavior
 
 				// perform all steps of fct fnSuccess (w/o calling it, b/c its argument is some data object and not a context
 				sGroupId = null;
@@ -2161,7 +2161,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 			var aBatchErrors;
 
 			// raise event here since there is no separate fnCompleted handler for batch requests
-			that.fireDataReceived();
+			that.fireDataReceived({data: oData});
 
 			//check for possible V1 errors
 			var oV1Errors = {};
@@ -2247,7 +2247,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 
 		if (oAnalyticalQueryRequest.getURIQueryOptionValue("$select") == null) {
 			// no dimensions and no measures requested, so create an artificial empty root context (synonym for the regular "/")
-			this.fireDataRequested(); // simulate the async behavior
+			this.fireDataRequested({__simulateAsyncAnalyticalBinding: true}); // simulate the async behavior
 
 			// perform all steps of fct fnSuccess (w/o calling it, b/c its argument is some data object and not a context
 			sGroupId = null;
@@ -2258,7 +2258,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 			// simulate the async behavior for the root context in case of having no sums (TODO: reconsider!)
 			setTimeout(function() {
 				if (that._cleanupGroupingForCompletedRequest(oRequestDetails.sRequestId)) {
-					that.fireDataReceived();
+					that.fireDataReceived({__simulateAsyncAnalyticalBinding: true});
 				}
 			});
 			this.bArtificalRootContext = true;
@@ -2322,17 +2322,17 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/TreeBinding', 'sap/ui/model/Ch
 			// the correct moment to clean up is after the success handler
 			// the error handler takes care of this itself
 			if (that.iModelVersion === AnalyticalVersionInfo.V2) {
-				fnCompleted();
+				fnCompleted(oData);
 			}
 		}
 
-		function fnCompleted() {
+		function fnCompleted(oData) {
 			if (iCurrentAnalyticalInfoVersion != that.iAnalyticalInfoVersionNumber) {
 				// discard responses for outdated analytical infos
 				return;
 			}
 			if (that._cleanupGroupingForCompletedRequest(oRequestDetails.sRequestId)) {
-				that.fireDataReceived();
+				that.fireDataReceived({data: oData});
 			}
 		}
 
