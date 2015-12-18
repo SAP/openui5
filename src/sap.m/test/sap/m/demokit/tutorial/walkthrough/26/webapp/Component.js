24cfd84173535c5f91f675cb2a1af77e1dcce206
Nabi Zamani <nabi.zamani@sap.com>
2015-12-18 16:13:26 +0100

    [INTERNAL] use automatic odatamodel and jsonmodel creation via app descriptor
    Change-Id: I6d6774c385889fb2a3a3b4555b4dae11b3d66f9b

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/26/webapp/Component.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/26/webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/demo/wt/controller/HelloDialog",
-	"sap/ui/model/odata/v2/ODataModel"
-], function (UIComponent, JSONModel, HelloDialog, ODataModel) {
+	"sap/ui/demo/wt/controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.demo.wt.Component", {
@@ -26,16 +25,10 @@ sap.ui.define([
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
 
-			// set invoice model - remote
-			var oConfig = this.getMetadata().getConfig();
-			var oInvoiceModel = new ODataModel(oConfig.invoiceRemote);
-
-			// debug code to show an alert for CORS issues in the tutorial
-			oInvoiceModel.attachMetadataFailed(function(oEvent) {
-				alert("Request to the remote service failed due to CORS issues - Read the tutorial (Walkthrough Step 26) to understand why no data is shown here.");
+			// debug code to show an alert for missing destination or CORS issues in the tutorial (see step 26 for details)
+			this.getModel("invoice").attachMetadataFailed(function(oEvent) {
+				alert("Request to the OData remote service failed.\nRead the Walkthrough Tutorial Step 26 to understand why you don't see any data here.");
 			});
-
-			this.setModel(oInvoiceModel, "invoice");
 
 			// set dialog
 			this.helloDialog = new HelloDialog();
