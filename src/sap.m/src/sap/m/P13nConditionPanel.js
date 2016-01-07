214b51d293c10238871262bd38dbcb67140b25b9
Peter Harbusch <peter.harbusch@sap.com>
2016-01-07 11:23:49 +0100

    [INTERNAL][FIX] P13nConditionPanel.js: changing the default selection of keyfield combobox
    Change-Id: I08860ed7c04d4280859462a8059043cc73ea7fce

--- src/sap.m/src/sap/m/P13nConditionPanel.js
+++ src/sap.m/src/sap/m/P13nConditionPanel.js
@@ -629,8 +629,7 @@ sap.ui.define([
 				"SpanFilter": "L3 M5 S10",
 				"SpanSort": "L5 M5 S12",
 				"SpanGroup": "L4 M4 S12",
-				"Control": "ComboBox",
-				"SelectedKey": "0"
+				"Control": "ComboBox"
 			}, {
 				"ID": "operationLabel",
 				"Text": "Sort Order",
@@ -644,8 +643,7 @@ sap.ui.define([
 				"SpanFilter": "L2 M5 S10",
 				"SpanSort": sap.ui.Device.system.phone ? "L5 M5 S8" : "L5 M5 S9" ,
 				"SpanGroup": "L2 M5 S10",
-				"Control": "ComboBox",
-				"SelectedKey": "0"
+				"Control": "ComboBox"
 			}, {
 				"ID": "value1",
 				"Label": this._sFromLabelText,
@@ -906,7 +904,6 @@ sap.ui.define([
 				case "ComboBox":
 					if (field["ID"] === "keyField") {
 						oControl = new sap.m.ComboBox({ // before we used the new sap.m.Select control
-							selectedKey: field["SelectedKey"],
 							width: "100%"
 						});
 
@@ -994,7 +991,6 @@ sap.ui.define([
 
 					if (field["ID"] === "operation") {
 						oControl = new sap.m.Select({
-							selectedKey: field["SelectedKey"],
 							width: "100%",
 							layoutData: new sap.ui.layout.GridData({
 								span: field["Span" + this._sConditionType]
