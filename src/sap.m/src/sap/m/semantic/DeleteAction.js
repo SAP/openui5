57cdff9dc20433038ce8822205b651c2a28bad7d
Diana Pazheva <diana.pazheva@sap.com>
2016-01-12 15:05:45 +0200

    [INTERNAL] sap.m.semantic.SemanticPage: Added Delete action
    Change-Id: I65fd14dfaa12b4376ce63e4b3795838cddde50e4

---
+++ src/sap.m/src/sap/m/semantic/DeleteAction.js
@@ -0,0 +1,37 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define(['sap/m/semantic/SemanticButton'], function(SemanticButton) {
+	"use strict";
+
+	/**
+	 * Constructor for a new DeleteAction.
+	 * @param {string} [sId] ID for the new control, generated automatically if no ID is given
+	 * @param {object} [mSettings] Custom initial settings for the new control
+	 *
+	 * @class
+	 * A DeleteAction button has default semantic-specific properties and is
+	 * eligible for aggregation content of a {@link sap.m.semantic.SemanticPage}.
+	 *
+	 * @extends sap.m.semantic.SemanticButton
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @constructor
+	 * @public
+	 * @since 1.30.0
+	 * @alias sap.m.semantic.DeleteAction
+	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
+	 */
+
+	var DeleteAction = SemanticButton.extend("sap.m.semantic.DeleteAction", /** @lends sap.m.semantic.DeleteAction.prototype */ {
+		metadata: {
+			library: "sap.m"
+		}
+	});
+
+	return DeleteAction;
+
+}, /* bExport= */ true);
