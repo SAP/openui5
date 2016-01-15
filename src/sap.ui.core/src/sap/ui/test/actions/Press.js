2fb96cd7209e33ac37ea27e188f1753a60957bb8
Tobias Oetzel <tobias.oetzel@sap.com>
2016-01-15 16:03:46 +0100

    [INTERNAL] sap.ui.test.action.Press - works for SearchField now
    - the type of the event was missing so jQuery.event.fixHooks where not applied
    - added a (in the first version private) adapter API for Press
    
    Change-Id: Id896f8479b39e24f981d7e4ffdcf8e6c8026a424

--- src/sap.ui.core/src/sap/ui/test/actions/Press.js
+++ src/sap.ui.core/src/sap/ui/test/actions/Press.js
@@ -5,6 +5,35 @@
 sap.ui.define(['jquery.sap.global', './Action', 'sap/ui/qunit/QUnitUtils'], function ($, Action, QUnitUtils) {
 	"use strict";
 
+	function triggerEvent (sName, $FocusDomRef) {
+		var oFocusDomRef = $FocusDomRef[0],
+			x = $FocusDomRef.offset().x,
+			y = $FocusDomRef.offset().y;
+
+		// See file jquery.sap.events.js for some insights to the magic
+		var oMouseEventObject = {
+			identifier: 1,
+			// Well offset should be fine here
+			pageX: x,
+			pageY: y,
+			// ignore scrolled down stuff in OPA
+			clientX: x,
+			clientY: y,
+			// Assume stuff is over the whole screen
+			screenX: x,
+			screenY: y,
+			target: $FocusDomRef[0],
+			radiusX: 1,
+			radiusY: 1,
+			rotationAngle: 0,
+			// left mouse button
+			button: 0,
+			// include the type so jQuery.event.fixHooks can copy properties properly
+			type: sName
+		};
+		QUnitUtils.triggerEvent(sName, oFocusDomRef, oMouseEventObject);
+	}
+
 	/**
 	 * @class The Press action is used to simulate a press interaction on a control's dom ref.
 	 * @extends sap.ui.test.actions.Action
@@ -13,7 +42,7 @@ sap.ui.define(['jquery.sap.global', './Action', 'sap/ui/qunit/QUnitUtils'], func
 	 * @author SAP SE
 	 * @since 1.34
 	 */
-	return Action.extend("sap.ui.test.actions.Press", /** @lends sap.ui.test.actions.Press.prototype */ {
+	var Press = Action.extend("sap.ui.test.actions.Press", /** @lends sap.ui.test.actions.Press.prototype */ {
 
 		metadata : {
 			publicMethods : [ "executeOn" ]
@@ -28,45 +57,34 @@ sap.ui.define(['jquery.sap.global', './Action', 'sap/ui/qunit/QUnitUtils'], func
 		 * @public
 		 */
 		executeOn : function (oControl) {
-			var oFocusDomRef = oControl.getFocusDomRef(),
-				$FocusDomRef = $(oFocusDomRef);
+			var $FocusDomRef,
+				sAdapterDomRef = Press._controlAdapters[oControl.getMetadata().getName()];
+
+			if (sAdapterDomRef) {
+				$FocusDomRef = oControl.$(sAdapterDomRef);
+			} else {
+				$FocusDomRef = $(oControl.getFocusDomRef());
+			}
 
 			if ($FocusDomRef.length) {
 				$FocusDomRef.focus();
-				// trigger 'tap' which is translated
-				// internally into a 'press' event
 				$.sap.log.debug("Pressed the control " + oControl, this._sLogPrefix);
-				var x = $FocusDomRef.offset().x,
-					y = $FocusDomRef.offset().y;
-
-				// See file jquery.sap.events.js for some insights to the magic
-				var oMouseEventObject = {
-					identifier: 1,
-					// Well offset should be fine here
-					pageX: x,
-					pageY: y,
-					// ignore scrolled down stuff in OPA
-					clientX: x,
-					clientY: y,
-					// Assume stuff is over the whole screen
-					screenX: x,
-					screenY: y,
-					target: oFocusDomRef,
-					radiusX: 1,
-					radiusY: 1,
-					rotationAngle: 0
-				};
-
 
 				// the missing events like saptouchstart and tap will be fired by the event simulation
-				QUnitUtils.triggerEvent("mousedown", oFocusDomRef,oMouseEventObject);
-				QUnitUtils.triggerEvent("selectstart", oFocusDomRef);
-				QUnitUtils.triggerEvent("mouseup", oFocusDomRef,oMouseEventObject);
-				QUnitUtils.triggerEvent("click", oFocusDomRef, oMouseEventObject);
+				triggerEvent("mousedown", $FocusDomRef);
+				QUnitUtils.triggerEvent("selectstart", $FocusDomRef[0]);
+				triggerEvent("mouseup", $FocusDomRef);
+				triggerEvent("click", $FocusDomRef);
 			} else {
 				$.sap.log.error("Control " + oControl + " has no dom representation", this._sLogPrefix);
 			}
 		}
 	});
+
+	Press._controlAdapters = {
+		"sap.m.SearchField" : "search"
+	};
+
+	return Press;
 
 }, /* bExport= */ true);
