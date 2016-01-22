80360071755c2a4b6a00ef65d4adba1a4fbf6e2f
Arley Triana Morin <arley.triana.morin@sap.com>
2016-01-22 09:39:33 +0100

    [FIX] sap.m.ComboBox: exclude disable items from the auto-complete suggestion list
    Change-Id: I1fa232120f3e8a32638f2183c5458ffe1579fb58
    BCP: 1670033530
    CR-Id: 012006153200000437332016

--- src/sap.m/src/sap/m/ComboBox.js
+++ src/sap.m/src/sap/m/ComboBox.js
@@ -338,7 +338,7 @@ sap.ui.define(['jquery.sap.global', './ComboBoxBase', './ComboBoxRenderer', './P
 				}
 
 				// first match of the value
-				if (bFirst && bMatch && sValue !== "") {
+				if (oItem.getEnabled() && bFirst && bMatch && sValue !== "") {
 					bFirst = false;
 
 					if (this._bDoTypeAhead) {
