28ffcb4d84f8f5a3c721c0a09d573faff421864a
Dimo Petrov <dimo.petrov@sap.com>
2016-01-29 18:09:33 +0200

    [FIX] NavContainer didn't get properly invalidated
    BCP: 1670005665
    Change-Id: I310da3e57231acc3cabd230921f7b1b8f911daba

--- src/sap.m/src/sap/m/NavContainer.js
+++ src/sap.m/src/sap/m/NavContainer.js
@@ -1538,7 +1538,7 @@ sap.ui.define([
 				}
 			}
 
-			if (!bIsInPages || oSource.getId() === this.getCurrentPage()) {
+			if (!bIsInPages || oSource === this.getCurrentPage()) {
 				// TODO: there will be more cases where invalidation is not required...
 				this.forceInvalidation();
 			} // else : the invalidation source is a non-current page, so do not rerender anything
