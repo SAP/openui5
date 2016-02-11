1f959093e9ef24547792ce1349599c08add2b11d
Jiawei Cao <jiawei.cao@sap.com>
2016-02-11 15:40:28 +0100

    [FIX] sap.ui.core.tutorial.navigation: explicit set log level for steps 16 and 17
    Change-Id: I266f7c06e43667ccc3551ca99cb5dfeb1218d662
    BCP: 1670070558

--- src/sap.ui.core/test/sap/ui/core/demokit/tutorial/navigation/17/webapp/controller/App.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/navigation/17/webapp/controller/App.controller.js
@@ -6,6 +6,13 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 
 		onInit: function () {
+			// This is ONLY for being used within the tutorial.
+			// The default log level of the current running environment may be higher than INFO,
+			// in order to see the debug info in the console, the log level needs to be explicitly
+			// set to INFO here.
+			// But for application development, the log level doesn't need to be set again in the code.
+			jQuery.sap.log.setLevel(jQuery.sap.log.Level.INFO);
+
 			var oRouter = this.getRouter();
 
 			oRouter.attachBypassed(function (oEvent) {
@@ -26,4 +33,3 @@ sap.ui.define([
 	});
 
 });
-
