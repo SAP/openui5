04fd2eb88ba1aa56f9ee01205f94f71cb3118c30
Siarhei Ulasenka <siarhei.ulasenka@sap.com>
2016-02-24 16:35:50 +0100

    [FEATURE] new sap.m.FormattedText control
    - sap.m.FormattedText control dispays rich text in HTML format.
      A subset of HTML tags can be used for formatting.
    
      See the control documentation for more information.
    
    Change-Id: If669d144aa346c09618ef1d50b8a646e87d5ec75

---
+++ src/sap.m/src/sap/m/FormattedTextRenderer.js
@@ -0,0 +1,42 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(['jquery.sap.global'],
+	function (jQuery) {
+		"use strict";
+
+
+		/**
+		 * FormattedText renderer.
+		 * @namespace
+		 */
+		var FormattedTextRenderer = {};
+
+		/**
+		 * Renders the HTML for the FormattedText, using the provided
+		 * {@link sap.ui.core.RenderManager}.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for
+		 * writing to the render output buffer
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered
+		 */
+		FormattedTextRenderer.render = function (oRm, oControl) {
+			var sHtml = oControl.getHtmlText();
+
+			oRm.write("<div");
+			oRm.writeControlData(oControl);
+			oRm.addClass("sapMFT");
+			oRm.writeClasses();
+			// render Tooltip
+			if (oControl.getTooltip_AsString()) {
+				oRm.writeAttributeEscaped("title", oControl.getTooltip_AsString());
+			}
+			oRm.write(">"); // span element
+			// render the remainder of the HTML
+			oRm.write(sHtml);
+			oRm.write("</div>");
+		};
+
+		return FormattedTextRenderer;
+
+	}, /* bExport= */ true);
