0b588cfe28ebdd73868b971b1cd6a5ba7bc52029
Tobias Oetzel <tobias.oetzel@sap.com>
2016-02-24 23:41:13 +0100

    [INTERNAL] sap.m.Shoppting cart try gherkin
    Change-Id: I189d771724c6038590bbea4ca3d0566b4c00d5e4

---
+++ src/sap.m/test/sap/m/demokit/cart/test/requireQUnit.js
@@ -0,0 +1,11 @@
+jQuery.sap.require("sap.ui.qunit.qunit-css");
+jQuery.sap.require("sap.ui.thirdparty.qunit");
+jQuery.sap.require("sap.ui.qunit.qunit-junit");
+jQuery.sap.require("sap.ui.qunit.qunit-coverage");
+jQuery.sap.require("sap.ui.thirdparty.sinon");
+jQuery.sap.require("sap.ui.thirdparty.sinon-qunit");
+
+/*
+ global QUnit
+ */
+QUnit.config.autostart = false;
