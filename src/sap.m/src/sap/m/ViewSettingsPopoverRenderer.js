63461290d0009df709155fb5b350e8a40cbf0328
Vesselin Petrunov <vesselin.petrunov@sap.com>
2016-02-29 17:44:04 +0200

    [INTERNAL] sap.m.ViewSettingsPopover: Initial implementation of new control.
    Change-Id: I1ec27333034e967506f8797dbda613ec755e1a4b

---
+++ src/sap.m/src/sap/m/ViewSettingsPopoverRenderer.js
@@ -0,0 +1,25 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define(["jquery.sap.global"],
+	function(jQuery) {
+		"use strict";
+
+		/**
+		 * ViewSettingsPopoverRenderer renderer.
+		 * @namespace
+		 */
+		var ViewSettingsPopoverRenderer = {};
+
+		/**
+		 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm the RenderManager that can be used for writing to the render output buffer
+		 * @param {sap.ui.core.Control} oControl an object representation of the control that should be rendered
+		 */
+		ViewSettingsPopoverRenderer.render = function (oRm, oControl) {};
+
+		return ViewSettingsPopoverRenderer;
+
+	}, /* bExport= */ true);
