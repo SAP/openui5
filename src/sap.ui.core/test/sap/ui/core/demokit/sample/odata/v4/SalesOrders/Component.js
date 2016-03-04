f4c173748fc12098e3b3ddc49a4c069e8f4a2c56
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2016-03-04 10:55:59 +0100

    [INTERNAL] sap.ui.core.demokit.sample.odata.v4.SalesOrders.Component: fix EsLint error
    PS1: remove wrong ,
    
    Change-Id: Iebc40772eb0488cffe0c53240775496378a9d2dc

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/SalesOrders/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/SalesOrders/Component.js
@@ -68,7 +68,7 @@ sap.ui.define([
 						"SalesOrderList(SalesOrderID='0500000004')?$expand=SO_2_SOITEM($expand=SOITEM_2_PRODUCT($expand=PRODUCT_2_BP($expand=BP_2_CONTACT)))&$select=ChangedAt,CreatedAt,LifecycleStatusDesc,Note,SalesOrderID" : {
 							source : "SalesOrderList_4.txt"
 						}
-					},
+					}
 				}, "sap/ui/core/demokit/sample/odata/v4/SalesOrders/data",
 				"/sap/opu/odata4/IWBEP/V4_SAMPLE/default/IWBEP/V4_GW_SAMPLE_BASIC/0001/");
 			}
