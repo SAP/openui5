a776757cbc8149b5f3cf78d7db8bf223a54411e4
Jens Pflueger <jens.pflueger@sap.com>
2016-03-07 11:13:31 +0100

    [FIX] sap.ui.unified.Menu: Resore focus when click outside the menu
    Change-Id: Ie2486d2f440a806d059b15e56775dcd9085ae9b9
    BCP: 1670001286
    CR-Id: 012003146900000253702016

--- src/sap.ui.unified/src/sap/ui/unified/Menu.js
+++ src/sap.ui.unified/src/sap/ui/unified/Menu.js
@@ -602,6 +602,8 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Popup', 
 		var isInMenuHierarchy = false,
 			touchEnabled = this.getPopup().touchEnabled;
 
+		this.bIgnoreOpenerDOMRef = false;
+
 		if (oEvent.type == "mousedown" || oEvent.type == "touchstart") {
 			// Suppress the delayed mouse event from mobile browser
 			if (touchEnabled && (oEvent.isMarked("delayedMouseEvent") || oEvent.isMarked("cancelAutoClose"))) {
@@ -628,10 +630,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/Popup', 
 					that = that.oOpenedSubMenu;
 				}
 			}
+			if (!isInMenuHierarchy) {
+				this.bIgnoreOpenerDOMRef = true;
+			}
 		}
 
 		if (!isInMenuHierarchy) {
-			this.bIgnoreOpenerDOMRef = true;
 			this.close();
 		}
 	};
