1bf3e02f6c64baddcff5345d9252355144c7c28b
Jens Himmelrath <jens.himmelrath@sap.com>
2016-03-16 17:47:06 +0100

    [INTERNAL] Databinding Tutorial Simplification
    - Replaced the manual binding in the onInit method with a binding
      declaration in the XML view
    - Simplified the ProductList factory method to be more understandable
    - Changed incorrect property binding of CheckBox
    
    BCP: 1580197442
    Change-Id: I9ad6a0b8b2cee4a8b6c92808cccb8ee65d42ef99

--- src/sap.ui.core/test/sap/ui/core/demokit/tutorial/databinding/15/webapp/controller/App.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/databinding/15/webapp/controller/App.controller.js
@@ -36,24 +36,17 @@ function(Controller, Currency) {
 				oUIControl = new sap.m.StandardListItem(sId, {
 					icon : "sap-icon://warning",
 					title : sDescription,
-					info : {
-						path : "i18n>discontinued"
-					},
+					info : { path: "i18n>discontinued" },
 					infoState : "Error"
 				});
 			} else {
 				// Nope, so we will create an
 				// ObjectListItem
+				
 				oUIControl = new sap.m.ObjectListItem(sId, {
 					title : sDescription,
 					number : {
-						parts : [
-								{
-									path : "products>UnitPrice"
-								},
-								{
-									path : "/currencyCode"
-								} ],
+						parts : [ "products>UnitPrice", "/currencyCode" ],
 						type : "sap.ui.model.type.Currency",
 						formatOptions : {
 							showMeasure : false
@@ -63,81 +56,22 @@ function(Controller, Currency) {
 						path : "/currencyCode"
 					}
 				});
-
-				// Is this item in stock?
-				if (oContext.getProperty("UnitsInStock") > 0) {
-					// First object attribute
-					// displays the current
-					// stock level
-					oUIControl.addAttribute(new sap.m.ObjectAttribute({
-						title : {
-							path : "i18n>unitsInStock"
-						},
-						text : {
-							path : "products>UnitsInStock"
-						}
-					}));
-
-					// Second object attribute displays the current stock value
-					oUIControl.addAttribute(new sap.m.ObjectAttribute({
-						title : {
-							path : "i18n>stockValue"
-						},
-						text : {
-							parts : [
-									{
-										path : "products>UnitPrice"
-									},
-									{
-										path : "products>UnitsInStock"
-									},
-									{
-										path : "/currencyCode"
-									} ],
-							formatter : this.formatStockValue
-						}
-					}));
-
-					// Has this product been discontinued?
-					if (oContext.getProperty("Discontinued")) {
-						// Yup, so we're selling off the last remaining stock items
-						// Set the status of the first attribute to "discontinued"
-						oUIControl.setFirstStatus(new sap.m.ObjectStatus({
-							text : {
-								path : "i18n>discontinued"
-							},
-							state : "Error"
-						}));
-					}
-				} else {
+				
+				// Is this item out of stock?
+				if (oContext.getProperty("UnitsInStock") < 1) {
 					// Nope, so this item is just temporarily out of stock
 					oUIControl.addAttribute(new sap.m.ObjectAttribute({
-							text : {
-								path : "i18n>outOfStock"
-							}
-						}
-					));
+						text : { path: "i18n>outOfStock" }
+					}));
 				}
 			}
+
 			// Set item active (so it is clickable) and attach the press event
 			// handler for showing the details
 			oUIControl.setType(sap.m.ListType.Active);
 			oUIControl.attachPress(this.onItemSelected, this);
 			return oUIControl;
 		},
-
-		onInit : function() {
-			// Get a reference to the Product List using the XML element's id
-			var oProductList = this.getView()
-					.byId("ProductList");
-
-			// In addition to binding the "items" aggregation in the list to the Products array
-			// in the "products" model, the productListFactory function is also passed as a parameter
-			oProductList.bindAggregation(
-					"items",
-					"products>/Products",
-					this.productListFactory.bind(this));
-		}
 
 	});
 });
