3cbc428b87cbe8e36a76686459918a236d3d688b
Mingxin Fu <mingxin.fu@sap.com>
2016-04-04 09:51:37 +0200

    [FIX] sap.m.GenericTile: accessibility for screen reader improved
    Change-Id: Ia062e1f3aa6f3680c6d0f836b733c8dd408c9349
    BCP: 1580245957
    CR-Id: 012003146900000360592016

--- src/sap.m/src/sap/m/GenericTileRenderer.js
+++ src/sap.m/src/sap/m/GenericTileRenderer.js
@@ -32,7 +32,11 @@ sap.ui.define([], function() {
 		oRm.addClass(oControl.getSize());
 		oRm.addClass(oControl.getFrameType());
 
-		oRm.writeAttribute("role", "presentation");
+		if (oControl.hasListeners("press")) {
+			oRm.writeAttribute("role", "button");
+		} else {
+			oRm.writeAttribute("role", "presentation");
+		}
 		oRm.writeAttributeEscaped("aria-label", oControl.getAltText());
 
 		if (oControl.hasListeners("press") && oControl.getState() != sap.m.LoadState.Disabled) {
