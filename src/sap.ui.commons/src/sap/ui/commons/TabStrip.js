bcd6f513078b7e4f8c43bd19d8dcbe118cd4f81c
I312031 <teodor.taushanov@sap.com>
2016-04-20 10:40:22 +0300

    [FIX] sap.ui.commons.TabStrip: Error is not thrown any more when Scroller in not initialized
    Change-Id: I48d7be05d5f30dfc7590709fa1e5e8af3a3df136
    BCP: 1670246494
    CR-Id: 012003146900000425102016

--- src/sap.ui.commons/src/sap/ui/commons/TabStrip.js
+++ src/sap.ui.commons/src/sap/ui/commons/TabStrip.js
@@ -366,7 +366,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control',
 		var aTabs = this.getTabs();
 		var oTab = aTabs[iSelectedIndex];
 
-		if (oTab && oTab.$().length > 0) {
+		if (this._oScroller && oTab && oTab.$().length > 0) {
 			this._scrollIntoView(oTab.$(), TabStrip.SCROLL_ANIMATION_DURATION);
 		}
 
@@ -1079,7 +1079,10 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control',
 			}
 		}
 
-		this._oScroller.scrollTo(iScrollTarget, 0, iDuration);
+		if (this._oScroller) {
+			this._oScroller.scrollTo(iScrollTarget, 0, iDuration);
+		}
+
 		this._iCurrentScrollLeft = iScrollTarget;
 	};
 
@@ -1116,7 +1119,10 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control',
 
 			// store current scroll state to set it after re-rendering
 			this._iCurrentScrollLeft = iNewScrollLeft;
-			this._oScroller.scrollTo(iNewScrollLeft, 0, iDuration);
+
+			if (this._oScroller) {
+				this._oScroller.scrollTo(iNewScrollLeft, 0, iDuration);
+			}
 		}
 	};
 
