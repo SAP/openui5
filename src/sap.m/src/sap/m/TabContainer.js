6a262ce5df113f14075006e5497ed559e231ddb5
Teodor Todorov <teodor.todorov@sap.com>
2016-04-21 12:11:33 +0300

    [INTERNAL][FIX] sap.m.TabContainer: Now correctly displayed as a full-page container
    Change-Id: Id0f5e8000304411bbbf38aecdd9ed966b1211351
    BCP: 1670208903

--- src/sap.m/src/sap/m/TabContainer.js
+++ src/sap.m/src/sap/m/TabContainer.js
@@ -8,13 +8,16 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		"use strict";
 
 		/**
-		 * Constructor for a new TabContainer.
+		 * Constructor for a new <code>TabContainer</code>.
 		 *
 		 * @param {string} [sId] ID for the new control, generated automatically if no ID is given
 		 * @param {object} [mSettings] Initial settings for the new control
 		 *
 		 * @class
-		 * The TabContainer control represents a collection of tabs with associated content.
+		 * The <code>TabContainer</code> control represents a collection of tabs with associated content.
+		 *
+		 * The <code>TabContainer</code> is a full-page container that takes 100% of the parent width and height.
+		 * As the control is expected to occupy the the whole parent, it should be the only child of its parent.
 		 *
 		 * @extends sap.ui.core.Control
 		 *
