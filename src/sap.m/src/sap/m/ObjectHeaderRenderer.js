2282890d6c5c8c7463bae2cd999c531886097954
Gergana Marcheva <gergana.marcheva@sap.com>
2016-04-22 11:39:00 +0300

    [FIX] sap.m.ObjectHeader: Active title link fixed
    Change-Id: I0cddb9bac2e61310c952dd318833157ccfd6775b
    BCP: 1670243145
    CR-Id: 012003146900000442482016

--- src/sap.m/src/sap/m/ObjectHeaderRenderer.js
+++ src/sap.m/src/sap/m/ObjectHeaderRenderer.js
@@ -503,7 +503,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/IconPool'],
 						oRM.writeAttributeEscaped("target", oOH.getTitleTarget());
 					}
 				} else {
-					oRM.writeAttribute("href", "#");
+					/*eslint-disable no-script-url */
+					oRM.writeAttribute("href", "javascript:void(0);");
+					/*eslint-enable no-script-url */
 				}
 
 				//ARIA attributes
@@ -1328,7 +1330,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/IconPool'],
 					oRM.writeAttributeEscaped("target", oOH.getTitleTarget());
 				}
 			} else {
-				oRM.writeAttribute("href", "#");
+				/*eslint-disable no-script-url */
+				oRM.writeAttribute("href", "javascript:void(0);");
+				/*eslint-enable no-script-url */
 			}
 
 			oRM.writeAttribute("tabindex", "0");
