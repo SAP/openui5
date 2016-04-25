640fce30a1954412f5829d918c46c61751dc96db
Matthias Osswald <mat.osswald@sap.com>
2016-04-25 16:38:50 +0200

    [INTERNAL] Update npm dependencies
    Update the "grunt-openui5" to latest version in order to use
    new features like inline theming parameters.
    Changed "relativeUrls" option to "false" as this was the default
    before. Relevant changes have only be made in v1.38+
    
    CR-Id: 012003146900000450052016
    Change-Id: I68483867d716c3ea0f3c6880d3bb325bb8121c4d
    (cherry picked from commit 6d803795b7dc6e2335fa7e26b9e2f834ac1ca1f0)

--- grunt/config/openui5_connect.js
+++ grunt/config/openui5_connect.js
@@ -15,6 +15,11 @@ module.exports = function(grunt, config) {
 			proxypath: 'proxy',
 			cors: {
 				origin: "*"
+			},
+			lessOptions: {
+				parser: {
+					relativeUrls: false
+				}
 			}
 
 		},
