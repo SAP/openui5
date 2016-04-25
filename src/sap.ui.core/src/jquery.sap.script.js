92980c08fcb2a53d3ae77f91b26b34ac501bb6b4
Malte Wedel <malte.wedel@sap.com>
2016-04-25 12:00:58 +0200

    [FEATURE] New diff algorithm and diff processing in ManagedObject
    Due to the bad performance of jQuery.sap.arrayDiff for large datasets, a
    new diff method was introduced, which does not compare entries with each
    other, but instead uses a symbol table and hashes of entries.
    
    Enhanced ListBinding.enableExtendedChangeDetection to allow
    configuration whether data updates within the same entity should be
    detected and to provide a key property/function for client models.
    
    Adapted ManagedObject.bindList to use new parameters on
    enableExtendedChangeDetection, and detect updates only if a factory
    function is used.
    
    Extended ManagedObject.updateAggregation to apply diff, if extended
    change detection is enabled
    
    Adapted JSONListBinding, XMLListBinding and v2.ODataListBinding to use
    new diff method and store compare data in serialized form to avoid
    cloning and comparing large object trees or XML nodes.
    
    BCP: 1570881249
    Change-Id: I37ade60ab7babb5254dcc04eb53682917384e2e0
    CR-Id: 012003146900000540562016

--- src/sap.ui.core/src/jquery.sap.script.js
+++ src/sap.ui.core/src/jquery.sap.script.js
@@ -402,11 +402,175 @@ sap.ui.define(['jquery.sap.global'],
 			};
 		}());
 
+	/**
+	 * This function generates a hash-code from a string
+	 * @param {string} sString The string to generate the hash-code from
+	 * @return {integer} The generated hash-code
+	 * @since 1.39
+	 * @private
+	 */
+	jQuery.sap.hashCode = function(sString) {
+		var iHash, iLength, iCharCode, i;
+		iHash = 0;
+		iLength = sString.length;
+
+		for (i = 0; i < iLength; i++) {
+			iCharCode = sString.charCodeAt(i);
+			iHash = (iHash << 5) - iHash + iCharCode;
+			iHash = iHash & iHash;
+		}
+		return iHash;
+	};
 
 	/**
 	 * Calculate delta of old list and new list
 	 * This implements the algorithm described in "A Technique for Isolating Differences Between Files"
 	 * (Commun. ACM, April 1978, Volume 21, Number 4, Pages 264-268)
+	 * @public
+	 * @param {Array} aOld Old Array
+	 * @param {Array} aNew New Array
+	 * @param {function} [fnSymbol] Function to get entry symbol
+	 * @return {Array} List of changes
+	 */
+	jQuery.sap.arraySymbolDiff = function(aOld, aNew, fnSymbol){
+		var mSymbols = {},
+			aOldRefs = [],
+			aNewRefs = [],
+			iOldLine,
+			vSymbol, oSymbol,
+			iOld = 0,
+			iNew = 0,
+			iOldRefLine,
+			iNewRefLine,
+			iOldDistance,
+			iNewDistance,
+			aDiff = [];
+
+		// If arrays are equal, don't try to diff them
+		if (aOld === aNew || jQuery.sap.equal(aOld, aNew)) {
+			return aDiff;
+		}
+
+		// If no symbol function is provided, we stringify, if it is not type string, and create a hash from it
+		fnSymbol = fnSymbol || function(vValue) {
+			if (typeof vValue !== "string") {
+				vValue = JSON.stringify(vValue);
+			}
+			return jQuery.sap.hashCode(vValue);
+		};
+
+		// Pass 1
+		for (var i = 0; i < aNew.length; i++) {
+			vSymbol = fnSymbol(aNew[i]);
+			oSymbol = mSymbols[vSymbol];
+			if (!oSymbol) {
+				oSymbol = mSymbols[vSymbol] = {
+					iNewCount: 0,
+					iOldCount: 0
+				};
+			}
+			oSymbol.iNewCount++;
+			aNewRefs[i] = {
+				symbol: oSymbol
+			};
+		}
+
+		// Pass 2
+		for (var i = 0; i < aOld.length; i++) {
+			vSymbol = fnSymbol(aOld[i]);
+			oSymbol = mSymbols[vSymbol];
+			if (!oSymbol) {
+				oSymbol = mSymbols[vSymbol] = {
+					iNewCount: 0,
+					iOldCount: 0
+				};
+			}
+			oSymbol.iOldCount++;
+			oSymbol.iOldLine = i;
+			aOldRefs[i] = {
+				symbol: oSymbol
+			};
+		}
+
+		// Pass 3
+		for (var i = 0; i < aNewRefs.length; i++) {
+			oSymbol = aNewRefs[i].symbol;
+			if (oSymbol.iNewCount === 1 && oSymbol.iOldCount === 1) {
+				aNewRefs[i].line = oSymbol.iOldLine;
+				aOldRefs[oSymbol.iOldLine].line = i;
+			}
+		}
+
+		// Pass 4
+		for (var i = 0; i < aNewRefs.length - 1; i++) {
+			iOldLine = aNewRefs[i].line;
+			if (iOldLine !== undefined && iOldLine < aOldRefs.length - 1) {
+				if (aOldRefs[iOldLine + 1].symbol === aNewRefs[i + 1].symbol) {
+					aOldRefs[iOldLine + 1].line = i + 1;
+					aNewRefs[i + 1].line = iOldLine + 1;
+				}
+			}
+		}
+
+		// Pass 5
+		for (var i = aNewRefs.length - 1; i > 0; i--) {
+			iOldLine = aNewRefs[i].line;
+			if (iOldLine !== undefined && iOldLine > 0) {
+				if (aOldRefs[iOldLine - 1].symbol === aNewRefs[i - 1].symbol) {
+					aOldRefs[iOldLine - 1].line = i - 1;
+					aNewRefs[i - 1].line = iOldLine - 1;
+				}
+			}
+		}
+
+		// Create diff
+		while (iOld < aOld.length || iNew < aNew.length) {
+			iNewRefLine = aOldRefs[iOld] && aOldRefs[iOld].line;
+			iOldRefLine = aNewRefs[iNew] && aNewRefs[iNew].line;
+			if (iOld < aOld.length && (iNewRefLine === undefined || iNewRefLine < iNew)) {
+				aDiff.push({
+					index: iNew,
+					type: "delete"
+				});
+				iOld++;
+			} else if (iNew < aNew.length && (iOldRefLine === undefined || iOldRefLine < iOld)) {
+				aDiff.push({
+					index: iNew,
+					type: "insert"
+				});
+				iNew++;
+			} else if (iNew === iNewRefLine) {
+				iNew++;
+				iOld++;
+			} else {
+				iNewDistance = iNewRefLine - iNew;
+				iOldDistance = iOldRefLine - iOld;
+				if (iNewDistance <= iOldDistance) {
+					aDiff.push({
+						index: iNew,
+						type: "insert"
+					});
+					iNew++;
+				} else {
+					aDiff.push({
+						index: iNew,
+						type: "delete"
+					});
+					iOld++;
+				}
+			}
+		}
+
+		return aDiff;
+	};
+
+	/**
+	 * Calculate delta of old list and new list
+	 * This partly implements the algorithm described in "A Technique for Isolating Differences Between Files"
+	 * but instead of working with hashes, it does compare each entry of the old list with each entry of the new
+	 * list, which causes terrible performane on large datasets.
+	 *
+	 * @deprecated
 	 * @public
 	 * @param {Array} aOld Old Array
 	 * @param {Array} aNew New Array
