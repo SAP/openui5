dc116daefc451ce79e7dd73624cd3c540b388aa7
Jan Mummenthaler <jan.mummenthaler@sap.com>
2016-04-28 10:50:03 +0200

    [FIX] ManagedObject: Propagate properties when setting parent to null
    In case of a 'move' - remove/add controls snychronous in an 
    aggregation - we should not propagate synchronous when setting the
    parent to null. Synchronous propagation destroys the bindings
    when removing a control from the aggregation and recreates them when
    adding the control again.
    This could lead to a data refetch, and in some scenarios to endless
    request loops.
    
    Change-Id: Ib2ddb387a06ac63cc56613669ab35199a9aefe6e
    CR-Id: 012003146900000464982016
    BCP: 1670131259

--- src/sap.ui.core/src/sap/ui/base/ManagedObject.js
+++ src/sap.ui.core/src/sap/ui/base/ManagedObject.js
@@ -2094,11 +2094,22 @@ sap.ui.define([
 			this.sParentAggregationName = null;
 			this.oPropagatedProperties = ManagedObject._oEmptyPropagatedProperties;
 
-			// if object is being destroyed no propagation needed
+			/* In case of a 'move' - remove/add controls snychronous in an aggregation -
+			 * we should not propagate synchronous when setting the parent to null.
+			 * Synchronous propagation destroys the bindings when removing a control
+			 * from the aggregation and recreates them when adding the control again.
+			 * This could lead to a data refetch, and in some scenarios to endless
+			 * request loops.
+			 */
 			if (!this._bIsBeingDestroyed) {
-				this.updateBindings(true, null);
-				this.updateBindingContext(false, undefined, true);
-				this.propagateProperties(true);
+				setTimeout(function() {
+					// if object is being destroyed or parent is set again (move) no propagation is needed
+					if (!this.oParent) {
+						this.updateBindings(true, null);
+						this.updateBindingContext(false, undefined, true);
+						this.propagateProperties(true);
+					}
+				}.bind(this), 0);
 			}
 
 			jQuery.sap.act.refresh();
