d3dfff3c086995abda155300b68ccd7c2b14d6e9
Stefan Scherer <stefan.scherer@sap.com>
2016-05-11 10:38:17 +0200

    [FIX] sap.ui.model.odata.type.Raw: throw proper exceptions
    PS1: throw Parse-/ValidateException instead of Error
    PS2: fixed CR-ID
    PS3: fixed the technical id of the Correction Request
    PS4: comments by PKS
    PS5: comments by TCH
    
    Change-Id: Ibb55825cbb201ea3f2835e83f6fe42d403c32f4c
    CR-Id: 012003146900000509882016

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/Raw.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/Raw.qunit.js
@@ -4,8 +4,10 @@
 sap.ui.require([
 	"sap/ui/model/FormatException",
 	"sap/ui/model/odata/type/ODataType",
-	"sap/ui/model/odata/type/Raw"
-], function (FormatException, ODataType, Raw) {
+	"sap/ui/model/odata/type/Raw",
+	"sap/ui/model/ParseException",
+	"sap/ui/model/ValidateException"
+], function (FormatException, ODataType, Raw, ParseException, ValidateException) {
 	/*global QUnit */
 	/*eslint max-nested-callbacks: 0*/
 	"use strict";
@@ -26,15 +28,17 @@ sap.ui.require([
 		["", "boolean", "int", "float", "object", "string"].forEach(function (sTargetType) {
 			assert.throws(function () {
 				oRaw.formatValue("", sTargetType);
-			}, new FormatException("Unsupported operation: sap.ui.model.odata.type.Raw#formatValue,"
-				+ " sTargetType must be 'any'"));
+			}, new FormatException(
+				"Type 'sap.ui.model.odata.type.Raw' does not support formatting"));
 		});
 		assert.throws(function () {
 			oRaw.parseValue("", "string");
-		}, new Error("Unsupported operation: sap.ui.model.odata.type.Raw#parseValue"));
+		}, new ParseException(
+			"Type 'sap.ui.model.odata.type.Raw' does not support parsing"));
 		assert.throws(function () {
 			oRaw.validateValue(null);
-		}, new Error("Unsupported operation: sap.ui.model.odata.type.Raw#validateValue"));
+		}, new ValidateException(
+			"Type 'sap.ui.model.odata.type.Raw' does not support validating"));
 
 		assert.throws(function () {
 			return new Raw(false);
