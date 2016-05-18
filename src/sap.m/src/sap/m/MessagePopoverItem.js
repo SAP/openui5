8503a96b9f3c3a090cfaac5e8bb1ddf1f7962cea
Yavor Ivanov <yavor.ivanov@sap.com>
2016-05-18 16:10:27 +0300

    [FIX] sap.m.MessagePopover: MessagePopoverItems' binding is properly updated
    There's a bug when item's property is being updated via MessagePopover API, nothing happens in the layout.
    Issue could be reproduced with the following SNIPPIX: http://veui5infra.dhcp.wdf.sap.corp:8080/snippix/#92242
    
    BCP: 1670235674
    
    Change-Id: Iab3862dfe68ccef562aa45dd531b19e55cd4ec3c
    CR-Id: 012003146900000562092016

--- src/sap.m/src/sap/m/MessagePopoverItem.js
+++ src/sap.m/src/sap/m/MessagePopoverItem.js
@@ -58,6 +58,29 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Item"],
 				}
 			});
 
+		MessagePopoverItem.prototype.setProperty = function (sPropertyName, oValue, bSuppressInvalidate) {
+			// BCP: 1670235674
+			// MessagePopoverItem acts as a proxy to StandardListItem
+			// So, we should ensure if something is changed in MessagePopoverItem, it would be propagated to the StandartListItem
+			var oParent = this.getParent(),
+				sType = this.getType().toLowerCase(),
+				fnUpdateProperty = function (sName, oItem) {
+					// Most of the properties have 1:1 mapping. However there are some exceptions, for example
+					// "subtitle" from MessagePopoverItem would become "description" in StandardListItem
+					var mMPItemToSLItem = {"subtitle": "description"};
+
+					if (oItem._oMessagePopoverItem.getId() === this.getId() && oItem.getMetadata().getProperty(mMPItemToSLItem[sName] || sName)) {
+						oItem.setProperty(sName, oValue);
+					}
+				};
+			if (oParent && ("_bItemsChanged" in oParent) && !oParent._bItemsChanged) {
+				oParent._oLists && oParent._oLists.all && oParent._oLists.all.getItems && oParent._oLists.all.getItems().forEach(fnUpdateProperty.bind(this, sPropertyName));
+				oParent._oLists && oParent._oLists[sType] && oParent._oLists[sType].getItems && oParent._oLists[sType].getItems().forEach(fnUpdateProperty.bind(this, sPropertyName));
+			}
+
+			return Item.prototype.setProperty.apply(this, arguments);
+		};
+
 		MessagePopoverItem.prototype.setDescription = function(sDescription) {
 			// Avoid showing result of '' + undefined
 			if (typeof sDescription === 'undefined') {
