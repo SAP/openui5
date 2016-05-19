78d0269b3ba645cc6c0181f3fa4fe015c4482eb2
Ilhan <ilhan.myumyun@sap.com>
2016-05-19 09:41:10 +0300

    [INTERNAL] m.ObjectPageHeader: properties description updated
    Change-Id: I357838dbc79790ea8f32309b7b1db143f20a950f
    BCP: 1670303770
    CR-Id: 012003146900000538772016
    (cherry picked from commit 576b091c940a228a17f19837e86167fea2005a39)

--- src/sap.uxap/src/sap/uxap/ObjectPageHeader.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageHeader.js
@@ -80,22 +80,22 @@ sap.ui.define([
 				},
 
 				/**
-				 * Determines whether the icon should always be visible or if it should be visible only when scrolling.
+				 * Determines whether the icon should always be visible or visible only when the header is snapped.
 				 */
 				isObjectIconAlwaysVisible: {type: "boolean", defaultValue: false},
 
 				/**
-				 * Determines whether the title should always be visible or if it should be visible only when scrolling.
+				 * Determines whether the title should always be visible or visible only when the header is snapped.
 				 */
 				isObjectTitleAlwaysVisible: {type: "boolean", defaultValue: true},
 
 				/**
-				 * Determines whether the subtitle should always be visible or if it should be visible only when scrolling.
+				 * Determines whether the subtitle should always be visible or visible only when the header is snapped.
 				 */
 				isObjectSubtitleAlwaysVisible: {type: "boolean", defaultValue: true},
 
 				/**
-				 * Determines whether the action buttons should always be visible or if they should be visible only when scrolling.
+				 * Determines whether the action buttons should always be visible or visible only when the header is snapped.
 				 */
 				isActionAreaAlwaysVisible: {type: "boolean", defaultValue: true},
 
