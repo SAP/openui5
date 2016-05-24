9b411e5709d4e0c7793c8bc5064ac1c5385d6942
Malte Wedel <malte.wedel@sap.com>
2016-05-24 13:59:25 +0200

    [FIX] v2.ODataModel: createKey must URI encode property values
    When creating the entity key from the key properties, all property
    values must be URI encoded to avoid creation of invalid URLs.
    
    BCP: 1670339733
    Change-Id: I2593b95edec541a321f2ae5f62557f64e0824ced
    CR-Id: 012003146900000554762016

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -1646,7 +1646,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 			sName = oEntityType.key.propertyRef[0].name;
 			jQuery.sap.assert(sName in oKeyProperties, "Key property \"" + sName + "\" is missing in object!");
 			oProperty = this.oMetadata._getPropertyMetadata(oEntityType, sName);
-			sKey += ODataUtils.formatValue(oKeyProperties[sName], oProperty.type);
+			sKey += encodeURIComponent(ODataUtils.formatValue(oKeyProperties[sName], oProperty.type));
 		} else {
 			jQuery.each(oEntityType.key.propertyRef, function(i, oPropertyRef) {
 				if (i > 0) {
@@ -1657,7 +1657,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/Model', 'sap/ui/model/odata/OD
 				oProperty = that.oMetadata._getPropertyMetadata(oEntityType, sName);
 				sKey += sName;
 				sKey += "=";
-				sKey += ODataUtils.formatValue(oKeyProperties[sName], oProperty.type);
+				sKey += encodeURIComponent(ODataUtils.formatValue(oKeyProperties[sName], oProperty.type));
 			});
 		}
 		sKey += ")";
