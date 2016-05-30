4d6a0e4893948281fd63526f0ffe85bf8b4e0991
Nikolay Vankov <nikolay.vankov@sap.com>
2016-05-30 11:38:02 +0300

    [Fix] sap.ui.commons.MessageBox: Setting title issue fixed.
    Change-Id: I87d4b7c8ca3e74d5935568b3f70816d1dc7235af
    BCP: 1580199951
    Fixes: https://github.com/SAP/openui5/issues/681
    CR-Id: 012006153200001157362016

--- src/sap.ui.commons/src/sap/ui/commons/MessageBox.js
+++ src/sap.ui.commons/src/sap/ui/commons/MessageBox.js
@@ -278,7 +278,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/core/ElementM
 			oDialog = new Dialog({
 				id: sDialogId,
 				applyContentPadding: false,
-				title: sTitle,
 				accessibleRole: AccessibleRole.AlertDialog,
 				resizable: false,
 				modal: true,
@@ -286,7 +285,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/core/ElementM
 				content: oContent,
 				defaultButton: oDefaultButton,
 				closed: onclose
-			});
+			}).setTitle(sTitle);
 
 			oDialog.open();
 
