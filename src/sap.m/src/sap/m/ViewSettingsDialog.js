244ac985506c8d7d7c8fca7c76c9e6e9c8d6f9c7
Christoph Gerold <c.gerold@gmail.com>
2016-06-02 11:54:23 +0200

    [FEATURE] sap.m.ViewSettingsDialog: search entire title
    Actually on search in ViewSettingsDialog it is checked if the title begins with the search query. Please extend this search behaviour to browse the entire title.

--- src/sap.m/src/sap/m/ViewSettingsDialog.js
+++ src/sap.m/src/sap/m/ViewSettingsDialog.js
@@ -2176,8 +2176,8 @@ function(jQuery, library, Control, IconPool, Toolbar, CheckBox, SearchField) {
 
 					//update the list items visibility
 					oFilterDetailList.getItems().forEach(function(oItem) {
-						var bStartsWithQuery = oItem.getTitle().toLowerCase().indexOf(sQuery) === 0;
-						oItem.setVisible(bStartsWithQuery);
+						var bContainsQuery = oItem.getTitle().toLowerCase().indexOf(sQuery) > -1;
+						oItem.setVisible(bContainsQuery);
 					});
 
 					//update Select All checkbox
