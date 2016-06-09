92100d0ea933d1091ba98e1ef827336dc148e713
Georgi Hristanov <georgi.hristanov@sap.com>
2016-06-09 13:14:39 +0300

    [INTERNAL] sap.m.DynamicPage messaging sample
    Change-Id: I455c292a9458eb0366e7433ae62f4d1c74b6a8bc

--- src/sap.m/test/sap/m/demokit/sample/DynamicPageAnalyticalTable/DynamicPageAnalyticalTable.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/DynamicPageAnalyticalTable/DynamicPageAnalyticalTable.controller.js
@@ -2,9 +2,12 @@ sap.ui.define([
 	"jquery.sap.global",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/Filter",
-	"sap/ui/model/json/JSONModel"
-], function (jQuery, Controller, Filter, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessagePopover",
+	"sap/m/MessagePopoverItem"
+], function (jQuery, Controller, Filter, JSONModel, MessagePopover, MessagePopoverItem) {
 	"use strict";
+
 
 	return Controller.extend("sap.m.sample.DynamicPageAnalyticalTable.DynamicPageAnalyticalTable", {
 		onInit: function () {
@@ -16,6 +19,13 @@ sap.ui.define([
 			this.oSelectName = this.getSelect("slName");
 			this.oSelectCategory = this.getSelect("slCategory");
 			this.oSelectSupplierName = this.getSelect("slSupplierName");
+
+			this._oMessageProcessor = new sap.ui.core.message.ControlMessageProcessor();
+			this._oMessageManager = sap.ui.getCore().getMessageManager();
+			this._oMessageManager.registerMessageProcessor(this._oMessageProcessor);
+
+			// Initializing popover
+			this.initializePopover();
 		},
 		onExit: function () {
 			this.aKeys = [];
@@ -25,7 +35,7 @@ sap.ui.define([
 		onToggleFooter: function () {
 			this.getPage().setShowFooter(!this.getPage().getShowFooter());
 		},
-		formatToggleButtonText: function (bValue){
+		formatToggleButtonText: function (bValue) {
 			return bValue ? "Collapse Header" : "Expand Header";
 		},
 		updateFilterCriteria: function (aFilterCriteria) {
@@ -76,6 +86,73 @@ sap.ui.define([
 		},
 		getSnappedLabel: function () {
 			return new sap.m.Label({text: "{/Filter/text}"});
+		},
+		// Function returning random message each time it is called
+		getRandomMessage: function () {
+			// Creating several options for messages, to make sure that different messages are added
+			var messageOptions = [{
+				message: "Error message",
+				description: "Error message description",
+				type: sap.ui.core.MessageType.Error,
+				processor: this._oMessageProcessor
+			}, {
+				message: "Information message",
+				description: "Information message description",
+				type: sap.ui.core.MessageType.Information,
+				processor: this._oMessageProcessor
+			}, {
+				message: "Success message",
+				description: "Success message description",
+				type: sap.ui.core.MessageType.Success,
+				processor: this._oMessageProcessor
+			}, {
+				message: "Warning message",
+				description: "Warning message description",
+				type: sap.ui.core.MessageType.Warning,
+				processor: this._oMessageProcessor
+			}];
+			return messageOptions[Math.floor(Math.random() * 4)]
+		},
+		initializePopover: function (oControl) {
+			this._messagePopover = new MessagePopover({
+				models: {message: this._oMessageManager.getMessageModel()},
+				items: {
+					path: "message>/",
+					template: new MessagePopoverItem({
+						type: "{message>type}",
+						title: "{message>message}",
+						description: "{message>description}"
+					})
+				}
+			});
+		},
+		onMessageButtonPress: function (oEvent) {
+			var oMessagesButton = oEvent.getSource();
+			// Either open the created popover if it isn't opened, or close it
+			this._messagePopover.toggle(oMessagesButton);
+		},
+		onAddMessage: function (oEvent) {
+			// Show footer if this is the first message
+			if (!this.oModel.getProperty("/messagesLength")) {
+				this.getPage().setShowFooter(true);
+			}
+			// Add a random message
+			this._oMessageManager.addMessages(
+				new sap.ui.core.message.Message(this.getRandomMessage())
+			);
+			// Update messages' length
+			this.oModel.setProperty("/messagesLength", this._oMessageManager.getMessageModel().getData().length);
+		},
+		onDeleteMessages: function () {
+			// Removing current messages
+			this._oMessageManager.removeAllMessages();
+			// Update messages' length
+			this.oModel.setProperty("/messagesLength", this._oMessageManager.getMessageModel().getData().length);
+			// Close both the popover and the footer, since there are no messages left
+			if (this._messagePopover) {
+				this._messagePopover.close();
+			}
+			this.getPage().setShowFooter(false);
 		}
 	});
 });
