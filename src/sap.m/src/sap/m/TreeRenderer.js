8d17ff75e5c79928dd8fb57a0e52581642bb4382
Wenqian.Wang <wenqian.wang@sap.com>
2016-06-09 16:11:55 +0200

    [FEATURE] sap.m.Tree: New control
    - Expand/Collapse
    - Selection
    - StandardTreeItem: Text + Icon
    - Keyboard handling
    - Screen reader support
    - Themes
    - Simple explored sample
    - Testsuite
    - Unit test
    
    Change-Id: I73b09fe2e18eaa5ddca006725bbc7eaa9c88ca58

---
+++ src/sap.m/src/sap/m/TreeRenderer.js
@@ -0,0 +1,28 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define(['sap/ui/core/Renderer', './ListBaseRenderer'],
+	function(Renderer, ListBaseRenderer) {
+	"use strict";
+
+	/**
+	 * Tree renderer.
+	 * @namespace
+	 *
+	 */
+	var TreeRenderer = Renderer.extend(ListBaseRenderer);
+
+	/**
+	 * Returns the ARIA accessibility role.
+	 *
+	 * @param {sap.ui.core.Control} oControl An object representation of the control
+	 * @returns {String}
+	 */
+	TreeRenderer.getAriaRole = function(oControl) {
+		return "tree";
+	};
+
+	return TreeRenderer;
+
+}, /* bExport= */ true);
