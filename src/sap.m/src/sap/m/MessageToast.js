caeda1cf57595a45c2adb2f3e9c9a1da5f4eb4fe
Elena Stoyanova <elena.stoyanova@sap.com>
2016-06-10 09:33:32 +0300

    [INTERNAL] sap.m.MessageToast: Belize adjustments
    Change-Id: I38af3b5474007919a1575eef6f1c19d30d2ebac7

--- src/sap.m/src/sap/m/MessageToast.js
+++ src/sap.m/src/sap/m/MessageToast.js
@@ -59,7 +59,9 @@ sap.ui.define(['jquery.sap.global', './InstanceManager', 'sap/ui/core/Popup'],
 
 		var OFFSET = "0 -64",
 			CSSCLASS = "sapMMessageToast",
-			ENABLESELECTIONCLASS = "sapUiSelectable";
+			ENABLESELECTIONCLASS = "sapUiSelectable",
+			BELIZECONTRAST = "sapContrast",
+			BELIZECONTRASTPLUS = "sapContrastPlus";
 
 		MessageToast._mSettings = {
 			duration: 3000,
@@ -194,7 +196,7 @@ sap.ui.define(['jquery.sap.global', './InstanceManager', 'sap/ui/core/Popup'],
 			var oMessageToastDomRef = document.createElement("div");
 
 			oMessageToastDomRef.style.width = mSettings.width;
-			oMessageToastDomRef.className = CSSCLASS + " " + ENABLESELECTIONCLASS;
+			oMessageToastDomRef.className = CSSCLASS + " " + ENABLESELECTIONCLASS + " " + BELIZECONTRAST + " " + BELIZECONTRASTPLUS;
 			oMessageToastDomRef.setAttribute("role", "alert");
 			oMessageToastDomRef.appendChild(document.createTextNode(mSettings.message));
 
