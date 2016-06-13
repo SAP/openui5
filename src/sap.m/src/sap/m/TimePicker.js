29d4034c839574d090f7574bc839456aa911a7e0
Alexander Ivanov <alexander.ivanov01@sap.com>
2016-06-13 17:21:55 +0300

    [FIX] sap.m.TimePicker: Change event fires correctly
    Change-Id: I1ce75b2d24da90ea2c8459ae4fee7f17c632d1bf
    BCP: 1670343229
    (cherry picked from commit 25c999ff773ce937641934ea1ed9a2b93168ba69)
    CR-Id: 012006153200001212692016

--- src/sap.m/src/sap/m/TimePicker.js
+++ src/sap.m/src/sap/m/TimePicker.js
@@ -410,6 +410,7 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 						this._synchronizeInput(sValue);
 					} else {
 						this.setProperty("value", sValue, true); // no rerendering
+						this._sLastChangeValue = sValue;
 					}
 				}
 			}
@@ -467,6 +468,7 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 
 			sValue = this.validateProperty('value', sValue);
 			MaskInput.prototype.setValue.call(this, sValue);
+			this._sLastChangeValue = sValue;
 			this._bValid = true;
 
 			// convert to date object
@@ -527,6 +529,7 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 
 			// set the property in any case but check validity on output
 			this.setProperty("value", sValue, true); // no rerendering
+			this._sLastChangeValue = sValue;
 
 			if (this.isActive()) {
 				// convert to output
@@ -1100,13 +1103,8 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 			}
 
 			if (sValue !== this._sLastChangeValue) {
-				//fire only when there is a change from a meaningful value or to a meaningful value
-				//not when the value changes from null to ""
-				if (sValue || this._sLastChangeValue) {
-					InputBase.prototype.fireChangeEvent.call(this, sValue, oParams);
-				}
-
 				this._sLastChangeValue = sValue;
+				InputBase.prototype.fireChangeEvent.call(this, sValue, oParams);
 			}
 		};
 
