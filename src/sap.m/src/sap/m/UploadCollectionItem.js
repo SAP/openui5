b5640bb11e75d7b3227aae4dc834e7714c8e9953
Petr Stribrny <petr.stribrny@sap.com>
2016-06-13 17:59:28 +0200

    [FIX] sap.m.UploadCollectionItem: Documentation to url property added
    Change-Id: Ic880d3cb1bc882e6592c45047047b943bb824b08
    CR-Id: 012006153200001213262016

--- src/sap.m/src/sap/m/UploadCollectionItem.js
+++ src/sap.m/src/sap/m/UploadCollectionItem.js
@@ -103,6 +103,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Element'],
 
 				/**
 				 * Specifies the URL where the file is located.
+				 * If the application doesn't provide a value for this property, the icon and the file name of the UploadCollectionItem are not clickable.
 				 */
 				url : {
 					type : "string",
