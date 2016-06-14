520f4077f4330d7685ac51b3ba9bb0d2079db154
Sebastian Ried <sebastian.ried@sap.com>
2016-06-14 16:25:42 +0200

    [INTERNAL] sap.ui.table.Table: refactor ResizeHandler code
    Change-Id: I4b65ce0ef7a9d4d833ea4c5b041d60942c7f1c6f

--- src/sap.ui.table/src/sap/ui/table/TableUtils.js
+++ src/sap.ui.table/src/sap/ui/table/TableUtils.js
@@ -3,8 +3,8 @@
  */
 
 // Provides helper sap.ui.table.TableUtils.
-sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
-	function(jQuery, Control, library) {
+sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/ResizeHandler', './library'],
+	function(jQuery, Control, ResizeHandler, library) {
 	"use strict";
 
 	// shortcuts
@@ -33,25 +33,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			COLUMNROWHEADER : "COLUMNROWHEADER" // select all row selector (top left cell)
 		},
 
-		/*
+		/**
 		 * Returns whether the table has a row header or not
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {boolean}
+		 * @private
 		 */
 		hasRowHeader : function(oTable) {
 			return oTable.getSelectionMode() !== SelectionMode.None
 					&& oTable.getSelectionBehavior() !== SelectionBehavior.RowOnly;
 		},
 
-		/*
+		/**
 		 * Returns whether the no data text is currently shown or not
 		 * If true, also CSS class sapUiTableEmpty is set on the table root element.
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {boolean}
+		 * @private
 		 */
 		isNoDataVisible : function(oTable) {
 			return oTable.getShowNoData() && !oTable._getRowCount()/*!oTable._hasData()*/;
 		},
 
-		/*
+		/**
 		 * Returns the text to be displayed as no data message.
 		 * If a custom noData control is set null is returned.
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {String|string|null}
+		 * @private
 		 */
 		getNoDataText : function(oTable) {
 			var oNoData = oTable.getNoData();
@@ -66,17 +75,21 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			}
 		},
 
-		/*
+		/**
 		 * Returns the number of currently visible columns
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {int}
+		 * @private
 		 */
 		getVisibleColumnCount : function(oTable) {
 			return oTable._getVisibleColumnCount();
 		},
 
-		/*
+		/**
 		 * Returns the height of the defined row, identified by its row index.
 		 * @param {Object} oTable current table object
 		 * @param {int} iRowIndex the index of the row which height is needed
+		 * @private
 		 */
 		getRowHeightByIndex : function(oTable, iRowIndex) {
 			var iRowHeight = 0;
@@ -98,19 +111,24 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			return iRowHeight;
 		},
 
-		/*
+		/**
 		 * Checks whether all conditions for pixel-based scrolling (Variable Row Height) are fulfilled.
 		 * @param {Object} oTable current table object
 		 * @returns {Boolean} true/false if fulfilled
+		 * @private
 		 */
 		isVariableRowHeightEnabled : function(oTable) {
 			return oTable._bVariableRowHeightEnabled && oTable.getNavigationMode() === NavigationMode.Scrollbar;
 		},
 
-		/*
+		/**
 		 * Returns the logical number of rows
 		 * Optionally empty visible rows are added (in case that the number of data
 		 * rows is smaller than the number of visible rows)
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {boolean} bIncludeEmptyRows
+		 * @return {int}
+		 * @private
 		 */
 		getTotalRowCount : function(oTable, bIncludeEmptyRows) {
 			var iRowCount = oTable._getRowCount();
@@ -120,8 +138,18 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			return iRowCount;
 		},
 
-		/*
+		/**
 		 * Returns a combined info about the currently focused item (based on the item navigation)
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {Object|null}
+		 * @type {Object}
+		 * @property {int} cell Index of focused cell in ItemNavigation
+		 * @property {int} columnCount Number of columns in ItemNavigation
+		 * @property {int} cellInRow Index of the cell in row
+		 * @property {int} row Index of row in ItemNavigation
+		 * @property {int} cellCount Number of cells in ItemNavigation
+		 * @property {Object|undefined} domRef Focused DOM reference of undefined
+		 * @private
 		 */
 		getFocusedItemInfo : function(oTable) {
 			var oIN = oTable._getItemNavigation();
@@ -138,24 +166,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			};
 		},
 
-		/*
+		/**
 		 * Returns the index of the column (in the array of visible columns (see Table._getVisibleColumns())) of the current focused cell
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {int}
+		 * @private
 		 */
 		getColumnIndexOfFocusedCell : function(oTable) {
 			var oInfo = TableUtils.getFocusedItemInfo(oTable);
 			return oInfo.cellInRow - (TableUtils.hasRowHeader(oTable) ? 1 : 0);
 		},
 
-		/*
+		/**
 		 * Returns the index of the row (in the rows aggregation) of the current focused cell
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {int}
+		 * @private
+		 *
 		 */
 		getRowIndexOfFocusedCell : function(oTable) {
 			var oInfo = TableUtils.getFocusedItemInfo(oTable);
 			return oInfo.row - oTable._getHeaderRowCount();
 		},
 
-		/*
+		/**
 		 * Returns whether the given cell is located in a group header.
+		 * @param {Object} oCellRef DOM reference of table cell
+		 * @return {boolean}
+		 * @private
 		 */
 		isInGroupingRow : function(oCellRef) {
 			var oInfo = TableUtils.getCellInfo(oCellRef);
@@ -167,8 +205,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			return false;
 		},
 
-		/*
+		/**
 		 * Returns whether the given cell is located in a analytical summary row.
+		 * @param {Object} oCellRef DOM reference of table cell
+		 * @return {boolean}
+		 * @private
 		 */
 		isInSumRow : function(oCellRef) {
 			var oInfo = TableUtils.getCellInfo(oCellRef);
@@ -180,23 +221,34 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			return false;
 		},
 
-		/*
+		/**
 		 * Returns whether column with the given index (in the array of visible columns (see Table._getVisibleColumns()))
 		 * is a fixed column.
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {int} iColIdx Index of column in the tables column aggregation
+		 * @return {boolean}
+		 * @private
 		 */
 		isFixedColumn : function(oTable, iColIdx) {
 			return iColIdx < oTable.getFixedColumnCount();
 		},
 
-		/*
+		/**
 		 * Returns whether the table has fixed columns.
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @return {boolean}
+		 * @private
 		 */
 		hasFixedColumns : function(oTable) {
 			return oTable.getFixedColumnCount() > 0;
 		},
 
-		/*
+		/**
 		 * Focus the item with the given index in the item navigation
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {int} iIndex Index of item in ItemNavigation which shall get the focus
+		 * @param {Object} oEvent
+		 * @private
 		 */
 		focusItem : function(oTable, iIndex, oEvent) {
 			var oIN = oTable._getItemNavigation();
@@ -205,11 +257,17 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			}
 		},
 
-		/*
+		/**
 		 * Returns the cell type and the jQuery wrapper object of the given cell dom ref or
 		 * null if the given dom element is not a table cell.
 		 * {type: <TYPE>, cell: <$CELL>}
+		 * @param {Object} oCellRef DOM reference of table cell
+		 * @return {Object}
+		 * @type {Object}
+		 * @property {sap.ui.table.CELLTYPES} type
+		 * @property {Object} cell jQuery object of the cell
 		 * @see TableUtils.CELLTYPES
+		 * @private
 		 */
 		getCellInfo : function(oCellRef) {
 			if (!oCellRef) {
@@ -228,9 +286,18 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			return null;
 		},
 
-		/*
+		/**
 		 * Returns the Row, Column and Cell instances for the given row index (in the rows aggregation)
 		 * and column index (in the array of visible columns (see Table._getVisibleColumns()).
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {int} iRowIdx Index of row in the tables rows aggregation
+		 * @param {int} iColIdx Index of column in the tables columns aggregation
+		 * @return {Object}
+		 * @type {Object}
+		 * @property {sap.ui.table.Row} row Row of the table
+		 * @property {sap.ui.table.Column} column Column of the table
+		 * @property {sap.ui.core.Control} cell Cell control of row/column
+		 * @private
 		 */
 		getRowColCell : function(oTable, iRowIdx, iColIdx) {
 			var oRow = oTable.getRows()[iRowIdx];
@@ -249,8 +316,88 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', './library'],
 			}
 
 			return {row: oRow, column: oColumn, cell: oCell};
-		}
+		},
 
+		/**
+		 * Registers a ResizeHandler for a DOM reference identified by its ID suffix. The ResizeHandler ID is tracked
+		 * in _mResizeHandlerIds of the table instance. The sIdSuffix is used as key.
+		 * Existing ResizeHandlers will be de-registered before the new one is registered.
+		 *
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {string} sIdSuffix ID suffix to identify the DOM element for which to register the ResizeHandler
+		 * @param {Function} fnHandler Function to handle the resize event
+		 * @param {boolean}[bRegisterParent] Flag to register the ResizeHandler for the parent DOM element of the one identified by sIdSuffix
+		 *
+		 * @return {int|undefined} ResizeHandler ID or undefined if the DOM element could not be found
+		 * @private
+		 */
+		registerResizeHandler : function(oTable, sIdSuffix, fnHandler, bRegisterParent) {
+			var oDomRef;
+			if (typeof sIdSuffix == "string") {
+				oDomRef = oTable.getDomRef(sIdSuffix);
+			} else {
+				jQuery.sap.log.error("sIdSuffix must be a string", oTable);
+				return;
+			}
+
+			if (typeof fnHandler !== "function") {
+				jQuery.sap.log.error("fnHandler must be a function", oTable);
+				return;
+			}
+
+			// make sure that each DOM element of the table can only have one resize handler in order to avoid memory leaks
+			this.deregisterResizeHandler(oTable, sIdSuffix);
+
+			if (!oTable._mResizeHandlerIds) {
+				oTable._mResizeHandlerIds = {};
+			}
+
+			if (bRegisterParent && oDomRef) {
+				oDomRef = oDomRef.parentNode;
+			}
+
+			if (oDomRef) {
+				oTable._mResizeHandlerIds[sIdSuffix] = ResizeHandler.register(oDomRef, fnHandler);
+			}
+
+			return oTable._mResizeHandlerIds[sIdSuffix];
+		},
+
+		/**
+		 * De-register ResizeHandler identified by sIdSuffix. If sIdSuffix is undefined, all know ResizeHandlers will be de-registered
+		 * @param {sap.ui.table.Table} oTable Instance of the table
+		 * @param {string|Array.<string>} [vIdSuffix] ID suffix to identify the ResizeHandler to de-register. If undefined, all will be de-registered
+		 * @private
+		 */
+		deregisterResizeHandler : function(oTable, vIdSuffix) {
+			var aIdSuffix;
+			if (!oTable._mResizeHandlerIds) {
+				// no resize handler registered so far
+				return;
+			}
+
+			if (typeof vIdSuffix == "string") {
+				aIdSuffix = [vIdSuffix];
+			} else if (vIdSuffix === undefined) {
+				aIdSuffix = [];
+				// de-register all resize handlers if no specific is named
+				for (var sKey in oTable._mResizeHandlerIds) {
+					if (typeof sKey == "string" && oTable._mResizeHandlerIds.hasOwnProperty(sKey)) {
+						aIdSuffix.push(sKey);
+					}
+				}
+			} else if (jQuery.isArray(vIdSuffix)) {
+				aIdSuffix = vIdSuffix;
+			}
+
+			for (var i = 0; i < aIdSuffix.length; i++) {
+				var sIdSuffix = aIdSuffix[i];
+				if (oTable._mResizeHandlerIds[sIdSuffix]) {
+					ResizeHandler.deregister(oTable._mResizeHandlerIds[sIdSuffix]);
+					oTable._mResizeHandlerIds[sIdSuffix] = undefined;
+				}
+			}
+		}
 	};
 
 	return TableUtils;
