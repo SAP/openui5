b63d95a1a97ee23f0ff2d1e0369d8f68e7d30ba2
Jiawei Cao <jiawei.cao@sap.com>
2016-06-27 10:39:41 +0200

    [FIX] sap.ui.core.Icon: Icon doesn't correctly mark the event on mobile device
    - Icon should listen to different event sets depending on whether the
      touch event is supported by the running browser. Because this control
      could be used either in sap.m library or other libraries which are mouse
      event based. When parent and child controls listen to different sets of
      events, for example parent control uses mouse event and child control
      uses touch event, the event bubbling can result in wrong order and the
      event marking doesn't work as expected. This leads to the error that
      both the event handler on parent and child control are called.
    
    - This is only a temp solution for the event bubbling issue when different
      sets of events are used through the control chain. To fix the root cause
      of the issue needs more time and discussion and therefore a temp
      solution has to be done first in order to give the application a fast
      fix. After the root cause is solved, this temp solution will be removed.
    
    Change-Id: I31c47c2997b71670029453ed7e1cf8ddb88b90f6
    BCP: 1680025561
    BCP: 1680047396
    CR-Id: 012006153200001262212016

--- src/sap.ui.core/src/sap/ui/core/Icon.js
+++ src/sap.ui.core/src/sap/ui/core/Icon.js
@@ -135,7 +135,7 @@ sap.ui.define(['jquery.sap.global', '../Device', './Control', './IconPool', './l
 	 * @param {jQuery.Event} oEvent The event object.
 	 * @private
 	 */
-	Icon.prototype.onmousedown = function(oEvent) {
+	Icon.prototype[Device.support.touch ? "ontouchstart" : "onmousedown"] = function(oEvent) {
 		if (this.hasListeners("press") || this.hasListeners("tap")) {
 
 			// mark the event for components that needs to know if the event was handled
@@ -171,7 +171,7 @@ sap.ui.define(['jquery.sap.global', '../Device', './Control', './IconPool', './l
 	 * @param {jQuery.Event} oEvent The event object.
 	 * @private
 	 */
-	Icon.prototype.onmouseup = function(oEvent) {
+	Icon.prototype[Device.support.touch ? "ontouchend" : "onmouseup"] = function(oEvent) {
 
 		// change the source back only when all fingers leave the icon
 		if (!oEvent.targetTouches || (oEvent.targetTouches && oEvent.targetTouches.length === 0)) {
@@ -214,11 +214,7 @@ sap.ui.define(['jquery.sap.global', '../Device', './Control', './IconPool', './l
 	 *
 	 * @private
 	 */
-	Icon.prototype.onclick = function() {
-		if (this._bPressFired) {
-			return;
-		}
-
+	Icon.prototype[Device.support.touch ? "ontap" : "onclick"] = function() {
 		this.firePress({/* no parameters */});
 	};
 
