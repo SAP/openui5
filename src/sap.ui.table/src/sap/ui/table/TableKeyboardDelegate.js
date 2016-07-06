1fe4abc0d328afcf3366572a15a6feda60481fa5
Jens Pflueger <jens.pflueger@sap.com>
2016-07-06 17:37:51 +0200

    [INTERNAL] sap.ui.table.*: Cleanup Group Collapse / Expand
    Change-Id: I26ee1db500b0a95eb2ea935bf9f89803ad7d21f3

--- src/sap.ui.table/src/sap/ui/table/TableKeyboardDelegate.js
+++ src/sap.ui.table/src/sap/ui/table/TableKeyboardDelegate.js
@@ -30,21 +30,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 			if (sType === TableExtension.TABLETYPES.ANALYTICAL) {
 
 				this.onsapselect = function(oEvent) {
-					if (jQuery(oEvent.target).hasClass("sapUiTableGroupIcon")) {
-						this._onNodeSelect(oEvent);
-					} else if (jQuery(oEvent.target).hasClass("sapUiAnalyticalTableSum")) {
+					if (jQuery(oEvent.target).hasClass("sapUiAnalyticalTableSum")) {
 						//Summs connot be selected
 						oEvent.preventDefault();
 						return;
 					} else {
-						var $Target = jQuery(oEvent.target),
-							$TargetDIV = $Target.closest('div.sapUiTableRowHdr');
-						if ($TargetDIV.hasClass('sapUiTableGroupHeader') && $TargetDIV.hasClass('sapUiTableRowHdr')) {
-							var iRowIndex = this.getFirstVisibleRow() + parseInt($TargetDIV.attr("data-sap-ui-rowindex"), 10);
-							var oBinding = this.getBinding("rows");
-							oBinding.toggleIndex(iRowIndex);
-							return;
-						}
 						if (TableKeyboardDelegate.prototype.onsapselect) {
 							TableKeyboardDelegate.prototype.onsapselect.apply(this, arguments);
 						}
@@ -52,16 +42,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 				};
 
 			} else if (sType === TableExtension.TABLETYPES.TREE) {
-
-				this.onsapselect = function(oEvent) {
-					if (jQuery(oEvent.target).hasClass("sapUiTableTreeIcon")) {
-						this._onNodeSelect(oEvent);
-					} else {
-						if (TableKeyboardDelegate.prototype.onsapselect) {
-							TableKeyboardDelegate.prototype.onsapselect.apply(this, arguments);
-						}
-					}
-				};
 
 				this.onkeydown = function(oEvent) {
 					TableKeyboardDelegate.prototype.onkeydown.apply(this, arguments);
@@ -301,20 +281,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 			jQuery.inArray(oEvent.srcControl,this.getColumns()) === -1) {
 			return;
 		}
-		var $Parent = jQuery(oEvent.target).closest('.sapUiTableGroupHeader');
-		if ($Parent.length > 0) {
-			var iRowIndex = this.getFirstVisibleRow() + parseInt($Parent.attr("data-sap-ui-rowindex"), 10);
-			var oBinding = this.getBinding("rows");
-			if (oBinding) {
-				if (oBinding.isExpanded(iRowIndex)) {
-					oBinding.collapse(iRowIndex);
-				} else {
-					oBinding.expand(iRowIndex);
-				}
-			}
+
+		if (TableUtils.toggleGroupHeader(this, oEvent.target)) {
 			oEvent.preventDefault();
 			return;
 		}
+
 		this._bShowMenu = true;
 		this._onSelect(oEvent);
 		this._bShowMenu = false;
@@ -401,9 +373,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 			// SHIFT + 10 should open the context menu
 			this.oncontextmenu(oEvent);
 		} else if (oEvent.keyCode === jQuery.sap.KeyCodes.NUMPAD_PLUS) {
-			this._expandGroupHeader(oEvent);
+			TableUtils.toggleGroupHeader(this, oEvent.target, true);
 		} else if (oEvent.keyCode === jQuery.sap.KeyCodes.NUMPAD_MINUS) {
-			this._collapseGroupHeader(oEvent);
+			TableUtils.toggleGroupHeader(this, oEvent.target, false);
 		}
 	};
 
@@ -535,7 +507,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 			}
 		} else if (oEvent.altKey) {
 			// Toggle group header on ALT + DOWN.
-			this._toggleGroupHeader(oEvent);
+			if (TableUtils.toggleGroupHeader(this, oEvent.target)) {
+				oEvent.preventDefault();
+				oEvent.setMarked("sapUiTableSkipItemNavigation");
+			}
 		}
 	};
 
@@ -562,7 +537,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', './library', './Row', 
 			}
 		} else if (oEvent.altKey) {
 			// Toggle group header on ALT + UP.
-			this._toggleGroupHeader(oEvent);
+			if (TableUtils.toggleGroupHeader(this, oEvent.target)) {
+				oEvent.preventDefault();
+				oEvent.setMarked("sapUiTableSkipItemNavigation");
+			}
 		}
 	};
 
