3ea5d5fdca0d03371f4da7882906879bc3a66e4f
Frank Weigel <frank.weigel@sap.com>
2016-07-08 16:04:45 +0200

    [INTERNAL][FIX] Core: accept conflicting sync and async preload requests
    When a library should be preloaded synchronously while an asynchronous
    preload request (<script> tag) for the same library is already pending, 
    just accept and execute the sync requests. This leads to double loading
    for the same JS file, but prevents errors in launchers that load
    components or libraries twice, async and then sync.
    
    Change-Id: I57e0842cfb75008fb8bc3d90d8b3497dddcb6350
    CR-Id: 012006153200001305912016
    BCP: 1680124328

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/scenario5/lib1/library-preload.js
@@ -0,0 +1,19 @@
+sap.ui.predefine('testlibs/scenario5/lib1/library',['sap/ui/core/Core', 'sap/ui/core/library'], function(Core, coreLib) {
+	sap.ui.getCore().initLibrary({
+		name: 'testlibs.scenario5.lib1',
+		dependencies: [
+			'testlibs.scenario5.lib3',
+			'testlibs.scenario5.lib4',
+			'testlibs.scenario5.lib5'
+		],
+		noLibraryCSS: true
+	});
+	return testlibs.scenario5.lib1;
+});
+jQuery.sap.registerPreloadedModules({
+	"version":"2.0",
+	"name":"testlibs.scenario5.lib1",
+	"modules":{
+		"testlibs/scenario5/lib1/manifest.json":"{\n\t\"sap.ui5\": {\n\t\t\"dependencies\": {\n\t\t\t\"libs\": {\n\t\t\t\t\"testlibs.scenario5.lib3\": {\n\t\t\t\t\t\"minVersion\": \"1.0.0\"\n\t\t\t\t},\n\t\t\t\t\"testlibs.scenario5.lib4\": {\n\t\t\t\t\t\"minVersion\": \"1.0.0\"\n\t\t\t\t},\n\t\t\t\t\"testlibs.scenario5.lib5\": {\n\t\t\t\t\t\"minVersion\": \"1.0.0\"\n\t\t\t\t}\n\t\t\t}\n\t\t}\n\t}\n}"
+	}
+});
---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/scenario5/lib6/library.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/ui/core/Core', 'sap/ui/core/library'], function(Core, coreLib) {
+	sap.ui.getCore().initLibrary({
+		name: 'testlibs.scenario5.lib6',
+		dependencies: [
+		],
+		noLibraryCSS: true
+	});
+	return testlibs.scenario5.lib6;
+});
