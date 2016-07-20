7a38db94d174275d5d2765ef8b45a1c2eec8cf24
Thomas Marz <thomas.marz@sap.com>
2016-07-20 15:41:34 +0200

    [FIX] walkthrough tutorial :  add only one dependent in step 19
    Change-Id: Ifc46e36a657186ed161ae85f131f7a6296e29608
    CR-Id: 012006153200001342962016

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/31/webapp/controller/HelloDialog.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/31/webapp/controller/HelloDialog.js
@@ -5,27 +5,22 @@ sap.ui.define([
 
 	return Object.extend("sap.ui.demo.wt.controller.HelloDialog", {
 
-		_getDialog: function () {
+		_getDialog : function (oView) {
 			// create dialog lazily
 			if (!this._oDialog) {
 				// create dialog via fragment factory
 				this._oDialog = sap.ui.xmlfragment("sap.ui.demo.wt.view.HelloDialog", this);
+				// connect dialog to view (models, lifecycle)
+				oView.addDependent(this._oDialog);
 			}
 			return this._oDialog;
 		},
 
-		open: function (oView) {
-
-			var oDialog = this._getDialog();
-
-			// connect dialog to view (models, lifecycle)
-			oView.addDependent(oDialog);
-
-			// open dialog
-			oDialog.open();
+		open : function (oView) {
+			this._getDialog(oView).open();
 		},
 
-		onCloseDialog: function () {
+		onCloseDialog : function () {
 			this._getDialog().close();
 		}
 	});
