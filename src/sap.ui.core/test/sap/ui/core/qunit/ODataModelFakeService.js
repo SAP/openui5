e1c79095181139abb94c2c06bfceb31b5746756d
Jan Mummenthaler <jan.mummenthaler@sap.com>
2016-07-26 15:17:11 +0200

    [FIX] v2.ODataModel: eTag missing for function calls
    - even when an eTag is passed to callFunction it will be ignored when
    the request is sent. The eTag will only be applied to the headers for 
    'POST' functions as 'GET' functions must not change data and so doesn't
    need an eTag.
    
    Change-Id: If08c002bf982a028b1467cd45dda183b7837b0e9
    CR-Id: 012006153200001361742016
    BCP: 1680094642

--- src/sap.ui.core/test/sap/ui/core/qunit/ODataModelFakeService.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/ODataModelFakeService.js
@@ -1238,6 +1238,9 @@ var sMetaData = '\
           <End Role="Employees" EntitySet="Employees" />\
           <End Role="Territories" EntitySet="Territories" />\
         </AssociationSet>\
+		<FunctionImport Name="GetProductsByRating" EntitySet="Products" ReturnType="Collection(NorthwindModel.Product)" m:HttpMethod="PUT">\
+			<Parameter Name="rating" Type="Edm.Int32" Mode="In"/>\
+		</FunctionImport>\
       </EntityContainer>\
     </Schema>\
   </edmx:DataServices>\
