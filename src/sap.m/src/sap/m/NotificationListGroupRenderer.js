fa7a6f0459fa251df9dc7c8d17d491b8e3070b0d
i032160 <evgeni.katsarski@sap.com>
2016-08-04 14:11:09 +0300

    [FEATURE] NotificationListGroup: Property to show empty groups is added
    BCP: 1680192713
    Git Hub issue: https://github.com/SAP/openui5/issues/1086
    
    Change-Id: I6def9f30a94091733059571107540227b3e1c09e

--- src/sap.m/src/sap/m/NotificationListGroupRenderer.js
+++ src/sap.m/src/sap/m/NotificationListGroupRenderer.js
@@ -36,7 +36,7 @@ sap.ui.define([], function () {
 	 */
 	NotificationListGroupRenderer.render = function (oRm, oControl) {
 		if (oControl.getVisible()) {
-			var _bShowGroupHdrFooter = (oControl._getVisibleItemsCount() > 0);
+			var _bShowGroupHdrFooter = oControl.getShowEmptyGroup() || (oControl._getVisibleItemsCount() > 0);
 			oRm.write('<li');
 			oRm.addClass(classNameItem);
 			oRm.addClass(classNameBase);
