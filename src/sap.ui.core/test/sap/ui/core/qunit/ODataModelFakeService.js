580bb7dd9fc96963ff31ee3fe983dd3d0b9ea04d
Jan Mummenthaler <jan.mummenthaler@sap.com>
2016-08-26 11:11:20 +0200

    [FIX] v2.ODataModel: error when customParam of a Binding is undefined
    When Bindings use custom parameters and a value of a parameter is
    undefined createCustomParams breaks. To make this more robust, we ommit
    the value if it is undefined and only use the param name:
    
    {custom: {"test" :undefined}} => ?test
    
    Change-Id: Id203f05c1b95fb0fc090815e1c43218e86ead9d1
    CR-Id: 012003146900000594912016
    BCP: 1670352369

--- src/sap.ui.core/test/sap/ui/core/qunit/ODataModelFakeService.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/ODataModelFakeService.js
@@ -190,6 +190,10 @@ xhr.onCreate = function(request) {
 				[200, oCountHeaders, "5"],
 			"Categories(1)":
 				[200, oJSONHeaders, sCategory1JSON],
+			"Categories(1)?test":
+				[200, oJSONHeaders, sCategory1JSON],
+			"Categories(1)?hubel=dubel&test":
+				[200, oJSONHeaders, sCategory1JSON],
 			"ZeroTest(1)":
 				[200, oJSONHeaders, sZeroTest],
 			"Categories(3)":
@@ -270,9 +274,9 @@ xhr.onCreate = function(request) {
 				[200, oXMLHeaders, sFaultTolerance2],
 			"Employees(2)/Employee1":
 				[204, oNodataHeaders, ""],
-			"SpecialHeaders": 
+			"SpecialHeaders":
 				[204, oSpecialHeaders, ""],
-			"SpecialHeadersError": 
+			"SpecialHeadersError":
 				[500, oSpecialHeaders, ""]
 		},
 		"POST":{
