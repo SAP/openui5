0fd6c405fca2e4e67915293a9e6ee529c5fd89d1
Martin R. Hristov <martin.r.hristov@sap.com>
2016-09-02 14:55:54 +0300

    [FIX] sap.m.ComboBox: On phone selecting an item is called with correct key
    Change-Id: I2b509ef1e55cbd5a811f4ac710c87e0f0f5ef164
    BCP: 1670351686

--- src/sap.m/src/sap/m/ComboBox.js
+++ src/sap.m/src/sap/m/ComboBox.js
@@ -554,6 +554,11 @@ sap.ui.define(['jquery.sap.global', './ComboBoxTextField', './ComboBoxBase', './
 			setTimeout(this.selectText.bind(this, this.getValue().length, this.getValue().length), 0);
 
 			if (this.getPickerType() === "Dialog") {
+				this.setSelection(oItem);
+				this.fireSelectionChange({
+					selectedItem: this.getSelectedItem()
+				});
+
 				this.onChange();
 			}
 		};
