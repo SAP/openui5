5cab7e512608a89288b62bc71686a56d96f8be3c
Dominic Holzwarth <dominic.holzwarth@sap.com>
2016-09-06 16:23:09 +0200

    [INTERNAL] sap.m.GenericTile: Testsuite for LineMode added
    Change-Id: I3a043c5e54a01123b3c5adec3df920199fdb00d5

--- src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
+++ src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
@@ -18,7 +18,30 @@ sap.ui.define([], function() {
 	 * @param {sap.m.GenericTile} oControl the control to be rendered
 	 */
 	GenericTileLineModeRenderer.render = function(oRm, oControl) {
+		oRm.write("<div");
+		oRm.writeControlData(oControl);
+		oRm.addClass("sapMGT");
+		oRm.addClass("sapMGTLineMode");
+		oRm.writeClasses();
+		oRm.write(">");
 
+		oRm.write("<div");
+		oRm.addClass("sapMGTHdrTxt");
+		oRm.writeClasses();
+		oRm.writeAttribute("id", oControl.getId() + "-hdr-text");
+		oRm.write(">");
+		oRm.writeEscaped(oControl._oTitle.getText());
+		oRm.write("</div>");
+
+		oRm.write("<div");
+		oRm.addClass("sapMGTSubHdrTxt");
+		oRm.writeClasses();
+		oRm.writeAttribute("id", oControl.getId() + "-subHdr-text");
+		oRm.write(">");
+		oRm.writeEscaped(oControl.getSubheader());
+		oRm.write("</div>");
+
+		oRm.write("</div>");
 	};
 
 	return GenericTileLineModeRenderer;
