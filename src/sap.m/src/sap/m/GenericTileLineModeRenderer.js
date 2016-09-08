e7215f4922a97404e04bd3d8a6ffd443e1b0ed37
Dominic Holzwarth <dominic.holzwarth@sap.com>
2016-09-08 14:00:36 +0200

    [INTERNAL] sap.m.GenericTile: Basic LineMode inline rendering
    Change-Id: I1127b47a500e5a05054162af57b1734085e1d0ff

--- src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
+++ src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
@@ -19,9 +19,7 @@ sap.ui.define([ "sap/m/GenericTileRenderer", "sap/m/LoadState" ],
 	 * @param {sap.m.GenericTile} oControl the control to be rendered
 	 */
 	GenericTileLineModeRenderer.render = function(oRm, oControl) {
-		var sTooltipText = oControl._getTooltipText();
-
-		oRm.write("<div");
+		oRm.write("<span");
 		oRm.writeControlData(oControl);
 		oRm.addClass("sapMGT");
 		oRm.addClass("sapMGTLineMode");
@@ -36,30 +34,23 @@ sap.ui.define([ "sap/m/GenericTileRenderer", "sap/m/LoadState" ],
 		oRm.writeClasses();
 		oRm.write(">");
 
-		if (oControl.getState() !== LoadState.Loaded) {
-			TileRenderer._renderStateOverlay(oRm, oControl, sTooltipText);
-		} else {
-			TileRenderer._renderHoverOverlay(oRm, oControl);
-		}
-		TileRenderer._renderFocusDiv(oRm, oControl);
-
-		oRm.write("<div");
+		oRm.write("<span");
 		oRm.addClass("sapMGTHdrTxt");
 		oRm.writeClasses();
 		oRm.writeAttribute("id", oControl.getId() + "-hdr-text");
 		oRm.write(">");
 		oRm.writeEscaped(oControl._oTitle.getText());
-		oRm.write("</div>"); //.sapMGTHdrTxt
+		oRm.write("</span>"); //.sapMGTHdrTxt
 
-		oRm.write("<div");
+		oRm.write("<span");
 		oRm.addClass("sapMGTSubHdrTxt");
 		oRm.writeClasses();
 		oRm.writeAttribute("id", oControl.getId() + "-subHdr-text");
 		oRm.write(">");
 		oRm.writeEscaped(oControl.getSubheader());
-		oRm.write("</div>"); //.sapMGTSubHdrTxt
+		oRm.write("</span>"); //.sapMGTSubHdrTxt
 
-		oRm.write("</div>"); //.sapMGT
+		oRm.write("</span>"); //.sapMGT
 	};
 
 	return GenericTileLineModeRenderer;
