76abcf891db4a0e2801902e29566fdbaba60629b
Nikolay Kolarov <nikolay.kolarov@sap.com>
2016-09-09 09:54:15 +0300

    [INTERNAL][FEATURE] sap.m.MessagePopover: The info state of the items is now set according the message type
    - The infoState of the StandardListItem is used to expose the message
      type for the users with assistive technologies
    
    Change-Id: I5ea23df344639525a2b1c2ed3198f0d98d122dc7
    CR-Id: 012003146900000664122016
    (cherry picked from commit 0e20bae5c8a37cf7d1768b97b84ae246b93dc6a4)

--- src/sap.m/src/sap/m/MessagePopover.js
+++ src/sap.m/src/sap/m/MessagePopover.js
@@ -603,6 +603,8 @@ sap.ui.define(["jquery.sap.global", "./ResponsivePopover", "./Button", "./Toolba
 					description: oMessagePopoverItem.getSubtitle(),
 					counter: oMessagePopoverItem.getCounter(),
 					icon: this._mapIcon(sType),
+					infoState: this._mapInfoState(sType),
+					info: "\r", // There should be a content in the info property in order to use the info states
 					type:  listItemType
 				}).addStyleClass(CSS_CLASS + "Item").addStyleClass(CSS_CLASS + "Item" + sType);
 
@@ -620,6 +622,36 @@ sap.ui.define(["jquery.sap.global", "./ResponsivePopover", "./Button", "./Toolba
 			oListItem._oMessagePopoverItem = oMessagePopoverItem;
 
 			return oListItem;
+		};
+
+		/**
+		 * Map ValueState according the MessageType of the message.
+		 *
+		 * @param {sap.ui.core.MessageType} sType Type of Message
+		 * @returns {sap.ui.core.ValueState | null} The ValueState
+		 * @private
+		 */
+		MessagePopover.prototype._mapInfoState = function (sType) {
+			if (!sType) {
+				return null;
+			}
+			var MessageType = sap.ui.core.MessageType,
+				ValueState = sap.ui.core.ValueState;
+
+			switch (sType) {
+				case MessageType.Warning:
+					return ValueState.Warning;
+				case MessageType.Error:
+					return ValueState.Error;
+				case MessageType.Success:
+					return ValueState.Success;
+				case MessageType.Information:
+				case MessageType.None:
+					return ValueState.None;
+				default:
+					jQuery.sap.log.warning("The provided MessageType is not mapped to a specific ValueState", sType);
+					return null;
+			}
 		};
 
 		/**
