be41b2bc15427a10c2a2fca989ed36261748dfd1
Elena Stoyanova <elena.stoyanova@sap.com>
2016-09-09 16:20:40 +0300

    [INTERNAL] sap.m.Popover, sap.m.SelectDialog: Visual tests added
    Change-Id: Ifc148b7ac66a95d1bf952eeb9051ed9f7668fbf0

---
+++ src/sap.m/test/sap/m/visual/Popover.spec.js
@@ -0,0 +1,57 @@
+describe("sap.m.Popover", function () {
+
+	it("Should load test page", function () {
+		expect(takeScreenshot()).toLookAs("initial");
+	});
+
+	it("Should open Popover Right", function () {
+		element(by.id("btn1")).click();
+		expect(takeScreenshot()).toLookAs("popover-right");
+	});
+
+	it("Should open Popover Bottom", function () {
+		element(by.id("btn0")).click();
+		expect(takeScreenshot()).toLookAs("popover-bottom");
+	});
+
+	it("Should open Popover Top", function () {
+		element(by.id("btn3")).click();
+		expect(takeScreenshot()).toLookAs("popover-top");
+	});
+
+	it("Should open Popover Horizontal", function () {
+		element(by.id("btn6")).click();
+		expect(takeScreenshot()).toLookAs("popover-horizontal");
+	});
+
+	it("Should open Popover Left", function () {
+		element(by.id("btn2")).click();
+		expect(takeScreenshot()).toLookAs("popover-left");
+	});
+
+	it("Should open Popover Vertical", function () {
+		element(by.id("btn4")).click();
+		expect(takeScreenshot()).toLookAs("popover-vertical");
+	});
+
+	it("Should open Popover with header and footer", function () {
+		element(by.id("with-h-with-f")).click();
+		expect(takeScreenshot(element(by.id("pop1")))).toLookAs("popover-header-footer");
+	});
+
+	it("Should open Popover without header and with footer", function () {
+		element(by.id("no-h-with-f")).click();
+		expect(takeScreenshot(element(by.id("pop1")))).toLookAs("popover-footer");
+	});
+
+	it("Should open Popover with header and no footer", function () {
+		element(by.id("with-h-no-f")).click();
+		expect(takeScreenshot(element(by.id("pop1")))).toLookAs("popover-header");
+	});
+
+	it("Should open Popover without header and footer", function () {
+		element(by.id("no-h-no-f")).click();
+		expect(takeScreenshot(element(by.id("pop1")))).toLookAs("popover-no-header-footer");
+	});
+
+});
---
+++ src/sap.m/test/sap/m/visual/SelectDialog.spec.js
@@ -0,0 +1,61 @@
+describe("sap.m.SelectDialog", function () {
+
+	it("Should load test page", function () {
+		expect(takeScreenshot()).toLookAs("initial");
+	});
+
+	it("Should open SelectDialog with Dialog Binding & List Binding & width set to 30rem", function () {
+		element(by.id("Button2")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog2-dialog")))).toLookAs("select-dialog-1");
+		browser.executeScript("sap.ui.getCore().byId('SelectDialog2-dialog').close();");
+	});
+
+	it("Should open SelectDialog with already initialized binding", function () {
+		element(by.id("Button3")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog3-dialog")))).toLookAs("select-dialog-2");
+		browser.executeScript("sap.ui.getCore().byId('SelectDialog3-dialog').close();");
+	});
+
+	it("Should open SelectDialog with web service and binding before opening the dialog", function () {
+		element(by.id("Button4")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog4-dialog")))).toLookAs("select-dialog-3");
+		browser.executeScript("sap.ui.getCore().byId('SelectDialog4-dialog').close();");
+	});
+
+	it("Should open SelectDialog with web service pre-filtered by \"id\" and binding before opening the dialog", function () {
+		element(by.id("Button4a")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog4a-dialog")))).toLookAs("select-dialog-4");
+		browser.executeScript("sap.ui.getCore().byId('SelectDialog4a-dialog').close();");
+	});
+
+	it("Open SelectDialog with late binding in MultiSelect prefiltered by \"ad\" mode with 1000px width", function () {
+		element(by.id("Button6")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog6-dialog")))).toLookAs("select-dialog-5");
+		element(by.id("SelectDialog6-ok")).click();
+	});
+
+	it("Should open SelectDialog in MultiSelect mode prefiltered by \"id_1\" with web service binding and 400px width", function () {
+		element(by.id("Button7")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog7-dialog")))).toLookAs("select-dialog-6");
+		element(by.id("SelectDialog7-ok")).click();
+	});
+
+	it("Open SelectDialog in MultiSelect mode with JSON binding and selection model and rememberSelections=false", function () {
+		element(by.id("Button8")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog8-dialog")))).toLookAs("select-dialog-7");
+		element(by.id("SelectDialog8-ok")).click();
+	});
+
+	it("Open SelectDialog in MultiSelect mode prefiltered by \"Hulk\" with JSON binding and selection model and rememberSelections=true", function () {
+		element(by.id("Button9")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog9-dialog")))).toLookAs("select-dialog-8");
+		element(by.id("SelectDialog9-ok")).click();
+	});
+
+	it("Open SelectDialog with the view as parent and delayed binding", function () {
+		element(by.id("Button10")).click();
+		expect(takeScreenshot(element(by.id("SelectDialog10-dialog")))).toLookAs("select-dialog-9");
+		element(by.id("SelectDialog10-ok")).click();
+	});
+
+});
