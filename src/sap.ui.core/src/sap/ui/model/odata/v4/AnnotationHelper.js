f74de77456fc00c1115ffe0a1b265cc1dd734245
Peter Buchholz <peter.buchholz@sap.com>
2016-09-21 17:13:08 +0200

    [INTERNAL] sap.ui.model.odata.v4.AnnotationHelper#getNavigationPath
    PS1: PP TCH/AMR; integration test; first steps
    PS2: rebase, comments from TCH, JIT
    PS3: jsdoc and clean up
    PS4: rebased
    PS5: line wrapping; removed a TODO
    PS6: getNavigationBinding
    PS7: comments by PKS
    
    Change-Id: I3a0b222513cdd085b80580dcb2908432db38b114
    JIRA: CPOUI5UISERVICESV3-240

--- src/sap.ui.core/src/sap/ui/model/odata/v4/AnnotationHelper.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/AnnotationHelper.js
@@ -4,7 +4,8 @@
 sap.ui.define([], function () {
 	"use strict";
 
-	var rCount = /\/\$count$/,
+	var rBadChars = /[\\\{\}:]/, // @see sap.ui.base.BindingParser: rObject, rBindingChars
+		rCount = /\/\$count$/,
 		/**
 		 * @classdesc
 		 * A collection of methods which help to consume
@@ -18,6 +19,79 @@ sap.ui.define([], function () {
 		 * @alias sap.ui.model.odata.v4.AnnotationHelper
 		 */
 		AnnotationHelper = {
+			/**
+			 * Returns a data binding according to the result of {@link #getNavigationPath}.
+			 *
+			 * @param {string} sPath
+			 *   The path value from the meta model, for example
+			 *   "ToSupplier/@com.sap.vocabularies.Communication.v1.Address" or
+			 *   "@com.sap.vocabularies.UI.v1.FieldGroup#Dimensions"
+			 * @returns {string}
+			 *   A data binding according to the result of {@link #getNavigationPath}, for example
+			 *   "{ToSupplier}" or ""
+			 * @throws {Error}
+			 *   If the result of {@link #getNavigationPath} contains segments which are not valid
+			 *   OData identifiers and violate the data binding syntax
+			 * @public
+			 */
+			getNavigationBinding : function (sPath) {
+				sPath = AnnotationHelper.getNavigationPath(sPath);
+				if (rBadChars.test(sPath)) {
+					throw new Error("Invalid OData identifier: " + sPath);
+				}
+				return sPath ? "{" + sPath + "}" : sPath;
+			},
+
+			/**
+			 * A function that helps to interpret OData V4 annotations. It knows about the syntax
+			 * of the path value used by the following dynamic expressions:
+			 * <ul>
+			 * <li>"14.5.2 Expression edm:AnnotationPath"</li>
+			 * <li>"14.5.11 Expression edm:NavigationPropertyPath"</li>
+			 * <li>"14.5.12 Expression edm:Path"</li>
+			 * <li>"14.5.13 Expression edm:PropertyPath"</li>
+			 * </ul>
+			 * It returns the path of structural and navigation properties from the given path
+			 * value, but removes "$count", types casts, term casts, and annotations of navigation
+			 * properties.
+			 *
+			 * @param {string} sPath
+			 *   The path value from the meta model, for example
+			 *   "ToSupplier/@com.sap.vocabularies.Communication.v1.Address" or
+			 *   "@com.sap.vocabularies.UI.v1.FieldGroup#Dimensions"
+			 * @returns {string}
+			 *   The path of structural and navigation properties, for example "ToSupplier" or ""
+			 * @public
+			 */
+			getNavigationPath : function (sPath) {
+				var iIndexOfAt;
+
+				if (!sPath || sPath[0] === "@") {
+					return "";
+				}
+
+				if (rCount.test(sPath)) {
+					return sPath.slice(0, -7);
+				}
+
+				iIndexOfAt = sPath.indexOf("@");
+				if (iIndexOfAt > -1) {
+					sPath = sPath.slice(0, iIndexOfAt);
+				}
+				if (sPath[sPath.length - 1] === "/") {
+					sPath = sPath.slice(0, -1);
+				}
+
+				if (sPath.indexOf(".")) {
+					sPath = sPath.split("/")
+						.filter(function (sSegment) { // remove type casts
+							return sSegment.indexOf(".") < 0;
+						}).join("/");
+				}
+
+				return sPath;
+			},
+
 			/**
 			 * A function that helps to interpret OData V4 annotations. It knows about the syntax
 			 * of the path value used by the following dynamic expressions:
@@ -37,7 +111,7 @@ sap.ui.define([], function () {
 			 * </pre>
 			 *
 			 * @param {string} sPath
-			 *   The path value from the meta model, e.g.
+			 *   The path value from the meta model, for example
 			 *   "ToSupplier/@com.sap.vocabularies.Communication.v1.Address" or
 			 *   "@com.sap.vocabularies.UI.v1.FieldGroup#Dimensions"
 			 * @param {object} oDetails
@@ -47,7 +121,7 @@ sap.ui.define([], function () {
 			 *   <code>oDetails.context.getProperty("") === sPath</code>
 			 * @param {string} oDetails.schemaChildName
 			 *   The qualified name of the schema child where the computed annotation has been
-			 *   found, e.g. "name.space.EntityType"
+			 *   found, for example "name.space.EntityType"
 			 * @returns {boolean}
 			 *   <code>true</code> if the given path ends with "$count" or with a multi-valued
 			 *   structural or navigation property, <code>false</code> otherwise
