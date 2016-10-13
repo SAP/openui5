c710f897e1582fcae68882a301cf85e57e0851f7
Nikolay Stoichkov <nikolay.stoichkov@sap.com>
2016-10-13 11:40:46 +0300

    [FEATURE] IconExplorer: search by tags enabled
    Icons can be found by name, tags, unicode
    Categories are updated
    
    Change-Id: I4132367a74f858a55a245c57dbd6929bc2226a28
    CR-Id: 012003146900000705222016

--- src/sap.ui.demokit/src/sap/ui/demokit/icex/Component.js
+++ src/sap.ui.demokit/src/sap/ui/demokit/icex/Component.js
@@ -26,10 +26,10 @@ sap.ui.define([
 			// call overridden init (calls createContent)
 			UIComponent.prototype.init.apply(this, arguments);
 
-			var sPath = jQuery.sap.getModulePath("sap.ui.demokit.icex");
+			this.sPath = jQuery.sap.getModulePath("sap.ui.demokit.icex");
 
 			// set img model
-			var imgModel = new JSONModel(sPath + "/model/img.json");
+			var imgModel = new JSONModel(this.sPath + "/model/img.json");
 			this.setModel(imgModel, "img");
 
 			// set favorite model
@@ -37,7 +37,7 @@ sap.ui.define([
 			this.setModel(favModel, "fav");
 
 			// set groups model	(with asyn processing)
-			jQuery.ajax(sPath +  "/model/groups.json", {
+			jQuery.ajax(this.sPath +  "/model/groups.json", {
 				dataType: "json",
 				success: jQuery.proxy(this.onGroupsLoaded, this)
 			});
@@ -75,15 +75,25 @@ sap.ui.define([
 					}
 				}
 
-				// add all icons from icon pool
+				// add all icons from icon pool and append tag info
 				// MUST BE DONE AFTER GROUP SORT AS THIS GROUP IS EXPECTED AT INDEX 0 IN MASTER VIEW
-				var iconNames = IconPool.getIconNames();
-				var icons = [];
+				var iconNames = IconPool.getIconNames(),
+				icons = [],
+				aIconsMetadata = jQuery.sap.syncGetJSON(this.sPath +  "/model/tags.json").data;
+
 				for (var i = 0 ; i < iconNames.length ; i++) {
-					icons[i] = {
-						name : iconNames[i]
+					var sCurrentIconName = iconNames[i],
+						oCurrentIconMetadata = aIconsMetadata[sCurrentIconName],
+						oCurrentIcon = {
+							name : sCurrentIconName,
+							tags : sCurrentIconName
 					};
+					if (oCurrentIconMetadata) {
+						oCurrentIcon.tags += "," + oCurrentIconMetadata.Tags.join();
+					}
+					icons[i] = oCurrentIcon;
 				}
+
 				icons.sort(Sorter.sortByName);
 				data.groups.splice(0, 0, {
 					name : "all",
