6d9dccd87f93d2b408d336ee8b87b0c345ef9985
Frank Weigel <frank.weigel@sap.com>
2016-10-16 23:10:19 +0200

    [INTERNAL] sap.ui.demokit: add new runner for prettify
    The original runner always tries to load CSS from a CDN, which should be
    avoided in the SDK scenario (which should be self contained)
    
    Furthermore support standalone usage of developer guide pages for
    testing purposes (parameter sap-ui-xx-standalone).
    
    Change-Id: I672d135ebb97a408213abf6cb5ffd4cef51f3bea

--- src/sap.ui.demokit/src/sap/ui/demokit/js/framewrapper.js
+++ src/sap.ui.demokit/src/sap/ui/demokit/js/framewrapper.js
@@ -1,5 +1,5 @@
 (function() {
-	if (window.parent === window) {
+	if (window.parent === window && !/(?:\?|&)sap-ui-xx-standalone(?:=(?:true|x|X))?(?:&|$)/.test(window.location.search)) {
 		var url = document.location.href.replace("/docs/", "/#docs/");
 		window.location.replace(url);
 	}
