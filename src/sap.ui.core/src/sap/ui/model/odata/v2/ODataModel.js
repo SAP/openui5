ff56529b9db97869021d4eb7b87431af0a57c13d
Jan Mummenthaler <jan.mummenthaler@sap.com>
2016-10-18 14:33:33 +0200

    [FIX] v2.ODataModel: bindElement status code 204
    When the status code of the request is 204 a Context with path
    '/undefined' is created. This context leads to resolving errors
    when it will be passed to relative bindings.
    
    Referencing parent to 'null' in this case. 
    
    Change-Id: I0c349c72fc3badb5a3d7fb55a5404b237cecdbbd
    BCP: 1670473768

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -1499,17 +1499,25 @@ sap.ui.define([
 		}
 
 		function handleSuccess(oData) {
-			var sKey = oData ? that._getKey(oData) : undefined;
-			if (sKey && oContext && bIsRelative) {
-				var sContextPath = oContext.getPath();
+			var sKey = oData ? that._getKey(oData) : null,
+				oRef = null,
+				sContextPath;
+
+			oNewContext = null;
+
+			if (sKey) {
+				oNewContext = that.getContext('/' + sKey);
+				oRef = {__ref: sKey};
+			}
+			if (oContext && bIsRelative) {
+				sContextPath = oContext.getPath();
 				// remove starting slash
 				sContextPath = sContextPath.substr(1);
 				// when model is refreshed, parent entity might not be available yet
 				if (that.oData[sContextPath]) {
-					that.oData[sContextPath][sPath] = {__ref: sKey};
+					that.oData[sContextPath][sPath] = oRef;
 				}
 			}
-			oNewContext = that.getContext('/' + sKey);
 			fnCallBack(oNewContext);
 		}
 
