5a7a3c53a9fc717f8e258305825a9219ec049553
Wenqian.Wang <wenqian.wang@sap.com>
2016-10-18 16:25:09 +0200

    [FEATURE] sap.m.Tree: support Initial expand state and collapse all nodes
    - new function: expandToLevel
    - new function: collapseAll
    - unit tests
    - explored samples
    
    Change-Id: I7ec924d5cf6c234f164ca610fdef028e8a2d7ed9

---
+++ src/sap.m/test/sap/m/demokit/sample/TreeCollapseAll/Page.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
+	function(Controller, JSONModel) {
+	"use strict";
+
+	var PageController = Controller.extend("sap.m.sample.TreeCollapseAll.Page", {
+		onInit : function (evt) {
+			// set explored app's demo model on this sample
+			var oModel = new JSONModel("test-resources/sap/m/demokit/sample/Tree/Tree.json");
+			this.getView().setModel(oModel);
+		},
+
+		handleSelectChange: function(oEvent) {
+			var iLevel = oEvent.getParameter("selectedItem").getKey();
+			this.getView().byId("Tree").expandToLevel(iLevel);
+		},
+
+		onCollapseAllPress : function(evt) {
+			var oTree = this.getView().byId("Tree");
+			oTree.collapseAll();
+		}
+	});
+
+	return PageController;
+
+});
