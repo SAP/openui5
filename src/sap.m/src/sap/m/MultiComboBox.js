88198f1f56271bc316815f16c5bec9cf1745be3a
Martin R. Hristov <martin.r.hristov@sap.com>
2016-10-31 11:06:39 +0200

    [FIX] sap.m.MultiComboBox: Returning the focus to the input after rendering the list removed
    Change-Id: I4d4f2e95dd7573cde73f2d3d9f33c01965502e3f
    BCP: 1670524483
    BCP: 1670505894
    BCP: 1670497658
    BCP: 1670444936

--- src/sap.m/src/sap/m/MultiComboBox.js
+++ src/sap.m/src/sap/m/MultiComboBox.js
@@ -1513,11 +1513,6 @@ sap.ui.define(['jquery.sap.global', './Bar', './InputBase', './ComboBoxBase', '.
 		if (this._iFocusedIndex != null && oList.getItems().length > this._iFocusedIndex) {
 			oList.getItems()[this._iFocusedIndex].focus();
 			this._iFocusedIndex = null;
-		} else {
-			var oFocusDomRef = this.getFocusDomRef();
-			if (oFocusDomRef) {
-				oFocusDomRef.focus();
-			}
 		}
 	};
 
