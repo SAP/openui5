f62f593c5dd4d6f9dd8d70a24c372015605d21e4
Marin Parushev <marin.parushev@sap.com>
2016-11-23 17:33:41 +0200

    [FIX] sap.m.MultiInput: Input value is visible when readonly
    Change-Id: I421932e8d2b8a95e88964b0e9b99e97bf1452626
    BCP: 1680259127

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -399,6 +399,7 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 			if (this.getDomRef()) {
 				setTimeout(function () {
 					that._setContainerSizes();
+					that._tokenizer.scrollToEnd();
 				}, 0);
 			}
 
@@ -406,12 +407,12 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 			this._isMultiLineMode = false;
 
 			this._showAllTokens();
-			this.setValue("");
+			this._setValueVisible();
 
 			if (this.getDomRef()) {
 				setTimeout(function () {
 					that._setContainerSizes();
-					that._scrollAndFocus();
+					that._tokenizer.scrollToEnd();
 				}, 0);
 			}
 		}
@@ -593,9 +594,8 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 			$this = this.$(),
 			$border = this.$().find(".sapMMultiInputBorder"),
 			availableWidth,// the space available for the tokenizer, the input/the indicator and the value help icon
-			shadowDiv, // the input's shadow div
 			$indicator,
-			inputMinWidthNeeded = this.getEditable() ? 4 * 16 : 0, // space for input should be at least 4rem for editable and 0 for non-editable
+			inputMinWidthNeeded = 3 * 16, // space for input should be at least 3rem
 			tokenizerWidth,
 			iIndicatorWidth, // the "N More" indicator width
 			iconWidth, // the value help icon width
@@ -619,10 +619,7 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 		availableWidth = $border.width();
 
 		// calculate minimal needed width for input field
-		shadowDiv = $this.children(".sapMMultiInputShadowDiv")[0];
 		$indicator = $border.find(".sapMMultiInputIndicator");
-
-		jQuery(shadowDiv).text(this.getValue());
 
 		iIndicatorWidth = $indicator.width();
 		tokenizerWidth = this._tokenizer.getScrollWidth();
@@ -634,7 +631,7 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 			inputMinWidthNeeded = iIndicatorWidth;
 		}
 
-		if (!this._bUseDialog && this._isMultiLineMode && !this._bShowIndicator && $border.length > 0) {
+		if (!this._bUseDialog && this.getEditable() && this._isMultiLineMode && !this._bShowIndicator && $border.length > 0) {
 			// PC/tablet AND in multiline mode AND indicator N more is hidden AND sapMMultiInputBorder elem exists
 			tokenizerWidth = availableWidth - iconWidth;
 
