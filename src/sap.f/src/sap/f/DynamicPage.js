04b74425a7893587289ffec0664fdcb5a99fd1cb
Ilhan <ilhan.myumyun@sap.com>
2016-11-25 16:54:12 +0200

    [INTERNAL][FIX] f.DynamicPage: toggleFooter when control not rendered corrected
    Change-Id: I858bb5ea3bd17c6508b310241b196de1ca490ac8
    BCP: 1680268490

--- src/sap.f/src/sap/f/DynamicPage.js
+++ src/sap.f/src/sap/f/DynamicPage.js
@@ -237,7 +237,9 @@ sap.ui.define([
 
 	DynamicPage.prototype.setShowFooter = function (bShowFooter) {
 		var vResult = this.setProperty("showFooter", bShowFooter, true);
+
 		this._toggleFooter(bShowFooter);
+
 		return vResult;
 	};
 
@@ -309,6 +311,10 @@ sap.ui.define([
 	 */
 	DynamicPage.prototype._toggleFooter = function (bShow) {
 		var oFooter = this.getFooter();
+
+		if (!exists(this.$())) {
+			return;
+		}
 
 		if (!exists(oFooter)) {
 			return;
