74738ef848ff83fe52b9d695188d91f6cec7b9f5
Annette Frei <a.frei@sap.com>
2016-11-29 17:01:15 +0100

    [INTERNAL][FIX] updated explored products data
    - deleted ProductsSet folder with duplicate mock data
    - updated linked sample ListGrowingUpwards to central instance of products.json
    - synced both product.json files
    
    Change-Id: Id5949f2ca3bd29437adc88562a2ea88586caf966

--- src/sap.m/test/sap/m/demokit/sample/ListGrowingUpwards/MockServer.js
+++ src/sap.m/test/sap/m/demokit/sample/ListGrowingUpwards/MockServer.js
@@ -1,44 +1,46 @@
 sap.ui.define(['jquery.sap.global','sap/ui/core/util/MockServer'],
 	function(jQuery, MockServer1) {
-	"use strict";
+		"use strict";
 
-	// load mock server
-	// NOTE TO DEVELOPERS: You do not need to reproduce this following section
-	// It is just so we can simulate a delay from the fictional back end, giving
-	// us some context to show delayed loading sequences.
+		// load mock server
+		// NOTE TO DEVELOPERS: You do not need to reproduce this following section
+		// It is just so we can simulate a delay from the fictional back end, giving
+		// us some context to show delayed loading sequences.
 
-	var MockServer = {
+		var MockServer = {
 
-		server : null,
-		serviceURL : "/ProductSet/",
-		dataPath : "test-resources/sap/ui/demokit/explored/mockserver/ProductSet/",
+			server : null,
 
-		start : function (oConfig) {
-			// configure respond to requests delay
-			MockServer1.config(jQuery.extend({
-				autoRespond : true,
-				autoRespondAfter : 10
-			}, oConfig));
+			rootUri : "/mockserver/",
 
-			// create mockserver
-			this.server = new MockServer1({
-				rootUri : this.serviceURL
-			});
+			dataPath : "test-resources/sap/ui/demokit/explored/mockserver/",
 
-			// simulate and start and return
-			this.server.simulate(this.dataPath + "metadata.xml", this.dataPath);
-			this.server.start();
-			return this.server;
-		},
+			start : function (oConfig) {
+				// configure respond to requests delay
+				MockServer1.config(jQuery.extend({
+					autoRespond : true,
+					autoRespondAfter : 2000
+				}, oConfig));
 
-		stop : function() {
-			this.server.stop();
-			this.server = null;
-		}
+				// create mockserver
+				this.server = new MockServer1({
+					rootUri : this.rootUri
+				});
 
-	};
+				// simulate and start and return
+				this.server.simulate(this.dataPath + "metadata.xml", this.dataPath);
+				this.server.start();
+				return this.server;
+			},
+
+			stop : function() {
+				this.server.stop();
+				this.server = null;
+			}
+
+		};
 
 
-	return MockServer;
+		return MockServer;
 
-}, /* bExport= */ true);
+	}, /* bExport= */ true);
