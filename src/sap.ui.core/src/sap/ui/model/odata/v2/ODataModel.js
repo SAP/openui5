96ab317e6ee8370d0c1ae1f7e8398f01e27040e4
Malte Wedel <malte.wedel@sap.com>
2016-12-01 11:29:28 +0100

    [FIX] v2.ODataModel: Don't try to reload created entities
    In case an entity has been created using createEntry, it does not have a
    valid ID known to the server, so we must never send a request to the
    server to load it.
    
    BCP: 1680286677
    Change-Id: Ic3c3fc27e51a62575af9a2046be237ab2e89a706

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -1741,6 +1741,12 @@ sap.ui.define([
 			oEntity = this._getObject(sPath),
 			aExpand = [], aSelect = [];
 
+		// Created entities should never be reloaded, as they do not exist on
+		// the server yet
+		if (this._isCreatedEntity(oEntity)) {
+			return false;
+		}
+
 		function checkReloadNeeded(oEntityType, oEntity, aSelect, aExpand) {
 			var aOwnSelect, aOwnExpand,
 				vNavData, oNavEntityType, oNavEntity, aNavSelect, aNavExpand,
@@ -4907,6 +4913,16 @@ sap.ui.define([
 		} else {
 			jQuery.sap.log.error("Tried to use createEntry without created-callback, before metadata is available!");
 		}
+	};
+
+	/**
+	 * Returns whether the given entity has been created using createEntry.
+	 * @param {object} oEntity The entity to check
+	 * @returns {boolean} Returns whether the entity is created
+	 * @private
+	 */
+	ODataModel.prototype._isCreatedEntity = function(oEntity) {
+		return !!(oEntity && oEntity.__metadata && oEntity.__metadata.created);
 	};
 
 	/**
