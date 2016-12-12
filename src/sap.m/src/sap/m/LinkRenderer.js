1d8ede22ddc77c6c4dc5c15fb7234fd53566672d
Mihail Yordanov <mihail.yordanov@sap.com>
2016-12-12 11:09:46 +0200

    [INTERNAL][FIX] sap.m.Link with empty text excluded from tab chain
    BCP: 1670557122
    -exluded empty links from the tab chain
    -qUnit tests added
    
    Change-Id: I40926964033f5cee272c8e472c0bfa27124aef5b
    CR-Id: 012006153200001512702016

--- src/sap.m/src/sap/m/LinkRenderer.js
+++ src/sap.m/src/sap/m/LinkRenderer.js
@@ -59,8 +59,10 @@
 			oRm.addClass("sapMLnkDsbl");
 			oRm.writeAttribute("disabled", "true");
 			oRm.writeAttribute("tabIndex", "-1"); // still focusable by mouse click, but not in the tab chain
-		} else {
+		} else if (oControl.getText()) {
 			oRm.writeAttribute("tabIndex", "0");
+		} else {
+			oRm.writeAttribute("tabIndex", "-1");
 		}
 		if (oControl.getWrapping()) {
 			oRm.addClass("sapMLnkWrapping");
