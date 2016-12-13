8b95cb7be68bf0e2d1e20a455541d6f57059dc07
Mingxin Fu <mingxin.fu@sap.com>
2016-12-13 14:52:39 +0100

    [INTERNAL] sap.m: Add CSS class in renderer improved
    Change-Id: I689da575bffbebab7bd96fc38e8ac58fa4c71e85
    CR-Id: 012006153200001527882016

--- src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
+++ src/sap.m/src/sap/m/GenericTileLineModeRenderer.js
@@ -82,7 +82,9 @@ sap.ui.define([ "sap/m/GenericTileRenderer", "sap/m/LoadState" ],
 
 		} else {
 			// cozy
-			this._renderFocusDiv(oRm, oControl);
+			if (oControl.getState() !== LoadState.Disabled) {
+				this._renderFocusDiv(oRm, oControl);
+			}
 
 			oRm.write("<div");
 			oRm.writeAttribute("id", oControl.getId() + "-touchArea");
@@ -103,7 +105,6 @@ sap.ui.define([ "sap/m/GenericTileRenderer", "sap/m/LoadState" ],
 			if (oControl.getSubheader()) {
 				this._renderSubheader(oRm, oControl);
 			}
-
 			oRm.write("</span>"); //.sapMGTLineModeHelpContainer
 
 			oRm.write("</div>"); //.sapMGTTouchArea
