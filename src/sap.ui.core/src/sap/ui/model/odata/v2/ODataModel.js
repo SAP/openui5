ab16742c8311b477770825be2e39d33ea25a3b1a
Jan Mummenthaler <jan.mummenthaler@sap.com>
2016-12-15 17:37:27 +0100

    [FIX] v2.ODataModel: Bindings fire no dataReceived event
    when a request is failing, no dataReceived event is fired by 
    the bindings. If a dataRequested event is fired a dataReceived event
    should also be fired.
    
    Change-Id: I60ee84286b48c01c6b00ce95dc920de6b157fd16
    BCP: 1680338300

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -1370,12 +1370,7 @@ sap.ui.define([
 				oBinding.checkUpdate(bForceUpdate, mChangedEntities);
 			}
 		}.bind(this));
-		//handle calls after update
-		var aCallAfterUpdate = this.aCallAfterUpdate;
-		this.aCallAfterUpdate = [];
-		for (var i = 0; i < aCallAfterUpdate.length; i++) {
-			aCallAfterUpdate[i]();
-		}
+		this._processAfterUpdate();
 	};
 
 	/**
@@ -2587,6 +2582,8 @@ sap.ui.define([
 					that._processError(oRequest.parts[i].request, oError, oRequest.parts[i].fnError);
 				}
 			}
+
+			that._processAfterUpdate();
 		}
 
 		oRequest.request.eventInfo = {
@@ -2688,6 +2685,8 @@ sap.ui.define([
 					processResponse(oRequest, oError, bAborted);
 				}
 			});
+
+			that._processAfterUpdate();
 
 			if (bAborted) {
 				that._processAborted(oBatchRequest, oError, fnError, true);
@@ -3246,6 +3245,19 @@ sap.ui.define([
 			} else {
 				 this.fireRequestCompleted(oEventInfo);
 			}
+		}
+	};
+
+	/**
+	 * Process handlers registered for execution after update.
+	 *
+	 * @private
+	 */
+	ODataModel.prototype._processAfterUpdate = function() {
+		var aCallAfterUpdate = this.aCallAfterUpdate;
+		this.aCallAfterUpdate = [];
+		for (var i = 0; i < aCallAfterUpdate.length; i++) {
+			aCallAfterUpdate[i]();
 		}
 	};
 
