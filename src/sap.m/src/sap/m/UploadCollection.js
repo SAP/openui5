48c16408cc987553519daed337caa304f6f84720
Petr Stribrny <petr.stribrny@sap.com>
2017-01-04 10:44:52 +0100

    [FIX] sap.m.UploadCollection: Focus handling in Upload Pending scenario improved
    Change-Id: Ic2f95f7e7f0085f61581de532b9c27efa75c5f9a
    BCP: 1670513143
    CR-Id: 012006153200000004862017

--- src/sap.m/src/sap/m/UploadCollection.js
+++ src/sap.m/src/sap/m/UploadCollection.js
@@ -931,10 +931,12 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './Dialog', './library', 'sa
 				for (i = 0; i < iToolbarElements; i++) {
 					// Only the newest instance of FileUploader is useful, which will be in the place holder position.
 					// Other ones can be hidden.
-					if (i === this._iFileUploaderPH && this._bFocusFileUploader) {
-						this._oHeaderToolbar.getContent()[i].$().find("button").focus();
-					} else {
-						this._oHeaderToolbar.getContent()[i].$().hide();
+					if (this._oHeaderToolbar.getContent()[i] instanceof sap.ui.unified.FileUploader) {
+						if (i === this._iFileUploaderPH && this._bFocusFileUploader) {
+							this._oHeaderToolbar.getContent()[i].$().find("button").focus();
+						} else {
+							this._oHeaderToolbar.getContent()[i].$().hide();
+						}
 					}
 				}
 			}
