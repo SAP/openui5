4a6bfc5f8138fc8d76e43ae2dda99db279547d73
Yavor Ivanov <yavor.ivanov@sap.com>
2017-01-19 14:57:26 +0200

    [FIX] sap.m.Slider: Global keyboard handling interruption fixed
    When the focus is on the Slider/RangeSlider the key combination of ALT + arrow left or right in the browser (going back and forth in history) is not working.
    
    BCP: 1780027203
    Fixes https://github.com/SAP/openui5/issues/1270
    
    Change-Id: I130776f4a250b49c279705d5abe438adb98cbb75

--- src/sap.m/src/sap/m/Slider.js
+++ src/sap.m/src/sap/m/Slider.js
@@ -960,7 +960,7 @@ sap.ui.define([
 		Slider.prototype.onsapincreasemodifiers = function(oEvent) {
 
 			// process the event if the target is not a composite control e.g.: a tooltip
-			if (oEvent.srcControl !== this) {
+			if (oEvent.srcControl !== this || oEvent.altKey) {
 				return;
 			}
 
@@ -1012,7 +1012,7 @@ sap.ui.define([
 		Slider.prototype.onsapdecreasemodifiers = function(oEvent) {
 
 			// process the event if the target is not a composite control e.g.: a tooltip
-			if (oEvent.srcControl !== this) {
+			if (oEvent.srcControl !== this || oEvent.altKey) {
 				return;
 			}
 
