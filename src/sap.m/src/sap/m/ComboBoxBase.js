8107dd40c1b81e7c29e219a33388f8aad20347d1
Martin R. Hristov <martin.r.hristov@sap.com>
2017-01-25 15:25:41 +0200

    [FIX] sap.m.ComboBox: Prevent the soft keyboard to be opened when opening the dropdown list
    Change-Id: Ie664c11d14e879df60de4cc4a419380da836e5fc
    BCP: 1780029945

--- src/sap.m/src/sap/m/ComboBoxBase.js
+++ src/sap.m/src/sap/m/ComboBoxBase.js
@@ -2,8 +2,8 @@
  * ${copyright}
  */
 
-sap.ui.define(['jquery.sap.global', './Dialog', './ComboBoxTextField', './Toolbar', './Button', './Bar', './Text', './Title', './SelectList', './Popover', 'sap/ui/core/InvisibleText', 'sap/ui/core/IconPool', 'sap/ui/core/ValueStateSupport', './library'],
-	function(jQuery, Dialog, ComboBoxTextField, Toolbar, Button, Bar, Text, Title, SelectList, Popover, InvisibleText, IconPool, ValueStateSupport, library) {
+sap.ui.define(['jquery.sap.global', './Dialog', './ComboBoxTextField', './Toolbar', './Button', './Bar', './Text', './Title', './SelectList', './Popover', 'sap/ui/core/InvisibleText', 'sap/ui/core/IconPool', 'sap/ui/core/ValueStateSupport', './library', 'sap/ui/Device'],
+	function(jQuery, Dialog, ComboBoxTextField, Toolbar, Button, Bar, Text, Title, SelectList, Popover, InvisibleText, IconPool, ValueStateSupport, library, Device) {
 		"use strict";
 
 		/**
@@ -254,9 +254,9 @@ sap.ui.define(['jquery.sap.global', './Dialog', './ComboBoxTextField', './Toolba
 			ComboBoxTextField.prototype.init.apply(this, arguments);
 
 			// sets the picker popup type
-			this.setPickerType(sap.ui.Device.system.phone ? "Dialog" : "Dropdown");
+			this.setPickerType(Device.system.phone ? "Dialog" : "Dropdown");
 
-			if (sap.ui.Device.system.phone) {
+			if (Device.system.phone) {
 				this.attachEvent("_change", this.onPropertyChange, this);
 			}
 
@@ -691,6 +691,20 @@ sap.ui.define(['jquery.sap.global', './Dialog', './ComboBoxTextField', './Toolba
 		 */
 		ComboBoxBase.prototype.isPickerDialog = function() {
 			return this.getPickerType() === "Dialog";
+		};
+
+		/*
+		 * Determines if the platform is a tablet.
+		 *
+		 * @returns {boolean}
+		 * @protected
+		 * @since 1.48
+		 */
+		ComboBoxBase.prototype.isPlatformTablet = function() {
+			var bNotCombi = !Device.system.combi,
+				bTablet = Device.system.tablet && bNotCombi;
+
+			return bTablet;
 		};
 
 		/*
