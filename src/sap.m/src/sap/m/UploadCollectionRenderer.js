87caab7a6d8a9a2fa023ffea1c4f155598a8c418
Mingxin Fu <mingxin.fu@sap.com>
2017-01-26 16:21:24 +0100

    [INTERNAL] sap.m.UploadCollection: Upload files from drag and drop by using existing FileUploader implemented
    - checks if the files are allowed by FileUploader
    - hides the no data description text when upload is disabled
    - shows info message when multiple is not allowed and multiple files are dropped
    - explored description for drag and drop adapted
    
    Change-Id: I993dd19782f43aabdf4619b8a00e491c7f4199da
    (cherry picked from commit c808ffc5fdd65495f10a8832b93373db0e89f0b5)
    CR-Id: 012006153200000070212017

--- src/sap.m/src/sap/m/UploadCollectionRenderer.js
+++ src/sap.m/src/sap/m/UploadCollectionRenderer.js
@@ -49,13 +49,15 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', 'sap/m/ListRenderer'
 		oRm.writeEscaped(oUploadCollection.getNoDataText());
 		oRm.write("</div>");
 
-		oRm.write("<div");
-		oRm.writeAttribute("id", oUploadCollection.getId() + "-no-data-description");
-		oRm.addClass("sapMUCNoDataDescription");
-		oRm.writeClasses();
-		oRm.write(">");
-		oRm.writeEscaped(oUploadCollection.getNoDataDescription());
-		oRm.write("</div>");
+		if (oUploadCollection.getUploadEnabled()) {
+			oRm.write("<div");
+			oRm.writeAttribute("id", oUploadCollection.getId() + "-no-data-description");
+			oRm.addClass("sapMUCNoDataDescription");
+			oRm.writeClasses();
+			oRm.write(">");
+			oRm.writeEscaped(oUploadCollection.getNoDataDescription());
+			oRm.write("</div>");
+		}
 		oRm.write("</div>");
 	};
 
