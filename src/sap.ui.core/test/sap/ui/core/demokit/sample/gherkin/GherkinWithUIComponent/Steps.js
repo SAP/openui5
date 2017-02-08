a653c31747fd3539734fd27d9dc09f764750c731
Jonathan Benn <jonathan.benn@sap.com>
2017-02-08 15:19:46 -0500

    [INTERNAL] Added sample of Gherkin loading a UIComponent
    Change-Id: I33e38c9f66e7dbdc43ef791163649c888e57ed05

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/gherkin/GherkinWithUIComponent/Steps.js
@@ -0,0 +1,58 @@
+sap.ui.define([
+  "jquery.sap.global",
+  "sap/ui/test/gherkin/StepDefinitions",
+  "sap/ui/test/Opa5",
+  "samples/components/button/Component"
+], function($, StepDefinitions, Opa5) {
+  "use strict";
+
+  var oOpa5 = new Opa5();
+
+  var Steps = StepDefinitions.extend("GherkinWithUIComponent.Steps", {
+    init: function() {
+
+      this.register(/^I start my UIComponent$/i, function() {
+        oOpa5.iStartMyUIComponent({
+          componentConfig: {
+            name: "samples.components.button"
+          },
+          hash: ""
+        });
+      });
+
+      this.register(/^the UIComponent loads successfully$/i, function() {
+        oOpa5.waitFor({
+          id: /mybutn$/,
+          success: function(controls) {
+            Opa5.assert.strictEqual(controls.length, 1, "Verified one control was found with correct ID");
+          }
+        });
+      });
+
+      this.register(/^I rename the button to "(.*?)"$/i, function(sText) {
+        oOpa5.waitFor({
+          id: /mybutn$/,
+          success: function(controls) {
+            controls[0].setText(sText);
+          }
+        });
+      });
+
+      this.register(/^the button is named "(.*?)"$/i, function(sText) {
+        oOpa5.waitFor({
+          id: /mybutn$/,
+          success: function(controls) {
+            Opa5.assert.strictEqual(controls[0].getText(), sText, "Verified button text is '" + sText + "'");
+          }
+        });
+      });
+    },
+
+    closeApplication: function() {
+      $.sap.log.info("Closing application");
+    }
+  });
+
+  return Steps;
+
+});
