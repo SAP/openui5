55a83a259217aeadabae910acd24a53e9ef474c0
Malte Wedel <malte.wedel@sap.com>
2017-02-09 09:38:10 +0100

    [INTERNAL] sap.ui.model.type: Better link to format options
    Make the link for format options direct to the NumberFormat/DateFormat
    constructor instead to the getInstance method, where the actual format
    options are listed.
    Unified documentation for DateTime, Date and Time.
    
    BCP: 1680052790
    Change-Id: Ie43f3a82cf05c437b215a3de8b8b55e59a4e6e94

--- src/sap.ui.core/src/sap/ui/model/type/Currency.js
+++ src/sap.ui.core/src/sap/ui/model/type/Currency.js
@@ -21,12 +21,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/format/NumberFormat', 'sap/ui/m
 	 *
 	 * @constructor
 	 * @public
-	 * @param {object} [oFormatOptions] formatting options. Supports the same options as {@link sap.ui.core.format.NumberFormat.getCurrencyInstance NumberFormat.getCurrencyInstance}
-	 * @param {object} [oFormatOptions.source] additional set of format options to be used if the property in the model is not of type string and needs formatting as well.
-	 * 										   In case an empty object is given, the default is disabled grouping and a dot as decimal separator.
-	 * @param {object} [oConstraints] value constraints.
-	 * @param {float} [oConstraints.minimum] smallest value allowed for this type
-	 * @param {float} [oConstraints.maximum] largest value allowed for this type
+	 * @param {object} [oFormatOptions] Formatting options. For a list of all available options, see {@link sap.ui.core.format.NumberFormat#constructor NumberFormat}.
+	 * @param {object} [oFormatOptions.source] Additional set of format options to be used if the property in the model is not of type <code>string</code> and needs formatting as well.
+	 * 										   If an empty object is given, the grouping is disabled and a dot is used as decimal separator.
+	 * @param {object} [oConstraints] Value constraints
+	 * @param {float} [oConstraints.minimum] Smallest value allowed for this type
+	 * @param {float} [oConstraints.maximum] Largest value allowed for this type
 	 * @alias sap.ui.model.type.Currency
 	 */
 	var Currency = CompositeType.extend("sap.ui.model.type.Currency", /** @lends sap.ui.model.type.Currency.prototype  */ {
