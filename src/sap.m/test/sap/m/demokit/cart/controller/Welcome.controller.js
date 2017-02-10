913b7f7ee0659d3c2dd0c87bea6fa727864cf1fe
Ayham Tanbari <ayham.tanbari@sap.com>
2017-02-10 09:25:47 +0100

    [INTERNAL][FIX] sap.m.demokit.cart-Fixing Routing bug and enhancing the code in the Welcome View
    - remove css from welcome view
    - remove sap.ui.layout.verticalLayout and use sap.m.FlexBox
    - Fix routing problems when pressing refresh button
    
    (cherry picked from commit d7f4d84ee86080ebcad5390473ce0a4a85274981)
    (cherry picked from commit 385e9621f7fa548ce5a11f3f77fe9422419cd6e4)
    
    Change-Id: Iac904ae99e9be20ecc70a9c1466308a9781d3246
    CR-Id: 012006153200000085412017

--- src/sap.m/test/sap/m/demokit/cart/controller/Welcome.controller.js
+++ src/sap.m/test/sap/m/demokit/cart/controller/Welcome.controller.js
@@ -18,20 +18,21 @@ sap.ui.define([
 			},
 
 
-			onInit: function () {
-				var oViewModel = new JSONModel({
-					welcomePictureUrl: 'img/Welcome.jpg',
-					Promoted: [],
-					Viewed: [],
-					Favorite: [],
-					Currency: "EUR"
-				});
-				this.getView().setModel(oViewModel, "view");
+		onInit: function () {
+			var oViewModel = new JSONModel({
+				welcomePictureUrl: 'img/Welcome.jpg',
+				Promoted: [],
+				Viewed: [],
+				Favorite: [],
+				Currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+			this.getRouter().attachRouteMatched(this._onRouteMatched, this);
+		},
 
-				this.getRouter().getRoute("home").attachPatternMatched(this._onRouteMatched, this);
-			},
-
-			_onRouteMatched: function () {
+		_onRouteMatched: function (oEvent) {
+			// we do not need to call this function if the url hash refers to product or cart product
+			if (oEvent.getParameter("name") !== "product" && oEvent.getParameter("name") !== "cartProduct") {
 				var aPromotedData = this.getView().getModel("view").getProperty("/Promoted");
 				if (!aPromotedData.length) {
 					var oModel = this.getModel();
@@ -50,56 +51,56 @@ sap.ui.define([
 						});
 					}.bind(this));
 				}
-			},
-
-			/**
-			* Event handler to determine which link the user has clicked
-			* @param {sap.ui.base.Event} oEvent the press event of the link
-			*/
-			onSelectProduct: function (oEvent) {
-				var oContext = oEvent.getSource().getBindingContext("view");
-				var sCategoryId = oContext.getProperty("Product/Category");
-				var sProductId = oContext.getProperty("Product/ProductId");
-				this.getOwnerComponent().getRouter().navTo("product", {
-					id: sCategoryId,
-					productId: sProductId
-				});
-			},
-
-			/**
-			 * Opens a lightbox when clicking on the picture
-			 * @param {sap.ui.base.Event} oEvent the press event of the image
-			 */
-			onPicturePress: function (oEvent) {
-				var sPath = "view>" + oEvent.getSource().getBindingContext("view").getPath() + "/Product";
-				this.byId("lightBox").bindElement({path: sPath});
-				this.byId("lightBox").open();
-			},
-
-			/**
-			* Event handler to determine which button was clicked
-			* @param {sap.ui.base.Event} oEvent the button press event
-			*/
-			onAddButtonPress: function (oEvent) {
-				var oResourceBundle = this.getModel("i18n").getResourceBundle();
-				var oProduct = oEvent.getSource().getBindingContext("view").getObject();
-				var oCartModel = this.getModel("cartProducts");
-				cart.addToCart(oResourceBundle, oProduct, oCartModel);
-
-			},
-
-			/**
-			 * Select two random elements from the promoted array
-			 * @private
-			 */
-			_selectPromotedItems: function () {
-				var aPromotedItems = this.getView().getModel("view").getProperty("/Promoted");
-				var aSelectedPromoted = [];
-				for (var i = 0; i < 2; i++) {
-					var oSelectedPromoted = aPromotedItems[Math.floor(Math.random() * aPromotedItems.length)];
-					aSelectedPromoted.push(oSelectedPromoted);
-				}
-				this.getModel("view").setProperty("/Promoted", aSelectedPromoted);
 			}
+		},
+
+		/**
+		* Event handler to determine which link the user has clicked
+		* @param {sap.ui.base.Event} oEvent the press event of the link
+		*/
+		onSelectProduct: function (oEvent) {
+			var oContext = oEvent.getSource().getBindingContext("view");
+			var sCategoryId = oContext.getProperty("Product/Category");
+			var sProductId = oContext.getProperty("Product/ProductId");
+			this.getOwnerComponent().getRouter().navTo("product", {
+				id: sCategoryId,
+				productId: sProductId
+			});
+		},
+
+		/**
+		 * Opens a lightbox when clicking on the picture
+		 * @param {sap.ui.base.Event} oEvent the press event of the image
+		 */
+		onPicturePress: function (oEvent) {
+			var sPath = "view>" + oEvent.getSource().getBindingContext("view").getPath() + "/Product";
+			this.byId("lightBox").bindElement({path: sPath});
+			this.byId("lightBox").open();
+		},
+
+		/**
+		* Event handler to determine which button was clicked
+		* @param {sap.ui.base.Event} oEvent the button press event
+		*/
+		onAddButtonPress: function (oEvent) {
+			var oResourceBundle = this.getModel("i18n").getResourceBundle();
+			var oProduct = oEvent.getSource().getBindingContext("view").getObject();
+			var oCartModel = this.getModel("cartProducts");
+			cart.addToCart(oResourceBundle, oProduct, oCartModel);
+		},
+
+		/**
+		 * Select two random elements from the promoted array
+		 * @private
+		 */
+		_selectPromotedItems: function () {
+			var aPromotedItems = this.getView().getModel("view").getProperty("/Promoted");
+			var aSelectedPromoted = [];
+			for (var i = 0; i < 2; i++) {
+				var oSelectedPromoted = aPromotedItems[Math.floor(Math.random() * aPromotedItems.length)];
+				aSelectedPromoted.push(oSelectedPromoted);
+			}
+			this.getModel("view").setProperty("/Promoted", aSelectedPromoted);
+		}
 	});
 });
