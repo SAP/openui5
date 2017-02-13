a80ab0bcd2655bbbae914f3a4f2036a8a72ad8f1
Krasimira Mashova <krasimira.mashova@sap.com>
2017-02-13 17:42:03 +0200

    [FIX][TEST] sap.m.ComboBox: Remove the cursor from visual test
    Change-Id: I6fa13b9b8435ea24ecd388d43a8e4e342d2030c8

--- src/sap.m/test/sap/m/visual/ComboBox.spec.js
+++ src/sap.m/test/sap/m/visual/ComboBox.spec.js
@@ -2,33 +2,42 @@ describe("sap.m.ComboBox", function() {
 
     // check initial
     it("should load test page",function(){
+        element(by.id("title_default")).click();
         expect(takeScreenshot()).toLookAs("initial");
     });
 
     // check ComboBox - default
     it("should open first ComboBox - Default", function() {
-        element(by.id("box_default-arrow")).click();
-        expect(takeScreenshot()).toLookAs("default_fullscreen");
+        var defaultArrow = element(by.id("box_default-arrow"));
+        defaultArrow.click();
         element.all(by.css('li.sapMSelectListItemBase[id*="box_default"]')).get(2).click();
+        defaultArrow.click();
+        expect(takeScreenshot()).toLookAs("default_fullscreen");
+        defaultArrow.click();
     });
 
    // check ComboBox - Two column layout
     it("should open second ComboBox - Two column layout", function() {
-        element(by.id("box_two_column-arrow")).click();
-        expect(takeScreenshot()).toLookAs("two_column_fullscreen");
+        var twoColumnArrow = element(by.id("box_two_column-arrow"));
+        twoColumnArrow.click();
         element.all(by.css('li.sapMSelectListItemBase[id*="box_two_column"]')).get(8).click();
+        twoColumnArrow.click();
+        expect(takeScreenshot()).toLookAs("two_column_fullscreen");
+        twoColumnArrow.click();
     });
 
     // check ComboBox - Placeholder
     it("should open third ComboBox - Placeholder", function() {
         var comboBoxPlaceholder = element(by.id("box_placeholder"));
         comboBoxPlaceholder.click();
+        element(by.id("title_placeholder")).click();
         expect(takeScreenshot(comboBoxPlaceholder)).toLookAs("placeholder");
     });
 
     // check ComboBox - Label and placeholder
     it("should open fourth ComboBox - Label and placeholder", function() {
         element(by.id("box_label_placeholder")).click();
+        element(by.id("title_label_placeholder")).click();
         expect(takeScreenshot(element(by.id("layout_label_placeholder")))).toLookAs("label_placeholder");
     });
 
@@ -43,13 +52,13 @@ describe("sap.m.ComboBox", function() {
     it("should open sixth ComboBox - Read only", function() {
         var comboBoxReadOnly = element(by.id("box_read_only"));
         comboBoxReadOnly.click();
-        comboBoxReadOnly.click();
         expect(takeScreenshot(comboBoxReadOnly)).toLookAs("read_only");
     });
 
     // check ComboBox - Warning state
     it("should open seventh ComboBox - Warning state", function() {
         element(by.id("box_warning")).click();
+        element(by.id("title_warning")).click();
         expect(takeScreenshot(element(by.id("layout_warning")))).toLookAs("warning");
     });
 
@@ -57,6 +66,7 @@ describe("sap.m.ComboBox", function() {
     it("should open eighth ComboBox - Success state", function() {
         var comboBoxSuccess = element(by.id("box_success"));
         comboBoxSuccess.click();
+        element(by.id("title_success")).click();
         expect(takeScreenshot(comboBoxSuccess)).toLookAs("success");
     });
 
@@ -64,6 +74,7 @@ describe("sap.m.ComboBox", function() {
     it("should open ninth ComboBox - Error state", function() {
         var comboBoxError = element(by.id("box_error"));
         comboBoxError.click();
+        element(by.id("title_error")).click();
         expect(takeScreenshot(comboBoxError)).toLookAs("error");
     });
 });
