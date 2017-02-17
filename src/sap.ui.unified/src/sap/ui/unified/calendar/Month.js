9911b68d1c0411be817bb1ff526c4b2921ab0e4c
I312974 <daniel.nanovski@sap.com>
2017-02-17 15:30:32 +0200

    [FIX] sap.m.Month - Fixes a regression in restoring focus introduced with another change
    * Now the focus is NOT restored only if the invalidate origin is from the correct type and comes from the "specialDates" aggregation
    * Fixes a regression previously introduced with change: Iab3430e1e0709aa5f88d4d48c3e950c34b7e674d
    * qUnit test added to cover this special case
    
    Change-Id: I988b1a59e8bc161842220576928c749ad666fd71
    BCP: 1780121409

--- src/sap.ui.unified/src/sap/ui/unified/calendar/Month.js
+++ src/sap.ui.unified/src/sap/ui/unified/calendar/Month.js
@@ -231,7 +231,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 		} else if (this.getDomRef() && !this._sInvalidateMonth) {
 			// DateRange changed -> only rerender days
 			// do this only once if more DateRanges / Special days are changed
-			this._bNoFocus = true; // Don't restore focus if special days are added
+			if (oOrigin && oOrigin.sParentAggregationName === "specialDates") {
+				// Don't restore focus if special dates are added
+				this._bNoFocus = true;
+			}
 			if (this._bInvalidateSync) { // set if calendar already invalidates in delayed call
 				_invalidateMonth.call(this);
 			} else {
