cd11a37880c1e40485093aff016695d20c0ec748
Andreas Kunz <andreas.kunz@sap.com>
2017-02-24 17:12:06 +0100

    [FIX] commons.MessageRenderer: write control Id
    Change-Id: I96a3bc53673f22d4bde5d1cc3f65c4f3a21158eb

--- src/sap.ui.commons/src/sap/ui/commons/MessageRenderer.js
+++ src/sap.ui.commons/src/sap/ui/commons/MessageRenderer.js
@@ -34,7 +34,9 @@ sap.ui.define(['jquery.sap.global'],
 
 		// Opening the outer container:
 		// tabindex="0": Message must be focusable for accessibility?!?
-		rm.write('<div class="sapUiMsg" tabindex="0">');
+		rm.write('<div class="sapUiMsg" tabindex="0"');
+		rm.writeControlData(oControl);
+		rm.write('>');
 
 		// Message icon:
 		rm.write('<div class="sapUiMsgIcon sapUiMsgIcon' + oControl.getType() + '"></div>');
