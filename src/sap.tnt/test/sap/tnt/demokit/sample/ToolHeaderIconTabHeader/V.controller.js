601e422ff678503da9530011dfe71a0eea717c68
I312031 <teodor.taushanov@sap.com>
2017-03-01 15:06:13 +0200

    [FEATURE] sap.tnt.IconTabHeader: sap.m.IconTabHeader is now correctly displayed inside sap.tnt.ToolHeader
    Change-Id: I3e1f8e1a0262f1feeeb741236afd943b97148b47

---
+++ src/sap.tnt/test/sap/tnt/demokit/sample/ToolHeaderIconTabHeader/V.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+		'jquery.sap.global',
+		'sap/ui/core/mvc/Controller',
+		'sap/m/Popover',
+		'sap/m/Button'
+	], function(jQuery, Controller, Popover, Button) {
+	"use strict";
+
+	var Controller = Controller.extend("sap.tnt.sample.ToolHeaderIconTabHeader.V", {
+
+		onInit: function () {
+
+		}
+	});
+
+
+	return Controller;
+
+});
