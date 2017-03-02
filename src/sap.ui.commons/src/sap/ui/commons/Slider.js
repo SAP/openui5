508841d987beefccbb3508a57d150ee83c3c8017
i310817 <gergana.marcheva@sap.com>
2017-03-02 14:30:03 +0200

    [FIX] sap.ui.commons.Slider: Setting of limit values work correctly
    BCP: 1780124586
    
    Change-Id: Idfee2c1adf36c8519596ee92612e93f991858f10

--- src/sap.ui.commons/src/sap/ui/commons/Slider.js
+++ src/sap.ui.commons/src/sap/ui/commons/Slider.js
@@ -1346,20 +1346,12 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		fBarWidth = this.getBarWidth();
 		bIsVertical = this.getVertical();
 
-		if (fNewValue >= fMax) {
+		if (fNewValue > fMax) {
 			fNewValue = fMax;
-			if (bIsVertical) {
-				iNewPos = 0;
-			} else {
-				iNewPos = fBarWidth;
-			}
-		} else if (fNewValue <= fMin) {
+			iNewPos = fBarWidth;
+		} else if (fNewValue < fMin) {
 			fNewValue = fMin;
-			if (bIsVertical) {
-				iNewPos = fBarWidth;
-			} else {
-				iNewPos = 0;
-			}
+			iNewPos = 0;
 		} else {
 			iNewPos = (( fNewValue - fMin ) / ( fMax - fMin )) * fBarWidth;
 		}
@@ -1372,7 +1364,6 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		this._lastValue = fNewValue;
 
 		return this;
-
 	};
 
 	/*
