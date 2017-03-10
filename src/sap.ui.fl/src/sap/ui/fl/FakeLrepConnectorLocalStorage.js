61334521b5f12a0451efa1479d05fd1ef0503680
Sebastian Wennemers <sebastian.wennemers@sap.com>
2017-03-10 16:34:04 +0100

    [INTERNAL] LrepConnector that works with LocalStorage
    Moved code from sapui5.runtime. Allow better (integration) testing against whole fl lib.
    
    Change-Id: Ieaca80a47e208d257b34f3f9825b90aad3baf018

---
+++ src/sap.ui.fl/src/sap/ui/fl/FakeLrepConnectorLocalStorage.js
@@ -0,0 +1,150 @@
+/*
+ * ! ${copyright}
+ */
+
+sap.ui.define([
+	"sap/ui/fl/FakeLrepLocalStorage",
+	"sap/ui/fl/FakeLrepConnector",
+	"sap/ui/fl/LrepConnector",
+	"sap/ui/fl/Cache"
+	], function(
+	FakeLrepLocalStorage, FakeLrepConnector, LrepConnector, Cache ) {
+	"use strict";
+
+	/**
+	 * Class for connecting to Fake LREP storing changes in localStorage
+	 *
+	 * @class
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @private
+	 * @static
+	 * @since 1.48
+	 * @alias sap.ui.fl.FakeLrepConnectorLocalStorage
+	 */
+	function FakeLrepConnectorLocalStorage(mSettings){
+		this.mSettings = jQuery.extend({
+			"isKeyUser": true,
+			"isAtoAvailable": false,
+			"isProductiveSystem": false
+		}, mSettings);
+	}
+
+	jQuery.extend(FakeLrepConnectorLocalStorage.prototype, FakeLrepConnector.prototype);
+
+	/**
+	 * Creates a Fake Lrep change in localStorage
+	 * @param  {Object|Array} vChange - the change array/object
+	 * @returns {Promise} Returns a promise to the result of the request
+	 */
+	FakeLrepConnectorLocalStorage.prototype.create = function(vChange) {
+
+		if (Array.isArray(vChange)) {
+			vChange.forEach(function(elem) {
+				FakeLrepLocalStorage.saveChange(elem.fileName, elem);
+			});
+		} else {
+			FakeLrepLocalStorage.saveChange(vChange.fileName, vChange);
+		}
+		return Promise.resolve();
+	};
+
+	/**
+	 * Deletes a Fake Lrep change in localStorage
+	 * @param  {Object} oChange - the change Object
+	 * @returns {Promise} Returns a promise to the result of the request
+	 */
+	FakeLrepConnectorLocalStorage.prototype.deleteChange = function(oChange) {
+
+		FakeLrepLocalStorage.deleteChange(oChange.fileName);
+
+		return Promise.resolve({
+			response: undefined,
+			status: "nocontent"
+		});
+	};
+
+	/**
+	 * Deletes all Fake Lrep changes in localStorage
+	 * @returns {Promise} Returns a promise to the result of the request
+	 */
+	FakeLrepConnectorLocalStorage.prototype.deleteChanges = function() {
+
+		FakeLrepLocalStorage.deleteChanges();
+
+		return Promise.resolve({
+			response: undefined,
+			status: "nocontent"
+		});
+	};
+
+	/**
+	 * Loads the changes for the given Component class name
+	 * from the FakeLrepLocalStorage
+	 * and also loads the mandatory FakeLrepConnector.json file.
+	 * The settings are take from the JSON file, but changes are replaced with
+	 * the changes from the local storage.
+	 *
+	 * @param {String} sComponentClassName - Component class name
+	 * @returns {Promise} Returns a Promise with the changes and componentClassName
+	 * @public
+	 */
+	FakeLrepConnectorLocalStorage.prototype.loadChanges = function(sComponentClassName) {
+
+		var aChanges = FakeLrepLocalStorage.getChanges();
+
+		return new Promise(function(resolve, reject){
+			var result = {
+				changes: {
+					changes : aChanges,
+					settings : this.mSettings
+				},
+				componentClassName: sComponentClassName
+			};
+			resolve(result);
+		}.bind(this));
+
+	};
+
+	/**
+	 * Hooks into the @link {sap.ui.fl.LrepConnector.createConnector} factory
+	 * function to enable the fake lrep connector.
+	 *
+	 * @param sInitialComponentJsonPath - the relative path to a test-component-changes.json file
+	 */
+	FakeLrepConnectorLocalStorage.enableFakeConnector = function(mSettings){
+		Cache._entries = {};
+
+		if (FakeLrepConnectorLocalStorage.fnOriginalCreateConnector){
+			return;
+		}
+
+		FakeLrepConnectorLocalStorage.fnOriginalCreateConnector = LrepConnector.createConnector;
+
+		LrepConnector.createConnector = function(){
+			if (!FakeLrepConnectorLocalStorage._oInstance) {
+				FakeLrepConnectorLocalStorage._oInstance = new FakeLrepConnectorLocalStorage(mSettings);
+			}
+
+			return FakeLrepConnectorLocalStorage._oInstance;
+		};
+	};
+
+	/**
+	 * Restore the original @link {sap.ui.fl.LrepConnector.createConnector} factory
+	 * function.
+	 */
+	FakeLrepConnectorLocalStorage.disableFakeConnector = function(){
+		Cache._entries = {};
+		if (FakeLrepConnectorLocalStorage.fnOriginalCreateConnector){
+			LrepConnector.createConnector = FakeLrepConnectorLocalStorage.fnOriginalCreateConnector;
+			delete FakeLrepConnectorLocalStorage.fnOriginalCreateConnector;
+			delete FakeLrepConnectorLocalStorage._oInstance;
+		}
+	};
+
+	return FakeLrepConnectorLocalStorage;
+
+}, /* bExport= */ true);
