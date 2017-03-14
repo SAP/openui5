b1d16eb75e3011cb86356d009d8a1ad363a1e38a
Sven Bernhard <sven.bernhard@sap.com>
2017-03-14 16:31:51 +0100

    [INTERNAL] sap.ui.model.odata.ODataMetaModel: added since info to JSDoc
    Added "since" information for v2 -> v4 annotation conversions that have
    not been part of 1.30 shipment.
    
    PS2: review comments
    
    Change-Id: I59c3e481e26ae239d85045071b8834ec7db450f5
    JIRA: CPOUI5UISERVICESV3-581

--- src/sap.ui.core/src/sap/ui/model/odata/ODataMetaModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/ODataMetaModel.js
@@ -103,14 +103,14 @@ sap.ui.define([
 	 * <code>pageable</code>, <code>requires-filter</code>, <code>searchable</code>,
 	 * <code>topable</code>, <code>updatable</code> and <code>updatable-path</code> on entity sets;
 	 * </li>
-	 * <li><code>creatable</code>, <code>creatable-path</code>, <code>filterable</code>
-	 * on navigation properties;</li>
-	 * <li><code>aggregation-role</code> ("dimension" and "measure"), <code>creatable</code>,
-	 * <code>display-format</code> ("UpperCase" and "NonNegative"), <code>field-control</code>,
-	 * <code>filterable</code>, <code>filter-restriction</code>, <code>heading</code>,
-	 * <code>precision</code>, <code>quickinfo</code>, <code>required-in-filter</code>,
-	 * <code>sortable</code>, <code>text</code>, <code>unit</code>, <code>updatable</code> and
-	 * <code>visible</code> on properties;</li>
+	 * <li><code>creatable</code> (since 1.41.0), <code>creatable-path</code> (since 1.41.0) and
+	 * <code>filterable</code> (since 1.39.0) on navigation properties;</li>
+	 * <li><code>aggregation-role</code> ("dimension" and "measure", both since 1.45.0),
+	 * <code>creatable</code>, <code>display-format</code> ("UpperCase" and "NonNegative"),
+	 * <code>field-control</code>, <code>filterable</code>, <code>filter-restriction</code>,
+	 * <code>heading</code>, <code>precision</code>, <code>quickinfo</code>,
+	 * <code>required-in-filter</code>, <code>sortable</code>, <code>text</code>, <code>unit</code>,
+	 * <code>updatable</code> and <code>visible</code> on properties;</li>
 	 * <li><code>semantics</code>; the following values are supported:
 	 * <ul>
 	 * <li>"bday", "city", "country", "email" (including support for types, for example
