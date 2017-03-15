7cdd13104fe0f4ef180991c7aae18c268653efe9
Tanbari <aehamtonbare@hotmail.com>
2017-03-15 13:12:52 +0100

    Add relatedProjects.js file
    Add new entry to the  related project JSON file

--- javascripts/relatedProjects.js
+++ javascripts/relatedProjects.js
@@ -3,41 +3,49 @@ function onLoad() {
 	this.oFeaturedProjects = {
 		"Ui5Strap": {
 			"name": "Ui5Strap",
-			"decription": "-",
+			"decription": "Ui5Strap is a multitasking environment, which means you can start multiple apps at the same time",
 			"githubLink": "https://github.com/pks5/ui5strap",
 			"documentaionLink": "http://www.ui5strap.com/manual/",
-			"owner": "-",
-			"license": "Apache 2"
+			"owner": "pks5",
+			"license": "Apache 2.0"
 		},
 		"UI5CustomControl": {
 			"name": "OpenUI5 Custom Controls for charting",
-			"decription": "-",
+			"decription": "This project shows how to use C3.js and Chart.js within OpenUI5 projects.",
 			"githubLink": "https://github.com/SAP/openui5-charting-custom-controls",
-			"documentaionLink": "#UI5CustomControl",
-			"owner": "Michael Graf",
-			"license": "-"
+			"documentaionLink": "",
+			"owner": "michadelic",
+			"license": "Apache 2.0"
 		},
 		"Meteor-UI5": {
 			"name": "Meteor-UI5",
-			"decription": "-",
+			"decription": "Meteor-UI5 is a collection of packages that brings to together two powerful open source JavaScript web frameworks: Meteor and OpenUI5.",
 			"githubLink": "https://github.com/propellerlabsio/meteor-ui5",
 			"documentaionLink": "https://meteor-ui5.propellerlabs.com/",
-			"owner": "-",
-			"license": "-"
+			"owner": "proehlen",
+			"license": "Apache 2.0"
+		},
+		"openui5-i18n-util": {
+			"name": "openui5-i18n-util",
+			"decription": "This script will search all your i18n strings and build auto-magically every translations file for you.",
+			"githubLink": "https://github.com/propellerlabsio/meteor-ui5",
+			"documentaionLink": "",
+			"owner": "StErMi",
+			"license": "Apache 2.0"
 		}
+		
+		
 	};
 	// generate template for each table entry
 	var tableNode = document.getElementById("projectsTable");
 	Object.keys(this.oFeaturedProjects).forEach(function (sKey) {
 		var sRowTemplate = document.createElement("tr");
-		var sTdTemplate1, sTdTemplate2, sTdTemplate3, sTdTemplate4, sTdTemplate5,sTdTemplate6;
-		sName  = "<td>" + oFeaturedProjects[sKey].name + "</td>";
-		sGitHubLink  = "<td><a href=" + oFeaturedProjects[sKey].githubLink + " " + "name=" + sKey + ">" + oFeaturedProjects[sKey].name + "</a></td>";
-		sOwner  = "<td>" + oFeaturedProjects[sKey].owner + "</td>";
+		sName  = "<td><a href=" + oFeaturedProjects[sKey].githubLink + " " + "name=" + sKey + ">" + oFeaturedProjects[sKey].name + "</a></td>";
+		sOwner  = "<td><a href=https://github.com/" + oFeaturedProjects[sKey].owner +">" + "@" + oFeaturedProjects[sKey].owner + "</a></td>";
 		sDescription  = "<td>" + oFeaturedProjects[sKey].decription + "</td>";
 		sDocumentationLink  = "<td><a href=" + oFeaturedProjects[sKey].documentaionLink + ">" + "Documentation</a></td>";
 		sLicense  = "<td>" + oFeaturedProjects[sKey].license + "</td>";
-		sRowTemplate.innerHTML = sName + sGitHubLink + sOwner + sDescription + sDocumentationLink + sLicense;
+		sRowTemplate.innerHTML = sName + sOwner + sDescription + sDocumentationLink + sLicense;
 		tableNode.appendChild(sRowTemplate);
 	});
 }
@@ -61,5 +69,15 @@ function onLoad() {
 	 $(sSecondRow).css("background-color", "#dcdcdc");
 	 $(sFirstRow).css("background-color", "#ffffff");
  }
+ // handel click event on the Meteor-UI5 project
+ /*
+ function onMeteor-UI5lClick () {
+	 var sThirdRow = $("#projectsTable").children()[3];
+	 var sFirstRow = $("#projectsTable").children()[1];
+	 var sSecondRow = $("#projectsTable").children()[2];
+	 $(sThirdRow).css("background-color", "#dcdcdc");
+	 $(sSecondRow).css("background-color", "#ffffff");
+	 $(sFirstRow).css("background-color", "#ffffff");
+ }*/
 
 	
