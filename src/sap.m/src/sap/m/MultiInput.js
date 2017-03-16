f4fa170603008836331d0cfda015bc62589e79af
I312031 <teodor.taushanov@sap.com>
2017-03-16 17:43:45 +0200

    [FIX] sap.m.MultiInput: When MultiInput.enabled=false, tokens can't be removed any more
    Change-Id: Idcf44bbc7e450fb31c39d46483db45f4b8d7aec8
    BCP: 1770062067
    CR-Id: 002075125900000814162017

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -3,8 +3,8 @@
  */
 
 // Provides control sap.m.MultiInput.
-sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './library', 'sap/ui/Device', 'sap/ui/core/InvisibleText'],
-	function (jQuery, Input, Tokenizer, Token, library, Device, InvisibleText) {
+sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './library', 'sap/ui/core/EnabledPropagator', 'sap/ui/Device', 'sap/ui/core/InvisibleText'],
+	function (jQuery, Input, Tokenizer, Token, library, EnabledPropagator, Device, InvisibleText) {
 		"use strict";
 
 
@@ -133,6 +133,8 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 			}
 		}
 	});
+
+	EnabledPropagator.apply(MultiInput.prototype, [true]);
 
 	var oRb = sap.ui.getCore().getLibraryResourceBundle("sap.m");
 
