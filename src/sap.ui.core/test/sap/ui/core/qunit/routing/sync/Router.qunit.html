SAP/openui5

commit 0a8e5f6116eab06dd31a8d9c871dff29a315946c

[FEATURE] core.routing.Router: new param to suppress hash parsing in 'initialize'
- new parameter is introduced in function 'initialize' to avoid parsing the
  current hash after router is initialized

Change-Id: Ie884b7e5de535cc70a45abc01cab31e711678c6e
BCP: 1780042552

src/sap.ui.core/src/sap/ui/core/routing/Router.js
@@ -259,10 +259,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/base/EventPro
 			/**
 			 * Attaches the router to the hash changer @see sap.ui.core.routing.HashChanger
 			 *
+			 * @param {boolean} [bIgnoreInitialHash=false] @since 1.48.0 whether the current url hash shouldn't be parsed after the router is initialized
 			 * @public
 			 * @returns {sap.ui.core.routing.Router} this for chaining.
 			 */
-			initialize : function () {
+			initialize : function (bIgnoreInitialHash) {
 				var that = this,
 					oHashChanger = this.oHashChanger = HashChanger.getInstance();
 
@@ -281,13 +282,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/base/EventPro
 					that._bHashChangedAfterTitleChange = true;
 				};
 
-
 				if (!oHashChanger) {
 					jQuery.sap.log.error("navTo of the router is called before the router is initialized. If you want to replace the current hash before you initialize the router you may use getUrl and use replaceHash of the Hashchanger.", this);
 					return;
 				}
-
-				oHashChanger.attachEvent("hashChanged", this.fnHashChanged);
 
 				if (this._oTargets) {
 					var oHomeRoute = this._oRoutes[this._oConfig.homeRoute];
@@ -319,7 +317,16 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/base/EventPro
 					}
 				}
 
-				if (!oHashChanger.init()) {
+				oHashChanger.init();
+
+				// The event handler needs to be attached after hash changer is
+				// initialized because whether the current hash is parsed is
+				// controlled by the 'bSuppressHashParsing' parameter and the
+				// 'hashchanged' event which may be fired from hashChanger.init()
+				// shouldn't be processed.
+				oHashChanger.attachEvent("hashChanged", this.fnHashChanged);
+
+				if (!bIgnoreInitialHash) {
 					this.parse(oHashChanger.getHash());
 				}
 
@@ -334,6 +341,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/base/EventPro
 			 * @public
 			 */
 			stop : function () {
+				// save the hash in order to decide whether to parse the hash
+				// when initialize is called again
+				this._sLastHashBeforeStop = this.oHashChanger.getHash();
 
 				if (!this._bIsInitialized) {
 					jQuery.sap.log.warning("Router is not initialized. But it got stopped", this);

src/sap.ui.core/test/sap/ui/core/qunit/routing/async/Router.qunit.html
@@ -114,6 +114,40 @@
 				router.destroy();
 			});
 
+			QUnit.test("Should parse the hash again when second initialize doesn't suppress hash parsing", function(assert) {
+				var router = fnCreateRouter();
+				var parseSpy = this.spy(router, "parse");
+
+				hasher.setHash("");
+
+				router.initialize();
+
+				assert.equal(parseSpy.callCount, 1, "did notify for initial hash");
+				router.stop();
+
+				router.initialize();
+				assert.equal(parseSpy.callCount, 2, "did notify when initialized with the same hash again");
+
+				router.destroy();
+			});
+
+			QUnit.test("Shouldn't parse the hash again when second initialize does suppress hash parsing", function(assert) {
+				var router = fnCreateRouter();
+				var parseSpy = this.spy(router, "parse");
+
+				hasher.setHash("");
+
+				router.initialize();
+
+				assert.equal(parseSpy.callCount, 1, "did notify for initial hash");
+				router.stop();
+
+				router.initialize(true /*suppress hash parsing*/);
+				assert.equal(parseSpy.callCount, 1, "did not notify when initialized with the same hash again");
+
+				router.destroy();
+			});
+
 			QUnit.test("Should destroy the router instance", function() {
 				//Arrange
 				var parseSpy,

src/sap.ui.core/test/sap/ui/core/qunit/routing/sync/Router.qunit.html
@@ -93,6 +93,40 @@
 			router.destroy();
 		});
 
+		QUnit.test("Should parse the hash when initialize doesn't suppress the hash parsing", function(assert) {
+			var router = new sap.ui.core.routing.Router();
+			var parseSpy = this.spy(router, "parse");
+
+			hasher.setHash("");
+
+			router.initialize();
+
+			assert.equal(parseSpy.callCount, 1, "did notify for initial hash");
+			router.stop();
+
+			router.initialize();
+			assert.equal(parseSpy.callCount, 2, "did notify when initialized with the same hash again");
+
+			router.destroy();
+		});
+
+		QUnit.test("Should not parse the hash when initialize does suppress the hash parsing", function(assert) {
+			var router = new sap.ui.core.routing.Router();
+			var parseSpy = this.spy(router, "parse");
+
+			hasher.setHash("");
+
+			router.initialize();
+
+			assert.equal(parseSpy.callCount, 1, "did notify for initial hash");
+			router.stop();
+
+			router.initialize(true /* suppress hash parsing */);
+			assert.equal(parseSpy.callCount, 1, "did not notify when initialized with the same hash again");
+
+			router.destroy();
+		});
+
 		test("Should destroy the router instance", function() {
 			//Arrange
 			var parseSpy,

commit e6a3617c9106adf4ba4404d2f0a58030238b4cdd

[FIX] core.routing.Router: 'stop' can be called before 'initialize'
- when stop is called before initialize, the this.oHashChanger is still
  undefined. This issue was introduced by change
  0a8e5f6116eab06dd31a8d9c871dff29a315946c

- the line of code which causes the issue is not needed for the new feature.
  It's left over from the first draft of the new feature

Change-Id: I208250a44ee667fdd1ab9cd58ae18353c6e061b4
BCP: 1770080082

src/sap.ui.core/src/sap/ui/core/routing/Router.js
@@ -341,10 +341,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/library', 'sap/ui/base/EventPro
 			 * @public
 			 */
 			stop : function () {
-				// save the hash in order to decide whether to parse the hash
-				// when initialize is called again
-				this._sLastHashBeforeStop = this.oHashChanger.getHash();
-
 				if (!this._bIsInitialized) {
 					jQuery.sap.log.warning("Router is not initialized. But it got stopped", this);
 				}

src/sap.ui.core/test/sap/ui/core/qunit/routing/async/Router.qunit.html
@@ -114,6 +114,17 @@
 				router.destroy();
 			});
 
+			QUnit.test("Should not raise any exeception when stop is called before initialize", function(assert) {
+				assert.expect(0)
+				var router = fnCreateRouter();
+
+				// call stop shouldn't raise any exception
+				router.stop();
+
+				//Cleanup
+				router.destroy();
+			});
+
 			QUnit.test("Should parse the hash again when second initialize doesn't suppress hash parsing", function(assert) {
 				var router = fnCreateRouter();
 				var parseSpy = this.spy(router, "parse");

src/sap.ui.core/test/sap/ui/core/qunit/routing/sync/Router.qunit.html
@@ -93,6 +93,17 @@
 			router.destroy();
 		});
 
+		QUnit.test("Should not raise any exeception when stop is called before initialize", function(assert) {
+			assert.expect(0)
+			var router = new sap.ui.core.routing.Router();
+
+			// call stop shouldn't raise any exception
+			router.stop();
+
+			//Cleanup
+			router.destroy();
+		});
+
 		QUnit.test("Should parse the hash when initialize doesn't suppress the hash parsing", function(assert) {
 			var router = new sap.ui.core.routing.Router();
 			var parseSpy = this.spy(router, "parse");