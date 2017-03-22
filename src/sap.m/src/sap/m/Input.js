aad107f919e84590020348cd199df8f1d3a2ea64
Marin Parushev <marin.parushev@sap.com>
2017-03-22 17:10:35 +0200

    [INTERNAL] Input: maxSuggestionWidth issue
    - when the property is set the Input has to set correct max-width to its
    Popover
    BCP: 1770092905
    
    Change-Id: If4fe7b6ab63e41ea94469db2eef3a3b1c68e14ac
    CR-Id: 002075125900000935692017

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -443,6 +443,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 		if (this._oList && this._oSuggestionPopup) {
 			if (this.getMaxSuggestionWidth()) {
 				this._oSuggestionPopup.$('cont').css('max-width', this.getMaxSuggestionWidth());
+				this._oSuggestionPopup._maxWidth = this.getMaxSuggestionWidth();
 			} else {
 				this._oSuggestionPopup.setContentWidth((this.$().outerWidth()) + "px");
 			}
@@ -1671,6 +1672,12 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 					oInput._triggerSuggest(sValue);
 					refreshListItems(oInput);
 				}));
+
+			if (oInput._oSuggestionPopup instanceof Popover) {
+				oInput._oSuggestionPopup._getMaxContentWidth = function(oPosParams) {
+					return oInput.getMaxSuggestionWidth() || (oPosParams._fDocumentWidth - oPosParams._fMarginLeft - oPosParams._fMarginRight - oPosParams._fPopoverBorderLeft - oPosParams._fPopoverBorderRight + "px");
+				};
+			}
 
 			oInput._oSuggestionPopup.addStyleClass("sapMInputSuggestionPopup");
 
