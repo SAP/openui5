9f2044e9c7b2b206c01e3b88fafb5821f257573a
Wenqian.Wang <wenqian.wang@sap.com>
2017-03-24 12:18:41 +0100

    [INTERNAL] sap.m.Tree: add accessibility position information to Tree
    Change-Id: Idb3ea1cab4490dc5eaa68b8fe9bd4bfbf58c405d
    (cherry picked from commit 0cd154c8eb99d9d0b0c55ebf72fd1194da2c8d4c)
    CR-Id: 002075125900001006802017

--- src/sap.m/src/sap/m/Tree.js
+++ src/sap.m/src/sap/m/Tree.js
@@ -178,6 +178,20 @@ sap.ui.define(['jquery.sap.global', './ListBase', './TreeItemBase', './library',
 		return sap.ui.getCore().getLibraryResourceBundle("sap.m").getText("ACC_CTR_TYPE_TREE");
 	};
 
+	Tree.prototype.getAccessbilityPosition = function(oItem) {
+		var iSetSize = 0,
+			iPosInset = 0,
+			oNodeContext = oItem.getItemNodeContext();
+
+		iSetSize = oNodeContext.parent.children.length;
+		iPosInset = oNodeContext.positionInParent + 1;
+
+		return {
+			setSize: iSetSize,
+			posInset: iPosInset
+		};
+	};
+
 	return Tree;
 
 }, /* bExport= */ true);
