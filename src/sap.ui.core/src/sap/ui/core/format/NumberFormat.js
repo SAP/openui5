97917d2d3a40885367774ab3694e0462e24ebc04
Jiawei <jiawei.cao@sap.com>
2017-03-31 13:00:25 +0200

    [FIX] core.NumberFormat: adapt decimals when number of integer isn't sufficient
    - when both 'decimals' and 'precision' are set and the number of integer digits
      which is calculated by (precision - decimals) is less than the integer digits
      in the given number, the decimals is still taken and adapted to make sure that
      the integer digits are displayed.
    
    - for example, 100 is formatted with precision 5 and decimal 3. The result
      should be 100.00 to fulfill the precision 5.
    
    Change-Id: Ifef0b4d1dc276648f40acd4ed09dc036c62b1f48
    BCP: 0020751294 0000126334 2017
    CR-Id: 002075125900001232792017

--- src/sap.ui.core/src/sap/ui/core/format/NumberFormat.js
+++ src/sap.ui.core/src/sap/ui/core/format/NumberFormat.js
@@ -381,6 +381,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'sap/ui/core/Locale', 
 		}
 		oFormat.oLocale = oLocale;
 		oFormat.oLocaleData = LocaleData.getInstance(oLocale);
+		oFormat.oOriginalFormatOptions = oFormatOptions;
 
 		// If a pattern is defined in the format option, parse it and add options
 		if (oFormatOptions) {
@@ -557,6 +558,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'sap/ui/core/Locale', 
 			bNegative = oValue < 0,
 			iDotPos = -1,
 			oOptions = jQuery.extend({}, this.oFormatOptions),
+			oOrigOptions = this.oOriginalFormatOptions,
 			aPatternParts,
 			oShortFormat;
 
@@ -579,6 +581,21 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'sap/ui/core/Locale', 
 				if (oOptions.shortDecimals !== undefined) {
 					oOptions.minFractionDigits = oOptions.shortDecimals;
 					oOptions.maxFractionDigits = oOptions.shortDecimals;
+				} else {
+					if (oOrigOptions.minFractionDigits === undefined
+						&& oOrigOptions.maxFractionDigits === undefined
+						&& oOrigOptions.decimals === undefined
+						&& oOrigOptions.precision === undefined
+						&& oOrigOptions.pattern === undefined) {
+						// if none of the options which can affect the decimal digits is set, the default precision is set to 2
+						oOptions.precision = 2;
+					}
+
+					if (oOrigOptions.maxFractionDigits === undefined) {
+						// overwrite the default setting of Integer instance because
+						// Integer with short format could have fraction part
+						oOptions.maxFractionDigits = 99;
+					}
 				}
 				// Always use HALF_AWAY_FROM_ZERO for short formats
 				oOptions.roundingMode = NumberFormat.RoundingMode.HALF_AWAY_FROM_ZERO;
@@ -587,8 +604,13 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/base/Object', 'sap/ui/core/Locale', 
 
 		// Must be done after calculating the short value, as it depends on the value
 		if (oOptions.precision !== undefined) {
-			oOptions.minFractionDigits = 0;
-			oOptions.maxFractionDigits = getDecimals(oValue, oOptions.precision);
+			// the number of decimal digits is calculated using (precision - number of integer digits)
+			// the maxFractionDigits is adapted if the calculated value is smaller than the maxFractionDigits
+			oOptions.maxFractionDigits = Math.min(oOptions.maxFractionDigits, getDecimals(oValue, oOptions.precision));
+
+			// if the minFractionDigits is greater than the maxFractionDigits, adapt the minFractionDigits with
+			// the same value of the maxFractionDigits
+			oOptions.minFractionDigits = Math.min(oOptions.minFractionDigits, oOptions.maxFractionDigits);
 		}
 
 		if (oOptions.type == mNumberType.PERCENT) {
