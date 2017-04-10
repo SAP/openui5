8f64720926c0c6c2714c2efb7a265e75af4b1cb8
Clemens Maier <c.maier@sap.com>
2017-04-10 13:39:36 +0200

    [INTERNAL][FIX] jquery.sap.dom: Selection of Text in Chrome
    - In Chrome 58 and above selection start is set to selection end when the
      first parameter of a setSelectionRange call is negative.
    
    BCP: 1770129134
    
    Change-Id: If4889ae107186a6a8673ad03560390392a6485a0

--- src/sap.ui.core/src/jquery.sap.dom.js
+++ src/sap.ui.core/src/jquery.sap.dom.js
@@ -131,8 +131,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device'],
 		var oDomRef = this.get(0);
 
 		try {
+			// In Chrome 58 and above selection start is set to selection end when the first parameter of a setSelectionRange call is negative.
 			if (typeof (oDomRef.selectionStart) === "number") {
-				oDomRef.setSelectionRange(iStart, iEnd);
+				oDomRef.setSelectionRange(iStart > 0 ? iStart : 0, iEnd);
 			}
 		} catch (e) {
 			// note: some browsers fail to read the "selectionStart" and "selectionEnd" properties from HTMLInputElement, e.g.: The input element's type "number" does not support selection.
