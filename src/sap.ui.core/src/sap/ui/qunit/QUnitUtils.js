86a54d9f19f7ab91c903dc9e3c73f121834ccaee
Peter Muessig <peter.muessig@sap.com>
2017-04-14 13:38:30 +0200

    [FIX] AppCacheBuster: support for async Component preload
    In order to support the asynchronous preloading for Components (via the
    function jQuery.sap._loadJSResourceAsync) the AppCacheBuster has been
    enhanced to override the HTMLScriptElement src property and the
    HTMLLinkElement href property. This replaces overriding the
    includeScript and includeStyleSheet functionality.
    
    Local test enhancement to support Object.getOwnPropertyDescriptor
    for PhantomJS testing. Unfortunately, PhantomJS doesn't overwrite the
    properties for HTMLElements properly due to a bug in PhantomJS and a
    special correction function is required to simulate the property
    definition behavior. Workaround deactivates itself once PhantomJS
    supports Object.defineProperty correctly.
    
    Change-Id: Ief3a27103b57818d80dcf3bd2daee0fdadc0aa76
    BCP: 002075129500001492642017
    Fixes https://github.com/SAP/openui5/issues/1368
    CR-Id: 002075125800002144132017

--- src/sap.ui.core/src/sap/ui/qunit/QUnitUtils.js
+++ src/sap.ui.core/src/sap/ui/qunit/QUnitUtils.js
@@ -57,20 +57,6 @@ sap.ui.define('sap/ui/qunit/QUnitUtils', ['jquery.sap.global', 'sap/ui/Device', 
 
 	}
 
-	// PhantomJS patch for Focus detection via jQuery:
-	// ==> https://code.google.com/p/phantomjs/issues/detail?id=427
-	//     ==> https://github.com/ariya/phantomjs/issues/10427
-	if (Device.browser.phantomJS) {
-		// workaround copied from above bug report
-		var $is = jQuery.fn.is;
-		jQuery.fn.is = function(sSelector) {
-			if (sSelector === ":focus") {
-				return this.get(0) === document.activeElement;
-			}
-			return $is.apply(this, arguments);
-		};
-	}
-
 	// Re-implement jQuery.now to always delegate to Date.now.
 	//
 	// Otherwise, fake timers that are installed after jQuery don't work with jQuery animations
@@ -80,9 +66,22 @@ sap.ui.define('sap/ui/qunit/QUnitUtils', ['jquery.sap.global', 'sap/ui/Device', 
 		return Date.now();
 	};
 
-	// PhantomJS fix for invalid date handling:
-	// ==> https://github.com/ariya/phantomjs/issues/11151
+	// PhantomJS fixes
 	if (Device.browser.phantomJS) {
+
+		// 1.) PhantomJS patch for Focus detection via jQuery:
+		// ==> https://code.google.com/p/phantomjs/issues/detail?id=427
+		//     ==> https://github.com/ariya/phantomjs/issues/10427
+		var $is = jQuery.fn.is;
+		jQuery.fn.is = function(sSelector) {
+			if (sSelector === ":focus") {
+				return this.get(0) === document.activeElement;
+			}
+			return $is.apply(this, arguments);
+		};
+
+		// 2.) PhantomJS fix for invalid date handling:
+		// ==> https://github.com/ariya/phantomjs/issues/11151
 
 		/*eslint-disable */
 		var NativeDate = Date,
@@ -150,6 +149,7 @@ sap.ui.define('sap/ui/qunit/QUnitUtils', ['jquery.sap.global', 'sap/ui/Device', 
 			}
 		});
 		/*eslint-enable */
+
 	}
 
 	/**
