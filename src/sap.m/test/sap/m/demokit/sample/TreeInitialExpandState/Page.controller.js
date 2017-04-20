3351a904454cf59d1105dea04fd1b7c15c73c47d
Wenqian.Wang <wenqian.wang@sap.com>
2017-04-20 17:29:52 +0200

    [INTERNAL] sap.m.Tree: add missing reference json file in explored samples
    Change-Id: I8c7ecb27ab6681d4aa03366b7b650ccc77d9686f
    BCP:1770180907

--- src/sap.m/test/sap/m/demokit/sample/TreeInitialExpandState/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/TreeInitialExpandState/Page.controller.js
@@ -5,7 +5,7 @@ sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel'],
 	var PageController = Controller.extend("sap.m.sample.TreeInitialExpandState.Page", {
 		onInit : function (evt) {
 			// set explored app's demo model on this sample
-			var oModel = new JSONModel("test-resources/sap/m/demokit/sample/Tree/Tree.json");
+			var oModel = new JSONModel("test-resources/sap/m/demokit/sample/TreeInitialExpandState/Tree.json");
 			this.getView().setModel(oModel);
 			this.getView().byId("Tree").expandToLevel(3);
 		}
