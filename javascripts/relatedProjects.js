7b751f7fc669be067250662a88efa44b25438e49
Ayham Tanbari <ayham.tanbari@sap.com>
2017-04-22 01:54:20 +0200

    [FIX]Fixed related projects page
    - Add more css classes to adjust the related projects
    page on tablets and phones devices

--- javascripts/relatedProjects.js
+++ javascripts/relatedProjects.js
@@ -5,11 +5,11 @@ function onLoad() {
 		$.each(oResult, function(sIndex, oEntry){
 			var sRowTemplate = document.createElement("tr");
 			sName  = "<td><a href=" + oEntry.githubLink +">" + oEntry.name + "</a></td>";
-			sType  = "<td>" + oEntry.type + "</td>";
+			sType  = "<td class='phone-cells'>" + oEntry.type + "</td>";
 			sOwner  = "<td><a href=https://github.com/" + oEntry.owner +">" + "@" + oEntry.owner + "</a></td>";
-			sDescription  = "<td>" + oEntry.decription + "</td>";
+			sDescription  = "<td class='phone-cells tablet-cells'>" + oEntry.decription + "</td>";
 			sDocumentationLink  = "<td><a href=" + oEntry.documentationLink + ">" + "Documentation</a></td>";
-			sLicense  = "<td>" + oEntry.license + "</td>";
+			sLicense  = "<td class='phone-cells tablet-cells'>" + oEntry.license + "</td>";
 			sRowTemplate.innerHTML = sName + sOwner + sType + sDescription + sDocumentationLink + sLicense;
 			oTableNode.appendChild(sRowTemplate);
 		});
