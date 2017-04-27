2b391b279926089b6253a52e86e5fea9764b9fb7
Vladislav Iliev <vladislav.iliev@sap.com>
2017-04-27 13:48:13 +0300

    [FIX] sap.m.PlanningCalendar: appointments are correctly positioned when property showRowHeader is set to false
    If showRowHeader is set to false, the header(1st column in the table is not shown).
    This may cause a problem with sap.m.Table when the second column is a popin one, where once column's minScreenWidth
     is reached (i.e. less than defined) the 1st (invisible) column occupies its original space and shifts the second to the right.
    
    The sap.m.Table relies that if there is a popin column, there will be always at least one visible other column. This is according to
    the spec and may be changed with a feature request.
    This change is a temporary legal workaround of the issue unless it is resolved by the sap.m.Table.
    
    (manually cherry picked from Idb7b11063ff7896fd8b26ef0724383f041f57f8e)
    
    BCP: 1770208038
    
    Change-Id: If0415b2aed19c911b991c5116864b378541567de
    CR-Id: 002075125800001674212017

--- src/sap.m/src/sap/m/PlanningCalendar.js
+++ src/sap.m/src/sap/m/PlanningCalendar.js
@@ -314,6 +314,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 		oClass: CalendarOneMonthInterval
 	};
 
+
+	//Defines the minimum screen width for the appointments column (it is a popin column)
+	var APP_COLUMN_MIN_SCREEN_WIDTH = sap.m.ScreenSize.Desktop;
+
 	var CalendarHeader = sap.ui.core.Control.extend("CalendarHeader", {
 
 		metadata : {
@@ -399,7 +403,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 				new sap.m.Column({
 					width: "80%",
 					styleClass: "sapMPlanCalAppRow",
-					minScreenWidth: sap.m.ScreenSize.Desktop,
+					minScreenWidth: APP_COLUMN_MIN_SCREEN_WIDTH,
 					demandPopin: true
 				})
 			],
@@ -993,6 +997,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 
 		var oTable = this.getAggregation("table");
 		oTable.getColumns()[0].setVisible(bShowRowHeaders);
+		this._toggleAppointmentsColumnPopinState(bShowRowHeaders);
 
 		this.$().toggleClass("sapMPlanCalNoHead", !bShowRowHeaders);
 		_positionSelectAllCheckBox.call(this);
@@ -1850,6 +1855,19 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 			oRow = aRows[i];
 			oRow.getCalendarRow().setStartDate(oDate);
 		}
+	};
+
+	/**
+	 * Enables/disables the popin nature of a the appointments column.
+	 * @param {boolean} popinEnabled
+	 * @private
+	 */
+	PlanningCalendar.prototype._toggleAppointmentsColumnPopinState = function(popinEnabled) {
+		var oTable = this.getAggregation("table"),
+			oAppointmentsCol = oTable.getColumns()[1];
+
+		oAppointmentsCol.setDemandPopin(popinEnabled);
+		oAppointmentsCol.setMinScreenWidth(popinEnabled ? APP_COLUMN_MIN_SCREEN_WIDTH : "");
 	};
 
 	function _handleTableSelectionChange(oEvent) {
