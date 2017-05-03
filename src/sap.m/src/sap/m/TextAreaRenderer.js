1264b05a05ef61147567990df2ab700cedfca601
Yavor Ivanov <yavor.ivanov@sap.com>
2017-05-03 15:39:40 +0300

    [FIX] sap.m.TextArea: IE11 new line is doubled.
     This fix is applied on top of 2 other commits:
     2a50566c90e5b7b02329a6f0d83cc8ac1721733a and
     89b02283525d22ea0f6bf6e629a69b6a4f07fb31
    
     These commits deal with an issue in IEs related to how
     IE interprets the new lines in TextArea.
     The first commit applied normalization in all browsers, while
     the other extends it a bit and makes it more specific to IE browsers
     below version 11.
    
     However it seems that the case that falls IE11 and all other non-IE
     browsers is redundant. Those browsers don't need normalization.
     
    BCP: 002028376500002091102017
    
    Change-Id: I2d8256cb656d2a8def9c87ea764916160d36f742

--- src/sap.m/src/sap/m/TextAreaRenderer.js
+++ src/sap.m/src/sap/m/TextAreaRenderer.js
@@ -64,8 +64,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer', './InputBaseRenderer
 		//Normalize the /n and /r to /r/n - Carriage Return and Line Feed
 		if (sap.ui.Device.browser.msie && sap.ui.Device.browser.version < 11) {
 			sValue = sValue.replace(/&#xd;&#xa;|&#xd;|&#xa;/g, "&#13;");
-		} else {
-			sValue = sValue.replace(/&#xd;&#xa;|&#xd;|&#xa;/g, "&#13;&#10;");
 		}
 		oRm.write(sValue);
 	};
