c37fd4aa8d1662ec67413e49883a30e3ff220e0a
Elena Makarenko <elena.makarenko@sap.com>
2017-05-04 15:30:21 +0200

    [INTERNAL] sap.m.UploadCollection: Link instantiation moved to UploadCollectionItem
    Change-Id: Icf239ecc8f136a9e6039d8c113e02820d4cd1ad5

--- src/sap.m/src/sap/m/UploadCollection.js
+++ src/sap.m/src/sap/m/UploadCollection.js
@@ -1611,10 +1611,14 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './Dialog', './library', 'sa
 			if (oFileName) {
 				oFileName.destroy();
 			}
-			oFileName = new sap.m.Link(sItemId + "-ta_filenameHL", {
-				enabled : bEnabled,
-				press : [that, this._triggerLink, this]
-			}).addStyleClass("sapMUCFileName");
+
+			oFileName = oItem._getControl("sap.m.Link", {
+				id: sItemId + "-ta_filenameHL",
+				enabled: bEnabled,
+				press: [that, this._triggerLink, this]
+			});
+
+			oFileName.addStyleClass("sapMUCFileName");
 			oFileName.setModel(oItem.getModel());
 			oFileName.setText(sFileNameLong);
 			return oFileName;
