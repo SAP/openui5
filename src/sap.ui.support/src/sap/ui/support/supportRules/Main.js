17dd645f8c83de932c1ca5afd78eb3ffb6cca66b
Svetoslav Spasov <svetoslav.spasov@sap.com>
2017-05-16 15:57:58 +0300

    [INTERNAL] sap.ui.support: Support Assistant minor fixes
    BCP: n/a
    CR-Id: 002075125900002146712017
    Change-Id: Iace2eb71b9934021f91e39ebfd35b6244ba940ba
    (cherry picked from commit 0f50a9b0d753ecedba9a3ca54f687f8c34e39afe)

--- src/sap.ui.support/src/sap/ui/support/supportRules/Main.js
+++ src/sap.ui.support/src/sap/ui/support/supportRules/Main.js
@@ -671,7 +671,8 @@ function (jQuery, ManagedObject, JSONModel, Analyzer, CoreFacade,
 				}
 			},
 			analysisDuration: this._oAnalyzer.getElapsedTimeString(),
-			analysisDurationTitle: reportConstants.analysisDurationTitle
+			analysisDurationTitle: reportConstants.analysisDurationTitle,
+			name: constants.SUPPORT_ASSISTANT_NAME
 		};
 	};
 
