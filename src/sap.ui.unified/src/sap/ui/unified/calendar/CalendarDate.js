SAP/openui5

commit 8d81a10399bbad55bcd104d6da943eebacef6797

[INTERNAL] Internal switch to CalendarDate for calendar based controls.
There is not built-in JavaScript object/class for just a date. It is only the JavaScript Date object (for short jsdate)
that keeps both date and time information within. Although a date is time and timezone irrelevant (see
link https://en.wikipedia.org/wiki/Calendar_date)

Due to this every time where pure date calculations are needed, the jsdate object, which is usually presented
 in a local timezone is being represented with the same datetime values, but considered as UTC ones.
The idea is to have one object representing dates in order to will simplify the implementation and reduce the
 errors during conversion.

Implementation hints:
- every date passed to an internal method of a behavior class or any method of a renderer is considered as a CalendarDate

- almost all usages of CalendarUtils._createUniversalUTCDate and CalendarUtils._createLocalDate are removed. Alternatively
 CalendarDate.fromLocalJSDate and CalendarDate.prototype.toLocalJSDate are used

- the CalendarDate is used internally only. No standalone usage.

- introduce new utility that works with CalendarDate - CalendarUtils._DATE (for internal use only). The utility compares
 and calculates dates.

- all  implementation files except PlanningCalendar are switched to CalendarDate. The use of UniversalDate is removed.

- PlanningCalendar, DatePicker, CalendarTimeInterval are not switched, as they maintain info for real datetimes. Still, they are modified,
due to the cases where interact interacts with some of the rest (already switched) by use of a private/protected methods.

- Public used date properties and API methods are not changed.

- Classes that are swtihed to the CalendarDate have such comment at the top.

- Classes that are sitched to the CalendarDate remove direct usasge of UniversalDate.

Change-Id: I453e70926220fd97eb89b05f9539abddcd873522

src/sap.ui.unified/src/sap/ui/unified/calendar/CalendarDate.js
@@ -1,0 +1,341 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides class sap.ui.unified.calendar.CalendarDate
+sap.ui.define(['sap/ui/base/Object', 'sap/ui/core/date/UniversalDate'],
+	function (BaseObject, UniversalDate) {
+		"use strict";
+
+
+		/**
+		 * Constructor for CalendarDate
+		 *
+		 * @class
+		 * Lightweight container for calendar date. It is a timezone agnostic and contains an year, month and date.
+		 * The difference between this class and the JavaScript Date object is: <br/>
+		 * <ul>
+		 *     <li>it does not contain the time part</li>
+		 *     <li>year is always considered as full year, i.e. year 10 means 10, not 1910.</li>
+		 * </ul>
+		 *
+		 * The constructor accepts the following parameters:
+		 * <ul>
+		 *     <li>CalendarDate(oCalendarDate) - Creates new CalendarDate by copying it from other CalendarDate</li>
+		 *
+		 *     <li>CalendarDate(oCalendarDate, sCalendarType) - Creates new CalendarDate by copying it from the given
+		 *     CalendarDate, but for the given sCalendarType. For calendar types, check sap.ui.core.CalendarType</li>
+		 *
+		 *     <li>CalendarDate(iYear, iMonth, iDate) - Creates new CalendarDate for the given year, month(0 based) and
+		 *     date. The parameters are considered as Gregorian. Once the CalendarDate is created, getters for year, month
+		 *     and date returns values according to the current (configuration based) calendar.</li>
+		 *
+		 *     <li>CalendarDate(iYear, iMonth, iDate, calendarType) - Creates new CalendarDate for the given year, month
+		 *     (0 based), date and calendar type. The year, month and date parameters are considered as Gregorian. Once
+		 *     the CalendarDate is created, getters for year, month and date returns values according to the given calendar.</li>
+		 * </ul>
+		 *
+		 * This class is used for internal purposes inside calendar related controls. Standalone usage is not supported.
+		 * The class is a wrapper of sap.ui.core.date.UniversalDate, which means that all of the functions are calendar aware.
+		 *
+		 * @private
+		 * @alias sap.ui.unified.calendar.CalendarDate
+		 */
+		var CalendarDate = BaseObject.extend("sap.ui.unified.calendar.CalendarDate", /** @lends sap.ui.unified.calendar.CalendarDate.prototype */ {
+			constructor: function () {
+				var aArgs = arguments,
+					oJSDate, oNow, sCalendarType;
+
+				switch (aArgs.length) {
+					case 0: // defaults to the current date
+						oNow = new Date();
+						return this.constructor(oNow.getFullYear(), oNow.getMonth(), oNow.getDate());
+
+					case 1: // CalendarDate
+					case 2: // CalendarDate, sCalendarType
+						if (!(aArgs[0] instanceof CalendarDate)) {
+							throw "Invalid arguments: the first argument must be of type sap.ui.unified.calendar.CalendarDate.";
+						}
+						sCalendarType = aArgs[1] ? aArgs[1] : aArgs[0]._oUDate.sCalendarType;
+						//Use source.valueOf() (returns the same point of time regardless calendar type) instead of
+						//source's getters to avoid non-gregorian Year, Month and Date may be used to construct a Gregorian date
+						oJSDate = new Date(aArgs[0].valueOf());
+
+						//Make this date really local. Now getters are safe.
+						oJSDate.setFullYear(oJSDate.getUTCFullYear(), oJSDate.getUTCMonth(), oJSDate.getUTCDate());
+						oJSDate.setHours(oJSDate.getUTCHours(), oJSDate.getUTCMinutes(), oJSDate.getUTCSeconds(), oJSDate.getUTCMilliseconds());
+
+						this._oUDate = createUniversalUTCDate(oJSDate, sCalendarType);
+						this._sUDate = this.toString();
+						break;
+
+					case 3: // year, month, date
+					case 4: // year, month, date, sCalendarType
+						checkNumericLike(aArgs[0], "Invalid year: " + aArgs[0]);
+						checkNumericLike(aArgs[1], "Invalid month: " + aArgs[1]);
+						checkNumericLike(aArgs[2], "Invalid date: " + aArgs[2]);
+
+						oJSDate = new Date(0,0,1);
+						oJSDate.setFullYear(aArgs[0], aArgs[1], aArgs[2]); // 2 digits year is not supported. If so, it is considered as full year as well.
+
+						if (aArgs[3]) {
+							sCalendarType = aArgs[3];
+						}
+						this._oUDate = createUniversalUTCDate(oJSDate, sCalendarType);
+						this._sUDate = this.toString();
+						break;
+
+					default:
+						throw "Invalid arguments. Accepted arguments are: 1) oCalendarDate, (optional)calendarType" +
+						"or 2) year, month, date, (optional) calendarType" + aArgs;
+				}
+			}
+		});
+
+		/**
+		 * Retrieves the year.
+		 * @returns {number} the year. Short format for year (2 digits) is not supported. This means that if the current year is 10,
+		 * this method will return 10.
+		 */
+		CalendarDate.prototype.getYear = function () {
+			return this._oUDate.getUTCFullYear();
+		};
+
+		/**
+		 * Sets an year.
+		 * @param {int} year. Short format for year (2 digits) is not supported. This means that if 10 is given, this will
+		 * be considered as year 10, not 1910, as in JS Date.
+		 * @returns {sap.ui.unified.calendar.CalendarDate} <code>this</code> for method chaining.
+		 */
+		CalendarDate.prototype.setYear = function (year) {
+			checkNumericLike(year, "Invalid year: " + year);
+			this._oUDate.setUTCFullYear(year);
+			this._sUDate = this.toString();
+			return this;
+		};
+
+		/**
+		 * Retrieves the month of an year.
+		 * @returns {number} zero based month in the range 0-11.
+		 */
+		CalendarDate.prototype.getMonth = function () {
+			return this._oUDate.getUTCMonth();
+		};
+
+		/**
+		 * Sets the given month as ordinal month of the year.
+		 * @param {int} month An integer between 0 and 11, representing the months January through December( or their
+		 * equivalent month names for the given calendar).
+		 * If the specified value is is outside of the expected range, this method attempts to update the date information
+		 * accordingly. For example, if 12 is given as a month, the year will be incremented by 1, and 1 will be used for month.
+		 * @returns {sap.ui.unified.calendar.CalendarDate} <code>this</code> for method chaining.
+		 */
+		CalendarDate.prototype.setMonth = function (month) {
+			checkNumericLike(month, "Invalid month: " + month);
+			this._oUDate.setUTCMonth(month);
+			this._sUDate = this.toString();
+			return this;
+		};
+
+		/**
+		 * Retrieves the ordinal date of a month.
+		 * @returns {number} date of the month
+		 */
+		CalendarDate.prototype.getDate = function () {
+			return this._oUDate.getUTCDate();
+		};
+
+		/**
+		 * Sets the given date as an ordinal date of the month.
+		 * @param {int} date An integer between certain date range for the current month. If the given date value is outside
+		 * the valid date range for the current month, this method attempts to update the date information accordingly.
+		 * For example, if the month has 30 days and 31 is given as a date, the month will be incremented by 1, and 1 will be used for date.
+		 * Also if 0 is given for a date, the month will be decremented by 1, and the date will be set to the last date of that previous month.
+		 * @returns {sap.ui.unified.calendar.CalendarDate} <code>this</code> for method chaining.
+		 */
+		CalendarDate.prototype.setDate = function (date) {
+			checkNumericLike(date, "Invalid date: " + date);
+			this._oUDate.setUTCDate(date);
+			this._sUDate = this.toString();
+			return this;
+		};
+
+		/**
+		 * Retrieves the day of week.
+		 * @returns {int} the number (0-6) representing the day of week.
+		 */
+		CalendarDate.prototype.getDay = function() {
+			return this._oUDate.getDay();
+		};
+
+		/**
+		 * Retrieves the calendar type associated with this instance;
+		 * @returns {sap.ui.core.CalendarType} the calendar type
+		 */
+		CalendarDate.prototype.getCalendarType = function() {
+			return this._oUDate.sCalendarType;
+		};
+
+		/**
+		 * Compares the current date to the given date.
+		 * @param {sap.ui.unified.calendar.CalendarDate} oCalendarDate to compare with.
+		 * @returns {boolean} true if the current date preceds the given calendar date, false otherwise
+		 */
+		CalendarDate.prototype.isBefore = function (oCalendarDate) {
+			checkCalendarDate(oCalendarDate);
+			return this.valueOf() < oCalendarDate.valueOf();
+		};
+
+		/**
+		 * Compares the current date to the given.
+		 * @param {sap.ui.unified.calendar.CalendarDate} oCalendarDate to compare with.
+		 * @returns {boolean} true if the current date follows the given calendar date, false otherwise.
+		 */
+		CalendarDate.prototype.isAfter = function (oCalendarDate) {
+			checkCalendarDate(oCalendarDate);
+			return this.valueOf() > oCalendarDate.valueOf();
+		};
+
+		/**
+		 * Compares the current date to the given date.
+		 * @param {sap.ui.unified.calendar.CalendarDate} oCalendarDate to compare with.
+		 * @returns {boolean} true if the current date equals or precedes the given calendar date, false otherwise
+		 */
+		CalendarDate.prototype.isSameOrBefore = function (oCalendarDate) {
+			checkCalendarDate(oCalendarDate);
+			return this.valueOf() <= oCalendarDate.valueOf();
+		};
+
+		/**
+		 * Compares the current date to the given.
+		 * @param {sap.ui.unified.calendar.CalendarDate} oCalendarDate to compare with.
+		 * @returns {boolean} true if the current date equals or follows the given calendar date, false otherwise.
+		 */
+		CalendarDate.prototype.isSameOrAfter = function (oCalendarDate) {
+			checkCalendarDate(oCalendarDate);
+			return this.valueOf() >= oCalendarDate.valueOf();
+		};
+
+		/**
+		 * Compares the current date to the given.
+		 * @param {sap.ui.unified.calendar.CalendarDate} oCalendarDate to compare with.
+		 * @returns {boolean} true if the current date equals the given calendar date, false otherwise.
+		 */
+		CalendarDate.prototype.isSame = function (oCalendarDate) {
+			checkCalendarDate(oCalendarDate);
+			return this.valueOf() === oCalendarDate.valueOf();
+		};
+
+		/**
+		 * Converts the underlying sap.ui.unified.calendar.CalendarDate to a JavaScript Gregorian date by setting its time part to zero.
+		 * For example if the date is 2017-01-10, this method will return JSDate object like: 2017-01-10 00:00:00 local time.
+		 * @returns {Date} the JavaScript Date corresponding to the underlying calendar date
+		 */
+		CalendarDate.prototype.toLocalJSDate = function () {
+			// Use this._oUDate.getTime()(returns the same point of time regardless calendar type)  instead of
+			// this._oUDate's getters to avoid non-gregorian Year, Month and Date to be used to construct a Gregorian date
+			var oLocalDate = new Date(this._oUDate.getTime());
+
+			//Make this date really local. Now getters are safe.
+			oLocalDate.setFullYear(oLocalDate.getUTCFullYear(), oLocalDate.getUTCMonth(), oLocalDate.getUTCDate());
+			oLocalDate.setHours(0, 0, 0, 0);
+
+			return oLocalDate;
+		};
+
+		/**
+		 * Converts the underlying sap.ui.unified.calendar.CalendarDate to a JavaScript Gregorian date by setting its time part to zero.
+		 * For example if the date is 2017-01-10, this method will return JSDate object like: 2017-01-10 00:00:00 utc time.
+		 * @returns {Date} the JavaScript Date corresponding to the underlying calendar date
+		 */
+		CalendarDate.prototype.toUTCJSDate = function () {
+			// Use this._oUDate.getTime()(returns the same point of time regardless calendar type)  instead of
+			// this._oUDate's getters to avoid non-gregorian Year, Month and Date to be used to construct a Gregorian date
+			var oUTCDate = new Date(this._oUDate.getTime());
+			oUTCDate.setUTCHours(0, 0, 0, 0);
+
+			return oUTCDate;
+		};
+
+		/**
+		 * Retrieves the string representation of the current instance.
+		 * @returns {string} a string representing the current CalendarDate object.
+		 */
+		CalendarDate.prototype.toString = function () {
+			return this._oUDate.sCalendarType + ": " + this.getYear() + "/" + (this.getMonth() + 1) + "/" + this.getDate();
+		};
+
+		/**
+		 * Retrieves the primitive representation of the object.
+		 * @returns {number} the number representing this object.
+		 */
+		CalendarDate.prototype.valueOf = function () {
+			return this._oUDate.getTime();
+		};
+
+		/**
+		 * Creates an instance from a local date information of a JavaScript Date. Time related information is cut.
+		 * For example, if this method is called with JavaScript Date "2017-12-21 01:00:00 GMT +02:00", the returned result would be "2017-12-21"
+		 * despite that the given JavaScript date corresponds to "2017-12-20 23:00:00 GMT".
+		 * @param {Date} oJSDate a JavaScript date object
+		 * @param {sap.ui.core.CalendarType} [sCalendarType] to be used. If not specified, the calendar type from configuration will be used.
+		 * For more details on the Configuration, please check sap.ui.core.Configuration#getCalendarType
+		 * @returns {sap.ui.unified.calendar.CalendarDate} the calendar date corresponding to the given JavaScript Date.
+		 */
+		CalendarDate.fromLocalJSDate = function (oJSDate, sCalendarType) {
+			if (!(oJSDate instanceof Date)) {
+				throw new Error("Date parameter must be a JavaScript Date object: [" + oJSDate + "].");
+			}
+			return new CalendarDate(oJSDate.getFullYear(), oJSDate.getMonth(), oJSDate.getDate(), sCalendarType);
+		};
+
+		/**
+		 * Creates an UniversalDate corresponding to the given date and calendar type.
+		 * @param {Date} oDate JavaScript date object to create the UniversalDate from. Local date information is used.
+		 * @param {sap.ui.core.CalendarType} [sCalendarType] to be used. If not specified, the calendar type from configuration will be used.
+		 * For more details on the Configuration, please check sap.ui.core.Configuration#getCalendarType
+		 * @returns {sap.ui.core.date.UniversalDate}
+		 */
+		function createUniversalUTCDate(oDate, sCalendarType) {
+			if (sCalendarType) {
+				return UniversalDate.getInstance(createUTCDate(oDate), sCalendarType);
+			} else {
+				return new UniversalDate(createUTCDate(oDate).getTime());
+			}
+		}
+
+		/**
+		 * Creates a JavaScript UTC Date corresponding to the given JavaScript Date.
+		 * @param {Date} oDate JavaScript date object. Time related information is cut.
+		 * @returns {Date} JavaScript date created from the date object, but this time considered as UTC date information.
+		 */
+		function createUTCDate(oDate) {
+			var oUTCDate = new Date(Date.UTC(0, 0, 1));
+
+			oUTCDate.setUTCFullYear(oDate.getFullYear(), oDate.getMonth(), oDate.getDate());
+
+			return oUTCDate;
+		}
+
+		function checkCalendarDate(oCalendarDate) {
+			if (!(oCalendarDate instanceof CalendarDate)) {
+				throw "Invalid calendar date: [" + oCalendarDate + "]. Expected: sap.ui.unified.calendar.CalendarDate";
+			}
+		}
+
+		/**
+		 * Verifies the given value is numeric like, i.e. 3, "3" and throws an error if it is not.
+		 * @param {any} value the value of any type to check. If null or undefined, this method throws an error.
+		 * @param {string} the message to be used if an error is to be thrown
+		 * @throws will throw an error if the value is null or undefined or is not like a number
+		 */
+		function checkNumericLike(value, message) {
+			if (value == undefined || value === Infinity || isNaN(value)) {//checks also for null.
+				throw message;
+			}
+		}
+
+		return CalendarDate;
+
+}, /* bExport= */ true);
+

src/sap.m/src/sap/m/DatePicker.js
@@ -3,8 +3,8 @@
  */
 
 // Provides control sap.m.DatePicker.
-sap.ui.define(['jquery.sap.global', 'sap/ui/Device', './InputBase', 'sap/ui/model/type/Date', 'sap/ui/core/date/UniversalDate', './library', 'sap/ui/unified/calendar/CalendarUtils'],
-	function(jQuery, Device, InputBase, Date1, UniversalDate, library, CalendarUtils) {
+sap.ui.define(['jquery.sap.global', 'sap/ui/Device', './InputBase', 'sap/ui/model/type/Date', 'sap/ui/core/date/UniversalDate', './library'],
+	function(jQuery, Device, InputBase, Date1, UniversalDate, library) {
 	"use strict";
 
 
@@ -1058,10 +1058,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device', './InputBase', 'sap/ui/mode
 	DatePicker.prototype._getVisibleDatesRange = function (oCalendar) {
 		var aVisibleDays = oCalendar._getVisibleDays();
 
-		// Convert from UTC to local Date
+		// Convert to local JavaScript Date
 		return new sap.ui.unified.DateRange({
-			startDate: CalendarUtils._createLocalDate(aVisibleDays[0].oDate), // First visible date
-			endDate: CalendarUtils._createLocalDate(aVisibleDays[aVisibleDays.length - 1].oDate) // Last visible date
+			startDate: aVisibleDays[0].toLocalJSDate(), // First visible date
+			endDate: aVisibleDays[aVisibleDays.length - 1].toLocalJSDate() // Last visible date
 		});
 	};
 

commit 98f0d33f5f07bab374a4dd4336dafa3e9335f9b7

[FIX] sap.ui.unified.calendar.Month off by one day.
This issue is related to wrongly calculated day of week and is
recently introduced by change 8d81a10399bbad55bcd104d6da943eebacef6797

Change-Id: I01095410a3c051032a5f2f646b422a14d6988102
BCP: 1770215589

src/sap.ui.unified/src/sap/ui/unified/calendar/CalendarDate.js
@@ -7,6 +7,10 @@ sap.ui.define(['sap/ui/base/Object', 'sap/ui/core/date/UniversalDate'],
 	function (BaseObject, UniversalDate) {
 		"use strict";
 
+		/*
+		 * All calculations in this class are done by representing the date(year, month, date) as UTC values for an
+		 * internal JavaScript Date object.
+		 */
 
 		/**
 		 * Constructor for CalendarDate
@@ -160,7 +164,7 @@ sap.ui.define(['sap/ui/base/Object', 'sap/ui/core/date/UniversalDate'],
 		 * @returns {int} the number (0-6) representing the day of week.
 		 */
 		CalendarDate.prototype.getDay = function() {
-			return this._oUDate.getDay();
+			return this._oUDate.getUTCDay();
 		};
 
 		/**

src/sap.ui.unified/test/sap/ui/unified/qunit/CalendarDate.qunit.html
@@ -22,31 +22,30 @@
 	<script src="../../../../../resources/sap/ui/qunit/qunit-junit.js"></script>
 	<script src="../../../../../resources/sap/ui/qunit/qunit-coverage.js"></script>
 	<script src="../../../../../resources/sap/ui/qunit/QUnitUtils.js"></script>
+	<script src="../../../../../resources/sap/ui/thirdparty/sinon.js"></script>
+	<script src="../../../../../resources/sap/ui/thirdparty/sinon-qunit.js"></script>
 
 	<!-- Test functions -->
 	<script>
-		jQuery.sap.require("sap.ui.thirdparty.sinon");
-		jQuery.sap.require("sap.ui.thirdparty.sinon-qunit");
 		jQuery.sap.require("sap.ui.unified.calendar.CalendarDate");
 		var CalendarDate = sap.ui.unified.calendar.CalendarDate;
 
 		QUnit.module("Constructor");
 		//check with no parameters
-		QUnit.test("works properly without any parameters", function (assert) {
+		QUnit.test("Without any parameters", function (assert) {
 			//Act
 			var oCurrentJSDate = new Date(),
 					oCalendarDate = new CalendarDate();
 
 			//Assert
-			assert.equal(oCalendarDate.getYear(), oCurrentJSDate.getFullYear(), "getYear() should return the same value as the one given in the constructor");
-			assert.equal(oCalendarDate.getMonth(), oCurrentJSDate.getMonth(), "getMonth() should return the same value as the one given in the constructor");
-			assert.equal(oCalendarDate.getDate(), oCurrentJSDate.getDate(), "getDate() should return the same value as the one given in the constructor");
-			assert.equal(oCalendarDate.getDay(), oCurrentJSDate.getDay(), "getDay() should return the same value as the one given in the constructor");
+			assert.equal(oCalendarDate.getYear(), oCurrentJSDate.getFullYear(), "getYear() should return the current year");
+			assert.equal(oCalendarDate.getMonth(), oCurrentJSDate.getMonth(), "getMonth() should return the current month");
+			assert.equal(oCalendarDate.getDate(), oCurrentJSDate.getDate(), "getDate() should return the current date");
+			assert.equal(oCalendarDate.getDay(), oCurrentJSDate.getDay(), "getDay() should return the current day of week");
 
 		});
 
-		//check with null and undefined as a parameter
-		QUnit.test("single parameter constructor does not accept invalid values", function () {
+		QUnit.test("With 1 parameter(CalendarDate), which does not accept invalid values", function () {
 			//Act
 			var oCalendarDate,
 					oObject = {};
@@ -84,8 +83,7 @@
 
 		});
 
-		//check with a parameter of the same type CalendarDate (passed as a parameter)
-		QUnit.test("works properly with a passed parameter of the same type - CalendarDate", function (assert) {
+		QUnit.test("With 1 parameter(CalendarDate)", function (assert) {
 			//Act
 			var oCurrentCalendarDate = new CalendarDate(),
 					oCalendarDate = new CalendarDate(oCurrentCalendarDate);
@@ -94,15 +92,15 @@
 			assert.notEqual(oCalendarDate.getYear(), undefined, "oCalendarDate.getYear() is not undefined. Some of the next tests may be skipped.");
 
 			if (oCalendarDate.getYear()) {
-				assert.equal(oCalendarDate.getYear(), oCurrentCalendarDate.getYear(), "getYear() should return the same value as the one given in the constructor");
-				assert.equal(oCalendarDate.getMonth(), oCurrentCalendarDate.getMonth(), "getMonth() should return the same value as the one given in the constructor");
-				assert.equal(oCalendarDate.getDate(), oCurrentCalendarDate.getDate(), "getDate() should return the same value as the one given in the constructor");
-				assert.equal(oCalendarDate.getDay(), oCurrentCalendarDate.getDay(), "getDay() should return the same value as the one given in the constructor");
+				assert.equal(oCalendarDate.getYear(), oCurrentCalendarDate.getYear(), "getYear() should return the same value as the CalendarDate given in the constructor");
+				assert.equal(oCalendarDate.getMonth(), oCurrentCalendarDate.getMonth(), "getMonth() should return the same value as the CalendarDate given in the constructor");
+				assert.equal(oCalendarDate.getDate(), oCurrentCalendarDate.getDate(), "getDate() should return the same value as the CalendarDate given in the constructor");
+				assert.equal(oCalendarDate.getDay(), oCurrentCalendarDate.getDay(), "getDay() should return the same value as the one CalendarDate given in the constructor");
 			}
 
 		});
 
-		QUnit.test("works properly with a passed parameter of the same type - CalendarDate and Non-gregorian calendar type", function () {
+		QUnit.test("With 2 parameters(CalendarDate, CalendarType(Non-gregorian))", function () {
 			//Act
 			var oSourceJSDate = new Date(2017, 3, 23),
 					oSourceCalendarDate = new CalendarDate(oSourceJSDate.getFullYear(), oSourceJSDate.getMonth(), oSourceJSDate.getDate(),
@@ -122,30 +120,32 @@
 
 		});
 
-		//check with 2 parameters- one of type CalendarDate (passed as a parameter) and one of type string (Calendar type) (Islamic, Gregorian, Japaneese)
-		QUnit.test("works properly with second parameter - a calendar type", function (assert) {
+		QUnit.test("With 2 parameters(CalendarDate, CalendarType(Gregorian))", function (assert) {
 			//Act
 			var oSourceDate = new CalendarDate(2017, 3, 22),
 					oTargetCalendarDate;
 
-			//Assert
-
+			//Act
 			oTargetCalendarDate = new CalendarDate(oSourceDate, "Gregorian");
+			//Assert
 			assert.notEqual(oTargetCalendarDate.getYear(), undefined, "getYear() is not undefined. Some of the next tests may be skipped.");
 			if (oTargetCalendarDate.getYear()) {
 				assert.equal(oTargetCalendarDate.getYear(), oSourceDate.getYear(), "works for a Gregorian calendar");
 
+				//Act
 				oTargetCalendarDate = new CalendarDate(oSourceDate, "Islamic");
+				//Assert
 				assert.notEqual(oTargetCalendarDate.getYear(), oSourceDate.getYear(), "works for an Islamic calendar");
 
+				//Act
 				oTargetCalendarDate = new CalendarDate(oSourceDate, "Japanese");
+				//Assert
 				assert.notEqual(oTargetCalendarDate.getYear(), oSourceDate.getYear(), "works for a Japanese calendar");
 			}
 		});
 
-		//check with 3 parameters - date, month, year
 		//check year
-		QUnit.test("accepts strings for the year, month and date parameters", function () {
+		QUnit.test("With 3 parameters(year, month, date) as strings. ", function () {
 			//Act
 			var oCalendarDate = new CalendarDate("2017", "0", "1");
 
@@ -156,7 +156,7 @@
 
 		});
 
-		QUnit.test("accepts numbers for the year, month and date parameters", function () {
+		QUnit.test("With 3 parameters(year, month, date) as numbers", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2017, 0, 1);
 
@@ -167,7 +167,7 @@
 
 		});
 
-		QUnit.test("accepts two digit year, which is considered as a full year", function () {
+		QUnit.test("With 3 parameters(year, month, date), where year is with 2 digits, which is considered as a full year", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(17, 0, 1);
 
@@ -176,7 +176,7 @@
 
 		});
 
-		QUnit.test("accepts 0 as a year parameter", function () {
+		QUnit.test("With 3 parameters(year, month, date), where 0 is accepted as a year", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(0, 0, 1);
 
@@ -185,7 +185,7 @@
 
 		});
 
-		QUnit.test("accepts -1 as a year parameter", function () {
+		QUnit.test("With 3 parameters(year, month, date), where -1 is accepted as a year", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(-1, 0, 1);
 
@@ -194,8 +194,7 @@
 
 		});
 
-		//check month
-		QUnit.test("works properly with a value outside month range", function () {
+		QUnit.test("With 3 parameters(year, month, date), where month value outside month range", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2017, 12, 1);
 
@@ -215,8 +214,7 @@
 
 		});
 
-		//check date
-		QUnit.test("works properly with a value outside date range", function () {
+		QUnit.test("With 3 parameters(year, month, date), where date value is outside date range", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2017, 0, 32);
 
@@ -245,7 +243,7 @@
 			assert.equal(oCalendarDate.getMonth(), 4, "getMonth() should return the previous month, when -1 as a date parameter is passed");
 		});
 
-		QUnit.test("works properly in a leap year)", function () {
+		QUnit.test("With 3 parameters(year, month, date), where the year is a leap one)", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2016, 1, 29);
 
@@ -255,7 +253,7 @@
 
 		});
 
-		QUnit.test("works properly with a value outside date range in a leap year", function () {
+		QUnit.test("With 3 parameters(year, month, date), where date value is outside date range and the year is leap one", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2016, 1, 30);
 
@@ -265,7 +263,7 @@
 
 		});
 
-		QUnit.test("works properly with a value outside date range with month and year recalculations", function () {
+		QUnit.test("With 3 parameters(year, month, date), where date value is outside date range with month and year recalculations", function () {
 			//Act
 			var oCalendarDate = new CalendarDate(2017, 11, 32);
 
@@ -276,8 +274,7 @@
 
 		});
 
-		//check with 4 parameters - date, month, year, string (Calendar type) (Islamic, Gregorian, Japaneese)
-		QUnit.test("works properly with a fourth parameter - a calendar type", function (assert) {
+		QUnit.test("With 4 parameters(year, month, date, calendarType)", function (assert) {
 			//Act
 			var oSourceDate = new CalendarDate(2017, 3, 22),
 					oTargetCalendarDate;
@@ -310,6 +307,7 @@
 			assert.equal(oCalendarDate.getYear(), 2000, "setYear() works properly");
 			assert.equal(oCalendarDate.getMonth(), 6, "setMonth() works properly");
 			assert.equal(oCalendarDate.getDate(), 23, "setDate() works properly");
+			assert.equal(oCalendarDate.getDay(), 0, "getDay() works properly");
 
 		});
 