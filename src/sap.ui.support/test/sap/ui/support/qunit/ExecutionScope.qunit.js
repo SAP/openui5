3376a9c474bb89aa9801dc8a76d7e3ff4097728f
Stefan Minchev <stefan.minchev@sap.com>
2017-05-22 13:21:55 +0300

    [INTERNAL] sap.ui.support.qunit.ExecutionScope: qUnit tests added.
    Change-Id: I7430ae9c2fe7688cb5a9627ffa588cb79e8914af

--- src/sap.ui.support/test/sap/ui/support/qunit/ExecutionScope.qunit.js
+++ src/sap.ui.support/test/sap/ui/support/qunit/ExecutionScope.qunit.js
@@ -1,12 +1,12 @@
-/*global QUnit*/
-
+/*global sinon, QUnit*/
 (function () {
 	"use strict";
 
 	jQuery.sap.require("sap/ui/support/supportRules/ExecutionScope");
+	jQuery.sap.require("sap/ui/thirdparty/sinon");
+	jQuery.sap.require("sap/ui/thirdparty/sinon-qunit");
 
 	var core;
-
 	sap.ui.getCore().registerPlugin({
 		startPlugin: function (oCore) {
 			core = oCore;
@@ -90,4 +90,129 @@
 		assert.ok(elements.length >= 4, "atleast 4 elements should be in this context");
 		assert.ok(elements.length < elementsInCore.length, "should be with less elements than global scope");
 	});
+
+
+	QUnit.test("possibleScopes has the correct values", function (assert) {
+		var possibleScopes = sap.ui.support.supportRules.ExecutionScope.possibleScopes;
+
+		assert.equal(possibleScopes.length, 3, " should contain 3 possible scopes");
+		assert.ok(possibleScopes[0] === "global", " global scope is defined");
+		assert.ok(possibleScopes[1] === "subtree", " subtree scope is defined");
+		assert.ok(possibleScopes[2] === "components", " components scope is defined");
+	});
+
+	QUnit.module("GetLoggedObjects test", {
+		setup: function () {
+			this.page = new sap.m.Page({
+				content: [
+					new sap.m.Panel({
+						id: "innerPanel"
+					})
+				]
+			});
+			this.page.placeAt("qunit-fixture");
+			sinon.stub(jQuery.sap.log, 'getLog',function fakeGetLog() {
+				return [
+					{supportInfo: {type: "panel", elementId: "innerPanel"}},
+					{supportInfo: {}},
+					{supportInfo: {elementId: "innerPanel"}},
+					{
+						component: "",
+						date: "2017-05-05",
+						details: "",
+						level: 3,
+						message: "SAP Logger started.",
+						time: "10:42:21.464139"
+					},
+					{
+						component: "innerPanel",
+						date: "2017-05-05",
+						details: "innerPanel",
+						level: 3,
+						message: "SAP Logger started.",
+						time: "10:42:21.464139"
+					}
+				];
+			});
+			this.es = sap.ui.support.supportRules.ExecutionScope(core, {
+				type: "global"
+			});
+			this.filteringFunction = function (logEntry) {
+				if (logEntry.supportInfo.hasOwnProperty("elementId") && logEntry.supportInfo.elementId == "innerPanel") {
+					return true;
+				}
+				return false;
+			};
+		},
+		teardown: function () {
+			this.es = null;
+			this.filteringFunction = null;
+			this.page.destroy();
+			jQuery.sap.log.getLog.restore();
+		}
+	});
+
+	QUnit.test("Called with undefined type", function (assert) {
+		var objectsFromLog = this.es.getLoggedObjects(),
+			allObjectsContainSupportInfo = true;
+
+		assert.equal(objectsFromLog.length, 3, "should contain 3 logs with support info object");
+
+		objectsFromLog.forEach(function (logEntry) {
+			if (!logEntry.hasOwnProperty("supportInfo")) {
+				allObjectsContainSupportInfo = false;
+				return;
+			}
+		});
+
+		assert.ok(allObjectsContainSupportInfo, "all objects has support info object");
+	});
+
+	QUnit.test("Called with specific type", function (assert) {
+		var objectsFromLog = this.es.getLoggedObjects("panel"),
+			loggedObject;
+
+		assert.equal(objectsFromLog.length, 1, "should contain one log object");
+		loggedObject = objectsFromLog[0];
+		assert.ok(loggedObject.hasOwnProperty("supportInfo"), "should contain support info object");
+		assert.ok(loggedObject.supportInfo.hasOwnProperty("type"), "should contain type property");
+		assert.ok(loggedObject.supportInfo.type === "panel","should match type");
+	});
+
+	QUnit.test("Called with custom filtering function", function (assert) {
+		var objectsFromLog = this.es.getLoggedObjects(this.filteringFunction);
+
+		assert.equal(objectsFromLog.length, 2, "should contain one log object");
+
+		for (var i = 0; i < objectsFromLog.length; i++) {
+			assert.ok(objectsFromLog[i].supportInfo.elementId === "innerPanel", "log " + (i + 1) + " should contain correct elementId");
+		}
+	});
+
+	QUnit.test("Containing none valid logs", function (assert) {
+		jQuery.sap.log.getLog.restore();
+		sinon.stub(jQuery.sap.log, 'getLog',function fakeGetLog() {
+			return [
+				{
+					component: "",
+					date: "2017-05-05",
+					details: "",
+					level: 3,
+					message: "SAP Logger started.",
+					time: "10:42:21.464139"
+				},
+				{
+					component: "innerPanel",
+					date: "2017-05-05",
+					details: "innerPanel",
+					level: 3,
+					message: "SAP Logger started.",
+					time: "10:42:21.464139"
+				}
+			];
+		});
+		var objectsFromLog = this.es.getLoggedObjects();
+		assert.equal(objectsFromLog.length, 0, "should be empty");
+	});
+
 }());
