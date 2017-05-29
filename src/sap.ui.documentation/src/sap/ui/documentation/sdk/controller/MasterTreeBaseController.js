d017cf7de6db423b0adf5153cb7a82e2a657d97e
Marin Parushev <marin.parushev@sap.com>
2017-05-29 17:14:21 +0300

    [INTERNAL][FIX] Demokit search results filtering improved
    Change-Id: I45f04ebdc1a3fafdf3d351d0dfd0c37c00d889f9
    JIRA: BGSOFUIPIRIN-1650

--- src/sap.ui.documentation/src/sap/ui/documentation/sdk/controller/MasterTreeBaseController.js
+++ src/sap.ui.documentation/src/sap/ui/documentation/sdk/controller/MasterTreeBaseController.js
@@ -111,7 +111,7 @@ sap.ui.define([
 
 					var aFilters = [];
 					if (sFilterArgument) {
-						var oNameFilter = new Filter("text", FilterOperator.Contains, sFilterArgument);
+						var oNameFilter = new Filter("name", FilterOperator.Contains, sFilterArgument);
 						aFilters.push(oNameFilter);
 					}
 
