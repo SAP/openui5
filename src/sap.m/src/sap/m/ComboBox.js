4cee828dfc666d8a69fdef0dfff212e6704087ac
Martin R. Hristov <martin.r.hristov@sap.com>
2017-06-05 15:24:20 +0300

    [FIX] sap.m.ComboBox: Input value is updated wrong if typing fast
    Change-Id: I669d0c37688b029bfea1785838d7f35a544c6f26
    BCP: 0020751295 0000218689 2017
    CR-Id: 002075125800002095342017

--- src/sap.m/src/sap/m/ComboBox.js
+++ src/sap.m/src/sap/m/ComboBox.js
@@ -446,6 +446,7 @@ sap.ui.define(['jquery.sap.global', './ComboBoxTextField', './ComboBoxBase', './
 
 				var bItemsVisible = !!aVisibleItems.length;
 				var oFirstVisibleItem = aVisibleItems[0]; // first item that matches the value
+				var bDesktopPlatform = sap.ui.Device.system.desktop;
 
 				if (!bEmptyValue && oFirstVisibleItem && oFirstVisibleItem.getEnabled()) {
 
@@ -463,8 +464,12 @@ sap.ui.define(['jquery.sap.global', './ComboBoxTextField', './ComboBoxBase', './
 
 					if (oControl._bDoTypeAhead) {
 
-						// note: timeout required for a Android and Windows Phone bug
-						setTimeout(fnSelectTextIfFocused.bind(oControl, sValue.length, oControl.getValue().length), 0);
+						if (bDesktopPlatform) {
+							fnSelectTextIfFocused.call(oControl, sValue.length, oControl.getValue().length);
+						} else {
+							// timeout required for an Android and Windows Phone bug
+							setTimeout(fnSelectTextIfFocused.bind(oControl, sValue.length, oControl.getValue().length), 0);
+						}
 					}
 				}
 
