fa979b269bb26a3238f58dd25f1c29acb330726f
Annika Rossel <annika.rossel@sap.com>
2017-06-06 09:31:31 +0200

    [FEATURE] Updated theming explored sample
    Change-Id: I1ada1752da463cf9b5974ff553f5f324afe758fd

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/BasicThemeParameters/BasicThemeParameters.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/BasicThemeParameters/BasicThemeParameters.controller.js
@@ -1,64 +1,12 @@
 sap.ui.define([
-		'jquery.sap.global',
-		'sap/ui/core/mvc/Controller',
-		'sap/ui/model/json/JSONModel',
-		'sap/ui/core/theming/Parameters'
-	], function(jQuery, Controller, JSONModel, Parameters) {
+		'sap/m/MessageBox',
+		'sap/ui/core/mvc/Controller'
+	], function(MessageBox, Controller) {
 	"use strict";
 
-	var BasicThemeParametersController = Controller.extend("sap.ui.core.sample.BasicThemeParameters.BasicThemeParameters", {
-
-	// #####################################################
-	//
-	// Entire controller code is only for sample display
-	//
-	// #####################################################
-
-		onInit: function(){
-
-			this.oModel = null;
-
-			// needed for reload of sample on view change
-			this.getView().attachAfterRendering(function(){
-				this.beforeShow();
-			}, this);
-
-			sap.ui.getCore().attachThemeChanged(this._reloadModel, this);
-			this._reloadModel();
-		},
-
-		// style and rename sample display
-		beforeShow: function(evt){
-			var $SampleElems = jQuery('.ParamSample');
-			$SampleElems.each(function(i,e){
-				e.style.background = e.innerHTML;
-				e.style.color = e.innerHTML;
-				e.title = e.innerHTML;
-			});
-		},
-
-		paramFormatter: function(sValue){
-			return "@"+sValue;
-		},
-
-		_reloadModel: function(oEvent){
-			var sPath = jQuery.sap.getModulePath("sap.ui.core.sample.BasicThemeParameters", "/parameters.json");
-			this.oModel = new JSONModel(sPath);
-			this.oModel.attachRequestCompleted(function(){
-				var oDat = this.oModel.getData().params;
-				for (var i in oDat){
-					this.oModel.setProperty("/params/" + i + "/value", Parameters.get(oDat[i].name));
-				}
-				// don`t use theme parameters in models or bind them to your view!!
-				// this is only used here for sample display and should not be implemented in productive apps
-				this.getView().setModel(this.oModel);
-				jQuery.sap.delayedCall(0, this, function(){
-					this.beforeShow();
-				});
-			},this);
-		}
+	var LinkGroupController = Controller.extend("sap.ui.core.sample.BasicThemeParameters.BasicThemeParameters", {
 	});
 
-	return BasicThemeParametersController;
+	return LinkGroupController;
 
 });
