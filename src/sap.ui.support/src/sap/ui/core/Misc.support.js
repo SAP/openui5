30a72bbe331281cfc0b3e9ef94b875fc2f07b491
Stefan Minchev <stefan.minchev@sap.com>
2017-06-07 11:28:09 +0300

    [FEATURE] sap.ui.support.Misc.support: Two additional rules added.
    Two rules description:
    - Checks and report for custom CSS files/styles that
       overwrite standard UI5 control's CSS values
    - Checks and report all overwritten standard control's CSS values
    
    Change-Id: Ic6282f853d61e63293c7d39802e591a5d3db24d9

--- src/sap.ui.support/src/sap/ui/core/Misc.support.js
+++ src/sap.ui.support/src/sap/ui/core/Misc.support.js
@@ -4,8 +4,8 @@
 /**
  * Defines miscellaneous support rules.
  */
-sap.ui.define(["jquery.sap.global", "sap/ui/support/library"],
-	function(jQuery, SupportLib) {
+sap.ui.define(["jquery.sap.global", "sap/ui/support/library", "./CoreHelper.support" ],
+	function(jQuery, SupportLib, CoreHelper) {
 	"use strict";
 
 	// shortcuts
@@ -53,6 +53,121 @@ sap.ui.define(["jquery.sap.global", "sap/ui/support/library"],
 						context: {
 							id: "WEBPAGE"
 						}
+					});
+				}
+			});
+
+			/***
+			 * Checks for custom css files
+			 */
+			oRuleSet.addRule({
+				id: "cssCheckCustomStyles",
+				audiences: [Audiences.Application],
+				categories: [Categories.Consistency],
+				enabled: true,
+				minversion: "1.38",
+				title: "CSS modifications - List of custom styles",
+				description: "Checks and report for custom CSS files/styles that overwrite standard UI5 control's CSS values ",
+				resolution: "Avoid CSS manipulations with custom CSS values as this could lead to rendering issues ",
+				resolutionurls: [{
+					text: 'CSS Styling Issues',
+					href: 'https://openui5.hana.ondemand.com/#docs/guide/9d87f925dfbb4e99b9e2963693aa00ef.html'
+				}, {
+					text: 'General Guidelines',
+					href: 'https://openui5.hana.ondemand.com/#docs/guide/5e08ff90b7434990bcb459513d8c52c4.html'
+				}],
+				check: function (issueManager, oCoreFacade, oScope) {
+					var cssFilesMessage = "Following stylesheet file(s) contain 'custom' CSS that could affects (overwrites) UI5 controls' own styles: \n",
+						externalStyleSheets = CoreHelper.getExternalStyleSheets(),
+						foundIssues = 0;
+
+					externalStyleSheets.forEach(function (styleSheet) {
+						var affectsUI5Controls = false;
+
+						Array.from(styleSheet.rules).forEach(function (rule) {
+							var selector = rule.selectorText,
+								matchedNodes = document.querySelectorAll(selector);
+
+							matchedNodes.forEach(function (node) {
+								var hasUI5Parent = CoreHelper.nodeHasUI5ParentControl(node, oScope);
+								if (hasUI5Parent) {
+									affectsUI5Controls = true;
+								}
+							});
+						});
+
+						if (affectsUI5Controls) {
+							cssFilesMessage += "- " + CoreHelper.getStyleSheetName(styleSheet) + "\n";
+							foundIssues++;
+						}
+					});
+
+					if (foundIssues > 0) {
+						issueManager.addIssue({
+							severity: sap.ui.support.Severity.Medium,
+							details: cssFilesMessage,
+							context: {
+								id: "WEBPAGE"
+							}
+						});
+					}
+				}
+			});
+
+			/***
+			 * Checks for custom styles applied on UI elements
+			 */
+			oRuleSet.addRule({
+				id: "cssCheckCustomStylesThatAffectControls",
+				audiences: [Audiences.Application],
+				categories: [Categories.Consistency],
+				enabled: true,
+				minversion: "1.38",
+				title: "CSS modifications - List of affected controls",
+				description: "Checks and report all overwritten standard control's CSS values ",
+				resolution: "Avoid CSS manipulations with custom CSS values as this could lead to rendering issues ",
+				resolutionurls: [{
+					text: 'CSS Styling Issues',
+					href: 'https://openui5.hana.ondemand.com/#docs/guide/9d87f925dfbb4e99b9e2963693aa00ef.html'
+				}, {
+					text: 'General Guidelines',
+					href: 'https://openui5.hana.ondemand.com/#docs/guide/5e08ff90b7434990bcb459513d8c52c4.html'
+				}],
+				check: function (issueManager, oCoreFacade, oScope) {
+					var controlCustomCssHashMap = {},
+						externalStyleSheets = CoreHelper.getExternalStyleSheets();
+
+					externalStyleSheets.forEach(function (styleSheet) {
+
+						Array.from(styleSheet.rules).forEach(function (rule) {
+							var selector = rule.selectorText,
+								matchedNodes = document.querySelectorAll(selector);
+
+							matchedNodes.forEach(function (node) {
+								var hasUI5Parent = CoreHelper.nodeHasUI5ParentControl(node, oScope);
+								if (hasUI5Parent) {
+									var ui5Control = jQuery(node).control()[0];
+
+									if (!controlCustomCssHashMap.hasOwnProperty(ui5Control.getId())) {
+										controlCustomCssHashMap[ui5Control.getId()] =  "";
+									}
+
+									var cssSource = CoreHelper.getStyleSource(styleSheet);
+									controlCustomCssHashMap[ui5Control.getId()] += "'" + selector + "'" + " from " + cssSource + ",\n";
+								}
+							});
+						});
+					});
+
+					Object.keys(controlCustomCssHashMap).forEach(function(id) {
+						issueManager.addIssue({
+							severity: sap.ui.support.Severity.Low,
+							details: "The following selector(s) " + controlCustomCssHashMap[id] + " affects standard style setting for control",
+							context: {
+								id: id
+							}
+						});
+
 					});
 				}
 			});
