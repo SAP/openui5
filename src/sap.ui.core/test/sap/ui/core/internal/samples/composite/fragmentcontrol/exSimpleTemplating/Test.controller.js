aafb776c639f8f0ab2b15272fa3245d34f2d8f55
Houghton-Larsen <thomas.houghton-larsen@sap.com>
2017-06-12 15:14:56 +0200

    [INTERNAL] fragmentControl examples: cleanup
    Small cleanup
    
    Change-Id: I704c0146470636e8e297638f564d4f08153f4daf

--- src/sap.ui.core/test/sap/ui/core/internal/samples/composite/fragmentcontrol/exSimpleTemplating/Test.controller.js
+++ src/sap.ui.core/test/sap/ui/core/internal/samples/composite/fragmentcontrol/exSimpleTemplating/Test.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller)
 {
 	"use strict";
-	return Controller.extend("sap.ui.core.internal.samples.composite.fragmentcontrol.ex1.Test",
+	return Controller.extend("sap.ui.core.internal.samples.composite.fragmentcontrol.exSimpleTemplating.Test",
 	{
 		handlePress: function(oEvent)
 		{
