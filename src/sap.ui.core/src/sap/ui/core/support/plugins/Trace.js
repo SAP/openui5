04346dad57a19f5926a5164b9e64e39135bc89a9
Diana Pazheva <diana.pazheva@sap.com>
2017-06-19 14:59:41 +0300

    [INTERNAL][FIX] sap.ui.support: Corrected trace output
    Change-Id: I573d68c82bec5c319963a1f59dd0b447c0d9009d

--- src/sap.ui.core/src/sap/ui/core/support/plugins/Trace.js
+++ src/sap.ui.core/src/sap/ui/core/support/plugins/Trace.js
@@ -140,7 +140,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/support/Plugin', "sap/ui/core/f
 				jContentRef.html("");
 				oPlugin._aLogEntries = [];
 			} else if (typeof (oEntry) === "string") {
-				jContentRef.html(oEntry);
+				jContentRef.html(jQuery.sap.encodeHTML(oEntry));
 				jContentRef[0].scrollTop = jContentRef[0].scrollHeight;
 			} else {
 				oEntry._levelInfo = getLevel(oEntry.level);
