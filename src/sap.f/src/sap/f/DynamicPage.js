dacbcd2a58797d144ca3f84e213accbee3515922
Mihail Yordanov <mihail.yordanov@sap.com>
2017-06-20 13:44:39 +0300

    [INTERNAL][FIX] f.DynamicPage header flickering in IE fixed
    BCP: 1780199147
    -added threshold for IE scroll position
    
    Change-Id: I9422829672ec69842234f7e06025d51f8ecb998d
    (cherry picked from commit b912a19cb64095bef955bb7d9d6c0a957c7d69c4)
    CR-Id: 002075125800002507652017

--- src/sap.f/src/sap/f/DynamicPage.js
+++ src/sap.f/src/sap/f/DynamicPage.js
@@ -212,6 +212,7 @@ sap.ui.define([
 		this._bExpandingWithAClick = false;
 		this._bSuppressToggleHeaderOnce = false;
 		this._headerBiggerThanAllowedHeight = false;
+		this._bMSBrowser = Device.browser.internet_explorer || Device.browser.edge || false;
 		this._oScrollHelper = new ScrollEnablement(this, this.getId() + "-content", {
 			horizontal: false,
 			vertical: true
@@ -728,14 +729,21 @@ sap.ui.define([
 
 	/**
 	 * Determines if it's possible for the header to collapse (snap) on scroll.
+	 * <code>Note:</code>
+	 * For IE and Edge we use 1px threshold,
+	 * because the clientHeight returns results in 1px difference compared to the scrollHeight,
+	 * the reason is not defined.
+	 *
 	 * @returns {boolean}
 	 * @private
 	 */
 	DynamicPage.prototype._canSnapHeaderOnScroll = function () {
-		var iMaxScrollPosition = this._getMaxScrollPosition();
+		var iMaxScrollPosition = this._getMaxScrollPosition(),
+			iThreshold = this._bMSBrowser ? 1 : 0;
 
 		if (this._bHeaderInTitleArea) { // when snapping with scroll, the header will be in the content area
 			iMaxScrollPosition += this._getHeaderHeight();
+			iMaxScrollPosition -= iThreshold;
 		}
 		return iMaxScrollPosition > this._getSnappingHeight();
 	};
@@ -767,7 +775,7 @@ sap.ui.define([
 	/**
 	 * Determines if the control would need a <code>ScrollBar</code>.
 	 * <code>Note:</code>
-	 * For IE and Edge we use 1px threshhold,
+	 * For IE and Edge we use 1px threshold,
 	 * because the clientHeight returns results in 1px difference compared to the scrollHeight,
 	 * the reason is not defined.
 	 *
@@ -775,8 +783,7 @@ sap.ui.define([
 	 * @private
 	 */
 	DynamicPage.prototype._needsVerticalScrollBar = function () {
-		var bMSBrowser = Device.browser.internet_explorer || Device.browser.edge || false,
-			iThreshold = bMSBrowser ? 1 : 0;
+		var iThreshold = this._bMSBrowser ? 1 : 0;
 
 		return this._getMaxScrollPosition() > iThreshold;
 	};
