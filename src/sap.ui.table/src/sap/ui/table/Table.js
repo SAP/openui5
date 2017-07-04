4995aa5acb3aae34cd0bdd92e8af5020e2ec2259
Alexander Deutschmann <alexander.deutschmann@sap.com>
2017-07-04 16:34:01 +0200

    [FIX] Table: Correctly update the NoData text
    Change-Id: Id8cd2731cfd2e1bfbf94536f898e21a7dc3bc5ec
    BCP: 1780155383
    CR-Id: 002075125800002900002017

--- src/sap.ui.table/src/sap/ui/table/Table.js
+++ src/sap.ui.table/src/sap/ui/table/Table.js
@@ -1919,6 +1919,8 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 	};
 
 	Table.prototype._onBindingLengthChange = function(sReason) {
+		this._bBindingLengthChanged = true;
+
 		if (sReason === ChangeReason.Refresh) {
 			return;
 		}
@@ -1927,7 +1929,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 		this._updateFixedBottomRows();
 		this._toggleVSb();
 		this._updateVSbRange();
-		this._bBindingLengthChanged = true;
+
 		// show or hide the no data container
 		if (sReason != "skipNoDataUpdate") {
 			// in order to have less UI updates, the NoData text should not be updated when the reason is refresh. When
