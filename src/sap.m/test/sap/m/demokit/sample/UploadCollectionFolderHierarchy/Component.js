d37c3e9dbb509b1eaa1ef62ff87c465dd8630699
Dominic Holzwarth <dominic.holzwarth@sap.com>
2017-07-07 16:08:17 +0200

    [INTERNAL] sap.m.UploadCollection: Explored sample for folder hierarchy added
    Change-Id: Ib122736a26ace69da9fb7b209399e2712b7765fa

---
+++ src/sap.m/test/sap/m/demokit/sample/UploadCollectionFolderHierarchy/Component.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.m.sample.UploadCollectionFolderHierarchy.Component", {
+
+		metadata: {
+			rootView: "sap.m.sample.UploadCollectionFolderHierarchy.Page",
+			dependencies: {
+				libs: ["sap.m", "sap.ui.unified"]
+			},
+			config: {
+				sample: {
+					stretch: true,
+					files: [
+						"Page.view.xml",
+						"Page.controller.js"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+});
