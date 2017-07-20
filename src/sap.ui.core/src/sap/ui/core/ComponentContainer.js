c5fac81998a0252780a7eb084fc1a3fe93b5d146
Jan Mummenthaler <jan.mummenthaler@sap.com>
2017-07-20 15:46:16 +0200

    [FIX] core.ComponentContainer: Propagate all models/listener
    The double propagation in setContainerComponent does not work as 
    all models/listeners are already propagated with the first call. 
    The second call does not detect any change in 'properties' and does
    no propagation at all.
    
    We should propagate everything in one call.
    
    Change-Id: I0051ebcb185c324e5b367059418a33de48ac4008
    CR-Id: 002075125800003372602017
    BCP: 305487

--- src/sap.ui.core/src/sap/ui/core/ComponentContainer.js
+++ src/sap.ui.core/src/sap/ui/core/ComponentContainer.js
@@ -141,8 +141,7 @@ sap.ui.define(['sap/ui/base/ManagedObject', './Control', './Component', './Core'
 			oComponent = oComponentContainer.getComponentInstance();
 			if (oComponent) {
 				oComponent.setContainer(oComponentContainer);
-				oComponentContainer.propagateProperties(); //default model
-				oComponentContainer.propagateProperties(false); //Listener
+				oComponentContainer.propagateProperties(true); //all models/listeners
 			}
 		}
 	}
