3d886aaf7d80be697aa0faa116811ed5424653f3
Joaquin Recio Huertas <joaquin.recio.huertas@sap.com>
2017-07-24 14:15:43 +0200

    [INTERNAL][FIX] sap.ui.fl: Fix FlexController's instance
    Change the code so that it works with an instance of FlexController
    
    Change-Id: I784bf803bd6f6a444f27443682e6b8de9e095410
    CR-Id: 002075125800003439492017
    BCP: 1780270047

--- src/sap.ui.fl/test/sap/ui/fl/qunit/FlexControllerFactory.qunit.js
+++ src/sap.ui.fl/test/sap/ui/fl/qunit/FlexControllerFactory.qunit.js
@@ -1,8 +1,10 @@
 /*globals QUnit*/
 jQuery.sap.require("sap.ui.fl.FlexControllerFactory");
 jQuery.sap.require("sap.ui.fl.FlexController");
+jQuery.sap.require("sap.ui.fl.ChangePersistenceFactory");
+jQuery.sap.require("sap.ui.fl.Utils");
 
-(function (FlexControllerFactory, FlexController) {
+(function (FlexControllerFactory, FlexController, ChangePersistenceFactory, Utils) {
 	'use strict';
 
 	QUnit.module("sap.ui.fl.FlexControllerFactory", {
@@ -34,5 +36,47 @@ jQuery.sap.require("sap.ui.fl.FlexController");
 		assert.strictEqual(oFlexController1, oFlexController2);
 	});
 
-}(sap.ui.fl.FlexControllerFactory, sap.ui.fl.FlexController));
+	QUnit.test("does not propagate if there are no changes for the component", function (assert) {
+		this.stub(ChangePersistenceFactory, "_getChangesForComponentAfterInstantiation").returns(Promise.resolve({}));
+		this.stub(Utils, "isApplication").returns(true);
+
+		var oComponent = {
+			getManifestObject: function () {
+				return {};
+			},
+			addPropagationListener: function () {}
+		};
+
+		var oAddPropagationListenerStub = this.stub(oComponent, "addPropagationListener");
+
+		FlexControllerFactory.getChangesAndPropagate(oComponent, {});
+
+		assert.equal(oAddPropagationListenerStub.callCount, 0, "no propagation was triggered");
+	});
+
+	QUnit.test("does propagate if there are changes for the component", function (assert) {
+
+		var done = assert.async();
+		assert.expect(0); // assert only the addPropagationListener to be called
+
+		var mDeterminedChanges = {
+			"someId": [{}]
+		};
+
+		this.stub(ChangePersistenceFactory, "_getChangesForComponentAfterInstantiation").returns(Promise.resolve(function() {return mDeterminedChanges;}));
+		this.stub(Utils, "isApplication").returns(true);
+
+		var oComponent = {
+			getManifestObject: function () {
+				return {};
+			},
+			addPropagationListener: function () {
+				done();
+			}
+		};
+
+		FlexControllerFactory.getChangesAndPropagate(oComponent, {});
+	});
+
+}(sap.ui.fl.FlexControllerFactory, sap.ui.fl.FlexController, sap.ui.fl.ChangePersistenceFactory, sap.ui.fl.Utils));
 
