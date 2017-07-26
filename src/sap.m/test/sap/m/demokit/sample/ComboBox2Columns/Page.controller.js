d428d6df99389699d26b14b32c52cd33e9b1853e
Yordanka Petkova <yordanka.petkova@sap.com>
2017-07-26 14:05:58 +0300

    [INTERNAL] sap.m.ComboBox: Improved Explored samples
    Added json file countriesCollection to better
    showcase the usage of the control
    
    Change-Id: Ifc629c9bba7642a9393f13c1ba7b7f0a56f91401

--- src/sap.m/test/sap/m/demokit/sample/ComboBox2Columns/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/ComboBox2Columns/Page.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([
 		onInit: function () {
 
 			// set explored app's demo model on this sample
-			var oModel = new JSONModel(jQuery.sap.getModulePath("sap.ui.demo.mock", "/products.json"));
+			var oModel = new JSONModel(jQuery.sap.getModulePath("sap.ui.demo.mock", "/countriesCollection.json"));
 			this.getView().setModel(oModel);
 		}
 	});
