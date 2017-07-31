0bca1bfad4bbdaafc5e9021b21f7416dbce2a245
Kevin Edinger <kevin.edinger@sap.com>
2017-07-31 10:05:05 +0200

    [INTERNAL] sap.ui.layout: HideSimpleForm from Group without Title fix
    Change-Id: I883f6d4a006c9d77c5ce891d077bcebf7a69e975
    Signed-off-by: Kevin Edinger <kevin.edinger@sap.com>
    BCP: 1780325154

--- src/sap.ui.layout/src/sap/ui/layout/form/SimpleForm.designtime.js
+++ src/sap.ui.layout/src/sap/ui/layout/form/SimpleForm.designtime.js
@@ -6,17 +6,6 @@
 sap.ui.define(["sap/ui/fl/changeHandler/ChangeHandlerMediator"], function(ChangeHandlerMediator) {
 	"use strict";
 
-	var fnHasContent = function(oFormContainer) {
-		if (oFormContainer.getTitle()) {
-			return true;
-		} else {
-			var oSimpleForm = oFormContainer.getParent().getParent();
-			return oSimpleForm.getContent().some(function(oControl) {
-				return oControl.getVisible();
-			});
-		}
-	};
-
 	var fnGetStableElements = function(oElement) {
 		var aStableElements = [];
 		var oLabel;
@@ -162,7 +151,7 @@ sap.ui.define(["sap/ui/fl/changeHandler/ChangeHandlerMediator"], function(Change
 				return {
 					changeType : "removeSimpleFormGroup",
 					changeOnRelevantContainer : true,
-					isEnabled : !(!oRemovedElement.getToolbar() && !fnHasContent.call(this, oRemovedElement)),
+					isEnabled : !!(oRemovedElement.getToolbar() || oRemovedElement.getTitle()),
 					getConfirmationText : function(oRemovedElement){
 						var bContent = false;
 						if (oRemovedElement.getMetadata().getName() === "sap.ui.layout.form.FormContainer"
