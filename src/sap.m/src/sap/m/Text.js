5386aa01ab76c2220db57bd2f1ae6567cc5725a7
Todor Nikolov <todor.nikolov@sap.com>
2017-07-31 10:11:39 +0300

    [INTERNAL][FIX] sap.m.Text: line-heigh has been reverted to previous state.
    Change-Id: I4c60cfb72df89540f129a7685d53dcf43d036259

--- src/sap.m/src/sap/m/Text.js
+++ src/sap.m/src/sap/m/Text.js
@@ -3,8 +3,8 @@
  */
 
 // Provides control sap.m.Text
-sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
-	function(jQuery, library, Control) {
+sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/Device'],
+	function(jQuery, library, Control, Device) {
 	"use strict";
 
 	/**
@@ -84,7 +84,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control'],
 	 * @protected
 	 * @type {number}
 	 */
-	Text.prototype.normalLineHeight = 1.4;
+	Text.prototype.normalLineHeight = Device.browser.webkit ? 1 : 1.2;
 
 	/**
 	 * Determines per instance whether line height should be cached or not.
