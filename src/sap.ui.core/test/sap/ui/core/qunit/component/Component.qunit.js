128310a76596f4de1e7681f7454f5ac48b7842f9
Peter Muessig <peter.muessig@sap.com>
2017-07-31 11:13:49 +0200

    [INTERNAL] Component: async loading when using of manifest property
    When using the new manifest property to either define loading the
    manifest first or specify a manifest url or passing the manifest the
    Component load switched by default to async mode.
    
    Change-Id: Ia116d9936e60891b2ff9d5cf7f5d83d37f0aeb91
    CR-Id: 002075125800003991892017
    BCP: n/a

--- src/sap.ui.core/test/sap/ui/core/qunit/component/Component.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/Component.qunit.js
@@ -666,8 +666,7 @@ sap.ui.define([
 		sap.ui.core.Component._fnOnInstanceCreated = undefined;
 
 		return sap.ui.component({
-			manifest: "/anylocation/manifest.json",
-			async: true
+			manifest: "/anylocation/manifest.json"
 		}).then(function(oComponent) {
 			assert.ok(true, "Component is loaded properly!");
 		}, function(oError) {
@@ -683,13 +682,42 @@ sap.ui.define([
 				"sap.app" : {
 					"id" : "samples.components.oneview"
 				}
-			},
-			async: true
+			}
 		}).then(function(oComponent) {
 			assert.equal(oComponent.getManifestObject().getComponentName(), "samples.components.oneview", "The proper component has been loaded!");
 		}, function(oError) {
 			assert.ok(false, "Component should be loaded!");
 		});
+
+	});
+
+	QUnit.test("Usage of manifest property in component configuration for URL (sync)", function(assert) {
+
+		sap.ui.core.Component._fnOnInstanceCreated = undefined;
+
+		var oComponent = sap.ui.component({
+			manifest: "/anylocation/manifest.json",
+			async: false
+		});
+		
+		assert.ok(oComponent instanceof sap.ui.core.UIComponent, "Component is loaded properly!");
+		assert.equal(oComponent.getManifestObject().getComponentName(), "samples.components.button", "The proper component has been loaded!");
+
+	});
+
+	QUnit.test("Usage of manifest property in component configuration for manifest object (sync)", function(assert) {
+
+		var oComponent = sap.ui.component({
+			manifest: {
+				"sap.app" : {
+					"id" : "samples.components.oneview"
+				}
+			},
+			async: false
+		});
+
+		assert.ok(oComponent instanceof sap.ui.core.UIComponent, "Component is loaded properly!");
+		assert.equal(oComponent.getManifestObject().getComponentName(), "samples.components.oneview", "The proper component has been loaded!");
 
 	});
 
