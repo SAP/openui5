caabb5683ad3e56054960e5b7bdfdc9affe8dc67
Dominic Holzwarth <dominic.holzwarth@sap.com>
2017-08-04 14:31:30 +0200

    [FIX] sap.m.UploadCollection: Deletion handling corrected
    - Deletion of bottommost item out of two items no longer breaks file download
    
    Change-Id: I4dbe9587fe9f26feaebfe26f2a80688b111270b8
    BCP: 0020751294 0000374930 2017
    CR-Id: 002075125800003799712017

--- src/sap.m/src/sap/m/UploadCollection.js
+++ src/sap.m/src/sap/m/UploadCollection.js
@@ -2546,11 +2546,12 @@ sap.ui.define(['jquery.sap.global', './MessageBox', './Dialog', './library', 'sa
 			this._oFileUploader.focus();
 		} else {
 			var iLineNumber = this.sDeletedItemId.split("-").pop();
-			//Deleted item is not the last one of the list
-			if ((iLength - 1) >= iLineNumber) {
+			// If the bottommost item has been deleted, its predecessor receives focus.
+			// If any other item has been deleted, its successor receives focus.
+			if (iLineNumber <= iLength - 1)  {
 				sLineId = this.sDeletedItemId + "-cli";
 			} else {
-				sLineId = this.aItems.pop().sId + "-cli";
+				sLineId = this.aItems[this.aItems.length - 1].sId + "-cli";
 			}
 			this._setFocusToLineItem(sLineId);
 		}
