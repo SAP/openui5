SAP/openui5

commit 0c05188bdb57d6d5efc119ced8649e154fcc5e99

[INTERNAL] mvc/XMLView: no error when cache key is missing
Before this change the XMLView made use of jQuery.sap.log.error if a cache
key was not delivered by the according Promise. That made it impossible
for adopters to handle a missing cache key increment silently or in their
own manner. Hence we do only log a debug message now in case a cache key
could not be provided.

Change-Id: I363cb35ab46257392779610ccb26023897df079a

src/sap.ui.core/src/sap/ui/core/mvc/XMLView.js
@@ -7,9 +7,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 	function(jQuery, XMLTemplateProcessor, library, View, ResourceModel, ManagedObject, Control, RenderManager, Cache/* , jQuerySap */) {
 	"use strict";
 
-	// shortcut for enum(s)
+	// actual constants
 	var RenderPrefixes = RenderManager.RenderPrefixes,
-		ViewType = library.mvc.ViewType;
+		ViewType = library.mvc.ViewType,
+		sXMLViewCacheError = "XMLViewCacheError";
 
 
 	/**
@@ -218,9 +219,13 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 		function getCacheInput(oView, mCacheSettings) {
 			var oRootComponent = getRootComponent(oView),
 				sManifest = oRootComponent ? JSON.stringify(oRootComponent.getManifest()) : null,
-				aFutureKeyParts = getCacheKeyPrefixes(oView, oRootComponent);
+				aFutureKeyParts = [];
 
-			aFutureKeyParts = aFutureKeyParts.concat(getVersionInfo(), getCacheKeyProviders(oView), mCacheSettings.keys);
+			aFutureKeyParts = aFutureKeyParts.concat(
+				getCacheKeyPrefixes(oView, oRootComponent),
+				getVersionInfo(), getCacheKeyProviders(oView),
+				mCacheSettings.keys
+			);
 
 			return validateCacheKey(oView, aFutureKeyParts).then(function(sKey) {
 				return {
@@ -241,6 +246,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 					return aKeys.join('_');
 				} else {
 					var e = new Error("Provided cache keys may not be empty or undefined.");
+					e.name = sXMLViewCacheError;
 					return Promise.reject(e);
 				}
 			});
@@ -259,11 +265,13 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 			var mPreprocessors = View._mPreprocessors["XML"],
 				oPreprocessorInfo = oView.getPreprocessorInfo(/*bSync =*/false),
 				aFutureCacheKeys = [];
+
 			function pushFutureKey(o) {
 				if (o.preprocessor.getCacheKey) {
 					aFutureCacheKeys.push(o.preprocessor.getCacheKey(oPreprocessorInfo));
 				}
 			}
+
 			for (var sType in mPreprocessors) {
 				mPreprocessors[sType].forEach(pushFutureKey);
 			}
@@ -373,9 +381,16 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 							return mCacheOutput.xml;
 						}
 					});
-				}).catch(function(e) {
-					jQuery.sap.log.error(e);
-					return processResource(sResourceName);
+				}).catch(function(error) {
+					if (error.name === sXMLViewCacheError) {
+						// no sufficient cache keys, processing can continue
+						jQuery.sap.log.debug(error.message, error.name, "sap.ui.core.mvc.XMLView");
+						jQuery.sap.log.debug("Processing the View without caching.", "sap.ui.core.mvc.XMLView");
+						return processResource(sResourceName);
+					} else {
+						// an unknown error occured and should be exposed
+						return Promise.reject(error);
+					}
 				});
 			}
 

src/sap.ui.core/test/sap/ui/core/qunit/AsyncXMLView.qunit.html
@@ -142,21 +142,34 @@ sap.ui.require(["jquery.sap.global",
 				});
 			});
 
-			QUnit.test("no cache key - sync", function(assert) {
+			QUnit.test("no cache key - sync part", function(assert) {
 				var error = new Error("No cache keys provided. At least one is required.");
 				assert.expect(2);
 				assert.throws(viewFactory.bind(null, "foo"), error, "invalid cache config");
 				assert.throws(viewFactory.bind(null, {keys: []}), error, "empty array");
 			});
 
-			QUnit.test("no cache key - async", function(assert) {
+			QUnit.test("no cache key - async part", function(assert) {
 				var error = new Error("Provided cache keys may not be empty or undefined."),
 					oSpy = this.oSpy,
-					oLogSpy = sinon.spy(jQuery.sap.log, "error");
-				assert.expect(2);
-			  return viewFactory({keys: [Promise.resolve()]}).loaded().then(function(oView) {
-					sinon.assert.calledWithExactly(oLogSpy, error);
+					oLogSpy = sinon.spy(jQuery.sap.log, "debug");
+				
+				assert.expect(3);
+				return viewFactory({keys: [Promise.resolve()]}).loaded().then(function(oView) {
+					sinon.assert.calledWith(oLogSpy, error.message, "XMLViewCacheError", "sap.ui.core.mvc.XMLView");
+					sinon.assert.calledWith(oLogSpy, "Insufficient cache keys, process the view w/o caching", "sap.ui.core.mvc.XMLView");
 					sinon.assert.notCalled(oSpy);
+					oView.destroy();
+				});
+			});
+
+			QUnit.test("cache key error", function(assert) {
+				var error = new Error("Some Error"),
+					oView = viewFactory({keys: [Promise.reject(error)]});
+				
+				assert.expect(1);
+				return oView.loaded().catch(function(_error) {
+					assert.equal(_error, error, "Loaded Promise should reject with the thrown error.");
 					oView.destroy();
 				});
 			});

commit 931d3a1892520141bb208b24029d59c6acc2592b

[INTERNAL][FIX] mvc/XMLView: fixed two issues
Introduced with: 0c05188bdb57d6d5efc119ced8649e154fcc5e99
- sap.ui.getVersionInfo could reject, which needs to be handled internally
- adapted the unit test to check for the new log message

Change-Id: Ieaa3a2d2326d9fc526376c4cb0b6345893823ff9

src/sap.ui.core/src/sap/ui/core/mvc/XMLView.js
@@ -289,6 +289,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/XMLTemplateProcessor', 'sap/ui/
 					});
 				}
 				return sTimestamp;
+			}).catch(function(error) {
+				// Do not populate the cache if the version info could not be retrieved.
+				jQuery.sap.log.warning("sap.ui.getVersionInfo could not be retrieved", "sap.ui.core.mvc.XMLView");
+				jQuery.sap.log.debug(error);
+				return "";
 			});
 		}
 

src/sap.ui.core/test/sap/ui/core/qunit/AsyncXMLView.qunit.html
@@ -157,7 +157,7 @@ sap.ui.require(["jquery.sap.global",
 				assert.expect(3);
 				return viewFactory({keys: [Promise.resolve()]}).loaded().then(function(oView) {
 					sinon.assert.calledWith(oLogSpy, error.message, "XMLViewCacheError", "sap.ui.core.mvc.XMLView");
-					sinon.assert.calledWith(oLogSpy, "Insufficient cache keys, process the view w/o caching", "sap.ui.core.mvc.XMLView");
+					sinon.assert.calledWith(oLogSpy, "Processing the View without caching.", "sap.ui.core.mvc.XMLView");
 					sinon.assert.notCalled(oSpy);
 					oView.destroy();
 				});