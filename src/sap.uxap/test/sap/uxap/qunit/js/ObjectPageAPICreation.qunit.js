aec195b4255307d381c50e316fc05ec3c5f72392
Diana Pazheva <diana.pazheva@sap.com>
2017-08-09 16:14:01 +0300

    [INTERNAL][FIX] sap.uxap.ObjectPageLayout: section selection refactored
    1) added adjustSelectedSectionByUXRules,
       to ensure that the value of selectedSection [that was set from the app] corresponds to a section that is valid (not filtered-out by UXRules)
    
    2) reworked _store/_restore scrollLocation,
       by:
       - removing the old "oStoredSection" (obsolete since we now have "selectedSection" aggregation)
       - but adding instead *_iStoredScrolledSubSectionId*,
         in order to preserve the finer granularity (*SUB*section, rather than section level) that we used to have with *oStoredSection* before this refactoring
    	 (because oStoredSection was of type sectionBASE)
    
    Change-Id: I9d6e193a8d0f26b88b26a7656a4a7f294318d048

--- src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageAPICreation.qunit.js
+++ src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageAPICreation.qunit.js
@@ -415,7 +415,57 @@
 		oPage.attachEvent("onAfterRenderingDOMReady", fnOnDomReady);
 	});
 
-	QUnit.test("select another section after rendering completed", function (assert) {
+	QUnit.test("select another section on before page rendering", function (assert) {
+
+		var oPage = helpers.generateObjectPageWithSubSectionContent(oFactory, 3, 2, true),
+			oSecondSection = oPage.getSections()[1],
+			done = assert.async(),
+			fnOnDomReady = function() {
+				var oExpected = {
+					oSelectedSection: oSecondSection,
+					sSelectedTitle: oSecondSection.getTitle()
+				};
+
+				//check
+				setTimeout(function() {
+					sectionIsSelected(oPage, assert, oExpected);
+
+					oPage.destroy();//cleanup
+					done();
+				}, 0);
+			};
+
+		oPage.addEventDelegate({onBeforeRendering: function() {
+			oPage.setSelectedSection(oSecondSection.getId());
+		}});
+		oPage.attachEvent("onAfterRenderingDOMReady", fnOnDomReady);
+		oPage.placeAt("qunit-fixture");
+	});
+
+	QUnit.test("select another section on after page rendering", function (assert) {
+		var oPage = this.oObjectPage,
+			oSecondSection = oPage.getSections()[1],
+			done = assert.async(),
+			fnOnDomReady = function() {
+				var oExpected = {
+					oSelectedSection: oSecondSection,
+					sSelectedTitle: oSecondSection.getTitle()
+				};
+
+				//check
+				setTimeout(function() {
+					sectionIsSelected(oPage, assert, oExpected);
+					done();
+				}, 0);
+			};
+
+		oPage.addEventDelegate({onAfterRendering: function() {
+			oPage.setSelectedSection(oSecondSection.getId());
+		}});
+		oPage.attachEvent("onAfterRenderingDOMReady", fnOnDomReady);
+	});
+
+	QUnit.test("select another section after dom rendering completed", function (assert) {
 		var oPage = this.oObjectPage,
 			oSecondSection = oPage.getSections()[1],
 			done = assert.async(),
--- src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageState.qunit.js
+++ src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageState.qunit.js
@@ -259,11 +259,12 @@
 		QUnit.test("Hide lower section", function (assert) {
 			//setup
 			var oPage = this.oObjectPage,
-				oSection1 = oPage.getSections()[1],
-				oSection1Subsection1 = oSection1.getSubSections()[1],
+				oSecondSection = oPage.getSections()[1],
+				oSecondSection_secondSubsection = oSecondSection.getSubSections()[1],
 				done = assert.async(),
 				fnOnDomReady = function() {
-					oPage.scrollToSection(oSection1Subsection1.getId(), 0);
+					oPage.detachEvent("onAfterRenderingDOMReady", fnOnDomReady);
+					oPage.scrollToSection(oSecondSection_secondSubsection.getId(), 0);
 
 					setTimeout(function() {
 						sectionIsSelected(oPage, assert, {
@@ -272,10 +273,9 @@
 						});
 
 						// act
-						oSection1.setVisible(false); /* hide subsection */
-						sap.ui.getCore().applyChanges();
+						oSecondSection.setVisible(false); // hide the entire section
 
-						sectionIsSelected(oPage, assert, {
+						sectionIsSelected(oPage, assert, { //selection moved to the first visible section
 							bSnapped: true,
 							iAnchorBarSelectionIndex: 0
 						});
@@ -290,27 +290,34 @@
 		QUnit.test("Hide lower subSection", function (assert) {
 			//setup
 			var oPage = this.oObjectPage,
-				oSection1Subsection1 = oPage.getSections()[1].getSubSections()[1],
+				oSecondSection_secondSubsection = oPage.getSections()[1].getSubSections()[1],
+				bExpectedSnapped = true,
 				done = assert.async(),
 				fnOnDomReady = function() {
-					oPage.scrollToSection(oSection1Subsection1.getId(), 0);
+					oPage.detachEvent("onAfterRenderingDOMReady", fnOnDomReady);
+
+					oPage.scrollToSection(oSecondSection_secondSubsection.getId(), 0);
 
 					setTimeout(function() {
 
 						sectionIsSelected(oPage, assert, {
-							bSnapped: true,
+							bSnapped: bExpectedSnapped,
 							iAnchorBarSelectionIndex: 1
 						});
 
 						// act
-						oSection1Subsection1.setVisible(false); /* hide subsection */
-						sap.ui.getCore().applyChanges();
+						oSecondSection_secondSubsection.setVisible(false); // hide the current subsection
 
-						sectionIsSelected(oPage, assert, {
-							bSnapped: true,
-							iAnchorBarSelectionIndex: 1
-						});
-						done();
+						if (oPage.getUseIconTabBar()) {
+							bExpectedSnapped = false; /* only one subsection remained => we are on top of the section => in iconTabBar no need to snap */
+						}
+						setTimeout(function() {
+							sectionIsSelected(oPage, assert, {
+								bSnapped: bExpectedSnapped,
+								iAnchorBarSelectionIndex: 1
+							});
+							done();
+						}, 0);
 					}, 0); //scroll delay
 				};
 			oPage.attachEvent("onAfterRenderingDOMReady", fnOnDomReady);
