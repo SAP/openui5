aed93cb3c5237f0ac9f9cb27607090fbcf9ae4e4
Alexander Deutschmann <alexander.deutschmann@sap.com>
2017-08-11 12:41:44 +0200

    [INTERNAL][FIX] Table: Header column widths in IE11 (11.0.9600.18738IS)
    Change-Id: I59883f680e1c1757f8164f03175e72c56ae1cbda
    BCP: 1780343705
    CR-Id: 002075125800003986662017

--- src/sap.ui.table/src/sap/ui/table/Table.js
+++ src/sap.ui.table/src/sap/ui/table/Table.js
@@ -964,7 +964,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 		if (aHeaderElements) {
 			var aColumns = this.getColumns();
 			for (var i = 0; i < aHeaderElements.length; i++) {
-				var iHeaderWidth = aHeaderElements[i].getBoundingClientRect().width;
+				var iHeaderWidth = parseInt(window.getComputedStyle(aHeaderElements[i]).width, 10);
 				aHeaderWidths.push(iHeaderWidth);
 
 				if (i < aColumns.length && aColumns[i] && !aColumns[i].getVisible()) {
