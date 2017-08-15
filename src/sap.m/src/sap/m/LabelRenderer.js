1de53ff145d3d6060ade8fc6f11d4f2afb45a6ae
Todor Nikolov <todor.nikolov@sap.com>
2017-08-15 10:21:50 +0300

    [INTERNAL] sap.m.Label: Unicode renderer has been reverted
    Change-Id: I73404ee0de129e92fb588c45ffae0685c19c108b

--- src/sap.m/src/sap/m/LabelRenderer.js
+++ src/sap.m/src/sap/m/LabelRenderer.js
@@ -29,9 +29,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer'],
 			sWidth = oLabel.getWidth(),
 			sLabelText = oLabel.getText(),
 			sTooltip = oLabel.getTooltip_AsString(),
-			// render bdi tag only if the browser is different from IE and Edge since it is not supported there
-			bIE_Edge = sap.ui.Device.browser.internet_explorer || sap.ui.Device.browser.edge,
-			bRenderBDI = (sTextDir === sap.ui.core.TextDirection.Inherit) && !bIE_Edge,
 			bDisplayOnly = oLabel.isDisplayOnly();
 
 		// write the HTML into the render managerr
@@ -95,14 +92,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Renderer'],
 
 		// write the label text
 		if (sLabelText) {
-			if (bRenderBDI) {
-				//TODO: To be removed after change completion of BLI incident #1770022720
-				rm.write('<bdi>');
-				rm.writeEscaped(sLabelText);
-				rm.write('</bdi>');
-			} else {
-				rm.writeEscaped(sLabelText);
-			}
+			rm.writeEscaped(sLabelText);
 		}
 		rm.write("</label>");
 	};
