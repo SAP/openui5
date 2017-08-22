630a90a035e55a966dfef4b383c1641e4dfbdfcd
Nikolay Cholakov <nikolay.cholakov@sap.com>
2017-08-22 13:25:18 +0300

    [INTERNAL][FIX] sap.m.TimePicker, sap.m.StepInput: Visual tests are now adapted for mobile execution
    - TimePicker.spec.js now has 'prepare environment' test which forms the correct element ids depending on the test environment (mobile or desktop)
    - StepInput.spec.js now has 'prepare environment' test which focuses the SUT making it possible a screen shot of it to be taken.
    
    Change-Id: I5a0697c7fadb5a435c1ea4ddb8ded3e50494158c

--- src/sap.m/test/sap/m/visual/TimePicker.spec.js
+++ src/sap.m/test/sap/m/visual/TimePicker.spec.js
@@ -1,18 +1,45 @@
-/*global describe,it,element,by,takeScreenshot,expect*/
+/*global describe,it,element,by,takeScreenshot,expect,browser*/
 
 describe("sap.m.TimePicker", function() {
 	"use strict";
 
+	var sSutId = "__picker0",
+		sSutIconClass = ".sapMInputValHelpInner",
+		sSutPickerId,
+		sSutPickerSliderId = sSutId + "-sliders-listSecs";
+
+	it("Prepare environment", function () {
+		var _prepareDesktopEnvironment = function () {
+			sSutPickerId = sSutId + "-RP-popover";
+			sSutPickerSliderId = sSutId + "-sliders-listSecs";
+		};
+		var _prepareMobileEnvironment = function () {
+			sSutPickerId = sSutId + "-RP-dialog";
+			sSutPickerSliderId = sSutId + "-sliders-listSecs";
+		};
+
+		browser.executeScript(function () {
+			return sap.ui.Device.system.phone;
+		}).then(function (response) {
+			var oResponse = JSON.parse(response);
+			if (oResponse && oResponse === true) {
+				_prepareMobileEnvironment();
+			} else {
+				_prepareDesktopEnvironment();
+			}
+		});
+	});
+
 	it("correct margins", function () {
-		var oTPValueHelp = element(by.css('#__picker0 .sapMInputValHelpInner')),
+		var oTPValueHelp = element(by.css('#' + sSutId + ' ' + sSutIconClass)),
 			oPicker,
 			oSecondsSlider;
 
 		//Open picker
 		oTPValueHelp.click();
 
-		oPicker = element(by.id("__picker0-RP-popover"));
-		oSecondsSlider = element(by.id("__picker0-sliders-listSecs"));
+		oPicker = element(by.id(sSutPickerId));
+		oSecondsSlider = element(by.id(sSutPickerSliderId));
 
 		//Assert
 		expect(takeScreenshot(oPicker)).toLookAs("minutes_slider");
