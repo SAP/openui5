03cc07fdcff3f37beb99e38c6d2ac221c2726562
Elena Makarenko <elena.makarenko@sap.com>
2017-08-23 16:03:21 +0200

    [INTERNAL] sap.m.Popover: Explored samples improved
    * removed redundant delay when opening a popover
    
    Change-Id: Ife18369bdbc864caeab42980da405d30a73cd3bf

--- src/sap.m/test/sap/m/demokit/sample/PopoverNavCon/C.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/PopoverNavCon/C.controller.js
@@ -22,11 +22,7 @@ sap.ui.define([
 				this.getView().addDependent(this._oPopover);
 			}
 
-			// delay because addDependent will do a async rerendering and the popover will immediately close without it
-			var oButton = oEvent.getSource();
-			jQuery.sap.delayedCall(0, this, function () {
-				this._oPopover.openBy(oButton);
-			});
+			this._oPopover.openBy(oEvent.getSource());
 		},
 
 		onNavToProduct : function (oEvent) {
