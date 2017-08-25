2bc43229f60c10f04b1307cfe809b391efbf14d2
Ayham Tanbari <ayham.tanbari@sap.com>
2017-08-25 17:28:34 +0200

    [INTERNAL] sap.m.demo.basicTemplate: Add a formatter reference
    The App controller should have a reference to the formatter
    
    Change-Id: I18fe16c8c9557241addfc365e2d1fb745dd2c80d

--- src/sap.m/test/sap/m/demokit/basicTemplate/webapp/controller/App.controller.js
+++ src/sap.m/test/sap/m/demokit/basicTemplate/webapp/controller/App.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/demo/basicTemplate/model/formatter"
+], function(Controller, formatter) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+
+		formatter: formatter,
 
 		onInit: function () {
 
