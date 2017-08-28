b1f483f9235fb6564e800053dad6caf7551580c8
Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
2017-08-28 17:48:06 +0200

    [FIX] Flexibility: ObjectPageLayoutSection not movable to header
    Change-Id: I4249c2be8a14a75378c0216e795b673def94b60a
    Signed-off-by: Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>

--- src/sap.uxap/test/sap/uxap/qunit/dt/ObjectPageLayout.qunit.js
+++ src/sap.uxap/test/sap/uxap/qunit/dt/ObjectPageLayout.qunit.js
@@ -1,9 +1,27 @@
+/*global QUnit*/
+
 (function () {
 	"use strict";
 
 	sap.ui.require([
-		"sap/ui/rta/test/controlEnablingCheck"
-	], function (rtaControlEnablingCheck) {
+		"sap/ui/rta/test/controlEnablingCheck",
+		'sap/uxap/ObjectPageLayout',
+		'sap/uxap/ObjectPageSection',
+		'sap/ui/layout/HorizontalLayout',
+		'sap/ui/dt/DesignTime',
+		'sap/ui/rta/command/CommandFactory',
+		'sap/ui/rta/plugin/DragDrop',
+		'sap/ui/dt/OverlayRegistry'
+	], function (
+		rtaControlEnablingCheck,
+		ObjectPageLayout,
+		ObjectPageSection,
+		HorizontalLayout,
+		DesignTime,
+		CommandFactory,
+		DragDropPlugin,
+		OverlayRegistry
+	) {
 
 		var fnConfirmGroupelement1IsOn2ndPosition = function(oAppComponent, oViewAfterAction, assert) {
 			assert.strictEqual( oViewAfterAction.byId("btn1").getId(),       // Id of element at first position in original view
@@ -66,6 +84,129 @@
 			afterAction : fnConfirmGroupelement1IsOn2ndPosition,
 			afterUndo : fnConfirmGroupelement1IsOn1stPosition,
 			afterRedo : fnConfirmGroupelement1IsOn2ndPosition
+		});
+
+		var fnConfirmSection1IsOn2ndPosition = function(oAppComponent, oViewAfterAction, assert) {
+			assert.strictEqual( oViewAfterAction.byId("section").getId(),       // Id of element at first position in original view
+				oViewAfterAction.byId("layout").getSections()[1].getId(),   // Id of second element in group after change has been applied
+				"then the section has been moved to the right position");
+		};
+		var fnConfirmSection1IsOn1stPosition = function(oAppComponent, oViewAfterAction, assert) {
+			assert.strictEqual( oViewAfterAction.byId("section").getId(),       // Id of element at first position in original view
+				oViewAfterAction.byId("layout").getSections()[0].getId(),   // Id of second element in group after change has been applied
+				"then the section has been moved to the previous position");
+		};
+
+		// Check moving sections
+		rtaControlEnablingCheck("Checking the move action for a sap.uxap.ObjectPageSection control", {
+			xmlView :
+			'<mvc:View xmlns:mvc="sap.ui.core.mvc" ' +
+			'xmlns:m="sap.m" xmlns:uxap="sap.uxap" >' +
+				'<uxap:ObjectPageLayout id="layout">' +
+					'<uxap:sections>' +
+						'<uxap:ObjectPageSection id="section">' +
+							'<uxap:subSections>' +
+								'<uxap:ObjectPageSubSection id="subSection" title="Subsection with button">' +
+									'<m:Button text="Button" />' +
+								'</uxap:ObjectPageSubSection>' +
+								'<uxap:ObjectPageSubSection id="subSection1" title="Subsection empty">' +
+								'</uxap:ObjectPageSubSection>' +
+							'</uxap:subSections>' +
+						'</uxap:ObjectPageSection>' +
+						'<uxap:ObjectPageSection id="section2">' +
+							'<uxap:subSections>' +
+								'<uxap:ObjectPageSubSection id="subSection3" title="Subsection3 with button">' +
+									'<m:Button text="Button2" />' +
+								'</uxap:ObjectPageSubSection>' +
+								'<uxap:ObjectPageSubSection id="subSection4" title="Subsection4 empty">' +
+								'</uxap:ObjectPageSubSection>' +
+							'</uxap:subSections>' +
+						'</uxap:ObjectPageSection>' +
+					'</uxap:sections>' +
+				'</uxap:ObjectPageLayout>' +
+			'</mvc:View>',
+			action : {
+				name : "move",
+				controlId : "section",
+				parameter : function(oView){
+					return {
+						movedElements : [{
+							element : oView.byId("section"),
+							sourceIndex : 0,
+							targetIndex : 1
+						}],
+						source : {
+							aggregation: "sections",
+							parent: oView.byId("layout")
+						},
+						target : {
+							aggregation: "sections",
+							parent: oView.byId("layout")
+						}
+					};
+				}
+			},
+			layer : "VENDOR",
+			afterAction : fnConfirmSection1IsOn2ndPosition,
+			afterUndo : fnConfirmSection1IsOn1stPosition,
+			afterRedo : fnConfirmSection1IsOn2ndPosition
+		});
+
+		QUnit.module("Given ObjectPageLayout with Sections and headerContent,", {
+			beforeEach : function(assert) {
+
+				// ObjectPageLayout
+				//    headerContent
+				//    sections
+				//        ObjectPageSection
+
+				this.oObjectPageSection = new ObjectPageSection("movedSection");
+				this.oHorizontalLayout = new HorizontalLayout("horizontalLayout");
+				this.oObjectPageLayout = new ObjectPageLayout("layout", {
+					headerContent : [this.oHorizontalLayout],
+					sections : [this.oObjectPageSection]
+				});
+
+				var oCommandFactory = new CommandFactory();
+				this.oDragDropPlugin = new DragDropPlugin({
+					commandFactory : oCommandFactory
+				});
+
+				this.oObjectPageLayout.placeAt("content");
+				sap.ui.getCore().applyChanges();
+
+				// create designtime
+				this.oDesignTime = new DesignTime({
+					rootElements: [
+						this.oObjectPageLayout
+					],
+					plugins: [this.oDragDropPlugin]
+				});
+
+				var done = assert.async();
+				this.oDesignTime.attachEventOnce("synced", function() {
+
+					this.oMovedSectionOverlay = OverlayRegistry.getOverlay(this.oObjectPageSection);
+					this.oHeaderContentAggregationOverlay = OverlayRegistry.getOverlay(this.oObjectPageLayout).getAggregationOverlay("headerContent");
+					this.oElementMover = this.oDragDropPlugin.getElementMover();
+					this.oElementMover.setMovedOverlay(this.oMovedSectionOverlay);
+
+					done();
+				}.bind(this));
+
+			},
+			afterEach : function(assert) {
+				this.oMovedSectionOverlay .destroy();
+				this.oHeaderContentAggregationOverlay.destroy();
+				this.oDesignTime.destroy();
+				this.oObjectPageLayout.destroy();
+			}
+		});
+
+		QUnit.test("when DT is loaded and trying to move the section into the headerContent...", function(assert) {
+			this.oElementMover.setMovedOverlay(this.oMovedSectionOverlay);
+			assert.notOk(this.oElementMover.checkTargetZone(this.oHeaderContentAggregationOverlay),
+				"then the headerContent aggregation is not a possible target zone");
 		});
 	});
 })();
