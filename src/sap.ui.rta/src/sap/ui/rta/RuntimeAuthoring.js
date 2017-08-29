95848291f67ec84dd4ab15205b40bf19c4c351e1
Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
2017-08-29 10:55:20 +0200

    [FIX] sap.ui.rta: Personalization reload not part of navigation history
    CR-Id: 002075125900002837542017
    BCP: 1770338469
    
    Change-Id: Idba281e573e6a4149d38edae3ec61ec5e2365a2a
    Signed-off-by: Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
    (cherry picked from commit 28b5cb88ac61881a25d7d547c330768f5efc9e7c)

--- src/sap.ui.rta/src/sap/ui/rta/RuntimeAuthoring.js
+++ src/sap.ui.rta/src/sap/ui/rta/RuntimeAuthoring.js
@@ -1539,10 +1539,10 @@ sap.ui.define([
 	};
 
 	/**
-	 * Builds the navigation arguments object required to trigger the navigation
-	 * using the CrossApplicationNavigation ushell service
-	 * @param  {map} mParsedHash The parsed URL hash
-	 * @return {map}             The argument map ("oArg" parameter of the "toExternal" function)
+	 * Build the navigation arguments object required to trigger the navigation
+	 * using the CrossApplicationNavigation ushell service.
+	 * @param  {Object} mParsedHash Parsed URL hash
+	 * @return {Object}             Returns argument map ("oArg" parameter of the "toExternal" function)
 	 */
 	RuntimeAuthoring.prototype._buildNavigationArguments = function(mParsedHash){
 		return {
@@ -1552,7 +1552,8 @@ sap.ui.define([
 				context : mParsedHash.contextRaw
 			},
 			params: mParsedHash.params,
-			appSpecificRoute : mParsedHash.appSpecificRoute
+			appSpecificRoute : mParsedHash.appSpecificRoute,
+			writeHistory : false
 		};
 	};
 
