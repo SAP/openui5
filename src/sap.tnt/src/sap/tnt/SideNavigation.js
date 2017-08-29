fa93f467c4af94e144c24ce5642b14430b9e890b
Nikolay Vankov <nikolay.vankov@sap.com>
2017-08-29 13:16:08 +0300

    [INTERNAL] sap.tnt.SideNavigation: Minor style changes
    Change-Id: I15468d88040bc1da0c771eb075cc44a5f1790085

--- src/sap.tnt/src/sap/tnt/SideNavigation.js
+++ src/sap.tnt/src/sap/tnt/SideNavigation.js
@@ -144,14 +144,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			}
 
 			that._hasActiveAnimation = true;
-
-			var isCompact = $this.parents('.sapUiSizeCompact').length > 0;
-
-			if (isCompact) {
-				width = isExpanded ? '15rem' : '2rem';
-			} else {
-				width = isExpanded ? '15rem' : '3rem';
-			}
+			width = isExpanded ? '15rem' : '3rem';
 
 			$this.animate({
 					width: width
