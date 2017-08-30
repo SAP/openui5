071a96761f490aef783f0a9606a1fa717dcddedb
Alexander Deutschmann <alexander.deutschmann@sap.com>
2017-08-30 12:03:17 +0200

    [INTERNAL][FIX] Table: Always update bindingLength cache on dataReceived
    - The table should know the total row count (binding length) as early as
    possible, therefore the internally cached value of the binding length is
    updated synchronously on every dataReceived event of the binding.
    - The binding event listeners of the table are registered before any
    other event listeners.
    - Add unit tests for bindRows legacy API.
    
    Change-Id: I08d7cbde29048022f4c162042c30e42977a91453
    BCP: 1770318675
    CR-Id: 002075125900002508532017

--- src/sap.ui.table/src/sap/ui/table/Table.js
+++ src/sap.ui.table/src/sap/ui/table/Table.js
@@ -1724,8 +1724,8 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 			// Wrap the event handler of the other party to add our handler.
 			var fOriginalHandler = oBindingInfo.events[sEventName];
 			oBindingInfo.events[sEventName] = function() {
-				fOriginalHandler.apply(this, arguments);
 				fHandler.apply(this, arguments);
+				fOriginalHandler.apply(this, arguments);
 			};
 		}
 	};
@@ -3998,6 +3998,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 	Table.prototype._onBindingDataReceivedListener = function (oEvent) {
 		if (oEvent.getSource() == this.getBinding("rows") && !oEvent.getParameter("__simulateAsyncAnalyticalBinding")) {
 			this._bPendingRequest = false;
+			this._updateBindingLength(true);
 
 			if (this._dataReceivedHandlerId != null) {
 				jQuery.sap.clearDelayedCall(this._dataReceivedHandlerId);
@@ -4013,7 +4014,6 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device',
 				if (this.getEnableBusyIndicator()) {
 					this.setBusy(false);
 				}
-				this._updateBindingLength(true);
 				this._updateNoData();
 				delete this._dataReceivedHandlerId;
 			});
