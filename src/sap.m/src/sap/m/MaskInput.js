ac6ba6ee994ed8393b4abbfad40a5838da28f12f
Vladislav Iliev <vladislav.iliev@sap.com>
2017-08-30 15:06:58 +0300

    [FIX] sap.m.MaskInput-on Firefox if the cursor is at the beginning of the field, left arrow does nothing.
    When at the beginning of a field the left arrow(right arrow in RTL) should not move the cursor position.
    (manually cherry picked from 80408d8ab042787dc0d2cf7eb230d0da27a32411).
    
    Change-Id: I120784758e63c99567a7600169430fb25b7b6a34
    BCP: 1770335684
    CR-Id: 002075125900002511622017

--- src/sap.m/src/sap/m/MaskInput.js
+++ src/sap.m/src/sap/m/MaskInput.js
@@ -610,14 +610,11 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInputRule', 'sap/ui/co
 
 	/**
 	 * Places the cursor on a given position (zero based).
-	 * @param {int} iPos The position the cursor to be placed on
+	 * @param {int} iPos The position the cursor to be placed on. If negative value is given, 0 is considered.
 	 * @private
 	 */
 	MaskInput.prototype._setCursorPosition = function (iPos) {
-		if (sap.ui.Device.browser.webkit && iPos < 0) {
-			/* For webkit browsers version >=58.0, negative value position the carret at the end of the string.
-			/* In previous versions the outcome was the same as if position is 0 => at the beginning of the string.
-			 */
+		if (iPos < 0) {
 			iPos = 0;
 		}
 		return jQuery(this.getFocusDomRef()).cursorPos(iPos);
