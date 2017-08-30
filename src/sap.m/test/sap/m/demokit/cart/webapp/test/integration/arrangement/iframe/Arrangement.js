d566a82b958c465502a9be9591a25f0c51f55209
Arnd vom Hofe <a.vom.hofe@sap.com>
2017-08-30 16:53:25 +0200

    [INTERNAL][FIX] sap.m.demo.cart - clear localStorage in OPA tests
    To avoid side effects when running several OPA tests the local storage
     is cleared in every "I start my App"-Call
     - Remove just the cart entries from the local storage
     - Remove double test in the delete journey
    
    Change-Id: I619904106dfab57b1ec23823f2960e20b4d895ab

--- src/sap.m/test/sap/m/demokit/cart/webapp/test/integration/arrangement/iframe/Arrangement.js
+++ src/sap.m/test/sap/m/demokit/cart/webapp/test/integration/arrangement/iframe/Arrangement.js
@@ -1,8 +1,16 @@
-sap.ui.define(['sap/ui/test/Opa5'], function (Opa5) {
+sap.ui.define([
+	'sap/ui/test/Opa5'
+], function (Opa5) {
 	"use strict";
 
 	return Opa5.extend("sap.ui.demo.cart.test.arrangement.DeleteProductJourneyArrangement", {
-		iStartMyApp : function (sAdditionalUrlParameters) {
+		iStartMyApp : function (bKeepStorage, sAdditionalUrlParameters) {
+			// The cart local storage should be deleted when the app starts except when testing it.
+			if (!bKeepStorage) {
+				jQuery.sap.require("jquery.sap.storage");
+				var oLocalStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+				oLocalStorage.remove("SHOPPING_CART");
+			}
 			sAdditionalUrlParameters = sAdditionalUrlParameters || "";
 			return this.iStartMyAppInAFrame('../../index.html?sap-ui-language=en&sap-ui-animation=false&serverDelay=0' + sAdditionalUrlParameters);
 		}
