e8ea3dbdc0e5baeca727affe5615b9231910b01f
Matthias Osswald <mat.osswald@sap.com>
2017-09-04 14:08:10 +0200

    [FIX] jQuery.sap.loadResource: Use preloaded resources by fixing URL to name guessing
    When calling "loadResource" with an "url" it will try to "guess" the
    resource name by checking the registered module paths in order to make
    use of the preload cache.
    In some cases this didn't work as the URLs might be relative and expected to
    be provided in the same relative format as the module path has been
    registered.
    This resulted into a network request every time a resource bundle has
    been initialized, even though it has already been preloaded via the
    Component preload.
    By converting all relative URLs into absolute URLs this check is now
    more robust.
    
    CR-Id: 002075125900002750262017
    Change-Id: I4c5892ad3e2693489588a0727fe057d6e3428842
    BCP: 1770349916
    BCP: 0020751295 0000401957 2017
    (cherry picked from commit aaabed04d41b104e7bf5be0eaa875504d79bce96)

--- src/sap.ui.core/src/jquery.sap.global.js
+++ src/sap.ui.core/src/jquery.sap.global.js
@@ -3006,13 +3006,16 @@
 				sUrlPrefix,
 				sResourceName;
 
+			// Make sure to have an absolute URL to check against absolute prefix URLs
+			sURL = new URI(sURL, sDocumentLocation).toString();
+
 			for (sNamePrefix in mUrlPrefixes) {
 				if ( mUrlPrefixes.hasOwnProperty(sNamePrefix) ) {
 
 					// Note: configured URL prefixes are guaranteed to end with a '/'
 					// But to support the legacy scenario promoted by the application tools ( "registerModulePath('Application','Application')" )
 					// the prefix check here has to be done without the slash
-					sUrlPrefix = mUrlPrefixes[sNamePrefix].url.slice(0, -1);
+					sUrlPrefix = mUrlPrefixes[sNamePrefix].absoluteUrl.slice(0, -1);
 
 					if ( sURL.indexOf(sUrlPrefix) === 0 ) {
 
@@ -3635,6 +3638,10 @@
 				if ( vUrlPrefix.url.slice(-1) != '/' ) {
 					vUrlPrefix.url += '/';
 				}
+
+				// calculate absolute url
+				// only to be used by 'guessResourceName'
+				vUrlPrefix.absoluteUrl = new URI(vUrlPrefix.url, sDocumentLocation).toString();
 
 				mUrlPrefixes[sResourceNamePrefix] = vUrlPrefix;
 
