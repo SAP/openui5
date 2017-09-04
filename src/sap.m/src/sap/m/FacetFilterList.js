c90bf29c29fee4a70876acc9b975c24cc97129ae
Adrian Bobev <adrian.bobev@sap.com>
2017-09-04 13:04:30 +0300

    [FIX] sap.m.FacetFilterList: keep selection when growing occurs.
    When items were selected from different than the first page, the selection was lost next time the FacetFilter is opened.
    
    Change-Id: Ie44e89c44d8305c948917928d919c888afedc6a9
    BCP: 1770333035
    BCP: 1780399312
    BCP: 1770344340
    CR-Id: 002075125900002636362017

--- src/sap.m/src/sap/m/FacetFilterList.js
+++ src/sap.m/src/sap/m/FacetFilterList.js
@@ -674,10 +674,10 @@ sap.ui.define(['jquery.sap.global', './List', './library'],
 	};
 
 	FacetFilterList.prototype._itemKeyExists = function(sKey) {
-		var aItems = this.getItems(),
+		var aItemKeys = this._getAllItemKeys(),
 			i;
-		for (i = 0; i < aItems.length; i++) {
-			if (aItems[i].getKey() === sKey) {
+		for (i = 0; i < aItemKeys.length; i++) {
+			if (aItemKeys[i] === sKey) {
 				return true;
 			}
 		}
@@ -789,6 +789,30 @@ sap.ui.define(['jquery.sap.global', './List', './library'],
 
 	FacetFilterList.prototype._preserveOriginalActiveState = function () {
 		this._bOriginalActiveState = this.getActive();
+	};
+
+	/*
+	 * Returns all keys of the FacetFilterList items.
+	 * If aggregation items is bound keys will be get from the binding
+	 * otherwise the keys will be get from items aggregation
+	 * @private
+	 */
+	FacetFilterList.prototype._getAllItemKeys = function() {
+		var oBinding = this.getBinding("items");
+
+		if (oBinding && oBinding.oList) {
+			return oBinding.oList.map(this._getBindingItemKeyOrText);
+		}
+
+		return this.getItems(true).map(this._getFacetFilterItemKeyOrText);
+	};
+
+	FacetFilterList.prototype._getBindingItemKeyOrText = function (item) {
+		return item.key || item.text;
+	};
+
+	FacetFilterList.prototype._getFacetFilterItemKeyOrText = function (item) {
+		return item.getKey() || item.getText();
 	};
 
 	return FacetFilterList;
