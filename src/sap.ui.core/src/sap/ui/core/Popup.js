21190068c34e969f4ffb98819591d5341f8c0e19
Jiawei <jiawei.cao@sap.com>
2017-09-15 16:25:07 +0200

    [FIX] core.Popup: use the 'deactivate' event on autoclose area in IE
    - the autoclose logic was updated to the new DOM after the rerendering of
      an autoclose area. But the wrong event 'blur' is used for IE which only
      work with 'deactivate' event.
    
    Change-Id: I326a72d269e15a8388f6288feba1d32bc264d50e
    BCP: 0020283766 0000385868 2017
    BCP: 0020283765 0000321046 2017
    CR-Id: 002075125900003042362017

--- src/sap.ui.core/src/sap/ui/core/Popup.js
+++ src/sap.ui.core/src/sap/ui/core/Popup.js
@@ -1863,49 +1863,68 @@ sap.ui.define([
 		//TODO: also handle the case when 'aAutoCloseAreas' is set with null
 		jQuery.sap.assert(Array.isArray(aAutoCloseAreas), "aAutoCloseAreas must be an array which contains either sap.ui.core.Element, DOM Element or an ID");
 
-		this._aAutoCloseAreas = [];
+		if (!this._aAutoCloseAreas) {
+			this._aAutoCloseAreas = [];
+		}
 
 		var createDelegate = function (oAutoCloseArea) {
 			return {
 				onBeforeRendering: function() {
 					var oDomRef = oAutocloseArea.getDomRef();
 					if (oDomRef && this.isOpen()) {
-						oDomRef.removeEventListener("blur", this.fEventHandler, true);
+						if (Device.browser.msie) {
+							jQuery(oDomRef).unbind("deactivate." + this._popupUID, this.fEventHandler);
+						} else {
+							oDomRef.removeEventListener("blur", this.fEventHandler, true);
+						}
 					}
 				},
 				onAfterRendering: function() {
 					var oDomRef = oAutocloseArea.getDomRef();
 					if (oDomRef && this.isOpen()) {
-						oDomRef.addEventListener("blur", this.fEventHandler, true);
+						if (Device.browser.msie) {
+							// 'deactivate' needs to be used for msie to achieve event handling in capturing phase
+							jQuery(oDomRef).bind("deactivate." + this._popupUID, this.fEventHandler);
+						} else {
+							oDomRef.addEventListener("blur", this.fEventHandler, true);
+						}
 					}
 				}
 			};
 		};
 
-		for (var i = 0, l = aAutoCloseAreas.length; i < l; i++) {
-			var sId = "",
-				oAutocloseArea = aAutoCloseAreas[i],
-				oDelegate,
-				oAreaRef = {};
+		var sId,
+			oAutocloseArea,
+			oDelegate,
+			oAreaRef;
 
+		for (var i = 0, l = aAutoCloseAreas.length; i < l; i++) {
+			oAutocloseArea = aAutoCloseAreas[i];
 
 			if (oAutocloseArea instanceof Element) {
-				oDelegate = createDelegate(oAutocloseArea);
-				oAutocloseArea.addEventDelegate(oDelegate, this);
 				sId = oAutocloseArea.getId();
-
-				oAreaRef.delegate = oDelegate;
 			} else if (typeof oAutocloseArea === "object") {
 				sId = oAutocloseArea.id;
 			} else if (typeof oAutocloseArea === "string") {
 				sId = oAutocloseArea;
 			}
 
-			oAreaRef.id = sId;
-			this._aAutoCloseAreas.push(oAreaRef);
-
 			if (this.getChildPopups().indexOf(sId) === -1) {
+				// when the autoclose area isn't registered, add it as a child popup and
+				// also to _aAutoCloseAreas
 				this.addChildPopup(sId);
+
+				oAreaRef = {
+					id: sId
+				};
+
+				if (oAutocloseArea instanceof Element) {
+					oDelegate = createDelegate(oAutocloseArea);
+					oAutocloseArea.addEventDelegate(oDelegate, this);
+					oAreaRef.delegate = oDelegate;
+				}
+
+				this._aAutoCloseAreas.push(oAreaRef);
 			}
 		}
 
