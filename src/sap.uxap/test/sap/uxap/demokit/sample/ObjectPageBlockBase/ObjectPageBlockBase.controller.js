cf20dc3ae686db84bac1112d68b9425e360dded3
Ilhan <ilhan.myumyun@sap.com>
2017-10-11 10:43:12 +0300

    [INTERNAL] sap.uxap: BlockBase Explored sample fixed
    (1) The removed code was used in terms of the old demokit
        to show the sample in full screen on load.
    
    (2) This is not relevant for Demokit 2.
    
    Change-Id: I12468cf7bd715bf9a106fac0fb41ebe52d69c488
    JIRA: BGSOFUIPIRIN-2104

--- src/sap.uxap/test/sap/uxap/demokit/sample/ObjectPageBlockBase/ObjectPageBlockBase.controller.js
+++ src/sap.uxap/test/sap/uxap/demokit/sample/ObjectPageBlockBase/ObjectPageBlockBase.controller.js
@@ -1,42 +1,17 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/mvc/Controller",
-	"sap/m/SplitContainer"
-], function (JSONModel, Controller, SplitContainer) {
+	"sap/ui/core/mvc/Controller"
+], function (JSONModel, Controller) {
 	"use strict";
 
 	return Controller.extend("sap.uxap.sample.ObjectPageBlockBase.ObjectPageBlockBase", {
 		onInit: function () {
-			this.oJsonConfigModel = new JSONModel(
-				{
-					subSectionLayout: "TitleOnTop"
-				});
+			this.oJsonConfigModel = new JSONModel({subSectionLayout: "TitleOnTop"});
 			this.getView().setModel(this.oJsonConfigModel, "ConfigModel");
 			this.isTitleOnTop = true;
-
-			// by default we always show the master
-			if (sap.ui.Device.system.desktop) {
-				this._oSplitContainer = sap.ui.getCore().byId("splitApp");
-				this._oSplitContainer.backToPage = jQuery.proxy(function () {
-
-					this.setMode("ShowHideMode");
-					this.showMaster();
-
-					SplitContainer.prototype.backToPage.apply(this, arguments);
-
-				}, this._oSplitContainer);
-			}
-
 		},
-		onBeforeRendering: function () {
-			//hide master for this page
-			if (sap.ui.Device.system.desktop) {
-				this._oSplitContainer.setMode("HideMode");
-				this._oSplitContainer.hideMaster();
-			}
-		},
+
 		toggleTitle: function () {
-
 			this.isTitleOnTop = !(this.isTitleOnTop);
 			this.oJsonConfigModel.setProperty("/subSectionLayout", this.isTitleOnTop ? "TitleOnTop" : "TitleOnLeft");
 		}
