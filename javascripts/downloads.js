4f4dcf983529bd55d20d56929dc9745b4cee6a56
AnnFr <a.frei@sap.com>
2017-10-16 13:41:41 +0200

    Update downloads.js

--- javascripts/downloads.js
+++ javascripts/downloads.js
@@ -21,7 +21,7 @@ function onLoad() {
 
             //check if beta token is set to true
             if (oEntry.beta) {
-                if (oUnstableVersion === null) {
+            	if (oUnstableVersion === null) {
                     oUnstableVersion = oEntry;
                 }
                 else if (oEntry.version > oUnstableVersion.version) {
@@ -65,24 +65,24 @@ function onLoad() {
     
         //replace spaceholder with values from JSON
 		if (bBeta === true) {
-			var sUnstableRelease = sUnstableVersionTemplate
-				.replace(/{{versionFull}}/g, oUnstableVersion.version)
-				.replace(/{{date}}/g, oUnstableVersion.date);
-			oUnstableVersionElement.append(sUnstableRelease);
+		    var sUnstableRelease = sUnstableVersionTemplate
+			.replace(/{{versionFull}}/g, oUnstableVersion.version)
+			.replace(/{{date}}/g, oUnstableVersion.date);
+		    oUnstableVersionElement.append(sUnstableRelease);
 		}
 
         //toggle beta section if a beta release is available
         if (bBeta === true) {
-			oNoUnstableVersionElement.css("display", "none");
-			oUnstableVersionElement.css("display", "block");
+	    oNoUnstableVersionElement.css("display", "none");
+	    oUnstableVersionElement.css("display", "block");
         } else {
-			var aTemp = oStableVersion.version.split("\.");
-			var sNextVersion = aTemp[0] + "." + (parseInt(aTemp[1]) + 2);
-			var sNoUnstableVersion = sNoUnstableVersionTemplate.replace(/{{versionNext}}/g, sNextVersion);
+	    var aTemp = oStableVersion.version.split("\.");
+	    var sNextVersion = aTemp[0] + "." + (parseInt(aTemp[1]) + 2);
+	    var sNoUnstableVersion = sNoUnstableVersionTemplate.replace(/{{versionNext}}/g, sNextVersion);
 
-			oNoUnstableVersionElement.replaceWith(sNoUnstableVersion);
-			oNoUnstableVersionElement.css("display", "block");
-			oUnstableVersionElement.css("display", "none");
+	    oNoUnstableVersionElement.replaceWith(sNoUnstableVersion);
+	    oNoUnstableVersionElement.css("display", "block");
+	    oUnstableVersionElement.css("display", "none");
         }
     })
 }
