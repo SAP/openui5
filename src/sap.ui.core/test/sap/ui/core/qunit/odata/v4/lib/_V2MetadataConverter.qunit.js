f259ee80c61fe34beb2318e66c78d64d9f001575
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2017-10-16 16:17:22 +0200

    [INTERNAL] sap.ui.model.odata.v4.lib._V2MetadataConverter: sap:updatable and sap:creatable at Property
    Change-Id: Iec70ba52d7fe5a8534093c7c52f122c175cc6b86
    (cherry picked from commit 56181d04849dbcdb321a35a00b73e2976d65eabb)
    CR-Id: 002075125900004052222017
    JIRA: CPOUI5UISERVICESV3-911
    BCP: n/a

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_V2MetadataConverter.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_V2MetadataConverter.qunit.js
@@ -1909,4 +1909,34 @@ sap.ui.require([
 	});
 	//TODO schema GWSAMPLE_BASIC.0000. with "forward reference" to EntityType processed later
 	//TODO such annotations @ (Navigation)Property @ ComplexType are not supported
+
+	//*********************************************************************************************
+	QUnit.test("sap:updatable and sap:creatable at Property", function (assert) {
+		var sXML = '\
+				<Schema Namespace="GWSAMPLE_BASIC.0001" Alias="GWSAMPLE_BASIC">\
+					<EntityType Name="BusinessPartner">\
+						<Property Name="Computed" Type="Edm.String" sap:creatable="false"\
+							sap:label="Computed" sap:updatable="false"/>\
+						<Property Name="Immutable" Type="Edm.String" sap:creatable="true"\
+							sap:label="Immutable" sap:updatable="false"/>\
+					</EntityType>\
+				</Schema>',
+			oExpectedResult = {
+				"GWSAMPLE_BASIC.0001." : {
+					"$Annotations" : {
+						"GWSAMPLE_BASIC.0001.BusinessPartner/Computed" : {
+							"@com.sap.vocabularies.Common.v1.Label": "Computed",
+							"@Org.OData.Core.V1.Computed" : true
+						},
+						"GWSAMPLE_BASIC.0001.BusinessPartner/Immutable" : {
+							"@com.sap.vocabularies.Common.v1.Label": "Immutable",
+							"@Org.OData.Core.V1.Immutable" : true
+						}
+					},
+					"$kind" : "Schema"
+				}
+			};
+
+		testConversion(assert, sXML, oExpectedResult, /*bSubset*/true);
+	});
 });
