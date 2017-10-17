b9df07be27c982be30174876c87425436ffff8ec
i024165 <bozhko.todorov@sap.com>
2017-10-17 14:35:46 +0300

    [INTERNAL][TEST] sap.ui.documentation: added initial OPA tests
    - Added basic Navigation Journey:
    - Checks if the landing page is loaded
    - Navigates through Welcome, Topic Master, Api Master, 
    Controls Master, Demo Apps and Tools pages by pressing the 
    respective Tab Buttons and check if each page is displayed.
    
    Change-Id: I5882370071c2dd155a1a5cf9f659ea96aba1195d

---
+++ src/sap.ui.documentation/test/sap/ui/documentation/sdk/integration/pageobjects/Welcome.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	'sap/ui/test/Opa5'
+], function (Opa5) {
+	"use strict";
+
+	Opa5.createPageObjects({
+		onTheWelcomePage: {
+			viewName: "Welcome",
+			actions: {
+
+				iLookAtTheScreen : function () {
+					return this;
+				}
+
+			},
+
+			assertions: {
+
+				iShouldSeeTheWelcomePage: function () {
+					return this.waitFor({
+						success: function () {
+							Opa5.assert.ok(true, "The Welcome page was successfully displayed");
+						},
+						errorMessage: "The Welcome page was not displayed"
+					});
+				}
+			}
+		}
+	});
+
+});
