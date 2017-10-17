218af2881d1f9e22051e83e159ca767c130498a3
Jay Barbhaiya <jay.barbhaiya@sap.com>
2017-10-17 11:08:09 +0200

    [INTERNAL] sap.m.Table: popinLayout feature explored sample
    Updating the existing Table example, where the new popinLayout feature
    can be explored.
    
    Change-Id: I01b00bebb5f061ea246686470ad8b15922423ea2

--- src/sap.m/test/sap/m/demokit/sample/Table/Table.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/Table/Table.controller.js
@@ -12,6 +12,26 @@ sap.ui.define([
 			// set explored app's demo model on this sample
 			var oModel = new JSONModel(jQuery.sap.getModulePath("sap.ui.demo.mock", "/products.json"));
 			this.getView().setModel(oModel);
+		},
+
+		onPopinLayoutChanged: function() {
+			var oTable = this.getView().byId("idProductsTable");
+			var oComboBox = this.getView().byId("idPopinLayout");
+			var sPopinLayout = oComboBox.getSelectedKey();
+			switch (sPopinLayout) {
+				case "Block":
+					oTable.setPopinLayout(sap.m.PopinLayout.Block);
+					break;
+				case "GridLarge":
+					oTable.setPopinLayout(sap.m.PopinLayout.GridLarge);
+					break;
+				case "GridSmall":
+					oTable.setPopinLayout(sap.m.PopinLayout.GridSmall);
+					break;
+				default:
+					oTable.setPopinLayout(sap.m.PopinLayout.Block);
+					break;
+			}
 		}
 	});
 
