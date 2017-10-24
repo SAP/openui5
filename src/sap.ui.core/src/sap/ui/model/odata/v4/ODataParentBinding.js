b77973d8738f1e24582154ef61249ba63fc40327
Sven Bernhard <sven.bernhard@sap.com>
2017-10-24 14:34:40 +0200

    [INTERNAL] sap.ui.model.odata.*: Fixed unresolved dependencies in private JSDoc
    PS1: Fixed JSDoc
    PS2: Review comments
    PS3: Review comments
    
    Change-Id: I6459bb28be9a2ba743c30ed1b4a40927fa8b1444

--- src/sap.ui.core/src/sap/ui/model/odata/v4/ODataParentBinding.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/ODataParentBinding.js
@@ -455,7 +455,7 @@ sap.ui.define([
 	/**
 	 * Returns the query options for the given path relative to this binding. Uses the options
 	 * resulting from the binding parameters or the options inherited from the parent binding by
-	 * using {@link Context#getQueryOptionsForPath}.
+	 * using {@link sap.ui.model.odata.v4.Context#getQueryOptionsForPath}.
 	 *
 	 * @param {string} sPath
 	 *   The relative path for which the query options are requested
