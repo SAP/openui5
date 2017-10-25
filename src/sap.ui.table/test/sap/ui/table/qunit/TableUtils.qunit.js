5b6fe7de44357d8d7c5e8a2dc75d1518f1229bc1
Alexander Deutschmann <alexander.deutschmann@sap.com>
2017-10-25 10:14:28 +0200

    [INTERNAL][FIX] TreeTable: BusyIndicator handling with a TreeBinding
    Fixes an issue introduced with change: 2860104
    The pending requests counter can not be used with the TreeBinding.
    
    Change-Id: I42ffff3351c4a09433b1bdade104df4e4db5b2d8
    BCP: 1770524514
    CR-Id: 002075125900003988052017

--- src/sap.ui.table/test/sap/ui/table/qunit/TableUtils.qunit.js
+++ src/sap.ui.table/test/sap/ui/table/qunit/TableUtils.qunit.js
@@ -793,6 +793,10 @@ QUnit.test("getCellInfo", function(assert) {
 
 		oTable.getBinding("rows").bUseBatchRequests = false;
 		assert.ok(!TableUtils.canUsePendingRequestsCounter(oTable), "AnalyticalBinding not using batch requests: Returned false");
+		oTable.getBinding("rows").bUseBatchRequests = true;
+
+		TableUtils.isInstanceOf.withArgs(oTable.getBinding("rows"), "sap/ui/model/TreeBinding").returns(true);
+		assert.ok(!TableUtils.canUsePendingRequestsCounter(oTable), "TreeBinding: Returned false");
 
 		TableUtils.isInstanceOf.restore();
 		delete oTable.getBinding("rows").bUseBatchRequests;
