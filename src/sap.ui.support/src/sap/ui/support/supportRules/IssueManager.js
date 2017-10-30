8715467c3daae63fdccb4975c6f2917efc5fede6
Todor Nikolov <todor.nikolov@sap.com>
2017-10-30 12:59:00 +0200

    [INTERNAL][FIX] sap.ui.support: Typo in IssueManager has been fixed
    - Issues of Medium severity were written as Medim - this 
    has been corrected.
    
    
    CR-Id: 002075125900004161852017
    BCP: 1770552462
    Change-Id: I67ccb09061c04ca6d7e17b0dd785f43e87183347
    (cherry picked from commit a69a077db5b1adb68538c0bfe74fd6767ba0d185)

--- src/sap.ui.support/src/sap/ui/support/supportRules/IssueManager.js
+++ src/sap.ui.support/src/sap/ui/support/supportRules/IssueManager.js
@@ -344,7 +344,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/base/Object", "sap/ui/support/suppor
 					}
 
 
-					treeTableModel[index].formatedName = treeTableModel[index].name + " (" + "<span style=\"color: " + constants.SUPPORT_ASSISTANT_SEVERITY_HIGH_COLOR +  "; \"> " +  iHighSeverityCount + " High, </span> " + "<span style=\"color:  " + constants.SUPPORT_ASSISTANT_SEVERITY_MEDIUM_COLOR +  ";\"> " +  iMediumSeverityCount + " Medim, </span> " + "<span style=\"color " + constants.SUPPORT_ASSISTANT_SEVERITY_LOW_COLOR +  ";\"> " +  iLowSeverityCount + " Low </span>)";
+					treeTableModel[index].formatedName = treeTableModel[index].name + " (" + "<span style=\"color: " + constants.SUPPORT_ASSISTANT_SEVERITY_HIGH_COLOR +  "; \"> " +  iHighSeverityCount + " High, </span> " + "<span style=\"color:  " + constants.SUPPORT_ASSISTANT_SEVERITY_MEDIUM_COLOR +  ";\"> " +  iMediumSeverityCount + " Medium, </span> " + "<span style=\"color " + constants.SUPPORT_ASSISTANT_SEVERITY_LOW_COLOR +  ";\"> " +  iLowSeverityCount + " Low </span>)";
 					treeTableModel[index].name += " (" + issueCount + " issues)";
 					treeTableModel[index].issueCount = issueCount;
 					issueCount = 0;
