2cd53b094137815f07983ff8b5922afd7d48d872
Elena Stoyanova <elena.stoyanova@sap.com>
2017-11-03 16:12:39 +0200

    [FIX] sap.m.Page: showHeader property working correctly with floatingFooter
    Change-Id: Id4d38e812880fb4277c8e059dca7e2048b19c745
    BCP: 1770463973
    CR-Id: 002075125900004230822017

--- src/sap.m/src/sap/m/Page.js
+++ src/sap.m/src/sap/m/Page.js
@@ -349,8 +349,10 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control", "sap/ui/
 				this.setProperty("showFooter", bShowFooter);
 				return this;
 			}
+
 			this.setProperty("showFooter", bShowFooter,true);
 
+			$footer.removeClass("sapUiHidden");
 			$footer.toggleClass("sapMPageFooterControlShow", bShowFooter);
 			$footer.toggleClass("sapMPageFooterControlHide", !bShowFooter);
 
@@ -361,8 +363,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control", "sap/ui/
 			if (useAnimation) {
 				jQuery.sap.delayedCall(Page.FOOTER_ANIMATION_DURATION, this, function () {
 					$footer.toggleClass("sapUiHidden", bShowFooter);
-			});
-
+				});
 			} else {
 				$footer.toggleClass("sapUiHidden", bShowFooter);
 			}
