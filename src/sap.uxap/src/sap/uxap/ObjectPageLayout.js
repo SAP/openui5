e788ad55955dadb08ea25deb0f2bd17d2da8e844
i024165 <bozhko.todorov@sap.com>
2017-11-10 16:55:38 +0200

    [INTERNAL][FIX] sap.uxap.ObjectPageLayout: Fix scrollToSection.
    - Fix scrollToSection when scroll duration is 0
    - Prevents scrolling, in case of only one visible section with
    little content
    - Internal logic improved to preserve expanded header if no need
    to stick
    
    JIRA: BGSOFUIPIRIN-2152
    
    Change-Id: I8347b6fa6bcab9451506e0de399b98b74a9137f9
    (cherry picked from commit 4c956a14eb493a4e4a8b2ef23620e1f8604c5ccc)
    CR-Id: 002075125900000000362018
    BCP: 1880044080

--- src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -579,6 +579,7 @@ sap.ui.define([
 		// some late callbacks may still have access to the page
 		// (and try to process the page) after the page is being destroyed
 		this._oFirstVisibleSection = null;
+		this._oFirstVisibleSubSection = null;
 	};
 
 	ObjectPageLayout.prototype._getCustomScrollBar = function () {
@@ -908,6 +909,7 @@ sap.ui.define([
 
 		this._setInternalAnchorBarVisible(bVisibleAnchorBar, bInvalidate);
 		this._oFirstVisibleSection = oFirstVisibleSection;
+		this._oFirstVisibleSubSection = this._getFirstVisibleSubSection(oFirstVisibleSection);
 	};
 
 	/*************************************************************************************
@@ -1673,12 +1675,15 @@ sap.ui.define([
 		return iSpacerHeight;
 	};
 
-	ObjectPageLayout.prototype._isFirstVisibleSectionBase = function (oSection) {
+	ObjectPageLayout.prototype._isFirstVisibleSectionBase = function (oSectionBase) {
 
-		var oSectionInfo = this._oSectionInfo[oSection.getId()];
-		if (oSectionInfo) {
-			return Math.abs(oSectionInfo.positionTop - this.iHeaderContentHeight) <= 1;
+		var sSectionBaseId;
+
+		if (oSectionBase && (this._oFirstVisibleSubSection || this._oFirstVisibleSection)) {
+			sSectionBaseId = oSectionBase.getId();
+			return sSectionBaseId === this._oFirstVisibleSection.getId() || sSectionBaseId === this._oFirstVisibleSubSection.getId();
 		}
+
 		return false;
 	};
 
