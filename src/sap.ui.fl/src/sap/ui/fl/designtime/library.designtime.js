76e4c25a58d5dddb4c8ccdb2256e1db8efcc5e56
Martin Schaus <martin.schaus@sap.com>
2017-11-13 11:44:50 +0100

    [INTERNAL] Adding library.designtime.js
    - Added library.designtime.js to 
      sap.ui.fl
    
    Change-Id: Id964b989b43d4be457818f0daef3387d40ace160

---
+++ src/sap.ui.fl/src/sap/ui/fl/designtime/library.designtime.js
@@ -0,0 +1,11 @@
+/*!
+ * ${copyright}
+ */
+
+/**
+ * Initialization of designtime Code and shared classes for the library sap.ui.fl.
+ */
+sap.ui.define([], function() {
+	"use strict";
+	return {};
+});
