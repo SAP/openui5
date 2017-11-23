5494f37cc665543d5b46d25444c82bf7a864e8e6
Adrian Bobev <adrian.bobev@sap.com>
2017-11-23 14:33:22 +0200

    [INTERNAL][FIX] sap.m.TimePickerSliders: align demo sample to the spec
    Change-Id: If27a0df6e10ad26cc03d8b5e6fd73876a8c9d9d7

--- src/sap.m/test/sap/m/demokit/sample/TimePickerSliders/TimePickerSliders.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/TimePickerSliders/TimePickerSliders.controller.js
@@ -11,24 +11,35 @@ sap.ui.define(['sap/ui/core/mvc/Controller','sap/ui/core/Fragment'],
 			},
 
 			handleOpenDialog: function (oEvent) {
-
 				// create popover
 				if (!this._oDialog) {
 					this._oDialog = sap.ui.xmlfragment("fragment", "sap.m.sample.TimePickerSliders.TimePickerSlidersDialog", this);
 					this.getView().addDependent(this._oDialog);
+					this._oDialog.attachAfterOpen(function () {
+						var oTP = Fragment.byId("fragment", "TPS2");
+
+						this._sOldValue = oTP.getValue();
+					}.bind(this));
 				}
 
 				this._oDialog.open();
 			},
 
 			handleOKPress: function () {
-				var oText = this.byId("T1");
-				var oTP = Fragment.byId("fragment", "TPS2");
-				var sValue = oTP.getValue();
+				var oText = this.byId("T1"),
+					oTP = Fragment.byId("fragment", "TPS2");
 
 				this._oDialog.close();
 
-				oText.setText("TimePickerSliders " + oTP.getId() + ": " + sValue);
+				oText.setText("TimePickerSliders " + oTP.getId() + ": " + oTP.getValue());
+			},
+
+			handleCancelPress: function () {
+				var oTP = Fragment.byId("fragment", "TPS2");
+
+				oTP.setValue(this._sOldValue);
+
+				this._oDialog.close();
 			}
 		});
 
