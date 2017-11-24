b7109c712b91a9fbf2d13d8b018c547028446e21
Malte Wedel <malte.wedel@sap.com>
2017-11-24 12:46:44 +0100

    [FIX] Filter/Sorter: Get comparator methods right
    Comparator methods cannot be the same for sorting and filtering. For
    sorting null/undefined values are ordered on the top in ascending order,
    so the comparisons with null must treat it as less than any other
    value. For filtering null/undefined values must be treated in a way,
    that they are neither less or greater than other values.
    
    Updated default comparator methods in Sorter/Filter to reflect this
    behaviour correctly. Added a wrapper around the OData comparator methods
    in v2.ODataListBinding for clientside sorting. 
    
    BCP: 0020751295 0000379313 2017
    Change-Id: Ice444f9385e9a4c3e8e568f9e1e81b2e8d28d9c9
    CR-Id: 002075125800004597182017

--- src/sap.ui.core/src/sap/ui/model/Sorter.js
+++ src/sap.ui.core/src/sap/ui/model/Sorter.js
@@ -117,10 +117,10 @@ sap.ui.define(['sap/ui/base/Object'],
 		if (a == b) {
 			return 0;
 		}
-		if (b == null) {
+		if (a == null) {
 			return -1;
 		}
-		if (a == null) {
+		if (b == null) {
 			return 1;
 		}
 		if (typeof a == "string" && typeof b == "string") {
