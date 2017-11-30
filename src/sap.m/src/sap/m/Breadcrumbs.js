9569e82d4a2067442b8d06719c87028d39901668
I311640 <mihail.yordanov@sap.com>
2017-11-30 12:38:51 +0200

    [FIX] sap.m.BreadCrumbs: Fixed keyboard handling for hidden items
    BCP: 1780411176
    -item navigation is not initialized now if there are no visible
    aggregated controls inside
    -added qUnit test
    
    Change-Id: Iffc02e28667b1f109d2e97cf379ed0e0c8e9ad6c

--- src/sap.m/src/sap/m/Breadcrumbs.js
+++ src/sap.m/src/sap/m/Breadcrumbs.js
@@ -493,6 +493,10 @@ sap.ui.define([
 			aItemsToNavigate = this._getItemsToNavigate(),
 			aNavigationDomRefs = [];
 
+		if (aItemsToNavigate.length === 0) {
+			return;
+		}
+
 		aItemsToNavigate.forEach(function (oItem, iIndex) {
 			if (iIndex === 0) {
 				oItem.$().attr("tabIndex", "0");
