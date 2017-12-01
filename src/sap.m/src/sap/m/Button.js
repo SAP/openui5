d0247c23fa06e50e18ca3f4642009054fab97263
Adrian Bobev <adrian.bobev@sap.com>
2017-12-01 15:36:31 +0200

    [FIX] sap.m.Button: fire press event on keydown for 'enter' and keyup for 'space'
    According to the spec press event of the button should be fired on
    keyup for 'space' key and on keydown for 'enter' key.
    
    BCP: 1780469622
    Change-Id: I4246a5ce0758fae16eaaa131c2388a999e1c0a77
    CR-Id: 002075125800004862062017

--- src/sap.m/src/sap/m/Button.js
+++ src/sap.m/src/sap/m/Button.js
@@ -255,6 +255,10 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			// set active button state
 			this._activeButton();
 		}
+
+		if (oEvent.which === jQuery.sap.KeyCodes.ENTER) {
+			this.firePress({/* no parameters */});
+		}
 	};
 
 	/**
@@ -272,6 +276,9 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 
 			// set inactive button state
 			this._inactiveButton();
+		}
+
+		if (oEvent.which === jQuery.sap.KeyCodes.SPACE) {
 			this.firePress({/* no parameters */});
 		}
 	};
