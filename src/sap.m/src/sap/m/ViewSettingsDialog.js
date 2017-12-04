5b1c38dbd80ff684028d3eb9d9f82f5ae6709755
Adrian Bobev <adrian.bobev@sap.com>
2017-12-04 14:04:03 +0200

    [FIX] sap.m.ViewSettingsDialog: Cloned item now works with binding
    When a cancel button is clicked we restore the previous state of the
    ViewSettingsDialog through cloned item. However, when a clone is used
    the binding is cloned but only the context without the values.
    In order to make values appear it is necessary to add the control in
    the model propagation tree (this can be done by adding the cloned item
    to the dependents aggregation).
    
    Change-Id: I9d6b577fc7a599e953afe41f1a32d218d368683f
    BCP: 1770487283
    BCP: 1770605019
    CR-Id: 002075125800004668302017

--- src/sap.m/src/sap/m/ViewSettingsDialog.js
+++ src/sap.m/src/sap/m/ViewSettingsDialog.js
@@ -1047,6 +1047,8 @@ function(jQuery, library, Control, IconPool, Toolbar, CheckBox, SearchField, Lis
 			contentItem : this._oContentItem ? this._oContentItem.clone() : null
 		};
 
+		this.addDependent(this._oPreviousState.contentItem);
+
 		//focus the first focusable item in current page's content
 		if (sap.ui.Device.system.desktop) {
 			this._getDialog().attachEventOnce("afterOpen", function () {
