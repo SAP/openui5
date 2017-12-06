5a4f13b2f135e67ac45daad6544fd546c6cb291e
Sebastian Wennemers <sebastian.wennemers@sap.com>
2017-12-06 12:10:05 +0100

    [INTERNAL][FIX] sap.ui.fl Get Binding Template in XML
    White space nodes have not been ignored when being asked for a
    binding template in XmlTreeModifier, this is fixed now.
    
    BCP: 1780459167
    CR-Id: 002075125800004753382017
    
    Change-Id: I9a3eae02239ecc226fa88b370f53fa321bef9842

--- src/sap.ui.fl/test/sap/ui/fl/qunit/changeHandler/XmlTreeModifier.qunit.js
+++ src/sap.ui.fl/test/sap/ui/fl/qunit/changeHandler/XmlTreeModifier.qunit.js
@@ -31,9 +31,14 @@ function(
 						});
 
 			this.oXmlString =
-				'<mvc:View id="testComponent---myView" xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m" xmlns:f="sap.f" xmlns:layout="sap.ui.layout">' +
-					'<VBox>' +
-						'<tooltip></tooltip>' +	//empty 0..1 aggregation
+				'<mvc:View id="testComponent---myView" ' +
+					'xmlns:mvc="sap.ui.core.mvc" ' +
+					'xmlns="sap.m" ' +
+					'xmlns:f="sap.f" ' +
+					'xmlns:layout="sap.ui.layout" ' +
+					'xmlns:fl="sap.ui.fl">' +
+					'<VBox>\n' +
+						'<tooltip>\n</tooltip>' +	//empty 0..1 aggregation
 						'<Label visible="true"></Label>' + //content in default aggregation
 						'<Label visible="false" design="Bold"></Label>' + //content in default aggregation, property set that has default value
 					'</VBox>' +
@@ -41,26 +46,26 @@ function(
 						'<tooltip>' +	//0..1 aggregation
 							'<TooltipBase xmlns="sap.ui.core"></TooltipBase>' + //inline namespace as sap.ui.core is use case for not existing namespace
 						'</tooltip>' +
-						'<items>' +
+						'<items>\n' +
 							'<Text id="' + this.TEXT_ID + '"></Text>' + //content in default aggregation
 						'</items>' +
 					'</HBox>' +
 					'<Bar tooltip="barTooltip">' + //control without default aggregation, tooltip aggregation filled with altType
-					'</Bar>' +
+					'</Bar>\n' +
 					'<VBox id="vbox1">' +
 						'<Button text="Button1"></Button>' +
-						'<Button text="Button2"></Button>' +
+						'<Button text="Button2"></Button>\n' +
 						'<Button text="Button3"></Button>' +
 					'</VBox>' +
-					'<f:DynamicPageTitle id="title1">' +
-						'<actions>' +
-							'<Button text="Action1"></Button>' +
-							'<Button text="Action2"></Button>' +
+					'<f:DynamicPageTitle id="title1">\n' +
+						'<actions>\n' +
+							'<Button text="Action1"></Button>\n' +
+							'<Button text="Action2"></Button>\n' +
 							'<Button text="Action3"></Button>' +
 						'</actions>' +
 					'</f:DynamicPageTitle>' +
 					'<VBox id="vbox2">' +
-						'<tooltip></tooltip>' +
+						'<tooltip>\n</tooltip>' +
 						'<Button text="Button1"></Button>' +
 						'<Button text="Button2"></Button>' +
 						'<Button text="Button3"></Button>' +
@@ -113,7 +118,7 @@ function(
 	});
 
 	QUnit.test("the default aggregation is returned if aggregation node is not available", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		var aDefaultAggregationElements = XmlTreeModifier.getAggregation(oVBox, 'items');
 		assert.equal(aDefaultAggregationElements.length, 2);
 		assert.equal(aDefaultAggregationElements[0].localName, "Label");
@@ -123,73 +128,74 @@ function(
 	});
 
 	QUnit.test("the default aggregation is returned if aggregation node is available", function (assert) {
-		var oHBox = this.oXmlView.childNodes[1];
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1];
 		var aDefaultAggregationElements = XmlTreeModifier.getAggregation(oHBox, 'items');
 		assert.equal(aDefaultAggregationElements.length, 1);
 		assert.equal(aDefaultAggregationElements[0].localName, "Text");
 	});
 
 	QUnit.test("the empty non default aggregation is returned ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		var aNonDefaultAggregationElements = XmlTreeModifier.getAggregation(oVBox, 'customData');
 		assert.equal(aNonDefaultAggregationElements.length, 0);
 	});
 
 	QUnit.test("the empty non default aggregation is returned when no default aggregation exists", function (assert) {
-		var oBar = this.oXmlView.childNodes[2];
+		var oBar = XmlTreeModifier._children(this.oXmlView)[2];
 		var aNonDefaultAggregationElements = XmlTreeModifier.getAggregation(oBar, 'contentRight');
 		assert.equal(aNonDefaultAggregationElements.length, 0);
 	});
 
 	QUnit.test("the empty single aggregation is returning nothing", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		var vAggregationElements = XmlTreeModifier.getAggregation(oVBox, 'tooltip');
 		assert.ok(!vAggregationElements);
 	});
 
 	QUnit.test("the not in xml view available single aggregation is returning nothing", function (assert) {
-		var oLabel = this.oXmlView.childNodes[0].childNodes[1];
+		var oLabel = XmlTreeModifier._children(this.oXmlView)[0].childNodes[2];
 		var vAggregationElements = XmlTreeModifier.getAggregation(oLabel, 'tooltip');
 		assert.ok(!vAggregationElements);
 	});
 
 	QUnit.test("the single aggregation is returning the control node directly", function (assert) {
-		var oHBox = this.oXmlView.childNodes[1];
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1];
 		var oAggregationElements = XmlTreeModifier.getAggregation(oHBox, 'tooltip');
 		assert.equal(oAggregationElements.localName, "TooltipBase");
 		assert.equal(oAggregationElements.namespaceURI, "sap.ui.core");
 	});
 
 	QUnit.test("the altType aggregation returns the property value", function (assert) {
-		var oBar = this.oXmlView.childNodes[2];
+		var oBar = XmlTreeModifier._children(this.oXmlView)[2];
 		var vAggregationElements = XmlTreeModifier.getAggregation(oBar, 'tooltip');
 		assert.equal(vAggregationElements, "barTooltip");
 	});
 
 	QUnit.test("the first non default aggregation childNode is added under a newly created aggregation node ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		var oCustomDataElement = XmlTreeModifier.createControl('sap.ui.core.CustomData', this.oComponent, this.oXmlView, "someId", {'key' : "someKey", "value": "someValue"});
 		XmlTreeModifier.insertAggregation(oVBox, "customData", oCustomDataElement, 0, this.oXmlView, this.oComponent);
-		assert.equal(oVBox.childNodes[3].localName, "customData", "aggregation node is appended at the end");
-		assert.equal(oVBox.childNodes[3].namespaceURI, "sap.m", "aggregation node is added with parents namespaceURI");
+		assert.equal(XmlTreeModifier._children(oVBox)[3].localName, "customData", "aggregation node is appended at the end");
+		assert.equal(XmlTreeModifier._children(oVBox)[3].namespaceURI, "sap.m", "aggregation node is added with parents namespaceURI");
 		var aNonDefaultAggregationElements = XmlTreeModifier.getAggregation(oVBox, 'customData');
 		assert.equal(aNonDefaultAggregationElements.length, 1);
 		assert.equal(aNonDefaultAggregationElements[0].localName, "CustomData");
 		assert.equal(aNonDefaultAggregationElements[0].namespaceURI, "sap.ui.core");
 	});
 	QUnit.test("a child is added to the default aggregation ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		var oCustomDataElement = XmlTreeModifier.createControl('sap.m.Text', this.oComponent, this.oXmlView);
 		XmlTreeModifier.insertAggregation(oVBox, "items", oCustomDataElement, 0, this.oXmlView, this.oComponent);
-		assert.equal(oVBox.childNodes.length, 4, "new control is added directly as child to the parent node");
-		assert.equal(oVBox.childNodes[0].localName, "tooltip");
-		assert.equal(oVBox.childNodes[0].namespaceURI, "sap.m");
-		assert.equal(oVBox.childNodes[1].localName, "Text");
-		assert.equal(oVBox.childNodes[1].namespaceURI, "sap.m");
-		assert.equal(oVBox.childNodes[2].localName, "Label");
-		assert.equal(oVBox.childNodes[2].namespaceURI, "sap.m");
-		assert.equal(oVBox.childNodes[3].localName, "Label");
-		assert.equal(oVBox.childNodes[3].namespaceURI, "sap.m");
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+		assert.equal(aChildNodes.length, 4, "new control is added directly as child to the parent node");
+		assert.equal(aChildNodes[0].localName, "tooltip");
+		assert.equal(aChildNodes[0].namespaceURI, "sap.m");
+		assert.equal(aChildNodes[1].localName, "Text");
+		assert.equal(aChildNodes[1].namespaceURI, "sap.m");
+		assert.equal(aChildNodes[2].localName, "Label");
+		assert.equal(aChildNodes[2].namespaceURI, "sap.m");
+		assert.equal(aChildNodes[3].localName, "Label");
+		assert.equal(aChildNodes[3].namespaceURI, "sap.m");
 		var aNonDefaultAggregationElements = XmlTreeModifier.getAggregation(oVBox, 'items');
 		assert.equal(aNonDefaultAggregationElements.length, 3);
 		assert.equal(aNonDefaultAggregationElements[0].localName, "Text");
@@ -201,26 +207,28 @@ function(
 	});
 
 	QUnit.test("a child is removed from the default aggregation ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
-		var oLabel = this.oXmlView.childNodes[0].childNodes[1];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
+		var oLabel = XmlTreeModifier._children(this.oXmlView)[0].childNodes[2];
 		XmlTreeModifier.removeAggregation(oVBox, "items", oLabel);
-		assert.equal(oVBox.childNodes.length, 2);
-		assert.equal(oVBox.childNodes[0].localName, "tooltip");
-		assert.equal(oVBox.childNodes[0].namespaceURI, "sap.m");
-		assert.equal(oVBox.childNodes[1].localName, "Label");
-		assert.equal(oVBox.childNodes[1].namespaceURI, "sap.m");
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+		assert.equal(aChildNodes.length, 2);
+		assert.equal(aChildNodes[0].localName, "tooltip");
+		assert.equal(aChildNodes[0].namespaceURI, "sap.m");
+		assert.equal(aChildNodes[1].localName, "Label");
+		assert.equal(aChildNodes[1].namespaceURI, "sap.m");
 	});
 
 	QUnit.test("removeAll from the default aggregation ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
 		XmlTreeModifier.removeAllAggregation(oVBox, "items");
-		assert.equal(oVBox.childNodes.length, 1);
-		assert.equal(oVBox.childNodes[0].localName, "tooltip");
-		assert.equal(oVBox.childNodes[0].namespaceURI, "sap.m");
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+		assert.equal(aChildNodes.length, 1);
+		assert.equal(aChildNodes[0].localName, "tooltip");
+		assert.equal(aChildNodes[0].namespaceURI, "sap.m");
 	});
 
 	QUnit.test("removeAll from the default aggregation ", function (assert) {
-		var oHBox = this.oXmlView.childNodes[1];
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1];
 		XmlTreeModifier.removeAllAggregation(oHBox, "items");
 		assert.equal(oHBox.childNodes.length, 1);
 		assert.equal(oHBox.childNodes[0].localName, "tooltip");
@@ -228,42 +236,47 @@ function(
 	});
 
 	QUnit.test("getVisible", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
-		var oVisibleLabel = oVBox.childNodes[1];
-		var oInvisibleLabel = oVBox.childNodes[2];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+		var oVisibleLabel = aChildNodes[1];
+		var oInvisibleLabel = aChildNodes[2];
 		assert.strictEqual(XmlTreeModifier.getVisible(oVBox), true, "not stating visible in xml means visible");
 		assert.strictEqual(XmlTreeModifier.getVisible(oVisibleLabel), true, "visible in xml");
 		assert.strictEqual(XmlTreeModifier.getVisible(oInvisibleLabel), false, "invisible in xml");
 	});
 
 	QUnit.test("setStash", function (assert) {
-		var oVBox = this.oXmlView.childNodes[7];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[7];
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+
 		XmlTreeModifier.setStashed(oVBox, true);
 		assert.strictEqual(oVBox.getAttribute("stashed"), "true", "stashed attribute can be added");
 
-		var oVisibleLabel = oVBox.childNodes[0];
+		var oVisibleLabel = aChildNodes[0];
 		XmlTreeModifier.setStashed(oVisibleLabel, true);
 		assert.strictEqual(oVisibleLabel.getAttribute("stashed"), "true", "stashed attribute can be changed");
 
-		var oStashedInvisibleLabel = oVBox.childNodes[1];
+		var oStashedInvisibleLabel = aChildNodes[1];
 		XmlTreeModifier.setStashed(oStashedInvisibleLabel, false);
 		assert.strictEqual(oStashedInvisibleLabel.getAttribute("stashed"), null, "stashed=false means not having it in xml as some controls behave differently if visible property is provided");
 		assert.strictEqual(oStashedInvisibleLabel.getAttribute("visible"), null, "Unstash also needs to make the control visible (which is done automatically in with stash API)");
 	});
 
 	QUnit.test("getProperty returns default value if not in xml", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0];
-		var oVisibleLabel = oVBox.childNodes[1];
-		var oInvisibleLabel = oVBox.childNodes[2];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
+		var aChildNodes = XmlTreeModifier._children(oVBox);
+
+		var oVisibleLabel = aChildNodes[1];
+		var oInvisibleLabel = aChildNodes[2];
 		assert.strictEqual(XmlTreeModifier.getProperty(oVisibleLabel, "design"), "Standard", "default value, property not in xml");
 		assert.strictEqual(XmlTreeModifier.getProperty(oVisibleLabel, "text"), "", "default value, property not in xml");
 		assert.strictEqual(XmlTreeModifier.getProperty(oInvisibleLabel, "design"), "Bold", "property from xml");
 	});
 
 	QUnit.test("_byId finds the node specified", function (assert) {
-		var oExpectedHBox = this.oXmlView.childNodes[1];
+		var oExpectedHBox = XmlTreeModifier._children(this.oXmlView)[1];
 		oExpectedHBox.setAttributeNS("http://schemas.sap.com/sapui5/extension/sap.ui.core.Internal/1", "id", true);
-		var oExpectedText = oExpectedHBox.childNodes[1].childNodes[0];
+		var oExpectedText = oExpectedHBox.childNodes[1].childNodes[1];
 		oExpectedText.setAttributeNS("http://schemas.sap.com/sapui5/extension/sap.ui.core.Internal/1", "id", true);
 
 		var oHBox = XmlTreeModifier._byId(this.HBOX_ID, this.oXmlView);
@@ -273,35 +286,36 @@ function(
 	});
 
 	QUnit.test("findIndexInParentAggregation returns the correct value: case 1 - control in aggregation 0..1 passed as parameter", function (assert) {
-		var oHBox = this.oXmlView.childNodes[1];
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1];
+
 		var oTooltip = oHBox.childNodes[0].childNodes[0];
 
 		assert.strictEqual(XmlTreeModifier.findIndexInParentAggregation(oTooltip), 0, "The function returned the correct index.");
 	});
 
 	QUnit.test("findIndexInParentAggregation returns the correct value: case 2 - default aggregation only in xml tree", function (assert) {
-		var oVBox = this.oXmlView.childNodes[3];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[3];
 		var oButton = oVBox.lastElementChild;
 
 		assert.strictEqual(XmlTreeModifier.findIndexInParentAggregation(oButton), 2, "The function returned the correct index.");
 	});
 
 	QUnit.test("findIndexInParentAggregation returns the correct value: case 3 - named aggregation only in xml tree", function (assert) {
-		var oDynamicPageTitle = this.oXmlView.childNodes[4];
-		var oButton = oDynamicPageTitle.childNodes[0].lastElementChild;
+		var oDynamicPageTitle = XmlTreeModifier._children(this.oXmlView)[4];
+		var oButton = XmlTreeModifier._children(oDynamicPageTitle)[0].lastElementChild;
 
 		assert.strictEqual(XmlTreeModifier.findIndexInParentAggregation(oButton), 2, "The function returned the correct index.");
 	});
 
 	QUnit.test("findIndexInParentAggregation returns the correct value: case 4 - mixed node with aggregation and default aggregation", function (assert) {
-		var oVBox = this.oXmlView.childNodes[5];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[5];
 		var oButton = oVBox.lastElementChild;
 
 		assert.strictEqual(XmlTreeModifier.findIndexInParentAggregation(oButton), 2, "The function returned the correct index.");
 	});
 
 	QUnit.test("findIndexInParentAggregation returns the correct value: case 5 - mixed node with aggregation and named aggregation", function (assert) {
-		var oDynamicPageTitle = this.oXmlView.childNodes[6];
+		var oDynamicPageTitle = XmlTreeModifier._children(this.oXmlView)[6];
 		var oButton = oDynamicPageTitle.childNodes[0].lastElementChild;
 		var oText = oDynamicPageTitle.childNodes[1].childNodes[1];
 
@@ -310,14 +324,14 @@ function(
 	});
 
 	QUnit.test("_getParentAggregationName returns the correct name: ", function (assert) {
-		var oVBox = this.oXmlView.childNodes[0],
-			oLabel = oVBox.childNodes[1];
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0],
+			oLabel = XmlTreeModifier._children(oVBox)[1];
 
-		var oHBox = this.oXmlView.childNodes[1],
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1],
 			oTooltip = oHBox.childNodes[0].childNodes[0],
 			oText = oHBox.childNodes[1].childNodes[0];
 
-		var oDynamicPageTitle = this.oXmlView.childNodes[6],
+		var oDynamicPageTitle = XmlTreeModifier._children(this.oXmlView)[6],
 			oButton = oDynamicPageTitle.childNodes[0].childNodes[0],
 			oText2 = oDynamicPageTitle.childNodes[1].childNodes[0];
 
@@ -326,5 +340,20 @@ function(
 		assert.strictEqual(XmlTreeModifier._getParentAggregationName(oHBox, oText), "items", "The function returned the correct name - 'items'.");
 		assert.strictEqual(XmlTreeModifier._getParentAggregationName(oDynamicPageTitle, oButton), "actions", "The function returned the correct name - 'actions'.");
 		assert.strictEqual(XmlTreeModifier._getParentAggregationName(oDynamicPageTitle, oText2), "snappedContent", "The function returned the correct name - 'snappedContent'.");
+	});
+
+	QUnit.test("when getBingingTemplate is called for an aggregation without nodes", function (assert) {
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[5];
+		assert.notOk(XmlTreeModifier.getBindingTemplate(oVBox, "tooltip"), "then nothing is returned");
+	});
+
+	QUnit.test("when getBingingTemplate is called for an aggregation with multiple nodes", function (assert) {
+		var oVBox = XmlTreeModifier._children(this.oXmlView)[0];
+		assert.notOk(XmlTreeModifier.getBindingTemplate(oVBox, "content"), "then nothing is returned");
+	});
+
+	QUnit.test("when getBingingTemplate is called for an aggregation that has a single real control, but text nodes", function (assert) {
+		var oHBox = XmlTreeModifier._children(this.oXmlView)[1];
+		assert.ok(XmlTreeModifier.getBindingTemplate(oHBox, "items"), "then content inside item is returned");
 	});
 });
