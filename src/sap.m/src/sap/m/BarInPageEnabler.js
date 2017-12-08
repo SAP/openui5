f01e3506a286c5bba7f21322562aafda88651336
Adrian Bobev <adrian.bobev@sap.com>
2017-12-08 14:57:34 +0200

    [FIX] sap.m.BarInPageEnabler: remove extra aria-labelledby attribute
    Previously an extra aria-labelledby attribute was added to the Bar
    if it used as a Header, SubHeader or Footer.
    
    Change-Id: Ic73797f170e1fba9a7f07969bf8611e4b65b728a
    BCP: 1780428289
    CR-Id: 002075125800004895312017

--- src/sap.m/src/sap/m/BarInPageEnabler.js
+++ src/sap.m/src/sap/m/BarInPageEnabler.js
@@ -27,26 +27,6 @@ sap.ui.define(['sap/ui/base/Object', './PageAccessibleLandmarkInfo', 'sap/ui/cor
 
 	var IBAR_CSS_CLASS = "sapMIBar";
 
-	var _mInvisibleTexts = {},
-		oBundle = sap.ui.getCore().getLibraryResourceBundle("sap.m");
-
-	/**
-	 * Creates (if not already created) and returns an invisible text element for screan reader support
-	 * @param sType - the type of the control we want to get a label for
-	 * @param sText - the text to be used
-	 * @private
-	 */
-	var _ensureInvisibleText = function(sType, sText) {
-
-		if (typeof _mInvisibleTexts[sType] === "undefined") {
-			_mInvisibleTexts[sType] = new InvisibleText({
-				text: sText
-			}).toStatic().getId();
-		}
-
-		return _mInvisibleTexts[sType];
-	};
-
 	/**
 	 * @class Helper Class for implementing the IBar interface. Should be created once per IBar instance.
 	 * @version 1.22
@@ -169,10 +149,6 @@ sap.ui.define(['sap/ui/base/Object', './PageAccessibleLandmarkInfo', 'sap/ui/cor
 				this.addStyleClass(IBAR_CSS_CLASS + "-CTX");
 			}
 
-			if (oOptions.internalAriaLabel) {
-				this._sInternalAriaLabelId = _ensureInvisibleText(oOptions.tag, oBundle.getText(oOptions.internalAriaLabel));
-			}
-
 			if (this.isContextSensitive()) {
 				this.addStyleClass(oOptions.contextClass);
 			}
@@ -247,12 +223,6 @@ sap.ui.define(['sap/ui/base/Object', './PageAccessibleLandmarkInfo', 'sap/ui/cor
 			oRM.write("<" + sTag);
 			oRM.addClass(IBAR_CSS_CLASS);
 
-			if (oControl._sInternalAriaLabelId) {
-				oRM.writeAccessibilityState(oControl, {
-					"labelledby": {value: oControl._sInternalAriaLabelId, append: true}
-				});
-			}
-
 			if (this.shouldAddIBarContext(oControl)) {
 				oRM.addClass(IBAR_CSS_CLASS + "-CTX");
 			}
@@ -296,17 +266,6 @@ sap.ui.define(['sap/ui/base/Object', './PageAccessibleLandmarkInfo', 'sap/ui/cor
 	 */
 	BarInPageEnabler.addChildClassTo = function (oControl) {
 		oControl.addStyleClass("sapMBarChild");
-	};
-
-	/**
-	 * Termination of the BarInPageEnabler control
-	 * @private
-	 */
-	BarInPageEnabler.prototype.exit = function () {
-		if (this._sInternalAriaLabelId) {
-			this._sInternalAriaLabelId.destroy();
-			this._sInternalAriaLabelId = null;
-		}
 	};
 
 	return BarInPageEnabler;
