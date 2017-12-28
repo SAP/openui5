2c8a684fa9cc07ed0e3e98a555864f102c43e995
Adrian Bobev <adrian.bobev@sap.com>
2017-12-28 14:23:00 +0200

    [FIX] sap.m.DateTimeField: setDateValue accepts JS Date Object from iframe
    setDateValue previously checked if the provided object is instanceof
    Date. However, this check is not reliable when iframe is used (eg. in OPA).
    So we change the check to use jQuery.type method which uses text the string
    representation of the constructor of the object.
    
    Change-Id: If75e4dae19b4c91820f39d082e0ced1be4ed020c
    BCP: 1780526208
    CR-Id: 002075125800000438252018

--- src/sap.m/src/sap/m/DateTimeField.js
+++ src/sap.m/src/sap/m/DateTimeField.js
@@ -114,7 +114,13 @@ sap.ui.define([
 
 	DateTimeField.prototype.setDateValue = function (oDate) {
 
-		if (oDate && !(oDate instanceof Date)) {
+		// Cross frame check for a date should be performed here otherwise setDateValue would fail in OPA tests
+		// because Date object in the test is different than the Date object in the application (due to the iframe).
+		// We can use jQuery.type or this method:
+		// function isValidDate (date) {
+		//	return date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
+		//}
+		if (oDate && jQuery.type(oDate) !== "date") {
 			throw new Error("Date must be a JavaScript date object; " + this);
 		}
 
