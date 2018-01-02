ff6a31ee6ccdf22c550eadd3c4b07ede12fa419e
Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
2018-01-02 10:20:56 +0100

    [FIX]sap.ui.rta: Local changes used when setting transports on reset
    When discarding the changes in RTA, we collect unsaved changes (local)
    with changes from LREP. The local changes should not be considered when
    setting the transport.
    
    CR-Id: 002075125800000026202018
    BCP: 1780513387
    
    Change-Id: Ia2453d1e8786d7c98ca33e01f9caafbdb61f7069
    Signed-off-by: Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
    (cherry picked from commit 2babb5a33f59a04a162620fe90cddda9838bdc11)

--- src/sap.ui.rta/test/sap/ui/rta/qunit/RuntimeAuthoring.qunit.js
+++ src/sap.ui.rta/test/sap/ui/rta/qunit/RuntimeAuthoring.qunit.js
@@ -20,6 +20,7 @@ sap.ui.require([
 	'sap/ui/fl/Change',
 	'sap/ui/fl/Utils',
 	'sap/ui/fl/FakeLrepLocalStorage',
+	'sap/ui/fl/transport/TransportSelection',
 	'sap/ui/rta/RuntimeAuthoring',
 	'sap/ui/rta/command/Stack',
 	'sap/ui/rta/command/CommandFactory',
@@ -51,6 +52,7 @@ sap.ui.require([
 	Change,
 	Utils,
 	FakeLrepLocalStorage,
+	TransportSelection,
 	RuntimeAuthoring,
 	Stack,
 	CommandFactory,
@@ -770,6 +772,35 @@ sap.ui.require([
 			assert.ok(fnHideBusyIndicatorSpy.calledOnce, "then the busy indicator is hidden");
 			assert.ok(fnReloadPageSpy.notCalled, "then the page does not reload");
 			assert.equal(oError, "Error", "and a message box shows the error to the user");
+			fnDone();
+		});
+
+		this.oRta._deleteChanges();
+	});
+
+	QUnit.test("when calling '_deleteChanges and there are 2 LREP changes together with 2 local changes', ", function(assert){
+		var fnSetTransportCalled = assert.async();
+		var fnDone = assert.async();
+
+		sandbox.stub(Settings.prototype, "isProductiveSystem").returns(false);
+		sandbox.stub(Settings.prototype, "hasMergeErrorOccured").returns(false);
+
+		sandbox.stub(this.oRta._getFlexController(), "getComponentChanges", function(){
+			return Promise.resolve(['change1', 'change2']);
+		});
+
+		sandbox.stub(TransportSelection.prototype, "setTransports", function(aChanges, oRootControl){
+			assert.equal(aChanges.length, 2, "then only the 2 persisted changes are passed to the transport");
+			fnSetTransportCalled();
+			return Promise.resolve();
+		});
+
+		sandbox.stub(this.oRta._getFlexController(), "discardChanges", function(aChanges){
+			assert.equal(aChanges.length, 4, "then all 4 changes are correctly passed to the Flex Controller for deletion");
+			return Promise.resolve();
+		});
+
+		sandbox.stub(this.oRta, "_reloadPage", function(){
 			fnDone();
 		});
 
