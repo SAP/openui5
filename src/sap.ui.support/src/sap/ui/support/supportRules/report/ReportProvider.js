893057ab2ad3a9a45521b43b260c635518cbc626
Svetoslav Spasov <svetoslav.spasov@sap.com>
2018-01-17 09:17:01 +0200

    [INTERNAL] sap.ui.support: Support Assistant report improvements
    * Added Audiences and Categories to the Available and selected rules
      section
    * Renamed the Issues table header "Title" to "Name"
    
    Change-Id: I50ce94ffb21639ac850c263538be2b0506602e37

--- src/sap.ui.support/src/sap/ui/support/supportRules/report/ReportProvider.js
+++ src/sap.ui.support/src/sap/ui/support/supportRules/report/ReportProvider.js
@@ -260,7 +260,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/handlebars', 'sap/ui/supp
 		try {
 			var groupNumber = 1;
 
-			content += '<table class="sapUiTable"><tbody><tr><th>Name</th><th>Description</th></tr></tbody>';
+			content += '<table class="sapUiTable"><tbody><tr><th>Name</th><th>Description</th><th>Categories</th><th>Audiences</th></tr></tbody>';
 
 			for (var group in groups) {
 				var rules = groups[group];
@@ -282,7 +282,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/handlebars', 'sap/ui/supp
 				for (var rule in rules) {
 					var issueCountElement = rules[rule].selected ? ' (' + rules[rule].issueCount + ' issues)' : '';
 					var checked = '<span class="checked" style="' + (rules[rule].selected ? '' : 'visibility: hidden;') + '"> &#10004; </span>';
-					rulesTable += '<tr><td>' + checked + rules[rule].title + issueCountElement + '</td><td>' + rules[rule].description + '</td></tr>';
+					rulesTable += '<tr>';
+					rulesTable += '<td>' + checked + rules[rule].title + issueCountElement + '</td>';
+					rulesTable += '<td>' + rules[rule].description + '</td>';
+					rulesTable += '<td>' + rules[rule].categories.join(', ') + '</td>';
+					rulesTable += '<td>' + rules[rule].audiences.join(', ') + '</td>';
+					rulesTable += '</tr>';
 				}
 
 				content += '<tbody id="section-selected-rules-group' + groupNumber + '">' + rulesTable + '</tbody>';
