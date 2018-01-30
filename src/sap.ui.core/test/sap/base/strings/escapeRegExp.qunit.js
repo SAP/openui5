21e3efa161b5b3bbff85eb23f4f2cf7e2a5a307f
Tobias Sorn <tobias.sorn@sap.com>
2018-01-30 09:22:32 +0100

    [INTERNAL] Revert "[INTERNAL] Modularization: sap.base modules"
    This reverts commit 87215d6a27b0ee03dd859cd1438cfadc30886e69.
    
    Change-Id: I1e59cdc2b96dc45e73a1f2db9dcb6a4378585b71

--- src/sap.ui.core/test/sap/base/strings/escapeRegExp.qunit.js
+++
@@ -1,8 +0,0 @@
-/* global require, QUnit */
-sap.ui.define(["sap/base/strings/escapeRegExp"], function(escapeRegExp) {
-	"use strict";
-
-	QUnit.test("EscapeRegExpOK", function (assert) {
-		assert.equal(escapeRegExp("ab.c"), "ab\\.c", "Dot character gets escaped");
-	});
-});
