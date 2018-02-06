95039c987144a27bf008bcb3fdd3de925b6c19b4
Sven Bernhard <sven.bernhard@sap.com>
2018-02-06 12:27:03 +0100

    [INTERNAL] sap.ui.model.odata.v4.ODataParentBinding: suspend/resume throws error if called on already/not suspended binding
    Change-Id: I94ec2cccee8a06845051ab29c33f75674a2dc015
    JIRA: CPOUI5UISERVICESV3-1140
    (cherry picked from commit b50cde5cc20c67d97dad9c445b206cfae8c70f69)
    BCP: n/a
    CR-Id: 002075125800001159282018

--- src/sap.ui.core/src/sap/ui/model/odata/v4/ODataParentBinding.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/ODataParentBinding.js
@@ -645,7 +645,7 @@ sap.ui.define([
 	 *
 	 * @throws {Error}
 	 *   If this binding is relative to a {@link sap.ui.model.odata.v4.Context} or if it is an
-	 *   operation binding
+	 *   operation binding or if it is not suspended
 	 *
 	 * @public
 	 * @see sap.ui.model.Binding#resume
@@ -664,7 +664,7 @@ sap.ui.define([
 		}
 
 		if (!this.bSuspended) {
-			return;
+			throw new Error("Cannot resume a not suspended binding: " + this);
 		}
 
 		this.bSuspended = false;
@@ -704,10 +704,12 @@ sap.ui.define([
 	 *
 	 * @throws {Error}
 	 *   If this binding is relative to a {@link sap.ui.model.odata.v4.Context} or if it is an
-	 *   operation binding
+	 *   operation binding or if it is already suspended or if it has pending changes
 	 *
 	 * @public
 	 * @see sap.ui.model.Binding#suspend
+	 * @see sap.ui.model.odata.v4.ODataContextBinding#hasPendingChanges
+	 * @see sap.ui.model.odata.v4.ODataListBinding#hasPendingChanges
 	 * @see #resume
 	 * @since 1.53.0
 	 */
@@ -718,6 +720,12 @@ sap.ui.define([
 		}
 		if (this.bRelative && (!this.oContext || this.oContext.fetchValue)) {
 			throw new Error("Cannot suspend a relative binding: " + this);
+		}
+		if (this.bSuspended) {
+			throw new Error("Cannot suspend a suspended binding: " + this);
+		}
+		if (this.hasPendingChanges()) {
+			throw new Error("Cannot suspend a binding with pending changes: " + this);
 		}
 
 		this.bSuspended = true;
