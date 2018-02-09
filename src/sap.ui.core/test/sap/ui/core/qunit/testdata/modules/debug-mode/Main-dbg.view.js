ba6259e542dec78b125f79426f777e62fc229f54
Benedikt Schoelch <benedikt.schoelch@sap.com>
2018-02-09 16:54:00 +0100

    [FIX] jquery.sap.global: configure ui5loader for debug mode
    With the modularization change 80b3ee3, the code that evaluates the
    sap-ui-debug option and propagates it to the ui5loader, was
    unintentionally removed.
    
    This change restores it and adds some basic tests for the debug
    mode.
    
    Note: Debug mode is still not implemented when async loading via script
    tags is activated.
    
    Change-Id: I4cd4d43cc30b10dddfa45d5f46903c303655ef88
    BCP: 1880151185
    CR-Id: 002075125800001117322018
    (cherry picked from commit f1a0eaeada5e5f09a09ddc223f2dd3da082e7759)

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/modules/debug-mode/Main-dbg.view.js
@@ -0,0 +1,9 @@
+sap.ui.define(function() {
+	sap.ui.jsview("modules.debug-mode.Main", {
+		getControllerName: function() {
+			return "modules.debug-mode.Main";
+		},
+		createContent: function() {
+		}
+	});
+});
