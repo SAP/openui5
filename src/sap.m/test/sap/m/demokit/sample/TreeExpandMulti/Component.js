e2c33696fe1a5cacc1af8ca0d121b7d302053274
Wenqian Wang <wenqian.wang@sap.com>
2018-02-14 15:46:26 +0100

    [FEATURE] sap.m.Tree: API to expand/collapse multiple nodes is added
    Change-Id: Ie65f7e92a96c826829aa4a74fb8c805a7d00d66f

--- src/sap.m/test/sap/m/demokit/sample/TreeExpandMulti/Component.js
+++ src/sap.m/test/sap/m/demokit/sample/TreeExpandMulti/Component.js
@@ -2,11 +2,11 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	function(UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("sap.m.sample.TreeCollapseAll.Component", {
+	var Component = UIComponent.extend("sap.m.sample.TreeExpandMulti.Component", {
 
 		metadata : {
 			rootView : {
-				"viewName": "sap.m.sample.TreeCollapseAll.Page",
+				"viewName": "sap.m.sample.TreeExpandMulti.Page",
 				"type": "XML",
 				"async": true
 			},
