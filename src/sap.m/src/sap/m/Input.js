51602def0996a3d05b735d4dfef46e5e35a70c21
Teodor Taushanov <teodor.taushanov@sap.com>
2018-02-14 17:04:03 +0200

    [FIX] sap.m.Input: SelectedItem is now correctly set when tap is pressed
    Usually the selection shouldn't happen on tap press,
    but it is a hard requirement here
    
    Change-Id: Ic32d5159c567d3ea58a33f42a1a629962ea9e8d0
    BCP: 1880065792
    CR-Id: 002075125900000238052018

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -687,6 +687,29 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 	};
 
 	/**
+	 * Updates selectedItem or selectedRow from the suggestion list or table.
+	 *
+	 * @private
+	 * @returns {boolean} Indicates if an item or row is selected
+	 */
+	Input.prototype._updateSelectionFromList = function () {
+		if (this._iPopupListSelectedIndex  < 0) {
+			return false;
+		}
+
+		var oSelectedItem = this._oList.getSelectedItem();
+		if (oSelectedItem) {
+			if (this._hasTabularSuggestions()) {
+				this.setSelectionRow(oSelectedItem, true);
+			} else {
+				this.setSelectionItem(oSelectedItem._oItem, true);
+			}
+		}
+
+		return true;
+	};
+
+	/**
 	 * Updates and synchronizes the <code>selectedItem</code> association and <code>selectedKey</code> properties.
 	 *
 	 * @name sap.m.Input.setSelectionItem
@@ -1510,20 +1533,7 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 		this.cancelPendingSuggest();
 
 		if (this._oSuggestionPopup && this._oSuggestionPopup.isOpen()) {
-			var oSelectedItem = this._oList.getSelectedItem();
-			if (oSelectedItem) {
-				if (this._hasTabularSuggestions()){
-					this.setSelectionRow(oSelectedItem, true);
-				} else {
-					this.setSelectionItem(oSelectedItem._oItem, true);
-				}
-			} else {
-				if (this._iPopupListSelectedIndex >= 0) {
-					this._fireSuggestionItemSelectedEvent();
-					this._doSelect();
-
-					this._iPopupListSelectedIndex = -1;
-				}
+			if (!this._updateSelectionFromList()) {
 				this._closeSuggestionPopup();
 			}
 		}
@@ -2188,9 +2198,9 @@ sap.ui.define(['jquery.sap.global', './Bar', './Dialog', './InputBase', './List'
 					initialFocus: oInput,
 					horizontalScrolling: true
 				}).attachAfterClose(function() {
-					if (oInput._iPopupListSelectedIndex  >= 0) {
-						oInput._fireSuggestionItemSelectedEvent();
-					}
+
+					oInput._updateSelectionFromList();
+
 					// only destroy items in simple suggestion mode
 					if (oInput._oList instanceof Table) {
 						oInput._oList.removeSelections(true);
