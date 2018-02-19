9fcd9bdb61a1ea8710dd7ab7e711ac8a93ad1752
Pavel Kornev <pavel.kornev@sap.com>
2018-02-19 21:27:22 +0100

    [INTERNAL] sap.ui.dt: fixed root overlay detection
    Change-Id: I5e93e0084cc4f625b4ada86e5991845566d00bb5

--- src/sap.ui.dt/src/sap/ui/dt/DesignTime.js
+++ src/sap.ui.dt/src/sap/ui/dt/DesignTime.js
@@ -553,7 +553,7 @@ function(
 				return this._mPendingOverlays[sElementId];
 			// 4. Create new ElementOverlay
 			} else {
-				if (typeof mParams.root === "undefined" && !mParams.element.getParent()) {
+				if (typeof mParams.root === "undefined" && !ElementUtil.getParent(mParams.element)) {
 					mParams.root = true;
 				}
 				this._mPendingOverlays[sElementId] = this._createElementOverlay(mParams)
@@ -730,6 +730,7 @@ function(
 						.map(function (oElement) {
 							return this.createOverlay({
 								element: oElement,
+								root: false,
 								parentMetadata: mAggregationMetadata
 							})
 								// If creation of one of the children is aborted, we still continue our execution
