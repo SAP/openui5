522cfa88a7468ff17237bb81408674fdcf497aa0
D062797 <lisa.klein@sap.com>
2018-03-01 09:30:14 +0100

    [INTERNAL] Rename emailType.js back to EmailType.js
    Classes should be named in upper case
    
    Change-Id: Id603435ca243bc83db373806d54f40eff0af8a21

--- src/sap.m/test/sap/m/demokit/cart/webapp/test/unit/model/EmailType.js
+++ src/sap.m/test/sap/m/demokit/cart/webapp/test/unit/model/EmailType.js
@@ -1,69 +1,69 @@
 /* global QUnit*/
 
 sap.ui.require([
-	"sap/ui/demo/cart/model/emailType"
+	"sap/ui/demo/cart/model/EmailType"
 ],
-function (emailType) {
+function (EmailType) {
 	"use strict";
-	QUnit.module("emailType - parsing");
+	QUnit.module("EmailType - parsing");
 	QUnit.test("Should throw an error when the E-Mail address is not valid", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue("inf");
+			new EmailType().validateValue("inf");
 		}, "Should throw an error when the E-Mail address is not valid");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue("info.bla");
+			new EmailType().validateValue("info.bla");
 		}, "Should throw an error was thrown when the E-Mail address is not valid");
 	});
 
 	QUnit.test("Should accept the value when it is a valid e-mail address", function (assert) {
 		// Act
-		new emailType().validateValue("info@sap.com");
+		new EmailType().validateValue("info@sap.com");
 		assert.ok(true, "no exception has happened");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid (edge case: empty field)", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue("");
+			new EmailType().validateValue("");
 		}, "Should throw an error when the E-Mail address is not valid (edge case: empty field)");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid (edge case: blank)", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue(" ");
+			new EmailType().validateValue(" ");
 		}, "Should throw an error when the E-Mail address is not valid (edge case: blank in field)");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid (edge case:no value property)", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue(undefined);
+			new EmailType().validateValue(undefined);
 		}, "Should throw an error when the E-Mail address is not valid (edge case:no value property)");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid (edge case: 5 characters)", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue("infor");
+			new EmailType().validateValue("infor");
 		}, "Should throw an error when the E-Mail address is not valid (edge case: 5 characters)");
 	});
 
 	QUnit.test("Should throw an error when the E-Mail address is not valid (edge case: 6 characters, but no valid format)", function (assert) {
 		// Act
 		assert.throws(function () {
-			new emailType().validateValue("in@f.o");
+			new EmailType().validateValue("in@f.o");
 		}, "Should throw an error when the E-Mail address is not valid (edge case: 6 characters, but no valid format)");
 	});
 
 	QUnit.test("Should accept the value when it is a valid e-mail address", function (assert) {
 		// Act
-		new emailType().validateValue("i@f.oo");
+		new EmailType().validateValue("i@f.oo");
 		assert.ok(true, "no exception has happened (6 characters value)");
 	});
 });
