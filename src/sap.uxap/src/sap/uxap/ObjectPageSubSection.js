6d0cb905e31859353f65052e67965cdbf3faa194
Marin Parushev <marin.parushev@sap.com>
2018-03-01 13:33:01 +0200

    [FIX] sap.uxap.ObjectPageSubSection: Fixed JS error in removeAggregation
    Private method _setAggregation was called with invalid signature.
    Corrected the signature so that the first parameter is a string.
    
    Change-Id: Ifaf40096c88faf0c837ff721d1f0b58e915476bd
    BCP: 1780191583

--- src/sap.uxap/src/sap/uxap/ObjectPageSubSection.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageSubSection.js
@@ -760,7 +760,7 @@ sap.ui.define([
 			aInternalAggregation.forEach(function (oObjectCandidate, iIndex) {
 				if (oObjectCandidate.getId() === oObject.getId()) {
 					aInternalAggregation.splice(iIndex, 1);
-					this._setAggregation(aInternalAggregation);
+					this._setAggregation(sAggregationName, aInternalAggregation);
 					bRemoved = true;
 				}
 				return !bRemoved;
