5c5206f4786ac3a0be96f9a94d2448166e3b371d
Vladislav Iliev <vladislav.iliev@sap.com>
2018-03-13 16:20:38 +0200

    [FIX] sap.m.TileContainer: after new page is added, scrollers remain visible.
    If the end-user is at second page, which is also last, and then some tiles
    are added so there is another page created, switching off the visibility
    of the TileContainer and then restoring it back to on, leads to disapeareance
    of the navigation buttons.
    
    The fix relies on the following logic:
    Whenever the pager is created from scratch, update of the scrollers is needed.
    
    (manually cherry picked from c3296c685651c79c44629b3cbc605ed6d4596be5)
    
    Change-Id: Iad5d81e5b0f30516e02889bbef1768dee65b44a2
    BCP: 1870041307
    CR-Id: 002075125800001677502018

--- src/sap.m/src/sap/m/TileContainer.js
+++ src/sap.m/src/sap/m/TileContainer.js
@@ -470,6 +470,10 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 				currentPageIsFirstChanged: function() {
 					return this.currentPageIsFirst() !== this.oldCurrentPageIsFirst();
 				},
+				/* true if current page's relative position is changed - the page becomes first, last or was first or last and now it is not*/
+				currentPageRelativePositionChanged: function() {
+					return this.currentPageIsFirstChanged() || this.currentPageIsLastChanged();
+				},
 				pageCountChanged: function() {
 					return iCount !== iOldCount;
 				},
@@ -1193,7 +1197,9 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 		var oPager,
 			oScrollLeft,
 			oScrollRight,
-			aHTML;
+			aHTML,
+			/* true if the pager is created as part of this function*/
+			bPagerJustCreated = false;
 
 		if (!this._oPagesInfo.pageCountChanged() && !this._oPagesInfo.currentPageChanged()) {
 			return;
@@ -1223,6 +1229,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 			oPager.style.display = "block";
 			oPager.childNodes[0].className = "sapMTCActive"; //initially active page is the 1st(span)
 			this._oPagesInfo.setPagerCreated(true);
+			bPagerJustCreated = true;
 		} else if (this._oPagesInfo.pageCountChanged()) {
 			if (this._oPagesInfo.getCount() - this._oPagesInfo.getOldCount() < 0) {//one page less
 				oPager.removeChild(oPager.lastChild);
@@ -1240,8 +1247,8 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/ui/core/Control', 'sap/ui/
 				oPager.childNodes[0].className = "";
 			}
 		}
-		if (sap.ui.Device.system.desktop &&
-			(this._oPagesInfo.currentPageIsFirstChanged() || this._oPagesInfo.currentPageIsLastChanged())) {
+
+		if (sap.ui.Device.system.desktop && (bPagerJustCreated || this._oPagesInfo.currentPageRelativePositionChanged())) {
 			if (this._bRtl) {
 				// Less builder swaps left and right in RTL styles,
 				// and that is not required here, otherwise left scroller will go right and vice versa.
