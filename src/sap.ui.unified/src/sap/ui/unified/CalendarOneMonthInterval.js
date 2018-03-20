430f550fa90900630f39781a797b43ac4dee3b47
Tereza Chobanova <tereza.chobanova@sap.com>
2018-03-20 16:30:12 +0200

    [FIX] sap.m.PlanningCalendar: Navigation in one month view works propperly
    Having a one month view with less than 31 days the control used to show
    an incorrect information when navigating with the keyboard arrows to the
    first/last date of the next/previous month which was also shown in an
    incorrect color.
    
    BCP: 1770572211
    BCP: 1770572146
    BCP: 1770543942
    Change-Id: I126a2d48981e7045b3db256fdab14549d8fbc96d

--- src/sap.ui.unified/src/sap/ui/unified/CalendarOneMonthInterval.js
+++ src/sap.ui.unified/src/sap/ui/unified/CalendarOneMonthInterval.js
@@ -182,10 +182,8 @@ sap.ui.define([
 			oOneMonthDatesRow.setDate(oLocalFocusDate);//really focus the given date
 			oOneMonthDatesRow._bNoRangeCheck = false;
 
-			//we need this to notify the planning calendar to update its rows
-			if (this.getSelectedDates().length) {//renders the appointments for the selected date, not focused one
-				this._setRowsStartDate(this.getSelectedDates()[0].getStartDate());
-			}
+			/* Planning Calendar is already notified about startDateChange event, so no need to manually update its
+			 row's startDate like we previously did  */
 
 			this._oFocusDateOneMonth = null;
 
