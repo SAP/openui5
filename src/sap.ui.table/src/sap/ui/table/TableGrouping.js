4c4fc65a89f419fdeb2b6a3c28f87fae3a606cc1
Jens Pflueger <jens.pflueger@sap.com>
2018-03-23 10:42:08 +0100

    [FIX] AnalyticalTable: Sum Row indent and hover effect corrected
    * Indent of the sum row should be similar to the indent of the
    corresponding group header.
    * No hover effect on the "row selector" on sum rows so far
    
    Change-Id: If739be66184b5ae74a0ba95440962e7345f33c2f
    BCP: 1770584241
    CR-Id: 002075125900000726492018

--- src/sap.ui.table/src/sap/ui/table/TableGrouping.js
+++ src/sap.ui.table/src/sap/ui/table/TableGrouping.js
@@ -290,9 +290,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Sorter'
 		 * @param {sap.ui.table.Table} oTable Instance of the table
 		 * @param {number} iLevel the hierarchy level
 		 * @param {boolean} bChildren whether the row is a group (has children) or not
+		 * @param {boolean} bSum whether the row is a summary row
 		 * @private
 		 */
-		_calcGroupIndent : function(oTable, iLevel, bChildren) {
+		_calcGroupIndent : function(oTable, iLevel, bChildren, bSum) {
 			if (TableGrouping.TableUtils.isInstanceOf(oTable, "sap/ui/table/TreeTable")) {
 				var iIndent = 0;
 				for (var i = 0; i < iLevel; i++) {
@@ -302,7 +303,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Sorter'
 			} else if (TableGrouping.TableUtils.isInstanceOf(oTable, "sap/ui/table/AnalyticalTable")) {
 				var iIndent = 0;
 				iLevel = iLevel - 1;
-				iLevel = !bChildren ? iLevel - 1 : iLevel;
+				iLevel = !bChildren && !bSum ? iLevel - 1 : iLevel;
 				iLevel = Math.max(iLevel, 0);
 				for (var i = 0; i < iLevel; i++) {
 					if (iIndent == 0) {
@@ -355,7 +356,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Sorter'
 		 * @param {boolean} bChildren whether the row is a group (has children) or not
 		 * @param {boolean} bExpanded whether the row should be expanded
 		 * @param {boolean} bExpanded whether the row content should be hidden
-		 * @param {boolean} bChildren whether the row should be a summary row
+		 * @param {boolean} bSum whether the row should be a summary row
 		 * @param {number} iLevel the hierarchy level
 		 * @param {string} sGroupHeaderText the title of the group header
 		 * @private
@@ -386,7 +387,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Element', 'sap/ui/model/Sorter'
 					.attr("title", oTable._getShowStandardTooltips() && sGroupHeaderText ? sGroupHeaderText : null)
 					.text(sGroupHeaderText || "");
 
-				TableGrouping._setIndent(oTable, $Row, $RowHdr, TableGrouping._calcGroupIndent(oTable, iLevel, bChildren));
+				TableGrouping._setIndent(oTable, $Row, $RowHdr, TableGrouping._calcGroupIndent(oTable, iLevel, bChildren, bSum));
 			}
 
 			var $TreeIcon = null;
