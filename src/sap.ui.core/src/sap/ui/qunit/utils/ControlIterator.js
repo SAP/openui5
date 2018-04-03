40a25d66311b6408d8c3fe4e30e5625bfe091b6f
Adrian Bobev <adrian.bobev@sap.com>
2018-04-03 15:45:21 +0300

    [FIX] sap.m.StepInput: do not create global NumericInput
    StepInput creates an internal class NumericInput and did not
    prefix it with sap.m. This created and global variable NumericInput.
    Now we prefix this with sap.m.internal.
    Introduced by I73a9aa5d3765106013255ff346d7cfa2fae5e750
    
    Change-Id: I8de530cef48eef0bf464d8251630f8ba8c190cb2
    BCP: 1870117517
    CR-Id: 002075125900000879952018

--- src/sap.ui.core/src/sap/ui/qunit/utils/ControlIterator.js
+++ src/sap.ui.core/src/sap/ui/qunit/utils/ControlIterator.js
@@ -75,6 +75,7 @@ sap.ui.define([ 'jquery.sap.global', 'sap/ui/core/Core', 'sap/ui/base/Object', '
 		"sap.m.Menu",
 		"sap.m.NotificationListItem",
 		"sap.m.NotificationListBase",
+		"sap.m.internal.NumericInput",
 		"sap.m.QuickViewBase",
 		"sap.m.QuickViewGroup",
 		"sap.m.QuickViewGroupElement",
