29ad772b9311cdfcebfba6634e4da229c00194ff
Diana Mihaylova <diana.mihaylova@sap.com>
2018-04-04 17:59:01 +0300

    [FIX] sap.m.DatePicker: Now in IE & touch devices the focus is correct after closing the picker
    Before this fix, when closing the picker, the input-inner was focused. This should not happen on touch devices.
    
    This fixes the failing qUnits of sap.m.DatePicker and sap.m.DateRangeSelection.
    
    Change-Id: I7b2e7419a0d640876aa3768ed40a87ac72fd6b71
    BCP:1870040613

--- src/sap.m/src/sap/m/DatePicker.js
+++ src/sap.m/src/sap/m/DatePicker.js
@@ -852,7 +852,7 @@ sap.ui.define([
 	};
 
 	DatePicker.prototype._storeInputSelection = function (oInput) {
-		if (Device.browser.msie || Device.browser.edge) {
+		if ((Device.browser.msie || Device.browser.edge) && !Device.support.touch) {
 			//For IE & Edge, any selection of the underlying input must be removed before opening the picker popup,
 			//otherwise the input will receive focus via TAB during the picker is opened. The selection is restored back
 			//when the popup is closed
@@ -866,7 +866,7 @@ sap.ui.define([
 	};
 
 	DatePicker.prototype._restoreInputSelection = function (oInput) {
-		if (Device.browser.msie || Device.browser.edge) {
+		if ((Device.browser.msie || Device.browser.edge) && !Device.support.touch) {
 			//The selection is restored back due to issue with IE & Edge. See _handleBeforeOpen
 			oInput.selectionStart = this._oInputSelBeforePopupOpen.iStart;
 			oInput.selectionEnd = this._oInputSelBeforePopupOpen.iEnd;
