a1f1b6a5cb3b77b7a43e558cf94bc02eb16adbaa
Matthias Osswald <mat.osswald@sap.com>
2018-04-10 11:28:03 +0200

    [FIX] jQuery.sap.includeStylesheet: Correct comparison with existing href
    The comparison of the old and new href didn't correctly work in all
    cases. This caused reloading or invalidation of stylesheets as the
    actual URL (or href) was the same but the <link> tag was still replaced.
    When accessing the "href" of a <link> tag, the browser already resolves
    the given href as absolute URL which can be compared against the
    existing href.
    This also makes the use of the URI.js library obsolete.
    
    BCP: N/A
    CR-Id: 002075125900001060752018
    Change-Id: Iddc60409b09221e71afb696b4e1f174c925b3c6c

--- src/sap.ui.core/src/jquery.sap.global.js
+++ src/sap.ui.core/src/jquery.sap.global.js
@@ -5022,7 +5022,7 @@
 		var oLink = _createLink(sUrl, mAttributes, fnLoadCallback, fnErrorCallback);
 		if (oOld && oOld.tagName === "LINK" && oOld.rel === "stylesheet") {
 			// link exists, so we replace it - but only if a callback has to be attached or if the href will change. Otherwise don't touch it
-			if (fnLoadCallback || fnErrorCallback || oOld.href !== resolveURL(sUrl)) {
+			if (fnLoadCallback || fnErrorCallback || oOld.href !== oLink.href) {
 				// if the attribute "data-sap-ui-foucmarker" exists and the value
 				// matches the id of the new link the new link will be put
 				// before the old link into the document and the id attribute
