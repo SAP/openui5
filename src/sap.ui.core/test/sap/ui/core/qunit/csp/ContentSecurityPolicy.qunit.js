7190a21086a81db9bdfba8fbc7bc304937bd8e0a
Dominik Garrecht <dominik.garrecht@sap.com>
2018-04-10 13:01:03 +0200

    [INTERNAL] Skip CSP QUnit Test for IE and Edge Browser
    As IE and Edge expect a non-standard CSP header
    which we currently not support, the QUnit test should
    be skipped (should be marked as successful) for these
    browsers.
    
    PS2: Add dummy test for IE and edge case
    
    BCP: 1870141299
    Change-Id: Ibaa2cebbd8d46d47af7d2bb1b7278d2998016f88

--- src/sap.ui.core/test/sap/ui/core/qunit/csp/ContentSecurityPolicy.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/csp/ContentSecurityPolicy.qunit.js
@@ -1,49 +1,54 @@
-sap.ui.define([], function() {
+sap.ui.define(["sap/ui/Device"], function(Device) {
 
 	"use strict";
 	/*global QUnit*/
 
 	QUnit.module("CSP Compliance");
 
-	QUnit.test("Check for occured CSP Violations", function(assert) {
-		var done = assert.async(),
-			sErrorMsg = "Please make sure to execute the ContentSecurityPolicy.qunit.html\n test with removing the sap-ui-debug " +
-				"URL parameter (if present) and with adding the following URL parameters\n sap-ui-xx-libraryPreloadFiles=none and " + "sap-ui-xx-csp-policy=almost-default:report-only (if not yet present) to\n not load library preload files and to " +
-				"tell the web server to set required CSP headers.";
+	if (Device.browser.msie || Device.browser.edge) {
+		QUnit.test("Skip CSP Test", function (assert) {
+			assert.ok(true, "Skip actual CSP Test for MS Internet Explorer and Edge Browser, as they expect non-standard CSP Headers");
+		});
+	} else {
+		QUnit.test("Check for occured CSP Violations", function (assert) {
+			var done = assert.async(),
+				sErrorMsg = "Please make sure to execute the ContentSecurityPolicy.qunit.html\n test with removing the sap-ui-debug " +
+					"URL parameter (if present) and with adding the following URL parameters\n sap-ui-xx-libraryPreloadFiles=none and " + "sap-ui-xx-csp-policy=almost-default:report-only (if not yet present) to\n not load library preload files and to " +
+					"tell the web server to set required CSP headers.";
 
-		// Make sure that the CSP headers (CSP or CSP-Report-Only) are set by web server
-		// and that the test fails in case none of them is set.
+			// Make sure that the CSP headers (CSP or CSP-Report-Only) are set by web server
+			// and that the test fails in case none of them is set.
 
-		var oReq = new XMLHttpRequest();
-		oReq.open("GET", document.documentURI, true);
-		oReq.send();
+			var oReq = new XMLHttpRequest();
+			oReq.open("GET", document.documentURI, true);
+			oReq.send();
 
-		oReq.onreadystatechange = function () {
-			if (this.readyState == this.HEADERS_RECEIVED) {
-				var sCsp = oReq.getResponseHeader("Content-Security-Policy");
-				var sCspReportOnly = oReq.getResponseHeader("Content-Security-Policy-Report-Only");
+			oReq.onreadystatechange = function () {
+				if (this.readyState == this.HEADERS_RECEIVED) {
+					var sCsp = oReq.getResponseHeader("Content-Security-Policy");
+					var sCspReportOnly = oReq.getResponseHeader("Content-Security-Policy-Report-Only");
 
-				if (sCsp || sCspReportOnly) {
-					// Check for reported CSP violations
-					assert.ok(sap.ui.getCore(), "UI5 Core has been booted");
-					assert.ok(window["ui5-core-csp-violations"] && window["ui5-core-csp-violations"].length === 0,
-						"CSP compliant - Just in case the test fails: " + sErrorMsg
-					);
+					if (sCsp || sCspReportOnly) {
+						// Check for reported CSP violations
+						assert.ok(sap.ui.getCore(), "UI5 Core has been booted");
+						assert.ok(window["ui5-core-csp-violations"] && window["ui5-core-csp-violations"].length === 0,
+							"CSP compliant - Just in case the test fails: " + sErrorMsg
+						);
 
-					done();
-				} else {
-					// Fail test as headers are not set as required
-					oReq.abort();
-					assert.ok(
-						false,
-						"CSP headers are not set by web server as required for this test. " +
-						sErrorMsg + "\n There might be also a server-side problem which leeds\n to not setting headers correctly."
-					);
+						done();
+					} else {
+						// Fail test as headers are not set as required
+						oReq.abort();
+						assert.ok(
+							false,
+							"CSP headers are not set by web server as required for this test. " +
+							sErrorMsg + "\n There might be also a server-side problem which leeds\n to not setting headers correctly."
+						);
 
-					done();
+						done();
+					}
 				}
-			}
-		};
-	});
-
+			};
+		});
+	}
 });
