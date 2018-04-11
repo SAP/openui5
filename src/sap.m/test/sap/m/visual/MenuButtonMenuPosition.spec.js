c3d561bf07f2375de3c888da2bc4b7ca7adf4dc8
i310817 <gergana.marcheva@sap.com>
2018-04-11 10:10:05 +0300

    [INTERNAL] sap.m.MenuButton: rearrange visual test page
    Change-Id: I22513df8912d035aa8d51c1c6cdafab59c5cf140

--- src/sap.m/test/sap/m/visual/MenuButtonMenuPosition.spec.js
+++ src/sap.m/test/sap/m/visual/MenuButtonMenuPosition.spec.js
@@ -1,111 +1,65 @@
-/*global describe,it,element,by,takeScreenshot,expect,browser*/
+/*global describe,it,element,by,takeScreenshot,expect,browser,protractor*/
 
 describe("sap.m.MenuButtonMenuPosition", function() {
 	"use strict";
 
 	browser.testrunner.currentSuite.meta.controlName = 'sap.m.MenuButton';
 
+	var fnClickThenCompare = function (bPosButtonClick, sTestMessage, sImageName) {
+		it(sTestMessage, function () {
+			if (bPosButtonClick) {
+				element(by.id("posButtonId")).click();
+			}
+			element(by.id("posMenuId")).click();
+			if (browser.testrunner.runtime.platformName != "android" && browser.testrunner.runtime.platformName != "ios" && browser.testrunner.runtime.browserName != "safari") {
+				// only on desktop and not in Safari (sendKeys needs an Element in Safari, so test makes no sense there)
+				browser.actions().sendKeys(protractor.Key.ARROW_UP).perform();
+				expect(takeScreenshot()).toLookAs(sImageName);
+			}
+		});
+	};
 	// verify MenuButton is opened and the menu position is BeginBottom
-	it("should open MenuButton in position BeginBottom", function() {
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_BeginBottom_position");
-	});
+	fnClickThenCompare(false, "should open MenuButton in position BeginBottom", "menu_in_BeginBottom_position");
 
 	// verify MenuButton is opened and the menu position is BeginTop
-	it("should open MenuButton in position BeginTop", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_BeginTop_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position BeginTop", "menu_in_BeginTop_position");
 
 	// verify MenuButton is opened and the menu position is BeginCenter
-	it("should open MenuButton in position BeginCenter", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_BeginCenter_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position BeginCenter", "menu_in_BeginCenter_position");
 
 	// verify MenuButton is opened and the menu position is LeftTop
-	it("should open MenuButton in position LeftTop", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_LeftTop_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position LeftTop", "menu_in_LeftTop_position");
 
 	// verify MenuButton is opened and the menu position is LeftCenter
-	it("should open MenuButton in position LeftCenter", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_LeftCenter_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position LeftCenter", "menu_in_LeftCenter_position");
 
 	// verify MenuButton is opened and the menu position is LeftBottom
-	it("should open MenuButton in position LeftBottom", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_LeftBottom_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position LeftBottom", "menu_in_LeftBottom_position");
 
 	// verify MenuButton is opened and the menu position is CenterTop
-	it("should open MenuButton in position CenterTop", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_CenterTop_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position CenterTop", "menu_in_CenterTop_position");
 
 	// verify MenuButton is opened and the menu position is LeftTop
-	it("should open MenuButton in position CenterCenter", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_CenterCenter_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position CenterCenter", "menu_in_CenterCenter_position");
 
 	// verify MenuButton is opened and the menu position is CenterBottom
-	it("should open MenuButton in position CenterBottom", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_CenterBottom_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position CenterBottom", "menu_in_CenterBottom_position");
 
 	// verify MenuButton is opened and the menu position is RightTop
-	it("should open MenuButton in position RightTop", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_RightTop_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position RightTop", "menu_in_RightTop_position");
 
 	// verify MenuButton is opened and the menu position is RightCenter
-	it("should open MenuButton in position RightCenter", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_RightCenter_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position RightCenter", "menu_in_RightCenter_position");
 
 	// verify MenuButton is opened and the menu position is RightBottom
-	it("should open MenuButton in position RightBottom", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_RightBottom_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position RightBottom", "menu_in_RightBottom_position");
 
 	// verify MenuButton is opened and the menu position is EndTop
-	it("should open MenuButton in position EndTop", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_EndTop_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position EndTop", "menu_in_EndTop_position");
 
 	// verify MenuButton is opened and the menu position is EndCenter
-	it("should open MenuButton in position EndCenter", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_EndCenter_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position EndCenter", "menu_in_EndCenter_position");
 
 	// verify MenuButton is opened and the menu position is EndBottom
-	it("should open MenuButton in position EndBottom", function() {
-		element(by.id("posButtonId")).click();
-		element(by.id("posMenuId")).click();
-		expect(takeScreenshot()).toLookAs("menu_in_EndBottom_position");
-	});
+	fnClickThenCompare(true, "should open MenuButton in position EndBottom", "menu_in_EndBottom_position");
 });
