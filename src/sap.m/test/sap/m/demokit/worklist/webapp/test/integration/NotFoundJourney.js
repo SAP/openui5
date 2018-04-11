ada2797da869c2ed974f8fd0e9187704ad2a7970
Ayham <ayham.tanbari@sap.com>
2018-04-11 17:59:16 +0200

    [INTERNAL] worklist,master-detail: downport generated templates to 1.52
    After fixing and testing templates in sapui5.dist project, we generated
     master and worklist templates for openui5 project, so we have the same
     code in booth
    
    Change-Id: I772111d5b2eddb6a05c2049fe0d0b4edf480ea4d
    CR-Id: 002075125900001119062018
    BCP: n/a

--- src/sap.m/test/sap/m/demokit/worklist/webapp/test/integration/NotFoundJourney.js
+++ src/sap.m/test/sap/m/demokit/worklist/webapp/test/integration/NotFoundJourney.js
@@ -29,9 +29,6 @@ sap.ui.define([
 	});
 
 	opaTest("Should see the not found text for no search results", function (Given, When, Then) {
-		// Arrangements
-		Given.iStartMyApp();
-
 		//Actions
 		When.onTheWorklistPage.iSearchForSomethingWithNoResults();
 
@@ -44,17 +41,16 @@ sap.ui.define([
 		When.onTheBrowser.iPressOnTheBackwardsButton();
 
 		// Assertions
-		Then.onTheNotFoundPage.iShouldSeeResourceNotFound().
-			and.iTeardownMyAppFrame();
+		Then.onTheNotFoundPage.iShouldSeeResourceNotFound();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Should see the 'Object not found' page if an invalid object id has been called", function (Given, When, Then) {
 		Given.iStartMyApp({
 			hash: "/Objects/SomeInvalidObjectId"
 		});
-
-		//Actions
-		When.onTheNotFoundPage.iLookAtTheScreen();
 
 		// Assertions
 		Then.onTheNotFoundPage.iShouldSeeObjectNotFound();
@@ -66,8 +62,10 @@ sap.ui.define([
 		When.onTheNotFoundPage.iPressTheObjectNotFoundShowWorklistLink();
 
 		// Assertions
-		Then.onTheWorklistPage.iShouldSeeTheTable().
-			and.iTeardownMyAppFrame();
+		Then.onTheWorklistPage.iShouldSeeTheTable();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 });
