b9b020ba62c6d26016316a79af3d104db13e2b8f
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2018-04-19 16:32:11 +0200

    [INTERNAL] sap.ui.model.odata.v4.lib._AggregationCache: forbid $filter
    PS1: PP PBU
    PS2: comments by SBE
    PS3: rebased
    PS4: comments by AMR
    
    Change-Id: I875a20bb1b843d85a2fb8383508724947830e5b5
    JIRA: CPOUI5UISERVICESV3-1267

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/DataAggregation/DataAggregation.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/DataAggregation/DataAggregation.controller.js
@@ -2,11 +2,8 @@
  * ${copyright}
  */
 sap.ui.define([
-	"sap/m/MessageBox",
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (MessageBox, Controller, Filter, FilterOperator) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.sample.odata.v4.DataAggregation.DataAggregation", {
@@ -15,44 +12,6 @@ sap.ui.define([
 
 			oTable.setBindingContext(oTable.getBinding("rows").getHeaderContext(), "headerContext");
 			oTable.setModel(oTable.getModel(), "headerContext");
-		},
-
-		onInit : function () {
-			var bMessageOpen = false,
-				oMessageManager = sap.ui.getCore().getMessageManager(),
-				oMessageModel = oMessageManager.getMessageModel();
-
-			this.oMessageModelBinding = oMessageModel.bindList("/", undefined,
-				[], new Filter("technical", FilterOperator.EQ, true));
-
-			this.oMessageModelBinding.attachChange(function (oEvent) {
-				var aContexts = oEvent.getSource().getContexts(),
-					aMessages = [],
-					sPrefix;
-
-				if (bMessageOpen || !aContexts.length) {
-					return;
-				}
-
-				// Extract and remove the technical messages
-				aContexts.forEach(function (oContext) {
-					aMessages.push(oContext.getObject());
-				});
-				oMessageManager.removeMessages(aMessages);
-
-				// Due to batching there can be more than one technical message. However the UX
-				// guidelines say "display a single message in a message box" assuming that there
-				// will be only one at a time.
-				sPrefix = aMessages.length === 1 ? ""
-					: "There have been multiple technical errors. One example: ";
-				MessageBox.error(sPrefix + aMessages[0].message, {
-					id : "serviceErrorMessageBox",
-					onClose: function () {
-						bMessageOpen = false;
-					}
-				});
-				bMessageOpen = true;
-			});
 		}
 	});
 });
