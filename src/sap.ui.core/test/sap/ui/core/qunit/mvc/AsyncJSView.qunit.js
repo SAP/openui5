6c6c24cb7191709ed3366f2b29bebfa0c92bc58c
Jan Mummenthaler <jan.mummenthaler@sap.com>
2018-04-24 12:44:53 +0200

    [FEATURE] JSView: Add new async only create() API
    The old sap.ui.jsview factory is deprecated
    
    Change-Id: I7e5949822399277b51e67f1a9cc188011bb65b81
    JIRA: CPOUIFPHOENIXCORE-2521

--- src/sap.ui.core/test/sap/ui/core/qunit/mvc/AsyncJSView.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/mvc/AsyncJSView.qunit.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-	"./AnyViewAsync.qunit"
-], function(asyncTestsuite) {
+	"./AnyViewAsync.qunit",
+	"sap/ui/core/mvc/JSView"
+], function(asyncTestsuite, JSView) {
 
+	"use strict";
 	// setup test config with generic factory
 	var oConfig = {
 		type : "JS",
@@ -24,4 +26,20 @@ sap.ui.define([
 	};
 	asyncTestsuite("JSView Factory", oConfig);
 
+	QUnit.test("New create factory", function(assert) {
+		assert.expect(3);
+		var done = assert.async();
+		var that = this;
+		this.oLogMock = this.mock(jQuery.sap.log);
+		this.oLogMock.expects("warning").never();
+
+		JSView.create({
+			viewName : "testdata.mvc.Async"
+		})
+		.then(function(oViewLoaded) {
+			assert.equal(that.oAfterInitSpy.callCount, 1, "AfterInit event fired before resolving");
+			assert.ok(oViewLoaded instanceof JSView, "Views equal deeply");
+			done();
+		});
+	});
 });
