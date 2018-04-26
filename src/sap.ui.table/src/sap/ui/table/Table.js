12482a36c03d910e25d609c3bb0273803c20ff44
Alexander Deutschmann <alexander.deutschmann@sap.com>
2018-04-26 16:57:58 +0200

    [FIX] Table: Correctly update the visibility of the NoData overlay
    The issue occurs, if refreshRows is called after bindRows when the table
    is already rendered. The visibility of the NoData overlay is not
    correctly updated then.
    
    Change-Id: I65122a23127437b9dbc0361d5a269f7677102380
    BCP: 1770364678
    CR-Id: 002075125900001556122018

--- src/sap.ui.table/src/sap/ui/table/Table.js
+++ src/sap.ui.table/src/sap/ui/table/Table.js
@@ -2225,10 +2225,12 @@ sap.ui.define([
 				});
 			}
 			// request contexts from binding
+			var bSuppressUpdate = false;
 			if (sReason === ChangeReason.Filter || sReason === ChangeReason.Sort) {
 				this.setFirstVisibleRow(0);
+				bSuppressUpdate = true;
 			}
-			this._updateBindingContexts(this._calculateRowsToDisplay(), true);
+			this._updateBindingContexts(this._calculateRowsToDisplay(), bSuppressUpdate);
 		}
 	};
 
