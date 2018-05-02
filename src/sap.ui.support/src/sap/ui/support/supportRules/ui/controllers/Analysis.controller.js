102d4faa16a572048e75a37770544fb4fb5a9a9c
Jordan Dichev <jordan.dichev@sap.com>
2018-05-02 14:20:14 +0300

    [INTERNAL] sap.ui.support: Analysis controller refactoring
    Change-Id: If789ced9ef8a48a106ae862820d083e73f6ec937

--- src/sap.ui.support/src/sap/ui/support/supportRules/ui/controllers/Analysis.controller.js
+++ src/sap.ui.support/src/sap/ui/support/supportRules/ui/controllers/Analysis.controller.js
@@ -19,12 +19,13 @@ sap.ui.define([
 	"sap/ui/support/supportRules/WindowCommunicationBus",
 	"sap/ui/support/supportRules/WCBChannels",
 	"sap/ui/support/supportRules/ui/models/SharedModel",
+	"sap/ui/support/supportRules/ui/models/SelectionUtils",
 	"sap/ui/support/supportRules/RuleSerializer",
 	"sap/ui/support/supportRules/Constants",
 	"sap/ui/support/supportRules/RuleSet",
 	"sap/ui/support/supportRules/Storage"
 ], function ($, BaseController, JSONModel, Panel, List, ListItemBase, StandardListItem, InputListItem, Button, Toolbar, ToolbarSpacer,
-	Label, MessageToast, CommunicationBus, channelNames, SharedModel, RuleSerializer, constants, Ruleset, storage) {
+	Label, MessageToast, CommunicationBus, channelNames, SharedModel, SelectionUtils, RuleSerializer, constants, Ruleset, storage) {
 	"use strict";
 
 
@@ -36,7 +37,7 @@ sap.ui.define([
 			this.tempRulesLoaded = false;
 
 			this.getView().setModel(this.model);
-			this.treeTable = this.byId("ruleList");
+			this.treeTable = SelectionUtils.treeTable = this.byId("ruleList");
 			this.ruleSetView = this.byId("ruleSetsView");
 			this.rulesViewContainer = this.byId("rulesNavContainer");
 
@@ -140,29 +141,6 @@ sap.ui.define([
 			}
 		},
 
-		updateTreeViewTempRulesSelection: function (treeTableTempLibrary) {
-			this.treeTable.expand(0);
-
-			for (var ruleIndex in treeTableTempLibrary) {
-
-				var iRuleIndex = Number.parseInt(ruleIndex, 10);
-
-				if (!Number.isInteger(iRuleIndex)) {
-					continue;
-				}
-
-				iRuleIndex++;
-
-				var rule = treeTableTempLibrary[ruleIndex];
-
-				if (rule.selected) {
-					this.treeTable.addSelectionInterval(iRuleIndex, iRuleIndex);
-				} else {
-					this.treeTable.removeSelectionInterval(iRuleIndex, iRuleIndex);
-				}
-			}
-		},
-
 		setCommunicationSubscriptions: function () {
 			CommunicationBus.subscribe(channelNames.UPDATE_SUPPORT_RULES, this.updatesupportRules, this);
 
@@ -192,7 +170,7 @@ sap.ui.define([
 					this.goToRuleProperties();
 					this.model.setProperty("/selectedRule", newRule);
 
-					this.updateTreeViewTempRulesSelection(treeTableTempLibrary);
+					SelectionUtils.updateTreeViewTempRulesSelection(treeTableTempLibrary);
 					this._updateRuleList();
 				} else {
 					MessageToast.show("Add rule failed because: " + result);
@@ -265,10 +243,10 @@ sap.ui.define([
 				if (bInitialLoading) {
 					this.model.setProperty("/initialRulesLoading", false);
 					this.model.setProperty("/treeViewModel", oTreeViewModelRules);
-					this.initializeSelection(bInitialLoading);
+					SelectionUtils.initializeSelection(bInitialLoading);
 				} else {
-					this._syncSelections(oTreeViewModelRules);
-					this.initializeSelection();
+					SelectionUtils._syncSelections(oTreeViewModelRules);
+					SelectionUtils.initializeSelection();
 				}
 			}, this);
 
@@ -832,445 +810,9 @@ sap.ui.define([
 			return sourceObject;
 		},
 
-		onToggleOpenState: function (oEvent) {
-			var iIndex = oEvent.getParameter("rowIndex"),
-				oRowContext = oEvent.getParameter("rowContext"),
-				bExpanded = oEvent.getParameter("expanded"),
-				oTreeTable = this.treeTable,
-				aRows = oTreeTable.getRows(),
-				iVisualIndex = this.getVisualIndex(oRowContext),
-				oRow = aRows[iVisualIndex],
-				oRowModel = oRow.getModel(),
-				oBindingContext = oRow.getBindingContext(),
-				sPath = oBindingContext.getPath(),
-				mRules = oRowModel.getProperty(sPath),
-				sKey,
-				bSelected,
-				iRuleIndex = iIndex + 1;
+		onToggleOpenState: SelectionUtils.toggleOpenStateHandler.bind(SelectionUtils),
 
-			if (!bExpanded) {
-				return;
-			}
-
-			for (sKey in mRules) {
-				if (Number.isInteger(Number.parseInt(sKey, 10))) {
-					bSelected = oRowModel.getProperty(sPath + "/" + sKey + "/selected");
-
-					if (bSelected) {
-						oTreeTable.addSelectionInterval(iRuleIndex, iRuleIndex);
-					} else {
-						oTreeTable.removeSelectionInterval(iRuleIndex, iRuleIndex);
-					}
-
-					iRuleIndex++;
-				}
-			}
-		},
-
-		onRowSelectionChange: function (oEvent) {
-			var iIndex = oEvent.getParameter("rowIndex"),
-				oRowContext = oEvent.getParameter("rowContext"),
-				aRowIndices = oEvent.getParameter("rowIndices"),
-				bUserInteraction = oEvent.getParameter("userInteraction");
-
-			if (oEvent.getParameter("selectAll")) {
-				this.selectAllRows(true);
-				return;
-			}
-
-			if (iIndex === -1) {
-				this.selectAllRows(false);
-				return;
-			}
-
-			if (!bUserInteraction ||
-				aRowIndices.length != 1 ||
-				aRowIndices[0] != iIndex) {
-				return;
-			}
-
-			var oTreeTable = this.treeTable,
-				aRows = oTreeTable.getRows(),
-				iVisualIndex = this.getVisualIndex(oRowContext),
-				oRow = aRows[iVisualIndex],
-				oRowModel = oRow.getModel(),
-				oBindingContext = oRow.getBindingContext(),
-				sPath = oBindingContext.getPath(),
-				mRules = oRowModel.getProperty(sPath),
-				bSelected = !oRowModel.getProperty(sPath + "/selected"),
-				oChildIndicesRange,
-				sKey;
-
-			oRowModel.setProperty(sPath + "/selected", bSelected);
-
-			if (this.isRowAGroup(oRow)) {
-				oChildIndicesRange = this.getChildIndicesRange(oRow, iIndex);
-
-				if (!oChildIndicesRange) {
-					return;
-				}
-
-				for (sKey in mRules) {
-					if (Number.isInteger(Number.parseInt(sKey, 10))) {
-						oRowModel.setProperty(sPath + "/" + sKey + "/selected", bSelected);
-					}
-				}
-
-				if (oTreeTable.isExpanded(iIndex)) {
-					if (bSelected) {
-						oTreeTable.addSelectionInterval(oChildIndicesRange.from, oChildIndicesRange.to);
-					} else {
-						oTreeTable.removeSelectionInterval(oChildIndicesRange.from, oChildIndicesRange.to);
-					}
-				}
-			} else {
-				this.updateLibrarySelection(sPath, bSelected);
-			}
-
-			if (storage.readPersistenceCookie(constants.COOKIE_NAME)) {
-				this.persistSelection();
-			}
-		},
-
-		/**
-		 * @param {Object} oRow the row to check
-		 * @returns {boolean} true if the row is a library
-		 */
-		isRowAGroup: function (oRow) {
-			if (!oRow) {
-				return false;
-			}
-
-			var oRowModel = oRow.getModel();
-
-			if (!oRowModel) {
-				return false;
-			}
-
-			var	oBindingContext = oRow.getBindingContext(),
-				sPath = oBindingContext.getPath();
-
-			return oRowModel.getProperty(sPath + "/rules") !== undefined;
-		},
-
-		/**
-		 * Calculates the indices of the child nodes of the library row.
-		 *
-		 * @param {Object} oRow from which the rules will be taken
-		 * @param {int} iRowIndex the index of the library row
-		 * @returns {Object} a range to be used for selection
-		 */
-		getChildIndicesRange: function (oRow, iRowIndex) {
-			var oRowModel = oRow.getModel(),
-				oBindingContext = oRow.getBindingContext(),
-				sPath = oBindingContext.getPath(),
-				mRules = oRowModel.getProperty(sPath),
-				iFrom = iRowIndex + 1,
-				iTo = iRowIndex,
-				sKey;
-
-			for (sKey in mRules) {
-				if (Number.isInteger(Number.parseInt(sKey, 10))) {
-					iTo++;
-				}
-			}
-
-			if (iFrom > iTo ) {
-				return;
-			}
-
-			return {
-				from: iFrom,
-				to: iTo
-			};
-		},
-
-		/**
-		 * Finds the visual index of the rowContext.
-		 *
-		 * @param {Object} oRowContext the rowContext to check
-		 * @returns {int} the visible index of the row
-		 */
-		getVisualIndex: function (oRowContext) {
-			var aRows = this.treeTable.getRows(),
-				oRow,
-				i;
-
-			for (i = 0; i < aRows.length; i++) {
-				oRow = aRows[i];
-
-				if (oRow.getBindingContext() == oRowContext) {
-					return i;
-				}
-			}
-		},
-
-		/**
-		 * Finds the index of the row, which matches a specific path.
-		 *
-		 * @param {Object} oRowContext the rowContext to check
-		 * @returns {int} the index of the row, which matches the path
-		 */
-		getRowContextIndexByPath: function (sPath) {
-			var iRowIndex = 0,
-				oRowContext = this.treeTable.getContextByIndex(iRowIndex);
-
-			while (oRowContext) {
-				if (oRowContext.getPath() == sPath) {
-					return iRowIndex;
-				}
-				iRowIndex++;
-				oRowContext = this.treeTable.getContextByIndex(iRowIndex);
-			}
-		},
-
-		/**
-		 * Creates the initial selection of the TreeTable rows.
-		 *
-		 * @param {boolean} bInitialLoading Signifies wether this is executed on initial load
-		 */
-		initializeSelection: function (bInitialLoading) {
-			var bPersistingSettings = this.model.getProperty("/persistingSettings"),
-				oModel = this.getView().getModel(),
-				mIndex = {},
-				mLibrariesToUpdate = [],
-				aSelectedRules;
-
-			if (bPersistingSettings) {
-				aSelectedRules = storage.getSelectedRules() || [];
-
-				if (aSelectedRules.length === 0) {
-					return;
-				}
-
-				mIndex = this._buildTreeTableIndex();
-
-				aSelectedRules.forEach(function (oRuleDescriptor) {
-					var oRuleInfo = mIndex[oRuleDescriptor.ruleId];
-
-					if (oRuleInfo) {
-						oModel.setProperty(oRuleInfo.path + "/selected", true);
-
-						this.treeTable.addSelectionInterval(oRuleInfo.index, oRuleInfo.index);
-
-						if (mIndex[oRuleInfo.libName]) {
-							mLibrariesToUpdate[oRuleInfo.libName] = mIndex[oRuleInfo.libName];
-						}
-					}
-				}, this);
-
-				// Only update the libraries which have selected rules.
-				this.updateLibrariesSelection(mLibrariesToUpdate);
-			} else if (bInitialLoading) {
-				this.treeTable.selectAll();
-			}
-		},
-
-		/**
-		 * Selects/Deselects all rules and libraries in the model and stores the selection.
-		 *
-		 * @param {boolean} bSelectAll
-		 */
-		selectAllRows: function (bSelectAll) {
-			var oModel = this.getView().getModel(),
-				oTreeViewModel = oModel.getProperty("/treeViewModel/"),
-				oLibrary;
-
-			// Set selected flag for each library node in the TreeTable
-			for (var i in oTreeViewModel) {
-				if (Number.isInteger(Number.parseInt(i, 10))) {
-					oModel.setProperty("/treeViewModel/" + i + "/selected", bSelectAll);
-					oLibrary = oModel.getProperty("/treeViewModel/" + i);
-
-					// Set selected flag for each rule node in the TreeTable
-					for (var k in oLibrary) {
-						if (Number.isInteger(Number.parseInt(k, 10))) {
-							oModel.setProperty("/treeViewModel/" + i + "/" + k + "/selected", bSelectAll);
-						}
-					}
-				}
-			}
-
-			if (storage.readPersistenceCookie(constants.COOKIE_NAME)) {
-				this.persistSelection();
-			}
-		},
-
-		/**
-		 * From the rule row index search for the library in the list.
-		 * After it finds the correct library it selects/deselects it and update the model.
-		 *
-		 * @param {int} sPath The row index of the selected rule
-		 * @param {boolean} bSelected If the rule row was selected or deselected
-		 */
-		updateLibrarySelection: function (sPath, bSelected) {
-			var oModel = this.getView().getModel(),
-				iRowIndex = this.getRowContextIndexByPath(sPath),
-				oLibrary,
-				rowContext;
-
-			// Find the library object and index from the selected rule index
-			while (iRowIndex >= 0) {
-				rowContext = this.treeTable.getContextByIndex(iRowIndex);
-				sPath = rowContext.getPath();
-				oLibrary = oModel.getProperty(sPath);
-
-				if (oLibrary.type === "lib") {
-					break;
-				}
-
-				iRowIndex--;
-			}
-
-			// Select / Deselect the library row
-			if (bSelected) {
-				for (var i in oLibrary) {
-					if (Number.isInteger(Number.parseInt(i, 10)) && !oLibrary[i].selected) {
-						return;
-					}
-				}
-
-				this.treeTable.addSelectionInterval(iRowIndex, iRowIndex);
-			} else {
-				this.treeTable.removeSelectionInterval(iRowIndex, iRowIndex);
-			}
-
-			oModel.setProperty(sPath + "/selected", bSelected);
-		},
-
-		/**
-		 * For every passed library check if it's rules are selected. If they are, select the row of the library.
-		 *
-		 * @param {Object} mLibrariesToUpdate A map with the libraries which row selection should be checked
-		 */
-		updateLibrariesSelection: function (mLibrariesToUpdate) {
-			var oModel = this.getView().getModel(),
-				oLibrary,
-				bLibrarySelected;
-
-			for (var sLibName in mLibrariesToUpdate) {
-				oLibrary = oModel.getProperty(mLibrariesToUpdate[sLibName].path);
-				bLibrarySelected  = true;
-
-				for (var i in oLibrary) {
-					if (Number.isInteger(Number.parseInt(i, 10))) {
-						if (!oLibrary[i].selected) {
-							bLibrarySelected = false;
-							break;
-						}
-					}
-				}
-
-				this.updateLibrarySelection(mLibrariesToUpdate[sLibName].path, bLibrarySelected);
-			}
-		},
-
-		/**
-		 *
-		 * Apply "selected" flags to the libraries and rules of the new tree table view model by using the old one.
-		 *
-		 * @param {Object} oNewTreeModel The new tree table view model which needs to be updated with the "selected" flags
-		 * @returns {Object} The new model with "selected" flags
-		 */
-		_syncSelections: function (oNewTreeModel) {
-			// Build an index based on the old TreeViewModel
-			var mIndex = this._buildTreeTableIndex(),
-				aLibrariesToUpdate = [],
-				mLibrariesToUpdate = {},
-				mNewIndex;
-
-			// Update the "selected" flag of the new model based on the old one.
-			for (var i in oNewTreeModel) {
-				if (Number.isInteger(Number.parseInt(i, 10))) {
-					for (var k in oNewTreeModel[i]) {
-						var oRule = mIndex[oNewTreeModel[i][k].id];
-
-						if (Number.isInteger(Number.parseInt(k, 10)) && oRule && oRule.selected) {
-							oNewTreeModel[i][k].selected = oRule.selected;
-
-							if (aLibrariesToUpdate.indexOf(oNewTreeModel[i][k].libName) < 0) {
-								aLibrariesToUpdate.push(oNewTreeModel[i][k].libName);
-							}
-						}
-					}
-				}
-			}
-
-			this.model.setProperty("/treeViewModel", oNewTreeModel);
-
-			// Build an index based on the new TreeViewModel
-			mNewIndex = this._buildTreeTableIndex();
-
-			aLibrariesToUpdate.forEach(function (sLibName) {
-				if (mNewIndex[sLibName]) {
-					mLibrariesToUpdate[sLibName] = mNewIndex[sLibName];
-				}
-			});
-
-			// Check for newly loaded libraries and add them to the map for selection update.
-			for (var sKey in mNewIndex) {
-				if (mNewIndex[sKey].type === "lib" && !mIndex[sKey]) {
-					mLibrariesToUpdate[sKey] = mNewIndex[sKey];
-				}
-			}
-
-			this.updateLibrariesSelection(mLibrariesToUpdate);
-		},
-
-		/**
-		 *
-		 * Create a map which returns all rows of the tree table from the binding.
-		 *
-		 * @returns {Object} All rows of the TreeTable
-		 */
-		_buildTreeTableIndex: function () {
-			var iIndex = 0,
-				sPath = this.treeTable.getBinding().getPath() + '/',
-				oModel = this.getView().getModel(),
-				mIndex = {},
-				oLibContext = oModel.getProperty(sPath + iIndex),
-				iSubIndex = 0,
-				iGlobalIndex = 0,
-				oRuleContext;
-
-			// As the binding of the TreeTable has no API for all rows or all rows indices
-			// (getRows returns the visible rows only) we have to discover them in the binding
-			// and make an index map for better performance.
-			while (oLibContext) {
-
-				mIndex[oLibContext.name] = {
-					index: iGlobalIndex,
-					path: sPath + iIndex,
-					libName: oLibContext.name,
-					type: "lib",
-					selected: oLibContext.selected
-				};
-
-				iGlobalIndex++;
-
-				oRuleContext = oModel.getProperty(sPath + iIndex + '/' + iSubIndex);
-
-				while  (oRuleContext) {
-
-					mIndex[oRuleContext.id] = {
-						index: iGlobalIndex,
-						path: sPath + iIndex + '/' + iSubIndex,
-						libName: oRuleContext.libName,
-						type: "rule",
-						selected: oRuleContext.selected
-					};
-
-					iSubIndex++;
-					iGlobalIndex++;
-					oRuleContext = oModel.getProperty(sPath + iIndex + '/' + iSubIndex);
-				}
-
-				iIndex++;
-				oLibContext = oModel.getProperty(sPath + iIndex);
-			}
-
-			return mIndex;
-		},
+		onRowSelectionChange: SelectionUtils.selectionChangeHandler.bind(SelectionUtils),
 
 		_updateRuleList: function() {
 			var oRuleList = this.getView().byId("ruleList"),
