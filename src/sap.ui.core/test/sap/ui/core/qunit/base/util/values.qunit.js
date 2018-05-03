2da80246335fa23dcd2bc99898bf29222113a602
Tobias Sorn <tobias.sorn@sap.com>
2018-05-03 10:02:34 +0200

    [INTERNAL] sap/base/util: includes and values
    Changed implementation of values and includes modules to match the
    commonly used lodash functionality of _.values and _.includes
    
    BCP: 1870260777
    CR-Id: 002075125900001879612018
    Change-Id: I43b13f2416f6d827084a1129d363d79e9119e534
    (cherry picked from commit 346fc7434d3ffa89081583399de3304b5c5f1255)

--- src/sap.ui.core/test/sap/ui/core/qunit/base/util/values.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/base/util/values.qunit.js
@@ -1,7 +1,7 @@
 /*!
  * ${copyright}
  */
-/*global QUnit */
+/*global QUnit sinon */
 sap.ui.define([
 	'sap/base/util/values'
 ], function (
@@ -9,31 +9,75 @@ sap.ui.define([
 ) {
 	"use strict";
 
+	var sandbox = sinon.sandbox.create();
+
 	QUnit.module("sap.base.util.values", function () {
 		QUnit.test("basic functionality", function (assert) {
-			assert.deepEqual(values({ a: 'b', c: 'd'}), ['b', 'd']);
-			assert.deepEqual(values({ a: 'b', c: 'd'}).sort(), ['d', 'b'].sort());
+			assert.deepEqual(values({a: 'b', c: 'd'}), ['b', 'd']);
+			assert.deepEqual(values({a: 'b', c: 'd'}).sort(), ['d', 'b'].sort());
 			assert.deepEqual(values({}), []);
+			assert.deepEqual(values(NaN), [], "NaN should return an empty array");
+			assert.deepEqual(values(undefined), []);
+			assert.deepEqual(values(null), []);
+			assert.deepEqual(values(''), [], "empty string should return an empty array");
+			assert.deepEqual(values('abc'), ['a', 'b', 'c'], "string abc should return an array with a, b and c");
+			assert.deepEqual(values(function () {}), []);
+			assert.deepEqual(values(), []);
+			assert.deepEqual(values(0), [], "number should return an empty array");
+			assert.deepEqual(values(3), [], "number should return an empty array");
+			assert.deepEqual(values(Infinity), [], "number should return an empty array");
+			var oObj = {a: 5};
+			oObj = Object.create(oObj);
+			oObj.b = 19;
+			assert.deepEqual(values(oObj), [19]);
 		});
-		QUnit.test("invalid argument", function (assert) {
-			assert.throws(function () {
-				values();
-			});
-			assert.throws(function () {
-				values([]);
-			});
-			assert.throws(function () {
-				values(null);
-			});
-			assert.throws(function () {
-				values(undefined);
-			});
-			assert.throws(function () {
-				values('');
-			});
-			assert.throws(function () {
-				values(function () {});
-			});
+		QUnit.test("getting values from a custom object", function (assert) {
+			function Foo() {
+				this.a = 1;
+				this.b = 2;
+			}
+			Foo.prototype.c = 3;
+			assert.deepEqual(values(new Foo()).sort(), [1, 2].sort());
+		});
+	});
+
+
+	QUnit.module("sap.base.util.values with mocked Object.values()", {
+		before: function () {
+			if (Object.values) {
+				sandbox.stub(Object, 'values').value(undefined);
+			}
+		},
+		after: function () {
+			sandbox.restore();
+		}
+	}, function () {
+		QUnit.test("basic functionality", function (assert) {
+			assert.deepEqual(values({a: 'b', c: 'd'}), ['b', 'd']);
+			assert.deepEqual(values({a: 'b', c: 'd'}).sort(), ['d', 'b'].sort());
+			assert.deepEqual(values({}), []);
+			assert.deepEqual(values(NaN), [], "NaN should return an empty array");
+			assert.deepEqual(values(undefined), []);
+			assert.deepEqual(values(null), []);
+			assert.deepEqual(values(''), [], "empty string should return an empty array");
+			assert.deepEqual(values('abc'), ['a', 'b', 'c'], "string abc should return an array with a, b and c");
+			assert.deepEqual(values(function () {}), []);
+			assert.deepEqual(values(), []);
+			assert.deepEqual(values(0), [], "number should return an empty array");
+			assert.deepEqual(values(3), [], "number should return an empty array");
+			assert.deepEqual(values(Infinity), [], "number should return an empty array");
+			var oObj = {a: 5};
+			oObj = Object.create(oObj);
+			oObj.b = 19;
+			assert.deepEqual(values(oObj), [19]);
+		});
+		QUnit.test("getting values from a custom object", function (assert) {
+			function Foo() {
+				this.a = 1;
+				this.b = 2;
+			}
+			Foo.prototype.c = 3;
+			assert.deepEqual(values(new Foo()).sort(), [1, 2].sort());
 		});
 	});
 });
