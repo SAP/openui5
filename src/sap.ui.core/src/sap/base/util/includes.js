346fc7434d3ffa89081583399de3304b5c5f1255
Tobias Sorn <tobias.sorn@sap.com>
2018-05-03 10:02:34 +0200

    [INTERNAL] sap/base/util: includes and values
    Changed implementation of values and includes modules to match the
    commonly used lodash functionality of _.values and _.includes
    
    Change-Id: I43b13f2416f6d827084a1129d363d79e9119e534

--- src/sap.ui.core/src/sap/base/util/includes.js
+++ src/sap.ui.core/src/sap/base/util/includes.js
@@ -6,34 +6,95 @@
  * Code other than the OpenUI5 libraries must not introduce dependencies to this module.
  */
 sap.ui.define([
-	"sap/base/util/isPlainObject",
 	"sap/base/util/values"
 ], function(
-	isPlainObject,
 	values
 ) {
 	"use strict";
 
 	/**
-	 * Checks if value is in collection
+	 * Compares 2 values based on SameValueZero comparison
+	 * @param a
+	 * @param b
+	 * @returns {boolean}
+	 * @see http://ecma-international.org/ecma-262/7.0/#sec-samevaluezero
+	 */
+	function equals(a, b) {
+		// self comparison to ensure that NaN and NaN return true
+		/*eslint-disable no-self-compare*/
+		return a === b || (a !== a && b !== b);
+		/*eslint-disable no-self-compare*/
+	}
+
+	/**
+	 * Checks if value is included in collection
+	 *
+	* <pre>
+	*     sap.ui.require([sap/base/util/includes], function(includes){
+	*         // arrays
+	*         includes(["1", "8", "7"], "8");         // true
+	*         includes(["1", "8", "7"], "8", 0);      // true
+	*
+	*         includes(["1", "8", "7"], "8", 1);      // true
+	*         includes(["1", "8", "7"], "8", 2);      // false
+	*         includes(["1", "8", "7"], "8", 3);      // false
+	*
+	*         includes(["1", "8", "7"], "8", -1);     // false
+	*         includes(["1", "8", "7"], "8", -2);     // true
+	*         includes(["1", "8", "7"], "8", -3);     // true
+	*
+	*         // strings
+	*         includes("187", "8");                   // true
+	*         includes("187", "8", 0);                // true
+	*
+	*         includes("187", "8", 1);                // true
+	*         includes("187", "8", 2);                // false
+	*         includes("187", "8", 3);                // false
+	*
+	*         includes("187", "8", -1);               // false
+	*         includes("187", "8", -2);               // true
+	*         includes("187", "8", -3);               // true
+	*     });
+	* </pre>
 	 *
 	 * @function
-	 * @private
 	 * @exports sap/base/util/includes
-	 * @param {Array|object} vCollection - Collection to be checked
+	 * @param {Array|object|string} vCollection - Collection to be checked
 	 * @param {*} vValue - The value to be checked
-	 * @throws {TypeError} when vCollection is of an unsupported type
-	 * @returns {boolean} - true if value is in the collection
+	 * @param {int} [iFromIndex=0] - optional start index, negative start index will start from the end
+	 * @returns {boolean} - true if value is in the collection, false otherwise
 	 * @private
 	 */
+	var fnIncludes = function (vCollection, vValue, iFromIndex) {
+		if (typeof iFromIndex !== 'number') {
+			iFromIndex = 0;
+		}
 
-	return function includes(vCollection, vValue) {
+		// Use native (Array.prototype.includes, String.prototype.includes) includes functions if available
 		if (Array.isArray(vCollection)) {
-			return vCollection.indexOf(vValue) !== -1;
-		} else if (isPlainObject(vCollection)) {
-			return values(vCollection).indexOf(vValue) !== -1;
+			if (typeof vCollection.includes === 'function') {
+				return vCollection.includes(vValue, iFromIndex);
+			}
+
+			iFromIndex = iFromIndex < 0 ? iFromIndex + vCollection.length : iFromIndex;
+			iFromIndex = iFromIndex < 0 ? 0 : iFromIndex;
+
+			for (var i = iFromIndex; i < vCollection.length; i++) {
+				if (equals(vCollection[i], vValue)) {
+					return true;
+				}
+			}
+			return false;
+		} else if (typeof vCollection === 'string') {
+			iFromIndex = iFromIndex < 0 ? vCollection.length + iFromIndex : iFromIndex;
+			if (typeof vCollection.includes === 'function') {
+				return vCollection.includes(vValue, iFromIndex);
+			}
+			return vCollection.indexOf(vValue, iFromIndex) !== -1;
 		} else {
-			throw new TypeError('vCollection must be an array or a plain object');
+			// values(...) always returns an array therefore deep recursion is avoided
+			return fnIncludes(values(vCollection), vValue, iFromIndex);
 		}
 	};
+	return fnIncludes;
 });
