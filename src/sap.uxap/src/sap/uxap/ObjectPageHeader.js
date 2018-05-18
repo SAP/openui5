bcf1a8f1a58e56ea05baa86f63c0613e17320b07
Pazheva <diana.pazheva@sap.com>
2018-05-18 14:19:56 +0300

    [INTERNAL][FIX] sap.uxap.ObjectPageHeader: Deregister resizeHandler when not in DOM
    Background:
    The ObjectPageHeader uses a ResizeHandler to adjust layout when its size changes
    
    Problem:
    The ResizeHandler *cannot detect size change* of the header in the following situation:
    
    0. Header is visible in the DOM => ResizeHandler saves its last width & height
    1. Header is removed from the DOM (its parent rerenders) => ResizeHandler ignores it (by design)
    2. Header is rendered again in the DOM *while hidden child of its parent* => layout cannot be done (because hidden, so metrics invalid) -> need to wait untill the header is made visible and ResizeHandler re-triggers layout
    3. Header is made visible but the ResizeHandler does not detect size change (because size is the same as the one obtained before rerendering)
    
    Solution:
    We need to make the ResizeHandler freshly record the Header size everytime on after rendering, so that it is able to detect when the control is rendered hidden and then shown.
    => layout re-calculations can be triggered upon showing the Header
    
    Change-Id: Ia419a91b51fe9899a627a05d02f7eca9da01f929
    BCP: 1880084906
    (cherry picked from commit 0d374981f08a49188ec02ca64de423b4261a684e)
    CR-Id: 002075125800002482252018

--- src/sap.uxap/src/sap/uxap/ObjectPageHeader.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageHeader.js
@@ -610,6 +610,10 @@ sap.ui.define([
 		this._oFavIcon.setVisible(this.getMarkFavorite());
 		this._oFlagIcon.setVisible(this.getMarkFlagged());
 		this._attachDetachActionButtonsHandler(false);
+		if (this._iResizeId) {
+			ResizeHandler.deregister(this._iResizeId);
+			this._iResizeId = null;
+		}
 		this._bFirstRendering = false;
 	};
 
@@ -989,6 +993,7 @@ sap.ui.define([
 		this._clearImageNotFoundHandler();
 		if (this._iResizeId) {
 			ResizeHandler.deregister(this._iResizeId);
+			this._iResizeId = null;
 		}
 	};
 
