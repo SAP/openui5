SAP/openui5

commit cf4228633b559d0b4f39721e2bf78d00b7c0e620

[INTERNAL] qunit-junit: allow usage without jQuery
Change-Id: Ic19301950517e57be0f1d51f0b1f959b4bbdf7c2

src/sap.ui.core/src/sap/ui/qunit/qunit-junit.js
@@ -30,7 +30,7 @@
 		}
 
 		if (sBaseUrl === null) {
-			if (jQuery && jQuery.sap &&  jQuery.sap.getModulePath) {
+			if (typeof jQuery !== 'undefined' && jQuery.sap && jQuery.sap.getModulePath) {
 				sFullUrl = jQuery.sap.getModulePath("sap.ui.thirdparty.qunit-reporter-junit", ".js");
 			} else {
 				throw new Error("qunit-junit.js: The script tag seems to be malformed!");
@@ -50,17 +50,27 @@
 		// HACK: insert our hook in front of QUnit's own hook so that we execute first
 		QUnit.config.callbacks.begin.unshift(function() {
 			// ensure proper structure of DOM
-			var $qunit = jQuery("#qunit");
-			var $qunitDetails = jQuery('#qunit-header,#qunit-banner,qunit-userAgent,#qunit-testrunner-toolbar,#qunit-tests');
-			var $qunitFixture = jQuery("#qunit-fixture");
-			if ( $qunit.size() === 0 && $qunitDetails.size() > 0 ) {
+			var qunitNode = document.querySelector("#qunit");
+			var qunitDetailNodes = document.querySelectorAll('#qunit-header,#qunit-banner,#qunit-userAgent,#qunit-testrunner-toolbar,#qunit-tests');
+			var qunitFixtureNode = document.querySelector("#qunit-fixture");
+			if ( qunitNode == null && qunitDetailNodes.length > 0 ) {
 				// create a "qunit" section and place it before the existing detail DOM
-				$qunit = jQuery("<div id='qunit'></div>").insertBefore($qunitDetails[0]);
+				qunitNode = document.createElement('DIV');
+				qunitNode.id = 'qunit';
+				qunitDetailNodes[0].parentNode.insertBefore(qunitNode, qunitDetailNodes[0]);
 				// move the existing DOM into the wrapper
-				$qunit.append($qunitDetails);
+				for ( var i = 0; i < qunitDetailNodes.length; i++) {
+					qunitNode.appendChild(qunitDetailNodes[i]);
+				}
 			}
-			if ( $qunitFixture.size === 0 ) {
-				$qunit.after("<div id='qunit-fixture'></div>");
+			if ( qunitFixtureNode == null ) {
+				qunitFixtureNode = document.createElement('DIV');
+				qunitFixtureNode.id = 'qunit-fixture';
+				if ( qunitNode.nextSibling ) {
+					qunitNode.parentNode.insertBefore(qunitFixtureNode, qunitNode.nextSibling);
+				} else {
+					qunitNode.parentNode.appendChild(qunitFixtureNode);
+				}
 			}
 		});
 

commit 9cd5bddb9a2a9461ab28fbcfcc854500227f655c

[FIX] qunit-junit reporter: Add null-check for qunit DOM node
This got broken with cf4228633b559d0b4f39721e2bf78d00b7c0e620

Change-Id: I5c86f9ea23c104ebb941cd59e06da9f66b8e703b
Fixes: https://github.com/SAP/karma-openui5/issues/14

src/sap.ui.core/src/sap/ui/qunit/qunit-junit.js
@@ -38,7 +38,7 @@
 				qunitNode.appendChild(qunitDetailNodes[i]);
 			}
 		}
-		if ( qunitFixtureNode == null ) {
+		if ( qunitFixtureNode == null && qunitNode ) {
 			qunitFixtureNode = document.createElement('DIV');
 			qunitFixtureNode.id = 'qunit-fixture';
 			qunitNode.parentNode.insertBefore(qunitFixtureNode, qunitNode.nextSibling);