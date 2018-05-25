SAP/openui5

commit 3bdeb179a7e8a8ea2ce057e9b180146d20e94f4d

[FIX] sap.m.PlanningCalendar: Dates from previous/next month in the calendar picker are now interactive
When sap.m.PlanningCalendar is used in Day or Week view the calendar picker
in the calendar interval can now navigate to the visible dates from the
previous or the next month.

Change-Id: If5af337c552ea5be2d5a48ffc3da882044dd1447
BCP: 1870150194

src/sap.ui.unified/src/sap/ui/unified/CalendarDateInterval.js
@@ -248,7 +248,8 @@ sap.ui.define([
 
 	CalendarDateInterval.prototype._handleCalendarPickerDateSelect = function(oEvent) {
 		var oCalendar = this._getCalendarPicker(),
-			oNewCalStartDate = new CalendarDate(oCalendar._getFocusedDate());
+			oSelectedDate = oCalendar.getSelectedDates()[0].getStartDate(),
+			oNewCalStartDate = new CalendarDate(oSelectedDate.getUTCFullYear(), oSelectedDate.getMonth(), oSelectedDate.getDate());
 
 		this._setStartDate(oNewCalStartDate);
 		this._setFocusedDate(oNewCalStartDate);

src/sap.ui.unified/src/sap/ui/unified/CalendarWeekInterval.js
@@ -159,17 +159,18 @@ sap.ui.define(['sap/ui/unified/calendar/CalendarUtils', 'sap/ui/unified/calendar
 
 		CalendarWeekInterval.prototype._handleCalendarPickerDateSelect = function(oEvent) {
 			var oCalPicker = this._getCalendarPicker(),
-				oFocusedDate = oCalPicker._getFocusedDate(),
+				oSelectedDate = oCalPicker.getSelectedDates()[0].getStartDate(),
+				oFocusedDate = new CalendarDate(oSelectedDate.getUTCFullYear(), oSelectedDate.getMonth(), oSelectedDate.getDate()),
 				oFirstWeekDate;
 
 			if (this._dateMatchesVisibleRange(oFocusedDate.toLocalJSDate())) {
-				this._oFocusDateWeek = oCalPicker._getFocusedDate();
+				this._oFocusDateWeek = oFocusedDate;
 				this._focusDate(this._oFocusDateWeek, false, true); // true means no fire startDateChange event (no start date change)
 			} else {
 				oFirstWeekDate = CalendarUtils._getFirstDateOfWeek(oFocusedDate);
 
 				this._setStartDate(oFirstWeekDate);
-				this._oFocusDateWeek = oCalPicker._getFocusedDate();
+				this._oFocusDateWeek = oFocusedDate;
 				this._focusDate(this._oFocusDateWeek, false, true); // true means no fire startDateChange event (we already did it in _setStartDate)
 			}
 

src/sap.m/src/sap/m/PlanningCalendarRow.js
@@ -212,7 +212,7 @@ sap.ui.define(['jquery.sap.global',
 			/**
 			 * Holds the special dates in the context of a row. A single date or a date range can be set.
 			 *
-			 * <b>Note</> Only date or date ranges of type <code>sap.ui.unified.CalendarDayType.NonWorking</code> will
+			 * <b>Note</b> Only date or date ranges of type <code>sap.ui.unified.CalendarDayType.NonWorking</code> will
 			 * be visualized in the <code>PlanningCalendarRow</code>. If the aggregation is set as another type,
 			 * the date or date range will be ignored and will not be displayed in the control.
 			 * @since 1.56

src/sap.m/test/sap/m/qunit/PlanningCalendar.qunit.html
@@ -20,6 +20,7 @@
 	<link rel="stylesheet" href="../../../../resources/sap/ui/thirdparty/qunit.css" type="text/css" media="screen">
 	<script src="../../../../resources/sap/ui/thirdparty/qunit.js"></script>
 	<script src="../../../../resources/sap/ui/qunit/qunit-junit.js"></script>
+	<script src="../../../../resources/sap/ui/qunit/qunit-coverage.js"></script>
 	<script src="../../../../resources/sap/ui/qunit/QUnitUtils.js"></script>
 	<script src="../../../../resources/sap/ui/thirdparty/sinon.js"></script>
 	<script src="../../../../resources/sap/ui/thirdparty/sinon-qunit.js"></script>
@@ -2864,6 +2865,74 @@
 					this.oPC2, "Navigating forward once");
 		});
 
+		QUnit.test("Selecting the visible dates from the previous/next month in the calendar picker", function (assert) {
+			// Prepare
+			var oApp30Aug2016 = new CalendarAppointment("app30Aug2016", {
+				startDate: new Date(2016, 7, 30, 6),
+				endDate: new Date(2016, 7, 30, 10)
+			}), oApp2ndSept2016 = new CalendarAppointment("app2ndSept2016", {
+				startDate: new Date(2016, 8, 2, 6),
+				endDate: new Date(2016, 8, 2, 10)
+			}), aDays, $FirstInterval;
+
+			this.oPC2.getRows()[0].addAppointment(oApp30Aug2016);
+			this.oPC2.getRows()[0].addAppointment(oApp2ndSept2016);
+
+			// Act
+			_switchToView(CalendarIntervalType.Hour, this.oPC2);
+			sap.ui.getCore().applyChanges();
+
+			qutils.triggerEvent("click", "startDateAtTheMiddleOfTheWeek-TimeInt--Head-B1");
+			qutils.triggerEvent("mousedown", "startDateAtTheMiddleOfTheWeek-TimeInt--Cal--Month0-20160902");
+			qutils.triggerEvent("mouseup", "startDateAtTheMiddleOfTheWeek-TimeInt--Cal--Month0-20160902");
+			sap.ui.getCore().applyChanges();
+
+			this.oPC2Interval = this.oPC2.getAggregation("table").getAggregation("infoToolbar").getContent()[1];
+			this.oPC2IntervalRow = this.oPC2Interval.getAggregation('timesRow');
+			aDays = this.oPC2IntervalRow.$("times").children();
+			$FirstInterval = aDays[0];
+
+			// Assert
+			assert.ok(oApp2ndSept2016.getDomRef(), "Once moved to date of previous/next month, the appointment there should be visible");
+			assert.equal($FirstInterval.getAttribute("data-sap-time"), "201609020100", "Once clicked on a date from the visible range of the previous/next month, the PlanningCalendar is displaying the right view.");
+
+			// Act
+			_switchToView(CalendarIntervalType.Day, this.oPC2);
+			sap.ui.getCore().applyChanges();
+
+			qutils.triggerEvent("click", "startDateAtTheMiddleOfTheWeek-DateInt--Head-B1");
+			qutils.triggerEvent("mousedown", "startDateAtTheMiddleOfTheWeek-DateInt--Cal--Month0-20160830");
+			qutils.triggerEvent("mouseup", "startDateAtTheMiddleOfTheWeek-DateInt--Cal--Month0-20160830");
+			sap.ui.getCore().applyChanges();
+
+			this.oPC2Interval = this.oPC2.getAggregation("table").getAggregation("infoToolbar").getContent()[1];
+			this.oPC2IntervalRow = this.oPC2Interval.getAggregation('month')[0];
+			aDays = this.oPC2IntervalRow.$("days").children();
+			$FirstInterval = aDays[0];
+
+			// Assert
+			assert.ok(oApp30Aug2016.getDomRef(), "Once moved to date of previous/next month, the appointment there should be visible");
+			assert.equal($FirstInterval.getAttribute("data-sap-day"), "20160830", "Once clicked on a date from the visible range of the previous/next month, the PlanningCalendar is displaying the right view.");
+
+			// Act
+			_switchToView(CalendarIntervalType.Week, this.oPC2);
+			sap.ui.getCore().applyChanges();
+
+			qutils.triggerEvent("click", "startDateAtTheMiddleOfTheWeek-WeekInt--Head-next");
+			qutils.triggerEvent("click", "startDateAtTheMiddleOfTheWeek-WeekInt--Head-B1");
+			qutils.triggerEvent("mousedown", "startDateAtTheMiddleOfTheWeek-WeekInt--Cal--Month0-20160830");
+			qutils.triggerEvent("mouseup", "startDateAtTheMiddleOfTheWeek-WeekInt--Cal--Month0-20160830");
+			sap.ui.getCore().applyChanges();
+
+			this.oPC2Interval = this.oPC2.getAggregation("table").getAggregation("infoToolbar").getContent()[1];
+			this.oPC2IntervalRow = this.oPC2Interval.getAggregation('month')[0];
+			aDays = this.oPC2IntervalRow.$("days").children();
+			$FirstInterval = aDays[0];
+
+			// Assert
+			assert.ok(oApp30Aug2016.getDomRef(), "Once moved to date of previous month/next, the appointment there should be visible");
+			assert.equal($FirstInterval.getAttribute("data-sap-day"), "20160829", "Once clicked on a date from the visible range of the previous/next month, the PlanningCalendar is displaying the right view.");
+		});
 
 		QUnit.test("'Today' button should be disabled when current day is visible", function(assert) {
 			//assert

commit fa73077179d1c9c82d764aaf948dbe72c269b7f1

[INTERNAL] sap.ui.unified.Calendar*Interval: calendar date and week interval controls use proper date
The CalendarDateInterval and CalendarWeekInterval now use the correct local date,
when one is selected.

Fixes an issue introduced by commit 3bdeb179a7e8a8ea2ce057e9b180146d20e94f4d

Change-Id: I128d58c5691c8c24cef8c7ec942366dbf7677b9a

src/sap.ui.unified/src/sap/ui/unified/CalendarDateInterval.js
@@ -251,7 +251,7 @@ sap.ui.define([
 	CalendarDateInterval.prototype._handleCalendarPickerDateSelect = function(oEvent) {
 		var oCalendar = this._getCalendarPicker(),
 			oSelectedDate = oCalendar.getSelectedDates()[0].getStartDate(),
-			oNewCalStartDate = new CalendarDate(oSelectedDate.getUTCFullYear(), oSelectedDate.getMonth(), oSelectedDate.getDate());
+			oNewCalStartDate = new CalendarDate.fromLocalJSDate(oSelectedDate);
 
 		this._setStartDate(oNewCalStartDate);
 		this._setFocusedDate(oNewCalStartDate);

src/sap.ui.unified/src/sap/ui/unified/CalendarWeekInterval.js
@@ -160,7 +160,7 @@ sap.ui.define(['sap/ui/unified/calendar/CalendarUtils', 'sap/ui/unified/calendar
 		CalendarWeekInterval.prototype._handleCalendarPickerDateSelect = function(oEvent) {
 			var oCalPicker = this._getCalendarPicker(),
 				oSelectedDate = oCalPicker.getSelectedDates()[0].getStartDate(),
-				oFocusedDate = new CalendarDate(oSelectedDate.getUTCFullYear(), oSelectedDate.getMonth(), oSelectedDate.getDate()),
+				oFocusedDate = new CalendarDate.fromLocalJSDate(oSelectedDate),
 				oFirstWeekDate;
 
 			if (this._dateMatchesVisibleRange(oFocusedDate.toLocalJSDate())) {