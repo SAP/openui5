SAP/openui5

commit 867c4c69984835d514d5a507c8f8701f915fa8a6

[FEATURE] sap.ui.core.IconPool - API for loading of additional fonts
* registerFont method and register font
* indirect loading of metadata via getIconInfo
* registration in app code through fontLoaded promise
* invalidate icon after font is loaded
* fine-tuned icon logging for additional collections
* added handling for registering fonts multiple times
* unit tests for all new features
* getIconInfo no longer tries to insert the default font face
  when that is already inserted
* changed warning about already inserted font face into an info

Change-Id: I5ed52a65e8ec7fccd9c3a7be18becb06bc772521

src/sap.ui.core/src/sap/ui/core/Icon.js
@@ -313,25 +313,29 @@ sap.ui.define([
 	/* =========================================================== */
 
 	Icon.prototype.setSrc = function(sSrc) {
-		var oIconInfo = IconPool.getIconInfo(sSrc),
+		var vIconInfo = IconPool.getIconInfo(sSrc, "mixed"),
 			$Icon = this.$(),
 			sIconLabel, sTooltip, bUseIconTooltip, aLabelledBy, oInvisibleText;
 
-		// when the given sSrc can't be found in IconPool, rerender the icon is needed.
-		this.setProperty("src", sSrc, !!oIconInfo);
+		// when the given sSrc can't be found in IconPool
+		// rerender the icon is needed.
+		this.setProperty("src", sSrc, !!vIconInfo);
 
-		if (oIconInfo && $Icon.length) {
-			$Icon.css("font-family", oIconInfo.fontFamily);
-			$Icon.attr("data-sap-ui-icon-content", oIconInfo.content);
-			$Icon.toggleClass("sapUiIconMirrorInRTL", !oIconInfo.suppressMirroring);
+		if (vIconInfo instanceof Promise) {
+			// trigger a rerendering once the icon info is available
+			vIconInfo.then(this.invalidate.bind(this));
+		} else if (vIconInfo && $Icon.length) {
+			$Icon.css("font-family", vIconInfo.fontFamily);
+			$Icon.attr("data-sap-ui-icon-content", vIconInfo.content);
+			$Icon.toggleClass("sapUiIconMirrorInRTL", !vIconInfo.suppressMirroring);
 
 			sTooltip = this.getTooltip_AsString();
 			aLabelledBy = this.getAriaLabelledBy();
 			bUseIconTooltip = this.getUseIconTooltip();
-			sIconLabel = this._getIconLabel();
+			sIconLabel = this._getIconLabel(vIconInfo);
 
-			if (sTooltip || (bUseIconTooltip && oIconInfo.text)) {
-				$Icon.attr("title", sTooltip || oIconInfo.text);
+			if (sTooltip || (bUseIconTooltip && vIconInfo.text)) {
+				$Icon.attr("title", sTooltip || vIconInfo.text);
 			} else {
 				$Icon.attr("title", null);
 			}
@@ -348,6 +352,7 @@ sap.ui.define([
 					oInvisibleText.setText(sIconLabel);
 				}
 			}
+
 		}
 
 		return this;
@@ -469,13 +474,12 @@ sap.ui.define([
 
 	/**
 	 * Returns the string which is set to the 'title' attribute of the DOM output
-	 *
+	 * @param {object} oIconInfo icon metadata
 	 * @return {string|undefined} the string which is output as title attribute
 	 * @private
 	 */
-	Icon.prototype._getOutputTitle = function() {
-		var oIconInfo = IconPool.getIconInfo(this.getSrc()),
-			sTooltip = this.getTooltip_AsString(),
+	Icon.prototype._getOutputTitle = function(oIconInfo) {
+		var sTooltip = this.getTooltip_AsString(),
 			bUseIconTooltip = this.getUseIconTooltip();
 
 		if (sTooltip || (bUseIconTooltip && oIconInfo && oIconInfo.text)) {
@@ -493,16 +497,16 @@ sap.ui.define([
 	 * which is set to the 'title' attribute of the DOM, this method returns undefined in
 	 * order not to set the aria-label or aria-labelledby attribute.
 	 *
+	 * @param {object} oIconInfo icon metadata
 	 * @return {string} the label when it's necessary to be output
 	 * @private
 	 */
-	Icon.prototype._getIconLabel = function() {
-		var oIconInfo = IconPool.getIconInfo(this.getSrc()),
-			sAlt = this.getAlt(),
+	Icon.prototype._getIconLabel = function(oIconInfo) {
+		var sAlt = this.getAlt(),
 			sTooltip = this.getTooltip_AsString(),
 			bUseIconTooltip = this.getUseIconTooltip(),
 			sLabel = sAlt || sTooltip || (bUseIconTooltip && oIconInfo && (oIconInfo.text || oIconInfo.name)),
-			sOutputTitle = this._getOutputTitle();
+			sOutputTitle = this._getOutputTitle(oIconInfo);
 
 		if (sLabel && sLabel !== sOutputTitle) {
 			return sLabel;
@@ -513,10 +517,10 @@ sap.ui.define([
 		var oInvisibleText = this.getAggregation("_invisibleText");
 
 		if (!oInvisibleText) {
+			// create control without rerendering
 			oInvisibleText = new InvisibleText(this.getId() + "-label", {
 				text: sText
 			});
-
 			this.setAggregation("_invisibleText", oInvisibleText, true);
 		} else {
 			// avoid triggering invalidation during rendering
@@ -526,10 +530,10 @@ sap.ui.define([
 		return oInvisibleText;
 	};
 
-	Icon.prototype._getAccessibilityAttributes = function() {
+	Icon.prototype._getAccessibilityAttributes = function(oIconInfo) {
 		var aLabelledBy = this.getAriaLabelledBy(),
 			mAccAttributes = {},
-			sIconLabel = this._getIconLabel(),
+			sIconLabel = this._getIconLabel(oIconInfo),
 			oInvisibleText;
 
 		if (this.getDecorative()) {
@@ -566,7 +570,7 @@ sap.ui.define([
 		}
 
 		var bHasPressListeners = this.hasListeners("press");
-		var oIconInfo = IconPool.getIconInfo(this.getSrc());
+		var oIconInfo = IconPool.getIconInfo(this.getSrc(), "sync");
 
 		return {
 			role: bHasPressListeners ? "button" : "img",

src/sap.ui.core/src/sap/ui/core/IconRenderer.js
@@ -22,23 +22,40 @@ sap.ui.define(['jquery.sap.global', './IconPool', './library'], function(jQuery,
 	 */
 	IconRenderer.render = function(oRm, oControl) {
 		// write the HTML into the render manager
-		var oIconInfo = IconPool.getIconInfo(oControl.getSrc()),
+		var vIconInfo = IconPool.getIconInfo(oControl.getSrc(), "mixed"),
 			sWidth = oControl.getWidth(),
 			sHeight = oControl.getHeight(),
 			sColor = oControl.getColor(),
 			sBackgroundColor = oControl.getBackgroundColor(),
 			sSize = oControl.getSize(),
 			bNoTabStop = oControl.getNoTabStop(),
-			aLabelledBy = oControl.getAriaLabelledBy(),
-			oAccAttributes = oControl._getAccessibilityAttributes(),
-			sTitle = oControl._getOutputTitle(),
+			sTitle = oControl._getOutputTitle(vIconInfo),
+			aLabelledBy,
+			oInvisibleText,
+			oAccAttributes,
+			bIconInfo = false;
+
+		if (vIconInfo instanceof Promise) {
+			// if the icon info is still being loaded,
+			// an invalidation is triggered after the icon info is available
+			vIconInfo.then(oControl.invalidate.bind(oControl));
+		} else if (vIconInfo) {
+			// render icon info in renderer
+			bIconInfo = true;
+			aLabelledBy = oControl.getAriaLabelledBy();
+
+			oAccAttributes = oControl._getAccessibilityAttributes(vIconInfo);
 			// oInvisibleText must be retrieved after calling _getAccessibilityAttributes
 			// because it may be created within the function
 			oInvisibleText = oControl.getAggregation("_invisibleText");
+		}
 
 		oRm.write("<span");
 		oRm.writeControlData(oControl);
-		oRm.writeAccessibilityState(oControl, oAccAttributes);
+		if (bIconInfo) {
+			oRm.writeAccessibilityState(oControl, oAccAttributes);
+		}
+
 
 		if (sTitle) {
 			oRm.writeAttributeEscaped("title", sTitle);
@@ -48,9 +65,9 @@ sap.ui.define(['jquery.sap.global', './IconPool', './library'], function(jQuery,
 			oRm.writeAttribute("tabindex", 0);
 		}
 
-		if (oIconInfo) {
-			oRm.writeAttributeEscaped("data-sap-ui-icon-content", oIconInfo.content);
-			oRm.addStyle("font-family", "'" + jQuery.sap.encodeHTML(oIconInfo.fontFamily) + "'");
+		if (bIconInfo) {
+			oRm.writeAttributeEscaped("data-sap-ui-icon-content", vIconInfo.content);
+			oRm.addStyle("font-family", "'" + jQuery.sap.encodeHTML(vIconInfo.fontFamily) + "'");
 		}
 
 		if (sWidth) {
@@ -76,7 +93,7 @@ sap.ui.define(['jquery.sap.global', './IconPool', './library'], function(jQuery,
 
 		oRm.addClass("sapUiIcon");
 
-		if (oIconInfo && !oIconInfo.suppressMirroring) {
+		if (bIconInfo && !vIconInfo.suppressMirroring) {
 			oRm.addClass("sapUiIconMirrorInRTL");
 		}
 
@@ -89,9 +106,9 @@ sap.ui.define(['jquery.sap.global', './IconPool', './library'], function(jQuery,
 
 		oRm.write(">");
 
-			if (aLabelledBy.length && oInvisibleText) {
-				oRm.renderControl(oInvisibleText);
-			}
+		if (aLabelledBy && aLabelledBy.length && oInvisibleText) {
+			oRm.renderControl(oInvisibleText);
+		}
 
 		oRm.write("</span>");
 	};

src/sap.ui.core/test/sap/ui/core/qunit/Icon.qunit.html
@@ -15,6 +15,9 @@
 		<script src="../../../../../resources/sap/ui/qunit/qunit-junit.js"></script>
 		<script src="../../../../../resources/sap/ui/qunit/QUnitUtils.js"></script>
 		<script src="../../../../../resources/sap/ui/qunit/qunit-coverage.js"></script>
+		<script src="../../../../../resources/sap/ui/thirdparty/sinon.js"></script>
+		<script src="../../../../../resources/sap/ui/thirdparty/sinon-ie.js"></script>
+		<script src="../../../../../resources/sap/ui/thirdparty/sinon-qunit.js"></script>
 
 		<script>
 			jQuery.sap.require("sap.ui.core.Icon");
@@ -422,6 +425,7 @@
 					}
 				}
 			});
+
 			QUnit.test("useIconTooltip = true (default) / without tooltip", function (assert) {
 				var oIconInfo = sap.ui.core.IconPool.getIconInfo("add");
 
@@ -434,6 +438,7 @@
 				assert.equal(this.oIcon.$().attr("aria-label"), undefined, "aria-label should be undefined");
 				assert.equal(this.oIcon.$().attr("aria-labelledby"), undefined, "aria-labelledby should be undefined");
 			});
+
 			QUnit.test("useIconTooltip = true (default) / with tooltip", function (assert) {
 				var sTooltip = "this is a tooltip";
 				this.createIcon({
@@ -444,6 +449,7 @@
 				assert.equal(this.oIcon.$().attr("aria-label"), undefined, "aria-label should be undefined");
 				assert.equal(this.oIcon.$().attr("aria-labelledby"), undefined, "aria-labelledby should be undefined");
 			});
+
 			QUnit.test("useIconTooltip = false / without tooltip", function (assert) {
 				this.createIcon({
 					src: sap.ui.core.IconPool.getIconURI("add"),
@@ -453,6 +459,7 @@
 				assert.equal(this.oIcon.$().attr("aria-label"), undefined, "aria-label should be undefined");
 				assert.equal(this.oIcon.$().attr("aria-labelledby"), undefined, "aria-labelledby should be undefined");
 			});
+
 			QUnit.test("useIconTooltip = false / with tooltip", function (assert) {
 				var sTooltip = "this is a tooltip";
 				this.createIcon({
@@ -465,10 +472,87 @@
 				assert.equal(this.oIcon.$().attr("aria-labelledby"), undefined, "aria-labelledby should be undefined");
 			});
 
+			QUnit.module("Loading of additional icon fonts", {
+				createIcon: function(mSettings, fnCallback) {
+					this.oIcon = new sap.ui.core.Icon(mSettings).placeAt("qunit-fixture");
+					sap.ui.getCore().applyChanges();
+				},
+				afterEach: function() {
+					if (this.oIcon) {
+						this.oIcon.destroy();
+						this.oIcon = null;
+					}
+				}
+			});
+
+			QUnit.test("Render icon without loading the corresponding font", function (assert) {
+				var IconPool = sap.ui.core.IconPool,
+					oErrorSpy = sinon.spy(jQuery.sap.log, "error");
+
+				this.createIcon({src: "sap-icon://tnt/technicalsystem"});
+				assert.strictEqual(oErrorSpy.callCount, 1, "Rendering of an icon without loading the corresponding font throws an error");
+
+				oErrorSpy.restore();
+			});
+
+			QUnit.test("Refresh icon content after font was loaded lazily", function (assert) {
+				var IconPool = sap.ui.core.IconPool;
+				var done = assert.async();
+
+				// load additional icon font
+				IconPool.registerFont({
+					collectionName: "tnt",
+					fontFamily: "SAP-icons-TNT",
+					fontURI: jQuery.sap.getModulePath("sap.tnt.themes.base.fonts"),
+					lazy: true
+				});
+
+				var oLoadFontMetadataSpy = sinon.spy(IconPool, "_loadFontMetadata");
+
+				// instantiate icon
+				this.createIcon({src: "sap-icon://tnt/technicalsystem"});
+				assert.strictEqual(this.oIcon.$().attr("data-sap-ui-icon-content"), undefined, "The icon content attribute is not set yet while the font is loading");
+				assert.ok(oLoadFontMetadataSpy.called, "The loading of metadata for new font is triggered");
+
+				return sap.ui.core.IconPool.getIconInfo("sap-icon://tnt/technicalsystem", "async").then(function() {
+					sap.ui.getCore().applyChanges();
+
+					assert.strictEqual(this.oIcon.$().attr("data-sap-ui-icon-content"), String.fromCharCode(0xe000), "Icon content has been set properly after the font is loaded");
+					assert.strictEqual(this.oIcon.$().css("font-family"), "SAP-icons-TNT", "Icon font family has been set properly after the font is loaded");
+
+					oLoadFontMetadataSpy.restore();
+					done();
+				}.bind(this));
+			});
+
+			QUnit.test("Render a non-existent icon", function (assert) {
+				var IconPool = sap.ui.core.IconPool,
+					oErrorSpy = sinon.spy(jQuery.sap.log, "warning");
+
+				this.createIcon({src: "sap-icon://tnt/doesnotexist"});
+				assert.ok(oErrorSpy.called, "Rendering of an icon without loading the corresponding font throws warning");
+
+				oErrorSpy.restore();
+			});
+
+			QUnit.test("The same icon is displayed immediately after font was loaded lazily", function (assert) {
+				this.createIcon({src: "sap-icon://tnt/technicalsystem"});
+
+				assert.strictEqual(this.oIcon.$().attr("data-sap-ui-icon-content"), String.fromCharCode(0xe000), "The icon content attribute is not set yet while the font is loading");
+				assert.strictEqual(this.oIcon.$().css("font-family"), "SAP-icons-TNT", "Icon font family has been set properly after the font is loaded");
+			});
+
+			QUnit.test("A different icon is displayed immediately after font was loaded lazily", function (assert) {
+				this.createIcon({src: "sap-icon://tnt/python"});
+
+				assert.strictEqual(this.oIcon.$().attr("data-sap-ui-icon-content"), String.fromCharCode(0xe00f), "The icon content attribute is not set yet while the font is loading");
+				assert.strictEqual(this.oIcon.$().css("font-family"), "SAP-icons-TNT", "Icon font family has been set properly after the font is loaded");
+			});
+
 		</script>
 	</head>
 	<body>
-		<h1 id="qunit-header">QUnit Page for HTML Control</h1>
+		<h1 id="qunit-header">QUnit Page for Icon Control</h1>
 		<h2 id="qunit-banner"></h2>
 		<h2 id="qunit-userAgent"></h2>
 		<ol id="qunit-tests"></ol>

commit 38eed731fec1188b7dbaceae2e47411d6f53fe03

[INTERNAL][FIX] Icon: Fix legacy 'src' without Icon URI
Although it is not mentioned in the API, the 'src' property also
accepted a icon name of the default collection as value instead of the
documented Icon URI (IconPool.getIconURI).
This is was caused by 867c4c69984835d514d5a507c8f8701f915fa8a6 and works
again, but is still not recommended. Therefore an assertion is added.

BCP: 1870325627
Change-Id: I9d88d26f2b23cc021675238b1efb8aa0d6c0d088

src/sap.ui.core/src/sap/ui/core/Icon.js
@@ -5,6 +5,7 @@
 // Provides control sap.ui.core.Icon.
 sap.ui.define([
     'jquery.sap.global',
+    'sap/base/assert',
     '../Device',
     './Control',
     './IconPool',
@@ -15,6 +16,7 @@ sap.ui.define([
 ],
 	function(
 	    jQuery,
+		assert,
 		Device,
 		Control,
 		IconPool,
@@ -325,7 +327,9 @@ sap.ui.define([
 	/* =========================================================== */
 
 	Icon.prototype.setSrc = function(sSrc) {
-		var vIconInfo = IconPool.getIconInfo(sSrc, "mixed"),
+		assert(IconPool.isIconURI(sSrc), this + ": Property 'src' (value: '" + sSrc + "') should be a valid Icon URI (sap-icon://...)");
+
+		var vIconInfo = IconPool.getIconInfo(sSrc, undefined, "mixed"),
 			$Icon = this.$(),
 			sIconLabel, sTooltip, bUseIconTooltip, aLabelledBy, oInvisibleText;
 
@@ -582,7 +586,7 @@ sap.ui.define([
 		}
 
 		var bHasPressListeners = this.hasListeners("press");
-		var oIconInfo = IconPool.getIconInfo(this.getSrc(), "sync");
+		var oIconInfo = IconPool.getIconInfo(this.getSrc(), undefined, "sync");
 
 		return {
 			role: bHasPressListeners ? "button" : "img",

src/sap.ui.core/src/sap/ui/core/IconRenderer.js
@@ -22,7 +22,7 @@ sap.ui.define(['jquery.sap.global', './IconPool', './library'], function(jQuery,
 	 */
 	IconRenderer.render = function(oRm, oControl) {
 		// write the HTML into the render manager
-		var vIconInfo = IconPool.getIconInfo(oControl.getSrc(), "mixed"),
+		var vIconInfo = IconPool.getIconInfo(oControl.getSrc(), undefined, "mixed"),
 			sWidth = oControl.getWidth(),
 			sHeight = oControl.getHeight(),
 			sColor = oControl.getColor(),

src/sap.ui.core/test/sap/ui/core/qunit/Icon.qunit.html
@@ -247,6 +247,23 @@
 				oIcon.destroy();
 			});
 
+			QUnit.test("src with icon name only should use default icon collection", function(assert) {
+				var oIcon = new sap.ui.core.Icon({
+					src: "delete"
+				});
+
+				oIcon.placeAt("uiAreaA");
+				sap.ui.getCore().applyChanges();
+
+				assert.equal(
+					oIcon.$().attr("data-sap-ui-icon-content"),
+					sap.ui.core.IconPool.getIconInfo("sap-icon://delete").content,
+					"Content attribute should be set correctly"
+				);
+
+				oIcon.destroy();
+			});
+
 			QUnit.module("ARIA Support", {
 				beforeEach: function () {
 					this.sIconName = "add";