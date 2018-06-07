9e06f2864a0f596b3c7a689b8e2903e1d1a2f2ce
Georgi Hristanov <georgi.hristanov@sap.com>
2018-06-07 15:03:59 +0300

    [INTERNAL] Demokit V2: Fiori2.0 tutorial implemented
    Change-Id: I92d052678378d9bb0ca679970e96025e96a22c9d
    CR-Id: 002075125900002994982018
    (cherry picked from commit 112bd8e54620ba30991b1ca6c94315347b624d58)

---
+++ src/sap.f/test/sap/f/demokit/tutorial/fiori2/08/webapp/controller/Detail.controller.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.fiori2.controller.Detail", {
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this.oModel = this.getOwnerComponent().getModel();
+
+			this.oRouter.getRoute("master").attachPatternMatched(this._onProductMatched, this);
+			this.oRouter.getRoute("detail").attachPatternMatched(this._onProductMatched, this);
+		},
+
+		_onProductMatched: function (oEvent) {
+			this._product = oEvent.getParameter("arguments").product || this._product || "0";
+			this.getView().bindElement({
+				path: "/ProductCollection/" + this._product,
+				model: "products"
+			});
+		},
+
+		onEditToggleButtonPress: function() {
+			var oObjectPage = this.getView().byId("ObjectPageLayout"),
+				bCurrentShowFooterState = oObjectPage.getShowFooter();
+
+			oObjectPage.setShowFooter(!bCurrentShowFooterState);
+		},
+
+		onExit: function () {
+			this.oRouter.getRoute("master").detachPatternMatched(this._onProductMatched, this);
+			this.oRouter.getRoute("detail").detachPatternMatched(this._onProductMatched, this);
+		}
+	});
+}, true);
