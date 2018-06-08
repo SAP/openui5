24052081fca76b1f29fb4a5b768cde6732f177f3
Vladislav Iliev <vladislav.iliev@sap.com>
2018-06-08 15:23:56 +0300

    [FIX] sap.m.DateTimePicker: set min/max date does not modify its time part now.
    Internal Min & Max dates should be the same as given by the app developer.
    This was not true as DateTimePicker relied on the parent's methods for
    setting them, where sap.m.DatePicker manipulated the time part in order
    to fit its use-cases as a date only
    (see commit 869b937b5f052594f7b3d6c37784497ed97222c6).
    
    This handling does not suites the DateTimePicker use-cases, where all
    the time part should not be modified.
    
    (cherry picked from commit 85d2b43103422f06fd21fe7fe905972548a9fcc6)
    
    Change-Id: I010b50faa340e29097192a25ae01be8159a7ec57
    BCP: 002075129500002625952018
    CR-Id: 002075125800002298072018

--- src/sap.m/src/sap/m/DateTimePicker.js
+++ src/sap.m/src/sap/m/DateTimePicker.js
@@ -294,6 +294,24 @@ sap.ui.define(['jquery.sap.global', './DatePicker', 'sap/ui/model/type/Date', '.
 
 	};
 
+	DateTimePicker.prototype.setMinDate = function (oDate) {
+		DatePicker.prototype.setMinDate.call(this, oDate);
+
+		if (oDate) { //make sure the time part is as the original one
+			this._oMinDate.setHours(oDate.getHours(), oDate.getMinutes(), oDate.getSeconds());
+		}
+		return this;
+	};
+
+	DateTimePicker.prototype.setMaxDate = function (oDate) {
+		DatePicker.prototype.setMaxDate.call(this, oDate);
+
+		if (oDate) { //make sure the time part is as the original one
+			this._oMaxDate.setHours(oDate.getHours(), oDate.getMinutes(), oDate.getSeconds());
+		}
+		return this;
+	};
+
 	DateTimePicker.prototype._getFormatInstance = function(oArguments, bDisplayFormat){
 
 		var oMyArguments = jQuery.extend({}, oArguments);
