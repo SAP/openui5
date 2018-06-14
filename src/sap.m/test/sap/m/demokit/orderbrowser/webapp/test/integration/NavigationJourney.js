c909d1736445ef82ce3df18188e500ed8067be3a
Ayham <ayham.tanbari@sap.com>
2018-06-14 13:15:07 +0200

    [INTERNAL] demoapps.orderbrowser: Update to Fiori 2.0 in 1.52
    * Use "sap.f.SemanticPage" in booth master and detail page
    * Fix preselected item when starting the app
    * Set the title in heading aggregation (UX feedback)
    * Remove selected item when closing the detail page
    * Set the default layout mode to "OneColumn"
    * Add header action buttons(full screen- exit full screen - close button)
    * Move filter and grouper to the header and use ViewSettingsControl
    * Remove pull to refresh button as it doesn't work with semantic page
    * Use sap.f.router
    * Update manifest routing configuration
    * Fix and write new opa test
    * Clean unit test
    * Refactor list selector code
    * Delete Grouper helper model
    * Delete App.controller from unit test
    
    Change-Id: Id952d22b55a648fee58be9a05d72b38234419dc4
    CR-Id: 002075125800002475352018
    BCP: n/a

--- src/sap.m/test/sap/m/demokit/orderbrowser/webapp/test/integration/NavigationJourney.js
+++ src/sap.m/test/sap/m/demokit/orderbrowser/webapp/test/integration/NavigationJourney.js
@@ -7,20 +7,41 @@ sap.ui.define([
 
 	QUnit.module("Desktop navigation");
 
-	opaTest("Should start the app with empty hash: the hash should reflect the selection of the first item in the list", function (Given, When, Then) {
+	opaTest("Should navigate on press", function (Given, When, Then) {
 		// Arrangements
-		Given.iStartTheApp();
+		Given.iStartTheApp({
+			hash: "Orders/7991/"
+		});
 
 		//Actions
 		When.onTheMasterPage.iRememberTheSelectedItem();
 
 		// Assertions
-		Then.onTheMasterPage.theFirstItemShouldBeSelected();
-		Then.onTheDetailPage.iShouldSeeTheRememberedObject().and.iShouldSeeNoBusyIndicator();
+		Then.onTheDetailPage.iShouldSeeTheRememberedObject().
+			and.iShouldSeeNoBusyIndicator().
+			and.iShouldSeeHeaderActionButtons();
 		Then.onTheBrowserPage.iShouldSeeTheHashForTheRememberedObject();
 	});
 
-	opaTest("Should react on hashchange", function (Given, When, Then) {
+	opaTest("Should press full screen toggle button: The app shows one column", function (Given, When, Then) {
+		// Actions
+		When.onTheDetailPage.iPressTheHeaderActionButton("enterFullScreen");
+
+		// Assertions
+		Then.onTheDetailPage.theAppShowsFCLDesign("MidColumnFullScreen").
+			and.iShouldSeeTheFullScreenToggleButton("exitFullScreen");
+	});
+
+	opaTest("Should press full screen toggle button: The app shows two columns", function (Given, When, Then) {
+		// Actions
+		When.onTheDetailPage.iPressTheHeaderActionButton("exitFullScreen");
+
+		// Assertions
+		Then.onTheDetailPage.theAppShowsFCLDesign("TwoColumnsMidExpanded").
+			and.iShouldSeeTheFullScreenToggleButton("enterFullScreen");
+	});
+
+	opaTest("Should react on hash change", function (Given, When, Then) {
 		// Actions
 		When.onTheMasterPage.iRememberTheIdOfListItemAtPosition(2);
 		When.onTheBrowserPage.iChangeTheHashToTheRememberedItem();
@@ -29,7 +50,6 @@ sap.ui.define([
 		Then.onTheDetailPage.iShouldSeeTheRememberedObject().and.iShouldSeeNoBusyIndicator();
 		Then.onTheMasterPage.theRememberedListItemShouldBeSelected();
 	});
-
 
 	opaTest("Should navigate on press", function (Given, When, Then) {
 		// Actions
@@ -58,8 +78,19 @@ sap.ui.define([
 		When.onTheBrowserPage.iChangeTheHashToTheRememberedItem();
 
 		// Assertions
-		Then.onTheDetailPage.iShouldSeeTheRememberedObject().
-			and.iTeardownMyAppFrame();
+		Then.onTheDetailPage.iShouldSeeTheRememberedObject();
+	});
+
+	opaTest("Should press close column button: The app shows one columns", function (Given, When, Then) {
+		// Actions
+		When.onTheDetailPage.iPressTheHeaderActionButton("closeColumn");
+
+		// Assertions
+		Then.onTheDetailPage.theAppShowsFCLDesign("OneColumn");
+		Then.onTheMasterPage.theListShouldHaveNoSelection();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Start the App and simulate metadata error: MessageBox should be shown", function (Given, When, Then) {
@@ -67,8 +98,10 @@ sap.ui.define([
 		Given.iStartMyAppOnADesktopToTestErrorHandler("metadataError=true");
 
 		// Assertions
-		Then.onTheAppPage.iShouldSeeTheMessageBox().
-			and.iTeardownMyAppFrame();
+		Then.onTheAppPage.iShouldSeeTheMessageBox();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Start the App and simulate bad request error: MessageBox should be shown", function (Given, When, Then) {
@@ -76,13 +109,17 @@ sap.ui.define([
 		Given.iStartMyAppOnADesktopToTestErrorHandler("errorType=serverError");
 
 		// Assertions
-		Then.onTheAppPage.iShouldSeeTheMessageBox().
-			and.iTeardownMyAppFrame();
+		Then.onTheAppPage.iShouldSeeTheMessageBox();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Start the app and should be on the shipping tab", function (Given, When, Then) {
 		//Arrangement
-		Given.iStartTheApp();
+		Given.iStartTheApp({
+			hash: "Orders/7991/"
+		});
 		//Assertions
 		Then.onTheDetailPage.iShouldSeeTheShippingInfo();
 	});
@@ -92,6 +129,9 @@ sap.ui.define([
 		When.onTheDetailPage.iPressProcessorTab();
 		//Assertions
 		Then.onTheDetailPage.iShouldSeeTheProcessorInfo();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Start the app with the processor tab in the hash I should be on the processor tab", function (Given, When, Then) {
@@ -101,15 +141,20 @@ sap.ui.define([
 		});
 		//Assertions
 		Then.onTheDetailPage.iShouldSeeTheProcessorInfo();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 
 	opaTest("Start the app with an invalid tab url I should be on the shipping tab", function (Given, When, Then) {
 		//Arrangement
 		Given.iStartTheApp({
-			hash: "/Orders(7827)/?tab=foo"
+			hash: "/Orders/7827/?tab=foo"
 		});
 		//Assertions
-		Then.onTheDetailPage.iShouldSeeTheShippingInfo().
-			and.iTeardownMyAppFrame();
+		Then.onTheDetailPage.iShouldSeeTheShippingInfo();
+
+		// Cleanup
+		Then.iTeardownMyAppFrame();
 	});
 });
