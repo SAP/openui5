52bff70d9db2d445fa5eaec3c753534418ef54de
Franz Müller <franz.mueller@sap.com>
2018-06-25 10:12:30 +0200

    [INTERNAL] fl.VariantManagement: remove experimental flag
    Change-Id: I9002b51479939982c6ea55230591079912ef1fea

--- src/sap.ui.fl/src/sap/ui/fl/variants/VariantManagement.js
+++ src/sap.ui.fl/src/sap/ui/fl/variants/VariantManagement.js
@@ -16,8 +16,7 @@ sap.ui.define([
 	 * @extends sap.ui.core.Control
 	 * @constructor
 	 * @public
-	 * @since 1.50
-	 * @experimental Since 1.50. This class is experimental and provides only limited functionality. Also the API might be changed in future.
+	 * @since 1.56
 	 * @alias sap.ui.fl.variants.VariantManagement
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
