db43681eedcd31f99da3b1a1f52ef76bf071557d
Radostina Tsiklovska <radostina.tsiklovska@sap.com>
2018-06-28 12:46:30 +0300

    [INTERNAL] sap.m.MultiInput: Input in visible when no indicator is shown
    Change-Id: I1e73bb4a7124fa87ac0a31246b6c054da4fe6813

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -970,7 +970,9 @@ function(
 
 		if (!bFocusIsInSelectedItemPopup && !bNewFocusIsInTokenizer) {
 			this._tokenizer._useCollapsedMode(true);
-			this._setValueInvisible();
+
+			// hide the text value only if the indicator is visible
+			this._tokenizer._getIndicatorVisibility() && this._setValueInvisible();
 		}
 	};
 
