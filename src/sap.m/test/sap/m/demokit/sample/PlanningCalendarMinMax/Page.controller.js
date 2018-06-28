ceeab1bc64965683a7abf7979b69cbf18f7d232d
Daniel Nanovski <daniel.nanovski@sap.com>
2018-06-28 15:59:33 +0300

    [INTERNAL] Update resource location for samples and Demo Apps
    Follow up from Gerrit change: 3451586
    
    After removal of SDK v1:
    
    - Resource location changed to point the new SDK
    
    Change-Id: Ifdede7abb19741df4ad7208be1f485b9a614c35a

--- src/sap.m/test/sap/m/demokit/sample/PlanningCalendarMinMax/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/PlanningCalendarMinMax/Page.controller.js
@@ -16,7 +16,7 @@ sap.ui.define([
 					minDate: new Date(2000, 0, 1, 0, 0, 0),
 					maxDate: new Date(2050, 11, 31, 23, 59, 59),
 					people: [{
-						pic: "test-resources/sap/ui/demokit/explored/img/John_Miller.png",
+						pic: "test-resources/sap/ui/documentation/sdk/images/John_Miller.png",
 						name: "John Miller",
 						role: "team member",
 						appointments: [{
--- src/sap.m/test/sap/m/demokit/sample/PlanningCalendarWithStickyHeader/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/PlanningCalendarWithStickyHeader/Page.controller.js
@@ -14,7 +14,7 @@ sap.ui.define([
 				oModel.setData({
 					startDate: new Date("2017", "0", "15", "8", "0"),
 					people: [{
-						pic: "test-resources/sap/ui/demokit/explored/img/John_Miller.png",
+						pic: "test-resources/sap/ui/documentation/sdk/images/John_Miller.png",
 						name: "John Miller",
 						role: "team member",
 						appointments: [
@@ -223,7 +223,7 @@ sap.ui.define([
 						]
 					},
 						{
-							pic: "test-resources/sap/ui/demokit/explored/img/Donna_Moore.jpg",
+							pic: "test-resources/sap/ui/documentation/sdk/images/Donna_Moore.jpg",
 							name: "Donna Moore",
 							role: "team member",
 							appointments: [
