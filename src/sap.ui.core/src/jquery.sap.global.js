4025959fd38009ede1e8a080af72518e6e71aaca
Tobias Sorn <tobias.sorn@sap.com>
2018-07-05 14:18:38 +0200

    [FIX] sap/ui/dom/computedStylePolyfill: document.body is not there yet
    The situation is the same as before. Firefox still returns null for getComputedStyle
    when called in an iframe with style "display:none".
    
    The previous fix failed when it was called synchronously from the HEAD element,
    as no document.body is present at that point in time.
    
    The fake body approach is also used by jQuery itself.
    
    In the incident the getComputedStyle call is triggered by the sap.m library.js for the
    font-size check.
    
    CR-Id: 002075125900002569602018
    Change-Id: I6c6ca21714e8b5eb5849527460098ce3084e5df6
    BCP: 002028376500003153742018
    (cherry picked from commit afe894e1db2a172b1828481465f67009c28c93e2)

--- src/sap.ui.core/src/jquery.sap.global.js
+++ src/sap.ui.core/src/jquery.sap.global.js
@@ -332,6 +332,21 @@
 		window.getComputedStyle = function(element, pseudoElt){
 			var oCSS2Style = fnGetComputedStyle.call(this, element, pseudoElt);
 			if (oCSS2Style === null) {
+				// If no body element exists yet, we create a fake one to return the style attribute.
+				// This approach is also used by some jQuery modules like "jQuery Mobile".
+				if (document.body == null) {
+					// create and insert a fake body into the HTML
+					var oFakeBody = document.createElement("body");
+					var oHTML = document.getElementsByTagName("html")[0];
+					oHTML.insertBefore( oFakeBody, oHTML.firstChild );
+
+					// get the style from this fake body
+					var oStyle = oFakeBody.style;
+
+					// remove the fake body again
+					oFakeBody.parentNode.removeChild(oFakeBody);
+					return oStyle;
+				}
 				//Copy StyleDeclaration of document.body
 				return document.body.cloneNode(false).style;
 			}
