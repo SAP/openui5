17b2597d1db9e388322e5574a86d385fc3575b17
Clemens Maier <c.maier@sap.com>
2018-07-10 09:41:17 +0200

    [INTERNAL] v2.ODataModel: Canceling requests
    With this change the ODataModel V2 request header 'sap-cancel-on-close'
    is defaulted to 'false'. Only requests triggered by ODataModel.read and
    security token requests are send with 'sap-cancel-on-close' equals true by
    default.
    
    If numbers are drawn at the back-end, the previous behavior could lead to inconsistent
    data, since some numbers are lost through canceling. A typical
    scenario for canceling a requests is closing the browser window.
    
    Furthermore, the header can now be set for all requests by passing the header via parameter to
    the ODataModel constructor, or make use of the setHeader API, or for an single request as header parameter
    to the ODataModel APIs (read, update, ...).
    
    Change-Id: Ifdb62cc33f9eeafcaf42f4d5122bd9e7c561ec56
    CR-Id: 002075125900002784882018
    BCP: 1880400449

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -341,7 +341,6 @@ sap.ui.define([
 			if (this.sMaxDataServiceVersion) {
 				this.oHeaders["MaxDataServiceVersion"] = this.sMaxDataServiceVersion;
 			}
-			this.oHeaders["sap-cancel-on-close"] = true;
 
 		},
 		metadata : {
@@ -2630,7 +2629,7 @@ sap.ui.define([
 
 		function requestToken(sRequestType, fnError) {
 			// trigger a read to the service url to fetch the token
-			var oRequest = that._createRequest(sUrl, sRequestType, that._getHeaders(), null, null, !!bAsync);
+			var oRequest = that._createRequest(sUrl, sRequestType, that._getHeaders(false, true), null, null, !!bAsync);
 			oRequest.headers["x-csrf-token"] = "Fetch";
 			return that._request(oRequest, handleSuccess, fnError, undefined, undefined, that.getServiceMetadata());
 		}
@@ -3005,10 +3004,20 @@ sap.ui.define([
 	ODataModel.prototype._createBatchRequest = function(aBatchRequests) {
 		var sUrl, oRequest,
 		oChangeHeader = {},
-		oPayload = {};
+		oPayload = {},
+		bCancelOnClose = true;
 
 		oPayload.__batchRequests = aBatchRequests;
 
+
+		// If one requests leads to data changes at the back-end side, the canceling of the batch request must be prevented.
+		for (var sIndex in aBatchRequests) {
+			if (aBatchRequests[sIndex] && aBatchRequests[sIndex].__changeRequests ||
+				aBatchRequests[sIndex] && aBatchRequests[sIndex].headers && !aBatchRequests[sIndex].headers['sap-cancel-on-close']) {
+				bCancelOnClose = false;
+				break;
+			}
+		}
 		sUrl = this.sServiceUrl	+ "/$batch";
 
 
@@ -3023,6 +3032,8 @@ sap.ui.define([
 
 		// reset
 		delete oChangeHeader["Content-Type"];
+
+		oChangeHeader['sap-cancel-on-close'] = bCancelOnClose;
 
 		oRequest = {
 				headers : oChangeHeader,
@@ -3290,7 +3301,7 @@ sap.ui.define([
 						}
 					}
 					if (aReadRequests.length > 0) {
-						var oBatchRequest = that._createBatchRequest(aReadRequests, true);
+						var oBatchRequest = that._createBatchRequest(aReadRequests);
 						oWrappedBatchRequestHandle.oRequestHandle = that._submitBatchRequest(oBatchRequest, aBatchGroup, fnSuccess, fnError);
 						aRequestHandles.push(oWrappedBatchRequestHandle.oRequestHandle);
 					}
@@ -4410,7 +4421,9 @@ sap.ui.define([
 		}
 
 		aUrlParams = ODataUtils._createUrlParamsArray(mUrlParams);
-		mHeaders = this._getHeaders(mHeaders);
+
+		mHeaders = this._getHeaders(mHeaders, true);
+
 		sMethod = "GET";
 		sETag = this._getETag(sPath, oContext);
 
@@ -5016,7 +5029,8 @@ sap.ui.define([
 		}
 	};
 
-	ODataModel.prototype._getHeaders = function(mHeaders) {
+	ODataModel.prototype._getHeaders = function(mHeaders, bCancelOnClose) {
+
 		var mCheckedHeaders = {},
 		that = this;
 		if (mHeaders) {
@@ -5029,7 +5043,7 @@ sap.ui.define([
 				}
 			});
 		}
-		return jQuery.extend({}, this.mCustomHeaders, mCheckedHeaders, this.oHeaders);
+		return jQuery.extend({'sap-cancel-on-close': !!bCancelOnClose}, this.mCustomHeaders, mCheckedHeaders, this.oHeaders);
 	};
 
 	/**
