b6d29131fe9840e2528a8bd5aaa2363039c7e7ae
Teodor Taushanov <teodor.taushanov@sap.com>
2018-07-16 15:02:41 +0300

    [FIX] sap.m.Dialog: Problem with z-index on Safari is now fixed
    Problem: After dialog was opened it was inserted behind the 
    dark overlay in Safari.
    
    Change-Id: I97459932bf8def01ab72ef611e9c365947f9cfaf
    BCP: 1880222547
    BCP: 002075129500002445352018
    CR-Id: 002075125900002841652018

--- src/sap.m/src/sap/m/Dialog.js
+++ src/sap.m/src/sap/m/Dialog.js
@@ -921,9 +921,12 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Associative
 				}
 
 				sTranslateY = '-' + Math.floor(iDialogHeight / 2) + "px";
-				$dialog.css('transform', 'translate(' + sTranslateX + ',' + sTranslateY + ') scale(1)');
+				var sCalculatedPosition = 'translate(' + sTranslateX + ',' + sTranslateY + ') scale(1) ';
+				$dialog.css('transform', sCalculatedPosition );
+				$dialog.css('-webkit-transform', sCalculatedPosition + ' translateZ(0px)');
 			} else {
 				$dialog.css('transform', '');
+				$dialog.css('-webkit-transform', '');
 			}
 		};
 
