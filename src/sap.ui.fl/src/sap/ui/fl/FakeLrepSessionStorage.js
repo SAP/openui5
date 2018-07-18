3d2e28553e0e5272df4d2ed2dd0ea7176af9e989
Vitor Seifert <vitor.eduardo.seifert.bazzo@sap.com>
2018-07-18 15:30:03 +0200

    [INTERNAL]sap.ui.fl - Documentation for FakeLRep Storage/Connectors
    Change-Id: I73a87e4dfec81984520519468c69508a3d9f3e25

--- src/sap.ui.fl/src/sap/ui/fl/FakeLrepSessionStorage.js
+++ src/sap.ui.fl/src/sap/ui/fl/FakeLrepSessionStorage.js
@@ -7,6 +7,20 @@ sap.ui.define([
 ], function(
 	FakeLrepStorage
 ) {
+	/**
+	 * Class handling the Fake Lrep storage for session storage
+	 *
+	 * @class
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @private
+	 * @static
+	 * @since 1.58
+	 * @alias sap.ui.fl.FakeLrepSessionStorage
+	 */
+
 	"use strict";
 
 	return FakeLrepStorage(window.sessionStorage);
