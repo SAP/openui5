164b7bf51b2a34bc883ec3de4ef4907ad7ae09f5
Ayham <ayham.tanbari@sap.com>
2018-07-23 17:56:55 +0200

    [INTERNAL][FIX] demoapps: fixed phone journeys
    * Fix phone journey in orderbrowser according to the FCL design
    * Fix phone journey in master-detail according to the FCL design
    * Fixed phone journey in cart (namespace - iStartTheApp->iStartMyApp)
    
    BCP: 1880290532
    CR-Id: 002075125900002837612018
    Change-Id: I9c8a4a329c4baf5d29bf7c992e679201484bb7a7

--- src/sap.m/test/sap/m/demokit/cart/webapp/test/integration/PhoneJourneys.js
+++ src/sap.m/test/sap/m/demokit/cart/webapp/test/integration/PhoneJourneys.js
@@ -7,6 +7,7 @@ QUnit.config.autostart = false;
 
 sap.ui.require([
 	"sap/ui/test/Opa5",
+	'sap/ui/demo/cart/test/arrangement/Arrangement',
 	"sap/ui/test/opaQunit",
 	//page objects
 	'sap/ui/demo/cart/test/pageobjects/Welcome',
@@ -15,17 +16,12 @@ sap.ui.require([
 	'sap/ui/demo/cart/test/pageobjects/Category',
 	'sap/ui/demo/cart/test/pageobjects/Product',
 	'sap/ui/demo/cart/test/pageobjects/Cart',
-	'sap/ui/demo/cart/test/pageobjects/Dialog'
-], function (Opa5, Common) {
+	'sap/ui/demo/cart/test/pageobjects/Dialog',
+	'sap/ui/demo/cart/test/PhoneNavigationJourney'
+], function (Opa5, Arrangement) {
 	"use strict";
 	Opa5.extendConfig({
-		arrangements: new Common(),
+		arrangements: new Arrangement(),
 		viewNamespace: "sap.ui.demo.cart.view."
-	});
-
-	sap.ui.require([
-		"sap/ui/demo/cart/test/integration/PhoneNavigationJourney"
-	], function () {
-		QUnit.start();
 	});
 });
