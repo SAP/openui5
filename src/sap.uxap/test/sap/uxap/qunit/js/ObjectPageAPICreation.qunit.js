b3fb5a4955b55fc7ca7af59d346aad9f24d69450
Pazheva <diana.pazheva@sap.com>
2018-07-30 10:50:23 +0300

    [INTERNAL] ObjectPageLayout: Corrected test timeouts
    Removed the hardcoded timeout values (that wait when the page is fully
    rendered) and replaced with the respective "onAfterRenderingDOMReady"
    event
    
    Change-Id: If882e37a369a989d1fc2ef498acc288fbd10be15
    CR-Id: 002075125900003343622018
    BCP: 1880453442

--- src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageAPICreation.qunit.js
+++ src/sap.uxap/test/sap/uxap/qunit/js/ObjectPageAPICreation.qunit.js
@@ -241,10 +241,10 @@
 			sSelectedTitle: this.oSecondSection.getSubSections()[0].getTitle() //subsection is promoted
 		};
 
-		setTimeout(function () {
+		oObjectPage.attachEventOnce("onAfterRenderingDOMReady", function() {
 			sectionIsSelected(oObjectPage, assert, oExpected);
 			done();
-		}, this.iLoadingDelay);
+		});
 
 		helpers.renderObject(this.oObjectPage);
 	});
@@ -280,7 +280,7 @@
 			oExpected,
 			done = assert.async(); //async test needed because tab initialization is done onAfterRenderingDomReady (after HEADER_CALC_DELAY)
 
-		setTimeout(function () {
+		oObjectPage.attachEventOnce("onAfterRenderingDOMReady", function() {
 
 			// initially, the second section is selected (from the module setup)
 			oExpected = {
@@ -300,7 +300,7 @@
 			};
 			sectionIsSelected(oObjectPage, assert, oExpected);
 			done();
-		}, this.iLoadingDelay);
+		});
 
 		helpers.renderObject(this.oObjectPage);
 	});
@@ -317,7 +317,7 @@
 		oObjectPage.setHeaderTitle(oFactory.getHeaderTitle());
 		oObjectPage.addHeaderContent(oFactory.getHeaderContent());
 
-		setTimeout(function () {
+		oObjectPage.attachEventOnce("onAfterRenderingDOMReady", function() {
 
 			// initially, the second section is selected (from the module setup)
 			oExpected = {
@@ -341,7 +341,7 @@
 				assert.equal(oObjectPage._bHeaderExpanded, true, "Header is expnded");
 				done();
 			}, 0);
-		}, this.iLoadingDelay);
+		});
 
 		helpers.renderObject(this.oObjectPage);
 	});
