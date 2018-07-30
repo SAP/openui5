616c89bf0f0e8d203c3ae0ba07421db9cfde1310
Frank Weigel <frank.weigel@sap.com>
2018-07-30 23:18:01 +0200

    [INTERNAL][FIX] ui5loader: make script load error more informative
    The browser's 'error' event object doesn't provide meaningful
    information about why loading a script source failed. But the ui5loader
    can at least provide module ID and URL.
    
    Additionally, the text has been made more consistent with the error
    message in the sync loading case.
    
    Change-Id: Ibd97ded407b6b02541b975795ecd62857513ab09

--- src/sap.ui.core/src/ui5loader.js
+++ src/sap.ui.core/src/ui5loader.js
@@ -1122,7 +1122,8 @@
 			}
 
 			log.error("failed to load Javascript resource: " + oModule.name);
-			oModule.fail(ensureStacktrace(new Error("script load error")));
+			oModule.fail(
+				ensureStacktrace(new Error("failed to load '" + oModule.name +  "' from " + oModule.url + ": script load error")));
 		}
 
 		oScript = document.createElement('SCRIPT');
