08474eedc2c49e0d5501f841d49e034be41061f3
Lisa Klein <lisa.klein@sap.com>
2018-08-03 12:28:48 +0200

    [INTERNAL] Walkthrough Tutorial - Update tests step 28-37
    * Add Arrangement.js
    * Update qunit and opa html files
    * replace require with define
    * Add AllJourneys.js to instantiate the Arrangement object
    
    Change-Id: Ia9eae9cfef570d226c2a16cbaded01ca31658cf9
    CR-Id: 002075125900003581372018
    BCP: n/a

---
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/36/webapp/test/integration/AllJourneys.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Arrangement",
+	"./NavigationJourney"
+], function (Opa5, Arrangement) {
+	"use strict";
+
+	Opa5.extendConfig({
+		arrangements: new Arrangement(),
+		viewNamespace: "sap.ui.demo.walkthrough.view.",
+		autoWait: true
+	});
+});
