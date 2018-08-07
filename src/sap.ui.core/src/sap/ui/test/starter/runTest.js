782b6dc60468a61f11ee0089cd03b6d8c2ffdd40
Frank Weigel <frank.weigel@sap.com>
2018-08-07 22:48:45 +0200

    [INTERNAL] Test Starter: register QUnit callbacks in correct order
    In order to avoid the embedded sync loading of qunit-reporter.junit.js
    (reporter) during the execution of qunit-junit.js (bridge), the new test
    starter loads the reporter before loading the bridge. 
    
    While this avoids one sync request and eval, it has the side effect that
    bridge and reporter now register their QUnit callbacks in the wrong,
    inverse order. As a consequence, test names are prefixed with the test
    page name too later, only after their names have been recorded for the 
    JUnit report.
    
    With this change, the order of most callbacks is corrected again. To
    achieve this, the test starter first creates a mock version of the
    reporter's API, which only collects the registered callbacks. Then it
    runs the bridge, then the reporter and finally applies the collected
    callbacks to the real reporter API.
    
    This ensures the right order of the callbacks for 'moduleStart',
    'testStart' (and 'testDone'), but not of those for 'log'. However, the
    two 'log' callbacks seem to be independent, so their order shouldn't
    matter.
    
    Change-Id: Icb6790bb28c16fb08d1bdd61b826433131772c7c

--- src/sap.ui.core/src/sap/ui/test/starter/runTest.js
+++ src/sap.ui.core/src/sap/ui/test/starter/runTest.js
@@ -147,7 +147,7 @@
 
 	function initTestModule(oConfig) {
 		var pQUnit, pSinon, pSinonQUnitBridge, pSinonConfig, pCoverage, pTestEnv,
-			sQUnit;
+			sQUnit, aJUnitDoneCallbacks;
 
 		document.title = oConfig.title;
 
@@ -181,9 +181,21 @@
 					throw new TypeError("unsupported qunit version " + oConfig.qunit.version);
 				}
 			}).then(function() {
+				// install a mock version of the qunit-reporter-junit API to collect jUnitDone callbacks
+				aJUnitDoneCallbacks = [];
+				window.jUnitDone = function(cb) {
+					aJUnitDoneCallbacks.push(cb);
+				};
+				return requireP("sap/ui/qunit/qunit-junit");
+			}).then(function() {
+				delete window.jUnitDone;
 				return requireP("sap/ui/thirdparty/qunit-reporter-junit");
 			}).then(function() {
-				return requireP("sap/ui/qunit/qunit-junit");
+				// now register the collected callbacks with the real qunit-reporter-junit API
+				aJUnitDoneCallbacks.forEach(function(cb) {
+					window.jUnitDone(cb);
+				});
+				aJUnitDoneCallbacks = undefined;
 			});
 		}
 
