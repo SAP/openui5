21a63c8d40461e91a84ef4bd55190044088a0227
Adrian Bobev <adrian.bobev@sap.com>
2018-08-13 08:51:40 +0300

    [INTERNAL] sap.m.FacetFilter: add protected method to refresh FacetList
    This protected method will update FacetList model with the newly
    created/deleted list items if the model is updated.
    
    It could be used if async update of the model occured when
    navigating from filter detail page happens to the filter page.
    
    Change-Id: I9e1a6107cd4053d550831963b6a0d81c3a928299
    BCP: 1880435856
    CR-Id: 002075125900003601852018

--- src/sap.m/src/sap/m/FacetFilter.js
+++ src/sap.m/src/sap/m/FacetFilter.js
@@ -1386,8 +1386,6 @@ sap.ui.define([
 				// Destroy the search field bar
 				oFromPage.destroySubHeader();
 
-				oFacetPage.getContent()[0].getModel().setData({ items: that._getMapFacetLists() });
-
 				jQuery.sap.assert(that._displayedList === null, "Filter items list should have been placed back in the FacetFilter aggregation before page content is destroyed.");
 				oFromPage.destroyContent(); // Destroy the select all checkbox bar
 
@@ -1621,7 +1619,7 @@ sap.ui.define([
 	 */
 	FacetFilter.prototype._createFacetList = function() {
 
-		var oFacetList =  new sap.m.List({
+		var oFacetList = this._oFacetList = new sap.m.List({
 			mode: ListMode.None,
 			items: {
 				path: "/items",
@@ -1662,6 +1660,20 @@ sap.ui.define([
 
 		oFacetList.setModel(oModel);
 		return oFacetList;
+	};
+
+	/**
+	 * This method refreshes the internal model for thr FacetList. It should be called everytime when the model
+	 * of FacetFilter is changed and update to the FacetList is needed
+	 *
+	 * @protected
+	 * @sap-restricted hpa.cei.mkt.cal -> FacetFilter.controller -> OnDisplayRefreshed
+	 * @returns {sap.m.FacetFilter}
+	 */
+	FacetFilter.prototype.refreshFacetList = function () {
+		this._oFacetList.getModel().setData({ items: this._getMapFacetLists() });
+
+		return this;
 	};
 
 	FacetFilter.prototype._getMapFacetLists = function () {
