SAP/openui5

commit 321f1ae26240d1c6375b00b9fac0ab4a0ed429c1

[INTERNAL] JSDoc: fix JSDoc errors as reported by build
Change-Id: Ie953e964a74d7a4f4f6b671edd1da8d15a94acff

src/sap.ui.table/src/sap/ui/table/TableUtils.js
@@ -1201,7 +1201,7 @@ sap.ui.define([
 		 * Facilitates dynamic calling.
 		 *
 		 * @param {function():T | T} vObject The object, or a function returning the object, on which methods will be called.
-		 * @param {function(this:U, T) | Object<string, *[]>} vCall Called if <code>vObject</code> is, or returns an object.
+		 * @param {function(this:U, T) | Object<string, Array.<*>} vCall Called if <code>vObject</code> is, or returns an object.
 		 * @param {U} [oThis] Context in the function calls, or in the callback if <code>vCall</code>is a function. Default is <code>vObject</code>.
 		 * @template T, U
 		 */

src/sap.m/src/sap/m/SliderTooltip.js
@@ -33,7 +33,7 @@ function(
 		 * @class
 		 * A Control that visualizes <code>Slider</code> and <code>RangeSlider</code> tooltips.
 		 *
-		 * @extends sap.ui.core.Control
+		 * @extends sap.m.SliderTooltipBase
 		 *
 		 * @author SAP SE
 		 * @version ${version}

src/sap.ui.core/src/sap/ui/core/routing/Views.js
@@ -25,7 +25,7 @@ sap.ui.define([
 		 * is used standalone without the involvement of a Router. Otherwise the async option is inherited from the Router.
 		 * @alias sap.ui.core.routing.Views
 		 */
-		return TargetCache.extend("sap.ui.core.routing.Views", /** @lends sap.ui.core.routing.Views.prototype */ {
+		var Views = TargetCache.extend("sap.ui.core.routing.Views", /** @lends sap.ui.core.routing.Views.prototype */ {
 			metadata: {
 				publicMethods: ["getView", "setView"]
 			},
@@ -136,5 +136,7 @@ sap.ui.define([
 				return this.fireEvent("created", mArguments);
 			}
 		});
+
+		return Views;
 
 	});

src/sap.ui.dt/src/sap/ui/dt/ContextMenuControl.js
@@ -34,7 +34,7 @@ sap.ui.define([
 	 * @param {string} [sId] id for the new control, generated automatically if no id is given
 	 * @param {object} [mSettings] initial settings for the new control
 	 * @class A simple ContextMenu.
-	 * @extends sap.ui.core.Control
+	 * @extends sap.ui.base.ManagedObject
 	 * @author SAP SE
 	 * @version ${version}
 	 * @constructor

src/sap.ui.fl/src/sap/ui/fl/support/apps/contentbrowser/utils/DataUtils.js
@@ -2,7 +2,7 @@
  * ${copyright}
  */
 
-sap.ui.define("sap/ui/fl/support/apps/contentbrowser/utils/DataUtils", ["sap/m/GroupHeaderListItem", "sap/ui/thirdparty/jquery"],
+sap.ui.define(["sap/m/GroupHeaderListItem", "sap/ui/thirdparty/jquery"],
 	function(GroupHeaderListItem, jQuery) {
 		"use strict";
 

src/sap.ui.fl/src/sap/ui/fl/support/apps/contentbrowser/utils/ErrorUtils.js
@@ -2,7 +2,7 @@
  * ${copyright}
  */
 
-sap.ui.define("sap/ui/fl/support/apps/contentbrowser/utils/ErrorUtils",[
+sap.ui.define([
 	"sap/m/MessagePopoverItem",
 	"sap/m/MessagePopover"],
 	function (MessagePopoverItem, MessagePopover) {

src/sap.ui.rta/src/sap/ui/rta/command/ControlVariantConfigure.js
@@ -54,7 +54,8 @@ sap.ui.define([
 	};
 
 	/**
-	 * @public Triggers the configuration of a variant
+	 * Triggers the configuration of a variant.
+	 * @public
 	 * @returns {Promise} Returns resolve after execution
 	 */
 	ControlVariantConfigure.prototype.execute = function() {
@@ -76,7 +77,8 @@ sap.ui.define([
 	};
 
 	/**
-	 * @public Undo logic for the execution
+	 * Undo logic for the execution.
+	 * @public
 	 * @returns {Promise} Returns resolve after undo
 	 */
 	ControlVariantConfigure.prototype.undo = function() {

src/sap.ui.rta/src/sap/ui/rta/command/ControlVariantDuplicate.js
@@ -57,7 +57,8 @@ sap.ui.define([
 	};
 
 	/**
-	 * @public Triggers the duplication of a variant
+	 * Triggers the duplication of a variant.
+	 * @public
 	 * @returns {Promise} Returns resolve after execution
 	 */
 	ControlVariantDuplicate.prototype.execute = function() {
@@ -92,7 +93,8 @@ sap.ui.define([
 	};
 
 	/**
-	 * @public Undo logic for the execution
+	 * Undo logic for the execution.
+	 * @public
 	 * @returns {Promise} Returns resolve after undo
 	 */
 	ControlVariantDuplicate.prototype.undo = function() {

commit 9999b2413409bd23f3f248a728dbf3f324396912

[INTERNAL] JSDoc: fix the JSDoc fix
Commit 321f1ae introduced a typo which is fixed now.

Change-Id: I1f00d33494bfac51c0ade7d1d8b9511b1301b4a4

src/sap.ui.table/src/sap/ui/table/TableUtils.js
@@ -1201,7 +1201,7 @@ sap.ui.define([
 		 * Facilitates dynamic calling.
 		 *
 		 * @param {function():T | T} vObject The object, or a function returning the object, on which methods will be called.
-		 * @param {function(this:U, T) | Object<string, Array.<*>} vCall Called if <code>vObject</code> is, or returns an object.
+		 * @param {function(this:U, T) | Object<string, Array.<*>>} vCall Called if <code>vObject</code> is, or returns an object.
 		 * @param {U} [oThis] Context in the function calls, or in the callback if <code>vCall</code>is a function. Default is <code>vObject</code>.
 		 * @template T, U
 		 */