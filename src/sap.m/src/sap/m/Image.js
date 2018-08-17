158e8a377f03a63734cb428375044c48b146d998
i350970 <plamen.ivanov01@sap.com>
2018-08-17 16:22:18 +0300

    [FIX] Image: Fixed broken indicator in some browsers
    The Problem: A broken image indicator(icon) is shown in some browsers,
    when the "src" property is invalid.
    
    The Root Cause: We are removing the "sapMNoImg" when we are not
    supposed to. By doing so, we are making the "broken" indicator
    icon - visible.
    
    The Solution:
    We are making the image visible(removing the "sapMNoImg" css class) only
    and only if we have "alt" property set to a custom string value and
    "decorative" set to false.
    
    The "decorative" property is mandatory to be set to false, in order for
    "alt" to take effect.
    
    Change-Id: I2bfaeeaee81370ef1103ca5cc9420964cb52063f
    BCP: 1880526262

--- src/sap.m/src/sap/m/Image.js
+++ src/sap.m/src/sap/m/Image.js
@@ -267,7 +267,8 @@ sap.ui.define([
 		$DomNode.addClass("sapMNoImg");
 
 		// if src is empty or there's no image existing, just stop
-		if (!sSrc || this._iLoadImageDensity === 1) {
+		// BCP: 1880526262
+		if ((!sSrc || this._iLoadImageDensity === 1) && this.getAlt() && !this.getDecorative()) {
 			// remove the "sapMNoImg" in order to show the alt text
 			$DomNode.removeClass("sapMNoImg");
 			this.fireError();
