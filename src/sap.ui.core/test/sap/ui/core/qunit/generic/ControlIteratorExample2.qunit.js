98902d79a2061a487e531e9c18081a0ec92d383e
Frank Weigel <frank.weigel@sap.com>
2018-08-18 10:45:10 +0200

    [INTERNAL] Core: migrate 'generic' testsuite to new test starter
    (cherry picked from commit dfed9887219598d9e8adef8a42c699dec2bab1ba)
    Change-Id: I33fb89751011c2b270107043342ca36963ad4ec3
    CR-Id: 002075125800003036742018
    BCP: n/a

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/generic/ControlIteratorExample2.qunit.js
@@ -0,0 +1,20 @@
+/*global QUnit */
+sap.ui.define([
+	"sap/ui/qunit/utils/ControlIterator"
+], function(ControlIterator) {
+
+	QUnit.test("Testing all controls", function(assert) { // one single QUnit test for all controls
+		var testDone = assert.async();                    // this test is asynchronous, hence need to tell QUnit later when done
+
+		ControlIterator.run(function(sControlName, oControlClass, oInfo) { // loop over all controls
+			assert.ok(true, sControlName + " would be tested now");        // do one or more asserts per control
+		},{
+			done: function(oResult) {
+				// do something when all control tests have been executed
+
+				testDone(); // tell QUnit that this test is done
+			}
+		});
+	});
+
+});
