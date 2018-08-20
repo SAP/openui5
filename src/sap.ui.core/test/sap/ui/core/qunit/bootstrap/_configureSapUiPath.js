60eb340c729a1e96b82ee6e91eaba59d1cb87d4e
Matthias Osswald <mat.osswald@sap.com>
2018-08-20 14:51:26 +0200

    [INTERNAL][FIX] Core: Bootstrap ResourceRoot QUnit tests in IE11
    In IE11, the sap/base/strings/NormalizePolyfill is loaded on demand
    which requires the "sap.base" namespace to be configured properly.
    
    Change-Id: Id14ae8286ed15d12a63a45a846d1ff063c65a978
    BCP: 1870331181

--- src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/_configureSapUiPath.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/_configureSapUiPath.js
@@ -2,6 +2,7 @@
 	"use strict";
 	window["sap-ui-config"] = {
 		resourceRoots : {
+			"sap.base": "../../../../../../resources/sap/base/",
 			"sap.ui" : "../../../../../../resources/sap/ui/"
 		}
 	};
