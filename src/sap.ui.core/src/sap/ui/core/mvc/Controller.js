535d71a3b8d144ae415d09d67bfdad394eba6c08
Jan Mummenthaler <jan.mummenthaler@sap.com>
2018-08-23 14:38:25 +0200

    [FIX] Controller: controllerFactory resolves wrong controller class
    Change-Id: I5379929836ea10d7362d6b97d7bdefa7d6895e27
    CR-Id: 002075125800003109342018
    BCP: 1870341066

--- src/sap.ui.core/src/sap/ui/core/mvc/Controller.js
+++ src/sap.ui.core/src/sap/ui/core/mvc/Controller.js
@@ -256,7 +256,7 @@ sap.ui.define([
 				throw new Error("Controller name ('sName' parameter) is required");
 			}
 
-			var sControllerName = sName.replace(/\./g, "/"),
+			var sControllerName = sName.replace(/\./g, "/") + ".controller",
 				ControllerClass = resolveClass(sap.ui.require(sControllerName));
 
 			function resolveClass(ControllerClass) {
@@ -269,8 +269,6 @@ sap.ui.define([
 					return jQuery.sap.getObject(sName);
 				}
 			}
-
-			sControllerName = sControllerName + ".controller";
 
 			if (bAsync) {
 				return new Promise(function(resolve, reject) {
