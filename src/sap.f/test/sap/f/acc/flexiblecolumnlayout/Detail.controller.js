58e2e266a4e527e42977bf52f21ad7d6034c6021
I311640 <mihail.yordanov@sap.com>
2018-08-23 11:30:07 +0300

    [INTERNAL] f.FlexibleColumnLayout: Accessibility test page refactored
    BCP: 1880228056
    -ACC test page refactored with a FCL routing example, to enable viewing
    in 200% or higher by ACC testers
    
    Change-Id: I7ad1b6710e19898e1ca58652397b6eff771f7bde

---
+++ src/sap.f/test/sap/f/acc/flexiblecolumnlayout/Detail.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/Controller"
+], function (JSONModel, Controller) {
+	"use strict";
+
+	return Controller.extend("flexiblecolumnlayout.Detail", {
+		onInit: function () {
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this.oModel = this.getOwnerComponent().getModel();
+		},
+		handleDetailPress: function () {
+			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(2);
+			this.oRouter.navTo("detailDetail", {layout: oNextUIState.layout});
+		},
+		handleClose: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/closeColumn");
+			this.oRouter.navTo("master", {layout: sNextLayout});
+		}
+	});
+}, true);
