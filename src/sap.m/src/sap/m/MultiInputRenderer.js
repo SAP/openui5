067c6662b15da7c2218a7904436aa9b787a62d55
Elena Stoyanova <elena.stoyanova@sap.com>
2018-08-23 15:30:07 +0300

    [FIX] sap.m.MultiInput: Token information is now announced
    Change-Id: Ia9d68190076e41be4d648b3e3e1afea76239ea56
    BCP: 1880542259
    CR-Id: 002075125800003838212018

--- src/sap.m/src/sap/m/MultiInputRenderer.js
+++ src/sap.m/src/sap/m/MultiInputRenderer.js
@@ -26,6 +26,21 @@ sap.ui.define(['./InputRenderer', 'sap/ui/core/Renderer'],
 		}
 	};
 
+	MultiInputRenderer.getAriaDescribedBy = function(oControl) {
+		// input method should be overwritten in order to add the tokens information
+		var sAriaDescribedBy = InputRenderer.getAriaDescribedBy.apply(this, arguments),
+			oInvisibleTextId = oControl.getAggregation("tokenizer") &&
+				oControl.getAggregation("tokenizer").getTokensInfoId();
+
+		if (sAriaDescribedBy) {
+			sAriaDescribedBy = sAriaDescribedBy + " " + oInvisibleTextId;
+		} else {
+			sAriaDescribedBy = oInvisibleTextId ;
+		}
+
+		return sAriaDescribedBy;
+	};
+
 	return MultiInputRenderer;
 
 }, /* bExport= */ true);
