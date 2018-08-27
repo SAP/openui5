960e83771fc3b5ec4d25a10d78f3e2645315d503
Adrian Bobev <adrian.bobev@sap.com>
2018-08-27 15:38:57 +0300

    [FIX] sap.m.StepInput: correct behavior when mousewheel is used
    Previously the native behavior of the input is used and when
    a mousewheel is used it increses and decreses the value.
    However, in IE and Edge this is not supported.
    
    Furthermore since we do not set max and min attributes to the
    input, the validation did not work in Chrome too.
    
    Now mousewheel events are handled properly
    
    Change-Id: I0b10f771be0083bcd534e3f56c90da93149e7bba
    BCP: 1870334890
    CR-Id: 002075125800003178382018

--- src/sap.m/src/sap/m/StepInput.js
+++ src/sap.m/src/sap/m/StepInput.js
@@ -441,6 +441,8 @@ function(
 			this._iRealPrecision = 0;
 			this._attachChange();
 			this._bPaste = false; //needed to indicate when a paste is made
+
+			this._onmousewheel = this._onmousewheel.bind(this);
 		};
 
 		/**
@@ -463,6 +465,16 @@ function(
 			}
 			this._disableButtons(vValue, fMax, fMin);
 
+		};
+
+		StepInput.prototype.onAfterRendering = function () {
+			var $domRef = this.$();
+			$domRef.unbind(Device.browser.firefox ? "DOMMouseScroll" : "mousewheel", this._onmousewheel);
+			$domRef.bind(Device.browser.firefox ? "DOMMouseScroll" : "mousewheel", this._onmousewheel);
+		};
+
+		StepInput.prototype.exit = function () {
+			this.$().unbind(Device.browser.firefox ? "DOMMouseScroll" : "mousewheel", this._onmousewheel);
 		};
 
 		StepInput.prototype.setProperty = function (sPropertyName, oValue, bSuppressInvalidate) {
@@ -969,6 +981,15 @@ function(
 		StepInput.prototype.onsapdown = function (oEvent) {
 			oEvent.preventDefault(); //prevents the value to decrease by one (Chrome and Firefox default behavior)
 			this._applyValue(this._calculateNewValue(1, false).displayValue);
+			this._verifyValue();
+		};
+
+		StepInput.prototype._onmousewheel = function (oEvent) {
+			oEvent.preventDefault();
+			var oOriginalEvent = oEvent.originalEvent,
+				bDirectionPositive = oOriginalEvent.detail ? (-oOriginalEvent.detail > 0) : (oOriginalEvent.wheelDelta > 0);
+
+			this._applyValue(this._calculateNewValue(1, bDirectionPositive).displayValue);
 			this._verifyValue();
 		};
 
