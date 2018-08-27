327ce9c019b53ba245c827bedc968beb7f40bd19
Matthias Osswald <mat.osswald@sap.com>
2018-08-27 15:30:31 +0200

    [INTERNAL][FIX] Core: compat testsuite
    - Convert indentation to tabs
    - Fix jquery.sap.stubs execution (test-mode, chunks)
      - jquery.sap.stubs requires a custom page as the ui5loader will always
        be configured "async" by the test starter which prevents testing the
        loader in sync mode
    
    Follow-up of f94add7f93dcf4bd6b79990f4a36bab86b0e26da
    
    BCP: 1870355774
    Change-Id: I71f892d444dfa4ade6448681153c34df0e784fff

--- src/sap.ui.core/test/sap/ui/core/qunit/compat/jquery.sap.stubs.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/compat/jquery.sap.stubs.qunit.js
@@ -1,264 +1,223 @@
 /* global QUnit, sinon */
 (function () {
-    "use strict";
+	"use strict";
 
-    function appendScript(sSrc, fnOnload, sId) {
-        var oScript = document.createElement("script");
-        oScript.src = sSrc;
-        oScript.id = sId;
-        oScript.addEventListener("load", fnOnload);
-        document.head.appendChild(oScript);
-    }
+	QUnit.config.autostart = false;
 
-    function boot() {
-        return new Promise(function (resolve, reject) {
+	function beforeEach() {
+		return new Promise(function (resolve, reject) {
 
-            function includeLoaderScripts(callback) {
-                // Load ui5loader without requiring Core.js to be able to require stubs individually
-                appendScript("resources/ui5loader.js", function () {
+			// Enable exporting stubbing config
+			window["jquery.sap.stubs-test"] = true;
 
-                    sap.ui.loader._.logger = {
-                        debug: function () {
-                            // Prevent spamming the conosle with unnecessary info
-                            //console.log.apply(console, arguments); // eslint-disable-line no-console
-                        },
-                        info: function () {
-                            // Prevent spamming the conosle with unnecessary info
-                            //console.log.apply(console, arguments); // eslint-disable-line no-console
-                        },
-                        warning: function () {
-                            console.warn.apply(console, arguments); // eslint-disable-line no-console
-                        },
-                        error: function () {
-                            console.error.apply(console, arguments); // eslint-disable-line no-console
-                        },
-                        isLoggable: function () { return true; }
-                    };
+			// Load stubbing layer and resolve/reject promise
+			sap.ui.require(["jquery.sap.stubs"], function () {
 
-                    appendScript("resources/ui5loader-autoconfig.js", callback, "sap-ui-bootstrap");
-                });
-            }
+				// Read exported stubbing config and remove globals
+				var mStubs = window["jquery.sap.stubs-test"];
+				delete window["jquery.sap.stubs-test"];
 
-            includeLoaderScripts(function () {
+				resolve(mStubs);
+			}, reject);
 
-                // Loading further modules needs to be delayed so that the define calls within
-                // ui5loader-autoconfig.js are executed first. This might happen in PhantomJS or
-                // Chrome when the tab is inactive.
-                // See @evo-todo in ui5loader-autoconfig.js
-                setTimeout(resolve);
+		});
+	}
 
-            });
+	function afterEach(mStubs) {
 
-        });
-    }
+		// Unload stubbing module
+		sap.ui.loader._.unloadResources("jquery.sap.stubs.js", false, true, true);
 
-    function beforeEach() {
-        return new Promise(function (resolve, reject) {
+		// Unload all modules which will be loaded by triggering a stub
+		Object.keys(mStubs).forEach(function (sStubName) {
+			var oStub = mStubs[sStubName];
+			var oStubs = oStub.stubs;
+			Object.keys(oStubs).forEach(function (sModule) {
+				sap.ui.loader._.unloadResources(sModule + ".js", false, true, true);
+			});
+		});
 
-            // Enable exporting stubbing config
-            window["jquery.sap.stubs-test"] = true;
+		// Unload jQuery and remove global variable
+		sap.ui.loader._.unloadResources("sap/ui/thirdparty/jquery.js", false, true, true);
+		delete window.jQuery;
+	}
 
-            // Load stubbing layer and resolve/reject promise
-            sap.ui.require(["jquery.sap.stubs"], function () {
+	function isStubbed(oTarget, sProperty) {
+		var oDescriptor = Object.getOwnPropertyDescriptor(oTarget, sProperty);
+		return !!(oDescriptor && oDescriptor.get && oDescriptor.get["jquery.sap.stubs"]);
+	}
 
-                // Read exported stubbing config and remove globals
-                var mStubs = window["jquery.sap.stubs-test"];
-                delete window["jquery.sap.stubs-test"];
+	function defineModule(sName) {
+		QUnit.module(sName, {
+			startup: function () {
+				return beforeEach().then(function (mStubs) {
+					this.mStubs = mStubs;
 
-                resolve(mStubs);
-            }, reject);
+					// Note: Log is used within jquery.sap.stubs, so it should be loaded already
+					var Log = sap.ui.require("sap/base/Log");
+					this.oWarningLogSpy = this.spy(Log, "warning");
 
-        });
-    }
+					return mStubs;
+				}.bind(this));
+			},
+			afterEach: function () {
+				afterEach(this.mStubs);
+			}
+		});
+	}
 
-    function afterEach(mStubs) {
+	var TEST_TYPE = {
+		"LAZY_LOADING": "lazy-loading",
+		"STUB_REPLACEMENT": "stub-replacement"
+	};
+	var defineTest = {};
 
-        // Unload stubbing module
-        sap.ui.loader._.unloadResources("jquery.sap.stubs.js", false, true, true);
+	defineTest[TEST_TYPE.LAZY_LOADING] = function (sStubName, sProperty, sModule) {
+		QUnit.test(sStubName + sProperty, function (assert) {
+			return this.startup().then(function (mStubs) {
 
-        // Unload all modules which will be loaded by triggering a stub
-        Object.keys(mStubs).forEach(function (sStubName) {
-            var oStub = mStubs[sStubName];
-            var oStubs = oStub.stubs;
-            Object.keys(oStubs).forEach(function (sModule) {
-                sap.ui.loader._.unloadResources(sModule + ".js", false, true, true);
-            });
-        });
+				// Make sure to get correct target reference as the stubbing layer is loaded for each test
+				var oTarget = mStubs[sStubName].target;
 
-        // Unload jQuery and remove global variable
-        sap.ui.loader._.unloadResources("sap/ui/thirdparty/jquery.js", false, true, true);
-        delete window.jQuery;
-    }
+				assert.ok(isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should be stubbed.");
 
-    function isStubbed(oTarget, sProperty) {
-        var oDescriptor = Object.getOwnPropertyDescriptor(oTarget, sProperty);
-        return !!(oDescriptor && oDescriptor.get && oDescriptor.get["jquery.sap.stubs"]);
-    }
+				// Trigger stubbing layer by accessing the property
+				var vValue = oTarget[sProperty];
 
-    function defineModule(sName) {
-        QUnit.module(sName, {
-            startup: function () {
-                return beforeEach().then(function (mStubs) {
-                    this.mStubs = mStubs;
+				// Stub should lazy load the module
+				sinon.assert.calledWith(this.oWarningLogSpy,
+					sinon.match("Sync loading of module"),
+					"jquery.sap.stubs"
+				);
 
-                    // Note: Log is used within jquery.sap.stubs, so it should be loaded already
-                    var Log = sap.ui.require("sap/base/Log");
-                    this.oWarningLogSpy = this.spy(Log, "warning");
+				assert.ok(!isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should not be stubbed anymore.");
 
-                    return mStubs;
-                }.bind(this));
-            },
-            afterEach: function () {
-                afterEach(this.mStubs);
-            }
-        });
-    }
+				assert.ok(typeof vValue !== "undefined", "Property  '" + sProperty + "' should have a value");
+				assert.ok(sap.ui.require(sModule), "Underlaying module should have been loaded");
 
-    var TEST_TYPE = {
-        "LAZY_LOADING": "lazy-loading",
-        "STUB_REPLACEMENT": "stub-replacement"
-    };
-    var defineTest = {};
+			}.bind(this));
+		});
+	};
+	defineTest[TEST_TYPE.STUB_REPLACEMENT] = function (sStubName, sProperty, sModule) {
+		QUnit.test(sStubName + sProperty, function (assert) {
+			return this.startup().then(function (mStubs) {
 
-    defineTest[TEST_TYPE.LAZY_LOADING] = function (sStubName, sProperty, sModule) {
-        QUnit.test(sStubName + sProperty, function (assert) {
-            return this.startup().then(function (mStubs) {
+				// Make sure to get correct target reference as the stubbing layer is loaded for each test
+				var oTarget = mStubs[sStubName].target;
 
-                // Make sure to get correct target reference as the stubbing layer is loaded for each test
-                var oTarget = mStubs[sStubName].target;
+				assert.ok(isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should be stubbed.");
 
-                assert.ok(isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should be stubbed.");
+				// Load underlaying module
+				return new Promise(function (resolve, reject) {
+					sap.ui.require([sModule], resolve, reject);
+				}).then(function () {
 
-                // Trigger stubbing layer by accessing the property
-                var vValue = oTarget[sProperty];
+					// Stub should not lazy load the module
+					sinon.assert.neverCalledWith(this.oWarningLogSpy,
+						sinon.match("Sync loading of module"),
+						"jquery.sap.stubs"
+					);
 
-                // Stub should lazy load the module
-                sinon.assert.calledWith(this.oWarningLogSpy,
-                    sinon.match("Sync loading of module"),
-                    "jquery.sap.stubs"
-                );
+					assert.ok(!isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should not be stubbed anymore.");
 
-                assert.ok(!isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should not be stubbed anymore.");
+					// Access previously stubbed property
+					var vValue = oTarget[sProperty];
 
-                assert.ok(typeof vValue !== "undefined", "Property  '" + sProperty + "' should have a value");
-                assert.ok(sap.ui.require(sModule), "Underlaying module should have been loaded");
+					assert.ok(typeof vValue !== "undefined", "Property  '" + sProperty + "' should have a value");
 
-            }.bind(this));
-        });
-    };
-    defineTest[TEST_TYPE.STUB_REPLACEMENT] = function (sStubName, sProperty, sModule) {
-        QUnit.test(sStubName + sProperty, function (assert) {
-            return this.startup().then(function (mStubs) {
+				}.bind(this));
+			}.bind(this));
+		});
+	};
 
-                // Make sure to get correct target reference as the stubbing layer is loaded for each test
-                var oTarget = mStubs[sStubName].target;
+	function defineTestCategory(oContext, oConfig, sTestType) {
+		if (oConfig.mode === "all" || oConfig.mode === sTestType) {
+			defineModule(oContext.moduleName);
+			oContext.properties.forEach(function (sProperty) {
+				oConfig.i++;
+				if (!oConfig.runInChunks || (oConfig.i >= oConfig.start && oConfig.i <= oConfig.end)) {
+					oConfig.testsDefined++;
+					defineTest[sTestType](oContext.stubName, sProperty, oContext.module);
+				}
+			});
+		}
+	}
 
-                assert.ok(isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should be stubbed.");
+	function getTestConfig() {
+		return new Promise(function (resolve, reject) {
+			sap.ui.require(["sap/base/util/UriParameters"], resolve, reject);
+		}).then(function (UriParameters) {
+			var oParams = new UriParameters(document.location.href);
+			var oConfig = {};
 
-                // Load underlaying module
-                return new Promise(function (resolve, reject) {
-                    sap.ui.require([sModule], resolve, reject);
-                }).then(function () {
+			oConfig.mode = oParams.get("test-mode") || "all";
+			if (oParams.get("chunk")) {
+				oConfig.chunk = parseInt(oParams.get("chunk"), 10);
+			}
 
-                    // Stub should not lazy load the module
-                    sinon.assert.neverCalledWith(this.oWarningLogSpy,
-                        sinon.match("Sync loading of module"),
-                        "jquery.sap.stubs"
-                    );
+			// Chunk paging logic
+			oConfig.i = 0;
+			oConfig.testsDefined = 0;
+			oConfig.runInChunks = !!oConfig.chunk;
+			if (oConfig.chunk) {
+				oConfig.start = ((oConfig.chunk - 1) * 50) + 1;
+				oConfig.end = (oConfig.chunk) * 50;
+			}
 
-                    assert.ok(!isStubbed(oTarget, sProperty), "Property '" + sProperty + "' should not be stubbed anymore.");
+			return oConfig;
+		});
+	}
 
-                    // Access previously stubbed property
-                    var vValue = oTarget[sProperty];
+	// Running startup once to define tests
+	beforeEach().then(function (mStubs) {
+		return getTestConfig().then(function (oConfig) {
+			var bLoaderAsync = sap.ui.loader.config().async;
 
-                    assert.ok(typeof vValue !== "undefined", "Property  '" + sProperty + "' should have a value");
+			document.title = "QUnit tests for jquery.sap.stubs: " +
+				(bLoaderAsync ? "async, " : "sync, ") +
+				oConfig.mode +
+				(oConfig.runInChunks ? (", chunk " + oConfig.chunk) : "");
 
-                }.bind(this));
-            }.bind(this));
-        });
-    };
+			Object.keys(mStubs).forEach(function (sStubName) {
+				var oStub = mStubs[sStubName];
+				var oStubs = oStub.stubs;
 
-    function defineTestCategory(oContext, oConfig, sTestType) {
-        if (oConfig.mode === "all" || oConfig.mode === sTestType) {
-            defineModule(oContext.moduleName);
-            oContext.properties.forEach(function (sProperty) {
-                oConfig.i++;
-                if (!oConfig.runInChunks || (oConfig.i >= oConfig.start && oConfig.i <= oConfig.end)) {
-                    oConfig.testsDefined++;
-                    defineTest[sTestType](oContext.stubName, sProperty, oContext.module);
-                }
-            });
-        }
-    }
+				Object.keys(oStubs).forEach(function (sModule) {
+					var aProperties = oStubs[sModule];
 
-    function getTestConfig() {
-        return new Promise(function (resolve, reject) {
-            sap.ui.require(["sap/base/util/UriParameters"], resolve, reject);
-        }).then(function (UriParameters) {
-            var oParams = new UriParameters(document.location.href);
-            var oConfig = {};
+					defineTestCategory({
+						moduleName: sModule + ": Lazy loading " + (bLoaderAsync ? " (async)" : "(sync)"),
+						module: sModule,
+						stubName: sStubName,
+						properties: aProperties
+					}, oConfig, TEST_TYPE.LAZY_LOADING);
 
-            oConfig.mode = oParams.get("test-mode") || "all";
-            if (oParams.get("chunk")) {
-                oConfig.chunk = parseInt(oParams.get("chunk"), 10);
-            }
+					defineTestCategory({
+						moduleName: sModule + ": Stub replacement " + (bLoaderAsync ? " (async)" : "(sync)"),
+						module: sModule,
+						stubName: sStubName,
+						properties: aProperties
+					}, oConfig, TEST_TYPE.STUB_REPLACEMENT);
 
-            // Chunk paging logic
-            oConfig.i = 0;
-            oConfig.testsDefined = 0;
-            oConfig.runInChunks = !!oConfig.chunk;
-            if (oConfig.chunk) {
-                oConfig.start = ((oConfig.chunk - 1) * 50) + 1;
-                oConfig.end = (oConfig.chunk) * 50;
-            }
+				});
+			});
 
-            return oConfig;
-        });
-    }
+			if (oConfig.testsDefined === 0) {
+				QUnit.test("No tests defined!", function (assert) {
+					assert.ok(false, "No tests have been defined. Please check the URL parameters");
+				});
+			}
 
-    // Running startup once to define tests
-    boot().then(beforeEach).then(function (mStubs) {
-        return getTestConfig().then(function (oConfig) {
-            Object.keys(mStubs).forEach(function (sStubName) {
-                var oStub = mStubs[sStubName];
-                var oStubs = oStub.stubs;
-
-                Object.keys(oStubs).forEach(function (sModule) {
-                    var aProperties = oStubs[sModule];
-
-                    defineTestCategory({
-                        moduleName: sModule + ": Lazy loading",
-                        module: sModule,
-                        stubName: sStubName,
-                        properties: aProperties
-                    }, oConfig, TEST_TYPE.LAZY_LOADING);
-
-                    defineTestCategory({
-                        moduleName: sModule + ": Stub replacement",
-                        module: sModule,
-                        stubName: sStubName,
-                        properties: aProperties
-                    }, oConfig, TEST_TYPE.STUB_REPLACEMENT);
-
-                });
-            });
-
-            if (oConfig.testsDefined === 0) {
-                QUnit.test("No tests defined!", function (assert) {
-                    assert.ok(false, "No tests have been defined. Please check the URL parameters");
-                });
-            }
-
-        }).then(function () {
-            afterEach(mStubs);
-        });
-    }).then(function () {
-        QUnit.start();
-    }, function (err) {
-        QUnit.test("Error during test setup", function (assert) {
-            assert.ok(false, err + "\n" + err.stack);
-        });
-        QUnit.start();
-    });
+		}).then(function () {
+			afterEach(mStubs);
+		});
+	}).then(function () {
+		QUnit.start();
+	}, function (err) {
+		QUnit.test("Error during test setup", function (assert) {
+			assert.ok(false, err + "\n" + err.stack);
+		});
+		QUnit.start();
+	});
 
 })();
