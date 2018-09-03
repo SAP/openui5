bf5bbac416c5430aae7e9cd6999c6a183ed310e8
Jay Barbhaiya <jay.barbhaiya@sap.com>
2018-09-03 17:27:47 +0200

    [INTERNAL] sap.m.Column: overwriting getFocusDomRef of the Element
    When the active column header is clicked, a popover open and after closing
    the popover, the focus is lost and not retained by the active column headers.
    Hence to retain the focus the Column control now overwrites the getFocusDomRef()
    where the focusable DIV is returned by the function.
    BCP: 1870361291
    
    Change-Id: Id286970ec9610222c22bea4b6e8d7c8d1197c9d5

--- src/sap.m/src/sap/m/Column.js
+++ src/sap.m/src/sap/m/Column.js
@@ -624,6 +624,19 @@ sap.ui.define([
 		this.clearLastValue();
 	};
 
+	// when the popover opens and later closed, the focus is lost
+	// hence overwriting the getFocusDomRef to restore the focus on the active column header
+	Column.prototype.getFocusDomRef = function() {
+		var oParent = this.getParent();
+		if (oParent && oParent.bActiveHeaders) {
+			var oColumnDomRef = this.getDomRef();
+			if (oColumnDomRef) {
+				return oColumnDomRef.firstChild;
+			}
+		}
+		return Element.prototype.getFocusDomRef.apply(this, arguments);
+	};
+
 	return Column;
 
 });
