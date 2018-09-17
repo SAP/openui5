3c5b26ed1d4176145121049a8087796008215a10
Jordan Dichev <jordan.dichev@sap.com>
2018-09-17 14:37:48 +0300

    [FIX] OverflowToolbar: Fixed resetting of child size cache
    The size cache of aggregated controls was being reset too aggressively
    and now it only happens when there is a theme changed.
    
    CR-Id: 002075125800003817332018
    Change-Id: I3efc7274cf4230beed63ce0d67176c03ab15eea0
    BCP: 1840234392
    (cherry picked from commit a5fccfc9377aa523988970e1eaee583c0cc64b86)

--- src/sap.m/src/sap/m/OverflowToolbar.js
+++ src/sap.m/src/sap/m/OverflowToolbar.js
@@ -609,12 +609,6 @@ sap.ui.define([
 
 		this._resetToolbar();
 
-		for (var iControlSize in this._aControlSizes) {
-			if (this._aControlSizes.hasOwnProperty(iControlSize)) {
-				this._aControlSizes[iControlSize] = 0; // reset saved control sizes
-			}
-		}
-
 		this._bControlsInfoCached = false;
 		this._bNestedInAPopover = null;
 		this._iPreviousToolbarWidth = null;
@@ -1114,6 +1108,12 @@ sap.ui.define([
 
 	OverflowToolbar.prototype.onThemeChanged = function () {
 		this._resetAndInvalidateToolbar();
+
+		for (var iControlSize in this._aControlSizes) {
+			if (this._aControlSizes.hasOwnProperty(iControlSize)) {
+				this._aControlSizes[iControlSize] = 0; // reset saved control sizes
+			}
+		}
 	};
 
 	/**
