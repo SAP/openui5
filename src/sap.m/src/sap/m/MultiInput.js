ce1d644e6777aff67b3472aa2c92dafdd4fd7fd8
Yavor Ivanov <yavor.ivanov@sap.com>
2018-09-28 13:51:58 +0300

    [FIX] sap.m.MultiInput: Fire change event on enter on mobile
    MultiInput should fire change when on mobile device and
    "Enter" is pressed.
    
    This change also reverts the behavior introduced with: Ib8f65f89a91ae1d37f76913b65abf9203958c597
    
    BCP: 002075129500004456722018
    
    Change-Id: I5dea225769d275db75bca2e6510bf4b4a3a2eebe
    CR-Id: 002075125800004108382018

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -488,6 +488,9 @@ sap.ui.define(['jquery.sap.global', './Input', './Tokenizer', './Token', './libr
 		//add token when no suggestion item
 		if (this._oSuggestionTable.getItems().length === 0) {
 			this._oPopupInput.onsapenter = function (oEvent) {
+				// Fire through the MultiInput Popup's input value and save it
+				that.onChange(oEvent, null, this.getValue());
+
 				that._validateCurrentText();
 				that._setValueInvisible();
 			};
