71c148a5cdfd827461cd64a824502e5cb121c638
Pavel Kornev <pavel.kornev@sap.com>
2018-10-04 11:44:33 +0200

    [INTERNAL] sap.ui.rta: move custom CSS file out of themes folder
    CR-Id: 002075125800004234542018
    Change-Id: Ic2cca906ae5a31071cc83c2d9629fa7177b5a35d
    BCP: 1880577401

--- src/sap.ui.rta/src/sap/ui/rta/util/StylesLoader.js
+++ src/sap.ui.rta/src/sap/ui/rta/util/StylesLoader.js
@@ -16,7 +16,7 @@ sap.ui.define([
 		 * @return {jqXHR} - returns jqXHR object which can be thenable
 		 */
 		module.loadStyles = function (sFileName) {
-			return jQuery.get(jQuery.sap.getModulePath('sap.ui.rta.themes.base.' + sFileName) + '.css')
+			return jQuery.get(jQuery.sap.getModulePath('sap.ui.rta.assets.' + sFileName) + '.css')
 				.then(function (sData) {
 					// TODO: check if it's possible to use UI5 standard mechanism for styles adjustments
 					if (sap.ui.getCore().getConfiguration().getRTL()) {
