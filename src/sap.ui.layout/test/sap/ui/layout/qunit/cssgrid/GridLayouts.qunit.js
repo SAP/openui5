e0e81c34b561fd4a93f1c94c426a4355f27ace96
Svetoslav Spasov <svetoslav.spasov@sap.com>
2018-10-09 17:17:05 +0300

    [INTERNAL] sap.ui.layout.cssgrid.CSSGrid: QUnit tests for layouting part
    Change-Id: I6b8913f7ba62dd7d406be16e3c9c5c4efd915e18

---
+++ src/sap.ui.layout/test/sap/ui/layout/qunit/cssgrid/GridLayouts.qunit.js
@@ -0,0 +1,368 @@
+/*global QUnit, sinon */
+
+sap.ui.define([
+	"jquery.sap.global",
+	"sap/ui/layout/cssgrid/GridLayoutBase",
+	"sap/ui/layout/cssgrid/GridBasicLayout",
+	"sap/ui/layout/cssgrid/GridResponsiveLayout",
+	"sap/ui/layout/cssgrid/GridSettings"
+], function (
+	jQuery,
+	GridLayoutBase,
+	GridBasicLayout,
+	GridResponsiveLayout,
+	GridSettings
+) {
+	"use strict";
+
+	function getGridSettings() {
+		return {
+			gridTemplateColumns: "repeat(auto-fit, 10rem)",
+			gridTemplateRows: "3rem",
+			gridAutoRows: "1fr",
+			gridGap: "1rem 1rem",
+			gridAutoFlow: "ColumnDense"
+		};
+	}
+
+	function getExpectedStyles() {
+		return {
+			"display": "grid",
+			"grid-auto-columns": "",
+			"grid-auto-flow": "column dense",
+			"grid-auto-rows": "1fr",
+			"grid-column-gap": "",
+			"grid-gap": "1rem 1rem",
+			"grid-row-gap": "",
+			"grid-template-columns": "repeat(auto-fit, 10rem)",
+			"grid-template-rows": "3rem"
+		};
+	}
+
+	QUnit.module("GridLayoutBase - Basic", {
+		beforeEach: function () {
+			this.oGridLayoutBase = new GridLayoutBase();
+			this.oRenderManagerMock = {
+				addStyle: function (sKey, sValue) {
+					this.styles[sKey] = sValue;
+				},
+				styles: {}
+			};
+		},
+		afterEach: function () {
+			this.oGridLayoutBase.destroy();
+			this.oGridLayoutBase = null;
+			this.oRenderManagerMock = null;
+		}
+	});
+
+	QUnit.test("isResponsive", function (assert) {
+		assert.notOk(this.oGridLayoutBase.isResponsive(), "Should NOT be responsive");
+	});
+
+	QUnit.test("getActiveGridSettings", function (assert) {
+		assert.throws(this.oGridLayoutBase.getActiveGridSettings, "Should throw an error when getActiveGridSettings function is not implemented");
+	});
+
+	QUnit.test("renderSingleGridLayout - with non-responsive GridLayout", function (assert) {
+
+		// Arrange
+		var oGridLayout = new GridBasicLayout(getGridSettings());
+		var oExpectedStyles = getExpectedStyles();
+
+		// Act
+		GridLayoutBase.renderSingleGridLayout(this.oRenderManagerMock, oGridLayout);
+
+		// Assert
+		for (var sProp in oExpectedStyles) {
+			assert.equal(this.oRenderManagerMock.styles[sProp], oExpectedStyles[sProp], "Should have set value '" + oExpectedStyles[sProp] + "' for property " + sProp);
+		}
+	});
+
+	QUnit.test("renderSingleGridLayout - with responsive GridLayout", function (assert) {
+
+		// Arrange
+		var oGridLayout = new GridResponsiveLayout({
+			layout: getGridSettings(),
+			layoutS: getGridSettings(),
+			layoutM: getGridSettings(),
+			layoutL: getGridSettings(),
+			layoutXL: getGridSettings()
+		});
+
+		// Act
+		GridLayoutBase.renderSingleGridLayout(this.oRenderManagerMock, oGridLayout);
+
+		// Assert
+		assert.equal(Object.keys(this.oRenderManagerMock.styles).length, 1, "Should have only one property set when the GridLayout is responsive");
+		assert.equal(this.oRenderManagerMock.styles["display"], "grid", "Should have 'display' property set to 'grid'");
+	});
+
+	QUnit.module("GridLayoutBase - Apply layout", {
+		beforeEach: function () {
+			var that = this;
+			this.oGridLayoutBase = new GridLayoutBase();
+			this.oRenderManagerMock = {
+				addStyle: function (sKey, sValue) {
+					this.styles[sKey] = sValue;
+				},
+				styles: {}
+			};
+			this.oHTMLElementMock = {
+				style: {
+					_styles: {
+						"display": "grid",
+						"grid-auto-columns": "",
+						"grid-auto-flow": "column dense",
+						"grid-auto-rows": "1fr",
+						"grid-column-gap": "",
+						"grid-gap": "1rem 1rem",
+						"grid-row-gap": "",
+						"grid-template-columns": "repeat(auto-fit, 10rem)",
+						"grid-template-rows": "3rem"
+					},
+					getProperty: function (sKey) {
+						return this._styles[sKey];
+					},
+					setProperty: function (sKey, sValue) {
+						this._styles[sKey] = sValue;
+					},
+					removeProperty: function (sKey) {
+						delete this._styles[sKey];
+					}
+				},
+				getDomRef: function () {
+					return this;
+				}
+			};
+			this.oControlMock = {
+				getDomRef: function () {
+					return that.oHTMLElementMock;
+				}
+			};
+			sinon.spy(GridLayoutBase.prototype, "_applySingleGridLayout");
+			sinon.spy(GridLayoutBase.prototype, "_setGridLayout");
+			sinon.spy(GridLayoutBase.prototype, "_removeGridLayout");
+		},
+		afterEach: function () {
+			this.oGridLayoutBase.destroy();
+			this.oGridLayoutBase = null;
+			this.oRenderManagerMock = null;
+			this.oControlMock = null;
+			this.oHTMLElementMock = null;
+			GridLayoutBase.prototype._applySingleGridLayout.restore();
+			GridLayoutBase.prototype._setGridLayout.restore();
+			GridLayoutBase.prototype._removeGridLayout.restore();
+		}
+	});
+
+	QUnit.test("applyGridLayout - no items", function (assert) {
+
+		// Act
+		this.oGridLayoutBase.applyGridLayout();
+
+		// Assert
+		assert.ok(GridLayoutBase.prototype._applySingleGridLayout.notCalled, "Should not call _applySingleGridLayout when no items are provided");
+	});
+
+	QUnit.test("applyGridLayout - with items and no GridSettings", function (assert) {
+
+		// Arrange
+		this.oGridLayoutBase.getActiveGridSettings = function () {
+			return null;
+		};
+
+		// Act
+		this.oGridLayoutBase.applyGridLayout([this.oControlMock]);
+
+		// Assert
+		assert.ok(GridLayoutBase.prototype._applySingleGridLayout.calledOnce, "Should call _applySingleGridLayout once");
+		assert.ok(GridLayoutBase.prototype._setGridLayout.notCalled, "Should NOT call _setGridLayout");
+		assert.ok(GridLayoutBase.prototype._removeGridLayout.calledOnce, "Should call _removeGridLayout once");
+		assert.equal(Object.keys(this.oHTMLElementMock.style._styles).length, 1, "Should have one style");
+		assert.equal(this.oHTMLElementMock.style._styles["display"], "grid", "Should have display:grid");
+	});
+
+	QUnit.test("applyGridLayout - with items and GridSettings", function (assert) {
+
+		// Arrange
+		this.oHTMLElementMock.style._styles = {};
+		var oExpectedStyles = getExpectedStyles();
+		this.oGridLayoutBase.getActiveGridSettings = function () {
+			return new GridSettings(getGridSettings());
+		};
+
+		// Act
+		this.oGridLayoutBase.applyGridLayout([this.oControlMock]);
+
+		// Assert
+		assert.ok(GridLayoutBase.prototype._applySingleGridLayout.calledOnce, "Should call _applySingleGridLayout once");
+		assert.ok(GridLayoutBase.prototype._setGridLayout.calledOnce, "Should call _setGridLayout once");
+		assert.ok(GridLayoutBase.prototype._removeGridLayout.notCalled, "Should NOT call _removeGridLayout");
+		assert.equal(Object.keys(this.oHTMLElementMock.style._styles).length, Object.keys(oExpectedStyles).length, "Should have expected number of styles");
+
+		for (var sProp in oExpectedStyles) {
+			assert.equal(this.oHTMLElementMock.style._styles[sProp], oExpectedStyles[sProp], "Should have set value '" + oExpectedStyles[sProp] + "' for property " + sProp);
+		}
+	});
+
+	QUnit.module("GridResponsiveLayout", {
+		beforeEach: function () {
+			this.fnLayoutChangeHandler = sinon.spy();
+			this.oGridLayout = new GridResponsiveLayout({
+				layoutChange: this.fnLayoutChangeHandler
+			});
+			sinon.spy(this.oGridLayout, "applySizeClass");
+		},
+		afterEach: function () {
+			this.oGridLayout.applySizeClass.restore();
+			this.oGridLayout.destroy();
+			this.oGridLayout = null;
+			this.fnLayoutChangeHandler = null;
+		}
+	});
+
+	QUnit.test("Init", function (assert) {
+		assert.ok(this.oGridLayout.isResponsive(), "Should be responsive");
+		assert.equal(this.oGridLayout._sActiveLayout, "layout", "Should have active layout with value 'layout'");
+		assert.notOk(this.oGridLayout.getActiveGridSettings(), "Should have no Grid settings");
+	});
+
+	QUnit.test("_getLayoutToApply", function (assert) {
+
+		// Arrange
+		this.oGridLayout.setLayout(new GridSettings(getGridSettings()));
+		this.oGridLayout.setLayoutL(new GridSettings(getGridSettings()));
+
+		// Assert
+		assert.equal(this.oGridLayout._getLayoutToApply("layout"), "layout", "Should return 'layout'");
+		assert.equal(this.oGridLayout._getLayoutToApply("layoutS"), "layout", "Should fall back to 'layout' when layoutS is empty");
+		assert.equal(this.oGridLayout._getLayoutToApply("layoutM"), "layout", "Should fall back to 'layout' when layoutM is empty");
+		assert.equal(this.oGridLayout._getLayoutToApply("layoutL"), "layoutL", "Should return 'layoutL' as it is set");
+		assert.equal(this.oGridLayout._getLayoutToApply("layoutXL"), "layout", "Should fall back to 'layout' when layoutXL is empty");
+	});
+
+	QUnit.test("onGridAfterRendering - active layout not changed", function (assert) {
+
+		// Arrange
+		this.oGridLayout.setLayout(new GridSettings(getGridSettings()));
+		var $GridMock = jQuery("<div></div>", { width: 500 });
+		var oGridMock = {
+			$: function () {
+				return $GridMock;
+			}
+		};
+
+		// Act
+		this.oGridLayout.onGridAfterRendering(oGridMock);
+
+		// Assert
+		assert.ok(this.fnLayoutChangeHandler.notCalled, "Should not trigger layoutChange onAfterRendering of the grid");
+		assert.equal(this.oGridLayout._sActiveLayout, "layout", "Should not change the active layout");
+		assert.ok(this.oGridLayout.applySizeClass.calledOnce, "Should add size class when layout is applied");
+		assert.equal(this.oGridLayout.applySizeClass.getCall(0).args[1], "sapUiLayoutCSSGridS", "Should add class sapUiLayoutCSSGridS for 'S' size");
+	});
+
+	QUnit.test("onGridAfterRendering - layout changed from 'layout' to 'layoutS'", function (assert) {
+
+		// Arrange
+		this.oGridLayout.setLayout(new GridSettings(getGridSettings()));
+		this.oGridLayout.setLayoutS(new GridSettings(getGridSettings()));
+
+		var $GridMock = jQuery("<div></div>", { width: 500 });
+		var oGridMock = {
+			$: function () {
+				return $GridMock;
+			}
+		};
+
+		// Act
+		this.oGridLayout.onGridAfterRendering(oGridMock);
+
+		// Assert
+		assert.ok(this.fnLayoutChangeHandler.notCalled, "Should not trigger layoutChange onAfterRendering of the grid");
+		assert.equal(this.oGridLayout._sActiveLayout, "layoutS", "Should change the active layout to S");
+		assert.ok(this.oGridLayout.applySizeClass.calledOnce, "Should add size class when layout is applied");
+		assert.equal(this.oGridLayout.applySizeClass.getCall(0).args[1], "sapUiLayoutCSSGridS", "Should add class sapUiLayoutCSSGridS for 'S' size");
+	});
+
+	QUnit.test("onGridAfterRendering - layout changed from 'layoutS' to 'layout'", function (assert) {
+
+		// Arrange
+		this.oGridLayout.setLayout(new GridSettings(getGridSettings()));
+		this.oGridLayout.setLayoutS(new GridSettings(getGridSettings()));
+
+		var $GridMock = jQuery("<div></div>", { width: 500 });
+		var oGridMock = {
+			$: function () {
+				return $GridMock;
+			}
+		};
+
+		// Act
+		this.oGridLayout.onGridAfterRendering(oGridMock);
+		$GridMock.width(1000);
+		this.oGridLayout.onGridAfterRendering(oGridMock);
+
+		// Assert
+		assert.ok(this.fnLayoutChangeHandler.notCalled, "Should not trigger layoutChange onAfterRendering of the grid");
+		assert.equal(this.oGridLayout._sActiveLayout, "layout", "Should change to 'layout'");
+		assert.ok(this.oGridLayout.applySizeClass.calledTwice, "Should add size class when layout is applied");
+		assert.equal(this.oGridLayout.applySizeClass.getCall(0).args[1], "sapUiLayoutCSSGridS", "Should add class sapUiLayoutCSSGridS for 'S' size");
+		assert.equal(this.oGridLayout.applySizeClass.getCall(1).args[1], "sapUiLayoutCSSGridM", "Should add class sapUiLayoutCSSGridM for 'M' size");
+	});
+
+	QUnit.test("onGridResize - Resize Event size 0", function (assert) {
+
+		// Arrange
+		var oResizeEventMock = {
+			size: {
+				width: 0
+			}
+		};
+		sinon.spy(GridResponsiveLayout.prototype, "setActiveLayout");
+
+		// Act
+		this.oGridLayout.onGridResize(oResizeEventMock);
+
+		// Assert
+		assert.ok(GridResponsiveLayout.prototype.setActiveLayout.notCalled, "Should NOT call setActiveLayout when Resize Event size is 0");
+
+		// Cleanup
+		GridResponsiveLayout.prototype.setActiveLayout.restore();
+	});
+
+	QUnit.test("onGridResize - Resize Event", function (assert) {
+
+		// Arrange
+		var $GridMock = jQuery("<div></div>", { width: 1000 });
+		var oGridMock = {
+			$: function () {
+				return $GridMock;
+			}
+		};
+		var oResizeEventMock = {
+			size: {
+				width: 1000
+			},
+			control: oGridMock
+		};
+
+		this.oGridLayout.setLayoutM(new GridSettings(getGridSettings()));
+		sinon.spy(GridResponsiveLayout.prototype, "setActiveLayout");
+
+		// Act
+		this.oGridLayout.onGridResize(oResizeEventMock);
+
+		// Assert
+		assert.ok(GridResponsiveLayout.prototype.setActiveLayout.calledOnce, "Should call setActiveLayout when the size of the Grid have changed");
+		assert.ok(this.fnLayoutChangeHandler.calledOnce, "Should trigger layoutChange onResize of the grid");
+		assert.equal(this.oGridLayout._sActiveLayout, "layoutM", "Should change to 'layoutM'");
+		assert.ok(this.oGridLayout.applySizeClass.calledOnce, "Should add size class when layout is applied");
+		assert.equal(this.oGridLayout.applySizeClass.getCall(0).args[1], "sapUiLayoutCSSGridM", "Should add class sapUiLayoutCSSGridM for 'M' size");
+
+		// Cleanup
+		GridResponsiveLayout.prototype.setActiveLayout.restore();
+	});
+
+});
