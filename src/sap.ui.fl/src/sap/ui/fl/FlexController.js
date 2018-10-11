493e05e71ff77781ecc8bbf7d36c76c8fff3df32
Aditya Bhatia <aditya.bhatia@sap.com>
2018-10-11 13:24:57 +0200

    [FIX] sap.ui.fl.FlexController: On control re-creation, initial dependencies from change persistence map are not copied for failing changes
    Change-Id: Id909ef70518aef8984e6cb027666e3ffec9abfac
    CR-Id: 002075125800004423622018
    BCP: 002075129400005198692018

--- src/sap.ui.fl/src/sap/ui/fl/FlexController.js
+++ src/sap.ui.fl/src/sap/ui/fl/FlexController.js
@@ -604,6 +604,7 @@ sap.ui.define([
 					});
 				}
 				delete oChange.PROCESSING;
+				oChange.PROCESSED = true;
 				return true;
 			}.bind(this))
 
@@ -641,7 +642,7 @@ sap.ui.define([
 					});
 				}
 				delete oChange.PROCESSING;
-				oChange.APPLIED = true;
+				oChange.PROCESSED = true;
 				return false;
 			}.bind(this));
 		}
@@ -932,7 +933,7 @@ sap.ui.define([
 
 	FlexController.prototype._checkIfDependencyIsStillValid = function(oAppComponent, oModifier, sChangeId) {
 		var oChange = Utils.getChangeFromChangesMap(this._oChangePersistence._mChanges.mChanges, sChangeId);
-		if (!oChange.APPLIED) {
+		if (!oChange.PROCESSED) {
 			return true;
 		}
 
@@ -968,11 +969,11 @@ sap.ui.define([
 
 			// if a change was already processed and is not applied anymore,
 			// then the control was destroyed and recreated. In this case we need to recreate/copy the dependencies.
-			if (oChange.APPLIED && !this._isChangeCurrentlyApplied(oControl, oChange, mPropertyBag.modifier)) {
+			if (oChange.PROCESSED && !this._isChangeCurrentlyApplied(oControl, oChange, mPropertyBag.modifier)) {
 				mChangesMap = this._oChangePersistence.copyDependenciesFromInitialChangesMap(oChange, this._checkIfDependencyIsStillValid.bind(this, oAppComponent, mPropertyBag.modifier));
 				mDependencies = mChangesMap.mDependencies;
 				mDependentChangesOnMe = mChangesMap.mDependentChangesOnMe;
-				delete oChange.APPLIED;
+				delete oChange.PROCESSED;
 			}
 
 			if (!mDependencies[oChange.getId()]) {
