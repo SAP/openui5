b4ddd58758730e3eabe0c2a39c95e8ea5bac0605
Tobias Sorn <tobias.sorn@sap.com>
2018-10-15 10:50:49 +0200

    [INTERNAL] Fixed typos
    Fixed typos.
    
    Change-Id: Ieb34e8712239b5a63b452f42104464cf2d5d90d1

--- src/sap.ui.core/src/sap/ui/core/util/MockServer.js
+++ src/sap.ui.core/src/sap/ui/core/util/MockServer.js
@@ -1920,7 +1920,7 @@ sap.ui
 			};
 
 			/**
-			 * Simulates an existing OData service by sepcifiying the metadata URL and the base URL for the mockdata. The server
+			 * Simulates an existing OData service by sepcifying the metadata URL and the base URL for the mockdata. The server
 			 * configures the request handlers depending on the service metadata. The mockdata needs to be stored individually for
 			 * each entity type in a separate JSON file. The name of the JSON file needs to match the name of the entity type. If
 			 * no base url for the mockdata is specified then the mockdata are generated from the metadata
