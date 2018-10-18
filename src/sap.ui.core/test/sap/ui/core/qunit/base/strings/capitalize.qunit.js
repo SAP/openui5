4dce187d81d81e5fecc3c1c8b0e2d59e98a5388b
Tobias Sorn <tobias.sorn@sap.com>
2018-10-18 16:21:46 +0200

    [INTERNAL] Fixed tests
    hyphenate test sometimes fails because hyphenate.qunit test is loaded
    randomly with all the other modularization tests which might do
    assertions within their module beforeEach. Since hyphenate test expects
    2 assertions to run it fails.
    
    Change-Id: Iea07fa56f9531cef88a6ee5f6c1bbaa0a717291f
    (cherry picked from commit 838092b0343e8eb8d3c2aeccf8f19b99a30bbd10)
    CR-Id: 002075125900004266872021
    BCP: n/a

--- src/sap.ui.core/test/sap/ui/core/qunit/base/strings/capitalize.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/base/strings/capitalize.qunit.js
@@ -2,6 +2,8 @@
 sap.ui.define(["sap/base/strings/capitalize"], function(capitalize) {
 	"use strict";
 
+	QUnit.module("Capitalize");
+
 	QUnit.test("capitalize", function (assert) {
 		assert.strictEqual(capitalize("gggT"), "GggT");
 		assert.strictEqual(capitalize("gs4T"), "Gs4T");
