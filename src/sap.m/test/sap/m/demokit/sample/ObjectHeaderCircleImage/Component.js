7a8ee6fb942f2ba417abb37bfeab79ffbde0483c
i310817 <gergana.marcheva@sap.com>
2018-10-23 15:17:32 +0300

    [FEATURE] sap.m.ObjectHeader: Rounded images are now supported
    JIRA: BGSOFUIBALKAN-4309
    
    Change-Id: I4973498aa710e440394fe192c4dca8aab10069d1

---
+++ src/sap.m/test/sap/m/demokit/sample/ObjectHeaderCircleImage/Component.js
@@ -0,0 +1,31 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.m.sample.ObjectHeaderCircleImage.Component", {
+
+		metadata : {
+			rootView : {
+				"viewName": "sap.m.sample.ObjectHeaderCircleImage.V",
+				"type": "XML",
+				"async": true
+			},
+			dependencies : {
+				libs : [
+					"sap.m",
+					"sap.ui.layout"
+				]
+			},
+			config : {
+				sample : {
+					files : [
+						"V.view.xml"
+					]
+				}
+			}
+		}
+	});
+
+	return Component;
+
+});
