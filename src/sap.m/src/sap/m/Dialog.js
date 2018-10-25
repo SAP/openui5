27e4dba2e61486adb9c68a0b079db7d90be70d16
Alexandar Mitsev <alexandar.mitsev@sap.com>
2018-10-25 18:44:29 +0300

    [FIX] sap.m.Dialog: Adjust width when dialog contains long items
    Do not truncate text of list items inside dialog with vertical scroll
    when the dialog content width is auto for IE, Edge and Firefox.
    
    Change-Id: I476accb5a37127ecfb3c7b90660984f8b69ee80e
    BCP: 1880565702
    CR-Id: 002075125800004942922018

--- src/sap.m/src/sap/m/Dialog.js
+++ src/sap.m/src/sap/m/Dialog.js
@@ -530,6 +530,9 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Associative
 			this.fireBeforeOpen();
 			oPopup.attachOpened(this._handleOpened, this);
 
+			// reset scroll fix check
+			this._iLastWidthAndHeightWithScroll = null;
+
 			// Open popup
 			oPopup.setContent(this);
 
@@ -876,8 +879,8 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Associative
 				$dialogContent.scrollTop(dialogContentScrollTop);
 			}
 
-			// IE and EDGE have specific container behavior (e.g. div with 500px width is about 15px smaller when it has vertical scrollbar
-			if ((oBrowser.internet_explorer || oBrowser.edge)) {
+			// IE, EDGE and Firefox (when width is auto) have specific container behavior (e.g. div with 500px width is about 17px smaller when it has vertical scrollbar
+			if ((oBrowser.internet_explorer || oBrowser.edge || oBrowser.firefox)) {
 
 				var iVerticalScrollBarWidth = Math.ceil($dialogContent.outerWidth() - dialogClientWidth),
 					iCurrentWidthAndHeight = $dialogContent.width() + "x" + $dialogContent.height();
@@ -888,9 +891,11 @@ sap.ui.define(['jquery.sap.global', './Bar', './InstanceManager', './Associative
 						!this.getStretch() && 							// - when the dialog is not stretched
 						$dialogContent.width() < maxDialogWidth) {		// - if the dialog can't grow anymore
 
+						$dialog.addClass("sapMDialogVerticalScrollIncluded");
 						$dialogContent.css({"padding-right" : iVerticalScrollBarWidth});
 						this._iLastWidthAndHeightWithScroll = iCurrentWidthAndHeight;
 					} else {
+						$dialog.removeClass("sapMDialogVerticalScrollIncluded");
 						$dialogContent.css({"padding-right" : ""});
 						this._iLastWidthAndHeightWithScroll = null;
 					}
