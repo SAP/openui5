c7aab3f093b23938284662b38eb9482943eb4e22
Gergana Kremenska <gergana.kremenska@sap.com>
2018-10-29 13:05:52 +0200

    [FEATURE] sap.f.ListCard: initial commit
    Change-Id: Ic6dd32baf5e137184ed8bdb27960a334e9b95e6d

--- src/sap.f/src/sap/f/CardRenderer.js
+++ src/sap.f/src/sap/f/CardRenderer.js
@@ -4,29 +4,29 @@
 
 // Provides default renderer for control sap.f.Card
 sap.ui.define(["sap/f/library", "sap/base/security/encodeXML", "sap/ui/core/IconPool"],
-    function (library, encodeXML, IconPool) {
+	function (library, encodeXML, IconPool) {
 		"use strict";
 
-        /**
-         * <code>Card</code> renderer.
-         * @author SAP SE
-         * @namespace
-         */
-        var CardRenderer = {};
+		/**
+		 * <code>Card</code> renderer.
+		 * @author SAP SE
+		 * @namespace
+		 */
+		var CardRenderer = {};
 
-        /**
-         * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
-         *
-         * @param {sap.ui.core.RenderManager} oRm the RenderManager that can be used for writing to the Render-Output-Buffer
-         * @param {sap.ui.core.Control} oCard an object representation of the control that should be rendered
-         */
-        CardRenderer.render = function (oRm, oCard) {
+		/**
+		 * Renders the HTML for the given control, using the provided {@link sap.ui.core.RenderManager}.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm the RenderManager that can be used for writing to the Render-Output-Buffer
+		 * @param {sap.ui.core.Control} oCard an object representation of the control that should be rendered
+		 */
+		CardRenderer.render = function (oRm, oCard) {
 
 			// TODO: Don't expect fully loaded Manifest in here.
 			// We should rather render a base card with certain size and then load manifest information and update the header/content.
 			if (!oCard._oCardManifest) {
 				oRm.write("<section");
-				oRm.writeElementData(oCard);
+				oRm.writeControlData(oCard);
 				oRm.writeAttribute("tabindex", "0");
 				oRm.addClass("sapFCard");
 
@@ -56,76 +56,55 @@ sap.ui.define(["sap/f/library", "sap/base/security/encodeXML", "sap/ui/core/Icon
             }
             oRm.writeClasses();
 
-            //styles
-            var sColor = oCard._oCardManifest.get("sap.card/color"),
-                sBackgroundColor = oCard._oCardManifest.get("sap.card/backgroundColor"),
-                sBackgroundImage = oCard._oCardManifest.get("sap.card/backgroundImage"),
-				sBackgroundImageSize = oCard._oCardManifest.get("sap.card/backgroundImageSize");
-
+			//styles
 			oRm.addStyle("width", oCard._oCardManifest.get("sap.card/width"));
 			oRm.addStyle("height", oCard._oCardManifest.get("sap.card/height"));
 
-            if (sColor) {
-                oRm.addStyle("color", sColor);
-                //TODO: Use this only in focus case
-                oRm.addStyle("border-color", sColor + " !important");
-            }
-            if (sBackgroundColor) {
-                oRm.addStyle("background-color", sBackgroundColor);
-            }
-            if (sBackgroundImage) {
-                oRm.addStyle("background-image", "url('" + sBackgroundImage + "')");
-                if (sBackgroundImageSize) {
-                    oRm.addStyle("background-size", sBackgroundImageSize);
-                }
-            }
+			oRm.writeStyles();
+			oRm.write(">");
 
-            oRm.writeStyles();
-            oRm.write(">");
-
-            //header
+			//header
             CardRenderer.renderHeaderSection(oRm, oCard);
 
-            //content
+			//content
 			oRm.write("<div class=\"sapFCardSeparator\" />");
 			CardRenderer.renderContentSection(oRm, oCard);
 
-            //end
+			//end
 			oRm.write("</section>");
-        };
+		};
 
-        /*
-         * renders the header of the card
-         */
+		/**
+		 * Render header section.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+		 */
         CardRenderer.renderHeaderSection = function (oRm, oCard) {
-            oRm.write("<header");
-            oRm.addClass("sapFCardHeader");
-            oRm.writeClasses();
-            oRm.writeStyles();
-            oRm.write(">");
-            if (oCard._oCardManifest.get("sap.card/icon")) {
-                CardRenderer.renderHeaderIcon(oRm, oCard);
-            }
-
-			oRm.write("<div>");
-			var sTitle = oCard._oCardManifest.get("sap.card/title");
-			oRm.write("<h1");
-			oRm.addClass("sapFCardTitle");
+			oRm.write("<header");
+			oRm.addClass("sapFCardHeader");
 			oRm.writeClasses();
+			oRm.writeStyles();
 			oRm.write(">");
-			oRm.writeEscaped(sTitle);
-			oRm.write("</h1>");
-			if (sTitle) {
-				var sSubtitle = oCard._oCardManifest.get("sap.card/subTitle");
-				oRm.write("<h2");
-				oRm.addClass("sapFCardSubtitle");
-				oRm.writeClasses();
-				oRm.write(">");
-				oRm.writeEscaped(sSubtitle);
-				oRm.write("</h2>");
+            if (oCard._oCardManifest.get("sap.card/icon")) {
+				this.renderHeaderIcon(oRm, oCard);
 			}
-			oRm.write("</div>");
+			var sTitle = oCard._oCardManifest.get("sap.card/title"),
+				sSubTitle = oCard._oCardManifest.get("sap.card/subTitle");
+            if (sTitle) {
+				oRm.write("<div");
+				oRm.addClass("sapFCardHeaderText");
+				oRm.writeClasses();
+				oRm.writeStyles();
+				oRm.write(">");
+				oRm.renderControl(oCard._oTitle);
+				if (sSubTitle) {
+					oRm.renderControl(oCard._oSubTitle);
+				}
+				oRm.write("</div>");
+			}
 
+			//TODO rename status to count
 			var sStatus = oCard._oCardManifest.get("sap.card/status");
             if (sStatus) {
 				oRm.write("<span");
@@ -135,59 +114,65 @@ sap.ui.define(["sap/f/library", "sap/base/security/encodeXML", "sap/ui/core/Icon
 				oRm.writeEscaped(sStatus);
 				oRm.write("</span>");
             }
-            oRm.write("</header>");
-        };
+			oRm.write("</header>");
+		};
+		/**
+		 * Render icon section.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+		 */
+		CardRenderer.renderHeaderIcon = function (oRm, oCard) {
+			oRm.renderControl(oCard._oAvatar);
+		};
 
-        /*
-         * renders the icon of the card
-         */
-        CardRenderer.renderHeaderIcon = function (oRm, oCard) {
-            var sIcon = oCard._oCardManifest.get("sap.card/icon"),
-                vIconInfo = IconPool.getIconInfo(oCard._oCardManifest.get("sap.card/icon"), undefined, "mixed"),
-                bIconInfo = false,
-                sColor = oCard._oCardManifest.get("sap.card/iconColor") || oCard._oCardManifest.get("sap.card/color"),
-                sIconBackgroundColor = oCard._oCardManifest.get("sap.card/iconBackgroundColor");
-            if (vIconInfo instanceof Promise) {
-                // if the icon info is still being loaded,
-                // an invalidation is triggered after the icon info is available
-                vIconInfo.then(oCard.invalidate.bind(oCard));
-            } else if (vIconInfo) {
-                // render icon info in renderer
-                bIconInfo = true;
-            }
-            if (sColor) {
-                oRm.addStyle("color", sColor);
+		/**
+		 * Render content section.
+		 * Will be overwritten by subclasses.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+		 */
+		CardRenderer.renderContentSection = function (oRm, oCard) {
+			oRm.write("<section");
+			oRm.writeAttribute("id", oCard.getId() + "-content");
+			var sColor = oCard._oCardManifest.get("sap.card/color"),
+				sBackgroundColor = oCard._oCardManifest.get("sap.card/backgroundColor"),
+				sBackgroundImage = oCard._oCardManifest.get("sap.card/backgroundImage"),
+				sBackgroundImageSize = oCard._oCardManifest.get("sap.card/backgroundImageSize");
 
-            }
-            if (sIconBackgroundColor) {
-                oRm.addStyle("background-color", sIconBackgroundColor);
-            }
-            oRm.write("<span");
-            oRm.addClass("sapFCardIcon");
-            if (!sIcon.startsWith("sap-icon://")) {
-                oRm.addStyle("background-image", "url('" + encodeXML(sIcon) + "')");
-            } else if (bIconInfo) {
-                oRm.writeAttributeEscaped("data-sap-ui-icon-content", vIconInfo.content);
-                oRm.addStyle("font-family", "'" + encodeXML(vIconInfo.fontFamily) + "'");
-                oRm.addClass("sapUiIcon");
-            }
-            oRm.writeClasses();
-            oRm.writeStyles();
-            oRm.write(">");
-            oRm.write("</span>");
-        };
+			if (sColor) {
+				oRm.addStyle("color", sColor);
+				//TODO: Use this only in focus case
+				oRm.addStyle("border-color", sColor + " !important");
+			}
+			if (sBackgroundColor) {
+				oRm.addStyle("background-color", sBackgroundColor);
+			}
+			if (sBackgroundImage) {
+				oRm.addStyle("background-image", "url('" + sBackgroundImage + "')");
+				if (sBackgroundImageSize) {
+					oRm.addStyle("background-size", sBackgroundImageSize);
+				}
+			}
+			oRm.addClass("sapFCardContent");
+			oRm.writeClasses();
+			oRm.write(">");
+			var oContent = oCard.getAggregation("_content");
+			if (oContent) {
+				oRm.renderControl(oContent);
+			}
+			oRm.write("</section>");
+		};
 
-        /*
-         * renders the content of the card
-         */
-        CardRenderer.renderContentSection = function (oRm, oCard) {
-            oRm.write("<section");
-            oRm.addClass("sapFCardContent");
-            oRm.writeClasses();
-            oRm.write(">");
-            oRm.renderControl(oCard.getAggregation("_content"));
-            oRm.write("</section>");
-        };
+		/**
+		 * Add a control type class class to input container.
+		 * Will be overwritten by subclasses.
+		 *
+		 * @param {sap.ui.core.RenderManager} oRm The RenderManager that can be used for writing to the render output buffer.
+		 * @param {sap.ui.core.Control} oControl An object representation of the control that should be rendered.
+		 */
+		CardRenderer.addCardTypeClass = function(oRm, oControl) {};
 
-        return CardRenderer;
-    }, /* bExport= */ true);
+		return CardRenderer;
+	}, /* bExport= */ true);
