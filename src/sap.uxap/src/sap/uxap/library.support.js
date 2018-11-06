6c9ebd8e8d271701f6071f6dbece5d7150186a6b
Naydenov <nayden.naydenov@sap.com>
2018-11-06 13:15:14 +0200

    [INTERNAL] sap.uxap.ObjectPageLayout: Added support rule
    Added support rule to avoid duplication of same elements
    in HeaderTitle and HeaderContent
    
    Change-Id: Ie3130b398d2db3ff9ee0e8cb76e5b1cc60bb4a1e
    JIRA: BGSOFUIPIRIN-3467

--- src/sap.uxap/src/sap/uxap/library.support.js
+++ src/sap.uxap/src/sap/uxap/library.support.js
@@ -4,16 +4,14 @@
 /**
  * Adds support rules of the sap.uxap library to the support infrastructure.
  */
-sap.ui.define([	"./rules/ObjectPageLayout.support"],
+sap.ui.define(["./rules/ObjectPageLayout.support"],
 	function(ObjectPageLayoutSupport) {
 	"use strict";
 
 	return {
 		name: "sap.uxap",
 		niceName: "ObjectPage library",
-		ruleset: [
-			ObjectPageLayoutSupport
-		]
+		ruleset: [ObjectPageLayoutSupport]
 	};
 
 }, true);
