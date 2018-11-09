e4dd5dc7375f40000e2d33aa22981a28ca679cd6
Matthias Osswald <mat.osswald@sap.com>
2018-11-09 14:56:32 +0100

    [INTERNAL] Core#setThemeRoot: Add forceUpdate parameter
    Introduces a new parameter for setThemeRoot to be able to change a theme
    root for some libraries of the currently applied theme and trigging an
    automatic update of the stylesheets without needing to switch/apply
    themes again.
    Note that the ThemeChanged event will not be influenced by this and that
    parameters consumed via sap.ui.core.Parameters.get() are not forcefully
    reloaded from the new location.
    
    CR-Id: 002075125900004585382018
    Change-Id: I7b4cb4b7c7da590584facea728de6ddfb32d99b6
    BCP: 1880689890
    (cherry picked from commit 244f193c66230c71333600f2566c287c511a9501)

--- src/sap.ui.core/src/sap/ui/core/Core.js
+++ src/sap.ui.core/src/sap/ui/core/Core.js
@@ -994,11 +994,12 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device', 'sap/ui/Global',
 	 * @param {string} sThemeName Name of the theme for which to configure the location
 	 * @param {string[]} [aLibraryNames] Optional library names to which the configuration should be restricted
 	 * @param {string} sThemeBaseUrl Base URL below which the CSS file(s) will be loaded from
+	 * @param {boolean} [bForceUpdate=false] Force updating URLs of currently loaded theme
 	 * @return {sap.ui.core.Core} the Core, to allow method chaining
 	 * @since 1.10
 	 * @public
 	 */
-	Core.prototype.setThemeRoot = function(sThemeName, aLibraryNames, sThemeBaseUrl) {
+	Core.prototype.setThemeRoot = function(sThemeName, aLibraryNames, sThemeBaseUrl, bForceUpdate) {
 		jQuery.sap.assert(typeof sThemeName === "string", "sThemeName must be a string");
 		jQuery.sap.assert((Array.isArray(aLibraryNames) && typeof sThemeBaseUrl === "string") || (typeof aLibraryNames === "string" && sThemeBaseUrl === undefined), "either the second parameter must be a string (and the third is undefined), or it must be an array and the third parameter is a string");
 
@@ -1007,7 +1008,8 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device', 'sap/ui/Global',
 		}
 
 		// normalize parameters
-		if (sThemeBaseUrl === undefined) {
+		if (typeof aLibraryNames === "string") {
+			bForceUpdate = sThemeBaseUrl;
 			sThemeBaseUrl = aLibraryNames;
 			aLibraryNames = undefined;
 		}
@@ -1023,6 +1025,11 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/Device', 'sap/ui/Global',
 		} else {
 			// registration of theme default base URL
 			this._mThemeRoots[sThemeName] = sThemeBaseUrl;
+		}
+
+		// Update theme urls when theme roots of currently loaded theme have changed
+		if (bForceUpdate && sThemeName === this.sTheme) {
+			this._updateThemeUrls(this.sTheme);
 		}
 
 		return this;
