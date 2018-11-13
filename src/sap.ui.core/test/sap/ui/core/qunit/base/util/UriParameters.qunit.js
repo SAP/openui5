c18db8f635f05fd0b2d1fea62d9f43969d8981b0
Wedel, Malte <malte.wedel@sap.com>
2018-11-13 20:43:38 +0100

    [FIX] UriParameters: Fixed parsing of query values with an equal sign
    - parameters containing an equal sign in their value were not parsed
      correctly
    - plus signs in parameter names should be transformed to blanks much
      as for parameter values
    
    Change-Id: Id7140caf74823da69ba9f5b41efead77aae71602
    CR-Id: 002075125900004557242018
    BCP: 1880708373

--- src/sap.ui.core/test/sap/ui/core/qunit/base/util/UriParameters.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/base/util/UriParameters.qunit.js
@@ -50,15 +50,52 @@ sap.ui.define(['sap/base/util/UriParameters'], function(UriParameters) {
 		var oUriParams = new UriParameters("/service?constructor=1&hasOwnProperty=2");
 		assert.deepEqual(oUriParams.get('constructor',true), ['1'], "param with Object.prototype name");
 		assert.deepEqual(oUriParams.get('hasOwnProperty',true), ['2'], "param with Object.prototype name");
+		assert.deepEqual(oUriParams.get('toString',true), [], "non-existing param with Object.prototype name");
+	});
+
+	QUnit.test("URL with param names with encoded characters", function(assert) {
+		var oUriParams = new UriParameters("?key1%3D=2");
+		assert.deepEqual(oUriParams.get('key1='), '2', "parameter with an encoded equal sign in the name");
+		var oUriParams = new UriParameters("?key1%3Dx%26=2");
+		assert.deepEqual(oUriParams.get('key1=x&'), '2', "parameter with an encoded ampersand sign in the name");
 	});
 
 	QUnit.test("URL with encoded values or spaces", function(assert) {
-		var oUriParams = new UriParameters("/service?key1=&key2&search=Rock+%26+Roll&rock%26roll=here+to+stay&weird=%26%CE%A8%E2%88%88");
+		var oUriParams = new UriParameters("?some+key=value");
+		assert.deepEqual(oUriParams.get('some key', true), ['value'], "plus-encoded parameter names should be retrievable by their decoded name");
+		assert.deepEqual(oUriParams.get('some+key', true), [], "plus-encoded parameter names should not be retrievable by their encoded name");
+		var oUriParams = new UriParameters("/service?key1=&key2&key3=abc&key3=&key3&search=Rock+%26+Roll&rock%26roll=here+to+stay&weird=%26%CE%A8%E2%88%88");
 		assert.deepEqual(oUriParams.get('key1',true), [''], "empty value with equals sign");
 		assert.deepEqual(oUriParams.get('key2',true), [''], "empty  value");
+		assert.deepEqual(oUriParams.get('key3',true), ['abc', '', ''], "mixed with empty values");
 		assert.deepEqual(oUriParams.get('rock&roll',true), ['here to stay'], "encoded key");
 		assert.deepEqual(oUriParams.get('search',true), ['Rock & Roll'], "encoded value");
 		assert.deepEqual(oUriParams.get('weird',true), ['&\u03A8\u2208'], "hex encoded value");
 		//alert('&\u03A8\u2208');
 	});
+
+	QUnit.test("URL with param values containing '='", function(assert) {
+		var oUriParams = new UriParameters("/service?a=b====c&d=====&e=====f&g=h====");
+		assert.deepEqual(oUriParams.get('a',true), ['b====c'], "value with '=' in the middle");
+		assert.deepEqual(oUriParams.get('d',true), ['===='], "value consisting of '=' only");
+		assert.deepEqual(oUriParams.get('e',true), ['====f'], "value starting with '='");
+		assert.deepEqual(oUriParams.get('g',true), ['h===='], "value ending with '='");
+	});
+
+	QUnit.skip("get() for empty and undefined parameters", function(assert) {
+		var oUriParams = new UriParameters("?x");
+		assert.deepEqual(oUriParams.get('x'), "", "parameters without a value should return the empty string as value");
+		assert.deepEqual(oUriParams.get('z'), null, "undefined parameters should return null");
+		var oUriParams = new UriParameters("?x&y=2");
+		assert.deepEqual(oUriParams.get('x'), "", "parameters without a value should return the empty string as value, even if another parameter follows");
+		assert.deepEqual(oUriParams.get('z'), null, "undefined parameters should return null");
+		var oUriParams = new UriParameters("?x=");
+		assert.deepEqual(oUriParams.get('x'), "", "parameters with an empty value should return an empty string as value");
+		assert.deepEqual(oUriParams.get('z'), null, "undefined parameters should return null");
+		var oUriParams = new UriParameters("?x=&y=2");
+		assert.deepEqual(oUriParams.get('y'), "2");
+		assert.deepEqual(oUriParams.get('x'), "", "parameters with an empty value should return an empty string as value, even if another parameter follows");
+		assert.deepEqual(oUriParams.get('y'), "2");
+	});
+
 });
