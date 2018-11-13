e9098cebff7d771849bf7941b47969f199f6ac35
Michael Graf <mi.graf@sap.com>
2018-11-13 20:03:47 +0100

    [INTERNAL][FIX] updated navigation tutorial to evo recommendations
    * changed startup to be CSP compliant
    * async mockserver
    * cleaned up XML views
    * removed our support component from manifest
    * minor changes to structure
    * Harmonized Tutorial title
    
    Change-Id: I7e88fb59d224b6956289b03555a8027002d63eb0

--- src/sap.ui.core/test/sap/ui/core/demokit/tutorial/navigation/13/webapp/localService/mockserver.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/navigation/13/webapp/localService/mockserver.js
@@ -1,39 +1,56 @@
 sap.ui.define([
-	"sap/ui/core/util/MockServer"
-], function (MockServer) {
+	"sap/ui/core/util/MockServer",
+	"sap/ui/model/json/JSONModel",
+	"sap/base/Log"
+], function (MockServer, JSONModel, Log) {
 	"use strict";
-	var _sAppModulePath = "sap/ui/demo/nav/",
-		_sJsonFilesModulePath = _sAppModulePath + "localService/mockdata";
+
+	var _sAppPath = "sap/ui/demo/nav/",
+		_sJsonFilesPath = _sAppPath + "localService/mockdata";
 
 	return {
 
 		init: function () {
-			var sManifestUrl = sap.ui.require.toUrl(_sAppModulePath + "manifest.json"),
-				sJsonFilesUrl = sap.ui.require.toUrl(_sJsonFilesModulePath),
-				//TODO: global jquery call found
-				oManifest = jQuery.sap.syncGetJSON(sManifestUrl).data,
-				oMainDataSource = oManifest["sap.app"].dataSources.employeeRemote,
-				sMetadataUrl = sap.ui.require.toUrl(_sAppModulePath + oMainDataSource.settings.localUri);
 
-			// create
-			var oMockServer = new MockServer({
-				rootUri: oMainDataSource.uri
+			return new Promise(function(fnResolve, fnReject) {
+				var sManifestUrl = sap.ui.require.toUrl(_sAppPath + "manifest.json"),
+					oManifestModel = new JSONModel(sManifestUrl);
+
+				oManifestModel.attachRequestCompleted(function () {
+					var sJsonFilesUrl = sap.ui.require.toUrl(_sJsonFilesPath),
+						oMainDataSource = oManifestModel.getProperty("/sap.app/dataSources/employeeRemote"),
+						sMetadataUrl = sap.ui.require.toUrl(_sAppPath + oMainDataSource.settings.localUri);
+
+					// create
+					var oMockServer = new MockServer({
+						rootUri: oMainDataSource.uri
+					});
+
+					// configure
+					MockServer.config({
+						autoRespond: true,
+						autoRespondAfter: 500
+					});
+
+					// simulate
+					oMockServer.simulate(sMetadataUrl, {
+						sMockdataBaseUrl: sJsonFilesUrl
+					});
+
+					// start
+					oMockServer.start();
+
+					Log.info("Running the app with mock data");
+					fnResolve();
+				});
+
+				oManifestModel.attachRequestFailed(function () {
+					var sError = "Failed to load application manifest";
+
+					Log.error(sError);
+					fnReject(new Error(sError));
+				});
 			});
-
-			// configure
-			MockServer.config({
-				autoRespond: true,
-				autoRespondAfter: 500
-			});
-
-			// simulate
-			oMockServer.simulate(sMetadataUrl, {
-				sMockdataBaseUrl : sJsonFilesUrl
-			});
-
-			// start
-			oMockServer.start();
 		}
 	};
-
 });
