2719c422fe7c0571c4b970d4b33ec4247292fa8c
Michael Graf <mi.graf@sap.com>
2018-11-16 07:11:09 +0100

    [INTERNAL][FIX] walkthrough: replace xmlfragment factory with async method
    Change-Id: I6615090f3eca452cc649e0f50336f15c25ab118e

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/18/webapp/controller/HelloPanel.controller.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/18/webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -18,16 +19,22 @@ sap.ui.define([
 
 		onOpenDialog : function () {
 			var oView = this.getView();
-			var oDialog = oView.byId("helloDialog");
-			// create dialog lazily
-			if (!oDialog) {
-				// create dialog via fragment factory
-				oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.walkthrough.view.HelloDialog", this);
-				// connect dialog to view (models, lifecycle)
-				oView.addDependent(oDialog);
-			}
 
-			oDialog.open();
+			// create dialog lazily
+			if (!this.byId("helloDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.walkthrough.view.HelloDialog",
+					controller: this
+				}).then(function(oDialog){
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			}
 		},
 
 		onCloseDialog : function () {
