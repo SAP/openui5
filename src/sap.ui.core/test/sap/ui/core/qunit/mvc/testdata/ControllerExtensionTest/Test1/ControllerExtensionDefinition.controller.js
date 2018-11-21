c5c2c98b4fc2533b00ea482314bcc57884622ac8
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2018-11-21 15:59:37 +0100

    [INTERNAL] Enhance ControllerExtension testdata
    Change-Id: If2782a0350dca4c8a40e4873f1a63bac59297494

--- src/sap.ui.core/test/sap/ui/core/qunit/mvc/testdata/ControllerExtensionTest/Test1/ControllerExtensionDefinition.controller.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/mvc/testdata/ControllerExtensionTest/Test1/ControllerExtensionDefinition.controller.js
@@ -2,7 +2,10 @@ sap.ui.define([], function () {
 	"use strict";
 
 	return {
-		"triple": function (x) {
+		"triple": function(x) {
+			var inp = this.getView().byId("inp");
+			inp.setValue(inp.getValue() * 3);
+
 			return x * 3;
 		}
 	};
