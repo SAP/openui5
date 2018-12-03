47d1c36eb8a01690b4ecdca8d533c87ad8cc2a27
Teodor Taushanov <teodor.taushanov@sap.com>
2018-12-03 17:17:45 +0200

    [FIX] sap.m.Input: showValueStateMessage now works correctly
    showValueStateMessage property is now always checked before opening
    the ValueStateMessage popup
    
    Change-Id: I48f3c9ff9c1d7b32358379741959fbb44a072015
    BCP: 012005040900000678252018
    CR-Id: 002075125900004463502018

--- src/sap.m/src/sap/m/InputBase.js
+++ src/sap.m/src/sap/m/InputBase.js
@@ -350,11 +350,8 @@ function(
 
 		this.$().toggleClass("sapMFocus", true);
 
-		if (this.shouldValueStateMessageBeOpened()) {
-
-			// open value state message popup when focus is in the input
-			this.openValueStateMessage();
-		}
+		// open value state message popup when focus is in the input
+		this.openValueStateMessage();
 	};
 
 	/**
@@ -841,7 +838,7 @@ function(
 	 * @protected
 	 */
 	InputBase.prototype.openValueStateMessage = function() {
-		if (this._oValueStateMessage) {
+		if (this._oValueStateMessage && this.shouldValueStateMessageBeOpened()) {
 			this._oValueStateMessage.open();
 		}
 	};
@@ -912,7 +909,7 @@ function(
 		if ($Input[0] === document.activeElement) {
 			if (sValueState === mValueState.None) {
 				this.closeValueStateMessage();
-			} else if (this.shouldValueStateMessageBeOpened()) {
+			} else {
 				this.openValueStateMessage();
 			}
 		}
