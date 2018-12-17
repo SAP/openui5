cf974b4455b394ddfd3c9829304a90cd8a6bc27d
Yavor Ivanov <yavor.ivanov@sap.com>
2018-12-17 17:32:20 +0200

    [FIX] sap.m.MessagePopover: MessageView's auto binding fix
    The MessageView has a feature which is when there's no items' binding,
    it gets auto bound to sap.ui.getCore().getMessageManager().getMessages().
    There are certain checks in the MessageView which are violated when it
    gets wrapped into the MessagePopover.
    So, the fix is to overwrite that check with the MessagePopover context
    when MessageView is used inside.
    
    BCP: 1870298128
    
    Change-Id: I8aa38a72442e17a3f76439bac2f2435d3db7156b
    CR-Id: 002075125900004934872018

--- src/sap.m/src/sap/m/MessageView.js
+++ src/sap.m/src/sap/m/MessageView.js
@@ -325,9 +325,7 @@ sap.ui.define([
 		}
 
 		// Bind automatically to the MessageModel if no items are bound
-		if (!this.getBindingInfo("items") && !aItems.length) {
-			this._makeAutomaticBinding();
-		}
+		this._makeAutomaticBinding();
 	};
 
 	/**
@@ -399,11 +397,25 @@ sap.ui.define([
 	};
 
 	/**
+	 * If there's no items binding, attach the MessageView to the sap.ui.getCore().getMessageManager().getMessageModel()
+	 *
+	 * @sap-restricted sap.m.MessagePopover
+	 * @private
+	 */
+	MessageView.prototype._makeAutomaticBinding = function () {
+		var aItems = this.getItems();
+
+		if (!this.getBindingInfo("items") && !aItems.length) {
+			this._bindToMessageModel();
+		}
+	};
+
+	/**
 	 * Makes automatic binding to the Message Model with default template
 	 *
 	 * @private
 	 */
-	MessageView.prototype._makeAutomaticBinding = function () {
+	MessageView.prototype._bindToMessageModel = function () {
 		var that = this;
 
 		this.setModel(sap.ui.getCore().getMessageManager().getMessageModel(), "message");
