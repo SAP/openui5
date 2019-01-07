196f180f06407d1160e3bb156f6de8afaa4f2e40
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2019-01-07 16:36:38 +0100

    [FEATURE] trace/FESR: Enhance API for improved FESR performance logging
    Change-Id: Ibad2a181e21315353bd2fb34556cadb8078fcff4

--- src/sap.ui.core/test/sap/ui/core/qunit/performance/trace/FESR.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/performance/trace/FESR.qunit.js
@@ -1,4 +1,4 @@
-/*global QUnit */
+/*global QUnit, sinon*/
 sap.ui.define(['sap/ui/performance/trace/FESR', 'sap/ui/performance/XHRInterceptor'], function(FESR, XHRInterceptor) {
 	"use strict";
 
@@ -14,6 +14,64 @@ sap.ui.define(['sap/ui/performance/trace/FESR', 'sap/ui/performance/XHRIntercept
 		FESR.setActive(false);
 		assert.notOk(FESR.getActive(), "should must not be active");
 		assert.notOk(XHRInterceptor.isRegistered("FESR", "open"), "FESR must not be registered");
+	});
+
+	QUnit.module("Interaction change");
+
+	QUnit.test("FESR", function(assert) {
+		var done = assert.async();
+
+		sap.ui.require(["sap/ui/performance/trace/Interaction"], function(Interaction) {
+			var onBeforeCreatedSpy,
+			 oSpyReturnValue,
+			 aAllInteractions,
+			 oLastInteraction,
+			 oInitialFESRHandle = {
+			  stepName:"undetermined_startup",
+			  appNameLong:"undetermined",
+			  appNameShort:"undetermined"
+			 };
+
+			// implement hook
+			FESR.onBeforeCreated = function(oFESRHandle, oInteraction) {
+				assert.throws(function() { oInteraction.component = "badComponent"; }, "Should throw an error after trying to overwrite the interaction object.");
+
+				return {
+					stepName: "newStepName",
+					appNameLong: "newAppNameLong",
+					appNameShort: "newAppNameShort"
+				};
+			};
+
+			onBeforeCreatedSpy = sinon.spy(FESR, "onBeforeCreated");
+
+			FESR.setActive(true);
+			Interaction.notifyStepStart(null, true);
+
+			assert.ok(Interaction.getActive(), "Implicit interaction activation successful");
+
+			Interaction.end(true);
+
+			aAllInteractions = Interaction.getAll();
+			oLastInteraction = aAllInteractions[aAllInteractions.length - 1];
+
+			assert.equal(onBeforeCreatedSpy.callCount, 1, "onBeforeCreated should be called once.");
+			assert.deepEqual(onBeforeCreatedSpy.getCall(0).args[0], oInitialFESRHandle, "Passed FESRHandle should be correct.");
+
+			assert.equal(oLastInteraction.component, "undetermined", "Component name is not modified.");
+			assert.equal(oLastInteraction.event, "startup", "Event name is not modified.");
+			assert.equal(oLastInteraction.trigger, "undetermined", "Trigger name is not modified.");
+
+			oSpyReturnValue = onBeforeCreatedSpy.returnValues[0];
+			assert.equal(oSpyReturnValue.stepName, "newStepName", "The correct stepName should be returned.");
+			assert.equal(oSpyReturnValue.appNameLong, "newAppNameLong", "The correct appNameLong should be returned.");
+			assert.equal(oSpyReturnValue.appNameShort, "newAppNameShort", "The correct appNameShort should be returned.");
+
+			onBeforeCreatedSpy.restore();
+
+			done();
+		});
+
 	});
 
 });
