23bae70b05660a430fef8475fe078cef92e9e4b2
Yuping Zuo <zypA13510@users.noreply.github.com>
2019-01-09 10:29:22 +0800

    Fixed sap.m.StepInput mouse wheel behavior
    Now the control will only alter its value when focused, instead of when hovered.

--- src/sap.m/src/sap/m/StepInput.js
+++ src/sap.m/src/sap/m/StepInput.js
@@ -899,12 +899,15 @@ function(
 		};
 
 		StepInput.prototype._onmousewheel = function (oEvent) {
-			oEvent.preventDefault();
-			var oOriginalEvent = oEvent.originalEvent,
-				bDirectionPositive = oOriginalEvent.detail ? (-oOriginalEvent.detail > 0) : (oOriginalEvent.wheelDelta > 0);
+			var bIsFocused = document.activeElement === this.$().getDomRef();
+			if (bIsFocused) {
+				oEvent.preventDefault();
+				var oOriginalEvent = oEvent.originalEvent,
+					bDirectionPositive = oOriginalEvent.detail ? (-oOriginalEvent.detail > 0) : (oOriginalEvent.wheelDelta > 0);
 
-			this._applyValue(this._calculateNewValue(1, bDirectionPositive).displayValue);
-			this._verifyValue();
+				this._applyValue(this._calculateNewValue(1, bDirectionPositive).displayValue);
+				this._verifyValue();
+			}
 		};
 
 		/**
