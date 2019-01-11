6d9120641c964662570f97a3865a1d0f7191b118
Kevin Edinger <kevin.edinger@sap.com>
2019-01-11 14:10:39 +0100

    [INTERNAL] sap.ui.fl: new sap.ui.fl.Scenario.VersionedAppVariant
    Change-Id: I41f47727d12cd319bd79590c288fbc8503d00ede
    Signed-off-by: Kevin Edinger <kevin.edinger@sap.com>
    JIRA: UICSFLEX-4493

--- src/sap.ui.fl/src/sap/ui/fl/library.js
+++ src/sap.ui.fl/src/sap/ui/fl/library.js
@@ -45,6 +45,7 @@ sap.ui.define([
 	 */
 	sap.ui.fl.Scenario = {
 		AppVariant: "APP_VARIANT",
+		VersionedAppVariant: "VERSIONED_APP_VARIANT",
 		AdaptationProject: "ADAPTATION_PROJECT",
 		FioriElementsFromScratch: "FE_FROM_SCRATCH",
 		UiAdaptation: "UI_ADAPTATION"
