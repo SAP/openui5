8d5f1eb1af7c6a9d6f7ba4e1defb366b93c3fad0
Sebastian Limbach <bastilimbach@gmail.com>
2019-01-15 16:33:03 +0100

    [INTERNAL] Add test to check if native image src is correctly set
    - This change adds a missing test to the LightBoxItem to check wherever
    the native image src attribute is correctly set. The attribute should
    only be set if the OpenState of the parent element is not 'OPEN'.
    
    - This change also fixes some typos and adds consistent whitespace to
    the file.
    
    Closes https://github.com/SAP/openui5/pull/1913

--- src/sap.m/test/sap/m/qunit/LightBoxItem.qunit.js
+++ src/sap.m/test/sap/m/qunit/LightBoxItem.qunit.js
@@ -2,74 +2,95 @@
 
 sap.ui.define(
 	["sap/m/LightBoxItem"],
-	function(LightBoxItem) {
+	function (LightBoxItem) {
 		'use strict';
 
-
-
 		QUnit.module('API', {
-			beforeEach: function() {
+			beforeEach: function () {
 				this.LightBoxItem = new LightBoxItem();
 			},
-			afterEach: function() {
+			afterEach: function () {
 				this.LightBoxItem.destroy();
 			}
 		});
 
 
-		QUnit.test('Setting image source', function(assert) {
-			//arange
+		QUnit.test('Setting image source', function (assert) {
+			//arrange
 			var imageSrc = 'test.jpg';
 			this.LightBoxItem.setImageSrc(imageSrc);
 			//act
 			var actualResult = this.LightBoxItem.getImageSrc();
 
 			//assert
-			assert.strictEqual(actualResult, imageSrc, 'Shoud set correct image source');
+			assert.strictEqual(actualResult, imageSrc, 'Should set correct image source');
 		});
 
-		QUnit.test('Setting image alt', function(assert) {
-			//arange
+		QUnit.test('Setting native image source when popup is not open', function (assert) {
+			//arrange
+			sinon.stub(this.LightBoxItem, 'getParent', function () {
+				return {
+					_oPopup: {
+						getOpenState: function () {
+							return 'LOADING';
+						}
+					}
+				};
+			});
+			var imageSrc = 'test.jpg';
+
+			// act
+			this.LightBoxItem.setImageSrc(imageSrc);
+
+			// assert
+			assert.strictEqual(this.LightBoxItem._getNativeImage().getAttribute('src'), imageSrc, 'setImageSrc should not set the image src if the popup is open');
+
+			// clean
+			this.LightBoxItem.getParent.restore();
+		});
+
+		QUnit.test('Setting image alt', function (assert) {
+			//arrange
 			var imageAlt = 'test image';
 			this.LightBoxItem.setAlt(imageAlt);
 			//act
 			var actualResult = this.LightBoxItem.getAlt();
 
 			//assert
-			assert.strictEqual(actualResult, imageAlt, 'Shoud set correct image alt');
+			assert.strictEqual(actualResult, imageAlt, 'Should set correct image alt');
 		});
 
-		QUnit.test('Setting image title', function(assert) {
-			//arange
+		QUnit.test('Setting image title', function (assert) {
+			//arrange
 			var imageTitle = 'test image';
 			this.LightBoxItem.setTitle(imageTitle);
 			//act
 			var actualResult = this.LightBoxItem.getTitle();
 
 			//assert
-			assert.strictEqual(actualResult, imageTitle, 'Shoud set correct image title');
+			assert.strictEqual(actualResult, imageTitle, 'Should set correct image title');
 		});
 
-		QUnit.test('Setting image subtitle', function(assert) {
-			//arange
+		QUnit.test('Setting image subtitle', function (assert) {
+			//arrange
 			var imageSubtitle = 'test image';
 			this.LightBoxItem.setSubtitle(imageSubtitle);
 			//act
 			var actualResult = this.LightBoxItem.getSubtitle();
 
 			//assert
-			assert.strictEqual(actualResult, imageSubtitle, 'Shoud set correct image subtitle');
+			assert.strictEqual(actualResult, imageSubtitle, 'Should set correct image subtitle');
 		});
 
-		QUnit.test('Setting image subtitle', function(assert) {
-			//arange
+		QUnit.test('Setting image subtitle', function (assert) {
+			//arrange
 			var imageSubtitle = 'test image';
 			this.LightBoxItem.setSubtitle(imageSubtitle);
 			//act
 			var actualResult = this.LightBoxItem.getSubtitle();
 
 			//assert
-			assert.strictEqual(actualResult, imageSubtitle, 'Shoud set correct image subtitle');
+			assert.strictEqual(actualResult, imageSubtitle, 'Should set correct image subtitle');
 		});
 
 		QUnit.test('Setting image when state is loading', function (assert) {
@@ -87,7 +108,6 @@ sap.ui.define(
 			var imageSrc = 'test.jpg';
 			var image = new window.Image();
 			image.src = imageSrc;
-
 
 			// act
 			this.LightBoxItem.setImageSrc(imageSrc);
