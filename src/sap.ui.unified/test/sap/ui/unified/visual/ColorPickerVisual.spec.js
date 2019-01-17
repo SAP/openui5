7e81f262ca181d7a2e7b454d03baec29925580ae
i310817 <gergana.marcheva@sap.com>
2019-01-17 10:55:43 +0200

    [INTERNAL] sap.ui.unified.ColorPicker: visual test fixed for Android
    JIRA: BGSOFUIBALKAN-4628
    
    Change-Id: Id9e4732b04a246c4099a366dd40ee8e3962657ae

--- src/sap.ui.unified/test/sap/ui/unified/visual/ColorPickerVisual.spec.js
+++ src/sap.ui.unified/test/sap/ui/unified/visual/ColorPickerVisual.spec.js
@@ -6,18 +6,21 @@ describe("sap.ui.unified.ColorPickerVisual", function() {
 
 	// Default mode
 	it("should render default mode HSL color picker with RGB output", function() {
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Default_ColorPicker_HSL_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Default_ColorPicker_HSL_RGB");
 	});
 
 	it("should render default mode HSV color picker with RGB output", function() {
 		element(by.id("hsv_hsl_btn")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Default_ColorPicker_HSV_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Default_ColorPicker_HSV_RGB");
 	});
 
 	it("should render default mode HSL color picker with HSL output", function() {
 		element(by.id("hsv_hsl_btn")).click();
 		element(by.id("cp-toggleMode")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Default_ColorPicker_HSL_HSL");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Default_ColorPicker_HSL_HSL");
 	});
 
 
@@ -25,12 +28,14 @@ describe("sap.ui.unified.ColorPickerVisual", function() {
 	it("should render large mode HSL color picker with RGB output", function() {
 		element(by.id("select_mode-label")).click();
 		element(by.id("large_mode")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Large_ColorPicker_HSV_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Large_ColorPicker_HSV_RGB");
 	});
 
 	it("should render large mode HSV color picker with RGB output", function() {
 		element(by.id("hsv_hsl_btn")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Large_ColorPicker_HSL_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Large_ColorPicker_HSL_RGB");
 	});
 
 
@@ -38,12 +43,14 @@ describe("sap.ui.unified.ColorPickerVisual", function() {
 	it("should render simplified mode HSL color picker with RGB output", function() {
 		element(by.id("select_mode-label")).click();
 		element(by.id("simplified_mode")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Simplified_ColorPicker_HSL_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Simplified_ColorPicker_HSL_RGB");
 	});
 
 	it("should render simplified mode HSV color picker with RGB output", function() {
 		element(by.id("hsv_hsl_btn")).click();
-		expect(takeScreenshot(element(by.id("ColorPickerArea-cont")))).toLookAs("Simplified_ColorPicker_HSV_RGB");
+		element(by.id("remove_focus_btn")).click();
+		expect(takeScreenshot(element(by.id("cp")))).toLookAs("Simplified_ColorPicker_HSV_RGB");
 	});
 
 });
