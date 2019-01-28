769073453455e2305779b67d69988e9252539b14
i310817 <gergana.marcheva@sap.com>
2019-01-28 10:48:58 +0200

    [FEATURE] sap.m.ObjectStatus: supports Large design now
    JIRA: BGSOFUIBALKAN-4634
    
    Change-Id: I12e9175760a3688be9997ad070e54e5edc1fcf58

--- src/sap.m/src/sap/m/ObjectStatus.js
+++ src/sap.m/src/sap/m/ObjectStatus.js
@@ -34,6 +34,10 @@ sap.ui.define([
 	 *
 	 * @class
 	 * Status information that can be either text with a value state, or an icon.
+	 *
+	 *
+	 * Since 1.63, Large design of the control is supported by setting <code>sapMObjectStatusLarge</code> CSS class to the <code>ObjectStatus</code>.
+	 *
 	 * @extends sap.ui.core.Control
 	 * @implements sap.ui.core.IFormContent
 	 * @version ${version}
