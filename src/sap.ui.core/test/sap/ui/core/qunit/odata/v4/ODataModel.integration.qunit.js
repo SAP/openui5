d656c7fe7e80c1e6d4a59abd8d4816ab7543880d
Uwe Reeder <uwe.reeder@sap.com>
2019-02-05 13:08:10 +0100

    [INTERNAL] odata.v4.ODataModel.integration.qunit.js: wait for messages, too
    Fixing the test "Auto-$expand/$select with dynamic filter, but no
    metadata" which only waited for a message, not for requests and not for
    changes.
    
    PS1: done
    PS2: review comments
    
    Change-Id: I70ba0ba3d55a5e0d20755dde816a9eec499d6ba0
    JIRA: CPOUI5UISERVICESV3-1726

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataModel.integration.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/ODataModel.integration.qunit.js
@@ -301,8 +301,9 @@ sap.ui.define([
 		},
 
 		/**
-		 * Checks the messages and finishes the test if no pending changes are left and all
-		 * expected requests have been received.
+		 * Checks the messages and finishes the test if no pending changes are left, all
+		 * expected requests have been received and the expected number of messages have been
+		 * reported.
 		 *
 		 * @param {object} assert The QUnit assert object
 		 */
@@ -311,6 +312,10 @@ sap.ui.define([
 
 			if (this.aRequests.length || this.iPendingResponses) {
 				return;
+			}
+			if (sap.ui.getCore().getMessageManager().getMessageModel().getObject("/").length
+					< this.aMessages.length) {
+				return; // expected messages still missing
 			}
 			for (sControlId in this.mChanges) {
 				if (this.mChanges[sControlId].length) {
@@ -991,6 +996,7 @@ sap.ui.define([
 						}
 					}
 				}
+				that.checkMessages(assert);
 			});
 		}
 	});
@@ -1832,7 +1838,7 @@ sap.ui.define([
 				.expectMessages([oMessage1, oMessage2]);
 
 			oTable.setFirstVisibleRow(1);
-			return that.waitForChanges();
+			return that.waitForChanges(assert);
 		});
 		//TODO: using an index for a bound message leads to a wrong target if for example
 		//      an entity with a lower index gets deleted, see CPOUI5UISERVICESV3-413
