de0a78c27588742c9bb4e72f2f419f67e93bc090
Alexander Ivanov <alexander.ivanov01@sap.com>
2019-02-11 15:10:48 +0200

    [FIX] ViewSettingsDialog: OK when custom item is used for filtering
    Change-Id: I3acb3e3caf652df7c2f73f888e3903d422b912ea
    BCP: 1970117193
    (cherry picked from commit 25ef16cb37fff33420cd40812d572adf671be7e3)
    CR-Id: 002075125800001162252019

--- src/sap.m/src/sap/m/ViewSettingsDialog.js
+++ src/sap.m/src/sap/m/ViewSettingsDialog.js
@@ -1759,6 +1759,10 @@ function(
 	 * @private
 	 */
 	ViewSettingsDialog.prototype._initFilterDetailItems = function(oItem) {
+		if (!(oItem instanceof sap.m.ViewSettingsFilterItem)) {
+			return;
+		}
+
 		var oListItem;
 		var bMultiSelectMode = oItem.getMultiSelect();
 		var aSubFilters = oItem.getItems();
