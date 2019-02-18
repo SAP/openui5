017fdd6c87637a01b3668542add1e88df6aaf929
Christian Voshage <christian.voshage@sap.com>
2019-02-18 17:22:51 +0100

    [INTERNAL][TEST] sap.ui.fl end-to-end test for personalizationAPI
    This commit introduces an application which can be used to show case as
    well as opa test basis the personalizationAPI (isPersonalized and
    resetChanges).
    
    Runnable under
    http://localhost:8080/test-resources/sap/ui/fl/testApps/controlPersonalizationAPI-reset/index.html
    after an npm start.
    
    Change-Id: I67592e46a99e0fb30914d2d5d4ee145b81c0fb00
    JIRA: UICSFLEX-4492
    JIRA: UICSFLEX-4611

---
+++ src/sap.ui.fl/test/sap/ui/fl/testApps/controlPersonalizationAPI/test/integration/NavigationJourney.js
@@ -0,0 +1,21 @@
+	/*global QUnit*/
+
+	sap.ui.define([
+		"sap/ui/test/opaQunit",
+		"./pages/MainView"
+	], function (opaTest) {
+		"use strict";
+
+		QUnit.module("Navigation Journey");
+
+		opaTest("Should see the initial page of the app", function (Given, When, Then) {
+			// Arrangements
+			Given.iStartMyApp();
+
+			// Assertions
+			Then.onTheAppPage.iShouldSeeTheApp();
+
+			//Cleanup
+			Then.iTeardownMyApp();
+		});
+	});
