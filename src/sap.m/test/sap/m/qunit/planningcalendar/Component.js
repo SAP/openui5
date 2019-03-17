e45b6e3e6ca68d5337e527cf59bf906e7c26a089
Frank Weigel <frank.weigel@sap.com>
2019-03-17 15:44:22 +0100

    [INTERNAL] sap.m: reduce sync XHR in integration tests
    Change-Id: Ifc9deecc293da5adb641a8d1c4d706b687937ccd

--- src/sap.m/test/sap/m/qunit/planningcalendar/Component.js
+++ src/sap.m/test/sap/m/qunit/planningcalendar/Component.js
@@ -1,32 +1,13 @@
 sap.ui.define(['sap/ui/core/UIComponent'],
-    function (UIComponent) {
-        "use strict";
+	function (UIComponent) {
+		"use strict";
 
-        var Component = UIComponent.extend("sap.m.sample.PlanningCalendar.Component", {
+		var Component = UIComponent.extend("sap.m.sample.PlanningCalendar.Component", {
+			metadata: {
+				manifest: "json"
+			}
+		});
 
-            metadata: {
-                rootView: {
-                    "viewName": "sap.m.sample.PlanningCalendar.Page",
-                    "type": "XML",
-                    "async": true
-                },
-                dependencies: {
-                    libs: [
-                        "sap.m",
-                        "sap.ui.unified"
-                    ]
-                },
-                config: {
-                    sample: {
-                        files: [
-                            "Page.view.xml",
-                            "Page.controller.js"
-                        ]
-                    }
-                }
-            }
-        });
+		return Component;
 
-        return Component;
-
-    });
+	});
