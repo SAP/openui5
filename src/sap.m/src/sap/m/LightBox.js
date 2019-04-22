1d2032254d61af9ef82b31b78b0ed0ebccd88614
Petar Dimov <petar.dimov@sap.com>
2019-04-22 18:24:02 +0300

    [INTERNAL] sap.m.LightBox: Adjusted padding on Fiori 3 theme
    Change-Id: I9c312d15100c753237c314ad3090c4b9e52f07c2
    JIRA: BGSOFUIRODOPI-2007

--- src/sap.m/src/sap/m/LightBox.js
+++ src/sap.m/src/sap/m/LightBox.js
@@ -17,7 +17,8 @@ sap.ui.define([
 	'sap/ui/core/library',
 	'./LightBoxRenderer',
 	'sap/m/BusyIndicator',
-	"sap/ui/thirdparty/jquery"
+	"sap/ui/thirdparty/jquery",
+	'sap/ui/core/Core'
 ],
 	function(
 		library,
@@ -34,7 +35,8 @@ sap.ui.define([
 		coreLibrary,
 		LightBoxRenderer,
 		BusyIndicator,
-		jQuery
+		jQuery,
+		Core
 	) {
 
 		'use strict';
@@ -571,17 +573,26 @@ sap.ui.define([
 		 * @returns {int} The height of the footer.
 		 */
 		LightBox.prototype._calculateFooterHeightInPx = function () {
+			var sTheme = Core.getConfiguration().getTheme();
 			var compact = this.$().parents().hasClass('sapUiSizeCompact');
 			var subtitle = this._getImageContent().getSubtitle();
 
 			var footerHeightRem = 2.5; // base height of the footer in rem
 
 			if (!compact) {
-				footerHeightRem += 0.5;
+				if (sTheme.startsWith("sap_fiori_")) {
+					footerHeightRem += 0.375;
+				} else {
+					footerHeightRem += 0.5;
+				}
 			}
 
 			if (subtitle) {
-				footerHeightRem += 1.5;
+				if (sTheme.startsWith("sap_fiori_")) {
+					footerHeightRem += 1.375;
+				} else {
+					footerHeightRem += 1.5;
+				}
 			}
 
 			return footerHeightRem * 16; // 1rem == 16px
