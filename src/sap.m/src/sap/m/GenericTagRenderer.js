8d0d2692436596906838de417dbf7167c0ec9993
Takov <georgi.takov@sap.com>
2019-05-02 10:04:12 +0300

    [FIX] sap.m.GenericTag: Fixed tooltip rendering
    Change-Id: Ic380871b11cea170cabba39abf766d561f4d6890
    BCP: 1980210776

--- src/sap.m/src/sap/m/GenericTagRenderer.js
+++ src/sap.m/src/sap/m/GenericTagRenderer.js
@@ -27,7 +27,8 @@ sap.ui.define([
 
 	GenericTagRenderer.render = function(oRm, oControl) {
 		var aLabelledBy = this._getAriaLabelledBy(oControl),
-			oResourceBundle = oCore.getLibraryResourceBundle("sap.m");
+			oResourceBundle = oCore.getLibraryResourceBundle("sap.m"),
+			sTooltip = oControl.getTooltip_AsString();
 
 		oRm.openStart("div");
 		oRm.class("sapMGenericTag");
@@ -42,6 +43,10 @@ sap.ui.define([
 			roledescription: oResourceBundle.getText("GENERICTAG_ROLEDESCRIPTION"),
 			labelledBy: aLabelledBy.join(" ")
 		});
+
+		if (sTooltip) {
+			oRm.writeAttributeEscaped("title", sTooltip);
+		}
 
 		oRm.openEnd();
 
