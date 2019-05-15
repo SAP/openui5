eed193a1efa5157e7e52e481ae7d0a02b173b42e
Alexandar Mitsev <alexandar.mitsev@sap.com>
2019-05-15 17:36:31 +0300

    [INTERNAL] Cards Explorer: Copy examples from demokit
    Change-Id: Ibba1fe0ba61b25304de18ecb8e9d1342ade9c8a6
    JIRA: BGSOFUIRODOPI-2029

--- src/sap.m/test/sap/m/demokit/cardExplorer/webapp/model/ExploreNavigationModel.js
+++ src/sap.m/test/sap/m/demokit/cardExplorer/webapp/model/ExploreNavigationModel.js
@@ -20,54 +20,98 @@ sap.ui.define([
 						key: 'list',
 						subSamples: [
 							{
-								title: 'General',
-								key: 'general',
-								manifestUrl: '/samples/list.manifest.json'
-							},
-							{
 								title: 'Highlight',
 								key: 'highlight',
-								manifestUrl: '/samples/list.highlight.manifest.json'
+								manifestUrl: '/samples/list/highlight.json'
+							},
+							{
+								title: 'General',
+								key: 'general',
+								manifestUrl: '/samples/list/general.json'
 							},
 							{
 								title: 'Icon',
 								key: 'icon',
-								manifestUrl: '/samples/list.icon.manifest.json'
+								manifestUrl: '/samples/list/icon.json'
 							},
 							{
 								title: 'Numeric',
 								key: 'numeric',
-								manifestUrl: '/samples/list.numeric.manifest.json'
+								manifestUrl: '/samples/list/numeric.json'
+							},
+							{
+								title: 'Quick Links',
+								key: 'quickLinks',
+								settings: {
+									width: 'auto'
+								},
+								manifestUrl: '/samples/list/quickLinks.json'
 							}
 						]
 					},
-
 					{
 						title: 'Analytical',
 						target: 'exploreSamples',
 						key: 'analytical',
-						settings: {
-							height: '400px'
-						},
-						manifestUrl: '/samples/analytical.manifest.json'
+						subSamples: [
+							{
+								title: 'Line',
+								key: 'line',
+								settings: {
+									height: '464px'
+								},
+								manifestUrl: '/samples/analytical/line.json'
+							},
+							{
+								title: 'Stacked Column',
+								key: 'stackedColumn',
+								settings: {
+									height: '464px'
+								},
+								manifestUrl: '/samples/analytical/stackedColumn.json'
+							},
+							{
+								title: 'Donut',
+								key: 'donut',
+								settings: {
+									width: '272px',
+									height: '272px'
+								},
+								manifestUrl: '/samples/analytical/donut.json'
+							}
+						]
 					},
 					{
 						title: 'Object',
 						target: 'exploreSamples',
 						key: 'object',
-						manifestUrl: '/samples/object.manifest.json'
+						manifestUrl: '/samples/object.json'
 					},
 					{
 						title: 'Table',
 						target: 'exploreSamples',
 						key: 'table',
-						manifestUrl: '/samples/table.manifest.json'
+						manifestUrl: '/samples/table.json'
 					},
 					{
 						title: 'Timeline',
 						target: 'exploreSamples',
 						key: 'timeline',
-						manifestUrl: '/samples/timeline.manifest.json'
+						subSamples: [
+							{
+								title: 'Upcoming Activities',
+								key: 'general',
+								manifestUrl: '/samples/timeline/activities.json',
+								settings: {
+									width: '272px'
+								}
+							},
+							{
+								title: 'New Team Members',
+								key: 'line',
+								manifestUrl: '/samples/timeline/members.json'
+							}
+						]
 					}
 				]
 			},
@@ -82,14 +126,23 @@ sap.ui.define([
 					{
 						key: 'actions',
 						target: 'exploreSamples',
-						title: 'Actions TODO',
-						manifestUrl: '/samples/list.manifest.json'
+						title: 'Actions',
+						settings: {
+							width: 'auto'
+						},
+						manifestUrl: '/samples/actions.json'
 					},
 					{
 						key: 'data',
 						target: 'exploreSamples',
 						title: 'Data TODO',
-						manifestUrl: '/samples/list.manifest.json'
+						manifestUrl: '/samples/table.json'
+					},
+					{
+						key: 'parameters',
+						target: 'exploreSamples',
+						title: 'Parameters',
+						manifestUrl: '/samples/parameters.json'
 					}
 				]
 			}
