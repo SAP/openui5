f444280449dfc482c66a8446aa92106586ee3c56
Tereza Chobanova <tereza.chobanova@sap.com>
2019-05-20 15:26:42 +0300

    [FIX] sap.ui.layout.DynamicSideContent: shows side content on small screen
    When used on smal devices and using showSideContent property,
    the control now shows the side content properly. In this case
    the behavior is similar to using the toggle method.
    
    Change-Id: I205ec8d174adddbc8701217f04dd44b73e900369
    BCP: 1980254677
    (cherry picked from commit 494f79dbafa756beaa8108726dea5af36a99e63a)
    CR-Id: 002075125900003538632019

--- src/sap.ui.layout/src/sap/ui/layout/DynamicSideContent.js
+++ src/sap.ui.layout/src/sap/ui/layout/DynamicSideContent.js
@@ -675,6 +675,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/ResizeHa
 			} else if (!this._SCVisible && !this._MCVisible) {
 				$mainContent.addClass(HIDDEN_CLASS);
 				$sideContent.addClass(HIDDEN_CLASS);
+			} else if (!this._SCVisible && this._MCVisible && bSideContentVisibleProperty && bMainContentVisibleProperty) {
+				$sideContent.removeClass().addClass(SPAN_SIZE_12_CLASS);
+				$mainContent.addClass(HIDDEN_CLASS);
 			} else if (this._MCVisible && bMainContentVisibleProperty) {
 				$mainContent.removeClass().addClass(SPAN_SIZE_12_CLASS);
 				$sideContent.addClass(HIDDEN_CLASS);
