fcd8bedee36f9d962938bf8291217e1843356e27
Petya Bogdanova <petya.markova-bogdanova@sap.com>
2019-05-22 12:27:16 +0300

    [FEATURE] sap.m.Breadcrumbs: Customisable separator option added
    The app developer now can choose how the separator between Breadcrumbs
    items should look like.
    
    Change-Id: I1ea04a3795b5ab62abc8a68fe86ee6e36a0ff202

--- src/sap.m/test/sap/m/demokit/sample/Breadcrumbs/controller/Breadcrumbs.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/Breadcrumbs/controller/Breadcrumbs.controller.js
@@ -1,9 +1,34 @@
-sap.ui.define(['sap/ui/core/mvc/Controller', "sap/m/MessageToast"], function (Controller, MessageToast) {
-	"use strict";
+sap.ui.define([
+	'sap/ui/core/mvc/Controller',
+	'sap/m/MessageToast',
+	'sap/ui/model/json/JSONModel'],
+	function (Controller, MessageToast, JSONModel) {
+	'use strict';
 
-	return Controller.extend("sap.m.sample.Breadcrumbs.controller.Breadcrumbs", {
+	return Controller.extend('sap.m.sample.Breadcrumbs.controller.Breadcrumbs', {
+		onInit: function() {
+			var oMData = [],
+				oModel =  new JSONModel().setDefaultBindingMode("TwoWay"),
+				BreadcrumbsSeparatorStyle = sap.m.BreadcrumbsSeparatorStyle;
+			Object.keys(BreadcrumbsSeparatorStyle).forEach(function(item) {
+				oMData.push({
+					'key' : item,
+					'text': BreadcrumbsSeparatorStyle[item]
+				});
+			});
+
+			oModel.setData({
+				items: oMData,
+				selected: oMData[0].text
+			});
+			this.getView().setModel(oModel);
+		},
 		onPress: function (oEvent) {
-			MessageToast.show(oEvent.getSource().getText() + " has been activated");
+			MessageToast.show(oEvent.getSource().getText() + ' has been activated');
+		},
+		onChange: function (oEvent) {
+			var oModel = this.getView().getModel();
+			oModel.setProperty('/selected', oEvent.getParameter('selectedItem').getKey());
 		}
 	});
 });
