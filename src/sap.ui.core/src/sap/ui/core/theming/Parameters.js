SAP/openui5

commit 4576c6899b34f81fb5e5d107eb2da9806be1b0ed

[FIX] sap.ui.core.theming.Parameters: Correctly rewrite  relative urls
Due to the new library parameters format (default/scopes) the
"checkAndResolveUrls" method was not working anymore.
It has now been fixed to alway check and rewrite relative urls.

Furthermore the check for relative URLs has been changed to use
URI.js instead of a RegExp to determine a relative URL which
also works with blob/data uris.

Change-Id: Ic62d3b6073392fa051cd315657232cf71d4e6b36
BCP: 1670436898

src/sap.ui.core/src/sap/ui/core/theming/Parameters.js
@@ -39,20 +39,39 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 
 		var aParametersToLoad = [];
 
+		// match a CSS url
+		var rCssUrl = /url[\s]*\('?"?([^\'")]*)'?"?\)/;
+
 		function resetParameters() {
 			mParameters = null;
 		}
 
-		function mergeParameterSet(mCurrent, mNew) {
+		function checkAndResolveRelativeUrl(sUrl, sThemeBaseUrl) {
+
+			var aMatch = rCssUrl.exec(sUrl);
+			if (aMatch) {
+				var oUri = new URI(aMatch[1]);
+				if (oUri.is("relative")) {
+					// Rewrite relative URLs based on the theme base url
+					// Otherwise they would be relative to the HTML page which is incorrect
+					var sNormalizedUrl = oUri.absoluteTo(sThemeBaseUrl).normalize().path();
+					sUrl = "url('" + sNormalizedUrl + "')";
+				}
+			}
+
+			return sUrl;
+		}
+
+		function mergeParameterSet(mCurrent, mNew, sThemeBaseUrl) {
 			for (var sParam in mNew) {
 				if (typeof mCurrent[sParam] === "undefined") {
-					mCurrent[sParam] = mNew[sParam];
+					mCurrent[sParam] = checkAndResolveRelativeUrl(mNew[sParam], sThemeBaseUrl);
 				}
 			}
 			return mCurrent;
 		}
 
-		function mergeParameters(mNewParameters) {
+		function mergeParameters(mNewParameters, sThemeBaseUrl) {
 
 			// check for old format:
 			// {
@@ -73,7 +92,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 			mParameters["scopes"] = mParameters["scopes"] || {};
 
 			// merge default parameters
-			mergeParameterSet(mParameters["default"], mNewParameters["default"]);
+			mergeParameterSet(mParameters["default"], mNewParameters["default"], sThemeBaseUrl);
 
 			// merge scopes
 			if (typeof mNewParameters["scopes"] === "object") {
@@ -81,27 +100,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 					// ensure scope object
 					mParameters["scopes"][sScopeName] = mParameters["scopes"][sScopeName] || {};
 					// merge scope set
-					mergeParameterSet(mParameters["scopes"][sScopeName], mNewParameters["scopes"][sScopeName]);
+					mergeParameterSet(mParameters["scopes"][sScopeName], mNewParameters["scopes"][sScopeName], sThemeBaseUrl);
 				}
 			}
-		}
-
-		function checkAndResolveUrls(mParams, sResourceUrl){
-			//only resolve relative urls
-			var rRelativeUrl = /^url\(['|"]{1}(?!https?:\/\/|\/)(.*)['|"]{1}\)$/,
-				sAbsolutePath = sResourceUrl.replace(/library-parameters\.json.*/, "");
-
-			/*eslint-disable no-loop-func */
-			for (var sId in mParams){
-				if (rRelativeUrl.test(mParams[sId])){
-					mParams[sId] = mParams[sId].replace(rRelativeUrl, function($0, $1, $2){
-						var sNormalizedPath = new URI(sAbsolutePath + $1).normalize().path();
-						return "url('" + sNormalizedPath + "')";
-					});
-				}
-			}
-			/*eslint-enable no-loop-func */
-			return mParams;
 		}
 
 		function forEachStyleSheet(fnCallback) {
@@ -113,7 +114,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 		/*
 		 * Load parameters for a library/theme combination as identified by the URL of the library.css
 		 */
-		function loadParameters(sId, sUrl) {
+		function loadParameters(sId, sStyleSheetUrl) {
+
+			// Remove CSS file name and query to create theme base url (to resolve relative urls)
+			var sThemeBaseUrl = new URI(sStyleSheetUrl).filename("").query("").toString();
 
 			// read inline parameters from css style rule
 			// (can be switched off for testing purposes via private URI parameter "sap-ui-xx-no-inline-theming-parameters=true")
@@ -133,7 +137,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 					}
 					try {
 						var oParams = jQuery.parseJSON(sParams);
-						mergeParameters(oParams);
+						mergeParameters(oParams, sThemeBaseUrl);
 						return;
 					} catch (ex) {
 						jQuery.sap.log.warning("Could not parse theme parameters from " + sUrl + ". Loading library-parameters.json as fallback solution.");
@@ -147,8 +151,8 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 
 			// derive parameter file URL from CSS file URL
 			// $1: name of library (incl. variants)
-			// $2: additional parameters, e.g. for sap-ui-merged use case
-			sUrl = sUrl.replace(/\/library([^\/.]*)\.(?:css|less)($|[?#])/, function($0,$1,$2) {
+			// $2: additional parameters, e.g. for sap-ui-merged, version/sap-ui-dist-version
+			var sUrl = sStyleSheetUrl.replace(/\/library([^\/.]*)\.(?:css|less)($|[?#])/, function($0, $1, $2) {
 				return "/library-parameters.json" + ($2 ? $2 : "");
 			});
 
@@ -168,12 +172,10 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 					// in the sap-ui-merged use case, multiple JSON files are merged into and transfered as a single JSON array
 					for (var j = 0; j < oResult.length; j++) {
 						var oParams = oResult[j];
-						oParams = checkAndResolveUrls(oParams, sUrl);
-						mergeParameters(oParams);
+						mergeParameters(oParams, sThemeBaseUrl);
 					}
 				} else {
-					oResult = checkAndResolveUrls(oResult, sUrl);
-					mergeParameters(oResult);
+					mergeParameters(oResult, sThemeBaseUrl);
 				}
 			} else {
 				// ignore failure at least temporarily as long as there are libraries built using outdated tools which produce no json file
@@ -186,7 +188,9 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 			// Inital loading
 			if (!mParameters) {
 
-				mergeParameters({});
+				// Merge an empty parameter set to initialize the internal object
+				mergeParameters({}, "");
+
 				sTheme = sap.ui.getCore().getConfiguration().getTheme();
 
 				forEachStyleSheet(loadParameters);
@@ -494,7 +498,7 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/thirdparty/URI', '../Element'],
 			sParamName = sParamName || "sapUiGlobalLogo";
 			var logo = Parameters.get(sParamName);
 			if (logo) {
-				var match = /url[\s]*\('?"?([^\'")]*)'?"?\)/.exec(logo);
+				var match = rCssUrl.exec(logo);
 				if (match) {
 					logo = match[1];
 				} else if (logo === "''" || logo === "none") {

src/sap.ui.core/test/sap/ui/core/qunit/ThemeParameters.qunit.html
@@ -213,6 +213,71 @@
 					"Key-value map for the given params 'sapUiTstTextColor' and 'sapUiText' should be returned");
 		});
 
+		QUnit.test("Relative URLs in parameters", function(assert) {
+
+			var expected = {
+				url1: new URI("testdata/uilib/img1.jpg", new URI()).path(),
+				url2: new URI("testdata/uilib/foo/img2.jpg", new URI()).path(),
+				url3: new URI("testdata/uilib/foo/bar/img3.jpg", new URI()).path(),
+				url4: new URI("testdata/uilib/themes/sap_hcb/", new URI()).path(),
+				url5: new URI("testdata/uilib/themes/sap_hcb/", new URI()).path(),
+				url6: new URI("testdata/uilib/themes/sap_hcb/", new URI()).path(),
+				url7: "blob:http://example.com/6e88648c-00e1-4512-9695-5b702d8455b4",
+				url8: "data:text/plain;utf-8,foo",
+				url9: {
+					plain: "none",
+					themeImage: null,
+					themeImageForce: sap.ui.resource("sap.ui.core", "themes/base/img/1x1.gif")
+				}
+			};
+
+			// plain values
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl1"), "url('" + expected.url1 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/img1.jpg'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl2"), "url('" + expected.url2 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/foo/img2.jpg'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl3"), "url('" + expected.url3 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/foo/bar/img3.jpg'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl4"), "url('" + expected.url4 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/themes/sap_hcb/'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl5"), "url('" + expected.url5 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/themes/sap_hcb/'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl6"), "url('" + expected.url6 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/themes/sap_hcb/'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl7"), "url('" + expected.url7 + "')", "Relative URL should be resolved correctly 'blob:http://example.com/6e88648c-00e1-4512-9695-5b702d8455b4'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl8"), "url('" + expected.url8 + "')", "Relative URL should be resolved correctly 'data:text/plain;utf-8,foo'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiTestUrl9"), expected.url9.plain, "'none' should stay as defined");
+
+			// _getThemeImage
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl1"), expected.url1, "Theme Image value should be correct for 'sapUiTestUrl1'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl2"), expected.url2, "Theme Image value should be correct for 'sapUiTestUrl2'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl3"), expected.url3, "Theme Image value should be correct for 'sapUiTestUrl3'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl4"), expected.url4, "Theme Image value should be correct for 'sapUiTestUrl4'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl5"), expected.url5, "Theme Image value should be correct for 'sapUiTestUrl5'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl6"), expected.url6, "Theme Image value should be correct for 'sapUiTestUrl6'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl7"), expected.url7, "Theme Image value should be correct for 'sapUiTestUrl7'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl8"), expected.url8, "Theme Image value should be correct for 'sapUiTestUrl8'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl9"), expected.url9.themeImage, "Theme Image value should be 'null' for parameter value 'none'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiTestUrl9", true), expected.url9.themeImageForce, "Theme Image value should be 'sap/ui/core/themes/base/img/1x1.gif' for parameter value 'none' (force).");
+
+		});
+
+		QUnit.test("Relative URLs in parameters (legacy library)", function(assert) {
+
+			var expected = {
+				url1: new URI("testdata/legacy-uilib/img1.jpg", new URI()).path(),
+				url2: {
+					plain: "none",
+					themeImage: null,
+					themeImageForce: sap.ui.resource("sap.ui.core", "themes/base/img/1x1.gif")
+				}
+			};
+
+			// plain values
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiLegacyTestUrl1"), "url('" + expected.url1 + "')", "Relative URL should be resolved correctly 'sap/ui/core/qunit/testdata/uilib/img1.jpg'.");
+			assert.equal(sap.ui.core.theming.Parameters.get("sapUiLegacyTestUrl2"), expected.url2.plain, "'none' should stay as defined");
+
+			// _getThemeImage
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiLegacyTestUrl1"), expected.url1, "Theme Image value should be correct for 'sapUiLegacyTestUrl1'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiLegacyTestUrl2"), expected.url2.themeImage, "Theme Image value should be 'null' for parameter value 'none'.");
+			assert.equal(sap.ui.core.theming.Parameters._getThemeImage("sapUiLegacyTestUrl2", true), expected.url2.themeImageForce, "Theme Image value should be 'sap/ui/core/themes/base/img/1x1.gif' for parameter value 'none' (force).");
+
+		});
+
     </script>
 	</head>
 	<body>

commit 5321f4dfe2cd436823e49a9918e1e40c0fcb283c

[FIX] Theming Parameters: Fix urls when UI5 is loaded from other domain
Resolving the urls was done correctly, but only the "path" segment of
the absolute URL has been used. So when UI5 is loaded from a different
domain, this does not work and the url points to the domain the
document is loaded from, not where the UI5 theme resources are located.

This fixes a regression caused by
4576c6899b34f81fb5e5d107eb2da9806be1b0ed

Also fixes a wrong path for the stylesheet test case.

Change-Id: I348391fc087c2a3e06f5b2708c8cf928a677ffab
BCP: 1970102993

src/sap.ui.core/src/sap/ui/core/theming/Parameters.js
@@ -61,7 +61,7 @@ sap.ui.define([
 				if (oUri.is("relative")) {
 					// Rewrite relative URLs based on the theme base url
 					// Otherwise they would be relative to the HTML page which is incorrect
-					var sNormalizedUrl = oUri.absoluteTo(sThemeBaseUrl).normalize().path();
+					var sNormalizedUrl = oUri.absoluteTo(sThemeBaseUrl).normalize().toString();
 					sUrl = "url('" + sNormalizedUrl + "')";
 				}
 			}

src/sap.ui.core/test/sap/ui/core/qunit/ThemeParameters.qunit.js
@@ -2,7 +2,7 @@
 sap.ui.define(["sap/ui/core/theming/Parameters", "sap/ui/core/Control", "sap/ui/core/Element"], function(Parameters, Control, Element) {
 	"use strict";
 
-	var sPath = new URI(sap.ui.require.toUrl("testdata/core"), document.baseURI).path();
+	var sPath = new URI(sap.ui.require.toUrl("testdata/core"), document.baseURI).toString();
 
 	QUnit.test("InitialCheck", function(assert) {
 		assert.ok(Parameters, "sap.ui.core.theming.Parameters must exist");
@@ -77,7 +77,7 @@ sap.ui.define(["sap/ui/core/theming/Parameters", "sap/ui/core/Control", "sap/ui/
 
 		// include 40 stylesheets to test IE9 stylesheet limit
 		var iNewStylesheets = 40;
-		var sStyleBaseUrl = "testdata/stylesheets/";
+		var sStyleBaseUrl = "test-resources/sap/ui/core/qunit/testdata/stylesheets/";
 		var i;
 		for (i = 0; i < iNewStylesheets; i++) {
 			var sStyleId = getStyleId(i);