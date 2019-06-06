867aa179f4ae12513361f478db640049194a3088
i336130 <tomas.piller@sap.com>
2019-06-06 13:42:48 +0200

    [INTERNAL] Expand UploadSet samples
    Change-Id: I1b36cef083d24bbcedf7639038d68ca296d1c858
    JIRA: FXUBRQ13-1305

--- src/sap.m/test/sap/m/demokit/sample/UploadSet/Page.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/UploadSet/Page.controller.js
@@ -8,37 +8,17 @@ sap.ui.define([
 ], function (jQuery, MobileLibrary, Controller, Item, JSONModel, Uploader) {
 	"use strict";
 
-	var CustomUploader = Uploader.extend("sap.m.sample.UploadSet.CustomUploader", {
-		metadata: {}
-	});
-
-	CustomUploader.prototype.uploadItem = function (oItem, aHeaders) {
-		var sNewUploadUrl = "../../../../upload"; // This value may be result of a backend request eg.
-		aHeaders.push(new Item({key: "SomePostKey", text: "SomePostText"}));
-		this.setUploadUrl(sNewUploadUrl);
-
-		Uploader.prototype.uploadItem.call(this, oItem, aHeaders);
-	};
-
-	CustomUploader.prototype.downloadItem = function (oItem, aHeaders, bAskForLocation) {
-		var sNewDownloadUrl = oItem.getUrl(); // This value may be result of a backend request eg.
-		aHeaders.push(new Item({key: "SomeGetKey", text: "SomeGetText"}));
-		this.setDownloadUrl(sNewDownloadUrl);
-
-		Uploader.prototype.downloadItem.call(this, oItem, aHeaders, bAskForLocation);
-	};
-
 	return Controller.extend("sap.m.sample.UploadSet.Page", {
 		onInit: function () {
 			var sPath = sap.ui.require.toUrl("sap/m/sample/UploadSet") + "/items.json",
-				oUploadSet = this.byId("UploadSet"),
-				oCustomUploader = new CustomUploader();
+				oUploadSet = this.byId("UploadSet");
 
 			this.getView().setModel(new JSONModel(sPath));
-
-			oUploadSet.setUploader(oCustomUploader);
-			oUploadSet.registerUploaderEvents(oCustomUploader);
 			oUploadSet.getList().setMode(MobileLibrary.ListMode.MultiSelect);
+
+			// Modify "add file" button
+			oUploadSet.getDefaultFileUploader().setButtonOnly(false);
+			oUploadSet.getDefaultFileUploader().setIcon("sap-icon://attachment");
 		},
 		onUploadSelectedButton: function () {
 			var oUploadSet = this.byId("UploadSet");
