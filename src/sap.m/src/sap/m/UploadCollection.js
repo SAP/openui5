68afe9415513c1532b31d9e8ec86c5ab0079968b
I344427 <d.makkalikkal@sap.com>
2019-06-12 10:50:50 +0530

    [FIX] sap.m.UploadCollection : Download of item not happening when factory binding is used.
    Problem:
    The item is not getting downloaded when fileName is clicked and in console dump is seen under triggerLink method.
    This happens only when factory binding is used in items aggregation
    
    Fix:
    Earlier the fileName id was used to determine the item position.FileName generated differs based on template or factory method used.
    Hence it was not reliable.Added fix so that the download url is taken from item directly.
    
    Change-Id: I6110af3cfb0b67f130d4f0f650c492e1416dca73
    BCP: 012005041000000451312019.
    (cherry picked from commit e6a3f05fd9e0f98e0fcdd6e81b3244a0bd7d8180)
    CR-Id: 002075125800003196462019

--- src/sap.m/src/sap/m/UploadCollection.js
+++ src/sap.m/src/sap/m/UploadCollection.js
@@ -1776,7 +1776,7 @@ sap.ui.define([
 		if (item.hasListeners("press")) {
 			item.firePress();
 		} else if (this.sErrorState !== "Error" && jQuery.trim(item.getProperty("url"))) {
-			this._triggerLink(event);
+			this._triggerLink(event, item);
 		}
 	};
 
@@ -2966,21 +2966,17 @@ sap.ui.define([
 	 * @param {sap.ui.base.Event} event The SAPUI5 event object
 	 * @private
 	 */
-	UploadCollection.prototype._triggerLink = function(event) {
-		var iLine, aId;
-
-		if (this.editModeItem) {
-			//In case there is a list item in edit mode, the edit mode has to be finished first.
-			this._handleOk(event, this.editModeItem, true);
-			if (this.sErrorState === "Error") {
-				//If there is an error, the link of the list item must not be triggered.
-				return;
-			}
-			this.sFocusId = event.getParameter("id");
+	UploadCollection.prototype._triggerLink = function(event, item) {
+	if (this.editModeItem) {
+		//In case there is a list item in edit mode, the edit mode has to be finished first.
+		this._handleOk(event, this.editModeItem, true);
+		 if (this.sErrorState === "Error") {
+			//If there is an error, the link of the list item must not be triggered.
+			return;
 		}
-		aId = event.oSource.getId().split("-");
-		iLine = aId[aId.length - 2];
-		MobileLibrary.URLHelper.redirect(this.aItems[iLine].getProperty("url"), true);
+		this.sFocusId = event.getParameter("id");
+	}
+	MobileLibrary.URLHelper.redirect(item.getProperty("url"),true);
 	};
 
 	// ================================================================================
