7b7db5a74b842584758dab68b05307aa461d1aa4
d036800 <martin.schaus@sap.com>
2019-06-13 07:34:56 +0200

    [INTERNAL] Allow nojQuery bundle recognition for sap-ui-integration
    Change-Id: Ie4b5eaeeff06182f608db724836d3c6061576ff7

--- src/sap.ui.integration/src/sap/ui/integration/sap-ui-integration-config.js
+++ src/sap.ui.integration/src/sap/ui/integration/sap-ui-integration-config.js
@@ -4,8 +4,7 @@
 
 //enforce complex binding syntax if using sap-ui-integration.js
 //this file is included to generated sap-ui-integration bundle
-window["sap-ui-config"] = {
-	"bindingSyntax": "complex",
-	"compatVersion": "edge",
-	"async": true
-};
+window["sap-ui-config"] = window["sap-ui-config"] || {};
+window["sap-ui-config"].bindingSyntax = "complex";
+window["sap-ui-config"].compatVersion = "edge";
+window["sap-ui-config"].async = true;
