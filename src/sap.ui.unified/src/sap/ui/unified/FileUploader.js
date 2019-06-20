6f8d4fe9308f322ae24612b73c1417288d1fe855
Tereza Chobanova <tereza.chobanova@sap.com>
2019-06-20 13:54:23 +0300

    [FIX] sap.ui.unified.FileUploader: JSDoc of change event adjusted
    In Chrome when the Cancel button of the uploads windows is pressed,
    the change event of the FileUploader is fired too. This is limitation,
    brought from the HTML input of type file element.
    
    Change-Id: Idecf5e7baa188ae6128558502074def46180c237
    BCP: 1970253137

--- src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
+++ src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
@@ -258,6 +258,10 @@ sap.ui.define([
 
 			/**
 			 * Event is fired when the value of the file path has been changed.
+			 *
+			 * <b>Note:</b> Keep in mind that because of the HTML input element of type file, the
+			 * event is also fired in Chrome browser when the Cancel button of the
+			 * uploads window is pressed.
 			 */
 			change : {
 				parameters : {
