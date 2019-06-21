ce6246b0e987c2d377e9e16001f73b97833c5dab
Diana Mihaylova <diana.mihaylova@sap.com>
2019-06-21 15:15:40 +0300

    [FIX] sap.ui.unified.calendar.TimesRow: Now custom data is respected over locale's one
    PlanningCalendar had issues with its Hours view if there was a custom
    time format set, different from the locale's one.
    
    BCP: 002075129500003246982019
    Fixes: https://github.com/SAP/openui5/issues/2540
    
    Change-Id: I425c2639b8a8dfd52e50d2c8600a6e88d17cc707
    CR-Id: 002075125800004021612019

--- src/sap.ui.unified/src/sap/ui/unified/calendar/TimesRow.js
+++ src/sap.ui.unified/src/sap/ui/unified/calendar/TimesRow.js
@@ -409,17 +409,20 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 			var iIntervalMinutes = this.getIntervalMinutes();
 			var oLocaleData = this._getLocaleData();
 			var sPattern;
+			var sTimeFormatShort = oLocaleData.getTimePattern("short");
 			this._oFormatTimeAmPm = undefined;
 
+			// don't display minutes
 			if (iIntervalMinutes % 60 == 0) {
-				// don't display minutes
-				sPattern = oLocaleData.getPreferredHourSymbol();
-				if (oLocaleData.getTimePattern("short").search("a") >= 0) {
+				//sPattern determines whether the shown format will be 12 or 24 hrs
+				sPattern = _getPreferredHourSymbol(sTimeFormatShort);
+
+				if (sTimeFormatShort.search("a") >= 0) {
 					// AP/PM indicator used
 					this._oFormatTimeAmPm = sap.ui.core.format.DateFormat.getTimeInstance({pattern: "a"}, oLocale);
 				}
 			} else {
-				sPattern = oLocaleData.getTimePattern("short");
+				sPattern = sTimeFormatShort;
 				// no leading zeros
 				sPattern = sPattern.replace("HH", "H");
 				sPattern = sPattern.replace("hh", "h");
@@ -1448,6 +1451,23 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/core/Control', 'sap/ui/core/LocaleDa
 		jQuery(window.document).unbind('mousemove', this._mouseMoveProxy);
 		this._bMouseMove = undefined;
 
+	}
+
+	/*
+	 * Getter for the time's preferred hour symbol. Possible options are h|H|k|K.
+	 * @param {String} sTimeFormatShort Hours time format
+	 * @return {String} Hours pattern.
+	 * @private
+	 */
+	function _getPreferredHourSymbol(sTimeFormatShort){
+		var sPattern;
+		if (sTimeFormatShort.toUpperCase().indexOf("K") > -1) {
+			sPattern = sTimeFormatShort.indexOf("k") > -1 ? "k" : "K";
+		} else {
+			sPattern = sTimeFormatShort.indexOf("h") > -1 ? "h" : "H";
+		}
+
+		return sPattern;
 	}
 
 	return TimesRow;
