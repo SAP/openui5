b7cc6743d72c2dc47b5768ac6269751320285d60
Alexandar Mitsev <alexandar.mitsev@sap.com>
2019-06-24 18:12:15 +0300

    [INTERNAL] sap.f.dnd.GridDropInfo: Unit tests added
    Change-Id: If100c223e0c506bf522b2b9ba83dc9510548879e
    JIRA: BGSOFUIRODOPI-2059

---
+++ src/sap.f/test/sap/f/qunit/dnd/GridDragOver.qunit.js
@@ -0,0 +1,80 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"sap/ui/thirdparty/jquery",
+	"sap/ui/qunit/utils/createAndAppendDiv",
+	"sap/f/dnd/GridDragOver",
+	"sap/f/GridContainer",
+	"sap/m/Text",
+	"sap/ui/core/Core",
+	"sap/ui/Device"
+], function(jQuery, createAndAppendDiv, GridDragOver, GridContainer, Text, Core, Device) {
+	"use strict";
+
+	createAndAppendDiv("content");
+
+	QUnit.module("Initialization");
+
+	QUnit.test("Instance", function(assert) {
+		var oInstance = GridDragOver.getInstance();
+
+		assert.ok(oInstance.isA("sap.f.dnd.GridDragOver"), "GridDragOver is initialized");
+	});
+
+	QUnit.module("Drag over grid", {
+		beforeEach: function () {
+			this.oGridDragOver = new GridDragOver();
+
+			var oDropItem = new Text({text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}),
+				oDragItem = new Text({text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."});
+
+			this.oGrid = new GridContainer({
+				dragDropConfig: this.oGridDropInfo,
+				items: [oDropItem, oDragItem]
+			});
+
+			this.oGrid.placeAt("content");
+			Core.applyChanges();
+
+			this.oGridDragOver.setCurrentContext(
+				oDragItem,
+				this.oGrid,
+				"items"
+			);
+		},
+		afterEach: function () {
+			this.oGridDragOver.destroy();
+			this.oGrid.destroy();
+		}
+	});
+
+	if (!Device.browser.msie) {
+		QUnit.test("Simulate drag over", function(assert) {
+			// Arrange
+			var done = assert.async(),
+				oFakeEvent = new jQuery.Event("dragover"),
+				oTargetControl = this.oGrid.getItems()[0],
+				mTargetRect = oTargetControl.getDomRef().getBoundingClientRect();
+
+			oFakeEvent.pageX = mTargetRect.left;
+			oFakeEvent.pageY = mTargetRect.top;
+			oFakeEvent.target = oTargetControl.getDomRef();
+
+			// Act
+			this.oGridDragOver.handleDragOver(oFakeEvent);
+
+			// wait 250ms and handle drag over again on same place
+			setTimeout(function () {
+				this.oGridDragOver.handleDragOver(oFakeEvent);
+
+				// Assert
+				var mPosition = this.oGridDragOver.getSuggestedDropPosition();
+				assert.ok(mPosition, "There is a suggested position after timeout");
+				assert.strictEqual(mPosition.targetControl.sId, oTargetControl.sId, "The target control is correct");
+				assert.strictEqual(mPosition.position, "Before", "The target position is 'Before'");
+
+				done();
+			}.bind(this), 250);
+		});
+	}
+});
