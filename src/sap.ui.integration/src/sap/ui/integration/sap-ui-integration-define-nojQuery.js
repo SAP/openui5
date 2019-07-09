23b1202f5a68e41481c1e7ea198576e8e3d4b709
d036800 <martin.schaus@sap.com>
2019-07-09 13:05:32 +0200

    [INTERNAL] sap-ui-integration-nojQuery
    - Bundling fixed
    
    Change-Id: Ice234765f2ac1eae996c7c5c7a7a0289559711c1

--- src/sap.ui.integration/src/sap/ui/integration/sap-ui-integration-define-nojQuery.js
+++ src/sap.ui.integration/src/sap/ui/integration/sap-ui-integration-define-nojQuery.js
@@ -5,10 +5,10 @@
 // define jQuery for for bundling sap-ui-integration-nojQuery.js
 (function() {
 	"use strict";
-	sap.ui.define("sap/ui/thirdparty/jquery", function() {
+	sap["ui"].define("sap/ui/thirdparty/jquery", function() {
 		return jQuery;
 	});
-	sap.ui.define("sap/ui/thirdparty/ui/jquery/jquery-ui-position", function() {
+	sap["ui"].define("sap/ui/thirdparty/jqueryui/jquery-ui-position", function() {
 		return jQuery;
 	});
 })();
