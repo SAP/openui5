f69ed41166948414b92663853109f1eb4734d17f
Martin Georgiev <martin.georgiev@sap.com>
2019-07-09 14:52:28 +0300

    [INTERNAL] sap.m.MessageToast: Sample section updated
    - Adjusting the sample to reflect the UI5 Evo recommendations so that
    developers only copy up-to-date code from the Demo Kit.
    
    JIRA: BGSOFUIRILA-2403
    Change-Id: I668a0170f7466ea1777a4689b59c53895faea5a0

--- src/sap.m/test/sap/m/demokit/sample/MessageToast/Component.js
+++ src/sap.m/test/sap/m/demokit/sample/MessageToast/Component.js
@@ -2,13 +2,10 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	function(UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("sap.m.sample.MessageToast.Component", {
+	return UIComponent.extend("sap.m.sample.MessageToast.Component", {
 
 		metadata : {
 		    manifest: "json"
 		}
 	});
-
-	return Component;
-
 });
