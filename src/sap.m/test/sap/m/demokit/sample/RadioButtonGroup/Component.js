aaf92e22d2792f024afec199d434f97ddb720c3e
Martin Georgiev <martin.georgiev@sap.com>
2019-07-11 08:10:57 +0300

    [INTERNAL] sap.m.RadioButton: Sample section updated
    - Adjusting the sample section to reflect the UI5 Evo recommendations
    so that developers only copy up-to-date code from the Demo Kit.
    
    JIRA: BGSOFUIRILA-2403
    Change-Id: I4c1d89607121b21198d1743ab15ac82fa7e68042

--- src/sap.m/test/sap/m/demokit/sample/RadioButtonGroup/Component.js
+++ src/sap.m/test/sap/m/demokit/sample/RadioButtonGroup/Component.js
@@ -2,13 +2,10 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	function(UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("sap.m.sample.RadioButtonGroup.Component", {
+	return UIComponent.extend("sap.m.sample.RadioButtonGroup.Component", {
 
 		metadata : {
 			manifest: "json"
 		}
 	});
-
-	return Component;
-
 });
