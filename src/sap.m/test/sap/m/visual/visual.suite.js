bb44d66d96c60498973c98a3012a8ffd86ccd6ae
Lidiya Georgieva <lidiya.georgieva@sap.com>
2019-07-16 15:07:13 +0300

    [FIX] sap.m.Input: vertical alignment issue fixed
    Change-Id: I14f07e043ffb8b7c2c7014c9d7f492f56d5b0375
    BCP: 1970315739
    Fixes: https://github.com/SAP/openui5/issues/2577

--- src/sap.m/test/sap/m/visual/visual.suite.js
+++ src/sap.m/test/sap/m/visual/visual.suite.js
@@ -21,6 +21,7 @@ module.exports = [
 	"IconTabHeaderResponsivePaddings.spec.js",
 	"Image.spec.js",
 	"InputVisualTests.spec.js",
+	"InputDescriptionVisualTests.spec.js",
 	"Label.spec.js",
 	"LightBox.spec.js",
 	"Link.spec.js",
