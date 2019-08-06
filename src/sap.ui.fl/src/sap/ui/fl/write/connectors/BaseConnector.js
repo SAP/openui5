edaa6aa396cc719d2bf391aa85deb0fbfd9d481c
Christian Voshage <christian.voshage@sap.com>
2019-08-06 13:25:44 +0200

    [INTERNAL] sap.ui.fl.write.connector.BaseConnector adding functions
    adding the following functions to the BaseConnector:
    * reset
    * publish
    * getFlexInfo
    
    Change-Id: I454f96f133be8b138c14b4715e7653ab4dd66fc6

--- src/sap.ui.fl/src/sap/ui/fl/write/connectors/BaseConnector.js
+++ src/sap.ui.fl/src/sap/ui/fl/write/connectors/BaseConnector.js
@@ -20,7 +20,7 @@ sap.ui.define([
 		/**
 		 * Interface called to write changes and variants.
 		 *
-		 * @param {map} mPropertyBag
+		 * @param {object} mPropertyBag
 		 * @param {sap.ui.fl.Change|sap.ui.fl.Change[]} mPropertyBag.payload Data to be stored
 		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer in which the data should be stored
 		 * @param {string} [mPropertyBag.url] Configured url for the connector
@@ -28,6 +28,54 @@ sap.ui.define([
 		 */
 		writeChanges:function (/* mPropertyBag */) {
 			return Promise.reject("writeChanges is not implemented");
+		},
+
+		/**
+		 * Interface called to reset data for a given application.
+		 *
+		 * @param {object} mPropertyBag Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer in which the reset should happen
+		 * @param {string} mPropertyBag.reference Flex reference
+		 * @param {string} [mPropertyBag.url] - Configured url for the connector
+		 * @param {string} [mPropertyBag.appVersion] Version of the application for which the reset takes place
+		 * @param {string} [mPropertyBag.generator] Generator with which the changes were created
+		 * @param {string[]} [mPropertyBag.selectorIds] Selector IDs of controls for which the reset should filter
+		 * @param {string} [mPropertyBag.changeTypes] Change types of the changes which should be reset
+		 * @returns {Promise} Promise resolves as soon as the writing was completed
+		 */
+		reset: function (/* mPropertyBag */) {
+			return Promise.reject("reset is not implemented");
+		},
+
+		/**
+		 * Interface called to publish data for a given application.
+		 *
+		 * @param {object} mPropertyBag Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer in which the reset should happen
+		 * @param {string} mPropertyBag.reference Flex reference
+		 * @param {string} [mPropertyBag.url] - Configured url for the connector
+		 * @param {string} [mPropertyBag.appVersion] Version of the application for which the reset takes place
+		 * @param {string} [mPropertyBag.generator] Generator with which the changes were created
+		 * @param {string[]} [mPropertyBag.selectorIds] Selector IDs of controls for which the reset should filter
+		 * @param {string} [mPropertyBag.changeTypes] Change types of the changes which should be reset
+		 * @returns {Promise} Promise resolves as soon as the writing was completed
+		 */
+		publish: function (/* mPropertyBag */) {
+			return Promise.reject("publish is not implemented");
+		},
+
+		/**
+		 * Interface called to publish data for a given application.
+		 *
+		 * @param {object} mPropertyBag Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer in which the reset should happen
+		 * @param {string} mPropertyBag.reference Flex reference
+		 * @param {string} [mPropertyBag.url] - Configured url for the connector
+		 * @param {string} [mPropertyBag.appVersion] Version of the application for which the reset takes place
+		 * @returns {Promise} Promise resolves as soon as the writing was completed
+		 */
+		getFlexInfo: function (/* mPropertyBag */) {
+			return Promise.reject("getFlexInfo is not implemented");
 		},
 
 		/**
