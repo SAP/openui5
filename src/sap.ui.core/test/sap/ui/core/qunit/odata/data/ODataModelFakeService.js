7db61961c3bac325fa200ae9e55b9d29c4218c4e
Wedel, Malte <malte.wedel@sap.com>
2019-08-06 16:38:00 +0200

    [FIX] v2.ODataListBinding: Don't use expanded data, if custom parameters are set
    In case a list binding has specific custom parameters, expanded data should not be
    used, even if it is available, as the custom parameters have not been used for
    requesting the expanded data.
    
    Change-Id: Id3715e36a0ede001360cddc9817951c473d2415a
    BCP: 1980011792

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/data/ODataModelFakeService.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/data/ODataModelFakeService.js
@@ -318,7 +318,11 @@ sap.ui.define([], function() {
           [200, oJSONHeaders, sCategorySelect2JSON],
         "Categories(1)?$expand=Products":
           [200, oJSONHeaders, sCategories1ExpandProducts],
-        "Categories(1)/Products?$skip=0&$top=100&$filter=ProductName%20eq%20%27Chai%27":
+        "Categories(1)/Products?$skip=0&$top=100&search=Test":
+          [200, oJSONHeaders, sCategories1ProductsSearch],
+        "Categories(1)/Products/$count?search=Test":
+          [200, oCountHeaders, "12"],
+       "Categories(1)/Products?$skip=0&$top=100&$filter=ProductName%20eq%20%27Chai%27":
           [200, oJSONHeaders, sCategories1ProductsFilterChai],
         "Categories(1)/Products/$count?$filter=ProductName%20eq%20%27Chai%27":
           [200, oCountHeaders, "1"],
@@ -9512,6 +9516,206 @@ sap.ui.define([], function() {
   }\
   ]\
   }\
+  }\
+  }';
+
+  var sCategories1ProductsSearch = '{\
+  "d" : {\
+  "results": [\
+  {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(1)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 1, "ProductName": "Chai", "SupplierID": 1, "CategoryID": 1, "QuantityPerUnit": "10 boxes x 20 bags", "UnitPrice": "18.0000", "UnitsInStock": 39, "UnitsOnOrder": 0, "ReorderLevel": 10, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(1)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(1)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(1)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(2)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 2, "ProductName": "Chang", "SupplierID": 1, "CategoryID": 1, "QuantityPerUnit": "24 - 12 oz bottles", "UnitPrice": "19.0000", "UnitsInStock": 17, "UnitsOnOrder": 40, "ReorderLevel": 25, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(2)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(2)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(2)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(24)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 24, "ProductName": "Guaran\u00e1 Fant\u00e1stica", "SupplierID": 10, "CategoryID": 1, "QuantityPerUnit": "12 - 355 ml cans", "UnitPrice": "4.5000", "UnitsInStock": 20, "UnitsOnOrder": 0, "ReorderLevel": 0, "Discontinued": true, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(24)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(24)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(24)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(34)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 34, "ProductName": "Sasquatch Ale", "SupplierID": 16, "CategoryID": 1, "QuantityPerUnit": "24 - 12 oz bottles", "UnitPrice": "14.0000", "UnitsInStock": 111, "UnitsOnOrder": 0, "ReorderLevel": 15, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(34)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(34)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(34)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(35)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 35, "ProductName": "Steeleye Stout", "SupplierID": 16, "CategoryID": 1, "QuantityPerUnit": "24 - 12 oz bottles", "UnitPrice": "18.0000", "UnitsInStock": 20, "UnitsOnOrder": 0, "ReorderLevel": 15, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(35)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(35)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(35)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(38)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 38, "ProductName": "C\u00f4te de Blaye", "SupplierID": 18, "CategoryID": 1, "QuantityPerUnit": "12 - 75 cl bottles", "UnitPrice": "263.5000", "UnitsInStock": 17, "UnitsOnOrder": 0, "ReorderLevel": 15, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(38)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(38)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(38)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(39)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 39, "ProductName": "Chartreuse verte", "SupplierID": 18, "CategoryID": 1, "QuantityPerUnit": "750 cc per bottle", "UnitPrice": "18.0000", "UnitsInStock": 69, "UnitsOnOrder": 0, "ReorderLevel": 5, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(39)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(39)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(39)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(43)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 43, "ProductName": "Ipoh Coffee", "SupplierID": 20, "CategoryID": 1, "QuantityPerUnit": "16 - 500 g tins", "UnitPrice": "46.0000", "UnitsInStock": 17, "UnitsOnOrder": 10, "ReorderLevel": 25, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(43)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(43)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(43)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(67)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 67, "ProductName": "Laughing Lumberjack Lager", "SupplierID": 16, "CategoryID": 1, "QuantityPerUnit": "24 - 12 oz bottles", "UnitPrice": "14.0000", "UnitsInStock": 52, "UnitsOnOrder": 0, "ReorderLevel": 10, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(67)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(67)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(67)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(70)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 70, "ProductName": "Outback Lager", "SupplierID": 7, "CategoryID": 1, "QuantityPerUnit": "24 - 355 ml bottles", "UnitPrice": "15.0000", "UnitsInStock": 15, "UnitsOnOrder": 10, "ReorderLevel": 30, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(70)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(70)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(70)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(75)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 75, "ProductName": "Rh\u00f6nbr\u00e4u Klosterbier", "SupplierID": 12, "CategoryID": 1, "QuantityPerUnit": "24 - 0.5 l bottles", "UnitPrice": "7.7500", "UnitsInStock": 125, "UnitsOnOrder": 0, "ReorderLevel": 25, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(75)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(75)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(75)/Supplier"\
+  }\
+  }\
+  }, {\
+  "__metadata": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(76)", "type": "NorthwindModel.Product"\
+  }, "ProductID": 76, "ProductName": "Lakkalik\u00f6\u00f6ri", "SupplierID": 23, "CategoryID": 1, "QuantityPerUnit": "500 ml", "UnitPrice": "18.0000", "UnitsInStock": 57, "UnitsOnOrder": 0, "ReorderLevel": 20, "Discontinued": false, "Category": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(76)/Category"\
+  }\
+  }, "Order_Details": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(76)/Order_Details"\
+  }\
+  }, "Supplier": {\
+  "__deferred": {\
+  "uri": "http://services.odata.org/V2/Northwind/Northwind.svc/Products(76)/Supplier"\
+  }\
+  }\
+  }\
+  ]\
   }\
   }';
 
