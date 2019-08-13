3c729e8a5c504407c69023bf6184c1d7c5ab63eb
Martin Georgiev <martin.georgiev@sap.com>
2019-08-13 08:43:15 +0300

    [INTERNAL] sap.m.MessageView: Sample and documentation updated
    The MessageView documentation is updated to describe better
    how developers should work with the MessageManager.
    
    JIRA: BGSOFUIRILA-2421
    Change-Id: I58a7a0e87470e8044c7435da7064eeae37930d5c

--- src/sap.m/test/sap/m/demokit/sample/MessageViewMessageManager/Component.js
+++ src/sap.m/test/sap/m/demokit/sample/MessageViewMessageManager/Component.js
@@ -2,7 +2,7 @@ sap.ui.define(['sap/ui/core/UIComponent'],
 	function(UIComponent) {
 		"use strict";
 
-		return UIComponent.extend("sap.m.sample.MessageViewMessageHandling.Component", {
+		return UIComponent.extend("sap.m.sample.MessageViewMessageManager.Component", {
 
 			metadata : {
 				manifest: "json"
