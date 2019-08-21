600d8ad0cb6a686d3378bd952fa7e4918cdbd463
Nayden Naydenov <nayden.naydenov@sap.com>
2019-08-21 14:30:36 +0300

    [INTERNAL] sap.m.Image: Updated Demo Kit sample
    Now, the sample shows that SVG extension is supported. SVG allows
    to clip custom shapes and it is supported in all browsers.
    
    Updated sample to evo recommendations.
    
    Change-Id: Ib3a0f04a273f76e32b7b4a328fe851d5279e2751
    JIRA: BGSOFUIPIRIN-4063

--- src/sap.m/test/sap/m/demokit/sample/Image/controller/ImageGroup.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/Image/controller/ImageGroup.controller.js
@@ -1,23 +1,27 @@
 sap.ui.define([
-		'jquery.sap.global',
 		'sap/m/MessageToast',
 		'sap/ui/Device',
 		'sap/ui/core/mvc/Controller',
 		'sap/ui/model/json/JSONModel'
-	], function(jQuery, MessageToast, Device, Controller, JSONModel) {
+	], function(MessageToast, Device, Controller, JSONModel) {
 	"use strict";
 
-	var ImageGroupController = Controller.extend("sap.m.sample.Image.ImageGroup", {
+	var ImageGroupController = Controller.extend("sap.m.sample.Image.controller.ImageGroup", {
 
 		onInit: function() {
-			var bIsPhone = Device.system.phone;
+			var bIsPhone = Device.system.phone,
+			svgLogo = sap.ui.require.toUrl("sap/m/sample/Image/images/sap-logo.svg"),
+			oImgModel;
+
 			this.getView().setModel(new JSONModel({
-				imageWidth: bIsPhone ? "5em" : "10em"
+				imageWidth: bIsPhone ? "5em" : "10em",
+				svgLogo: svgLogo
 			}));
 
 			// set explored app's demo model on this sample
-			var oImgModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock") + "/img.json");
+			oImgModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock") + "/img.json");
 			this.getView().setModel(oImgModel, "img");
+
 		},
 
 		handleImage3Press: function(evt) {
