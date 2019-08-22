7264f2f0f4a26c12fa08c33613ce4809f9dc23bc
Christian Voshage <christian.voshage@sap.com>
2019-08-22 12:05:54 +0200

    [INTERNAL] sap.ui.fl...apply.BrowserStorageConnector adjusted response
    The not yet used BrowserConnector is prepared to provide the response
    for the loadFlexData request in a different format. Instead of building
    the variant section on its own the Connector.js will be in charge with a
    following commit.
    The accepted format in a flex response with:
    {
    	changes: [],
    	variants: [],
    	variantChanges: [],
    	variantDependentControlChanges: [],
    	variantManagementChanges: []
    }
    or an array of such objects. Each entry in the array is reflecting a
    "layer" (derived by layer or connector like 'VENDOR' or 'CUSTOMER_BASE'
    which are already actual 2 layers... one from the changes-bundle.json
    and the back end.
    
    Change-Id: Id1f33b029d9778f395b914aaf4f8b93935d19e38

--- src/sap.ui.fl/src/sap/ui/fl/apply/_internal/connectors/BrowserStorageConnector.js
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/connectors/BrowserStorageConnector.js
@@ -4,14 +4,96 @@
 
 sap.ui.define([
 	"sap/base/util/merge",
+	"sap/ui/fl/library",
 	"sap/ui/fl/apply/connectors/BaseConnector",
-	"sap/ui/fl/apply/_internal/connectors/BrowserStorageUtils"
+	"sap/ui/fl/apply/_internal/connectors/BrowserStorageUtils",
+	"sap/ui/fl/Utils"
 ], function(
 	merge,
+	library,
 	BaseConnector,
-	BrowserStorageUtils
+	BrowserStorageUtils,
+	FlUtils
 ) {
 	"use strict";
+
+	function loadDataFromStorage (oStorage, sReference) {
+		var aFlexObjects = [];
+		var oFlexObject;
+		// _items is used in the internal keys of the JsObjectStorage
+		BrowserStorageUtils.forEachChangeInStorage(oStorage._items || oStorage, function(sKey) {
+			oFlexObject = JSON.parse(oStorage.getItem(sKey));
+			var bSameReference = oFlexObject.reference === sReference || oFlexObject.reference + ".Component" === sReference;
+			if (bSameReference) {
+				aFlexObjects.push(oFlexObject);
+			}
+		}.bind(this));
+
+		return aFlexObjects;
+	}
+
+	function getGroupedFlexObjects (aFlexObjects) {
+		var mGroupedFlexObjects = {};
+
+		// build empty groups
+		Object.keys(library.Layer).forEach(function (sLayer) {
+			mGroupedFlexObjects[sLayer] = {
+				variants: [],
+				variantChanges: [],
+				variantManagementChanges: [],
+				variantDependentControlChanges: [],
+				changes: [],
+				index: FlUtils.getLayerIndex(sLayer)
+			};
+		});
+
+		// fill groups
+		aFlexObjects.forEach(function(oFlexObject) {
+			var sLayer = oFlexObject.layer;
+
+			if (oFlexObject.fileType === "ctrl_variant" && oFlexObject.variantManagementReference) {
+				mGroupedFlexObjects[sLayer].variants.push(oFlexObject);
+			} else if (oFlexObject.fileType === "ctrl_variant_change") {
+				mGroupedFlexObjects[sLayer].variantChanges.push(oFlexObject);
+			} else if (oFlexObject.fileType === "ctrl_variant_management_change") {
+				mGroupedFlexObjects[sLayer].variantManagementChanges.push(oFlexObject);
+			} else if (oFlexObject.fileType === "change") {
+				if (oFlexObject.variantReference) {
+					mGroupedFlexObjects[sLayer].variantDependentControlChanges.push(oFlexObject);
+				} else {
+					mGroupedFlexObjects[sLayer].changes.push(oFlexObject);
+				}
+			}
+		});
+
+		// sort groups
+		Object.keys(mGroupedFlexObjects).forEach(function (sLayer) {
+			BrowserStorageUtils.sortGroupedFlexObjects(mGroupedFlexObjects[sLayer]);
+		});
+
+		return mGroupedFlexObjects;
+	}
+
+	function filterAndSortResponses(mGroupedFlexObjects) {
+		var aResponses = [];
+		Object.keys(mGroupedFlexObjects).forEach(function (sLayer) {
+			aResponses.push(mGroupedFlexObjects[sLayer]);
+		});
+
+		aResponses = aResponses.filter(function (oResponse) {
+			return oResponse.changes.length > 0
+				|| oResponse.variants.length > 0
+				|| oResponse.variantChanges.length > 0
+				|| oResponse.variantManagementChanges.length > 0
+				|| oResponse.variantDependentControlChanges.length > 0;
+		});
+
+		aResponses.sort(function(a, b) {
+			return a.index - b.index;
+		});
+
+		return aResponses;
+	}
 
 	/**
 	 * Base Connector for requesting data from session or local storage
@@ -32,53 +114,14 @@ sap.ui.define([
 		 * Changes can be filtered by reference and layer.
 		 *
 		 * @param {object} mPropertyBag properties needed by the connectors
-		 * @param {string} [mPropertyBag.reference] reference of the application
-		 * @param {string} [mPropertyBag.layer] layer of the changes
+		 * @param {string} mPropertyBag.reference reference of the application
 		 * @returns {Promise<Object>} resolving with an object containing a data contained in the changes-bundle
 		 */
 		loadFlexData: function (mPropertyBag) {
-			var aChanges = [];
-			var oChange;
-
-			// _items is used in the internal keys of the JsObjectStorage
-			BrowserStorageUtils.forEachChangeInStorage(this.oStorage._items || this.oStorage, function(sKey) {
-				oChange = JSON.parse(this.oStorage.getItem(sKey));
-				var bSameReference = oChange.reference === mPropertyBag.reference || oChange.reference + ".Component" === mPropertyBag.reference;
-				var bSameLayer = oChange.layer === mPropertyBag.layer;
-				if (
-					mPropertyBag.reference && !bSameReference
-					|| mPropertyBag.layer && !bSameLayer
-				) {
-					return;
-				}
-				aChanges.push(oChange);
-			}.bind(this));
-
-			var mGroupedChanges = {
-				variants: [],
-				controlVariantChanges: [],
-				controlVariantManagementChanges: [],
-				uiChanges: []
-			};
-			aChanges.forEach(function(oChange) {
-				if (oChange.fileType === "ctrl_variant" && oChange.variantManagementReference) {
-					mGroupedChanges.variants.push(oChange);
-				} else if (oChange.fileType === "ctrl_variant_change") {
-					mGroupedChanges.controlVariantChanges.push(oChange);
-				} else if (oChange.fileType === "ctrl_variant_management_change") {
-					mGroupedChanges.controlVariantManagementChanges.push(oChange);
-				} else {
-					mGroupedChanges.uiChanges.push(oChange);
-				}
-			});
-
-			var mResult = BrowserStorageUtils.createChangesMapWithVariants(mGroupedChanges.variants);
-			BrowserStorageUtils.addChangesToMap(mResult, mGroupedChanges);
-			//now all changes are combined and in the right section => sort them all
-			BrowserStorageUtils.sortChanges(mResult);
-			BrowserStorageUtils.assignVariantReferenceChanges(mResult);
-
-			return Promise.resolve(mResult);
+			var aFlexObjects = loadDataFromStorage(this.oStorage, mPropertyBag.reference);
+			var mGroupedFlexObjects = getGroupedFlexObjects(aFlexObjects);
+			var aResponses = filterAndSortResponses(mGroupedFlexObjects);
+			return Promise.resolve(aResponses);
 		}
 	});
 
--- src/sap.ui.fl/src/sap/ui/fl/apply/_internal/connectors/BrowserStorageUtils.js
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/connectors/BrowserStorageUtils.js
@@ -2,13 +2,7 @@
  * ! ${copyright}
  */
 
-sap.ui.define([
-	"sap/base/util/merge",
-	"sap/ui/fl/Utils"
-], function (
-	merge,
-	Utils
-) {
+sap.ui.define([], function () {
 	"use strict";
 
 	/**
@@ -23,63 +17,6 @@ sap.ui.define([
 
 	var FL_CHANGE_KEY = "sap.ui.fl.change";
 	var FL_VARIANT_KEY = "sap.ui.fl.variant";
-
-	function fakeStandardVariant(sVariantManagementReference) {
-		return {
-			fileName: sVariantManagementReference,
-			fileType: "ctrl_variant",
-			variantManagementReference: sVariantManagementReference,
-			variantReference: "",
-			content: {
-				title: sap.ui.getCore().getLibraryResourceBundle("sap.ui.fl").getText("STANDARD_VARIANT_TITLE")
-			}
-		};
-	}
-
-	function getVariantStructure(oVariant, aControlChanges, mVariantChanges) {
-		return {
-			content: oVariant,
-			controlChanges: aControlChanges,
-			variantChanges: mVariantChanges
-		};
-	}
-
-	function getVariantManagementStructure(aVariants, mVariantManagementChanges) {
-		return {
-			variants : aVariants,
-			variantManagementChanges : mVariantManagementChanges
-		};
-	}
-
-	function forEachVariant(mResult, fnCallback) {
-		Object.keys(mResult.variantSection).forEach(function (sVariantManagementReference) {
-			var aVariants = mResult.variantSection[sVariantManagementReference].variants;
-			aVariants.forEach(fnCallback);
-		});
-	}
-
-	function forSpecificVariant(mResult, sVariantManagementReference, sVariantReference, fnCallback) {
-		mResult.variantSection[sVariantManagementReference].variants.some(function(oVariant) {
-			if (oVariant.content.fileName === sVariantReference) {
-				fnCallback(oVariant);
-				return true;
-			}
-		});
-	}
-
-	function getReferencedChanges(mResult, oCurrentVariant) {
-		var aReferencedChanges = [];
-		forSpecificVariant(mResult, oCurrentVariant.content.variantManagementReference, oCurrentVariant.content.variantReference, function (oVariant) {
-			aReferencedChanges = oVariant.controlChanges.filter(function (oReferencedChange) {
-				return Utils.compareAgainstCurrentLayer(oReferencedChange.layer, oCurrentVariant.layer) === -1;
-			});
-			if (oVariant.content.variantReference) {
-				aReferencedChanges = getReferencedChanges(mResult, oVariant).concat(aReferencedChanges);
-			}
-		});
-
-		return aReferencedChanges;
-	}
 
 	return {
 		/**
@@ -119,151 +56,21 @@ sap.ui.define([
 			}
 		},
 
-		createChangesMapWithVariants: function(aVariants) {
-			function checkForDuplicates(aExistingVariants, oNewVariantFromChange) {
-				return aExistingVariants.some(function(oVariant) {
-					return oVariant.content.fileName === oNewVariantFromChange.fileName;
-				});
-			}
-
-			var mResult = {
-				changes: [],
-				variantSection: {}
-			};
-
-			aVariants = aVariants || [];
-			var oVariantManagementSection = {};
-			aVariants.forEach(function(oVariant) {
-				oVariantManagementSection = mResult.variantSection[oVariant.variantManagementReference];
-				//if VariantManagement doesn't exist
-				if (!oVariantManagementSection) {
-					var oStandardVariant = fakeStandardVariant(oVariant.variantManagementReference);
-					oVariantManagementSection = getVariantManagementStructure(
-						[getVariantStructure(oStandardVariant, [], {}), getVariantStructure(oVariant, [], {})],
-						{}
-					);
-					mResult.variantSection[oVariant.variantManagementReference] = oVariantManagementSection;
-				} else if (!checkForDuplicates(oVariantManagementSection.variants, oVariant)) {
-					oVariantManagementSection.variants.push(getVariantStructure(oVariant, [], {}));
-				}
-			});
-
-			return mResult;
-		},
-
-		addChangesToMap: function(mChangesMap, mGroupedChanges) {
-			function addChangeToVariant(mChangesMap, sVariantManagementReference, oChange) {
-				forSpecificVariant(mChangesMap, sVariantManagementReference, oChange.variantReference, function (oVariant) {
-					oVariant.controlChanges.push(oChange);
-				});
-			}
-
-			function addVariantChangeToVariant(mChangesMap, sVariantManagementReference, oVariantChange) {
-				forSpecificVariant(mChangesMap, sVariantManagementReference, oVariantChange.selector.id, function (oVariant) {
-					if (!oVariant.variantChanges[oVariantChange.changeType]) {
-						oVariant.variantChanges[oVariantChange.changeType] = [];
-					}
-					oVariant.variantChanges[oVariantChange.changeType].push(oVariantChange);
-				});
-			}
-
-			function checkIfVariantExists(mChangesMap, sVariantReference) {
-				return Object.keys(mChangesMap.variantSection).some(function (sVariantManagementReference) {
-					var aVariants = mChangesMap.variantSection[sVariantManagementReference].variants;
-					return aVariants.some(function(oVariant) {
-						return oVariant.content.fileName === sVariantReference;
-					});
-				});
-			}
-
-			if (!mChangesMap || !mChangesMap.changes || !mChangesMap.variantSection) {
-				throw Error("Passed changes map is not valid");
-			}
-
-			var mVariantManagementChanges = {};
-			mGroupedChanges.controlVariantManagementChanges.forEach(function(oVariantManagementChange) {
-				var sVariantManagementReference = oVariantManagementChange.selector.id;
-				if (!mChangesMap.variantSection[sVariantManagementReference]) {
-					mChangesMap.variantSection[sVariantManagementReference] = getVariantManagementStructure(
-						[getVariantStructure(fakeStandardVariant(sVariantManagementReference), [], {})],
-						{}
-					);
-				}
-				mVariantManagementChanges = mChangesMap.variantSection[sVariantManagementReference].variantManagementChanges;
-				if (!mVariantManagementChanges[oVariantManagementChange.changeType]) {
-					mVariantManagementChanges[oVariantManagementChange.changeType] = [];
-				}
-				mVariantManagementChanges[oVariantManagementChange.changeType].push(oVariantManagementChange);
-			});
-
-			mGroupedChanges.uiChanges.forEach(function(oChange) {
-				if (!oChange.variantReference) {
-					mChangesMap.changes.push(oChange);
-				} else if (!checkIfVariantExists(mChangesMap, oChange.variantReference)) {
-					mChangesMap.variantSection[oChange.variantReference] = getVariantManagementStructure(
-						[getVariantStructure(fakeStandardVariant(oChange.variantReference), [oChange], {})],
-						{}
-					);
-				} else {
-					Object.keys(mChangesMap.variantSection).forEach(function(sVariantManagementReference) {
-						addChangeToVariant(mChangesMap, sVariantManagementReference, oChange);
-					});
-				}
-			});
-
-			mGroupedChanges.controlVariantChanges.forEach(function(oVariantChange) {
-				if (!checkIfVariantExists(mChangesMap, oVariantChange.selector.id)) {
-					var mVariantChanges = {};
-					mVariantChanges[oVariantChange.changeType] = [oVariantChange];
-					mChangesMap.variantSection[oVariantChange.selector.id] = getVariantManagementStructure(
-						[getVariantStructure(fakeStandardVariant(oVariantChange.selector.id), [], mVariantChanges)],
-						{}
-					);
-				} else {
-					Object.keys(mChangesMap.variantSection).forEach(function (sVariantManagementReference) {
-						addVariantChangeToVariant(mChangesMap, sVariantManagementReference, oVariantChange);
-					});
-				}
-			});
-
-			return mChangesMap;
-		},
-
-		sortChanges: function(mResult) {
-			function byLayerThenCreation(oChangeA, oChangeB) {
-				var iLayerA = Utils.getLayerIndex(oChangeA.layer);
-				var iLayerB = Utils.getLayerIndex(oChangeB.layer);
-				if (iLayerA !== iLayerB) {
-					return iLayerA - iLayerB;
-				}
+		sortGroupedFlexObjects: function(mResult) {
+			function byCreation(oChangeA, oChangeB) {
 				return new Date(oChangeA.creation) - new Date(oChangeB.creation);
 			}
 
-			if (mResult.changes) {
-				mResult.changes.sort(byLayerThenCreation);
-			}
-			if (mResult.variantSection) {
-				forEachVariant(mResult, function (oVariant) {
-					oVariant.controlChanges.sort(byLayerThenCreation);
-				});
-			}
-
-			return mResult;
-		},
-
-		assignVariantReferenceChanges: function(mResult) {
-			var mOriginalChangesMap = merge({}, mResult);
-			forEachVariant(mOriginalChangesMap, function (oVariant) {
-				var sVariantReference = oVariant.content.variantReference;
-				var aExistingChanges = oVariant.controlChanges;
-				if (sVariantReference) {
-					// Referenced changes should be applied first
-					aExistingChanges = getReferencedChanges(mOriginalChangesMap, oVariant).concat(aExistingChanges);
-				}
-				forSpecificVariant(mResult, oVariant.content.variantManagementReference, oVariant.content.fileName, function (oCurrentVariant) {
-					oCurrentVariant.controlChanges = aExistingChanges;
-				});
+			[
+				"changes",
+				"variantChanges",
+				"variants",
+				"variantDependentControlChanges",
+				"variantManagementChanges"
+			].forEach(function (sSectionName) {
+				mResult[sSectionName] = mResult[sSectionName].sort(byCreation);
 			});
+
 
 			return mResult;
 		}
