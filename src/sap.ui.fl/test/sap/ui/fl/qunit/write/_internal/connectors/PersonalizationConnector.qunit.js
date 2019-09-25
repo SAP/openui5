80c2647a5dd157bcdfa4cd745622754f6f261f3e
Christian Voshage <christian.voshage@sap.com>
2019-09-25 13:56:07 +0200

    [INTERNAL][FIX] sap/ui/fl/.../PersonalizationConnector test fix
    Two commits were merged which affect the expectation of a newly introduced test.
    The productive code is correct, but the test does the wrong assumption.
    This commit aligns the test with the productive code.
    
    Change-Id: I4c34a910cd3a7e540438085ed6588387c4694965
    CR-Id: 002075125900004929742019
    BCP: n/a

--- src/sap.ui.fl/test/sap/ui/fl/qunit/write/_internal/connectors/PersonalizationConnector.qunit.js
+++ src/sap.ui.fl/test/sap/ui/fl/qunit/write/_internal/connectors/PersonalizationConnector.qunit.js
@@ -123,7 +123,7 @@ sap.ui.define([
 
 		QUnit.test("given reset is called with optional parameters", function (assert) {
 			var mPropertyBag = {
-				url: "/flex/personalization",
+				url: "/flexPersonalization",
 				reference: "reference",
 				generator: undefined,
 				selectorIds: undefined,
@@ -131,14 +131,14 @@ sap.ui.define([
 				changeTypes: undefined,
 				somethingNotNecessary: "somethingNotNecessary"
 			};
-			var sExpectedUrl = "/flex/personalization/v1/changes/?reference=reference&appVersion=1.0.1";
+			var sExpectedUrl = "/flexPersonalization/flex/personalization/v1/changes/?reference=reference&appVersion=1.0.1";
 			var sExpectedMethod = "DELETE";
 
 			var oStubSendRequest = sandbox.stub(ApplyUtils, "sendRequest").resolves({});
 			var oSpyGetUrl = sandbox.spy(ApplyUtils, "getUrl");
 
 			return WritePersonalizationConnector.reset(mPropertyBag).then(function() {
-				assert.equal(oSpyGetUrl.getCall(0).args[0], "/v1/changes/", "with correct route path");
+				assert.equal(oSpyGetUrl.getCall(0).args[0], "/flex/personalization/v1/changes/", "with correct route path");
 				assert.equal(oSpyGetUrl.getCall(0).args[1], mPropertyBag, "with correct property bag");
 				assert.ok(oStubSendRequest.calledOnce, "sendRequest is called once");
 				assert.equal(oStubSendRequest.getCall(0).args[0], sExpectedUrl, "with correct url");
