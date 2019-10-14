d2f07ff3a4c5c213790330400311d4c09cbab675
Diana <diana.pazheva@sap.com>
2019-10-14 14:18:51 +0300

    [FIX] sap.uxap.ObjectPageLayout: Prevented iOS scroll issue on overflow change
    Background:
    --The _obtainSnappedTitleHeight and _obtainExpandedTitleHeight
    temporarily append a clone of the title element to the DOM
    (to measure its height in its alternative state)
    --When the above is executed, the scrolling of the parent element is
    temporarily disabled, to prevent unwanted scrollbar *only because*
    of this temporary appending of another child
    
    Problem:
    On iOS 13, the temporary disabling of scrolling of the parent element
    has a side-effect: when we restore scrolling, *the browser automatically
    scrolls the content back to top* (snippix 49160)
    
    Solution:
    Instead of disabling the scrolling of the parent element
    (the one to which we append the clone), we can instead change
    the positioning of the clone to 'absolute' to take it out
    of the flow => it will not change its parent's layout
    
    BCP: 002075129400005875712019
    Snippix: 49160
    (cherry picked from commit fd901bb875796908a14682f1b30165aada8790e7)
    CR-Id: 002075125900005327322019
    
    Change-Id: I9734d82921055b70ee7feded0cf275a5e7ddfb94

--- src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -3055,7 +3055,7 @@ sap.ui.define([
 
 		var $headerTitleClone = this._$headerTitle.clone();
 		//prepare: make sure it won't be visible ever and fix width to the original headerTitle which is 100%
-		$headerTitleClone.css({left: "-10000px", top: "-10000px", width: this._$headerTitle.width() + "px"});
+		$headerTitleClone.css({left: "-10000px", top: "-10000px", width: this._$headerTitle.width() + "px", position:"absolute"});
 		$headerTitleClone.toggleClass("sapUxAPObjectPageHeaderStickied", bEnableStickyMode);
 		$headerTitleClone.appendTo(this._$headerTitle.parent());
 
@@ -3099,13 +3099,9 @@ sap.ui.define([
 			iHeight;
 
 		if (bViaClone) {
-			// BCP: 1870298358 - setting overflow-y to hidden of the wrapper element during clone to eliminate unwanted
-			// scrollbar appearing during measurement of cloned header
-			this._$opWrapper.css("overflow-y", "hidden");
 			$Clone = this._appendTitleCloneToDOM(false /* disable snapped mode */);
 			iHeight = $Clone.is(":visible") ? $Clone.height() - this.iAnchorBarHeight : 0;
 			$Clone.remove(); //clean dom
-			this._$opWrapper.css("overflow-y", "auto");
 		} else if (oTitle && oTitle.unSnap) {
 			oTitle.unSnap(false);
 			iHeight = oTitle.$().outerHeight();
