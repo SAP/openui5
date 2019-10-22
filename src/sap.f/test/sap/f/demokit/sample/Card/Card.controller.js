b27d54bfd340d2ffa2a59ecb2b9e119fc7e53363
Konstantin Kondov <konstantin.kondov@sap.com>
2019-10-22 17:17:20 +0300

    [INTERNAL] sap.f.Card: Evo recommendations applied on sample
    Change-Id: I33be43b260922a531b68e2e18d569b352057af2b
    JIRA: BGSOFUIRODOPI-2143

--- src/sap.f/test/sap/f/demokit/sample/Card/Card.controller.js
+++ src/sap.f/test/sap/f/demokit/sample/Card/Card.controller.js
@@ -7,62 +7,11 @@ sap.ui.define([
 
 	return Controller.extend("sap.f.sample.Card.Card", {
 		onInit: function () {
-			var oModel = new JSONModel({
-				"cities": [
-					{
-						"text": "Berlin",
-						"key": "BR"
-					},
-					{
-						"text": "London",
-						"key": "LN"
-					},
-					{
-						"text": "Madrid",
-						"key": "MD"
-					},
-					{
-						"text": "Prague",
-						"key": "PR"
-					},
-					{
-						"text": "Paris",
-						"key": "PS"
-					},
-					{
-						"text": "Sofia",
-						"key": "SF"
-					},
-					{
-						"text": "Vienna",
-						"key": "VN"
-					}
-				],
-				"productItems": [
-					{
-						"title": "Notebook HT",
-						"subtitle": "ID23452256-D44",
-						"revenue": "27.25K EUR",
-						"status": "success",
-						"statusSchema": "Success"
-					},
-					{
-						"title": "Notebook XT",
-						"subtitle": "ID27852256-D47",
-						"revenue": "7.35K EUR",
-						"status": "exceeded",
-						"statusSchema": "Error"
-					},
-					{
-						"title": "Notebook ST",
-						"subtitle": "ID123555587-I05",
-						"revenue": "22.89K EUR",
-						"status": "warning",
-						"statusSchema": "Warning"
-					}
-				]
-			});
-			this.getView().setModel(oModel);
+			var oCitiesModel = new JSONModel(sap.ui.require.toUrl("sap/f/sample/Card/model/cities.json")),
+				oProductsModel =  new JSONModel(sap.ui.require.toUrl("sap/f/sample/Card/model/products.json"));
+
+			this.getView().setModel(oCitiesModel, "cities");
+			this.getView().setModel(oProductsModel, "products");
 		},
 		onBookPress: function() {
 			MessageToast.show(
