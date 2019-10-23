f5aa17b5042f951b879c69aa6811dc0d8a2b9999
Stefan Blamberg <stefan.blamberg@sap.com>
2019-10-23 11:38:37 +0200

    [INTERNAL] sap.ui.integration: Binding string support for numerical and boolean editors
    Change-Id: Iaafb4de1c66ef3b1ea733704b13a21643e64d51a
    JIRA: UICSFLEX-5997

--- src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/propertyEditor/numberEditor/NumberEditor.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/propertyEditor/numberEditor/NumberEditor.js
@@ -4,21 +4,23 @@
 sap.ui.define([
 	"sap/ui/integration/designtime/baseEditor/propertyEditor/BasePropertyEditor",
 	"sap/m/Input",
-	"sap/m/InputType"
+	"sap/ui/base/BindingParser",
+	"sap/ui/core/format/NumberFormat"
 ], function (
 	BasePropertyEditor,
 	Input,
-	InputType
+	BindingParser,
+	NumberFormat
 ) {
 	"use strict";
 
 	/**
 	 * @class
 	 * Constructor for a new <code>NumberEditor</code>.
-	 * This allows to set numeric values for a specified property of a JSON object.
-	 * The editor is rendered as a {@link sap.m.Input} of type {@link sap.m.InputType.Number}, which prevents non-numeric user input.
+	 * This allows to set numeric values or binding paths for a specified property of a JSON object.
+	 * The editor is rendered as a {@link sap.m.Input}, which prevents non-numeric user input unless it is a valid binding path.
 	 * To get notified about changes made with the editor, you can use the <code>attachPropertyChanged</code> method,
-	 * which passes the current property value as a float to the provided callback function when the state changes.
+	 * which passes the current property value as a number or binding string to the provided callback function when the state changes.
 	 *
 	 * @extends sap.ui.integration.designtime.baseEditor.propertyEditor.BasePropertyEditor
 	 * @alias sap.ui.integration.designtime.baseEditor.propertyEditor.numberEditor.NumberEditor
@@ -33,12 +35,29 @@ sap.ui.define([
 	var NumberEditor = BasePropertyEditor.extend("sap.ui.integration.designtime.baseEditor.propertyEditor.numberEditor.NumberEditor", {
 		constructor: function() {
 			BasePropertyEditor.prototype.constructor.apply(this, arguments);
-			this._oInput = new Input({value: "{value}", type: InputType.Number});
+			this._oInput = new Input({value: "{value}"});
 			this._oInput.attachLiveChange(function(oEvent) {
-				var nInput = parseFloat(this._oInput.getValue());
-				this.firePropertyChanged(nInput);
+				var sInput = this._validate(this._oInput.getValue());
+				if (sInput !== null) {
+					this.firePropertyChanged(sInput);
+				}
 			}, this);
 			this.addContent(this._oInput);
+		},
+		_validate: function(sValue) {
+			try {
+				var oParsed = BindingParser.complexParser(sValue);
+				var nValue = NumberFormat.getFloatInstance().parse(sValue);
+				if (!oParsed && sValue && isNaN(nValue)) {
+					throw "NaN";
+				}
+				this._oInput.setValueState("None");
+				return oParsed || !sValue ? sValue : nValue;
+			} catch (vError) {
+				this._oInput.setValueState("Error");
+				this._oInput.setValueStateText(this.getI18nProperty("BASE_EDITOR.NUMBER.INVALID_BINDING_OR_NUMBER"));
+				return null;
+			}
 		},
 		renderer: BasePropertyEditor.getMetadata().getRenderer().render
 	});
