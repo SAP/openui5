b9c5c8bfd121c88833832b14217417db26d424e6
Janik <janik.koeppel@sap.com>
2019-10-24 10:23:47 +0200

    [INTERNAL] Form: fix for OpaTest timing problems
    Change-Id: I214b78dc6bfdf8e0a5cd2c3972a8707ad573d877

--- src/sap.ui.layout/test/sap/ui/layout/demokit/sample/tests/formTests.js
+++ src/sap.ui.layout/test/sap/ui/layout/demokit/sample/tests/formTests.js
@@ -1,79 +1,80 @@
-/*global QUnit*/
+/* global QUnit */
 QUnit.config.autostart = false;
 
-sap.ui.define([
-	"sap/ui/test/Opa5",
-	"sap/ui/test/opaQunit",
-	"./actions/formTests",
-	"./assertions/formTests"
-], function (Opa5, opaTest, Actions, Assertions) {
+sap.ui.getCore().attachInit(function () {
 	"use strict";
 
-	var sSampleName = document.querySelector("[data-sample-component]").dataset.sampleComponent;
+	sap.ui.require([
+		"sap/ui/test/Opa5",
+		"sap/ui/test/opaQunit",
+		"sap/ui/layout/sample/tests/actions/formTests",
+		"sap/ui/layout/sample/tests/assertions/formTests"
+	], function (Opa5, opaTest, Actions, Assertions) {
+		var sSampleName = document.querySelector("[data-sample-component]").dataset.sampleComponent;
 
-	Opa5.extendConfig({
-		viewNamespace : sSampleName + ".",
-		arrangements : new Opa5({
-			iStartTheFormSample : function () {
-				return this.iStartMyUIComponent({
-					componentConfig: {
-						name: sSampleName,
-						manifest: true
-					}
-				});
-			}
-		}),
-		autoWait: true,
-		viewName : "Page",
-		actions : Actions,
-		assertions: Assertions
+		Opa5.extendConfig({
+			viewNamespace: sSampleName + ".",
+			arrangements: new Opa5({
+				iStartTheFormSample: function () {
+					return this.iStartMyUIComponent({
+						componentConfig: {
+							name: sSampleName,
+							manifest: true
+						}
+					});
+				}
+			}),
+			autoWait: true,
+			viewName: "Page",
+			actions: Actions,
+			assertions: Assertions
+		});
+
+		QUnit.module("EditSave");
+
+		opaTest("Should go to the edit page", function (Given, When, Then) {
+			// Arrange
+			Given.iStartTheFormSample();
+
+			// Act
+			When.iPressOnEdit();
+
+			// Assert
+			Then.iShouldBeOnTheEditPage();
+		});
+
+		opaTest("Should persist the values", function (Given, When, Then) {
+			// Act
+			When.iChangeValuesInTheForm().
+				and.iPressOnSave();
+
+			// Assert
+			Then.theValuesShouldBePersisted().
+				and.iTeardownMyUIComponent();
+		});
+
+		QUnit.module("EditCancel");
+
+		opaTest("Should go to the edit page", function (Given, When, Then) {
+			// Arrange
+			Given.iStartTheFormSample();
+
+			// Act
+			When.iPressOnEdit();
+
+			// Assert
+			Then.iShouldBeOnTheEditPage();
+		});
+
+		opaTest("Should restore the values", function (Given, When, Then) {
+			// Act
+			When.iChangeValuesInTheForm().
+				and.iPressOnCancel();
+
+			// Assert
+			Then.theValuesShouldNotBePersisted().
+				and.iTeardownMyUIComponent();
+		});
+		QUnit.start();
 	});
-
-	QUnit.module("EditSave");
-
-	opaTest("Should go to the edit page", function(Given, When, Then) {
-		// Arrange
-		Given.iStartTheFormSample();
-
-		// Act
-		When.iPressOnEdit();
-
-		// Assert
-		Then.iShouldBeOnTheEditPage();
-	});
-
-	opaTest("Should persist the values", function(Given, When, Then) {
-		// Act
-		When.iChangeValuesInTheForm().
-			and.iPressOnSave();
-
-		// Assert
-		Then.theValuesShouldBePersisted().
-			and.iTeardownMyUIComponent();
-	});
-
-	QUnit.module("EditCancel");
-
-	opaTest("Should go to the edit page", function(Given, When, Then) {
-		// Arrange
-		Given.iStartTheFormSample();
-
-		// Act
-		When.iPressOnEdit();
-
-		// Assert
-		Then.iShouldBeOnTheEditPage();
-	});
-
-	opaTest("Should restore the values", function(Given, When, Then) {
-		// Act
-		When.iChangeValuesInTheForm().
-			and.iPressOnCancel();
-
-		// Assert
-		Then.theValuesShouldNotBePersisted().
-			and.iTeardownMyUIComponent();
-	});
-
-	QUnit.start();
 });
