c9c85f4acac0a97f2f4ade59c292301ff880613a
Sven Bernhard <sven.bernhard@sap.com>
2019-11-04 14:33:25 +0100

    [INTERNAL] test.sap.ui.core.qunit.internal.1Ring.qunit: Moved tests to 1RingModels.qunit
    Tests are now included in 1RingModels.qunit
    
    Change-Id: I69bf9e188fbbf2ef831010a5348ce044d77d3644
    JIRA: CPOUI5MODELS-16

--- src/sap.ui.core/test/sap/ui/core/qunit/internal/1Ring.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/internal/1Ring.qunit.js
@@ -17,37 +17,9 @@ sap.ui.require([
 	sap.ui.require([
 		// alphabetic sort order according to module names
 		// base
-		"sap/ui/core/qunit/BindingParser.qunit",
-		"sap/ui/core/qunit/ExpressionParser.qunit",
 		"sap/ui/core/qunit/util/SyncPromise.qunit",
 		// core
 		"sap/ui/core/qunit/util/XMLPreprocessor.qunit",
-		// OData types
-		"sap/ui/core/qunit/odata/type/Boolean.qunit",
-		"sap/ui/core/qunit/odata/type/Currency.qunit",
-		"sap/ui/core/qunit/odata/type/Date.qunit",
-		"sap/ui/core/qunit/odata/type/DateTimeBase.qunit",
-		"sap/ui/core/qunit/odata/type/Decimal.qunit",
-		"sap/ui/core/qunit/odata/type/Double.qunit",
-		"sap/ui/core/qunit/odata/type/Guid.qunit",
-		"sap/ui/core/qunit/odata/type/Int.qunit",
-		"sap/ui/core/qunit/odata/type/Int64.qunit",
-		"sap/ui/core/qunit/odata/type/ODataType.qunit",
-		"sap/ui/core/qunit/odata/type/Raw.qunit",
-		"sap/ui/core/qunit/odata/type/Single.qunit",
-		"sap/ui/core/qunit/odata/type/Stream.qunit",
-		"sap/ui/core/qunit/odata/type/String.qunit",
-		"sap/ui/core/qunit/odata/type/Time.qunit",
-		"sap/ui/core/qunit/odata/type/TimeOfDay.qunit",
-		"sap/ui/core/qunit/odata/type/Unit.qunit",
-		"sap/ui/core/qunit/odata/type/UnitMixin.qunit",
-		// OData V2
-		// Note: some types use lazy loading and are used by AnnotationHelper tests!
-		"sap/ui/core/qunit/odata/AnnotationHelper.qunit",
-		"sap/ui/core/qunit/odata/ODataMetaModel.qunit",
-		"sap/ui/core/qunit/odata/_AnnotationHelperBasics.qunit",
-		"sap/ui/core/qunit/odata/_AnnotationHelperExpression.qunit",
-		"sap/ui/core/qunit/odata/_ODataMetaModelUtils.qunit",
 		// OData V4
 		"sap/ui/core/qunit/internal/ODataV4.qunit",
 		// test
