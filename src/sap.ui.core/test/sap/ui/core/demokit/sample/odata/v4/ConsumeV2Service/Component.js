bee44f80ec44533a1886c56c91c972e838df0ef2
D057216 <silke.spang@sap.com>
2019-11-27 12:26:58 +0100

    [INTERNAL] sap.ui.core.sample.*: use SandboxModel in all apps owned by CA-UI5-COR-OD4
    PS1: First draft for ConsumeV2Service, ListBinding, SalesOrdersTemplate,
         SalesOrderTP100_V2, SalesOrderTP100_V4
    PS2-3: Review comments
    PS4: Rebase for version change
    PS5-6: Review comments
    PS7: Remove unused reference ODataModel in SalesOrders
    PS8: Review comments
    
    JIRA: CPOUI5ODATAV4-31
    Change-Id: Ib11272c5534cba5e4abef8c3f79b6828aa0989cb

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ConsumeV2Service/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ConsumeV2Service/Component.js
@@ -8,71 +8,13 @@
  * @version @version@
  */
 sap.ui.define([
-	"sap/base/util/UriParameters",
-	"sap/ui/core/library",
-	"sap/ui/core/mvc/View", // sap.ui.view()
-	"sap/ui/core/sample/common/Component",
-	"sap/ui/model/odata/OperationMode",
-	"sap/ui/model/odata/v4/ODataModel",
-	"sap/ui/test/TestUtils"
-], function (UriParameters, library, View, BaseComponent, OperationMode, ODataModel, TestUtils) {
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
 	"use strict";
 
-	// shortcut for sap.ui.core.mvc.ViewType
-	var ViewType = library.mvc.ViewType;
-
-	return BaseComponent.extend("sap.ui.core.sample.odata.v4.ConsumeV2Service.Component", {
+	return UIComponent.extend("sap.ui.core.sample.odata.v4.ConsumeV2Service.Component", {
 		metadata : {
 			manifest : "json"
-		},
-
-		createContent : function () {
-			var sGroupId = UriParameters.fromQuery(window.location.search).get("$direct")
-					? "$direct" // switch off batch
-					: undefined,
-				bHasOwnProxy = this.proxy !== BaseComponent.prototype.proxy,
-				oModel = this.getModel(),
-				fnProxy = bHasOwnProxy
-					? this.proxy
-					: TestUtils.proxy,
-				sServiceUrl = fnProxy(oModel.sServiceUrl);
-
-			if (oModel.sServiceUrl !== sServiceUrl || sGroupId) {
-				//replace model from manifest in case of proxy
-				oModel.destroy();
-				oModel = new ODataModel({
-					autoExpandSelect : true,
-					groupId : sGroupId,
-					odataVersion : "2.0",
-					operationMode : OperationMode.Server,
-					serviceUrl : sServiceUrl,
-					synchronizationMode : "None"
-				});
-				this.setModel(oModel);
-			}
-
-			if (!bHasOwnProxy) {
-				TestUtils.setupODataV4Server(this.oSandbox, {
-						"$metadata" : {
-							source : "metadataV2.xml"
-						},
-						"EdmTypesCollection?$select=Boolean,Byte,GlobalUID,ID,Int16,Int32,SByte,String&$skip=0&$top=100" : {
-							source : "EdmTypesV2.json"
-						},
-						"EdmTypesCollection('1')?$select=Binary,Boolean,Byte,Date,DateTime,DateTimeOffset,Decimal,Double,Float,GlobalUID,ID,Int16,Int32,Int64,SByte,Single,String,Time" : {
-							source : "EdmTypesV2_SingleEntity.json"
-						}
-					}, "sap/ui/core/sample/odata/v4/ConsumeV2Service/data",
-					"/sap/opu/odata/sap/ZUI5_EDM_TYPES/");
-			}
-
-			return sap.ui.view({
-				async : true,
-				id : "sap.ui.core.sample.odata.v4.ConsumeV2Service.Main",
-				models : oModel,
-				type : ViewType.XML,
-				viewName : "sap.ui.core.sample.odata.v4.ConsumeV2Service.Main"
-			});
 		}
 	});
 });
