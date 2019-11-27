9e3cff7446f932193fe9063d9fdea1def9c19d46
i850991 <arnaud.buchholz@sap.com>
2019-11-27 17:57:19 -0500

    [INTERNAL] sap.ui.fl Updates of AddIFrame change handler - Changes the unit tests to reflect reality (there was a confusion in the meaning of completeChangeContent) - Enforces reusability by exposing _getTargetAggregationIndex
    Change-Id: I23349558eb5aa2fc4b78bf954059791749cf4bfc

--- src/sap.ui.fl/src/sap/ui/fl/changeHandler/AddIFrame.js
+++ src/sap.ui.fl/src/sap/ui/fl/changeHandler/AddIFrame.js
@@ -3,9 +3,13 @@
  */
 
 sap.ui.define([
-	"sap/ui/fl/changeHandler/common/revertAddedControls"
+	"sap/ui/fl/changeHandler/common/revertAddedControls",
+	"sap/ui/fl/changeHandler/common/getTargetAggregationIndex",
+	"sap/ui/fl/changeHandler/common/createIFrame"
 ], function(
-	revertAddedControls
+	revertAddedControls,
+	getTargetAggregationIndex,
+	createIFrame
 ) {
 	"use strict";
 
@@ -19,27 +23,6 @@ sap.ui.define([
 	 * @private
 	 */
 	var AddIFrame = {};
-
-	/**
-	 * Add the IFrame control to the target control within the target aggregation.
-	 *
-	 * @param {sap.ui.fl.Change} oChange Change object with instructions to be applied on the control map
-	 * @param {object} mPropertyBag Map of properties
-	 * @param {object} mPropertyBag.modifier Modifier for the controls
-	 * @ui5-restricted sap.ui.fl
-	 */
-	AddIFrame._createIFrame = function (oChange, mPropertyBag, sIFrameId) {
-		var oModifier = mPropertyBag.modifier;
-		var oChangeDefinition = oChange.getDefinition();
-		var oView = mPropertyBag.view;
-		var oComponent = mPropertyBag.appComponent;
-		var mIFrameSettings = {};
-		["url", "width", "height"].forEach(function (sIFrameProperty) {
-			mIFrameSettings[sIFrameProperty] = oChangeDefinition.content[sIFrameProperty];
-		});
-		var oIFrame = oModifier.createControl("sap.ui.fl.util.IFrame", oComponent, oView, sIFrameId, mIFrameSettings, false);
-		return oIFrame;
-	};
 
 	/**
 	 * Add the IFrame control to the target control within the target aggregation.
@@ -60,12 +43,8 @@ sap.ui.define([
 			throw new Error("The given Aggregation is not available in the given control: " + oModifier.getId(oControl));
 		}
 		var sIFrameId = "iframe_" + oChangeDefinition.content.baseId;
-		var iIndex = oChangeDefinition.content.index;
-		if (undefined === iIndex) {
-			var aAggregationContent = oModifier.getAggregation(oControl, sAggregationName);
-			iIndex = aAggregationContent.length /* last by default */;
-		}
-		var oIFrame = AddIFrame._createIFrame(oChange, mPropertyBag, sIFrameId);
+		var iIndex = getTargetAggregationIndex(oChange, oControl, mPropertyBag);
+		var oIFrame = createIFrame(oChange, mPropertyBag, sIFrameId);
 		oModifier.insertAggregation(oControl, sAggregationName, oIFrame, iIndex, oView);
 		oChange.setRevertData([oModifier.getId(oIFrame)]);
 	};
@@ -85,23 +64,24 @@ sap.ui.define([
 	 * Completes the change by adding change handler specific content.
 	 *
 	 * @param {sap.ui.fl.Change} oChange Change object to be completed
-	 * @param {object} oSpecificChangeInfo Must contain UI extension settings
-	 * @param {string} oSpecificChangeInfo.targetAggregation Aggregation to add the extension to
-	 * @param {string} oSpecificChangeInfo.baseId Base ID to allocate controls
-	 * @param {string} oSpecificChangeInfo.width IFrame Width
-	 * @param {string} oSpecificChangeInfo.height IFrame Height
-	 * @param {string} oSpecificChangeInfo.url IFrame Url
+	 * @param {object} oSpecificChangeInfo Specific change information
+	 * @param {object} oSpecificChangeInfo.content Must contain UI extension settings
+	 * @param {string} oSpecificChangeInfo.content.targetAggregation Aggregation to add the extension to
+	 * @param {string} oSpecificChangeInfo.content.baseId Base ID to allocate controls
+	 * @param {string} [oSpecificChangeInfo.content.width] IFrame Width
+	 * @param {string} [oSpecificChangeInfo.content.height] IFrame Height
+	 * @param {string} oSpecificChangeInfo.content.url IFrame Url
 	 * @ui5-restricted sap.ui.fl
 	 */
 	AddIFrame.completeChangeContent = function (oChange, oSpecificChangeInfo) {
 		var oChangeJson = oChange.getDefinition();
 		// Required settings
 		["targetAggregation", "baseId", "url"].forEach(function (sRequiredProperty) {
-			if (!Object.prototype.hasOwnProperty.call(oSpecificChangeInfo, sRequiredProperty)) {
+			if (!Object.prototype.hasOwnProperty.call(oSpecificChangeInfo.content, sRequiredProperty)) {
 				throw new Error("Attribute missing from the change specific content '" + sRequiredProperty + "'");
 			}
 		});
-		oChangeJson.content = Object.assign({}, oSpecificChangeInfo);
+		oChangeJson.content = Object.assign(oChangeJson.content || {}, oSpecificChangeInfo.content);
 	};
 
 	return AddIFrame;
