cdfcc43e8c457a35bcc57f31bee0a5ecd41df189
Irina Goetzenberger <irina.goetzenberger@sap.com>
2019-12-04 15:27:21 +0100

    [INTERNAL] m.ColumnHeaderPopover: refactoring and width adjustment.
    Refactoring, width adjustment and stability of the code. Remove data binding on toolbar content.
    
    Change-Id: I6b88bf3dee4e0f21346d416f1cab5a35770d54be

--- src/sap.m/src/sap/m/ColumnPopoverItem.js
+++ src/sap.m/src/sap/m/ColumnPopoverItem.js
@@ -23,15 +23,16 @@ sap.ui.define(['sap/ui/core/Element'], function(Element) {
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	var ColumnPopoverItem = Element.extend("sap.m.ColumnPopoverItem", /** @lends sap.m.ColumnPopoverItem.prototype */
-	{
-		metadata : {
-			library: "sap.m",
-			properties: {
-				visible : { type : "boolean",  group : "Misc", defaultValue : true }
+		{
+			metadata: {
+				library: "sap.m",
+				properties: {
+					visible: {type: "boolean", group: "Misc", defaultValue: true}
+				}
 			}
-		}
-
-	});
-
+		});
+		ColumnPopoverItem.prototype._createButton = function() {
+			throw new Error('Abstract function _create Button must be implemented in your Item (that extends this abstract object).');
+		};
 	return ColumnPopoverItem;
 });
