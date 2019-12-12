6e1030e213ba2efcf7541c2ef789e7a2e0108a88
Nia Peeva <nia.peeva@sap.com>
2019-12-12 14:32:58 +0200

    [INTERNAL] sap.f.cards.AdaptiveContent: UI5InputDate implementation
    Change-Id: I2a66d476ca0ca14cbdfc61f0bb8ae1b6ed86885c

--- src/sap.f/src/sap/f/cards/adaptivecards/elements/UI5InputDate.js
+++ src/sap.f/src/sap/f/cards/adaptivecards/elements/UI5InputDate.js
@@ -4,15 +4,27 @@
 sap.ui.define(["sap/ui/integration/thirdparty/adaptivecards"], function (AdaptiveCards) {
 	"use strict";
 	function UI5InputDate() {
-		AdaptiveCards.NumberInput.apply(this, arguments);
+		AdaptiveCards.DateInput.apply(this, arguments);
 	}
-
+	/**
+	 * Constructor for a new <code>UI5InputDate</code>.
+	 *
+	 * @class
+	 * An object that overwrites Microsoft's Adaptive Card <code>Input.Date</code> element by replacing it with
+	 * <code>ui5-datepicker</code> web component.
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @private
+	 * @since 1.74
+	 */
 	UI5InputDate.prototype = Object.create(AdaptiveCards.DateInput.prototype);
 	UI5InputDate.prototype.internalRender = function () {
 		this._dateInputElement = document.createElement("ui5-datepicker");
 
 		this._dateInputElement.id = this.id;
-		this._dateInputElement.placeholder = this.placeholder || "";
+		this._dateInputElement.placeholder = this.placeholder;
 		this._dateInputElement.formatPattern = "yyyy-MM-dd";
 		this._dateInputElement.value = this.defaultValue || "";
 
