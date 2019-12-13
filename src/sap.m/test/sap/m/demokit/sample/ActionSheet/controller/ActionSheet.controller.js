ed630793e2f3618ae0d8f1f3fafd1191318b1be2
boghyon <boghyon+github@gmail.com>
2019-12-13 17:55:32 +0100

    [INTERNAL] sap.m.ActionSheet: Updated Demo Kit sample
    The updated Demo Kit sample now reflects the latest Evo recommendations for developing UI5 applications.
    
    - Dot prefix at the event handler name as an indicator for controller member
    - Reduced dependency to controller by
      + Adding fragment directly to the `dependents` aggregation in the view
      + Requiring Modules directly in the fragment
    - Removed dependency to unused control library ("sap.ui.layout")
    
    Change-Id: I8bef88870fccbc9735773662906084e73d25bcab
    Closes https://github.com/SAP/openui5/pull/2756

--- src/sap.m/test/sap/m/demokit/sample/ActionSheet/controller/ActionSheet.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/ActionSheet/controller/ActionSheet.controller.js
@@ -1,25 +1,12 @@
-sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/m/MessageToast'],
-	function(Controller, MessageToast) {
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
 	"use strict";
 
 	return Controller.extend("sap.m.sample.ActionSheet.controller.ActionSheet", {
-		handleOpen : function (oEvent) {
+		onButtonPress: function(oEvent) {
 			var oButton = oEvent.getSource();
-
-			// create action sheet only once
-			if (!this._actionSheet) {
-				this._actionSheet = sap.ui.xmlfragment(
-					"sap.m.sample.ActionSheet.view.ActionSheet",
-					this
-				);
-				this.getView().addDependent(this._actionSheet);
-			}
-
-			this._actionSheet.openBy(oButton);
-		},
-
-		actionSelected : function(oEvent){
-			MessageToast.show("Selected action is '" + oEvent.getSource().getText() + "'");
+			this.byId("actionSheet").openBy(oButton);
 		}
 	});
 });
