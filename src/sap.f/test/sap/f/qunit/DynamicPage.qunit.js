5dc6abd7cfceeee5f14e9978de9baa860c403f5b
Diana <diana.pazheva@sap.com>
2019-12-13 16:36:51 +0200

    [FIX] sap.f.DynamicPage: Deregister resizeListener before rendering
    Background:
    DynamicPage registers a resizeListener to monitor the sizes
    of its children => adjusts its layout if needed
    
    Problem:
    The resizeListener can be called during re-rendering,
    in the time interval between the (1) onBeforeRendering and
    (2) onAfterRendering hooks (see the qunit and snippix 71071).
    When the resizeListener is called at that time, any cached
    DOM references point to elements that are already removed from
    DOM => invalid layout adjustment are performed
    
    Solution:
    Deregister the resizeListener onBeforeRendering
    
    Change-Id: Ib41d4e7867ccda7882f33ed6bc6fde7f60b1758b
    BCP: 1980469448

--- src/sap.f/test/sap/f/qunit/DynamicPage.qunit.js
+++ src/sap.f/test/sap/f/qunit/DynamicPage.qunit.js
@@ -14,6 +14,8 @@ sap.ui.define([
 	"sap/m/library",
 	"sap/f/DynamicPageAccessibleLandmarkInfo",
 	"sap/ui/core/mvc/XMLView",
+	'sap/ui/core/Control',
+	'sap/ui/core/IntervalTrigger',
 	"sap/ui/qunit/QUnitUtils",
 	"sap/ui/events/KeyCodes"
 ],
@@ -32,6 +34,8 @@ function (
 	mLibrary,
 	DynamicPageAccessibleLandmarkInfo,
 	XMLView,
+	Control,
+	IntervalTrigger,
 	QUnitUtils,
 	KeyCodes
 ) {
@@ -415,6 +419,60 @@ function (
 
 		// assert
 		assert.ok($pinButton.hasClass("sapUiHidden"), "The DynamicPage Header Pin Button is hidden");
+	});
+
+	QUnit.module("DynamicPage - Rendering lifecycle", {
+		beforeEach: function () {
+			this.oDynamicPage = oFactory.getDynamicPageWithPreserveHeaderOnScroll();
+		},
+		afterEach: function () {
+			this.oDynamicPage.destroy();
+			this.oDynamicPage = null;
+		}
+	});
+
+	QUnit.test("resizeListener is not called before the control is rerendered", function (assert) {
+
+		var oDynamicPage = this.oDynamicPage,
+			oSpy = sinon.spy(this.oDynamicPage, "_onChildControlsHeightChange"),
+			iHeightBeforeResize,
+			oDummyControl,
+			done = assert.async(),
+			DummyControl = Control.extend("sap.m.DummyControl", {
+				renderer: function(oRm) {
+					oRm.write("<div></div>");
+				}
+			});
+
+		DummyControl.prototype.onAfterRendering = function() {
+			// will cause the <code>ResizeHandler.prototype.checkSizes<code>
+			// to be executed *synchronously*
+			IntervalTrigger.addListener(function() {
+				// content non important
+			});
+		};
+
+		this.oDynamicPage.addEventDelegate({
+			"onAfterRendering": function() {
+				iHeightBeforeResize = oDynamicPage.getDomRef().offsetHeight;
+				oDummyControl = new DummyControl();
+				oDummyControl.addEventDelegate({
+					"onAfterRendering": function() {
+						// assert
+						assert.strictEqual(oSpy.callCount, 0, "dynamicPage resize listener not called");
+						done();
+					}
+				});
+
+				oDynamicPage.getHeader().addContent(oDummyControl);
+				oDynamicPage.getDomRef().style.height = (iHeightBeforeResize / 2) + "px";
+				oSpy.reset();
+				oDynamicPage.invalidate();
+				oDynamicPage.rerender();
+				oDynamicPage.removeEventDelegate(this);
+			}
+		});
+		this.oDynamicPage.placeAt("qunit-fixture");
 	});
 
 	QUnit.module("DynamicPage - Rendering - No Header", {
