daa8b8dc9c692eb83aa4b1859a25757fcd902329
i350970 <plamen.ivanov01@sap.com>
2019-12-23 11:55:10 +0200

    [INTERNAL] sap.m.OverflowToolbarButton: Enabled Remove and Reveal RTA actions
    Now the sap.m.OverflowToolbarButton is allowed to be removed/revealed
    during design-time adaptation.
    
    Change-Id: I91163e90bdd8312bb458d04e54d599e6c6503bca
    BCP: 002075129400007554632019
    CR-Id: 002075125900006629092019
    (cherry picked from commit 8114a0dc6f4723f6a13e4c25cb2df5fcb3adda5c)

---
+++ src/sap.m/test/sap/m/qunit/designtime/OverflowToolbarButton.qunit.js
@@ -0,0 +1,52 @@
+sap.ui.define([
+	"sap/ui/qunit/utils/createAndAppendDiv",
+	"sap/ui/rta/test/controlEnablingCheck"
+], function (
+	createAndAppendDiv,
+	controlEnablingCheck
+) {
+	"use strict";
+	createAndAppendDiv("content");
+
+	// Remove and reveal actions
+	var fnConfirmOverflowToolbarButtonIsInvisible = function (oUiComponent, oViewAfterAction, assert) {
+		assert.strictEqual(oViewAfterAction.byId("OverflowToolbarButton").getVisible(), false, "then the OverflowToolbarButton element is invisible");
+	};
+
+	var fnConfirmOverflowToolbarButtonIsVisible = function (oUiComponent, oViewAfterAction, assert) {
+		assert.strictEqual(oViewAfterAction.byId("OverflowToolbarButton").getVisible(), true, "then the OverflowToolbarButton element is visible");
+	};
+
+	controlEnablingCheck("Checking the remove action for OverflowToolbarButton", {
+		xmlView:
+			'<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m">"' +
+				'<OverflowToolbar>' +
+					'<OverflowToolbarButton text="Option 1" icon="sap-icon://sort" id="OverflowToolbarButton" />' +
+				'</OverflowToolbar>' +
+			'</mvc:View>',
+		action: {
+			name: "remove",
+			controlId: "OverflowToolbarButton"
+		},
+		afterAction: fnConfirmOverflowToolbarButtonIsInvisible,
+		afterUndo: fnConfirmOverflowToolbarButtonIsVisible,
+		afterRedo: fnConfirmOverflowToolbarButtonIsInvisible
+	});
+
+	controlEnablingCheck("Checking the reveal action for a OverflowToolbarButton", {
+		xmlView:
+			'<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns="sap.m">"' +
+				'<OverflowToolbar>' +
+					'<OverflowToolbarButton text="Option 1" icon="sap-icon://sort" id="OverflowToolbarButton" visible="false"/>' +
+				'</OverflowToolbar>' +
+			'</mvc:View>',
+		action: {
+			name: "reveal",
+			controlId: "OverflowToolbarButton"
+		},
+		afterAction: fnConfirmOverflowToolbarButtonIsVisible,
+		afterUndo: fnConfirmOverflowToolbarButtonIsInvisible,
+		afterRedo: fnConfirmOverflowToolbarButtonIsVisible
+	});
+
+});
