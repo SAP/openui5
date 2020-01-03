ba38f1e4e83e5a7efd8822c5b6254cdd8e052cdb
Alexander Ivanov <alexander.ivanov01@sap.com>
2020-01-03 15:47:21 +0200

    [FIX] sap.m.TileContainer: pre-renders tiles in both directions
    CR-Id: 002075125900000793512020
    BCP: 002075129400007904862019
    
    Change-Id: I50cfa3430ff56eb688cce7e1b4613062f6c97690
    (cherry picked from commit c4432cf8a899ccd4c4c49e13f625dea7b374b93c)

--- src/sap.m/src/sap/m/TileContainer.js
+++ src/sap.m/src/sap/m/TileContainer.js
@@ -1136,7 +1136,7 @@ function(
 		aVisibleTiles = aVisibleTiles || this._getVisibleTiles();
 		iIndex = this._indexOfVisibleTile(aAllTiles[iIndex]);//find tile's index amongst visible tiles
 
-		if (!!iIndex && iIndex >= 0) {
+		if (iIndex > -1) {
 			this._renderTilesInTheSamePage(iIndex, aVisibleTiles);
 		}
 
@@ -1678,10 +1678,11 @@ function(
 			}
 			var aVisibleTiles = this._getVisibleTiles();
 
-			var iNextPageStartTileIndex = this._iCurrentTileStartIndex + this._iMaxTiles;
-			var iNextPageEndTileIndex = iNextPageStartTileIndex + this._iMaxTiles - 1;
+			var iDirection = oTouchSession.fDiffX > 0 ? 1 : -1;
+			var iGoToPageStartTileIndex = this._iCurrentTileStartIndex + iDirection * this._iMaxTiles;
+			var iGoToPageEndTileIndex = iGoToPageStartTileIndex + this._iMaxTiles - 1;
 
-			this._renderTiles(aVisibleTiles, iNextPageStartTileIndex, iNextPageEndTileIndex);
+			this._renderTiles(aVisibleTiles, iGoToPageStartTileIndex, iGoToPageEndTileIndex);
 			this._applyTranslate(this.$("cnt"),iNewLeft,0,false);
 		}
 	};
