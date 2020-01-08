7139eab90eba77cb4f777861b5d0d7fe2bd3483b
Stefan Scherer <stefan.scherer@sap.com>
2020-01-08 14:06:36 +0100

    [INTERNAL] sap.ui.model.type.String: cleanup jsdoc
    Fix ESLint warnings, language and formatting errors in jsdoc
    
    PS1: PP SBE/SSC first try
    PS2: review comments
    PS3: review comments PKS
    PS4: review comments
    PS5: rebase
    PS6: review comments LKI
    PS7: review comments PKS
    
    Change-Id: I078473135a2f7290fce3abe3eee3591cb3a48a08
    JIRA: CPOUI5MODELS-44

--- src/sap.ui.core/src/sap/ui/model/type/String.js
+++ src/sap.ui.core/src/sap/ui/model/type/String.js
@@ -4,40 +4,53 @@
 
 // Provides the base implementation for all model implementations
 sap.ui.define([
-	'sap/ui/model/SimpleType',
-	'sap/ui/model/FormatException',
-	'sap/ui/model/ParseException',
-	'sap/ui/model/ValidateException',
+	"sap/base/Log",
+	"sap/ui/model/SimpleType",
+	"sap/ui/model/FormatException",
+	"sap/ui/model/ParseException",
+	"sap/ui/model/ValidateException",
 	"sap/ui/thirdparty/jquery"
 ],
-	function(SimpleType, FormatException, ParseException, ValidateException, jQuery) {
+	function(Log, SimpleType, FormatException, ParseException, ValidateException, jQuery) {
 	"use strict";
 
-
 	/**
-	 * Constructor for a String type.
+	 * Constructor for a <code>String</code> type.
 	 *
 	 * @class
-	 * This class represents string simple types.
+	 * This class represents the string simple type.
 	 *
 	 * @extends sap.ui.model.SimpleType
 	 *
 	 * @author SAP SE
 	 * @version ${version}
 	 *
-	 * @public
-	 * @param {object} [oFormatOptions] formatting options. String doesn't support any formatting options
-	 * @param {object} [oConstraints] value constraints. All given constraints must be fulfilled by a value to be valid
-	 * @param {int} [oConstraints.maxLength] maximum length (in characters) that a string of this value may have
-	 * @param {int} [oConstraints.minLength] minimum length (in characters) that a string of this value may have
-	 * @param {string} [oConstraints.startsWith] a prefix that any valid value must start with
-	 * @param {string} [oConstraints.startsWithIgnoreCase] a prefix that any valid value must start with, ignoring case
-	 * @param {string} [oConstraints.endsWith] a suffix that any valid value must end with
-	 * @param {string} [oConstraints.endsWithIgnoreCase] a suffix that any valid value must end with, ignoring case
-	 * @param {string} [oConstraints.contains] an infix that must be contained in any valid value
-	 * @param {string} [oConstraints.equals] only value that is allowed
-	 * @param {RegExp} [oConstraints.search] a regular expression that the value must match
 	 * @alias sap.ui.model.type.String
+	 * @param {object} [oFormatOptions]
+	 *   Format options; this type ignores them, since it does not support any format options
+	 * @param {object} [oConstraints]
+	 *   Constraints; {@link #validateValue} throws an error if any constraint is
+	 *   violated
+	 * @param {int} [oConstraints.maxLength]
+	 *   The maximal allowed length of the string; unlimited if not defined
+	 * @param {int} [oConstraints.minLength]
+	 *   The minimal allowed length of the string
+	 * @param {string} [oConstraints.startsWith]
+	 *   A prefix that any valid value must start with
+	 * @param {string} [oConstraints.startsWithIgnoreCase]
+	 *   A prefix that any valid value must start with, ignoring case
+	 * @param {string} [oConstraints.endsWith]
+	 *   A suffix that any valid value must end with
+	 * @param {string} [oConstraints.endsWithIgnoreCase]
+	 *   A suffix that any valid value must end with, ignoring case
+	 * @param {string} [oConstraints.contains]
+	 *   An infix that must be contained in any valid value
+	 * @param {string} [oConstraints.equals]
+	 *   The only value that is allowed
+	 * @param {RegExp|string} [oConstraints.search]
+	 *   A regular expression, or a string defining a regular expression, that the value must match
+	 *
+	 * @public
 	 */
 	var StringType = SimpleType.extend("sap.ui.model.type.String", /** @lends sap.ui.model.type.String.prototype */ {
 
@@ -48,14 +61,31 @@ sap.ui.define([
 				this.oConstraints.search = new RegExp(this.oConstraints.search);
 			}
 		}
-
 	});
 
-	StringType.prototype.formatValue = function(sValue, sInternalType) {
+	/**
+	 * Formats the given value to the given target type.
+	 *
+	 * @param {string} sValue
+	 *   The value to be formatted
+	 * @param {string} sTargetType
+	 *   The target type; may be "any", "boolean", "float", "int" or "string", or a type with "any",
+	 *   "boolean", "float", "int" or "string" as its
+	 *   {@link sap.ui.base.DataType#getPrimitiveType primitive type}
+	 * @returns {string|number|boolean}
+	 *   The formatted output value in the target type; <code>undefined</code> and <code>null</code>
+	 *   are always formatted to <code>null</code>
+	 * @throws {sap.ui.model.FormatException}
+	 *   If <code>sTargetType</code> is unsupported or the string cannot be formatted to the target
+	 *   type
+	 *
+	 * @public
+	 */
+	StringType.prototype.formatValue = function (sValue, sTargetType) {
 		if (sValue == undefined || sValue == null) {
 			return null;
 		}
-		switch (this.getPrimitiveType(sInternalType)) {
+		switch (this.getPrimitiveType(sTargetType)) {
 			case "string":
 			case "any":
 				return sValue;
@@ -80,84 +110,114 @@ sap.ui.define([
 				}
 				throw new FormatException(sValue + " is not a valid boolean value");
 			default:
-				throw new FormatException("Don't know how to format String to " + sInternalType);
+				throw new FormatException("Don't know how to format String to " + sTargetType);
 		}
 	};
 
-	StringType.prototype.parseValue = function(oValue, sInternalType) {
-		switch (this.getPrimitiveType(sInternalType)) {
+	/**
+	 * Parses the given value, which is expected to be of the given type, to a string.
+	 *
+	 * @param {string|number|boolean} vValue
+	 *   The value to be parsed
+	 * @param {string} sSourceType
+	 *   The source type (the expected type of <code>vValue</code>); may be "boolean", "float",
+	 *   "int" or "string", or a type with "boolean", "float", "int" or "string" as its
+	 *   {@link sap.ui.base.DataType#getPrimitiveType primitive type}
+	 * @returns {string}
+	 *   The parsed value
+	 * @throws {sap.ui.model.ParseException}
+	 *   If <code>sSourceType</code> is unsupported
+	 *
+	 * @public
+	 */
+	StringType.prototype.parseValue = function (vValue, sSourceType) {
+		switch (this.getPrimitiveType(sSourceType)) {
 			case "string":
-				return oValue;
+				return vValue;
 			case "boolean":
 			case "int":
 			case "float":
-				return oValue.toString();
+				return vValue.toString();
 			default:
-				throw new ParseException("Don't know how to parse String from " + sInternalType);
+				throw new ParseException("Don't know how to parse String from " + sSourceType);
 		}
 	};
 
-	StringType.prototype.validateValue = function(sValue) {
+	/**
+	 * Validates whether the given value in model representation is valid and meets the defined
+	 * constraints, see {@link #constructor}.
+	 *
+	 * @param {string} sValue
+	 *   The value to be validated
+	 * @throws {sap.ui.model.ValidateException}
+	 *   If the value is not valid
+	 *
+	 * @public
+	 */
+	StringType.prototype.validateValue = function (sValue) {
 		if (this.oConstraints) {
 			var oBundle = sap.ui.getCore().getLibraryResourceBundle(),
 				aViolatedConstraints = [],
 				aMessages = [];
-			jQuery.each(this.oConstraints, function(sName, oContent) {
+			jQuery.each(this.oConstraints, function (sName, vConstraint) {
 				switch (sName) {
-					case "maxLength":  // expects int
-						if (sValue.length > oContent) {
+					case "maxLength":
+						if (sValue.length > vConstraint) {
 							aViolatedConstraints.push("maxLength");
-							aMessages.push(oBundle.getText("String.MaxLength", [oContent]));
+							aMessages.push(oBundle.getText("String.MaxLength", [vConstraint]));
 						}
 						break;
-					case "minLength":  // expects int
-						if (sValue.length < oContent) {
+					case "minLength":
+						if (sValue.length < vConstraint) {
 							aViolatedConstraints.push("minLength");
-							aMessages.push(oBundle.getText("String.MinLength", [oContent]));
+							aMessages.push(oBundle.getText("String.MinLength", [vConstraint]));
 						}
 						break;
 					case "startsWith":  // expects string
-						if (!(typeof oContent == "string" && oContent.length > 0 && sValue.startsWith(oContent))) {
+						if (!(typeof vConstraint == "string" && vConstraint.length > 0 && sValue.startsWith(vConstraint))) {
 							aViolatedConstraints.push("startsWith");
-							aMessages.push(oBundle.getText("String.StartsWith", [oContent]));
+							aMessages.push(oBundle.getText("String.StartsWith", [vConstraint]));
 						}
 						break;
 					case "startsWithIgnoreCase":  // expects string
-						if (!((typeof oContent == "string" && oContent != "" ? sValue.toLowerCase().startsWith(oContent.toLowerCase()) : false))) {
+						if (!((typeof vConstraint == "string" && vConstraint != "" ? sValue.toLowerCase().startsWith(vConstraint.toLowerCase()) : false))) {
 							aViolatedConstraints.push("startsWithIgnoreCase");
-							aMessages.push(oBundle.getText("String.StartsWith", [oContent]));
+							aMessages.push(oBundle.getText("String.StartsWith", [vConstraint]));
 						}
 						break;
 					case "endsWith":  // expects string
-						if (!(typeof oContent == "string" && oContent.length > 0 && sValue.endsWith(oContent))) {
+						if (!(typeof vConstraint == "string" && vConstraint.length > 0 && sValue.endsWith(vConstraint))) {
 							aViolatedConstraints.push("endsWith");
-							aMessages.push(oBundle.getText("String.EndsWith", [oContent]));
+							aMessages.push(oBundle.getText("String.EndsWith", [vConstraint]));
 						}
 						break;
 					case "endsWithIgnoreCase": // expects string
-						if (!((typeof oContent == "string" && oContent != "" ? sValue.toLowerCase().endsWith(oContent.toLowerCase()) : false))) {
+						if (!((typeof vConstraint == "string" && vConstraint != "" ? sValue.toLowerCase().endsWith(vConstraint.toLowerCase()) : false))) {
 							aViolatedConstraints.push("endsWithIgnoreCase");
-							aMessages.push(oBundle.getText("String.EndsWith", [oContent]));
+							aMessages.push(oBundle.getText("String.EndsWith", [vConstraint]));
 						}
 						break;
 					case "contains": // expects string
-						if (sValue.indexOf(oContent) == -1) {
+						if (sValue.indexOf(vConstraint) == -1) {
 							aViolatedConstraints.push("contains");
-							aMessages.push(oBundle.getText("String.Contains", [oContent]));
+							aMessages.push(oBundle.getText("String.Contains", [vConstraint]));
 						}
 						break;
 					case "equals": // expects string
-						if (sValue != oContent) {
+						if (sValue != vConstraint) {
 							aViolatedConstraints.push("equals");
-							aMessages.push(oBundle.getText("String.Equals", [oContent]));
+							aMessages.push(oBundle.getText("String.Equals", [vConstraint]));
 						}
 						break;
 					case "search": // expects regex
-						if (sValue.search(oContent) == -1) {
+						if (sValue.search(vConstraint) == -1) {
 							aViolatedConstraints.push("search");
-							aMessages.push(oBundle.getText("String.Search", [oContent]));
+							aMessages.push(oBundle.getText("String.Search", [vConstraint]));
 						}
 						break;
+					default:
+						Log.warning("Ignoring unknown constraint: '" + sName + "'", null,
+							"sap.ui.model.type.String");
 				}
 			});
 			if (aViolatedConstraints.length > 0) {
@@ -166,8 +226,5 @@ sap.ui.define([
 		}
 	};
 
-
-
 	return StringType;
-
 });
