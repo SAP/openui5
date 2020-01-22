950f14a24d243edc1cecdbb1e1e2ab478bcede92
Nia Peeva <nia.peeva@sap.com>
2020-01-22 14:42:16 +0200

    [INTERNAL] sap.f.cards.AdaptiveContent: OPA tests added
    - Action.Submit and Action.OpenUrl are tested via
    OPA tests.
    
    JIRA: BGSOFUIRILA-2681
    
    Change-Id: I9e1156e512df15640f99d163be7d78bc5f3ca932

--- src/sap.f/src/sap/f/cards/adaptivecards/elements/UI5InputTime.js
+++ src/sap.f/src/sap/f/cards/adaptivecards/elements/UI5InputTime.js
@@ -11,13 +11,14 @@ sap.ui.define(["sap/ui/integration/thirdparty/adaptivecards"], function (Adaptiv
 	UI5InputTime.prototype = Object.create(AdaptiveCards.TimeInput.prototype);
 
 	UI5InputTime.prototype.internalRender = function () {
-		var oElement = AdaptiveCards.TimeInput.prototype.internalRender.apply(this, arguments);
+		AdaptiveCards.TimeInput.prototype.internalRender.apply(this, arguments);
 
-		oElement.classList.add("sapMInputBaseInner");
-        oElement.classList.add("sapMInputBaseContentWrapper");
-        this._inputControlContainerElement.classList.add("sapMInputBase");
+		this._timeInputElement.id = this.id;
+		this._timeInputElement.classList.add("sapMInputBaseInner");
+		this._timeInputElement.classList.add("sapMInputBaseContentWrapper");
+		this._inputControlContainerElement.classList.add("sapMInputBase");
 
-		return oElement;
+		return this._timeInputElement;
 	};
 
 	return UI5InputTime;
