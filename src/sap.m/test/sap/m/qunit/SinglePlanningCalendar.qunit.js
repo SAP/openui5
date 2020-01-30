45696fe2bc4d8bd080bc98585a27a4de1102f078
Nikolay Hristov <n.hristov@sap.com>
2020-01-30 17:05:17 +0200

    [INTERNAL][FIX] sap.m.SinglePlanningCalendar - now gets proper ID of the selectedView
    - Until now, the control set in the selectedView association SegmentedButton instead
    of View, now this is fixed. Also, the setter of this association is fixed in order
    to get only existing View (by ID or by View object)
    
    Change-Id: If1a146c1ddb0da4ce5317df6ab2510f9960d03d9
    BCP: 2080082119

--- src/sap.m/test/sap/m/qunit/SinglePlanningCalendar.qunit.js
+++ src/sap.m/test/sap/m/qunit/SinglePlanningCalendar.qunit.js
@@ -143,6 +143,46 @@ sap.ui.define([
 		oSPC.destroy();
 	});
 
+	QUnit.test("selectedView: Simulate PRESS on segmented button of a view", function(assert) {
+		var	oSPC = new SinglePlanningCalendar({
+				views: [
+					new SinglePlanningCalendarDayView({
+						key: "DayView",
+						title: "Day View"
+					}),
+					new SinglePlanningCalendarDayView({
+						key: "MonthView",
+						title: "Month View"
+					})
+				]
+			}),
+			oMonthViewSegmentedButtonItem = oSPC._getHeader()._getOrCreateViewSwitch().getItems()[1],
+			sMonthViewId = oSPC.getViews()[1].getId(),
+			oDayViewSegmentedButtonItem = oSPC._getHeader()._getOrCreateViewSwitch().getItems()[0],
+			sDayViewId = oSPC.getViews()[0].getId();
+
+		oSPC.placeAt("qunit-fixture");
+		sap.ui.getCore().applyChanges();
+
+		// Act
+		oMonthViewSegmentedButtonItem.oButton.firePress();
+		sap.ui.getCore().applyChanges();
+
+		// assert
+		assert.equal(oSPC.getSelectedView(), sMonthViewId, "The proper View Id is stored in selectedView association");
+
+		// Act
+		oDayViewSegmentedButtonItem.oButton.firePress();
+		sap.ui.getCore().applyChanges();
+
+		// assert
+		assert.equal(oSPC.getSelectedView(), sDayViewId, "The proper View Id is stored in selectedView association");
+
+		// cleanup
+		oSPC.removeAllViews();
+		oSPC = null;
+	});
+
 	QUnit.test("getSelectedAppointments", function (assert) {
 		var oAppointment1 = new CalendarAppointment({
 				title: "Appointment1",
@@ -1774,4 +1814,76 @@ sap.ui.define([
 		assert.deepEqual(newAppPos.startDate, oAppEndDate, "Start date hout is correct");
 		assert.deepEqual(newAppPos.endDate, new Date(2017, 10, 13, 2, 30, 0), "End date hour is correct");
 	});
+
+	QUnit.module("Helper private methods", {
+		beforeEach: function() {
+			this.oSPC = new SinglePlanningCalendar({
+				views: [
+					new SinglePlanningCalendarDayView({
+						key: "DayView",
+						title: "Day View"
+					}),
+					new SinglePlanningCalendarDayView({
+						key: "MonthView",
+						title: "Month View"
+					})
+				]
+			});
+		},
+		afterEach: function() {
+			this.oSPC.removeAllViews();
+			this.oSPC = null;
+		}
+	});
+
+	QUnit.test("_getViewByKey: Return the view with matching key, or null if there is no match", function(assert) {
+
+		var oView;
+
+		// Act: try to find a view with "DayView" key
+		oView = this.oSPC._getViewByKey("DayView");
+
+		// assert
+		assert.equal(this.oSPC._isViewKeyExisting("DayView"), true, "View with key 'DayView' exists, dedicated method _isViewKeyExisting returns true");
+		assert.deepEqual(this.oSPC.getViews()[0], oView, "Returned View with key 'DayView' is correct");
+
+		// Act: try to find a view with "MonthView" key
+		oView = this.oSPC._getViewByKey("MonthView");
+
+		// assert
+		assert.equal(this.oSPC._isViewKeyExisting("MonthView"), true, "View with key 'MonthView' exists, dedicated method _isViewKeyExisting returns true");
+		assert.deepEqual(this.oSPC.getViews()[1], oView, "Returned View with key 'MonthView' is correct");
+
+		// Act: try to find a view with "WeekView" key
+		oView = this.oSPC._getViewByKey("WeekView");
+
+		// assert
+		assert.equal(this.oSPC._isViewKeyExisting("WeekView"), false, "View with key 'WeekView' is missing, dedicated method _isViewKeyExisting returns false");
+		assert.equal(oView, null, "View with key 'WeekView' is missing, null is returned");
+	});
+
+
+	QUnit.test("_getViewByID: Return the view with matching ID, or null if there is no match", function(assert) {
+
+		var oView;
+
+		// Act: try to find a view with "view0" id
+		oView = this.oSPC._getViewById(this.oSPC.getViews()[0].getId());
+
+		// assert
+		assert.deepEqual(this.oSPC.getViews()[0], oView, "Returned View is correct");
+
+		// Act: try to find a view with "view1" key
+		oView = this.oSPC._getViewById(this.oSPC.getViews()[1].getId());
+
+		// assert
+		assert.deepEqual(this.oSPC.getViews()[1], oView, "Returned View is correct");
+
+		// Act: try to find a view with "WeekView" key
+		oView = this.oSPC._getViewById("myNonExistingId");
+
+		// assert
+		assert.equal(oView, null, "View with id 'myNonExistingId' is missing, null is returned");
+	});
+
 });
