ad24d21845eb46dcb332dc6432a282a8c6e65ec6
i310817 <gergana.marcheva@sap.com>
2020-02-03 21:50:34 +0200

    [FEATURE] sap.m.Link: now supports Rename action in RTA
    JIRA: BGSOFUIBALKAN-5258
    
    Change-Id: I38b520f2a29d83e323199741f8a1b4546327f569

--- src/sap.m/test/sap/m/qunit/designtime/Link.qunit.js
+++ src/sap.m/test/sap/m/qunit/designtime/Link.qunit.js
@@ -28,6 +28,39 @@ sap.ui.define([
 		});
 	})
 	.then(function() {
+		// Rename action
+		var fnConfirmLinkIsRenamedWithNewValue = function (oLink, oViewAfterAction, assert) {
+			assert.strictEqual(oViewAfterAction.byId("myLink").getText(),
+				"New Value",
+				"then the control has been renamed to the new value (New Value)");
+		};
+
+		var fnConfirmLinkIsRenamedWithOldValue = function (oUiComponent, oViewAfterAction, assert) {
+			assert.strictEqual(oViewAfterAction.byId("myLink").getText(),
+				"Link 1",
+				"then the control has been renamed to the old value (Link 1)");
+		};
+
+		elementActionTest("Checking the rename action for a Link", {
+			xmlView: '<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns:m="sap.m">"' +
+			'<m:Link text="Link 1" id="myLink" />' +
+			'</mvc:View>'
+			,
+			action: {
+				name: "rename",
+				controlId: "myLink",
+				parameter: function (oView) {
+					return {
+						newValue: 'New Value',
+						renamedElement: oView.byId("myLink")
+					};
+				}
+			},
+			afterAction: fnConfirmLinkIsRenamedWithNewValue,
+			afterUndo: fnConfirmLinkIsRenamedWithOldValue,
+			afterRedo: fnConfirmLinkIsRenamedWithNewValue
+		});
+
 		// Remove and reveal actions
 		var fnConfirmLinkIsInvisible = function (oUiComponent, oViewAfterAction, assert) {
 			assert.strictEqual(oViewAfterAction.byId("myLink").getVisible(), false, "then the Link element is invisible");
