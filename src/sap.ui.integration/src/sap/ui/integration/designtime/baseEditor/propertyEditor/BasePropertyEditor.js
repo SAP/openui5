a415aed2c1e9cec46aeccd7a06be24d8ba1b983d
Pavel Kornev <pavel.kornev@sap.com>
2020-02-03 10:58:18 +0100

    [INTERNAL] sap.ui.integration: move ObjectBinding class to binding folder
    Change-Id: Ia95d7e06ab64e223dd32c1dba5f0e4ed7a45c0e1
    JIRA: UICSFLEX-6525

--- src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/propertyEditor/BasePropertyEditor.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/propertyEditor/BasePropertyEditor.js
@@ -3,7 +3,7 @@
  */
 sap.ui.define([
 	"sap/ui/core/Control",
-	"./../util/ObjectBinding",
+	"sap/ui/integration/designtime/baseEditor/util/binding/ObjectBinding",
 	"./../util/isAggregationTemplate",
 	"sap/ui/model/json/JSONModel",
 	"sap/m/Label",
