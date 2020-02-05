6bbd9326a7a1e2f00c809d10dab4d6088ed06e32
Stefan Blamberg <stefan.blamberg@sap.com>
2020-02-05 10:24:00 +0100

    [INTERNAL] sap.ui.integration: Add editor ready handling integration test
    Change-Id: Icab694b5ffa3fa9f2a2f0f21759598f3136a433d
    JIRA: UICSFLEX-6525

---
+++ src/sap.ui.integration/test/sap/ui/integration/qunit/designtime/baseEditor/integration/ReadyHandling.qunit.js
@@ -0,0 +1,87 @@
+/* global QUnit */
+
+sap.ui.define([
+	"sap/ui/integration/designtime/baseEditor/BaseEditor",
+	"sap/ui/qunit/QUnitUtils"
+], function (
+	BaseEditor,
+	QUnitUtils
+) {
+	"use strict";
+
+	QUnit.module("Given a nested array editor", {
+		beforeEach: function (assert) {
+			var fnDone = assert.async();
+			var mConfig = {
+				"properties": {
+					"arrayLevel1": {
+						"path": "arrayLevel1",
+						"type": "array",
+						"template": {
+							"arrayLevel2": {
+								"path": "arrayLevel2",
+								"type": "array",
+								"template": {
+									"stringLevel3": {
+										"path": "stringLevel3",
+										"type": "string"
+									}
+								}
+							}
+						}
+					}
+				},
+				"propertyEditors": {
+					"array": "sap/ui/integration/designtime/baseEditor/propertyEditor/arrayEditor/ArrayEditor",
+					"string": "sap/ui/integration/designtime/baseEditor/propertyEditor/stringEditor/StringEditor"
+				}
+			};
+			var mJson = {
+				arrayLevel1: [{
+					arrayLevel2: [{
+						stringLevel3: "Foo"
+					}]
+				}]
+			};
+
+			this.oBaseEditor = new BaseEditor({
+				config: mConfig,
+				json: mJson
+			});
+			this.oBaseEditor.placeAt("qunit-fixture");
+
+			this.oBaseEditor.attachEventOnce("propertyEditorsReady", function () {
+				fnDone();
+			});
+		},
+		afterEach: function () {
+			this.oBaseEditor.destroy();
+		}
+	}, function () {
+		QUnit.test("When a nested complex editor changes", function (assert) {
+			var fnDone = assert.async();
+
+			var oArrayLevel1 = this.oBaseEditor.getPropertyEditorSync("arrayLevel1");
+			oArrayLevel1.attachEventOnce("ready", function () {
+				assert.ok(true, "Then the ready event bubbles to the outer array editor");
+				assert.deepEqual(
+					this.oBaseEditor.getJson(),
+					{
+						arrayLevel1: [{
+							arrayLevel2: [{
+								stringLevel3: "Foo"
+							},
+							{}]
+						}]
+					},
+					"Then the base editor has received the value change"
+				);
+				fnDone();
+			}, this);
+
+			var oArrayLevel2 = oArrayLevel1.getAggregation("propertyEditor")._aEditorWrappers[0].getAggregation("propertyEditors")[0];
+			var oAddButton = oArrayLevel2.getAggregation("propertyEditor").getContent().getItems()[1];
+			QUnitUtils.triggerEvent("tap", oAddButton.getDomRef());
+		});
+	});
+});
