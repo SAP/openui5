587ac50ee68b1978909fd5e005b547a57d1ce3c4
Arthur Trauter <arthur.trauter@sap.com>
2020-02-11 09:28:07 +0100

    [FIX] sap.ui.dt - DesignTime improvement on insertAggregation
    On multiple insertAggregation calls there is no waiting for 'synched'
    event anymore. The insertAggregation calls need to be handled into a
    designtime sync scope. Otherwise there might be some issues with
    'editable' evaluations for the corresponding overlays (e.g. move, remove)
    
    BCP: 2080084573
    
    Change-Id: I7644f76d453620e5b00326072d16936db121f1a4
    Signed-off-by: Arthur Trauter <arthur.trauter@sap.com>

--- src/sap.ui.dt/src/sap/ui/dt/DesignTime.js
+++ src/sap.ui.dt/src/sap/ui/dt/DesignTime.js
@@ -1128,16 +1128,7 @@ function (
 		switch (oParams.type) {
 			case "addOrSetAggregation":
 			case "insertAggregation":
-				if (this.getStatus() === DesignTimeStatus.SYNCING) {
-					this.attachEventOnce("synced", oParams, function () {
-						// DesignTime instance at this point might be destroyed by third-parties on synced event
-						if (!this.bIsDestroyed) {
-							this._onAddAggregation(arguments[1].value, arguments[1].target, arguments[1].name);
-						}
-					}, this);
-				} else {
-					this._onAddAggregation(oParams.value, oParams.target, oParams.name);
-				}
+				this._onAddAggregation(oParams.value, oParams.target, oParams.name);
 				break;
 			case "setParent":
 				// timeout is needed because UI5 controls & apps can temporary "detach" controls from control tree
