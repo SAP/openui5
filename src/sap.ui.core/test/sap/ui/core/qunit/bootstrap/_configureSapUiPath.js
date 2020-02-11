d3af61280432dc06096d75d4cf8e0b7f11551e99
Frank Weigel <frank.weigel@sap.com>
2020-02-11 16:04:55 +0100

    [INTERNAL] sap.ui.core: make resource root tests executable w/o build
    So far, ResourceRoot* tests relied on a full build (including a bundled
    sap-ui-core.js). With this change, they can execute also in a non-build
    environment.
    
    Change-Id: I7da59217c3c94250c08511d11edf765d710db16a
    BCP: 2080058375

--- src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/_configureSapUiPath.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	"use strict";
-	window["sap-ui-config"] = {
-		resourceRoots : {
-			"sap.base": "../../../../../../resources/sap/base/",
-			"sap.ui" : "../../../../../../resources/sap/ui/"
-		}
-	};
-}());
