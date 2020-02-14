8c995f0d82d487cc2c8e7921f8b96a76365b50e4
Marco Chiaradonna <m.chiaradonna@sap.com>
2020-02-14 11:26:53 +0100

    [FEATURE] sap.m.Table: Automatic pop-in mode added
    Provide an automatic pop-in mode in the ResponsiveTable.
    The pop-in-related properties on the visible columns are automatically set
    based on the column importance.
    
    Change-Id: I16512a9e3dbfc1c7a10067f756cd50e804558e20
    Jira: CPOUIFTEAMB-1461

---
+++ src/sap.m/test/sap/m/demokit/sample/TableAutoPopin/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+		"use strict";
+
+		return UIComponent.extend("sap.m.sample.TableAutoPopin.Component", {
+
+			metadata: {
+				manifest: "json"
+			}
+		});
+
+	});
