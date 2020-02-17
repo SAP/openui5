c890cc0d2f23c396c605a3b02b0c99453b46bfb1
Arthur Trauter <arthur.trauter@sap.com>
2020-02-17 13:51:12 +0100

    [INTERNAL][FIX] sap.ui.dt - insert aggregation with pending parentOverlay
    Grid table provides an usecase on start Designtime where onAddAggregation
    event the parent overlay is not yet registered in the OverlayRegistry.
    For this case we are now waiting for the overlay creation to avoid errors.
    
    Change-Id: Ie26d9842c5a759bbe1fcf102a1884995b6a8d35b
    Signed-off-by: Arthur Trauter <arthur.trauter@sap.com>

--- src/sap.ui.dt/test/sap/ui/dt/qunit/integration/TablesInDesignTime.qunit.js
+++ src/sap.ui.dt/test/sap/ui/dt/qunit/integration/TablesInDesignTime.qunit.js
@@ -8,7 +8,11 @@ sap.ui.define([
 	"sap/ui/table/AnalyticalTable",
 	"sap/ui/table/AnalyticalColumn",
 	"sap/m/Label",
-	"sap/ui/Device"
+	"sap/m/Button",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/Device",
+	"sap/base/Log",
+	"sap/ui/thirdparty/sinon-4"
 ],
 function (
 	DesignTime,
@@ -18,9 +22,63 @@ function (
 	AnalyticalTable,
 	AnalyticalColumn,
 	Label,
-	Device
+	Button,
+	JSONModel,
+	Device,
+	Log,
+	sinon
 ) {
 	"use strict";
+
+	var sandbox = sinon.sandbox.create();
+
+	function _createJSONModel() {
+		return new JSONModel([
+			{text: "item1-bound"},
+			{text: "item2-bound"}
+		]);
+	}
+
+	function _createTable(oModel) {
+		var oTable = new Table({
+			title: "Table Example",
+			visibleRowCount: 5,
+			width : "200px",
+			rows: "{/}"
+		});
+		if (oModel) {
+			oTable.setModel(oModel);
+		}
+
+		oTable.placeAt("qunit-fixture");
+		sap.ui.getCore().applyChanges();
+
+		oTable.addColumn(new Column({
+			label: new Label({text: "Last Name"}),
+			width: "200px",
+			template: new Label({id: "text-1", text: "{text}"})
+		}));
+		oTable.addColumn(new Column({
+			label: new Label({text: "First Name"}),
+			width: "100px",
+			template: new Label({id: "text-2", text: "{text}"})
+		}));
+		oTable.addColumn(new Column({
+			label: new Label({text: "Checked"}),
+			width: "75px",
+			template: new Button({id: "button-1", text: "{text}"})
+		}));
+		oTable.addColumn(new Column({
+			label: new Label({text: "Web Site"}),
+			template: new Button({id: "button-2", text: "{text}"})
+		}));
+		oTable.addColumn(new Column({
+			label: new Label({text: "Image"}),
+			width: "75px",
+			template: "dummy"
+		}));
+		return oTable;
+	}
 
 	if (Device.browser.edge) {
 		QUnit.module("dummy test module", function() {
@@ -29,46 +87,42 @@ function (
 			});
 		});
 	} else {
+		QUnit.module("Given the sap.ui.table.Table is created", {
+			beforeEach: function () {
+				this.oModel = _createJSONModel();
+				this.oTable = _createTable(this.oModel);
+			},
+			afterEach: function () {
+				this.oTable.destroy();
+				sandbox.restore();
+			}
+		}, function () {
+			QUnit.test("when design time is started with sap.ui.table.Table as root element", function(assert) {
+				var done = assert.async();
+				var oOnErrorSpy = sandbox.spy(Log, "error");
+				var oDesignTime = new DesignTime({
+					rootElements: [this.oTable]
+				});
+				oDesignTime.attachEventOnce("syncFailed", function() {
+					assert.notOk(true, "then syncFailed event should not be thrown");
+				});
+				oDesignTime.attachEventOnce("synced", function() {
+					assert.ok(true, "then sync event should be thrown");
+					var oTableOverlay = OverlayRegistry.getOverlay(this.oTable);
+					var oColumnsOverlay = oTableOverlay.getAggregationOverlay("columns");
+					assert.strictEqual(oColumnsOverlay.getChildren().length, 5, "then there should be 5 children overlays for column available");
+					assert.notOk(oOnErrorSpy.called, "then design time should not throw errors in console on start");
+					done();
+				}.bind(this));
+			});
+		});
+
 		QUnit.module("Given that design time is created for a sap.ui.table.Table", {
 			beforeEach : function(assert) {
 				var done = assert.async();
 
-				this.oTable = new Table({
-					title: "Table Example",
-					visibleRowCount: 5,
-					width : "200px"
-				});
-
-				this.oTable.placeAt("qunit-fixture");
-
-				sap.ui.getCore().applyChanges();
-
-				this.oColumn = new Column({
-					label: new Label({text: "Last Name"}),
-					width: "200px",
-					template: "dummy"
-				});
-
-				this.oTable.addColumn(this.oColumn);
-				this.oTable.addColumn(new Column({
-					label: new Label({text: "First Name"}),
-					width: "100px",
-					template: "dummy"
-				}));
-				this.oTable.addColumn(new Column({
-					label: new Label({text: "Checked"}),
-					width: "75px",
-					template: "dummy"
-				}));
-				this.oTable.addColumn(new Column({
-					label: new Label({text: "Web Site"}),
-					template: "dummy"
-				}));
-				this.oTable.addColumn(new Column({
-					label: new Label({text: "Image"}),
-					width: "75px",
-					template: "dummy"
-				}));
+				this.oTable = _createTable();
+				this.oColumn = this.oTable.getColumns()[0];
 
 				this.oDesignTime = new DesignTime({
 					rootElements : [this.oTable]
@@ -78,11 +132,7 @@ function (
 					sap.ui.getCore().applyChanges();
 
 					// TODO: Temporal solution. Remove when synced in DesignTime event wait for all async processes to be done.
-					if (Device.browser.internet_explorer || Device.browser.edge) {
-						setTimeout(done, 16);
-					} else {
-						done();
-					}
+					requestAnimationFrame(done);
 				});
 			},
 			afterEach : function() {
