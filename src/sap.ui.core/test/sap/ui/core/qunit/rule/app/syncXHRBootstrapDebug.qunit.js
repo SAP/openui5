dec2291eb6e5e9f3cea41e443bda5bfa5b2f15bb
Florian Vogt <florian.vogt@sap.com>
2020-02-17 14:29:43 +0100

    [INTERNAL] enrich tests for syncXHRBootstrap and syncXHRBoostrapDebug
    With this change not only the total number of sync requests are tested, also which resources are requested. Now, the expectations are implemented in the asserts and not longer as comments.
    BCP: 2080116126
    
    Change-Id: Ibe66e53eb22bd1b27b19b73fcd9fb08b5f47f0e1

--- src/sap.ui.core/test/sap/ui/core/qunit/rule/app/syncXHRBootstrapDebug.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/rule/app/syncXHRBootstrapDebug.qunit.js
@@ -4,38 +4,51 @@
 
 	QUnit.module("Core boot Debug", {
 		before: function() {
-			var oTestModule = this;
 			this.requireSyncStub = sinon.spy(sap.ui, "requireSync");
 			window["sap-ui-config"] = {debug: true};
 			return new Promise(function(resolve) {
-				sap.ui.require(["sap/ui/Device", "sap/base/Log"], function(Device, Log) {
-
-					// Note: Expected synchronous requests for the following modules:
-
-					// sap/ui/core/support/Support.js
-					// sap/ui/support/Bootstrap.js
-					// sap/ui/base/Object.js
-					// In case preloads are used, there is also an additional sync request for the sap.ui.core library-preload.js
-
-					oTestModule.iExpectedMaxSyncCalls = 4;
-
-					// the Normalize Polyfill is optionally required sync by the FilterProcessor
-					if (!String.prototype.normalize && Device.system.desktop) {
-						oTestModule.iExpectedMaxSyncCalls = 5;
-					}
-
+				sap.ui.require(["sap/base/Log", "sap/ui/Device"], function(Log, Device) {
+					this.bIsNormalizePolyfillNeeded = !String.prototype.normalize && Device.system.desktop;
 					Log.logSupportInfo(true);
 					Log.setLevel(4);
 					sap.ui.require(["sap/ui/core/Core"], function(core) {
 						core.boot();
 						core.attachInit(resolve);
 					});
-				});
-			});
+				}.bind(this));
+			}.bind(this));
 		},
 		after: function(assert) {
-			assert.ok(this.requireSyncStub.callCount <= this.iExpectedMaxSyncCalls,
-				"the number of sync requests does not exceed the defined maximum of " + this.iExpectedMaxSyncCalls + " calls");
+			var iLoadedModuleIndex = 0;
+			var iExpectedMaxSyncCalls = 3;
+
+			var fnAssertRequireSync = function(sModuleName) {
+				assert.strictEqual(this.requireSyncStub.getCall(iLoadedModuleIndex).args[0], sModuleName, "At position " + iLoadedModuleIndex + " the module '" + sModuleName + "' should be loaded");
+				iLoadedModuleIndex++;
+			}.bind(this);
+
+			var fnGetModuleName = function(iPosition){
+				return this.requireSyncStub.getCall(iPosition).args[0];
+			}.bind(this);
+
+			// the Normalize Polyfill is optionally required sync by the FilterProcessor
+			if (this.bIsNormalizePolyfillNeeded) {
+				fnAssertRequireSync("sap/base/strings/NormalizePolyfill");
+				iExpectedMaxSyncCalls++;
+			}
+
+			// In case preloads are used, there is also an additional sync request for the sap.ui.core library-preload.js
+			if (fnGetModuleName(iLoadedModuleIndex) === "sap/ui/core/library-preload") {
+				fnAssertRequireSync("sap/ui/core/library-preload");
+				iExpectedMaxSyncCalls++;
+			}
+
+			fnAssertRequireSync("sap/ui/base/Object");
+			fnAssertRequireSync("sap/ui/core/library");
+			fnAssertRequireSync("sap/ui/debug/DebugEnv");
+
+			assert.equal(this.requireSyncStub.callCount, iExpectedMaxSyncCalls,
+				"The number of sync requests should be exactly " + iExpectedMaxSyncCalls);
 			this.requireSyncStub.restore();
 		}
 	});
