9630b8266e6432ea14b4b33690f8bc0ad2252876
Minkova <yana.minkova@sap.com>
2020-02-25 13:54:44 +0200

    [FEATURE] sap.uxap.ObjectPageLayout: Header facet pattern added
    New samples with header facets are added to
    sap.uxap.ObjectPageLayout.
    
    JIRA: BGSOFUIPIRIN-4452
    Change-Id: I45e1eece8d1f5a312d5ca9dfc362f7874f4f0a4a

---
+++ src/sap.uxap/test/sap/uxap/demokit/sample/HeaderFacetSample2/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.uxap.sample.HeaderFacetSample2.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+	return Component;
+}, true);
