f96ad8dc251edb06148ec3035638f6e4b0ff0784
Thuy Boehm <thuy.sophie.boehm@sap.com>
2020-02-26 08:46:10 +0100

    [INTERNAL] sap.ui.fl update JSDoc
    Change-Id: Idcfdb6196e4a83250d4d2c41080af22b5f27bbe2

--- src/sap.ui.fl/src/sap/ui/fl/write/_internal/Storage.js
+++ src/sap.ui.fl/src/sap/ui/fl/write/_internal/Storage.js
@@ -73,7 +73,7 @@ sap.ui.define([
 	/**
 	 * Determines the connector in charge for a given layer.
 	 *
-	 * @param {string} sLayer Layer on which the file should be stored
+	 * @param {string} sLayer - Layer on which the file should be stored
 	 * @returns {Promise<sap.ui.fl.write.connectors.BaseConnector>} Returns the connector in charge for the layer or rejects in case no connector can be determined
 	 */
 	function _getConnectorConfigByLayer(sLayer) {
@@ -120,11 +120,11 @@ sap.ui.define([
 	 * Stores the flex data by calling the according write of the connector in charge of the passed layer;
 	 * The promise is rejected in case the writing failed or no connector is configured to handle the layer.
 	 *
-	 * @param {object} mPropertyBag Contains additional information for all the Connectors
-	 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer on which the file should be stored
-	 * @param {object[]} mPropertyBag.flexObjects Data to be stored
-	 * @param {string} [mPropertyBag._transport] The transport ID which will be handled internally, so there is no need to be passed
-	 * @param {boolean} [mPropertyBag.isLegacyVariant] Whether the update data has file type .variant or not
+	 * @param {object} mPropertyBag - Contains additional information for all the Connectors
+	 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer on which the file should be stored
+	 * @param {object[]} mPropertyBag.flexObjects - Data to be stored
+	 * @param {string} [mPropertyBag._transport] - The transport ID which will be handled internally, so there is no need to be passed
+	 * @param {boolean} [mPropertyBag.isLegacyVariant] - Whether the update data has file type .variant or not
 	 * @param {boolean} [mPropertyBag.draft=false] - Indicates if changes should be written as a draft
 	 * @returns {Promise} Promise resolving as soon as the writing was completed or rejects in case of an error
 	 */
@@ -136,10 +136,10 @@ sap.ui.define([
 	 * Delete an existing flex data by calling the according remove of the connector in charge of the passed layer;
 	 * The promise is rejected in case the removing failed or no connector is configured to handle the layer.
 	 *
-	 * @param {object} mPropertyBag Contains additional information for all the Connectors
-	 * @param {sap.ui.fl.Change} mPropertyBag.flexObject Flex Object to be deleted
-	 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer on which the data should be deleted
-	 * @param {string} [mPropertyBag._transport] The transport ID which will be handled internally, so there is no need to be passed
+	 * @param {object} mPropertyBag - Contains additional information for all the Connectors
+	 * @param {sap.ui.fl.Change} mPropertyBag.flexObject - Flex Object to be deleted
+	 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer on which the data should be deleted
+	 * @param {string} [mPropertyBag._transport] - The transport ID which will be handled internally, so there is no need to be passed
 	 * @returns {Promise} Promise resolving as soon as the writing was completed or rejects in case of an error
 	 */
 	Storage.remove = function(mPropertyBag) {
@@ -150,10 +150,10 @@ sap.ui.define([
 	 * Update an existing flex data by calling the according update of the connector in charge of the passed layer;
 	 * The promise is rejected in case the writing failed or no connector is configured to handle the layer.
 	 *
-	 * @param {object} mPropertyBag Contains additional information for all the Connectors
-	 * @param {object} mPropertyBag.flexObject Flex object to be deleted
-	 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer on which the data should be deleted
-	 * @param {string} [mPropertyBag._transport] The transport ID which will be handled internally, so there is no need to be passed
+	 * @param {object} mPropertyBag - Contains additional information for all the Connectors
+	 * @param {object} mPropertyBag.flexObject - Flex object to be deleted
+	 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer on which the data should be deleted
+	 * @param {string} [mPropertyBag._transport] - The transport ID which will be handled internally, so there is no need to be passed
 	 * @returns {Promise} Promise resolving as soon as the writing was completed or rejects in case of an error
 	 */
 	Storage.update = function(mPropertyBag) {
@@ -164,14 +164,14 @@ sap.ui.define([
 	 * Resets the flex data by calling the according reset of the connector in charge of the passed layer;
 	 * The promise is rejected in case the writing failed or no connector is configured to handle the layer.
 	 *
-	 * @param {object} mPropertyBag Contains additional information for all the Connectors
-	 * @param {string} mPropertyBag.reference Flexibility reference
-	 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer on which the reset should take place
-	 * @param {sap.ui.fl.Change[]} mPropertyBag.changes Changes of the selected layer and flex reference
-	 * @param {string} [mPropertyBag.appVersion] Version of the application
-	 * @param {string} [mPropertyBag.generator] Generator with which the changes were created
-	 * @param {string[]} [mPropertyBag.selectorIds] Selector IDs of controls for which the reset should filter
-	 * @param {string} [mPropertyBag.changeTypes] Change types of the changes which should be reset
+	 * @param {object} mPropertyBag - Contains additional information for all the Connectors
+	 * @param {string} mPropertyBag.reference - Flexibility reference
+	 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer on which the reset should take place
+	 * @param {sap.ui.fl.Change[]} mPropertyBag.changes - Changes of the selected layer and flex reference
+	 * @param {string} [mPropertyBag.appVersion] - Version of the application
+	 * @param {string} [mPropertyBag.generator] - Generator with which the changes were created
+	 * @param {string[]} [mPropertyBag.selectorIds] - Selector IDs of controls for which the reset should filter
+	 * @param {string} [mPropertyBag.changeTypes] - Change types of the changes which should be reset
 	 * @returns {Promise} Resolves after the reset is completed and rejects in case of an error
 	 */
 	Storage.reset = function(mPropertyBag) {
@@ -183,11 +183,11 @@ sap.ui.define([
 	 * The flexibility info is a JSON string that has boolean properties 'isPublishEnabled' and 'isResetEnabled'
 	 * that indicate if for the given application and layer a publish and reset shall be enabled, respectively
 	 *
-	 * @param {object} mPropertyBag Property bag
-	 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer
-	 * @param {string} mPropertyBag.reference Flex reference
-	 * @param {string} [mPropertyBag.url] Configured url for the connector
-	 * @param {string} [mPropertyBag.appVersion] Version of the application
+	 * @param {object} mPropertyBag - Property bag
+	 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer
+	 * @param {string} mPropertyBag.reference - Flex reference
+	 * @param {string} [mPropertyBag.url] - Configured url for the connector
+	 * @param {string} [mPropertyBag.appVersion] - Version of the application
 	 * @returns {Promise<object>} Promise resolves as soon as the writing was completed
 	 */
 	Storage.getFlexInfo = function(mPropertyBag) {
@@ -208,16 +208,16 @@ sap.ui.define([
 	/**
 	 * Transports all the UI changes and app variant descriptor (if exists) to the target system.
 	 *
-	 * @param {object} mPropertyBag Property bag
-	 * @param {string} mPropertyBag.url Configured url for the connector
-	 * @param {object} mPropertyBag.transportDialogSettings Settings for Transport dialog
-	 * @param {object} mPropertyBag.transportDialogSettings.rootControl The root control of the running application
-	 * @param {string} mPropertyBag.transportDialogSettings.styleClass Style class name to be added in the TransportDialog
-	 * @param {string} mPropertyBag.layer Working layer
-	 * @param {string} mPropertyBag.reference Flex reference of the application
-	 * @param {string} mPropertyBag.appVersion Version of the application for which the reset takes place
-	 * @param {sap.ui.fl.Change[]} mPropertyBag.localChanges Local changes to be published
-	 * @param {object[]} [mPropertyBag.appVariantDescriptors] An array of app variant descriptors which needs to be transported
+	 * @param {object} mPropertyBag - Property bag
+	 * @param {string} mPropertyBag.url - Configured url for the connector
+	 * @param {object} mPropertyBag.transportDialogSettings - Settings for Transport dialog
+	 * @param {object} mPropertyBag.transportDialogSettings.rootControl - The root control of the running application
+	 * @param {string} mPropertyBag.transportDialogSettings.styleClass - Style class name to be added in the TransportDialog
+	 * @param {string} mPropertyBag.layer - Working layer
+	 * @param {string} mPropertyBag.reference - Flex reference of the application
+	 * @param {string} mPropertyBag.appVersion - Version of the application for which the reset takes place
+	 * @param {sap.ui.fl.Change[]} mPropertyBag.localChanges - Local changes to be published
+	 * @param {object[]} [mPropertyBag.appVariantDescriptors] - An array of app variant descriptors which needs to be transported
 	 * @returns {Promise} Promise that resolves when all the artifacts are successfully transported
 	 */
 	Storage.publish = function(mPropertyBag) {
@@ -228,9 +228,9 @@ sap.ui.define([
 		/**
 		 * Loads the versions for a given application and layer.
 		 *
-		 * @param {object} mPropertyBag Property bag
-		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer
-		 * @param {string} mPropertyBag.reference Flex reference
+		 * @param {object} mPropertyBag - Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer
+		 * @param {string} mPropertyBag.reference - Flex reference
 		 * @returns {Promise<sap.ui.fl.Version[]>} Promise resolving with a list of versions if available;
 		 * rejects if an error occurs or the layer does not support draft handling
 		 */
@@ -242,10 +242,10 @@ sap.ui.define([
 		/**
 		 * Activates the draft for a given application and layer.
 		 *
-		 * @param {object} mPropertyBag Property bag
-		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer
-		 * @param {string} mPropertyBag.reference Flex reference
-		 * @param {string} mPropertyBag.title Title of the to be activated version
+		 * @param {object} mPropertyBag - Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer
+		 * @param {string} mPropertyBag.reference - Flex reference
+		 * @param {string} mPropertyBag.title - Title of the to be activated version
 		 * @returns {Promise<sap.ui.fl.Version>} Promise resolving with the activated version;
 		 * rejects if an error occurs or the layer does not support draft handling
 		 */
@@ -257,9 +257,9 @@ sap.ui.define([
 		/**
 		 * Discards the draft for a given application and layer.
 		 *
-		 * @param {object} mPropertyBag Property bag
-		 * @param {sap.ui.fl.Layer} mPropertyBag.layer Layer
-		 * @param {string} mPropertyBag.reference Flex reference
+		 * @param {object} mPropertyBag - Property bag
+		 * @param {sap.ui.fl.Layer} mPropertyBag.layer - Layer
+		 * @param {string} mPropertyBag.reference - Flex reference
 		 * @returns {Promise} Promise resolving after the draft is discarded;
 		 * rejects if an error occurs or the layer does not support draft handling
 		 */
