cb470e9f5a67e1eaa40e7414711838b99dff0bc0
Markus Romaniw <markus.romaniw@sap.com>
2020-03-10 15:42:34 +0100

    [INTERNAL][FIX] sap.ui.fl Only use our startRta functionality in case of no ushell
    Issue:
    - Enter RTA
    -> Reload needed because of perso changes
    -> BaseRTA plugin sets status to stopped
    -> checkForRtaStartOnDraftAndReturnResult triggers RTA start after
    reload
    -> status stays on stopped and fails when trying to stop it when exiting
    
    Fix:
    now only use our checkForRtaStartOnDraftAndReturnResult method in case
    of no ushell
    
    BCP: 2070067904
    Change-Id: I666b01cb7236f6fe402c95b8f0e76c3a82506634

--- src/sap.ui.fl/src/sap/ui/fl/FlexControllerFactory.js
+++ src/sap.ui.fl/src/sap/ui/fl/FlexControllerFactory.js
@@ -93,6 +93,11 @@ sap.ui.define([
 	 * @return {Promise} Promise resolving with the initially passed result
 	 */
 	function checkForRtaStartOnDraftAndReturnResult(oResult, oComponent) {
+		// Only check for RTA start without ushell
+		if (!Utils.getUshellContainer()) {
+			return Promise.resolve(oResult);
+		}
+
 		var sRestartingComponent = window.sessionStorage.getItem("sap.ui.rta.restart." + Layer.CUSTOMER);
 		if (sRestartingComponent) {
 			var sComponentId = Utils.getComponentClassName(oComponent);
