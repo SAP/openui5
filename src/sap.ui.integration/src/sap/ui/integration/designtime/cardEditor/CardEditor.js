230d034fb77f89c9c32db2a3dfeeb041e7761834
Pavel Kornev <pavel.kornev@sap.com>
2020-03-10 09:25:02 +0100

    [INTERNAL] sap.ui.integration: get rid of "defaultConfig"
    There were no way to replace the "default" configuration of CardEditor.
    Using "addDefaultConfig" extended the existent default config. setConfig
    was also an extenssion on top of default configuration. As we found out
    that the concept of default configuration is not flexible and not
    understandable by our consumers, we decided to discontinue it in favor of
    simple setConfig() (always replaces everything) and addConfig() (adds the
    specified configuration on top of previously set configutation).
    
    Change-Id: I120f9879ab38eeab43f40f269fec6f7ea535df8d
    JIRA: UICSFLEX-6185

--- src/sap.ui.integration/src/sap/ui/integration/designtime/cardEditor/CardEditor.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/cardEditor/CardEditor.js
@@ -26,7 +26,7 @@ sap.ui.define([
 	var CardEditor = BaseEditor.extend("sap.ui.integration.designtime.cardEditor.CardEditor", {
 		constructor: function() {
 			BaseEditor.prototype.constructor.apply(this, arguments);
-			this.addDefaultConfig(oDefaultCardConfig);
+			this.addConfig(oDefaultCardConfig);
 		},
 		renderer: BaseEditor.getMetadata().getRenderer()
 	});
