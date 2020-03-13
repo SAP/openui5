fcd5c955133ffaddc5bb0eb8d6a52fd92a63bfd5
Georgi Minkov <georgi.minkov@sap.com>
2020-03-13 16:49:00 +0200

    [INTERNAL] sap.m.IconTabBar: Adjust 'shifting tabs' behavior
    If an sap.m.IconTabFilter has been 'shifted' to the tab strip, then
    another item is selected, the previously 'shifted' item will return back
    to its original order.
    
    Change-Id: Icfce3317124227d0b6d42189f16dcb5910256eb7

--- src/sap.m/test/sap/m/qunit/IconTabHeader.qunit.js
+++ src/sap.m/test/sap/m/qunit/IconTabHeader.qunit.js
@@ -17,14 +17,17 @@ sap.ui.define([
 
 	createAndAppendDiv(DOM_RENDER_LOCATION);
 
-	function createHeaderWithItems() {
+	function createHeaderWithItems(iNum) {
+		var aItems = [];
+		for (var i = 0; i < iNum; i++) {
+			aItems.push(new IconTabFilter({
+				text: "Tab " + i,
+				key: i
+			}));
+		}
+
 		return new IconTabHeader({
-			items: [
-				new IconTabFilter({text: "tab1"}),
-				new IconTabFilter({text: "tab2"}),
-				new IconTabFilter({text: "tab3"}),
-				new IconTabFilter({text: "tab4"})
-			]
+			items: aItems
 		});
 	}
 
@@ -32,7 +35,7 @@ sap.ui.define([
 
 	QUnit.test("when there is not enough space, items should be hidden", function(assert) {
 		// arrange
-		var oITH = createHeaderWithItems(),
+		var oITH = createHeaderWithItems(4),
 			oLastItem = oITH.getItems()[3];
 
 		oITH.placeAt(DOM_RENDER_LOCATION);
@@ -48,6 +51,33 @@ sap.ui.define([
 		assert.ok(oLastItem.$().hasClass("sapMITBFilterHidden"), "the last filter is hidden");
 
 		// clean up
+		oITH.destroy();
+	});
+
+	QUnit.module("shifting behavior");
+
+	QUnit.test("selecting an overflown tab causes it to show up in the tab strip", function (assert) {
+		// Arrange
+		var oITH = createHeaderWithItems(100);
+		var oTargetTab = oITH.getItems()[99];
+		oITH.placeAt(DOM_RENDER_LOCATION);
+		Core.applyChanges();
+
+		// Assert
+		var aVisibleTabs = oITH.$().find(".sapMITBItem:not(.sapMITBFilterHidden)").toArray();
+		assert.strictEqual(aVisibleTabs.indexOf(oTargetTab.getDomRef()), -1, "The target tab is not in the tab strip before it gets picked");
+		assert.strictEqual(oITH._getItemsInStrip().indexOf(oTargetTab), -1, "The target tab is not in the tab strip before it gets picked");
+
+		// Act
+		oITH.setSelectedKey("99");
+		Core.applyChanges();
+
+		// Assert
+		aVisibleTabs = oITH.$().find(".sapMITBItem:not(.sapMITBFilterHidden)").toArray();
+		assert.notStrictEqual(aVisibleTabs.indexOf(oTargetTab.getDomRef()), -1, "The target tab is not in the tab strip before it gets picked");
+		assert.notStrictEqual(oITH._getItemsInStrip().indexOf(oTargetTab), -1, "The target tab is now in the tab strip after it got selected");
+
+		// Clean-up
 		oITH.destroy();
 	});
 });
