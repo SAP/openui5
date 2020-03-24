2f73ed0388e9ba14e2f8b6201bd2d48373c22264
Pavel Kornev <pavel.kornev@sap.com>
2020-03-24 20:40:20 +0100

    [INTERNAL] sap.ui.integration: improvement for escapeParameter utility
    Added a validator function to give the consumer of the utiliry whether
    ability to have custom logic which parameters should be escaped. E.g.
    in case of PropertyEditor wrapper we do escape not only by name (as it
    was before), but also checking where "type" property of the specified
    configuration object is registered in the PropertyEditorFactory.
    
    Additionally, escape utility now iterates through all the plain object
    which are specified in mSettings for the ManagedObject and make an
    escape attempt based on the validator decision.
    
    Change-Id: Ib96d94d4a752f0c654ddac43d453528efaeea8d8
    JIRA: UICSFLEX-6185

--- src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/util/escapeParameter.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/baseEditor/util/escapeParameter.js
@@ -2,33 +2,45 @@
  * ${copyright}
  */
 sap.ui.define([
-	"sap/base/util/isPlainObject"
+	"sap/base/util/isPlainObject",
+	"sap/base/util/each"
 ], function (
-	isPlainObject
+	isPlainObject,
+	each
 ) {
 	"use strict";
 
 	/**
-	* Escapes a special parameter of a set of arguments.
-	* @param {object} oArguments - Object with a set of arguments.
-	* @param {string} sParameterName - Name of the parameter which has to be escaped.
-	* @returns {Array} Array of arguments where the given parameter is escaped.
+	* Escapes object literals (plain objects) which are passed as settings map to a ManagedObject
+	* @param {object} oArguments - Array-like object with a set of arguments
+	* @param {function} [fnValidator] - Validator function. The validator is invoked with 2 arguments: (sKey, oValue).
+	*                                   When <code>true</code> is returned, then object will be escaped.
+	* @returns {Array} Array of arguments with escaped object literals
 	* @function
 	* @experimental
 	* @private
 	*/
-	return function (oArguments, sParameterName) {
+	return function (oArguments, fnValidator) {
+		fnValidator = fnValidator || function () { return true; };
 		var aArguments = Array.from(oArguments);
 		var iSettingsPosition = typeof aArguments[0] !== "string" && aArguments[0] !== undefined ? 0 : 1;
 
+
 		if (isPlainObject(aArguments[iSettingsPosition])) {
 			var mSettings = Object.assign({}, aArguments[iSettingsPosition]);
-			if (isPlainObject(mSettings[sParameterName])) {
-				mSettings[sParameterName] = Object.assign({}, mSettings[sParameterName], {
-					ui5object: true
-				});
-				aArguments[iSettingsPosition] = mSettings;
-			}
+
+			each(mSettings, function (sKey, vValue) {
+				if (
+					isPlainObject(vValue)
+					&& fnValidator(vValue, sKey)
+				) {
+					mSettings[sKey] = Object.assign({}, vValue, {
+						ui5object: true
+					});
+				}
+			});
+
+			aArguments[iSettingsPosition] = mSettings;
 		}
 
 		return aArguments;
