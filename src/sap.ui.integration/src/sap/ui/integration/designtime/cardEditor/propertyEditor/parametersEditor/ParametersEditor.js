783e3ce3f8ad6c3b4863e64bc2d6eb47d95ae809
Stefan Blamberg <stefan.blamberg@sap.com>
2020-03-27 18:00:53 +0100

    [INTERNAL] sap.ui.integration: Beautify ParametersEditor for SWZ
    - Create new fragment for SWZ configuration scenario
    - Add editor for parameter labels
    
    Change-Id: I0aa02b99a76179d21bb888635cab7c8de3f5339d
    JIRA: UICSFLEX-6864

--- src/sap.ui.integration/src/sap/ui/integration/designtime/cardEditor/propertyEditor/parametersEditor/ParametersEditor.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/cardEditor/propertyEditor/parametersEditor/ParametersEditor.js
@@ -2,11 +2,15 @@
  * ${copyright}
  */
 sap.ui.define([
+	"sap/ui/integration/designtime/baseEditor/propertyEditor/BasePropertyEditor",
 	"sap/ui/integration/designtime/baseEditor/propertyEditor/mapEditor/MapEditor",
-	"sap/base/util/includes"
+	"sap/base/util/includes",
+	"sap/base/util/restricted/_merge"
 ], function (
+	BasePropertyEditor,
 	MapEditor,
-	includes
+	includes,
+	_merge
 ) {
 	"use strict";
 
@@ -29,28 +33,58 @@ sap.ui.define([
 	 * @ui5-restricted
 	 */
 	var ParametersEditor = MapEditor.extend("sap.ui.integration.designtime.cardEditor.propertyEditor.parametersEditor.ParametersEditor", {
-		formatInputValue: function(oValue) {
-			return oValue;
-		},
-
-		formatOutputValue: function(oValue) {
-			return oValue;
-		},
-
-		_isValidItem: function(oItem, oOriginalItem) {
-			// If invalid entries should be excluded, only keep items which have a type in the manifest or have a string value
-			var sType = oOriginalItem.type;
-			var vValue = oOriginalItem.value;
-			var aAllowedTypes = this._getAllowedTypes();
-
-			return (
-				sType && aAllowedTypes.indexOf(sType) >= 0 ||
-				typeof vValue === "string" && includes(aAllowedTypes, "string")
-			);
-		},
-
-		renderer: MapEditor.getMetadata().getRenderer().render
+		xmlFragment: "sap.ui.integration.designtime.cardEditor.propertyEditor.parametersEditor.ParametersEditor",
+		renderer: BasePropertyEditor.getMetadata().getRenderer().render
 	});
+
+	ParametersEditor.prototype.processInputValue = function(oValue) {
+		return oValue;
+	};
+
+	ParametersEditor.prototype.processOutputValue = function(oValue) {
+		return oValue;
+	};
+
+	ParametersEditor.prototype._itemsFormatter = function(aItems) {
+		return aItems.map(function (oItem) {
+			var oValue = _merge({}, oItem.value[0]);
+			if (!oValue.label) {
+				oValue.label = oItem.key;
+			}
+			return oValue;
+		});
+	};
+
+	ParametersEditor.prototype._onLabelChange = function(oEvent, sKey) {
+		var oEditorValue = _merge({}, this.getValue());
+		var sNewLabel =  oEvent.getParameter("newValue");
+
+		var oItemToEdit = oEditorValue[sKey];
+		oItemToEdit.label = sNewLabel;
+		oEditorValue[sKey] = this.processOutputValue(oItemToEdit);
+
+		this.setValue(oEditorValue);
+	};
+
+	ParametersEditor.prototype.setConfig = function(oConfig) {
+		// Config scenario
+		if (oConfig.allowTypeChange === false && oConfig.allowKeyChange === false) {
+			this.setFragment("sap.ui.integration.designtime.cardEditor.propertyEditor.parametersEditor.ParametersConfigurationEditor");
+		}
+		MapEditor.prototype.setConfig.apply(this, arguments);
+	};
+
+	ParametersEditor.prototype._isValidItem = function(oItem, oOriginalItem) {
+		// If invalid entries should be excluded, only keep items which have a type in the manifest or have a string value
+		var sType = oOriginalItem.type;
+		var vValue = oOriginalItem.value;
+		var aAllowedTypes = this._getAllowedTypes();
+
+		return (
+			sType && includes(aAllowedTypes, sType) ||
+			typeof vValue === "string" && includes(aAllowedTypes, "string")
+		);
+	};
 
 	return ParametersEditor;
 });
