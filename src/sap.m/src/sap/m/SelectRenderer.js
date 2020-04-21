8448a30d6bffa08dc7b0e777229d08691650f99e
Boyan Rakilovski <boyan.rakilovski@sap.com>
2020-04-21 19:25:10 +0300

    [FIX] sap.m.Select: reinforced check now added inside control renderer
    Problem description:
    sap.m.SelectRenderer uses sap.m.Select.prototype._getValueIcon method
    without considering the fact the this method can return 'null' value.
    
    Fixes: https://github.com/SAP/openui5/issues/2874
    BCP: 2070173532
    Change-Id: Iff5f4c05ef8eb26c13c09c0de9f1b24cedb063a1

--- src/sap.m/src/sap/m/SelectRenderer.js
+++ src/sap.m/src/sap/m/SelectRenderer.js
@@ -355,6 +355,7 @@ sap.ui.define(['sap/ui/core/Renderer', 'sap/ui/core/IconPool', 'sap/m/library', 
 			var sValueState = this._getValueStateString(oSelect),
 				oSelectedItem = oSelect.getSelectedItem(),
 				bIconOnly = oSelect.getType() === SelectType.IconOnly,
+				oValueIcon = oSelect._getValueIcon(),
 				oAriaLabelledBy,
 				sActiveDescendant,
 				sDesc;
@@ -375,7 +376,7 @@ sap.ui.define(['sap/ui/core/Renderer', 'sap/ui/core/IconPool', 'sap/m/library', 
 			}
 
 			oAriaLabelledBy = {
-				value: sDesc ? oSelect._getValueIcon().getId() : oSelect.getId() + "-label" + sValueState,
+				value: (sDesc && oValueIcon) ? oValueIcon.getId() : oSelect.getId() + "-label" + sValueState,
 				append: true
 			};
 
