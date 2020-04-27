bec6fd67f6c0c6bd759bf98ddcf8c9d0a8067254
Kevin Edinger <kevin.edinger@sap.com>
2020-04-27 08:40:37 +0200

    [INTERNAL] sap.ui.rta: improve RTA VariantManagement Test App
    the app uses some controls with byId() which could be unavailable due to
    a stash change. added safeguards to not throw an exception
    
    Change-Id: I2df598f02fb5583d664c01637ba9bba5342579f3
    BCP: 1970518014

--- src/sap.ui.rta/test/sap/ui/rta/internal/testdata/variantManagement/controller/Main.controller.js
+++ src/sap.ui.rta/test/sap/ui/rta/internal/testdata/variantManagement/controller/Main.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
+	"sap/base/Log",
+	"sap/m/MessageBox",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/fl/LayerUtils",
 	"sap/ui/fl/ControlPersonalizationAPI",
 	"sap/ui/rta/api/startKeyUserAdaptation"
-], function (
+], function(
+	Log,
+	MessageBox,
 	Controller,
 	LayerUtils,
 	ControlPersonalizationAPI,
@@ -14,12 +18,12 @@ sap.ui.define([
 	return Controller.extend("sap.ui.rta.test.variantManagement.controller.Main", {
 		_data: [],
 
-		onInit: function () {
+		onInit: function() {
 			this.iCounter = 0;
 			var oView = this.getView();
 			oView.addStyleClass("sapUiSizeCompact");
 			this._data.push(
-				new Promise(function (resolve) {
+				new Promise(function(resolve) {
 					oView.bindElement({
 						path: "/EntityTypes(Property01='propValue01',Property02='propValue02',Property03='propValue03')",
 						events: {
@@ -31,25 +35,29 @@ sap.ui.define([
 					});
 				}),
 
-				new Promise(function (resolve) {
-					oView.byId("MainForm").bindElement({
-						path: "/EntityTypes2(EntityType02_Property01='EntityType02Property01Value')",
-						events: {
-							dataReceived: resolve
-						},
-						parameters: {
-							expand: "to_EntityType02Nav"
-						}
-					});
+				new Promise(function(resolve) {
+					if (oView.byId("MainForm")) {
+						oView.byId("MainForm").bindElement({
+							path: "/EntityTypes2(EntityType02_Property01='EntityType02Property01Value')",
+							events: {
+								dataReceived: resolve
+							},
+							parameters: {
+								expand: "to_EntityType02Nav"
+							}
+						});
+					} else {
+						resolve();
+					}
 				})
 			);
 		},
 
-		switchToAdaptionMode: function () {
+		switchToAdaptionMode: function() {
 			startKeyUserAdaptation({rootControl: this.getOwnerComponent()});
 		},
 
-		createChanges: function (oEvent) {
+		createChanges: function(oEvent) {
 			var oButton = oEvent.getSource();
 			var mChangeSpecificData = {};
 
@@ -58,81 +66,82 @@ sap.ui.define([
 				layer: LayerUtils.getCurrentLayer()
 			});
 
-			sap.m.MessageBox.show(
-				"Do you want to create personalization changes?", {
-					icon: sap.m.MessageBox.Icon.INFORMATION,
-					title: "Personalization Dialog",
-					actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
-					onClose: function (oAction) {
-						if (oAction === "YES") {
-							if (this.iCounter === 0) {
-								// on first press of "Personalization Changes button"
-								// add dirty changes2
-								// change1: move sections with simple form
-								var oMoveChangeData = {
-									selectorControl: this.byId("ObjectPageLayout"),
-									changeSpecificData: {
-										changeType: "moveControls",
-										movedElements: [{
-											id: this.createId("ObjectPageSectionWithForm"),
-											sourceIndex: 0,
-											targetIndex: 1
-										}],
-										source: {
-											id: this.createId("ObjectPageLayout"),
-											aggregation: "sections"
-										},
-										target: {
-											id: this.createId("ObjectPageLayout"),
-											aggregation: "sections"
-										}
-									}
-								};
-								// change2: remove section with smart form
-								var oRemoveChangeData = {
-									selectorControl: this.byId("ObjectPageSectionWithSmartForm"),
-									changeSpecificData: {
-										changeType: "stashControl"
-									}
-								};
-								ControlPersonalizationAPI.addPersonalizationChanges({controlChanges: [oMoveChangeData, oRemoveChangeData]});
-								this.iCounter++;
-							} else if (this.iCounter === 1) {
-								// on second press of "Personalization Changes button"
-								var oMoveChangeData2 = {
-									selectorControl: this.byId("ObjectPageLayout"),
-									changeSpecificData: {
-										changeType: "moveControls",
-										movedElements: [{
-											id: this.createId("ObjectPageSectionWithVM"),
-											sourceIndex: 2,
-											targetIndex: 0
-										}],
-										source: {
-											id: this.createId("ObjectPageLayout"),
-											aggregation: "sections"
-										},
-										target: {
-											id: this.createId("ObjectPageLayout"),
-											aggregation: "sections"
-										}
-									}
-								};
-
-								ControlPersonalizationAPI.addPersonalizationChanges({
-									controlChanges: [oMoveChangeData2]
-								});
-
-								oButton.setEnabled(false);
-								this.iCounter++;
-							}
+			MessageBox.confirm("Do you want to create personalization changes?", {
+				onClose: function(oAction) {
+					if (oAction === MessageBox.Action.OK) {
+						// there need to be some controls available in order to create the changes
+						if (!this.byId("ObjectPageLayout") || !this.byId("ObjectPageSectionWithSmartForm")) {
+							Log.error("No changes created, Controls missing.");
+							return;
 						}
-					}.bind(this)
-				}
-			);
+
+						if (this.iCounter === 0) {
+							// on first press of "Personalization Changes button"
+							// add dirty changes2
+							// change1: move sections with simple form
+							var oMoveChangeData = {
+								selectorControl: this.byId("ObjectPageLayout"),
+								changeSpecificData: {
+									changeType: "moveControls",
+									movedElements: [{
+										id: this.createId("ObjectPageSectionWithForm"),
+										sourceIndex: 0,
+										targetIndex: 1
+									}],
+									source: {
+										id: this.createId("ObjectPageLayout"),
+										aggregation: "sections"
+									},
+									target: {
+										id: this.createId("ObjectPageLayout"),
+										aggregation: "sections"
+									}
+								}
+							};
+							// change2: remove section with smart form
+							var oRemoveChangeData = {
+								selectorControl: this.byId("ObjectPageSectionWithSmartForm"),
+								changeSpecificData: {
+									changeType: "stashControl"
+								}
+							};
+							ControlPersonalizationAPI.addPersonalizationChanges({controlChanges: [oMoveChangeData, oRemoveChangeData]});
+							this.iCounter++;
+						} else if (this.iCounter === 1) {
+							// on second press of "Personalization Changes button"
+							var oMoveChangeData2 = {
+								selectorControl: this.byId("ObjectPageLayout"),
+								changeSpecificData: {
+									changeType: "moveControls",
+									movedElements: [{
+										id: this.createId("ObjectPageSectionWithVM"),
+										sourceIndex: 2,
+										targetIndex: 0
+									}],
+									source: {
+										id: this.createId("ObjectPageLayout"),
+										aggregation: "sections"
+									},
+									target: {
+										id: this.createId("ObjectPageLayout"),
+										aggregation: "sections"
+									}
+								}
+							};
+
+							ControlPersonalizationAPI.addPersonalizationChanges({
+								controlChanges: [oMoveChangeData2]
+							});
+
+							oButton.setEnabled(false);
+							this.iCounter++;
+						}
+					}
+				}.bind(this)
+			});
 		},
 
-		isDataReady: function () {
+		isDataReady: function() {
 			return Promise.all(this._data);
 		}
 	});
