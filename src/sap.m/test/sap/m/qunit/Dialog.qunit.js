SAP/openui5

commit da997ba896c6e86db9b735c85b35f2ec56e571da

[FIX] sap.m.Dialog: Scrollbar detection improved for IE
On IE, the check for vertical scrollbar was sometimes giving false positives.
Now this check is improved for IE.

Change-Id: I1409d18ce66a659e79d0348d4a91c958340759f2
BCP: 1980317311

src/sap.m/src/sap/m/Dialog.js
@@ -955,14 +955,13 @@ function(
 			// Browsers except chrome do not increase the width of the container to include scrollbar (when width is auto). So we need to compensate
 			if (Device.system.desktop && !oBrowser.chrome) {
 
-				var bHasVerticalScrollbar = $dialogContent[0].clientHeight < $dialogContent[0].scrollHeight,
-					iCurrentWidthAndHeight = $dialogContent.width() + "x" + $dialogContent.height(),
+				var iCurrentWidthAndHeight = $dialogContent.width() + "x" + $dialogContent.height(),
 					bMinWidth = $dialog.css("min-width") !== $dialog.css("width");
 
 				// Apply the fix only if width or height did actually change.
 				// And when the width is not equal to the min-width.
 				if (iCurrentWidthAndHeight !== this._iLastWidthAndHeightWithScroll && bMinWidth) {
-					if (bHasVerticalScrollbar &&						// - there is a vertical scroll
+					if (this._hasVerticalScrollbar() &&					// - there is a vertical scroll
 						(!sContentWidth || sContentWidth == 'auto') &&	// - when the developer hasn't set it explicitly
 						!this.getStretch() && 							// - when the dialog is not stretched
 						$dialogContent.width() < maxDialogWidth) {		// - if the dialog can't grow anymore
@@ -981,6 +980,23 @@ function(
 			if (!this.getStretch() && !this._oManuallySetSize && !this._bDisableRepositioning) {
 				this._applyCustomTranslate();
 			}
+		};
+
+		/**
+		 * Checks if the dialog has a vertical scrollbar.
+		 * @private
+		 * @return {boolean} True if there is a vertical scrollbar, false otherwise
+		 */
+		Dialog.prototype._hasVerticalScrollbar = function() {
+			var $dialogContent = this.$('cont');
+
+			if (Device.browser.msie) {
+				// The scrollHeight property may return incorrect value in IE
+				// so we do the check based on the width of the vertical scrollbar
+				return $dialogContent[0].clientWidth < $dialogContent.outerWidth();
+			}
+
+			return $dialogContent[0].clientHeight < $dialogContent[0].scrollHeight;
 		};
 
 		/**

commit a09cf844f261b04fa820cdf3d1c872e0c2dfe59f

[FIX] sap.m.Dialog: Texts are no longer blurred in Chrome
Chrome has a known issues, that fonts
are arbitrarily blurry at > 100% zoom, when translate transform is used:
https://bugs.chromium.org/p/chromium/issues/detail?id=783388

The fix is not to use translate transform, but changing
the left and top position of the dialog

CR-Id: 002075125800002012472020
BCP: 002075129400004104102019
BCP: 002075129400002683242020
BCP: 002075129400002526122020

This downport includes several fixes: da997ba896c6e86db9b735c85b35f2ec56e571da,
70aa7e7d17e5d2aeaf12333730e18ab76562518d

Change-Id: I416257ae3e5cd75ec21efbf5e9e012f2db1f9c3f

src/sap.m/src/sap/m/Dialog.js
@@ -63,6 +63,9 @@ function(
 		// shortcut for sap.ui.core.ValueState
 		var ValueState = coreLibrary.ValueState;
 
+		// HTML container scrollbar width
+		var iScrollbarWidth = 17;
+
 		/**
 		* Constructor for a new Dialog.
 		*
@@ -409,48 +412,17 @@ function(
 			 * @private
 			 */
 			this.oPopup._applyPosition = function (oPosition, bFromResize) {
-				var scrollPosY;
-				var scrollPosX;
 
 				that._setDimensions();
 				that._adjustScrollingPane();
 
-				//set to hard 50% or the values set from a drag or resize
-				oPosition.at = {};
-
 				if (that._oManuallySetPosition) {
-					oPosition.at.left = that._oManuallySetPosition.x;
-					oPosition.at.top = that._oManuallySetPosition.y;
+					oPosition.at = {
+						left: that._oManuallySetPosition.x,
+						top: that._oManuallySetPosition.y
+					};
 				} else {
-					// the top and left position need to be calculated with the
-					// window scroll position
-					if (window.scrollY === undefined) {
-						scrollPosY = window.pageYOffset;
-					} else {
-						scrollPosY = window.scrollY;
-					}
-
-					// on iOS this can be a negative integer
-					// which is causing the dialog to be rendered partially off-screen
-					if (Device.os.ios || scrollPosY < 0) {
-						scrollPosY = 0;
-					}
-
-					oPosition.at.top = 'calc(50% + ' + scrollPosY + 'px)';
-
-					if (that._bRTL) {
-						oPosition.at.left = 'auto'; // RTL mode adds right 50% so we have to remove left 50%
-					} else {
-						if (window.scrollX === undefined) {
-							scrollPosX = window.pageXOffset;
-						} else {
-							scrollPosX = window.scrollX;
-						}
-						if (Device.os.ios || scrollPosX < 0) {
-							scrollPosX = 0;
-						}
-						oPosition.at.left = 'calc(50% + ' + scrollPosX + 'px)';
-					}
+					oPosition.at = that._calcCenter();
 				}
 
 				//deregister the content resize handler before repositioning
@@ -557,6 +529,7 @@ function(
 		 * @ui5-metamodel This method also will be described in the UI5 (legacy) designtime metamodel
 		 */
 		Dialog.prototype.open = function () {
+
 			var oPopup = this.oPopup;
 			// Set the initial focus to the dialog itself.
 			// The initial focus should be set because otherwise the first focusable element will be focused.
@@ -850,7 +823,7 @@ function(
 			$this.css(oStyles);
 
 			if (!bStretch && !this._oManuallySetSize && !this._bDisableRepositioning) {
-				this._applyCustomTranslate();
+				this._centerDialog();
 			}
 
 			//In Chrome when the dialog is stretched the footer is not rendered in the right position;
@@ -900,14 +873,14 @@ function(
 		Dialog.prototype._onResize = function () {
 			var $dialog = this.$(),
 				$dialogContent = this.$('cont'),
-				dialogClientWidth = $dialogContent[0].clientWidth,
 				dialogContentScrollTop,
 				sContentHeight = this.getContentHeight(),
 				sContentWidth = this.getContentWidth(),
 				iDialogHeight,
 				maxDialogWidth =  Math.floor(window.innerWidth * 0.9), //90% of the max screen size
-				BORDER_THICKNESS = 2, // solves Scrollbar issue in IE when Table is in Dialog// TODO remove after 1.62 version
-				oBrowser = Device.browser;
+				BORDER_THICKNESS = 2, // solves Scrollbar issue in IE when Table is in Dialog// TODO remove after the end of support for Internet Explorer
+				oBrowser = Device.browser,
+				iTotalChildrenHeight = 0;
 
 			//if height is set by manually resizing return;
 			if (this._oManuallySetSize) {
@@ -926,27 +899,34 @@ function(
 					height: 'auto'
 				});
 
-				//set the newly calculated size by getting it from the browser rendered layout - by the max-height
-				iDialogHeight = parseFloat($dialog.height()) + BORDER_THICKNESS;
-				$dialogContent.height(Math.round( iDialogHeight));
+				$dialog.children().each(function() {
+					iTotalChildrenHeight += jQuery(this).outerHeight(true);
+				});
+				if (this.getStretch() ||  iTotalChildrenHeight > $dialog.innerHeight()) {
+					//set the newly calculated size by getting it from the browser rendered layout - by the max-height
+					iDialogHeight = parseFloat($dialog.height()) + BORDER_THICKNESS;
+					$dialogContent.height(Math.round( iDialogHeight));
+				}
 
 				$dialogContent.scrollTop(dialogContentScrollTop);
 			}
 
-			// IE, EDGE and Firefox (when width is auto) have specific container behavior (e.g. div with 500px width is about 17px smaller when it has vertical scrollbar
-			if ((oBrowser.internet_explorer || oBrowser.edge || oBrowser.firefox)) {
+			// Browsers except chrome do not increase the width of the container to include scrollbar (when width is auto). So we need to compensate
+			if (Device.system.desktop && !oBrowser.chrome) {
 
-				var iVerticalScrollBarWidth = Math.ceil($dialogContent.outerWidth() - dialogClientWidth),
-					iCurrentWidthAndHeight = $dialogContent.width() + "x" + $dialogContent.height();
+				var iCurrentWidthAndHeight = $dialogContent.width() + "x" + $dialogContent.height(),
+					bMinWidth = $dialog.css("min-width") !== $dialog.css("width");
 
-				if (iCurrentWidthAndHeight !== this._iLastWidthAndHeightWithScroll) { // apply the fix only if width or height did actually change
-					if (iVerticalScrollBarWidth > 0 &&					// - there is a vertical scroll
+				// Apply the fix only if width or height did actually change.
+				// And when the width is not equal to the min-width.
+				if (iCurrentWidthAndHeight !== this._iLastWidthAndHeightWithScroll && bMinWidth) {
+					if (this._hasVerticalScrollbar() &&					// - there is a vertical scroll
 						(!sContentWidth || sContentWidth == 'auto') &&	// - when the developer hasn't set it explicitly
 						!this.getStretch() && 							// - when the dialog is not stretched
 						$dialogContent.width() < maxDialogWidth) {		// - if the dialog can't grow anymore
 
 						$dialog.addClass("sapMDialogVerticalScrollIncluded");
-						$dialogContent.css({"padding-right" : iVerticalScrollBarWidth});
+						$dialogContent.css({"padding-right" : iScrollbarWidth});
 						this._iLastWidthAndHeightWithScroll = iCurrentWidthAndHeight;
 					} else {
 						$dialog.removeClass("sapMDialogVerticalScrollIncluded");
@@ -957,36 +937,52 @@ function(
 			}
 
 			if (!this.getStretch() && !this._oManuallySetSize && !this._bDisableRepositioning) {
-				this._applyCustomTranslate();
+				this._centerDialog();
 			}
 		};
 
 		/**
+		 * Checks if the dialog has a vertical scrollbar.
+		 * @private
+		 * @return {boolean} True if there is a vertical scrollbar, false otherwise
+		 */
+		Dialog.prototype._hasVerticalScrollbar = function() {
+			var $dialogContent = this.$('cont');
+
+			if (Device.browser.msie) {
+				// The scrollHeight property may return incorrect value in IE
+				// so we do the check based on the width of the vertical scrollbar
+				return $dialogContent[0].clientWidth < $dialogContent.outerWidth();
+			}
+
+			return $dialogContent[0].clientHeight < $dialogContent[0].scrollHeight;
+		};
+
+		/**
+		 * Centers the dialog
 		 *
 		 * @private
 		 */
-		Dialog.prototype._applyCustomTranslate = function() {
-			var $dialog = this.$(),
-				sTranslateX,
-				sTranslateY,
-				iDialogWidth = $dialog.innerWidth(),
-				iDialogHeight = $dialog.innerHeight();
+		Dialog.prototype._centerDialog = function() {
+			this.$().css(this._calcCenter());
+		};
 
-			if (Device.system.desktop && (iDialogWidth % 2 !== 0 || iDialogHeight % 2 !== 0)) {
-				if (!this._bRTL) {
-					sTranslateX = '-' + Math.floor(iDialogWidth / 2) + "px";
-				} else {
-					sTranslateX = Math.floor(iDialogWidth / 2) + "px";
-				}
+		/**
+		 * Calculates "left" and "top" positions, so the dialog is centered.
+		 *
+		 * @private
+		 */
+		Dialog.prototype._calcCenter = function () {
+			var windowWidth = window.innerWidth,
+				windowHeight = window.innerHeight,
+				$this = this.$(),
+				dialogWidth = $this.outerWidth(),
+				dialogHeight  = $this.outerHeight();
 
-				sTranslateY = '-' + Math.floor(iDialogHeight / 2) + "px";
-				var sCalculatedPosition = 'translate(' + sTranslateX + ',' + sTranslateY + ') scale(1) ';
-				$dialog.css('transform', sCalculatedPosition );
-				$dialog.css('-webkit-transform', sCalculatedPosition + ' translateZ(0px)');
-			} else {
-				$dialog.css('transform', '');
-				$dialog.css('-webkit-transform', '');
-			}
+			return {
+				left: Math.round((windowWidth - dialogWidth) / 2),
+				top: Math.round((windowHeight - dialogHeight) / 2)
+			};
 		};
 
 		/**
@@ -1764,7 +1760,6 @@ function(
 
 					//call the reposition
 					this.oPopup && this.oPopup._applyPosition(this.oPopup._oLastPosition, true);
-					this._$dialog.removeClass('sapMDialogTouched');
 
 					//BCP: 1880238929
 					$dialogContent.css({
@@ -1840,8 +1835,6 @@ function(
 					that._bDisableRepositioning = true;
 
 					that._$dialog.addClass('sapDialogDisableTransition');
-					//remove the transform translate
-					that._$dialog.addClass('sapMDialogTouched');
 
 					that._oManuallySetPosition = {
 						x: initial.position.x,
@@ -1852,9 +1845,7 @@ function(
 					that._$dialog.css({
 						left: Math.min(Math.max(0, that._oManuallySetPosition.x), windowWidth - initial.width),
 						top: Math.min(Math.max(0, that._oManuallySetPosition.y), windowHeight - initial.height),
-						width: initial.width,
-						height: initial.height,
-						transform: ""
+						width: initial.width
 					});
 				}
 
@@ -1877,8 +1868,7 @@ function(
 							//move the dialog
 							that._$dialog.css({
 								left: Math.min(Math.max(0, that._oManuallySetPosition.x), windowWidth - initial.width),
-								top: Math.min(Math.max(0, that._oManuallySetPosition.y), windowHeight - initial.outerHeight),
-								transform: ""
+								top: Math.min(Math.max(0, that._oManuallySetPosition.y), windowHeight - initial.outerHeight)
 							});
 						});
 					});
@@ -1914,7 +1904,6 @@ function(
 
 							if (that._bRTL) {
 								styles.left = Math.min(Math.max(event.pageX, 0), maxLeftOffset);
-								styles.transform = "";
 								that._oManuallySetSize.width = initial.width + initial.x - Math.max(event.pageX, 0);
 							}
 

src/sap.m/src/sap/m/themes/base/Dialog.less
@@ -15,16 +15,17 @@
 }
 
 .sapMDialog {
+	// scale(1) - Chrome does not clip children when transform is not applied resulting in non-visible border-radius
+	// translateZ(0px) - Safari Problem with z-index - After dialog was opened it was inserted behind the dark overlay
+	-webkit-transform: scale(1) translateZ(0px);
 	background-clip: padding-box;
 	min-height: 6rem;
 	outline: none;
 	overflow: hidden;
-	position: absolute;
+	position: fixed !important;
 	opacity: 0;
-	-webkit-transition: opacity .15s, transform 0.2s;
-	transition: opacity .15s, transform 0.2s;
-	transform: translate(-50%, -50%) scale(1);
-	-webkit-transform: translate(-50%, -50%) scale(1) translateZ(0px);
+	-webkit-transition: opacity .15s, left 0.2s, top 0.2s;
+	transition: opacity .15s, left 0.2s, top 0.2s;
 	border-radius: 0.25rem;
 	box-shadow: @sapUiShadowLevel3;
 
@@ -34,15 +35,6 @@
 		-webkit-filter: blur(0);
 		filter: blur(0);
 	}
-}
-
-//the rtl will change the transform-translate from (-50% -50%) to (50% -50%)
-[dir="rtl"] .sapMDialog {
-	left: 50%; //this will be changed 'right: 50%' from the rtl mode
-}
-
-[dir="rtl"] .sapMDialog.sapMDialogTouched {
-	left: auto;
 }
 
 // set max height and max width ========================================================================================
@@ -108,8 +100,6 @@
 }
 
 .sapMDialog.sapMDialogStretched {
-	transform: none;
-	-webkit-transform: scale(1) translateZ(0px); // Chrome does not clip children when transform is not applied resulting in non-visible border-radius
 	top: 5% !important; //this is for preventing inline size/top sizes set the top
 	bottom: 5% !important;
 	left: 3.5% !important; //this is for preventing inline size/top sizes set the top
@@ -253,8 +243,6 @@ html.sap-tablet:not(.sap-combi) .sapMDialog {
 
 .sapMDialogOpen {
 	opacity: 1;
-	transform: translate(-50%, -50%) scale(1);
-	-webkit-transform: translate(-50%, -50%) scale(1) translateZ(0px);
 }
 
 .sapMDialogBlockLayerAnimation {
@@ -423,19 +411,13 @@ html.sap-tablet:not(.sap-combi) .sapMDialog {
 	user-select: none;
 }
 
-//remove the transform translate
-.sapMDialogTouched {
-	transform: scale(1);
-	-webkit-transform: scale(1) translateZ(0px);
-}
-
 .sapDialogDisableTransition {
 	transition: none;
 	-webkit-transition: none;
 }
 
 .sapMDialogResizeHandler {
-	bottom: 0rem;
+	bottom: 0;
 	cursor: se-resize;
 	position: absolute;
 	right: 0;

src/sap.m/test/sap/m/qunit/Dialog.qunit.js
@@ -55,26 +55,11 @@ sap.ui.define([
 	var ButtonType = mobileLibrary.ButtonType;
 
 	createAndAppendDiv("content");
-	var styleElement = document.createElement("style");
-	styleElement.textContent =
-		"html {" +
-		"    /* This is needed because Popup.js sets the overflow hidden to html tag after modal popup is positioned," +
-		"       which makes the position calculation wrong */" +
-		"    overflow: hidden;" +
-		"}";
-	document.head.appendChild(styleElement);
-
-
-	var realTimeout = setTimeout;
-
-
 
 	function marginCompare(value1, value2, margin) {
-		var margin = margin || 2;
-		return (Math.abs(value1 - value2)) < margin;
+		var _margin = margin || 2;
+		return (Math.abs(value1 - value2)) < _margin;
 	}
-
-
 
 	function overwriteAnimationIE(oDialog) {// TODO remove after 1.62 version
 		if (Device.browser.msie) {
@@ -357,11 +342,11 @@ sap.ui.define([
 		assert.ok(oDialog.isOpen(), "Dialog is already open");
 		this.clock.tick(600);
 		var $Dialog = jQuery.sap.byId("dialog"),
-				$ScrollDiv = oDialog.$("scroll"),
-				oTitleDom = jQuery.sap.domById(oDialog.getId() + "-title"),
-				oSubHeaderDom = $Dialog.children(".sapMDialogSubHeader")[0],
-				oIconDom = jQuery.sap.domById(oDialog.getId() + "-icon"),
-				oSearchField = sap.ui.getCore().byId("__field0").getFocusDomRef();
+			$ScrollDiv = oDialog.$("scroll"),
+			oTitleDom = jQuery.sap.domById(oDialog.getId() + "-title"),
+			oSubHeaderDom = $Dialog.children(".sapMDialogSubHeader")[0],
+			oIconDom = jQuery.sap.domById(oDialog.getId() + "-icon"),
+			oSearchField = sap.ui.getCore().byId("__field0").getFocusDomRef();
 		assert.ok(jQuery.sap.domById("dialog"), "dialog is rendered after it's opened.");
 		assert.ok($Dialog.closest("#sap-ui-static")[0], "dialog should be rendered inside the static uiArea.");
 		assert.ok(oSubHeaderDom, "Sub header should be rendered inside the dialog");
@@ -464,12 +449,12 @@ sap.ui.define([
 			resultingScrollPaneWidth;
 
 		//System under Test
-				sut = new Dialog({
-					contentWidth: "500px",
-					content: new Text({
-						text: "This is just a sample text with width set to 700 px. We are testing vertical scrolling of wider content.",
-						width: "700px"})
-				});
+		sut = new Dialog({
+			contentWidth: "500px",
+			content: new Text({
+				text: "This is just a sample text with width set to 700 px. We are testing vertical scrolling of wider content.",
+				width: "700px"})
+		});
 
 		//Act
 		sut.open();
@@ -495,11 +480,11 @@ sap.ui.define([
 		});
 
 		var resultingContentWidth,
-				resultingScrollPaneWidth,
-		//System under Test
-				sut = new Dialog({
-					content: new HTML({content: '<p style="width: 90px"></p>'})
-				});
+			resultingScrollPaneWidth,
+			//System under Test
+			sut = new Dialog({
+				content: new HTML({content: '<p style="width: 90px"></p>'})
+			});
 
 		//Act
 		sut.open();
@@ -596,12 +581,12 @@ sap.ui.define([
 		this.stub(Device, "system", oSystem);
 
 		var oDialog = new Dialog({
-					content: new HTML({
-						content: "<div style='width: 1000px; height: 1000px'></div>"
-					}),
-					contentWidth: "500px"
+				content: new HTML({
+					content: "<div style='width: 1000px; height: 1000px'></div>"
 				}),
-				iValue = 500;
+				contentWidth: "500px"
+			}),
+			iValue = 500;
 
 		oDialog.open();
 		this.clock.tick(500);
@@ -690,24 +675,24 @@ sap.ui.define([
 		var done = assert.async();
 		// arrange
 		var escapeHandlerFunction = function (oPromise) {
-			oPromise.resolve();
-		},
+				oPromise.resolve();
+			},
 
-		fnEscapeHandlerFunctionSpy = this.spy(escapeHandlerFunction),
+			fnEscapeHandlerFunctionSpy = this.spy(escapeHandlerFunction),
 
-		oDialog = new Dialog('escapeDialog', {
-			escapeHandler: fnEscapeHandlerFunctionSpy,
-			afterClose: function (oEvent) {
-				assert.strictEqual(oDialog.isOpen(), false, 'Dialog should be closed.');
-				assert.notOk(oEvent.getParameter("origin"), 'Origin should not be set.');
+			oDialog = new Dialog('escapeDialog', {
+				escapeHandler: fnEscapeHandlerFunctionSpy,
+				afterClose: function (oEvent) {
+					assert.strictEqual(oDialog.isOpen(), false, 'Dialog should be closed.');
+					assert.notOk(oEvent.getParameter("origin"), 'Origin should not be set.');
 
-				oDialog.destroy();
-				oDialog = null;
+					oDialog.destroy();
+					oDialog = null;
 
-				done();
-				done = null;
-			}
-		});
+					done();
+					done = null;
+				}
+			});
 
 		// act
 		oDialog.open();
@@ -736,10 +721,10 @@ sap.ui.define([
 		var oBBtn = new Button("beginButton", { text: "beginButton" });
 		var oEBtn = new Button("endButton", { text: "endButton" });
 		var aButtons = [
-				new Button("buttons1", { text: "buttons1" }),
-				new Button("buttons2", { text: "buttons2" }),
-				new Button("buttons3", { text: "buttons3" })
-			];
+			new Button("buttons1", { text: "buttons1" }),
+			new Button("buttons2", { text: "buttons2" }),
+			new Button("buttons3", { text: "buttons3" })
+		];
 
 		// instantiate dialog with begin and endButton
 		var oDialog = new Dialog("testDialog", {
@@ -804,10 +789,10 @@ sap.ui.define([
 		var oBBtn = new Button("beginButton", { text: "beginButton" });
 		var oEBtn = new Button("endButton", { text: "endButton" });
 		var aButtons = [
-				new Button("buttons1", { text: "buttons1" }),
-				new Button("buttons2", { text: "buttons2" }),
-				new Button("buttons3", { text: "buttons3" })
-			];
+			new Button("buttons1", { text: "buttons1" }),
+			new Button("buttons2", { text: "buttons2" }),
+			new Button("buttons3", { text: "buttons3" })
+		];
 
 		// instantiate dialog with begin/endButton
 		var oDialog = new Dialog("testDialog", {
@@ -957,7 +942,7 @@ sap.ui.define([
 		var subHeaderId = oDialog.$().find('.sapMDialogSubHeader > .sapMBar').attr('id');
 		var dialogAriaLabelledBy = oDialog.getAriaLabelledBy();
 		assert.strictEqual(oDialog.getDomRef().getAttribute('aria-labelledby'), dialogTitleId + ' ' + subHeaderId +
-				' ' + TEXT_ID);
+			' ' + TEXT_ID);
 		assert.strictEqual(dialogAriaLabelledBy.length, 3);
 		assert.strictEqual(dialogAriaLabelledBy.indexOf(dialogTitleId), 0);
 		assert.strictEqual(dialogAriaLabelledBy.indexOf(subHeaderId), 1);
@@ -1175,10 +1160,10 @@ sap.ui.define([
 	QUnit.test("Container Padding Classes", function (assert) {
 		// System under Test + Act
 		var oContainer = new Dialog(),
-				sContentSelector = "section > .sapMDialogScroll > .sapMDialogScrollCont",
-				sResponsiveSize = (Device.resize.width <= 599 ? "0" : (Device.resize.width <= 1023 ? "16px" : "16px 32px")), // eslint-disable-line no-nested-ternary
-				aResponsiveSize = sResponsiveSize.split(" "),
-				$containerContent;
+			sContentSelector = "section > .sapMDialogScroll > .sapMDialogScrollCont",
+			sResponsiveSize = (Device.resize.width <= 599 ? "0" : (Device.resize.width <= 1023 ? "16px" : "16px 32px")), // eslint-disable-line no-nested-ternary
+			aResponsiveSize = sResponsiveSize.split(" "),
+			$containerContent;
 
 		// Act
 		oContainer.placeAt("qunit-fixture");
@@ -1400,6 +1385,63 @@ sap.ui.define([
 		assert.ok(iInitialHeight === iAfterDragHeight, "The height of the dialog should not change after dragging it.");
 
 		// cleanup
+		jQuery(document).off("mouseup mousemove");
+		oDialog.destroy();
+	});
+
+	QUnit.test("Check if dialog height is getting smaller after removing part of the content", function(assert) {
+		// arrange
+		var oDialog = new Dialog({
+			draggable: true,
+			title: "Some title",
+			beginButton: new Button({text: "button"}),
+			content: [
+				// add 3 texts to avoid same height - because there is min height of 3 rems
+				new Text({
+					text: "Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag"
+				}),
+				new Text({
+					text: "Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag"
+				}),
+				new Text("txt", {
+					text: "Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag Some looooooong looong looooong long text that shouldn't affect the dialog's size on drag"
+				})
+			]
+		});
+
+		// act
+		oDialog.open();
+
+		this.clock.tick(500);
+
+		var oMockEvent = {
+			pageX: 608,
+			pageY: 646,
+			offsetX: 177,
+			offsetY: 35,
+			preventDefault: function () {},
+			stopPropagation: function () {},
+			target: oDialog.getAggregation("_header").$().find(".sapMBarPH")[0]
+		};
+
+		var iInitialHeight = oDialog.$().height();
+
+		oDialog.onmousedown(oMockEvent);
+
+		this.clock.tick(500);
+
+		sap.ui.getCore().byId("txt").setVisible(false);
+		sap.ui.getCore().applyChanges();
+		oDialog._onResize();
+
+		var iAfterResizeHeight = oDialog.$().height();
+
+		// assert
+		assert.ok(iInitialHeight > iAfterResizeHeight, "The height of the dialog is smaller than the initial height.");
+
+		// cleanup
+		jQuery(document).off("mouseup mousemove");
+
 		oDialog.destroy();
 	});
 
@@ -1426,48 +1468,14 @@ sap.ui.define([
 
 		this.clock.tick(500);
 
-		window.scrollY = 50;
-
-		this.oDialog.oPopup._applyPosition(this.mockPosition);
-
-		// assert
-		assert.strictEqual(this.mockPosition.at.top, "calc(50% + 0px)", "The top position should not add additional pixels in scrollPosY ");
-		assert.strictEqual(this.mockPosition.at.left, "calc(50% + 0px)", "The left position should not add additional pixels in scrollPosX");
-
-	});
-
-	QUnit.test("ScrollY should be included in the top position calculation", function(assert) {
-		this.stub(Device, "os", {ios: false});
-
-		// act
-		this.oDialog.open();
-
-		this.clock.tick(500);
-
-		window.scrollY = 50;
-
-		this.oDialog.oPopup._applyPosition(this.mockPosition);
-
-		// assert
-		assert.strictEqual(this.mockPosition.at.top, "calc(50% + 50px)", "The top position should include scrollPosY");
-		assert.strictEqual(this.mockPosition.at.left, "calc(50% + 0px)", "The left position should not add additional pixels in scrollPosX");
-	});
-
-	QUnit.test("ScrollY should not be included in the top position calculation", function(assert) {
-		this.stub(Device, "os", {ios: false});
-
-		// act
-		this.oDialog.open();
-
-		this.clock.tick(500);
-
 		window.scrollY = -50;
 
 		this.oDialog.oPopup._applyPosition(this.mockPosition);
 
 		// assert
-		assert.strictEqual(this.mockPosition.at.top, "calc(50% + 0px)", "The top position should not add additional pixels in scrollPosY ");
-		assert.strictEqual(this.mockPosition.at.left, "calc(50% + 0px)", "The left position should not add additional pixels in scrollPosX");
+		assert.strictEqual(this.mockPosition.at.top, Math.round((window.innerHeight - this.oDialog.$().outerHeight()) / 2), "The top position should not add additional pixels in scrollPosY ");
+		assert.strictEqual(this.mockPosition.at.left, Math.round((window.innerWidth - this.oDialog.$().outerWidth()) / 2), "The left position should not add additional pixels in scrollPosX");
+
 	});
 
 	QUnit.module("Resizable Dialog",{