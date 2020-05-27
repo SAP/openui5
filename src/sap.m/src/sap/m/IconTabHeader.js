a55233537843d9c5321518d566eda95ff0e956d0
Lidiya Georgieva <lidiya.georgieva@sap.com>
2020-05-27 15:48:34 +0300

    [FIX] sap.m.IconTabBar: "More" button no longer blinks on initial rendering
    Change-Id: Id61295fc02208cb0b4c36e4f4f268e088868eedc
    BCP: 2070253730

--- src/sap.m/src/sap/m/IconTabHeader.js
+++ src/sap.m/src/sap/m/IconTabHeader.js
@@ -900,6 +900,7 @@ sap.ui.define([
 			oItem.classList.add("sapMITBFilterHidden");
 		}
 
+		this._getOverflow().$().toggleClass("sapMITHOverflowVisible", iLastVisible + 1 !== aItems.length);
 		this.$().toggleClass("sapMITHOverflowList", iLastVisible + 1 !== aItems.length);
 	};
 
