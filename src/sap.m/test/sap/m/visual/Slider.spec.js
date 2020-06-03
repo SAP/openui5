54d7b9b05207b88d2a09cbe41e7b3ff18f9a692a
Martin Georgiev <martin.georgiev@sap.com>
2020-06-03 08:50:39 +0300

    [INTERNAL] sap.m.Slider: Visual tests selectors updated
    The broken selectors are updated in order to allow
    the tests to run successful.
    
    Change-Id: Id502bbf7eba36193e91e8f90cf4d625893b7222d

--- src/sap.m/test/sap/m/visual/Slider.spec.js
+++ src/sap.m/test/sap/m/visual/Slider.spec.js
@@ -36,30 +36,30 @@ describe("sap.m.Slider", function() {
 	});
 
 	it("should show Slider with tickmarks", function() {
-		expect(takeScreenshot(element(by.id("__slider12")))).toLookAs("slider-tickmarks");
+		expect(takeScreenshot(element(by.id("__slider10")))).toLookAs("slider-tickmarks");
 	});
 
 	it("should show Slider with tickmarks and labels", function() {
-		expect(takeScreenshot(element(by.id("__slider13")))).toLookAs("slider-tickmarks-and-labels");
+		expect(takeScreenshot(element(by.id("__slider11")))).toLookAs("slider-tickmarks-and-labels");
 	});
 
 	it("should show Slider with tickmarks and labels 70% width", function() {
-		expect(takeScreenshot(element(by.id("__slider14")))).toLookAs("slider-tickmarks-70percent");
+		expect(takeScreenshot(element(by.id("__slider12")))).toLookAs("slider-tickmarks-70percent");
 	});
 
 	it("should show Slider with tickmarks and labels 300px width", function() {
-		expect(takeScreenshot(element(by.id("__slider15")))).toLookAs("slider-tickmarks-300px");
+		expect(takeScreenshot(element(by.id("__slider13")))).toLookAs("slider-tickmarks-300px");
 	});
 
 	it("should show hide some tickmarks when Slider is resized", function() {
-		browser.executeScript('sap.ui.getCore().byId("__slider14").setWidth("20%")').then(function() {
-			expect(takeScreenshot(element(by.id("__slider14")))).toLookAs("slider-tickmarks-and-labels-resized");
+		browser.executeScript('sap.ui.getCore().byId("__slider12").setWidth("20%")').then(function() {
+			expect(takeScreenshot(element(by.id("__slider12")))).toLookAs("slider-tickmarks-and-labels-resized");
 		});
 	});
 
 	it("should show the same amount of tickmarks after the size is back to the initial", function() {
-		browser.executeScript('sap.ui.getCore().byId("__slider14").setWidth("70%")').then(function() {
-			expect(takeScreenshot(element(by.id("__slider14")))).toLookAs("slider-tickmarks-and-labels-resized2");
+		browser.executeScript('sap.ui.getCore().byId("__slider12").setWidth("70%")').then(function() {
+			expect(takeScreenshot(element(by.id("__slider12")))).toLookAs("slider-tickmarks-and-labels-resized2");
 		});
 	});
 });
