47fbd98ac009d65b86383f00178d0ec22403356f
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2020-06-30 17:18:21 +0200

    [INTERNAL] sap.ui.core.sample.odata.v4.(Flat)DataAggregation
    PS1: avoid $direct in manifest.json (use &$direct=true in URL!),
      drop obsolete mockdata
    
    Change-Id: I48377e22610d2a75d94c14c1c425d54c96872092

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/FlatDataAggregation/SandboxModel.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/FlatDataAggregation/SandboxModel.js
@@ -13,30 +13,6 @@ sap.ui.define([
 
 	var oMockData = {
 		mFixture : {
-			"$metadata" : {
-				source : "metadata.xml"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/concat(aggregate(SalesNumber,$count%20as%20UI5__count),top(0))" : {
-				source : "BusinessPartners_0_1.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/concat(aggregate(SalesNumber,$count%20as%20UI5__count),top(4))" : {
-				source : "BusinessPartners_0_5.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/concat(aggregate(SalesNumber,$count%20as%20UI5__count),top(9))" : {
-				source : "BusinessPartners_0_10.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/concat(aggregate(SalesNumber,$count%20as%20UI5__count),top(14))" : {
-				source : "BusinessPartners_0_15.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/skip(1)/top(4)" : {
-				source : "BusinessPartners_2_6.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/skip(4)/top(5)" : {
-				source : "BusinessPartners_5_10.json"
-			},
-			"BusinessPartners?$apply=groupby((Region),aggregate(SalesNumber))/filter(SalesNumber%20gt%200)/orderby(Region%20desc)/skip(9)/top(5)" : {
-				source : "BusinessPartners_10_15.json"
-			}
 		},
 		sFilterBase : "/serviceroot.svc/",
 		sSourceBase : "sap/ui/core/sample/odata/v4/FlatDataAggregation/data"
