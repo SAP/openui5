3093397d82ea5e4b4524e7eb811209557f4eff33
Konstantin Kondov <konstantin.kondov@sap.com>
2020-07-02 16:44:55 +0300

    [INTERNAL] CardExplorer: Images are now displayed when switching between card samples
    FileUtils.js was expecting responseType to be "Blob" resulting in images not to be loaded and displayed in the editor after MockServer stubbed window.XMLHTTPRequest with fake one (which doesn't support responseType 'blob').
    
    Change-Id: I99d1e155fc4c86004c8ba6a04f215ac94e9f7131
    BCP: 2080184296

--- src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/util/FileUtils.js
+++ src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/util/FileUtils.js
@@ -75,19 +75,48 @@ sap.ui.define([
 			return new Promise(function (resolve, reject) {
 				jQuery.ajax(sUrl, {
 					xhrFields: {
-						responseType: "blob"
-					}
-				}).done(function (oData) {
-					var oReader = new FileReader();
-					oReader.readAsDataURL(oData);
-					oReader.onloadend = function() {
-						var sBase64data = oReader.result;
-						resolve(sBase64data);
-					};
+						responseType: "text" // don't use 'blob' because MockServer doesn't support it
+					},
+					mimeType: "text/plain; charset=x-user-defined"
+				}).done(function (sData) {
+
+					var sBase64EncodedData = 'data:image/png; base64,' + oFileUtils._base64Encode(sData);
+
+					resolve(sBase64EncodedData);
+
 				}).fail(function (jqXHR, sTextStatus, sError) {
 					reject(sError);
 				});
 			});
+		},
+
+		// Used to encode images returned as text to base64
+		_base64Encode: function (sToEncode) {
+			var sCHARSList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+			var sResult = "", iIndex = 0, c1, c2, c3;
+			while (iIndex < sToEncode.length) {
+				c1 = sToEncode.charCodeAt(iIndex++) & 0xff;
+				if (iIndex == sToEncode.length) {
+					sResult += sCHARSList.charAt(c1 >> 2);
+					sResult += sCHARSList.charAt((c1 & 0x3) << 4);
+					sResult += "==";
+					break;
+				}
+				c2 = sToEncode.charCodeAt(iIndex++);
+				if (iIndex == sToEncode.length) {
+					sResult += sCHARSList.charAt(c1 >> 2);
+					sResult += sCHARSList.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+					sResult += sCHARSList.charAt((c2 & 0xF) << 2);
+					sResult += "=";
+					break;
+				}
+				c3 = sToEncode.charCodeAt(iIndex++);
+				sResult += sCHARSList.charAt(c1 >> 2);
+				sResult += sCHARSList.charAt(((c1 & 0x3) << 4) | ((c2 & 0xF0) >> 4));
+				sResult += sCHARSList.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >> 6));
+				sResult += sCHARSList.charAt(c3 & 0x3F);
+			}
+			return sResult;
 		}
 	};
 
