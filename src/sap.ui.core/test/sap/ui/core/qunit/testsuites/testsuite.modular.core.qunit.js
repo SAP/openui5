62cb54d2021661fe5a93f4c23e0671fa51c40cfe
Sven Bender <sven.bender@sap.com>
2020-07-06 20:58:40 +0200

    [INTERNAL] jQuery: Fix htmlPrefilter and show incompatibility error
    Since jQuery 3.5.0, the htmlPrefilter method does not replace
    self-closing elements with additional closing tags anymore.
    This change also removes the functionality and adds a test-mode
    to show an incompatibility hint when a non-void HTML5 element
    is using the self-closing syntax.
    
    CR-Id: 002075125900003172662020
    BCP: 2070230774
    Change-Id: I93bfbccd1d26fba23cfe84c826b7b72fe801ec7f

--- src/sap.ui.core/test/sap/ui/core/qunit/testsuites/testsuite.modular.core.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/testsuites/testsuite.modular.core.qunit.js
@@ -77,6 +77,30 @@ sap.ui.define(function() {
 			},
 			"base/util/LoaderExtensions": {
 				title: "sap.base.util.LoaderExtensions"
+			},
+			"jqueryCompatibilityCheck": {
+				page: "resources/sap/ui/test/starter/Test.qunit.html?testsuite={suite}&test={name}&sap-ui-xx-self-closing-check=true",
+				title: "jqueryCompatibilityCheck",
+				qunit: {
+					version: "edge",
+					reorder: false
+				},
+				sinon: {
+					version: "edge",
+					qunitBridge: true
+				}
+			},
+			"jqueryCompatibilityCheckDisabled": {
+				page: "resources/sap/ui/test/starter/Test.qunit.html?testsuite={suite}&test=jqueryCompatibilityCheck",
+				title: "jqueryCompatibility",
+				qunit: {
+					version: "edge",
+					reorder: false
+				},
+				sinon: {
+					version: "edge",
+					qunitBridge: true
+				}
 			}
 		}
 	};
