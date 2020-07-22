5314457436685d977152e63902dc7812415a6442
Martin Häuser <martin.haeuser@sap.com>
2020-07-22 10:37:45 +0200

    [INTERNAL]P13n*OpaTests: increase timeout for startup
    Change-Id: I434b58e7531ebe4d24b59c1776a841e541d543e2

--- src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/P13nOpa.qunit.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/P13nOpa.qunit.js
@@ -15,6 +15,10 @@ sap.ui.require([
 	"use strict";
 
 	Opa5.extendConfig({
+
+		//Increase the timeout due to failures of the initial startup
+		timeout: 45,
+
 		autoWait: true,
 		async: true,
 		appParams: {
