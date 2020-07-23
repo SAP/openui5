8d5be8291c06e5be91e3b25facc2a9be52f30ac8
Georgi Minkov <georgi.minkov@sap.com>
2020-07-23 17:11:29 +0300

    [INTERNAL] sap.ui.integration.Extension: Requests can now specify their dataType
    This change introduces the capability for Extensions
    to declare what is the Content-Type of the data in the response they expect.
    
    Change-Id: I5eb2b3f58441a13975bc9d1479e1589bb4bcf611
    JIRA: BGSOFUIRODOPI-2387

--- src/sap.ui.integration/test/sap/ui/integration/qunit/extensions/Extension1.js
+++ src/sap.ui.integration/test/sap/ui/integration/qunit/extensions/Extension1.js
@@ -19,16 +19,29 @@ sap.ui.define(["sap/ui/integration/Extension"], function (Extension) {
 
 	oExtension.getData = function () {
 		return Promise.resolve([
-				{ city: "Berlin", description: "Germany" },
-				{ city: "Tokyo", description: "Japan" }
-			]);
+			{ city: "Berlin", description: "Germany" },
+			{ city: "Tokyo", description: "Japan" }
+		]);
 	};
 
 	oExtension.getDataForContent = function () {
 		return Promise.resolve([
-				{ city: "Berlin", description: "Germany" },
-				{ city: "Tokyo", description: "Japan" }
-			]);
+			{ city: "Berlin", description: "Germany" },
+			{ city: "Tokyo", description: "Japan" }
+		]);
+	};
+
+	oExtension.requestWithCustomDataType = function () {
+		return this.getCard().request({
+			url: "some/url",
+			dataType: "xml"
+		}).then(function (oXMLDocument) {
+			var aCities = oXMLDocument.querySelectorAll("City");
+
+			return Array.prototype.map.call(aCities, function (oCity) {
+				return oCity.getAttribute("Name");
+			});
+		});
 	};
 
 	oExtension.getDataForHeader = function () {
