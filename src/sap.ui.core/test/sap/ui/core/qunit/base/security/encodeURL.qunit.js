3cf8b9623c91403869cbe6eddb658c0eaacee290
Frank Weigel <frank.weigel@sap.com>
2020-07-27 23:22:46 +0200

    [FIX] encodeURL: extend encoding to code points beyond 0xffff
    Change-Id: Icdb2c43a00fc50f40fbdf767292d3ea52442b183
    BCP: 2080272749

--- src/sap.ui.core/test/sap/ui/core/qunit/base/security/encodeURL.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/base/security/encodeURL.qunit.js
@@ -17,6 +17,7 @@ sap.ui.define(["sap/base/security/encodeURL"], function(encodeURL) {
 		assert.equal(encodeURL("\x00\x01\x02"), "%00%01%02", "0x00 0x01 0x02");
 		assert.equal(encodeURL(">&<\"\'\\/"), "%3e%26%3c%22%27%5c%2f", ">&<\"\'\\/");
 		assert.equal(encodeURL(String.fromCharCode(1337 * 2)), "%e0%a9%b2", "unicode character above 2048");
+		assert.equal(encodeURL(String.fromCodePoint(151851)), "%f0%a5%84%ab", "Unicode code point above 0xffff");
 	});
 
 	QUnit.test("should not encode", function(assert){
