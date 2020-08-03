ec2b3584988a1fabb8f2d2e0294368fa3a079899
Sven Bender <sven.bender@sap.com>
2020-08-03 11:39:53 +0200

    [FIX] jquery: prevent auto-detection of content type
    Change-Id: I9f18790d0946dd54742d822b0866c192139d5157
    CR-Id: 002075125900003744852020
    BCP: n/a

--- src/sap.ui.core/src/sap/ui/thirdparty/jquery/jquery-1.11.1.js
+++ src/sap.ui.core/src/sap/ui/thirdparty/jquery/jquery-1.11.1.js
@@ -9738,8 +9738,14 @@ function createActiveXHR() {
 	} catch( e ) {}
 }
 
-
-
+// ##### BEGIN: MODIFIED BY SAP
+// Prevent auto-execution of scripts when no explicit dataType was provided (See gh-2432)
+jQuery.ajaxPrefilter( function( s ) {
+	if ( s.crossDomain ) {
+		s.contents.script = false;
+	}
+} );
+// ##### END: MODIFIED BY SAP
 
 // Install script dataType
 jQuery.ajaxSetup({
