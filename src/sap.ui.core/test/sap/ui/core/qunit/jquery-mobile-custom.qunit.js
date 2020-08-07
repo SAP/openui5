709e807bf715e6b72e52f7d219e2059613bc3fed
Florian Vogt <florian.vogt@sap.com>
2020-08-07 16:04:57 +0200

    [INTERNAL] refactor testsuite for swipe event
    Change-Id: I8b59f79baf845c16d267a1c00d04f8f567f5d6a8

--- src/sap.ui.core/test/sap/ui/core/qunit/jquery-mobile-custom.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/jquery-mobile-custom.qunit.js
@@ -2,35 +2,37 @@
 sap.ui.define(["sap/ui/Device"], function (Device) {
     "use strict";
 
-    // this test module should only be executed for browsers other than MS Edge and MS Internet Explorer
-    if (!(Device.browser.msie || Device.browser.edge)) {
-        QUnit.module("Test Touch Support for modern Browsers");
+    QUnit.module("Test Touch Support");
 
-        QUnit.test("Should detect the touch capibilities correctly on touch devices", function (assert) {
-            // Preparation
-            var done = assert.async();
+    QUnit.test("Should detect the touch capibilities correctly on touch devices", function (assert) {
+        // Preparation
+        var done = assert.async();
 
-            // Stub touch support
-            this.stub(Device, "support").value({ touch: true });
+        // Stub touch support
+        this.stub(Device, "support").value({ touch: true });
 
-            // load and boot the core
-            sap.ui.require(["sap/ui/core/Core"], function (core) {
-                sap.ui.getCore().attachInit(function () {
-                    assert.ok(true, "Core should initalize after loading and booting it");
+        // load and boot the core
+        sap.ui.require(["sap/ui/core/Core"], function (core) {
+            sap.ui.getCore().attachInit(function () {
+                assert.ok(true, "Core should initialize after loading and booting it");
 
-                    // its a touch device (tablet, hybrid)
-                    assert.ok(Device.support.touch, "Device is recognized correctly as touch screen");
+                // its a touch device (tablet, hybrid)
+                assert.ok(Device.support.touch, "Device is recognized correctly as touch screen");
 
+                if (Device.browser.edge || Device.browser.msie){
+                    // jQuery.mobile.support.touch should not be overriden by Device.support.touch
+                    assert.notOk(jQuery.mobile.support.touch, "The 'ontouchend' event is not available on MS Edge Legacy and MS Internet Explorer");
+                } else {
                     // jQuery.mobile.support.touch should be overriden by Device.support.touch
                     assert.ok(jQuery.mobile.support.touch, "The 'ontouchend' event is available on the browser");
+                }
 
-                    done();
-                });
-                core.boot();
-            }, function (oErr) {
-                assert.strictEqual(oErr, {}, "Requiring the Core must not fail");
+                done();
             });
-
+            core.boot();
+        }, function (oErr) {
+            assert.strictEqual(oErr, {}, "Requiring the Core must not fail");
         });
-    }
+
+    });
 });
