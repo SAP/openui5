504e8f31619e4b06a45481c917e51876fe345fc7
Frank Weigel <frank.weigel@sap.com>
2020-08-13 17:35:47 +0200

    [FIX] sap/ui/thirdparty/datajs: make OData the default AMD export
    When the UI5 loader is used, its shims ensure that the global 'OData' is
    used as export of the 'sap/ui/thirdparty/datajs' module.
    
    When UI5 exposes itself as standard AMD loader, datajs will export two
    modules: 'datajs' and 'OData'. As none of them matches the requested
    name 'sap/ui/thirdparty/datajs', the loader will pick the first export
    and export it a 2nd time, using the requested name as an alias.
    
    For datajs, this led to the wrong export (datajs instead of OData). As a
    fix, the order could be reversed.
    
    To make the solution more obvious, an explicit export to
    'sap/ui/thirdparty/datajs' has beeen added.
    
    Change-Id: I76e117b2e902e58e8a1fbf4fdb74b2cd49197e87
    Fixes: https://github.com/SAP/openui5/issues/2957
    BCP: 2080356740

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v2/datajs.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v2/datajs.qunit.js
@@ -5,10 +5,16 @@ sap.ui.define([], function() {
 	QUnit.test("datajs loaded and executed", function(assert) {
 		var done = assert.async();
 		sap.ui.require(['sap/ui/thirdparty/datajs'], function(datajs) {
-			assert.ok(typeof window.datajs === 'object' && window.datajs != null, "global datajs property should be a valid object");
-			assert.ok(typeof window.OData === 'object' && window.OData != null, "global OData property should be a valid object");
 			assert.ok(typeof datajs === 'object' && datajs != null, "datajs module should export a valid object");
-			assert.strictEqual(datajs, window.OData, "export of datajs and global OData should be the same");
+			assert.ok(typeof datajs.request === 'function', "datajs export should have a request function");
+			assert.ok(typeof datajs.read === 'function', "datajs export should have a read function");
+
+			if ( !(window.define && window.define.amd) ) {
+				// global exports have to be expected only when there's no standard AMD loader
+				assert.ok(typeof window.datajs === 'object' && window.datajs != null, "global datajs property should be a valid object");
+				assert.ok(typeof window.OData === 'object' && window.OData != null, "global OData property should be a valid object");
+				assert.strictEqual(datajs, window.OData, "export of datajs and global OData should be the same");
+			}
 			done();
 		});
 	});
