94680deae5ce584b0c938b38c3686333c37b2c43
Matthias Osswald <mat.osswald@sap.com>
2020-08-18 18:20:49 +0200

    [FIX] sap/ui/base/syncXHRFix: Properly handle failing sync XHR
    The readyState and SyncRequestOngoing flag need to be updated even when
    the XHR send method fails, which could be due to a failing CORS request.
    
    Otherwise setTimeout callbacks might not be fired anymore until another
    XHR successfully calls the send method.
    
    Change-Id: I0fe5257518eb9a13bacaf4e18db6eee7827ef7d6
    BCP: 002075129400002823642020
    CR-Id: 002075125900003867942020

--- src/sap.ui.core/src/jquery.sap.global.js
+++ src/sap.ui.core/src/jquery.sap.global.js
@@ -573,9 +573,12 @@
 								case "send":
 									return function() {
 										bSyncRequestOngoing = bSync;
-										vProp.apply(oTarget, arguments);
-										iReadyState = oTarget.readyState;
-										bSyncRequestOngoing = false;
+										try {
+											vProp.apply(oTarget, arguments);
+										} finally {
+											iReadyState = oTarget.readyState;
+											bSyncRequestOngoing = false;
+										}
 									};
 							}
 							// All functions need to be wrapped, so they are called on the correct object
