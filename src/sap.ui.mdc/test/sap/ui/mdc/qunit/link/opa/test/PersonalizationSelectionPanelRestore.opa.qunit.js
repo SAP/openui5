378f776532c7cef2d7d0fe14acd0717577808cd9
Janik <janik.koeppel@sap.com>
2020-08-19 11:44:08 +0200

    [INTERNAL] mdc.Link: fix for instability in p13n reset
    Change-Id: Ib8046c450f86e0d49a2b7b72a6790b8079810184

--- src/sap.ui.mdc/test/sap/ui/mdc/qunit/link/opa/test/PersonalizationSelectionPanelRestore.opa.qunit.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/qunit/link/opa/test/PersonalizationSelectionPanelRestore.opa.qunit.js
@@ -142,4 +142,100 @@ sap.ui.define([
 		Given.closeAllNavigationPopovers();
 		Then.iTeardownMyAppFrame();
 	});
+
+	opaTest("When I click on 'Gladiator MX' link in the 'Name' column, popover should open with initiallyVisible link", function(Given, When, Then) {
+		Given.iStartMyAppInAFrame('test-resources/sap/ui/mdc/qunit/link/opa/appUnderTest/start.html');
+		Given.iEnableTheLocalLRep();
+		Given.iClearTheLocalStorageFromRtaRestart();
+
+		When.iClickOnLink("Gladiator MX");
+
+		Then.iShouldSeeNavigationPopoverOpens();
+		Then.iShouldSeeOrderedLinksOnNavigationContainer([
+			"Name Link2 (Superior)"
+		]);
+		Then.iShouldSeeOnNavigationPopoverPersonalizationLinkText();
+	});
+
+	opaTest("When I click on 'More Links' button, the selection dialog opens", function(Given, When, Then) {
+		When.iPressOnLinkPersonalizationButton();
+		Then.thePersonalizationDialogOpens();
+
+		Then.iShouldSeeLinkItemOnPosition("FactSheet of Name", 0);
+		Then.iShouldSeeLinkItemWithSelection("FactSheet of Name", false);
+		Then.iShouldSeeLinkItemAsEnabled("FactSheet of Name", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link2 (Superior)", 1);
+		Then.iShouldSeeLinkItemWithSelection("Name Link2 (Superior)", true);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link2 (Superior)", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link3", 2);
+		Then.iShouldSeeLinkItemWithSelection("Name Link3", false);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link3", true);
+
+		Then.iShouldSeeRestoreButtonWhichIsEnabled(false);
+	});
+
+	opaTest("When I select the 'FactSheet of Name' item, the 'Restore' button should be enabled", function(Given, When, Then) {
+		When.iSelectLink("FactSheet of Name");
+		Then.iShouldSeeRestoreButtonWhichIsEnabled(true);
+
+		Then.iShouldSeeLinkItemOnPosition("FactSheet of Name", 0);
+		Then.iShouldSeeLinkItemWithSelection("FactSheet of Name", true);
+		Then.iShouldSeeLinkItemAsEnabled("FactSheet of Name", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link2 (Superior)", 1);
+		Then.iShouldSeeLinkItemWithSelection("Name Link2 (Superior)", true);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link2 (Superior)", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link3", 2);
+		Then.iShouldSeeLinkItemWithSelection("Name Link3", false);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link3", true);
+
+		When.iPressOkButton();
+
+		Then.thePersonalizationDialogShouldBeClosed();
+		Then.iShouldSeeOrderedLinksOnNavigationContainer([
+			"FactSheet of Name",
+			"Name Link2 (Superior)"
+		]);
+	});
+
+	opaTest("When I click on 'More Links' button, the selection dialog opens", function(Given, When, Then) {
+		When.iPressOnLinkPersonalizationButton();
+		Then.thePersonalizationDialogOpens();
+
+		Then.iShouldSeeLinkItemOnPosition("FactSheet of Name", 0);
+		Then.iShouldSeeLinkItemWithSelection("FactSheet of Name", true);
+		Then.iShouldSeeLinkItemAsEnabled("FactSheet of Name", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link2 (Superior)", 1);
+		Then.iShouldSeeLinkItemWithSelection("Name Link2 (Superior)", true);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link2 (Superior)", true);
+
+		Then.iShouldSeeLinkItemOnPosition("Name Link3", 2);
+		Then.iShouldSeeLinkItemWithSelection("Name Link3", false);
+		Then.iShouldSeeLinkItemAsEnabled("Name Link3", true);
+
+		Then.iShouldSeeRestoreButtonWhichIsEnabled(true);
+	});
+
+	opaTest("When I press 'Restore' then select 'FactSheet of Name' again and then 'OK' button, popover should show the same link selection again", function(Given, When, Then) {
+		When.iPressRestoreButton();
+		Then.iShouldSeeRestoreButtonWhichIsEnabled(false);
+		When.iSelectLink("FactSheet of Name");
+		Then.iShouldSeeRestoreButtonWhichIsEnabled(true);
+		When.iPressOkButton();
+
+		Then.thePersonalizationDialogShouldBeClosed();
+		Then.iShouldSeeNavigationPopoverOpens();
+		Then.iShouldSeeOrderedLinksOnNavigationContainer([
+			"FactSheet of Name",
+			"Name Link2 (Superior)"
+		]);
+		Then.iShouldSeeOnNavigationPopoverPersonalizationLinkText();
+
+		Given.closeAllNavigationPopovers();
+		Then.iTeardownMyAppFrame();
+	});
 });
