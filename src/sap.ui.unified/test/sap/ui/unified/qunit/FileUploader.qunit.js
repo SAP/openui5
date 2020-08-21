f7d6686a3c567f6f4d0c42e3f225e0c6e0655c90
Alexander Ivanov <alexander.ivanov01@sap.com>
2020-08-21 17:16:38 +0300

    [FIX] sap.ui.unified.FileUploader: Fix the unit tests for IE
    Change-Id: I0ea94038aa7bc5ce55e98de521e1bce6bbe75834
    BCP: 2080379176
    BCP: 2080371007
    CR-Id: 002075125900003973072020

--- src/sap.ui.unified/test/sap/ui/unified/qunit/FileUploader.qunit.js
+++ src/sap.ui.unified/test/sap/ui/unified/qunit/FileUploader.qunit.js
@@ -1148,6 +1148,7 @@ sap.ui.define([
 		//Arrange
 		var oSetRequestHeaderSpy = this.spy(XMLHttpRequest.prototype, "setRequestHeader");
 		var oFileUploader = new sap.ui.unified.FileUploader("fu", {
+			uploadUrl: "test",
 			sendXHR: true,
 			useMultipart: false,
 			headerParameters: [
