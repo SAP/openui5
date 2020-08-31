b98ec707d2afd9f213ffac00f324c0033696760c
Sven Bernhard <sven.bernhard@sap.com>
2020-08-31 09:26:25 +0200

    [INTERNAL] sap.ui.core.qunit.internal.BeforePush.Models: fixed path for testsuite
    Depending on the tomcat deployment the path to the test resources  may
    have a prefix like "/testsuite". With this change different deployment
    roots are supported.
    
    http://localhost:8080/testsuite/test-resources/sap/ui/core/qunit/internal/BeforePushModels.html?sap-ui-language=en-US
    
    PS1: Done
    PS2: Review comments
    
    Change-Id: Ie2331a42faac3c0bf60142889eba60cf7cdcc242

--- src/sap.ui.core/test/sap/ui/core/qunit/internal/BeforePush.Models.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/internal/BeforePush.Models.js
@@ -10,6 +10,13 @@
 (function () {
 	"use strict";
 
+	var sPathName = window.location.pathname,
+		sTestSuite = "../qunit/testrunner.html?testpage="
+			// enable both URLs with /testsuite/ (maven/grunt) and without (npm)
+			+ encodeURIComponent(sPathName.slice(0, sPathName.indexOf("/test-resources/"))
+				+ "/test-resources/sap/ui/core/qunit/internal/testsuite.models.qunit.html")
+			+ "&autostart=true";
+
 	window.BeforePush = {
 		tests : {
 			'internal/samples/odata/v2/SalesOrders/Opa.qunit.html' : 'both',
@@ -17,8 +24,9 @@
 			'demokit/sample/ViewTemplate/scenario/Opa.qunit.html?supportAssistant=true' : 'both',
 			'demokit/sample/ViewTemplate/types/Opa.qunit.html?supportAssistant=true' : 'both',
 			'demokit/sample/ViewTemplate/types/Opa.qunit.html?realOData=true' : 'both',
-			'qunit/internal/AnnotationParser.qunit.html?hidepassed&coverage' : 'both',
-			'../qunit/testrunner.html?testpage=%2Ftest-resources%2Fsap%2Fui%2Fcore%2Fqunit%2Finternal%2Ftestsuite.models.qunit.html&autostart=true' : "both"
+			'qunit/internal/AnnotationParser.qunit.html?hidepassed&coverage' : 'both'
 		}
 	};
+
+	window.BeforePush.tests[sTestSuite] = "both";
 }());
