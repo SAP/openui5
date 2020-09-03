7a20955bf94237d19c693a829d153a3562646ff4
Sven Bernhard <sven.bernhard@sap.com>
2020-09-03 16:54:55 +0200

    [FIX] sap.ui.model.analytics.AnalyticalTreeBindingAdapter#setNumberOfExpandedLevels: expand only as many levels as grouped dimensions are available
    A dimension (e.g. CompanyCode) and its text (e.g. CompanyCodeName) can
    be selected as grouped columns. For the analytical table each grouped
    column is counted as a group level, but for the AnalyticalBinding only
    the dimensions are counted as groups. CompanyCode and CompanyCodeName
    represent the same dimension so the AnalyticalBindings counts these two
    grouped columns only as one group.
    AnalyticalTreeBindingAdapter#setNumberOfExpandedLevels needs to restrict
    the maximum number of expand levels to load the leaf entries properly.
    
    http://localhost:8080/testsuite/test-resources/sap/ui/core/qunit/analytics/testsuite4analytics.qunit.html
    
    PS1: Done
    PS2-3: Review comments
    
    Change-Id: I756d24c2a9a7e62d9a890456bdefd311d6b705ce
    BCP: 002075129500005340402020

--- src/sap.ui.core/test/sap/ui/core/qunit/analytics/AnalyticalBinding.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/analytics/AnalyticalBinding.qunit.js
@@ -188,6 +188,16 @@ sap.ui.define([
 			total: false,
 			visible: true
 		},
+		// a grouped text property of a dimension
+		oCostElementTextGrouped = {
+			name: "CostElementText",
+			grouped: true,
+			inResult: false,
+			sortOrder: "Ascending",
+			sorted: false,
+			total: false,
+			visible: true
+		},
 		// Analytical information for properties of "TypeWithHierarchies"
 		oCostCenterDrillstate = {
 			name: "CostCenter_Drillstate",
@@ -3118,6 +3128,56 @@ sap.ui.define([
 				AnalyticalBinding.prototype._findKeyIndex.call(oBinding, "/", oFixture.inputKey[n]),
 				oFixture.resultKeyIndex[n]);
 		}
+	});
+});
+
+	//*********************************************************************************************
+[true, false].forEach(function (bSuppressResetData) {
+	[
+		{iLevels : undefined, numberOfExpandedLevels : 0},
+		{
+			iLevels : -1,
+			log : "Number of expanded levels was set to 0. Negative values are prohibited",
+			numberOfExpandedLevels : 0
+		},
+		{iLevels : 0, numberOfExpandedLevels : 0},
+		{iLevels : 1, numberOfExpandedLevels : 1},
+		{iLevels : 2, numberOfExpandedLevels : 2},
+		// simulate expand all from analytical table
+		{
+			iLevels : 3,
+			log : "Number of expanded levels was reduced from 3 to 2 which is the number of grouped"
+				+ " dimensions",
+			numberOfExpandedLevels : 2
+		}
+	].forEach(function (oFixture) {
+	var sTitle = "setNumberOfExpandedLevels: iLevels = " + oFixture.iLevels
+			+ ", bSuppressResetData = " + bSuppressResetData;
+
+	QUnit.test(sTitle, function (assert) {
+		var done = assert.async(),
+			that = this;
+
+		setupAnalyticalBinding(2, {}, function (oBinding) {
+			// that.oLogMock cannot be used as it mocks AnalyticalBinding.Logger which is not used
+			// in sap.ui.model.analytics.AnalyticalTreeBindingAdapter
+			that.mock(Log).expects("warning")
+				.withExactArgs(oFixture.log, sinon.match.same(oBinding),
+					"sap.ui.model.analytics.AnalyticalTreeBindingAdapter")
+				.exactly(oFixture.log ? 1 : 0);
+			that.mock(oBinding).expects("resetData")
+				.withExactArgs()
+				.exactly(bSuppressResetData ? 0 : 1);
+
+			// code under test
+			oBinding.setNumberOfExpandedLevels(oFixture.iLevels, bSuppressResetData);
+
+			assert.strictEqual(oBinding.mParameters.numberOfExpandedLevels,
+				oFixture.numberOfExpandedLevels);
+
+			done();
+		}, [oCostCenterGrouped, oCostElementGrouped, oCostElementTextGrouped, oActualCostsTotal]);
+	});
 	});
 });
 });
