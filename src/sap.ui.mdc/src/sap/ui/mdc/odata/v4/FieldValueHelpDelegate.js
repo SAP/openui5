cc7922ce61ed737d1970b0a00a8e06feb2cb005a
Thomas Büchler <thomas.buechler@sap.com>
2020-09-03 10:10:30 +0200

    [FIX] mdc.odata.v4.FieldValueHelpDelegate: Handle ListBinding errors
    Change-Id: Ib6c6234899ad812d9bcb7b079733fd152456301b
    (cherry picked from commit 8cf0b1897910b20769f9c1452cc517cad12e5cdd)
    
    CR-Id: 002075125800003167102020
    BCP: 2070280125

--- src/sap.ui.mdc/src/sap/ui/mdc/odata/v4/FieldValueHelpDelegate.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/odata/v4/FieldValueHelpDelegate.js
@@ -48,15 +48,23 @@ sap.ui.define([
 
 	ODataFieldValueHelpDelegate.executeFilter = function(oPayload, oListBinding, oFilter, fnCallback, iRequestedItems) {
 
-		var fnHandleChange = function(oParameters) {
-			if (oParameters.mParameters.detailedReason) {
-				return; // only use the final change event
+		var _bIsFilterExecutionComplete = false;
+
+		var fnHandleListBindingEvent = function (oParameters) {
+			if (oParameters.mParameters.detailedReason) { // do not consider virtualcontext events triggered during automatic determination of $expand and $select
+				return;
 			}
-			oListBinding.detachEvent("change", fnHandleChange);
-			fnCallback();
+
+			if (!_bIsFilterExecutionComplete) {
+				_bIsFilterExecutionComplete = true;
+				oListBinding.detachEvent("change", fnHandleListBindingEvent);
+				fnCallback();
+			}
 		};
 
-		oListBinding.attachEvent("change", fnHandleChange);
+		oListBinding.attachEvent("change", fnHandleListBindingEvent); // Note: The change event might not be fired in error scenarios
+		oListBinding.attachEventOnce("dataReceived", fnHandleListBindingEvent); // Note: According to an earlier change the dataReceived event may not always be fired in some caching scenarios
+
 		oListBinding.initialize();
 		oListBinding.filter(oFilter, FilterType.Application);
 		oListBinding.getContexts(0, iRequestedItems); // trigger request. not all entries needed, we only need to know if there is one, none or more
