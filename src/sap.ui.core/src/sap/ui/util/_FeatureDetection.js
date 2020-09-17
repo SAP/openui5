ed78db3b1c3fd0b4f4635dcc90030051246e0aa3
Jan Mummenthaler <jan.mummenthaler@sap.com>
2020-09-17 12:34:58 +0200

    [FIX] scrollLeftRTL: adapt to chrome behavior change
    Change Chrome's behavior such that when a scrollable element
    has content overflowing to the left then its horizontal scroll
    position will be zero at its initial position and negative when
    scrolled leftward.
    
    This change fixes the scrollPosition calculation in the UI5
    jQuery functions that uses scrollLeft.
    
    BCP: 2070328364
    CR-Id: 002075125800003666302020
    Change-Id: I717da3dfe213693aeda25cc61367fcc518abca7a

---
+++ src/sap.ui.core/src/sap/ui/util/_FeatureDetection.js
@@ -0,0 +1,40 @@
+/*!
+ * ${copyright}
+ */
+/*
+ * IMPORTANT: This is a private module, its API must not be used and is subject to change.
+ * Code other than the OpenUI5 libraries must not introduce dependencies to this module.
+ */
+sap.ui.define([], function() {
+    "use strict";
+    var detectedFeatures = {};
+
+    function detectFeatures() {
+        /* detect scrolling behavior - Begin */
+        // inspired by jQuery.rtl-scroll-type
+        var div = document.createElement("div");
+        div.innerHTML = '<div dir="rtl" style="width: 1px; height: 1px; position: fixed; top: 0px; left: 0px; overflow: hidden"><div style="width: 2px"><span style="display: inline-block; width: 1px"></span><span style="display: inline-block; width: 1px"></span></div></div>';
+        document.documentElement.appendChild(div);
+        var definer = div.firstChild;
+        //check initial value
+        detectedFeatures.initialZero = definer.scrollLeft == 0;
+        //check if scrolling left goes negative
+        definer.scrollLeft = -1;
+        detectedFeatures.canNegative = definer.scrollLeft < 0;
+        document.documentElement.removeChild(div);
+        /* detect scrolling behavior - End */
+    }
+
+    detectFeatures();
+
+    var _FeatureDetection = {
+        canScrollToNegative: function() {
+            return detectedFeatures.canNegative;
+        },
+        initialScrollPositionIsZero: function() {
+            return detectedFeatures.initialZero;
+        }
+    };
+
+    return _FeatureDetection;
+});
