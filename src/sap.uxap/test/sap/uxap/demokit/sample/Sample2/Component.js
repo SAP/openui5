588d59280cf45f136fa1600e85edf844e3e822b7
Minkova <yana.minkova@sap.com>
2020-09-28 10:45:41 +0300

    [INTERNAL] sap.uxap.ObjectPageLayout: Updated samples
    Pattern samples with Header Facet are renamed, in order to meet
    the UX specification.
    
    JIRA: BGSOFUIPIRIN-4844
    Change-Id: I7ee68c693cd59fb769f6213a31445337d5f9d2aa

--- src/sap.uxap/test/sap/uxap/demokit/sample/Sample2/Component.js
+++ src/sap.uxap/test/sap/uxap/demokit/sample/Sample2/Component.js
@@ -1,7 +1,7 @@
 sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("sap.uxap.sample.HeaderFacetSample1.Component", {
+	var Component = UIComponent.extend("sap.uxap.sample.Sample2.Component", {
 		metadata: {
 			manifest: "json"
 		}
