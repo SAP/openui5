2942dd39bbc47f080ef34a119db5434922497b6c
Martin Häuser <martin.haeuser@sap.com>
2020-10-13 12:36:32 +0200

    [INTERNAL]ChartOpaApp: use V4 server + stabilize test
    * use OData V4 server instead of a manually mocked setup
    * use modified matchers for the assertions
    * restructure 'PersonalizationChart' and 'PersonalizationChartVariants'
    * remove duplicates
    
    Change-Id: Ica89e12053624cc64a35ab52d3a4f179fda4da96

--- src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/appUnderTestChart/Component.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/appUnderTestChart/Component.js
@@ -9,12 +9,10 @@
  */
 sap.ui.define([
 	"sap/ui/core/sample/common/Component",
-	"sap/ui/test/TestUtils",
 	"sap/ui/fl/FakeLrepConnectorLocalStorage",
 	'sap/chart/library' // In here as chart lib cannot be loaded in manifest due to interference with sinon - workarround
 ], function (
 	CommonComponent,
-	TestUtils,
 	FakeLrepConnectorLocalStorage,
 	chartLib // In here as chart lib cannot be loaded in manifest due to interference with sinon - workarround
 ) {
@@ -31,7 +29,7 @@ sap.ui.define([
 			FakeLrepConnectorLocalStorage.forTesting.synchronous.store("id_1550048095839_52_Copy", {
 				conditions: {},
 				content: {
-					title: "Country Visible On First Position"
+					title: "Language Visible On First Position"
 				},
 				creation: "2019-02-13T08:54:55.879Z",
 				fileName: "id_1550048095839_52_Copy",
@@ -61,57 +59,13 @@ sap.ui.define([
 				conditions: {},
 				content: {
 					index: 0,
-					kind: "Dimension",
-					label: "Country",
-					name: "Country",
-					propertyPath: "Country",
+					name: "language_code",
 					role: "category"
 				},
 				context: "",
 				creation: "2019-03-18T09:55:05.883Z",
 				dependentSelector: {},
 				fileName: "id_1550048095839_53_addItem",
-				fileType: "change",
-				jsOnly: false,
-				layer: "VENDOR",
-				moduleName: "",
-				namespace: "apps/appUnderTestChart/changes/",
-				oDataInformation: {},
-				originalLanguage: "EN",
-				packageName: "$TMP",
-				projectId: "appUnderTestChart",
-				reference: "appUnderTestChart.Component",
-				selector: {id: "IDViewOfAppUnderTestChart--IDChartOfAppUnderTestChart", idIsLocal: true},
-				id: "IDViewOfAppUnderTestChart--IDChartOfAppUnderTestChart",
-				idIsLocal: true,
-				support: {
-					generator: "Change.createInitialFileContent",
-					sapui5Version: "1.64.0-SNAPSHOT",
-					user: "",
-					service: ""
-				},
-				compositeCommand: "",
-				sourceChangeFileName: "",
-				texts: {},
-				validAppVersions: {creation: "${version}", from: "${version}"},
-				from: "${version}",
-				variantReference: "id_1550048095839_52_Copy"
-			});
-			FakeLrepConnectorLocalStorage.forTesting.synchronous.store("id_1550048095839_54_addItem", {
-				changeType: "addItem",
-				conditions: {},
-				content: {
-					index: 0,
-					kind: "Dimension",
-					label: "Country",
-					name: "Country",
-					propertyPath: "Country",
-					role: "category"
-				},
-				context: "",
-				creation: "2019-03-18T09:55:05.883Z",
-				dependentSelector: {},
-				fileName: "id_1550048095839_54_addItem",
 				fileType: "change",
 				jsOnly: false,
 				layer: "USER",
@@ -140,44 +94,6 @@ sap.ui.define([
 			});
 
 			FakeLrepConnectorLocalStorage.enableFakeConnector();
-			TestUtils.setupODataV4Server(this.oSandbox, {
-				"$metadata": {
-					source: "metadata.xml"
-				},
-				"SalesDenormalized?$apply=groupby((Date,ProductName),aggregate(Amount))": {
-					source: "SalesDenormalized01.json"
-				},
-				"SalesDenormalized?$apply=groupby((Date,ProductName),aggregate(Amount,Forecast))": {
-					source: "SalesDenormalized02.json"
-				},
-				"SalesDenormalized": {
-					source: "SalesDenormalized03.json"
-				},
-				"SalesDenormalized?$apply=groupby((Country,Date,ProductName),aggregate(Amount,Forecast))": {
-					source: "SalesDenormalized04.json"
-				},
-				"SalesDenormalized?$apply=groupby((Country,Date,ProductName),aggregate(Amount))": {
-					source: "SalesDenormalized05.json"
-				},
-				"SalesDenormalized?$apply=groupby((Country,Date,ProductName),aggregate(Amount,Forecast))&$orderby=Forecast": {
-					source: "SalesDenormalized06.json"
-				},
-				"SalesDenormalized?$apply=groupby((ProductName),aggregate(Amount))": {
-					source: "SalesDenormalized07.json"
-				},
-				"SalesDenormalized?$apply=groupby((ProductName),aggregate(Amount,Forecast))": {
-					source: "SalesDenormalized08.json"
-				},
-				"SalesDenormalized?$apply=groupby((ProductName),aggregate(Forecast))": {
-					source: "SalesDenormalized09.json"
-				},
-				"SalesDenormalized?$apply=groupby((ProductName),aggregate(Forecast))&$orderby=Forecast": {
-					source: "SalesDenormalized10.json"
-				},
-				"SalesDenormalized?$apply=groupby((Country,CurrencyCode,Date,ProductName),aggregate(Amount))": {
-					source: "SalesDenormalized11.json"
-				}
-			}, "appUnderTestChart/data", "/sap/opu/odata4/IWBEP/TEA/default/IWBEP/TEA_BUSI/0001/");
 		}
 	});
 });
