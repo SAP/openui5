81a100c9171ca2774d12ffc97e20736e401ebcd2
Christopher Pielka <c.pielka@sap.com>
2020-10-15 09:20:32 +0200

    [INTERNAL] Added TableWithFilterBar example to OpenUI5 repository
    The TableWithFilterBar application was moved to the OpenUI5 repository for easier access when developing mdc functionality.
    The manifest.json remains inside sapui5.runtime since it contains internal URLs as datasources and is loaded from there during runtime.
    JIRA: CPOUIFDENVER-1475
    
    Change-Id: I2c3950b344b11035a88b2b589a5f4d39fd66962e

---
+++ src/sap.ui.mdc/test/sap/ui/mdc/internal/TableWithFilterBar/delegate/Authors.Table.delegate.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+	"sap/ui/mdc/odata/v4/TableDelegate",
+	"./Books.FB.delegate",
+	"sap/ui/mdc/Field",
+	"sap/m/Column"
+], function (ODataTableDelegate, Field, Column, BooksFBDelegate) {
+	"use strict";
+	var AuthorsTableDelegate = Object.assign({}, ODataTableDelegate);
+
+	AuthorsTableDelegate._createColumnTemplate = function (oInfo) {
+
+		var oProps = { value: "{" + (oInfo.path || oInfo.name) + "}", editMode: "Display", multipleLines: false};
+
+		if (oInfo.name === "countryOfOrigin_code") {
+			oProps.value = "{countryOfOrigin/descr}";
+		}
+
+		if (oInfo.name === "regionOfOrigin_code") {
+			oProps.value = "{regionOfOrigin/text}";
+		}
+
+		if (oInfo.name === "cityOfOrigin_city") {
+			oProps.value = "{cityOfOrigin/text}";
+		}
+
+		return Promise.resolve(new Field(oProps));
+	};
+
+	ODataTableDelegate.getFilterDelegate = function() {
+		return {
+			addFilterItem: function(oItem, oTable) {
+				return BooksFBDelegate._createFilterField(oItem, oTable);
+			}
+		};
+	};
+
+	return AuthorsTableDelegate;
+});
---
+++ src/sap.ui.mdc/test/sap/ui/mdc/internal/TableWithFilterBar/delegate/Books.AuthorVH.delegate.js
@@ -0,0 +1,62 @@
+sap.ui.define([
+	"sap/ui/mdc/odata/v4/FieldValueHelpDelegate",
+	"sap/ui/mdc/FilterField"
+], function (ODataFieldValueHelpDelegate, FilterField) {
+	"use strict";
+	var Delegate = Object.assign({}, ODataFieldValueHelpDelegate);
+
+	Delegate.contentRequest = function(oPayload, oFieldHelp, bSuggestion) {
+		var isSuggest = bSuggestion;
+
+		oFieldHelp.setFilterFields("$search");
+
+		if (!oFieldHelp.getContent().getTable()) {
+			oFieldHelp.getContent().setTable(
+				new sap.m.Table({
+					growing: true, growingScrollToLoad: true, growingThreshold: 20,
+					autoPopinMode: true,
+					contextualWidth: "Auto",
+					hiddenInPopin: ["Low"],
+					columns: [
+						new sap.m.Column({width: '5rem', importance:"High", header: new sap.m.Text({text : "ID"})}),
+						new sap.m.Column({header: new sap.m.Text({text : "Name "})}),
+						new sap.m.Column({width: '8rem', visible: !isSuggest, importance:"Low", header: new sap.m.Text({text : "Date of Birth"})})
+					],
+					items: {
+						path : "/Authors",
+						template : new sap.m.ColumnListItem({
+							type: "Active",
+							cells: [new sap.m.Text({text: "{ID}"}),
+									new sap.m.Text({text: "{name}"}),
+									new sap.m.Text({text: "{dateOfBirth}"})]
+						})
+					},
+					width: isSuggest ? "20rem" : "100%"
+				})
+			);
+		} else {
+			var oTableWrapper = oFieldHelp.getContent();
+			var oTable = oTableWrapper.getTable();
+
+			oTable.getColumns()[2].setVisible(!isSuggest);
+			if (isSuggest) {
+				oTable.setWidth("20rem");
+				oTable.getColumns()[1].setWidth("10rem");
+				oTable.getColumns()[2].setVisible(false);
+			} else {
+				oTable.setWidth("100%");
+				oTable.getColumns()[1].setWidth(null);
+				oTable.getColumns()[2].setVisible(true);
+			}
+		}
+
+		return Promise.resolve();
+	};
+
+	Delegate.determineSearchSupported = function(oPayload, oFieldHelp) {
+		oFieldHelp.setFilterFields("$search");
+		return Promise.resolve();
+	};
+
+	return Delegate;
+});
