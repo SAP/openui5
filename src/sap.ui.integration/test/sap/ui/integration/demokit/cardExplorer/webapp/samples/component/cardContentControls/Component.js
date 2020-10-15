e6cc8c06684aa9647d94c816abaea42229bc5b3a
Alexandar Mitsev <alexandar.mitsev@sap.com>
2020-10-15 18:53:27 +0300

    [INTERNAL] Card Explorer: Component card samples are fixed
    Solving the following problem:
    When a Component card sample from the Card Explorer is used in
    SAP Workzone it shows errors for parameters not being translated.
    
    The errors were caused due to the component was loading
    the manifest second time and was parsing it again.
    Solution: Setting "manifest: json" should not be used.
    
    Now the samples are updated so that they don't use this setting.
    
    Change-Id: Idc8a4006f43e7d5c3fd8f6b671199717a71487fa
    BCP: 2070402435

--- src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/component/cardContentControls/Component.js
+++ src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/component/cardContentControls/Component.js
@@ -1,14 +1,9 @@
-sap.ui.define(['sap/ui/core/UIComponent'],
-	function(UIComponent) {
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("my.component.sample.cardContentControls.Component", {
-
-		metadata : {
-			manifest: "json"
-		}
-	});
+	var Component = UIComponent.extend("my.component.sample.cardContentControls.Component");
 
 	return Component;
-
 });
