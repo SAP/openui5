fbb21add6ba7ed7be0f26d1dc8bf8eb9cea198d8
Tobias Sorn <tobias.sorn@sap.com>
2020-10-19 13:20:08 +0200

    [INTERNAL] inclusive-language: change to inclusive terms
    Change from blacklist to blocklist
    Change from whitelist to allowlist
    
    JIRA: CPOUI5FOUNDATION-256
    Change-Id: I8d04d096ce9a93ada5fc1a2fc875bb3ff6a69e29

--- src/sap.ui.core/src/sap/ui/core/rules/CoreHelper.support.js
+++ src/sap.ui.core/src/sap/ui/core/rules/CoreHelper.support.js
@@ -9,15 +9,16 @@ sap.ui.define(["sap/ui/thirdparty/jquery", "sap/ui/dom/jquery/control"],  // jQu
 		"use strict";
 
 		var CoreHelper = {
-			/***
-			 * Checks of passed node has parent control of type UI5.
+			/**
+			 * Checks if passed node has parent control of type UI5.
+			 *
 			 * @param node HTML element that will be checked.
-			 * @param oScope Scope in witch checking will be executed.
+			 * @param oScope Scope in which checking will be executed.
 			 * @returns {boolean} If node has parent of type UI5 control it will return true, otherwise false.
 			 */
 			nodeHasUI5ParentControl : function (node, oScope) {
 				/**
-				 * Here we white list all controls that can contain DOM elements with style different than the framework style
+				 * Here we list all controls that can contain DOM elements with style different than the framework style
 				 */
 				// jQuery Plugin "control"
 				var skipParents = ["sap.ui.core.HTML"],
