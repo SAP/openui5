548de6aec8ab93ac9229e142046b24af41aa7bae
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2020-10-22 16:43:22 +0200

    [FIX] CustomizingConfiguration: respect owner component for configuration
    The owner component ID is now passed into the CustomizingConfiguration.
    This was implicitly the case for synchronous loading.
    The asynchronous controller loading was missing the owner component.
    
    CR-Id: 002075125900004313272020
    Change-Id: Ibbe6d72c7b8fa4adfcbd545e26b654ada9ac423e
    BCP: 0020751294 0000537443 2020
    BCP: 2070417704
    JIRA: CPOUI5FRAMEWORK-158
    (cherry picked from commit 7b71a7749889d169b4c1080db77b299e95916945)

--- src/sap.ui.core/test/sap/ui/core/qunit/component/Customizing_multi.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/Customizing_multi.qunit.js
@@ -5,8 +5,9 @@ sap.ui.define([
 	'sap/ui/core/UIComponent',
 	'sap/ui/core/mvc/Controller',
 	'sap/ui/core/mvc/View',
+	'sap/ui/core/mvc/XMLView',
 	'sap/ui/qunit/QUnitUtils'
-], function(jQuery, Component, ComponentContainer, UIComponent, Controller, View, qutils) {
+], function(jQuery, Component, ComponentContainer, UIComponent, Controller, View, XMLView, qutils) {
 
 	"use strict";
 	/*global QUnit, sinon */
@@ -226,6 +227,233 @@ sap.ui.define([
 		assert.equal(oFirstItem.getText(), "(Customer's replacement ListItem)", "First ListItem should be the customized one");
 		assert.ok(sap.ui.getCore().byId("__jsview0--defaultContentTextView"), "JS extension point 1 should contain default content");
 		assert.ok(sap.ui.getCore().byId("iHaveCausedDestruction"), "JS Extension Point 45 Content has been correctly replaced");
+	});
+
+	QUnit.module("Owner-Component Handling (Controller Extension) - synchronous", {
+		before: function() {
+			sap.ui.predefine("testdata/customizing/synchronous/sap/CompA/Component", ["sap/ui/core/UIComponent"], function(UIComponent) {
+				return UIComponent.extend("testdata.customizing.synchronous.sap.CompA.Component", {
+					metadata: {
+						version : "1.0",
+						customizing: {
+							"sap.ui.controllerExtensions": {
+								"testdata.customizing.synchronous.sap.RootController": {
+									controllerName: "testdata.customizing.synchronous.sap.CompA.ext.Controller"
+								}
+							}
+						}
+					}
+				});
+			});
+
+			sap.ui.predefine("testdata/customizing/synchronous/sap/CompB/Component", ["sap/ui/core/UIComponent"], function(UIComponent) {
+				return UIComponent.extend("testdata.customizing.synchronous.sap.CompB.Component", {
+					metadata: {
+						version : "1.0",
+						customizing: {
+							"sap.ui.controllerExtensions": {
+								"testdata.customizing.synchronous.sap.RootController": {
+									controllerName: "testdata.customizing.synchronous.sap.CompB.ext.Controller"
+								}
+							}
+						}
+					}
+				});
+			});
+
+			sap.ui.controller("testdata.customizing.synchronous.sap.RootController", {
+				getValue: function () {
+					return "ControllerRoot";
+				}
+			});
+
+			sap.ui.controller("testdata.customizing.synchronous.sap.CompA.ext.Controller", {
+				getValue: function () {
+					return "ControllerA";
+				}
+			});
+
+			sap.ui.controller("testdata.customizing.synchronous.sap.CompB.ext.Controller", {
+				getValue: function () {
+					return "ControllerB";
+				}
+			});
+		}
+	});
+
+	QUnit.test("Controller Extension of owner component is used", function(assert){
+		var oCompA = sap.ui.component({
+			id: "componentA",
+			name: "testdata.customizing.synchronous.sap.CompA",
+			manifest: false,
+			async: false
+		});
+
+		var oCompB = sap.ui.component({
+			id: "componentB",
+			name: "testdata.customizing.synchronous.sap.CompB",
+			manifest: false,
+			async: false
+		});
+
+		oCompB.runAsOwner(function() {
+			sap.ui.xmlview({
+				id: oCompB.createId("rootView"),
+				viewContent: '<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" controllerName="testdata.customizing.synchronous.sap.RootController">'
+				+ ' <core:Icon src="sap-icon://doctor"></core:Icon>'
+				+ ' </mvc:View>'
+			});
+		});
+
+		assert.strictEqual(oCompB.byId("rootView").getController().getValue(), "ControllerB", "The correct controller extension is used.");
+
+		oCompB.byId("rootView").destroy();
+		oCompA.destroy();
+		oCompB.destroy();
+	});
+
+	QUnit.test("Controller Extension of component name is used", function(assert){
+		var oCompA = sap.ui.component({
+			id: "componentA",
+			name: "testdata.customizing.synchronous.sap.CompA",
+			manifest: false,
+			async: false
+		});
+
+		var oRootView = sap.ui.xmlview({
+			id: "rootView",
+			viewContent: '<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" controllerName="testdata.customizing.synchronous.sap.RootController">'
+			+ ' <core:Icon src="sap-icon://doctor"></core:Icon>'
+			+ ' </mvc:View>'
+		});
+
+		assert.strictEqual(oRootView.getController().getValue(), "ControllerA", "The correct controller extension is used.");
+
+		oRootView.destroy();
+		oCompA.destroy();
+	});
+
+	QUnit.module("Owner-Component Handling (Controller Extension) - asynchronous", {
+		before: function() {
+			sap.ui.predefine("testdata/customizing/asynchronous/sap/CompA/Component", ["sap/ui/core/UIComponent"], function(UIComponent) {
+				return UIComponent.extend("testdata.customizing.asynchronous.sap.CompA.Component", {
+					metadata: {
+						version : "1.0",
+						customizing: {
+							"sap.ui.controllerExtensions": {
+								"testdata.customizing.asynchronous.sap.RootController": {
+									controllerName: "testdata.customizing.asynchronous.sap.CompA.ext.Controller"
+								}
+							}
+						}
+					}
+				});
+			});
+
+			sap.ui.predefine("testdata/customizing/asynchronous/sap/CompB/Component", ["sap/ui/core/UIComponent"], function(UIComponent) {
+				return UIComponent.extend("testdata.customizing.asynchronous.sap.CompB.Component", {
+					metadata: {
+						version : "1.0",
+						customizing: {
+							"sap.ui.controllerExtensions": {
+								"testdata.customizing.asynchronous.sap.RootController": {
+									controllerName: "testdata.customizing.asynchronous.sap.CompB.ext.Controller"
+								}
+							}
+						}
+					}
+				});
+			});
+
+
+			sap.ui.predefine("testdata/customizing/asynchronous/sap/RootController.controller", ["sap/ui/core/mvc/Controller"], function(Controller) {
+				return Controller.extend("testdata.customizing.asynchronous.sap.RootController", {
+					getValue: function () {
+						return "ControllerRoot";
+					}
+				});
+			});
+
+			sap.ui.predefine("testdata/customizing/asynchronous/sap/CompA/ext/Controller.controller", ["sap/ui/core/mvc/Controller"], function(Controller) {
+				return {
+					getValue: function () {
+						return "ControllerA";
+					}
+				};
+			});
+
+			sap.ui.predefine("testdata/customizing/asynchronous/sap/CompB/ext/Controller.controller", ["sap/ui/core/mvc/Controller"], function(Controller) {
+				return {
+					getValue: function () {
+						return "ControllerB";
+					}
+				};
+			});
+
+		}
+	});
+
+	QUnit.test("Controller Extension of owner component is used", function(assert){
+		var pCompA = Component.create({
+			id: "componentA",
+			name: "testdata.customizing.asynchronous.sap.CompA",
+			manifest: false
+		});
+
+		var pCompB = Component.create({
+			id: "componentB",
+			name: "testdata.customizing.asynchronous.sap.CompB",
+			manifest: false
+		});
+
+		return Promise.all([pCompA, pCompB]).then(function(aPromises){
+			var oCompA = aPromises[0],
+				oCompB = aPromises[1];
+
+			return new Promise(function(fnResolve, fnReject){
+				oCompB.runAsOwner(function() {
+					XMLView.create({
+						id: oCompB.createId("rootView"),
+						definition: '<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" controllerName="testdata.customizing.asynchronous.sap.RootController">'
+							+ ' <core:Icon src="sap-icon://doctor"></core:Icon>'
+							+ ' </mvc:View>'
+					}).then(function(oView) {
+						assert.strictEqual(oView.getController().getValue(), "ControllerB", "The correct controller extension is used.");
+
+						oView.destroy();
+						oCompA.destroy();
+						oCompB.destroy();
+						fnResolve();
+
+					}).catch(function(error){
+						fnReject(error);
+					});
+				});
+			});
+		});
+	});
+
+	QUnit.test("Controller Extension of component name is used", function(assert){
+		var pCompA = Component.create({
+			id: "componentA",
+			name: "testdata.customizing.asynchronous.sap.CompA",
+			manifest: false
+		});
+
+		return pCompA.then(function(oCompA) {
+			return XMLView.create({
+				id: "rootView",
+				definition: '<mvc:View xmlns:mvc="sap.ui.core.mvc" xmlns:core="sap.ui.core" controllerName="testdata.customizing.asynchronous.sap.RootController">'
+					+ ' <core:Icon src="sap-icon://doctor"></core:Icon>'
+					+ ' </mvc:View>'
+			}).then(function(oView) {
+				assert.strictEqual(oView.getController().getValue(), "ControllerA", "The correct controller extension is used.");
+
+				oView.destroy();
+				oCompA.destroy();
+			});
+		});
+
 	});
 
 });
