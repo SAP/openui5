69d3a7e5799f59e56e6cb8c83f5e53144c5211b8
Kevin Edinger <kevin.edinger@sap.com>
2020-10-30 14:49:52 +0100

    [INTERNAL] sap.ui.rta/dt: fix performance test
    some rta performance tests are using apps in dt, but they were not ready
    for it after the last refactoring.
    
    Change-Id: I22c79fe5152768fae1e028f9ed1b486f6ef15600

---
+++ src/sap.ui.dt/test/sap/ui/dt/performance/DesigntimeRegularOverlays/Component.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"dt/performance/PerformanceTestUtil",
+	"sap/ui/core/UIComponent",
+	"sap/ui/layout/VerticalLayout",
+	"sap/ui/layout/HorizontalLayout",
+	"rta/performance/RtaPerformanceTestUtil" // needed for some test function that are defined
+], function(
+	PerformanceTestUtil,
+	UIComponent,
+	VerticalLayout,
+	HorizontalLayout
+) {
+	"use strict";
+
+	return UIComponent.extend("dt.performance.designtimeRegularOverlays.Component", {
+		metadata: {
+			manifest: "json"
+		},
+
+		createContent: function () {
+			//create Vertical Layout
+			var oLayout = new VerticalLayout("visibleLayout");
+			PerformanceTestUtil.addMixedControlsTo(oLayout, 1, 200, true /*visible*/);
+			PerformanceTestUtil.addMixedControlsTo(oLayout, 201, 500, false /*invisible*/);
+
+			var oInvisibleLayout = new VerticalLayout("invisibleLayout", {
+				visible : false
+			});
+			PerformanceTestUtil.addMixedControlsTo(oInvisibleLayout, 2001, 3000, true /*visible*/);
+
+			var oHorizontalLayout = new HorizontalLayout("HorizontalLayout", {
+				content : [oLayout, oInvisibleLayout]
+			});
+			return oHorizontalLayout;
+		}
+	});
+});
