95349aff9d781319c650f7c16b79f6f2e1f97f79
d036800 <martin.schaus@sap.com>
2020-11-25 13:58:45 +0100

    [INTERNAL][TEST] sap.ui.integration: Card Editor
    - make test language independent
    
    Change-Id: Idc101044be3fbb25aa18b5fe807e3ffce8493c2f

--- src/sap.ui.integration/src/sap/ui/integration/designtime/editor/CardEditor.js
+++ src/sap.ui.integration/src/sap/ui/integration/designtime/editor/CardEditor.js
@@ -652,7 +652,7 @@ sap.ui.define([
 				oData[n] = oContextData[n];
 			}
 			//card internal
-			oData["card.internal"] = CardEditor._contextEntries.cardinternal;
+			oData["card.internal"] = CardEditor._contextEntries["card.internal"];
 			oContextModel.setData(oData);
 			oFlatContextModel.setData(flattenData(oData, "label"));
 		});
@@ -1381,7 +1381,7 @@ sap.ui.define([
 			placeholder: "",
 			value: ""
 		},
-		cardinternal: {
+		"card.internal": {
 			label: oResourceBundle.getText("CARDEDITOR_CONTEXT_CARD_INTERNAL_VAL"),
 			todayIso: {
 				type: "string",
