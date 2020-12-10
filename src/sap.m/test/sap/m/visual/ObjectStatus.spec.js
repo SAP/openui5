69608a4b85484f3883dd3e087d8ef7819a086c6a
Tereza Chobanova <tereza.chobanova@sap.com>
2020-12-10 10:15:02 +0200

    [INTERNAL] sap.m.ObjectStatus: stabilizing visual tests
    Change-Id: I387186c5f7c02bdca3dc427eee02fd3bcca799e8

--- src/sap.m/test/sap/m/visual/ObjectStatus.spec.js
+++ src/sap.m/test/sap/m/visual/ObjectStatus.spec.js
@@ -7,6 +7,9 @@ describe('sap.m.ObjectStatus', function() {
 
 	// Rendered properly
 	it('should be rendered properly', function () {
+		browser.executeScript(function() { // ensure the page is scrolled to top
+			document.getElementById("testPage-cont").scrollTop = 0;
+		});
 		expect(takeScreenshot(element(by.id('testPage')))).toLookAs('objectStatus_page');
 	});
 
@@ -29,6 +32,20 @@ describe('sap.m.ObjectStatus', function() {
 			document.getElementById("testPage-cont").scrollTop = 1550;
 		});
 		expect(takeScreenshot(element(by.id('testPage')))).toLookAs('objectStatus_page4');
+	});
+
+	it('should be rendered properly5', function () {
+		browser.executeScript(function() { // scroll page down
+			document.getElementById("testPage-cont").scrollTop = 2050;
+		});
+		expect(takeScreenshot(element(by.id('testPage')))).toLookAs('objectStatus_page5');
+	});
+
+	it('should be rendered properly6', function () {
+		browser.executeScript(function() { // scroll page down
+			document.getElementById("testPage-cont").scrollTop = 2550;
+		});
+		expect(takeScreenshot(element(by.id('testPage')))).toLookAs('objectStatus_page6');
 	});
 
 	it('should render properly the focus', function () {
