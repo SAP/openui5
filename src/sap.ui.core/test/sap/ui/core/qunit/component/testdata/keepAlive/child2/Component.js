9c8642a2cfbf15a32b5b96f61b629506d7bf2b20
Tommy Vinh Lam <tommy.vinh.lam@sap.com>
2020-12-14 11:04:04 +0100

    [FEATURE] sap.ui.core.UIComponent: Introduce activate/deactivate API
    - Stop/initialize Router
    - Suspend/resume EventBus
    - Activate/Deactivate nested components
    - Suspend/resume elements
    - Introduce isKeepAliveSupported API
    
    JIRA: CPOUI5FRAMEWORK-191
    
    Change-Id: Ib8462b7712d54b21c83973c754acff63e9220fb4

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/testdata/keepAlive/child2/Component.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+	"use strict";
+	return UIComponent.extend("testdata.keepAlive.child2.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
