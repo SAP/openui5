9c454627b8cd7f75982a02d71ee1f7e7016f2cb0
ze-chao zhang <ze-chao.zhang@sap.com>
2021-01-08 10:25:49 +0800

    [INTERNAL] sap.ui.integration: BAS Editor
    a. add hint column for a group or an item;
    b. add qunit tests for hint in the wz editor;
    c. update card explorer api document.
    
    Change-Id: I7c1dbdee5df327afec7c97cb7b80545d9a1fd81b

--- src/sap.ui.integration/test/sap/ui/integration/internal/designtime/withlargedt/dt/configuration.js
+++ src/sap.ui.integration/test/sap/ui/integration/internal/designtime/withlargedt/dt/configuration.js
@@ -523,13 +523,15 @@ sap.ui.define(["sap/ui/integration/Designtime"], function (
 					},
 					"group": {
 						"label": "Dependent",
-						"type": "group"
+						"type": "group",
+						"hint": "Please refer to the <a href='https://www.sap.com'>documentation</a> lets see how this will behave if the text is wrapping to the next line and has <a href='https://www.sap.com'>two links</a>. good?"
 					},
 					"string1": {
 						"manifestpath": "/sap.card/configuration/parameters/string1/value",
 						"defaultValue": "editable",
 						"label": "String: editable, visible, label",
-						"type": "string"
+						"type": "string",
+						"hint": "Please refer to the <a href='https://www.sap.com'>documentation</a> lets see how this will behave if the text is wrapping to the next line and has <a href='https://www.sap.com'>two links</a>. good?"
 					},
 					"dependentString1": {
 						"manifestpath": "/sap.card/configuration/parameters/dependentString1/value",
