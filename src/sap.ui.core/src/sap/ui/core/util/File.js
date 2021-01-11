fcb516e1e462a0f740ca34ea90044200766d1f76
Pramod Jayaram <pramod.jayaram@sap.com>
2021-01-11 11:31:27 +0100

    [INTERNAL] sap.ui.core.File: Documentation updated
    The JS doc on the limitation for save API on Core export is updated.
    
    BCP: 2180000850
    Change-Id: If0a6344e2ad0e362897a1ee1b0e0344339c79072

--- src/sap.ui.core/src/sap/ui/core/util/File.js
+++ src/sap.ui.core/src/sap/ui/core/util/File.js
@@ -23,20 +23,6 @@ sap.ui.define(["sap/ui/thirdparty/jquery"],
 		/**
 		 * <p>Triggers a download / save action of the given file.</p>
 		 *
-		 * <p>There are limitations for this feature in some browsers:<p>
-		 *
-		 * <p><b>macOS Safari < 10.1 / iOS Safari</b><br>
-		 * A new window or tab is opened.
-		 * In macOS, the user has to save the file manually (by using key combination "CMD + S", choosing the page source format, and specifying a file name).
-		 * In iOS, the content can be opened in another app (Mail, Notes, ...) or can be copied to the clipboard.
-		 * If a pop-up blocker prevents this action, an error will be thrown which can be used to notify the user that the pop-up blocker needs to be disabled.</p>
-		 *
-		 * <p><b>Android Browser</b><br>
-		 * Not supported</p>
-		 *
-		 * <p><b>Windows Phone 10 Edge</b><br>
-		 * Not supported</p>
-		 *
 		 * @param {string} sData file content
 		 * @param {string} sFileName file name
 		 * @param {string} sFileExtension file extension
