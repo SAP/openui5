f3a7f6803a45131e2038a641e1daf24583972793
Boyan Rakilovski <boyan.rakilovski@sap.com>
2021-02-10 10:28:45 +0200

    [INTERNAL] sap.ui.unified.FileUploader: improve demokit samples
    Change-Id: I5ef7c2fd64ce18300f1baeedd15e9f80a824d245
    JIRA: BGSOFUIBALKAN-5727

--- src/sap.ui.unified/test/sap/ui/unified/sample/FileUploaderComplex/Controller.controller.js
+++ src/sap.ui.unified/test/sap/ui/unified/sample/FileUploaderComplex/Controller.controller.js
@@ -1,21 +1,16 @@
-sap.ui.define(['jquery.sap.global','sap/m/MessageToast','sap/ui/core/mvc/Controller'],
-	function(jQuery, MessageToast, Controller) {
+sap.ui.define(['sap/m/MessageToast','sap/ui/core/mvc/Controller'],
+	function(MessageToast, Controller) {
 	"use strict";
 
 	return Controller.extend("sap.ui.unified.sample.FileUploaderComplex.Controller", {
 		handleUploadComplete: function(oEvent) {
-			var sResponse = oEvent.getParameter("response");
-			if (sResponse) {
-				var sMsg = "";
-				var m = /^\[(\d\d\d)\]:(.*)$/.exec(sResponse);
-				if (m[1] == "200") {
-					sMsg = "Return Code: " + m[1] + "\n" + m[2] + "(Upload Success)";
-					oEvent.getSource().setValue("");
-				} else {
-					sMsg = "Return Code: " + m[1] + "\n" + m[2] + "(Upload Error)";
-				}
+			var sResponse = oEvent.getParameter("response"),
+				iHttpStatusCode = parseInt(/\d{3}/.exec(sResponse)[0]),
+				sMessage;
 
-				MessageToast.show(sMsg);
+			if (sResponse) {
+				sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+				MessageToast.show(sMessage);
 			}
 		},
 
@@ -36,11 +31,12 @@ sap.ui.define(['jquery.sap.global','sap/m/MessageToast','sap/ui/core/mvc/Control
 
 		handleTypeMissmatch: function(oEvent) {
 			var aFileTypes = oEvent.getSource().getFileType();
-			jQuery.each(aFileTypes, function(key, value) {aFileTypes[key] = "*." +  value;});
-			var sSupportedFileTypes = aFileTypes.join(", ");
+			aFileTypes.map(function(sType) {
+				return "*." + sType;
+			});
 			MessageToast.show("The file type *." + oEvent.getParameter("fileType") +
 									" is not supported. Choose one of the following types: " +
-									sSupportedFileTypes);
+									aFileTypes.join(", "));
 		},
 
 		handleValueChange: function(oEvent) {
