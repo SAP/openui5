4a9906d7212040ff04f0af4a1b592a0b7801efa0
Petar Dimov <petar.dimov@sap.com>
2021-02-17 14:24:12 +0200

    [INTERNAL] sap.m.Carousel: Remove redundant model from sample
    Removed unused model from "sap.m.sample.Carousel"
    and "sap.m.sample.CarouselWithControls" components
    
    Change-Id: I42ac014aefaa527cdf84740b687aafc2450877d9
    JIRA: BGSOFUIRODOPI-2178

--- src/sap.m/test/sap/m/demokit/sample/Carousel/C.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/Carousel/C.controller.js
@@ -7,9 +7,6 @@ sap.ui.define([
 	return Controller.extend("sap.m.sample.Carousel.C", {
 
 		onInit: function () {
-			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
-			this.getView().setModel(oModel);
-
 			var oImgModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/img.json"));
 			this.getView().setModel(oImgModel, "img");
 		}
