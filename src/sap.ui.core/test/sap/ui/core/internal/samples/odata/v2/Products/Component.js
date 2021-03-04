634caa7594778d7cd1568dcb5ff2b687f24da083
Sven Bernhard <sven.bernhard@sap.com>
2021-03-04 14:40:58 +0100

    [INTERNAL] sap.ui.core.internal.samples.odata.v2.Products: enhanced application
    Added mock data for code lists and added links to show code list values.
    
    PS1: Take over application changes from change 5173797 and improved it
    PS2: use $skip and &top for code list requests
    PS3: updated mock data for code lists as preparation for MIT/OPA
    
    Change-Id: Ic6873d8709502e7d210828ca7d1b039ac473b96b
    JIRA: CPOUI5MODELS-389

--- src/sap.ui.core/test/sap/ui/core/internal/samples/odata/v2/Products/Component.js
+++ src/sap.ui.core/test/sap/ui/core/internal/samples/odata/v2/Products/Component.js
@@ -7,13 +7,33 @@
  * OData service.
  */
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function (UIComponent) {
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/test/TestUtils"
+], function (UIComponent, JSONModel, TestUtils) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.core.internal.samples.odata.v2.Products.Component", {
 		metadata : {
 			manifest : "json"
+		},
+
+		init : function () {
+			var bRealOData = TestUtils.isRealOData(),
+				sServicePrefix = "/sap/opu/odata/sap/ZUI5_GWSAMPLE_BASIC/",
+				sURLPrefix = "test-resources/sap/ui/core/internal/samples/odata/v2/Products/data/";
+
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this.setModel(new JSONModel({
+				realOData : bRealOData,
+				currenciesURL : bRealOData
+					? TestUtils.proxy(sServicePrefix + "SAP__Currencies?$format=json")
+					: sURLPrefix + "SAP__Currencies.json",
+				unitsOfMeasureURL : bRealOData
+					? TestUtils.proxy(sServicePrefix + "SAP__UnitsOfMeasure?$format=json")
+					: sURLPrefix + "SAP__UnitsOfMeasure.json"
+			}), "ui");
 		}
 	});
 });
