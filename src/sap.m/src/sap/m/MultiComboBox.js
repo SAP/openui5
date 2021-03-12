596ce400801c8b11038ed4d3b40ee05128c653a3
Yavor Ivanov <yavor.ivanov@sap.com>
2021-03-12 11:38:31 +0200

    [INTERNAL] sap.m.MultiComboBox: Clear text if selection occurs
    BCP: 0020751294 0000179879 2021
    
    Change-Id: I3b7927f41abaed7371bea1214e161f2efb826199

--- src/sap.m/src/sap/m/MultiComboBox.js
+++ src/sap.m/src/sap/m/MultiComboBox.js
@@ -2279,6 +2279,8 @@ function(
 	 */
 	MultiComboBox.prototype.onpaste = function (oEvent) {
 		var sOriginalText;
+		var bItemSelected = false;
+		var aSelectedItems = this.getSelectedItems();
 
 		// for the purpose to copy from column in excel and paste in MultiInput/MultiComboBox
 		if (window.clipboardData) {
@@ -2288,26 +2290,36 @@ function(
 		} else {
 
 			// Chrome, Firefox, Safari
-			sOriginalText =  oEvent.originalEvent.clipboardData.getData('text/plain');
+			sOriginalText = oEvent.originalEvent.clipboardData.getData('text/plain');
 		}
 
 		var aSeparatedText = sOriginalText.split(/\r\n|\r|\n/g);
 
 		if (aSeparatedText && aSeparatedText.length > 0) {
-			ListHelpers.getSelectableItems(this.getItems()).forEach(function(oItem) {
+			ListHelpers.getSelectableItems(this.getItems())
+				.filter(function (oItem) {
+					return aSelectedItems.indexOf(oItem) === -1;
+				})
+				.forEach(function (oItem) {
+					if (aSeparatedText.indexOf(oItem.getText()) > -1) {
+						bItemSelected = true;
 
-				if (aSeparatedText.indexOf(oItem.getText()) > -1) {
-					this.setSelection({
-						item: oItem,
-						id: oItem.getId(),
-						key: oItem.getKey(),
-						fireChangeEvent: true,
-						fireFinishEvent: true,
-						suppressInvalidate: true,
-						listItemUpdated: false
-					});
-				}
-			}, this);
+						this.setSelection({
+							item: oItem,
+							id: oItem.getId(),
+							key: oItem.getKey(),
+							fireChangeEvent: true,
+							fireFinishEvent: true,
+							suppressInvalidate: true,
+							listItemUpdated: false
+						});
+					}
+				}, this);
+		}
+
+		if (bItemSelected) {
+			oEvent.stopPropagation();
+			oEvent.preventDefault();
 		}
 	};
 
--- src/sap.m/test/sap/m/qunit/MultiComboBox.qunit.js
+++ src/sap.m/test/sap/m/qunit/MultiComboBox.qunit.js
@@ -3026,6 +3026,46 @@ sap.ui.define([
 		assert.ok(oHandleTypeAheadSpy.called, "Type ahead should be called while pasting value");
 	});
 
+	QUnit.test("Paste and select behaviour", function (assert) {
+		var oMultiComboBox = new MultiComboBox({
+			items: [new Item({
+				key: "DZ",
+				text: "Algeria"
+			})]
+		}).placeAt("MultiComboBox-content");
+		sap.ui.getCore().applyChanges();
+
+		oMultiComboBox.syncPickerContent();
+		sap.ui.getCore().applyChanges();
+
+		var oEventStub = this.stub({
+			stopPropagation: function () {
+			},
+			preventDefault: function () {
+			},
+			originalEvent: {
+				clipboardData: {
+					getData: function () {
+						return "Algeria\n";
+					}
+				}
+			}
+		});
+
+		// Act
+		window.clipboardData = null;
+		oMultiComboBox.onpaste(oEventStub);
+		sap.ui.getCore().applyChanges();
+
+		// Assert
+		assert.strictEqual(oMultiComboBox.getSelectedItems().length, 1, "Should have one selected item");
+		assert.strictEqual(oMultiComboBox.getValue(), "", "Input's value should be empty");
+		assert.ok(oEventStub.stopPropagation.calledOnce, "Value paste should have been prevented");
+
+		// Cleanup
+		oMultiComboBox.destroy();
+	});
+
 
 	QUnit.test("Focus out cleanup", function (assert) {
 		// Arrange
