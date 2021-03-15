eb94787a2facfcbb2fb99725150c7911deff6cc5
Kevin Edinger <kevin.edinger@sap.com>
2021-03-15 09:45:29 +0100

    [INTERNAL] sap.ui.fl: refine getPersistencyKey function for SVM
    to also support other controls using a SmartVariantManagement control
    but don't have a getVariantManagement function on their controls
    
    BCP: 2170036623
    Change-Id: I90d9e36cd90e26ca9eeb16aecd222dd3a223519a

---
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/flexState/compVariants/Utils.js
@@ -0,0 +1,21 @@
+/*
+ * ! ${copyright}
+ */
+
+sap.ui.define([], function() {
+	"use strict";
+
+	var Utils = {};
+
+	Utils.getPersistencyKey = function(oControl) {
+		if (oControl) {
+			var oVMControl = oControl.getVariantManagement && oControl.getVariantManagement() || oControl;
+			if (oVMControl.getPersonalizableControlPersistencyKey) {
+				return oVMControl.getPersonalizableControlPersistencyKey();
+			}
+			return oVMControl.getPersistencyKey && oVMControl.getPersistencyKey();
+		}
+	};
+
+	return Utils;
+});
