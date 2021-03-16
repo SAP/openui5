44ec295f6e92ad7ef29db01ee9861f4eec898127
Patric Ksinsik <p.ksinsik@sap.com>
2021-03-16 16:54:11 +0100

    [INTERNAL] sap.ui.model.TreeBindingAdapter#collapseToLevel: Set number of expanded levels
    PS1: integration test incl. enhancement by checkValue method using ManagedObjectObserver
      instead of formatter function to check values in order to allow for testing tree tables
      with expand and collapse. POC implementation of fix which makes the test green.
    PS2: Fix POC, voter should be green
    PS3: Rebase
    PS4: TDD for fix, minor improvements to integration test
    PS5: Fix jsdoc in integration test
    PS6: Review comments
    
    Change-Id: I816e4fd08b0b0acb0a64f0727f11e181a4269ad9
    BCP: 002075129500000660392021

--- src/sap.ui.core/test/sap/ui/core/qunit/model/TreeBindingAdapter.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/model/TreeBindingAdapter.qunit.js
@@ -3,11 +3,12 @@
  */
 sap.ui.define([
 	"sap/base/Log",
+	"sap/ui/model/ChangeReason",
 	"sap/ui/model/TreeAutoExpandMode",
 	"sap/ui/model/TreeBinding",
 	"sap/ui/model/TreeBindingAdapter",
 	"sap/ui/test/TestUtils"
-], function (Log, TreeAutoExpandMode, TreeBinding, TreeBindingAdapter, TestUtils) {
+], function (Log, ChangeReason, TreeAutoExpandMode, TreeBinding, TreeBindingAdapter, TestUtils) {
 	/*global QUnit */
 	"use strict";
 
@@ -154,4 +155,26 @@ sap.ui.define([
 		// code under test - parameters are not relevant for this test
 		assert.deepEqual(TreeBindingAdapter.prototype._getContextsOrNodes.call(oBinding), []);
 	});
+
+	//*********************************************************************************************
+[undefined, true].forEach(function (bCollapseRecursive, i) {
+	QUnit.test("collapseToLevel: set number of expanded levels, " + i, function (assert) {
+		var oBinding = {
+				bCollapseRecursive : bCollapseRecursive,
+				_fireChange : function () {},
+				_mTreeState : {
+					expanded : {}
+				},
+				setNumberOfExpandedLevels : function () {}
+			};
+
+		this.mock(oBinding).expects("setNumberOfExpandedLevels")
+			.exactly(bCollapseRecursive ? 1 : 0)
+			.withExactArgs(42);
+		this.mock(oBinding).expects("_fireChange").withExactArgs({reason: ChangeReason.Collapse});
+
+		// code under test
+		TreeBindingAdapter.prototype.collapseToLevel.call(oBinding, 42);
+	});
+});
 });
