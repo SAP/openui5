e23948cdb2e0cb9e2c24d0e9a94c0bedbfdb74a5
Florian Vogt <florian.vogt@sap.com>
2021-03-17 11:01:31 +0100

    [INTERNAL] sap.ui.core.LocalBusyIndicatorRenderer: fix code smells
    Change-Id: I9952b4afbf1df54400323dc6ae2cb41a2a7989fc

--- src/sap.ui.core/src/sap/ui/core/LocalBusyIndicatorRenderer.js
+++ src/sap.ui.core/src/sap/ui/core/LocalBusyIndicatorRenderer.js
@@ -4,6 +4,36 @@
 sap.ui.define(function() {
 	"use strict";
 
+	/**
+	 * Renders the HTML for the given control, using the provided
+	 * {@link sap.ui.core.RenderManager}.
+	 *
+	 * @param {sap.ui.core.RenderManager}
+	 *            oRm the RenderManager that can be used for writing to the
+	 *            render output buffer
+	 * @param {sap.ui.core.Control}
+	 *            oControl an object representation of the control that should
+	 *            be rendered
+	 * @private
+	 */
+	var fnRenderFlickerDivs = function(oRm, oControl) {
+		var sId = oControl.getId();
+		var sIdAnimation = sId + "-animation";
+		var aBoxEnum = [ "-leftBox", "-middleBox", "-rightBox" ];
+
+		oRm.openStart('div', sIdAnimation);
+		oRm.class("sapUiLocalBusyIndicatorAnimation");
+		oRm.openEnd();
+
+		for ( var i = 0; i < aBoxEnum.length; i++) {
+			oRm.openStart('div', sId + aBoxEnum[i]);
+			oRm.class("sapUiLocalBusyIndicatorBox");
+			oRm.openEnd();
+			oRm.close("div");
+		}
+
+		oRm.close("div");
+	};
 
 	/**
 	 * LocalBusyIndicator renderer.
@@ -31,25 +61,6 @@ sap.ui.define(function() {
 		oRm.openEnd();
 
 		fnRenderFlickerDivs(oRm, oControl);
-
-		oRm.close("div");
-	};
-
-	var fnRenderFlickerDivs = function(oRm, oControl) {
-		var sId = oControl.getId();
-		var sIdAnimation = sId + "-animation";
-		var aBoxEnum = [ "-leftBox", "-middleBox", "-rightBox" ];
-
-		oRm.openStart('div', sIdAnimation);
-		oRm.class("sapUiLocalBusyIndicatorAnimation");
-		oRm.openEnd();
-
-		for ( var i = 0; i < aBoxEnum.length; i++) {
-			oRm.openStart('div', sId + aBoxEnum[i]);
-			oRm.class("sapUiLocalBusyIndicatorBox");
-			oRm.openEnd();
-			oRm.close("div");
-		}
 
 		oRm.close("div");
 	};
