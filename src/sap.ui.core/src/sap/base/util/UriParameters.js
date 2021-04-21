c1d7901d77688efc094768ecc7afa0e001d3425b
Frank Weigel <frank.weigel@sap.com>
2021-04-21 08:55:00 +0200

    [INTERNAL] base.util.UrlParameters: fix JSDoc samples for method 'has'
    Change-Id: I888e4b59f341e8f2ecf66a522036bac1a01937bb

--- src/sap.ui.core/src/sap/base/util/UriParameters.js
+++ src/sap.ui.core/src/sap/base/util/UriParameters.js
@@ -107,9 +107,9 @@ sap.ui.define([
 		 * @example
 		 *
 		 * UriParameters.fromQuery("?foo=bar").has("foo") === true
-		 * UriParameters.fromQuery("?foo").get("foo") === true
-		 * UriParameters.fromQuery("?foo").get("bar") === false
-		 * UriParameters.fromQuery("?foo+bar=ba%7a").get("foo bar") === true
+		 * UriParameters.fromQuery("?foo").has("foo") === true
+		 * UriParameters.fromQuery("?foo").has("bar") === false
+		 * UriParameters.fromQuery("?foo+bar=ba%7a").has("foo bar") === true
 		 *
 		 * @param {string} sName Name of the query parameter to check
 		 * @returns {boolean} Whether the parameter has been defined
