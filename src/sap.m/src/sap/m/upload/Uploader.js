d4abe325baa95b82a69d55823a833d8b3f3ba5d8
Ashwin Kumar Bolar <ashwin.kumar.bolar@sap.com>
2021-05-06 12:03:38 +0530

    [INTERNAL] Documentation update for UploadSet
    BCP: 2170159907
    Change-Id: I16dc5efbab13e7a286afa8908be1d783ccf1ef11

--- src/sap.m/src/sap/m/upload/Uploader.js
+++ src/sap.m/src/sap/m/upload/Uploader.js
@@ -26,7 +26,7 @@ sap.ui.define([
 	 *
 	 * @constructor
 	 * @public
-	 * @since 1.62
+	 * @since 1.63
 	 * @alias sap.m.upload.Uploader
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
