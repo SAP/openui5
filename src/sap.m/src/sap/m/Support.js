e8f6497cde47aadeca8f88606b5f76630e44e72e
Petar Dimov <petar.dimov@sap.com>
2021-06-08 18:46:54 +0300

    [INTERNAL] sap.m.Support: Replace requireSync calls
    Dependencies are now loaded asynchronously.
    Circular dependencies are still considered.
    
    Change-Id: I1babf9304564b8247b21d763c769a76aba29dc86
    JIRA: BGSOFUIRODOPI-2574

--- src/sap.m/src/sap/m/Support.js
+++ src/sap.m/src/sap/m/Support.js
@@ -3,18 +3,27 @@
  */
 
 sap.ui.define([
-	'sap/ui/thirdparty/jquery',
-	'sap/ui/Device',
+	"sap/ui/thirdparty/jquery",
+	"sap/ui/Device",
 	"sap/base/security/encodeXML",
 	"sap/base/util/isPlainObject"
 ],
-	function(jQuery, Device, encodeXML, isPlainObject) {
+	function(
+		jQuery,
+		Device,
+		encodeXML,
+		isPlainObject
+	) {
 		"use strict";
+
+		// Load these dependencies later to avoid circular dependencies of type
+		// library -> Support -> Button -> library
+		var Button, Dialog, Label, Panel, Toolbar, MessageToast, HTML, mLibrary;
 
 		/**
 		 * <pre>
 		 * <code>sap.m.Support</code> shows the technical information for SAPUI5 Mobile Applications.
-		 * This technical information includes
+		 * This technical information includes:
 		 *    * SAPUI5 Version
 		 *    * User Agent
 		 *    * Configurations (Bootstrap and Computed)
@@ -22,26 +31,24 @@ sap.ui.define([
 		 *    * All loaded module names
 		 *
 		 * In order to show the device information, the user must follow the following gestures.
-		 *    1 - Hold two finger for 3 seconds minimum.
+		 *    1 - Hold two fingers for 3 seconds minimum.
 		 *    2 - Tab with a third finger while holding the first two fingers.
 		 *
 		 * NOTE: This class is internal and all its functions must not be used by an application
 		 *
-		 * As <code>sap.m.Support</code> is a static class, a <code>sap.ui.requireSync("sap/m/Support");</code>
-		 * statement must be implicitly executed before the class is used.
-		 *
-		 *
 		 * Enable Support:
 		 * --------------------------------------------------
-		 * //import library
-		 * sap.ui.requireSync("sap/m/Support");
+		 * //import
+		 * sap.ui.require("sap/m/Support", function (Support) {
+		 *   // Support is initialized and is listening for fingers gestures combination
+		 * });
 		 *
 		 * //By default after require, support is enabled but implicitly we can call
-		 * sap.m.Support.on();
+		 * Support.on();
 		 *
 		 * Disable Support:
 		 * --------------------------------------------------
-		 * sap.m.Support.off();
+		 * Support.off();
 		 * </pre>
 		 *
 		 * @author SAP SE
@@ -167,7 +174,7 @@ sap.ui.define([
 
 				html.push("</tbody></table>");
 
-				return new sap.ui.core.HTML({
+				return new HTML({
 					content: html.join("").replace(/\{/g, "&#123;").replace(/\}/g, "&#125;")
 				});
 			}
@@ -183,18 +190,18 @@ sap.ui.define([
 
 				libsCount = arContent.length;
 				jQuery.each(arContent.sort(), function (i, module) {
-					arDivContent.push(new sap.m.Label({text: " - " + module}).addStyleClass("sapUiSupportPnlLbl"));
+					arDivContent.push(new Label({text: " - " + module}).addStyleClass("sapUiSupportPnlLbl"));
 				});
 
 				// insert content into div placeholders
-				var objPanel = new sap.m.Panel({
+				var objPanel = new Panel({
 					expandable: true,
 					expanded: false,
-					headerToolbar: new sap.m.Toolbar({
-						design: sap.m.ToolbarDesign.Transparent,
-						content: [new sap.m.Label({
+					headerToolbar: new Toolbar({
+						design: mLibrary.ToolbarDesign.Transparent,
+						content: [new Label({
 							text: panelHeader + " (" + libsCount + ")",
-							design: sap.m.LabelDesign.Bold
+							design: mLibrary.LabelDesign.Bold
 						})]
 					}),
 					content: arDivContent
@@ -204,7 +211,7 @@ sap.ui.define([
 			}
 
 			// setup dialog elements and bind some events
-			function setupDialog() {
+			function setupDialog(E2eTraceLib) {
 				// setup e2e values as log level and content
 				if (dialog.traceXml) {
 					dialog.$(e2eTraceConst.taContent).text(dialog.traceXml);
@@ -224,12 +231,12 @@ sap.ui.define([
 					dialog.traceXml = "";
 					dialog.$(e2eTraceConst.taContent).text("");
 
-					sap.ui.core.support.trace.E2eTraceLib.start(dialog.e2eLogLevel, function (traceXml) {
+					E2eTraceLib.start(dialog.e2eLogLevel, function (traceXml) {
 						dialog.traceXml = traceXml;
 					});
 
 					// show info message about the E2E trace activation
-					sap.m.MessageToast.show(e2eTraceConst.infoText, {duration: e2eTraceConst.infoDuration});
+					MessageToast.show(e2eTraceConst.infoText, {duration: e2eTraceConst.infoDuration});
 
 					//close the dialog, but keep it for later use
 					dialog.close();
@@ -241,12 +248,6 @@ sap.ui.define([
 				if (dialog) {
 					return dialog;
 				}
-
-				var Dialog = sap.ui.requireSync("sap/m/Dialog");
-				var Button = sap.ui.requireSync("sap/m/Button");
-				sap.ui.requireSync("sap/ui/core/HTML");
-				sap.ui.requireSync("sap/m/MessageToast");
-				sap.ui.requireSync("sap/ui/core/support/trace/E2eTraceLib");
 
 				dialog = new Dialog({
 					title: "Technical Information",
@@ -316,13 +317,33 @@ sap.ui.define([
 			}
 
 			function show() {
-				sap.ui.require(['sap/ui/core/support/ToolsAPI'], function (ToolsAPI) {
+				sap.ui.require([
+					"sap/ui/core/support/ToolsAPI",
+					"sap/ui/core/support/trace/E2eTraceLib",
+					"sap/ui/core/HTML",
+					"sap/m/library",
+					"sap/m/Button",
+					"sap/m/Dialog",
+					"sap/m/Label",
+					"sap/m/Panel",
+					"sap/m/Toolbar",
+					"sap/m/MessageToast"
+				], function (ToolsAPI, E2eTraceLib, _HTML, _mLibrary, _Button, _Dialog, _Label, _Panel, _Toolbar, _MessageToast) {
+					HTML = _HTML;
+					mLibrary = _mLibrary;
+					Button = _Button;
+					Dialog = _Dialog;
+					Label = _Label;
+					Panel = _Panel;
+					Toolbar = _Toolbar;
+					MessageToast = _MessageToast;
+
 					var container = getDialog();
 					container.removeAllAggregation("content");
 					container.addAggregation("content", getTechnicalContent(ToolsAPI.getFrameworkInformation()));
 
 					dialog.open();
-					setupDialog();
+					setupDialog(E2eTraceLib);
 				});
 			}
 
