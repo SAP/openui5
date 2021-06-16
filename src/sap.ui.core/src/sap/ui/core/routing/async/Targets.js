d411f805c6b40ea88e0de5876b750ffd07ee8135
Jiawei Cao <jiawei.cao@sap.com>
2021-06-16 09:47:27 +0200

    [FIX] core.routing: propagate 'routeRelevant' to parent target
    - When displaying a target manually with the flag 'routeRelevant', all
      of its parent target should be marked as 'routeRelevant' as well so
      that the hash of the corresponding RouterHashChanger is kept and the
      parent targets are suspended when navigating away from the current
      route.
    
    BCP: 2180218006
    Change-Id: Id7e66edc9efd0024e20a06951fe0f85a3288ef1e

--- src/sap.ui.core/src/sap/ui/core/routing/async/Targets.js
+++ src/sap.ui.core/src/sap/ui/core/routing/async/Targets.js
@@ -53,7 +53,8 @@ sap.ui.define(["sap/base/Log"], function(Log) {
 					propagateTitle: oTargetInfo.propagateTitle || false,
 					ignoreInitialHash: oTargetInfo.ignoreInitialHash,
 					placeholder: oTargetInfo.placeholder,
-					repeatedRoute: oTargetInfo.repeatedRoute
+					repeatedRoute: oTargetInfo.repeatedRoute,
+					routeRelevant: oTargetInfo.routeRelevant || false
 				};
 
 				// gather view infos while processing Promise chain
@@ -111,7 +112,6 @@ sap.ui.define(["sap/base/Log"], function(Log) {
 				oTarget = this.getTarget(sName);
 
 			if (oTarget !== undefined) {
-				oTarget._routeRelevant = oTargetInfo.routeRelevant || false;
 				if (oTargetInfo.routeRelevant) {
 					this._addDynamicTargetToRoute(oTargetInfo);
 				}
