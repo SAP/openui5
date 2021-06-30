c6db696ddd8d907d4c37208889485715f64197ba
Petar Dimov <petar.dimov@sap.com>
2021-06-30 15:55:57 +0300

    [INTERNAL] sap.f.cards.NumericHeader: Fix visual test with preview
    Change-Id: Ib9f850786df61fc6fecb99058d7418b12edba0fc
    BCP: 2180243820

--- src/sap.f/test/sap/f/cardsdemo/controller/Preview.controller.js
+++ src/sap.f/test/sap/f/cardsdemo/controller/Preview.controller.js
@@ -8,8 +8,7 @@ sap.ui.define([
 		onInit: function () {
 			this.byId("card1")._setPreviewMode(true);
 			this.byId("card2")._setPreviewMode(true);
-			// comment temporary until NumericHeader preview is fixed
-			// this.byId("card3")._setPreviewMode(true);
+			this.byId("card3")._setPreviewMode(true);
 		}
 	});
 });
