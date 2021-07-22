22a57d692a41201ccb5c16ece439faafbc17d902
Frank Weigel <frank.weigel@sap.com>
2021-07-22 11:03:47 +0200

    [FIX] sap.ui.VersionInfo: copy 'hasOwnPreload' flag for components
    When the sap-ui-version.json contains the information that a component
    is packaged separately from its containing library (property
    'hasOwnPreload'), then the sap.ui.VersionInfo should propagate this
    information when asked for the dependency information of that component.
    
    Before this change, the property 'hasOwnPreload' was omitted from the
    returned dependency info.
    
    (cherry picked from 399aac16c72bcc705bc2d816cb786044df42dba5 and
    8cd20c4eb1fb2bf607932710d8ffc14ae8314efc)
    Change-Id: Ia5cd3b7fe4f61a21949bd709ea1a07c5c706115a
    CR-Id: 002075125900004075932021
    BCP: 2180290030

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/component/Component_containedInLibrary.qunit.js
@@ -0,0 +1,88 @@
+/*global QUnit sinon */
+sap.ui.define([
+	"sap/ui/Global",
+	'sap/ui/core/Component'
+], function(Global, Component) {
+	"use strict";
+
+	function noop() {}
+
+	QUnit.module("When a Component is contained in a Library,", {
+		before: function() {
+			// inject mocked version info
+			Global.versioninfo = {
+				"name": "qunit",
+				"version": "1.0.0",
+				"buildTimestamp": "<TIMESTAMP>",
+				"scmRevision": "<HASH>",
+				"gav": "<GAV>",
+				"libraries": [],
+				"components": {
+					"sap.ui.test.componentContainedInLibrary.Comp1": {
+						"library": "sap.ui.test.componentContainedInLibrary",
+						"manifestHints": {
+							"dependencies": {
+								"libs": {
+									"sap.m": {},
+									"sap.ui.core": {},
+									"sap.ui.layout": {}
+								}
+							}
+						}
+					},
+					"sap.ui.test.componentContainedInLibrary.Comp2": {
+						"hasOwnPreload": true,
+						"library": "sap.ui.test.componentContainedInLibrary",
+						"manifestHints": {
+							"dependencies": {
+								"libs": {
+									"sap.m": {},
+									"sap.ui.core": {},
+									"sap.ui.layout": {},
+									"sap.ui.unified": {}
+								}
+							}
+						}
+					}
+				}
+			};
+		}
+	});
+
+	QUnit.test("and bundled as part of the library-preload...", function(assert) {
+		var success = Promise.resolve();
+		var loadLibs = this.stub(sap.ui.getCore(), "loadLibraries").returns(success);
+		var loadPreload = this.stub(sap.ui.loader._, "loadJSResourceAsync").returns(success);
+
+		return Component.create({
+			name: "sap.ui.test.componentContainedInLibrary.Comp1",
+			manifest: false
+		}).catch(noop).finally(function() {
+			assert.ok(loadLibs.calledWith(
+				sinon.match.array.contains(["sap.m", "sap.ui.core", "sap.ui.layout", "sap.ui.test.componentContainedInLibrary"])),
+				"...then that library should be implicitly loaded");
+			assert.ok(
+				loadPreload.neverCalledWith(sinon.match(/sap\/ui\/test\/componentContainedInLibrary\/Comp1\/Component-preload\.js$/)),
+				"...then no Component-preload file should be requested for the component");
+		});
+	});
+
+	QUnit.test("and bundled separately...", function(assert) {
+		var success = Promise.resolve();
+		var loadLibs = this.stub(sap.ui.getCore(), "loadLibraries").returns(success);
+		var loadPreload = this.stub(sap.ui.loader._, "loadJSResourceAsync").returns(success);
+
+		return Component.create({
+			name: "sap.ui.test.componentContainedInLibrary.Comp2",
+			manifest: false
+		}).catch(noop).finally(function() {
+			assert.ok(
+				loadLibs.neverCalledWith(sinon.match.array.contains(["sap.ui.test.componentContainedInLibrary"])),
+				"...then the containing library should not be loaded");
+			assert.ok(
+				loadPreload.calledWith(sinon.match(/sap\/ui\/test\/componentContainedInLibrary\/Comp2\/Component-preload\.js$/)),
+				"...then a Component-preload file should be requested for the component");
+		});
+	});
+
+});
