14a40f0a2137e6cba5889129de94ca3f78c6bd91
Naidenov <maxim.naidenov@sap.com>
2021-07-28 14:49:39 +0300

    [INTERNAL][TEST]: resourceWaiter: make the test fully sequential
    Change-Id: I4ad1f07f6b9bb2e5c44c686b98e2c7faf6e50b86

--- src/sap.ui.core/test/sap/ui/core/qunit/opa/autowaiter/_resourceWaiter.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/opa/autowaiter/_resourceWaiter.js
@@ -30,54 +30,32 @@ sap.ui.define([
 		}
 	});
 
-	function assertImagePending(sSrc, assert) {
+	function assertPendingStartedAndFinished(sSrc, bPass, assert) {
 		var bHasPending = _resourceWaiter.hasPending();
 		var sLogs = oLogCollector.getAndClearLog();
-		if (bHasPending) {
-			assert.ok(true, "Has pending resources");
-			// image could be cached or loaded in previous steps
-			assert.ok(sLogs.match("Pending resource: .*" + sSrc) ||
-				sLogs.match("Image with src '.*" + sSrc + "' is updated and pending again"),
-				"Image with src " + sSrc + " is pending");
-		} else {
-			// when CPU is very slow, hasPending may be called before load handler
-			assert.ok(true, "Has no pending resources");
-			assert.ok(sLogs.match("Image with src '.*" + sSrc + "' completed") ||
-				sLogs.match("Image with src '.*" + sSrc + "' loaded successfully"),
-				"Image already loaded");
-		}
-	}
-
-	// workaround for image loading (async) - there can multiple images created for 1 test, but we only check one at a time
-	function assertNoRelevantPending(sRelevantSrc, bPass, assert) {
-		_resourceWaiter.hasPending();
-		var sLogs = oLogCollector.getAndClearLog();
-		var aRelevantPending = sLogs.match("Pending resource: .*" + sRelevantSrc);
-		assert.ok(!aRelevantPending, "Should have no pending resources with src " + sRelevantSrc);
-		assert.ok(sLogs.match("Image with src '.*" + sRelevantSrc + (bPass ? "' loaded successfully" : "' failed to load")),
-			"Image with src " + sRelevantSrc + " is not pending");
+		assert.ok(!bHasPending, "Shoud not have pending images");
+		assert.ok(sLogs.match("Image with src '.*" + sSrc + "' is pending") ||
+			sLogs.match("Image with src '.*" + sSrc + "' is updated and pending again"),
+			"Should have start pending log for image with src " + sSrc);
+		assert.ok(sLogs.match("Image with src '.*" + sSrc + (bPass ? "' loaded successfully" : "' failed to load")),
+			"Should have stop pending log for image with src " + sSrc);
 	}
 
 	QUnit.test("Should wait for image to load", function (assert) {
 		var fnDone = assert.async();
 		this.oImageExistingSrc = new Image({
-			src: this.sExistingImageSrc
+			src: this.sExistingImageSrc,
+			load: fnOnComplete().bind(this)
 		});
 		this.oImageExistingSrc.placeAt("qunit-fixture");
 		sap.ui.getCore().applyChanges();
 
-		// wait for the rendering to start
-		setTimeout(function () {
-			assertImagePending(this.sExistingImageSrc, assert);
-
-			this.oImageExistingSrc.attachLoad(fnOnComplete(true).bind(this));
-		}.bind(this), 0);
-
-		function fnOnComplete(bPass) {
+		function fnOnComplete() {
 			return function () {
 				// wait for other load handlers
 				setTimeout(function () {
-					assertNoRelevantPending(this.sExistingImageSrc, bPass, assert);
+					// assert image was pending and then completed retrospectively from logs
+					assertPendingStartedAndFinished(this.sExistingImageSrc,true,assert);
 
 					// cleanup
 					this.oImageExistingSrc.destroy();
@@ -92,13 +70,10 @@ sap.ui.define([
 		var fnDone = assert.async();
 		this.oNestedImage = new Image("nestedImage", {
 			src: this.sNestedImageSrc,
-			load: function () {
-				this.oText.setVisible(true);
-			}.bind(this)
+			load: fnOnComplete().bind(this)
 		});
 		this.oText = new Text("headline", {
-			text: "Headline",
-			visible: false
+			text: "Headline"
 		});
 		this.oPanel = new Panel({
 			content: [new BlockLayout({
@@ -115,18 +90,12 @@ sap.ui.define([
 		this.oPanel.placeAt("qunit-fixture");
 		sap.ui.getCore().applyChanges();
 
-		// wait for the rendering to start
-		setTimeout(function () {
-			assertImagePending(this.sNestedImageSrc, assert);
-
-			this.oNestedImage.attachLoad(fnOnComplete(true).bind(this));
-		}.bind(this), 0);
-
-		function fnOnComplete(bPass) {
+		function fnOnComplete() {
 			return function () {
 				// wait for other load handlers
 				setTimeout(function () {
-					assertNoRelevantPending(this.sNestedImageSrc, bPass, assert);
+					// assert image was pending and then completed retrospectively from logs
+					assertPendingStartedAndFinished(this.sNestedImageSrc,true,assert);
 
 					// cleanup
 					this.oPanel.destroy();
@@ -140,64 +109,58 @@ sap.ui.define([
 	QUnit.test("Should wait for image to complete with error", function (assert) {
 		var fnDone = assert.async();
 		this.oImageWrongSrc = new Image({
-			src: this.sNotFoundSrc
+			src: this.sNotFoundSrc,
+			error: fnOnError().bind(this)
 		});
 		this.oImageWrongSrc.placeAt("qunit-fixture");
 		sap.ui.getCore().applyChanges();
 
-		// wait for the rendering to start
-		setTimeout(function () {
-			assertImagePending(this.sNotFoundSrc, assert);
-
-			this.oImageWrongSrc.attachError(function () {
+		function fnOnError() {
+			return function () {
 				// wait for other load handlers
 				setTimeout(function () {
-					assertNoRelevantPending(this.sNotFoundSrc, false, assert);
+					// assert image was pending and then completed retrospectively from logs
+					assertPendingStartedAndFinished(this.sNotFoundSrc,false,assert);
 
 					// cleanup
 					this.oImageWrongSrc.destroy();
 					sap.ui.getCore().applyChanges();
 					fnDone();
 				}.bind(this), 0);
-			}.bind(this));
-		}.bind(this), 0);
+			};
+		}
 	});
 
 	QUnit.test("Should wait for image to load when src is changed", function (assert) {
 		var fnDone = assert.async();
 		this.oImageWrongSrc = new Image({
-			src: this.sNotFoundSrc
+			src: this.sNotFoundSrc,
+			error: fnOnError().bind(this),
+			load: fnOnComplete().bind(this)
 		});
 		this.oImageWrongSrc.placeAt("qunit-fixture");
 		sap.ui.getCore().applyChanges();
 
-		var bSrcChanged = false;
-
-		this.oImageWrongSrc.attachError(function () {
-			if (!bSrcChanged) {
-				bSrcChanged = true;
+		function fnOnError() {
+			return function () {
+				// wait for other load handlers
 				setTimeout(function () {
-					assertNoRelevantPending(this.sNotFoundSrc, false, assert);
+					// assert image was pending and then completed retrospectively from logs
+					assertPendingStartedAndFinished(this.sNotFoundSrc,false,assert);
 
 					// update image - from sNotFoundSrc to sReplacerImageSrc
 					this.oImageWrongSrc.setSrc(this.sReplacerImageSrc);
 					sap.ui.getCore().applyChanges();
-
-					// wait for the rendering to start
-					setTimeout(function () {
-						assertImagePending(this.sReplacerImageSrc, assert);
-
-						this.oImageWrongSrc.attachLoad(fnOnComplete(true).bind(this));
-					}.bind(this), 0);
 				}.bind(this), 0);
-			}
-		}.bind(this));
+			};
+		}
 
-		function fnOnComplete(bPass) {
+		function fnOnComplete() {
 			return function () {
 				// wait for load handlers
 				setTimeout(function () {
-					assertNoRelevantPending(this.sReplacerImageSrc, bPass, assert);
+					// assert image was pending and then completed retrospectively from logs
+					assertPendingStartedAndFinished(this.sReplacerImageSrc,true,assert);
 
 					// cleanup
 					this.oImageWrongSrc.destroy();
