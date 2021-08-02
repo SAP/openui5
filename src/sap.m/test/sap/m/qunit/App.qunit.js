8dfe37ab86293ecd22656aa6e5b7ed2316278ac5
Iliana Bobeva <iliana.bobeva@sap.com>
2021-08-02 09:37:51 +0300

    [FIX] sap.m.App: Fix error when App is invisible and has no parent
    Problem: Error is thrown, when sap.m.App is rendered initially with
    property visible="false" and has no parent. The problem comes from the
    logic, which tries to set 100% height and of the parent DOM elements
    of the App.
    
    Solution: Check if the sap.m.App has its own DOM reference, before
    trying to traverse the parent elements.
    
    BCP: 2180221467
    Change-Id: I785754bb20db6b3f25c7cec2888e8a1f659d88c4
    CR-Id: 002075125900004105602021

--- src/sap.m/test/sap/m/qunit/App.qunit.js
+++ src/sap.m/test/sap/m/qunit/App.qunit.js
@@ -233,4 +233,20 @@ sap.ui.define([
 		oAppDom = getBgDomElement(oApp);
 		assert.strictEqual(oAppDom.onmouseover, oHandlerBeforeTest, "preserved handler value");
 	});
+
+	QUnit.module("Invisible App", {
+		beforeEach: function () {
+			this.oApp = new sap.m.App({ visible: false });
+			this.oApp.placeAt("qunit-fixture");
+			sap.ui.getCore().applyChanges();
+		},
+		afterEach: function () {
+			this.oApp.destroy();
+			this.oApp = null;
+		}
+	});
+
+	QUnit.test("Error not thrown when App is invisible and has no parent", function(assert) {
+		assert.ok(true, "Error is not thrown when there is no parent of the App and it's initially invisible");
+	});
 });
