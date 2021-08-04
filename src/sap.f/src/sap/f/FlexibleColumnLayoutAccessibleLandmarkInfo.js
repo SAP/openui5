bba8b6c143c208cedcc7304de21b2d944afc8043
Ivanov <plamen.ivanov01@sap.com>
2021-08-04 18:56:17 +0300

    [FEATURE] sap.f.FlexibleColumnLayout: Added landmarkInfo aggregation
    There is a new aggregation for the sap.f.FlexibleColumnLayout control
    called landmarkInfo and it's of type
    sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo.
    
    With it, custom labels can be used for the FCL columns.
    
    There is also a new Demo Kit sample which demonstrates the new feature.
    
    JIRA: BGSOFUIPIRIN-5361
    Change-Id: Ib08e156a6c8faacc4d114ffa67d7434d5a8c401e

---
+++ src/sap.f/src/sap/f/FlexibleColumnLayoutAccessibleLandmarkInfo.js
@@ -0,0 +1,60 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides control sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo.
+sap.ui.define(['sap/ui/core/Element', './library'],
+	function(Element, library) {
+	"use strict";
+
+
+	/**
+	 * Constructor for a new <code>sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo</code> element.
+	 *
+	 * @param {string} [sId] ID for the new element, generated automatically if no ID is given
+	 * @param {object} [mSettings] Initial settings for the new element
+	 *
+	 * @class
+	 * Settings for accessible landmarks which can be applied to the container elements of a <code>sap.f.FlexibleColumnLayout</code> control.
+	 * For example, these landmarks are used by assistive technologies (such as screen readers) to provide a meaningful columns overview.
+	 * @extends sap.ui.core.Element
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 * @since 1.95
+	 *
+	 * @constructor
+	 * @public
+	 * @alias sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo
+	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
+	 */
+	var FlexibleColumnLayoutAccessibleLandmarkInfo = Element.extend("sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo", /** @lends sap.f.FlexibleColumnLayoutAccessibleLandmarkInfo.prototype */ { metadata : {
+
+		library : "sap.f",
+		properties : {
+			/**
+			 * Text that describes the landmark of the first column of the corresponding <code>sap.f.FlexibleColumnLayout</code> control.
+			 *
+			 * If not set, a predefined text is used.
+			 */
+			firstColumnLabel : {type : "string", defaultValue : null},
+
+			/**
+			 * Text that describes the landmark of the middle column of the corresponding <code>sap.f.FlexibleColumnLayout</code> control.
+			 *
+			 * If not set, a predefined text is used.
+			 */
+			middleColumnLabel : {type : "string", defaultValue : null},
+
+			/**
+			 * Text that describes the landmark of the last column of the corresponding <code>sap.f.FlexibleColumnLayout</code> control.
+			 *
+			 * If not set, a predefined text is used.
+			 */
+			lastColumnLabel : {type : "string", defaultValue : null}
+
+		}
+	}});
+
+	return FlexibleColumnLayoutAccessibleLandmarkInfo;
+});
