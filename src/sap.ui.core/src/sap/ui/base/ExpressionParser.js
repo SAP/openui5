1c18c18c03b10765a33f1fc6db78e70463880b8f
Tobias Sorn <tobias.sorn@sap.com>
2021-08-12 15:01:46 +0200

    [INTERNAL] sap.ui.base.ExpressionParser: requireSync replacement
    Used probing to replace requireSync calls.
    sap.ui.requireSync is replaced by a preceding sap.ui.require call
    which returns the module once preloaded for odata.compare and
    odata.uriEncode expressions.
    fillUriTemplate already had a preloaded check (URI.expand presence)
    therefore no probing is required.
    
    PS1: SSC/TSO: implement TDD
    PS2: SSC/TSO: review comments
    
    JIRA: CPOUI5MODELS-495
    Change-Id: I811fc0fd1c7c07cda865b738f1f3dddcaa38d14c

--- src/sap.ui.core/src/sap/ui/base/ExpressionParser.js
+++ src/sap.ui.core/src/sap/ui/base/ExpressionParser.js
@@ -44,22 +44,25 @@ sap.ui.define([
 					});
 				},
 				"compare": function () {
-					var ODataUtils;
+					var oODataUtils = sap.ui.require("sap/ui/model/odata/v4/ODataUtils")
+							|| sap.ui.requireSync("sap/ui/model/odata/v4/ODataUtils");
 
-					ODataUtils = sap.ui.requireSync("sap/ui/model/odata/v4/ODataUtils");
-					return ODataUtils.compare.apply(ODataUtils, arguments);
+					return oODataUtils.compare.apply(oODataUtils, arguments);
 				},
 				"fillUriTemplate": function (sExpression, mData) {
 					if (!URI.expand) {
+						// probing is not required since the presence of URI.expand is the indicator
+						// that URITemplate has been loaded already
 						/* URITemplate = */ sap.ui.requireSync("sap/ui/thirdparty/URITemplate");
 					}
+
 					return URI.expand(sExpression.trim(), mData).toString();
 				},
 				"uriEncode": function () {
-					var ODataUtils;
+					var oODataUtils = sap.ui.require("sap/ui/model/odata/ODataUtils")
+							|| sap.ui.requireSync("sap/ui/model/odata/ODataUtils");
 
-					ODataUtils = sap.ui.requireSync("sap/ui/model/odata/ODataUtils");
-					return ODataUtils.formatValue.apply(ODataUtils, arguments);
+					return oODataUtils.formatValue.apply(oODataUtils, arguments);
 				}
 			},
 			"parseFloat": parseFloat,
