76c76d0c7aa74b156bf309b891d89ac4b9334fbc
Petar Dimov <petar.dimov@sap.com>
2021-08-13 15:34:03 +0300

    [FIX] sap.m.SearchField: Improved aria-describedby
    SearchField is now announced as "Search field".
    
    Change-Id: I946bc22c6edbb8d6badbabe5144b194c978489bf
    BCP: 2170128516

--- src/sap.m/src/sap/m/SearchFieldRenderer.js
+++ src/sap.m/src/sap/m/SearchFieldRenderer.js
@@ -47,7 +47,12 @@ sap.ui.define([
 			sId = oSF.getId(),
 			bShowRefreshButton = oSF.getShowRefreshButton(),
 			bShowSearchBtn = oSF.getShowSearchButton(),
-			oAccAttributes = {}, // additional accessibility attributes
+			oAccAttributes = {
+				describedby: {
+					value: SearchFieldRenderer._getDescribedBy(oSF),
+					append: true
+				}
+			},
 			sToolTipValue,
 			sRefreshToolTip = oSF.getRefreshButtonTooltip(),
 			sResetToolTipValue;
@@ -121,17 +126,6 @@ sap.ui.define([
 
 			rm.attr("value", sValue);
 
-			// ARIA attributes
-			if (oSF.getEnabled() && bShowRefreshButton) {
-				var sAriaF5LabelId = InvisibleText.getStaticId("sap.m", "SEARCHFIELD_ARIA_F5");
-				if (sAriaF5LabelId) {
-					oAccAttributes.describedby = {
-						value: sAriaF5LabelId,
-						append: true
-					};
-				}
-			}
-
 			oAccAttributes.disabled = null;
 
 			rm.accessibilityState(oSF, oAccAttributes);
@@ -195,6 +189,16 @@ sap.ui.define([
 			}
 
 		rm.close("div");
+	};
+
+	SearchFieldRenderer._getDescribedBy = function (oSF) {
+		var sDescribedBy = InvisibleText.getStaticId("sap.m", "SEARCHFIELD_ARIA_DESCRIBEDBY");
+
+		if (oSF.getEnabled() && oSF.getShowRefreshButton()) {
+			sDescribedBy += " " + InvisibleText.getStaticId("sap.m", "SEARCHFIELD_ARIA_F5");
+		}
+
+		return sDescribedBy;
 	};
 
 	return SearchFieldRenderer;
