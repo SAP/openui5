80e01d078899bf05a51f5a7112658dc551244119
Frank Weigel <frank.weigel@sap.com>
2021-08-13 21:18:46 +0200

    [INTERNAL] walkthrough: avoid server absolute URLs in SDK samples
    Server absolute URLs highly depend on the execution environment and fail
    e.g. in versioned SDK deployments or in J2EE deployments that use a web
    context name.
    
    This is a follow-up to Ieb7b8c5ebe9de2b54f420cce9fcdf061daa65bd4.
    
    Change-Id: I1542cde0be95da246d0ce16aef870d09315dfdfc

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/36/webapp/localService/mockserver.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/36/webapp/localService/mockserver.js
@@ -20,7 +20,7 @@ sap.ui.define([
 			});
 
 			// simulate
-			var sPath = "../localService";
+			var sPath = sap.ui.require.toUrl("sap/ui/demo/walkthrough/localService");
 			oMockServer.simulate(sPath + "/metadata.xml", sPath + "/mockdata");
 
 			// start
