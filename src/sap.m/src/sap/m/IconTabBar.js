f7393b79e42d780083f7d7d3216cede47a8e19c7
Teodor Taushanov <teodor.taushanov@sap.com>
2021-08-25 14:04:37 +0300

    [FIX] sap.m.IconTabBar: Unnecessary invalidation is now avoided
    BCP: 1970240933
    Change-Id: I75cfbf849880d58df0eb4ee991ff3e8879accef8

--- src/sap.m/src/sap/m/IconTabBar.js
+++ src/sap.m/src/sap/m/IconTabBar.js
@@ -625,6 +625,16 @@ sap.ui.define([
 			this._getIconTabHeader().$().remove();
 		}
 	};
+
+	IconTabBar.prototype.invalidate = function (oOrigin) {
+		// oOrigin is undefined when IconTabBar invalidates itself.
+		// Otherwise, it's set with the control that triggers the invalidates.
+		if (!oOrigin || oOrigin.getDomRef()) {
+			return Control.prototype.invalidate.apply(this, arguments);
+		}
+		return this;
+	};
+
 	/* =========================================================== */
 	/*           begin: reflectors for header properties           */
 	/* =========================================================== */
