fb402365657f7f291e2c7557591e9bbd8e70adfd
Alexander Ivanov <alexander.ivanov01@sap.com>
2021-09-21 17:09:09 +0300

    [FIX] sap.m.TimePicker: Support KK display format
    BCP: 2170188015
    Change-Id: Id1549154c128053934addd7154a700d6c052b34f
    CR-Id: 002075125800002781502021

--- src/sap.m/src/sap/m/TimePicker.js
+++ src/sap.m/src/sap/m/TimePicker.js
@@ -1198,7 +1198,8 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 			}
 
 			this.b24H = sDisplayFormat.indexOf("H") !== -1;
-			this.bLeadingZero = sDisplayFormat.indexOf("HH") !== -1 || sDisplayFormat.indexOf("hh") !== -1;
+			this.bLeadingZero = sDisplayFormat.indexOf("HH") !== -1 || sDisplayFormat.indexOf("hh") !== -1
+				|| sDisplayFormat.indexOf("KK") !== -1;
 			this.sLeadingChar = this.bLeadingZero ? "0" : " ";
 			this.sAlternativeLeadingChar = this.bLeadingZero ? " " : "0";
 			this.sLeadingRegexChar = this.bLeadingZero ? "0" : "\\s";
@@ -1206,7 +1207,7 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 			oTimePicker.setPlaceholderSymbol(PLACEHOLDER_SYMBOL);
 
 			//set hours allowed chars in the mask
-			sMask = sMask.replace(/hh/ig, "h").replace(/h/ig, "h9");
+			sMask = sMask.replace(/hh/ig, "h").replace(/KK/ig, "K").replace(/K/ig, "h").replace(/h/ig, "h9");
 			if (this.b24H) {
 				sAllowedHourChars = "[" + this.sLeadingRegexChar + "012]";
 			} else {
@@ -1305,6 +1306,10 @@ sap.ui.define(['jquery.sap.global', './InputBase', './MaskInput', './MaskInputRu
 			function genValidHourValues(b24H, sLeadingChar) {
 				var iStart = b24H ? 0 : 1,
 					iEnd = b24H ? 23 : 12;
+
+				if (this._oTimePicker.getDisplayFormat().indexOf("K") > -1) {
+					iEnd = 11;
+				}
 
 				return genValues(iStart, iEnd, sLeadingChar);
 			}
