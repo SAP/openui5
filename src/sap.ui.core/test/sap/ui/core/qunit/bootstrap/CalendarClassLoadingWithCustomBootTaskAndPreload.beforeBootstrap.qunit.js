4ab7465c9da6fa8ef2ec30d83c1e51020844030f
Jiawei Cao <jiawei.cao@sap.com>
2021-09-23 14:41:53 +0200

    [INTERNAL] core.Core: load calendar class during bootstrap
    PS#6: Thorsten's proposal - Add calendar class to list of modules
    PS#7: Adapt Bootstrap rule tests
    PS#8: Enhance test
    PS#9: Eslint fix
    PS#10: Frank's Review
    PS#11: Jiawei's Review
    
    JIRA: CPOUI5FRAMEWORK-302
    Change-Id: I9b1b8a52f33b8448fda56452423f7654f3a04bf9

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/CalendarClassLoadingWithCustomBootTaskAndPreload.beforeBootstrap.qunit.js
@@ -0,0 +1,11 @@
+(function() {
+	"use strict";
+
+	window["sap-ui-config"] = window["sap-ui-config"] || {};
+	window["sap-ui-config"]["xx-bootTask"] = function(callback) {
+		// Calendar type is changed to "Japanese" after legacy date format is set to "7"
+		sap.ui.getCore().getConfiguration().getFormatSettings().setLegacyDateFormat("7");
+		callback();
+	};
+
+}());
