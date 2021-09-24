fcf6fd52c5fadcf21246e30869af63ea8dc9a231
Jan Mummenthaler <jan.mummenthaler@sap.com>
2021-09-24 14:50:15 +0200

    [INTERNAL] Placeholder: add configuration for disabling PH
    Change-Id: I6eabe6ccd6bd9a94e7e1952540db1671028b807b

--- src/sap.ui.core/src/sap/ui/core/Configuration.js
+++ src/sap.ui.core/src/sap/ui/core/Configuration.js
@@ -136,7 +136,8 @@ sap.ui.define([
 					"support"               : { type : "string[]", defaultValue : null },
 					"testRecorder"          : { type : "string[]", defaultValue : null },
 					"activeTerminologies"   : { type : "string[]", defaultValue: undefined},
-					"securityTokenHandlers"  : { type : "function[]", defaultValue: [],  noUrl: true },
+					"securityTokenHandlers"	: { type : "function[]", defaultValue: [],  noUrl: true },
+					"xx-placeholder"		: { type : "boolean",  defaultValue : true },
 					"xx-rootComponentNode"  : { type : "string",   defaultValue : "",        noUrl:true },
 					"xx-appCacheBusterMode" : { type : "string",   defaultValue : "sync" },
 					"xx-appCacheBusterHooks": { type : "object",   defaultValue : undefined, noUrl:true }, // e.g.: { handleURL: fn, onIndexLoad: fn, onIndexLoaded: fn }
@@ -600,6 +601,10 @@ sap.ui.define([
 		 */
 		getTheme : function () {
 			return this.theme;
+		},
+
+		getPlaceholder : function() {
+			return this["xx-placeholder"];
 		},
 
 		/**
