1f8eef592895e2741d92fcb295063ccc9996fb4d
Max Reichmann <max.reichmann@sap.com>
2021-09-28 09:22:06 +0200

    [INTERNAL] sap.ui.core.demokit.tutorial.odatav4.11
    Added Step 11
    
    PS1: init
    PS2: changes for step 11
    PS3: gerrit code review
    PS4: gerrit code review
    PS5: opa test init
    PS6: added opa test
    PS7: added steps 10 & 11 to demokit
    PS8: gerrit code review
    PS9: gerrit code review
    
    Change-Id: I0b998c747709d2582fd52cb372fb8bb7a78cdee9
    JIRA: CPOUI5ODATAV4-1177

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/odatav4/11/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"sap/ui/core/tutorial/odatav4/test/integration/AllJourneys"
+	], function() {
+		QUnit.start();
+	});
+});
