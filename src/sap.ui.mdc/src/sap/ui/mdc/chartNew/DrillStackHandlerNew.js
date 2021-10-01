b8ad411f146bd265b946e95182885d7be3fd1020
Christopher Pielka <c.pielka@sap.com>
2021-10-01 10:40:22 +0200

    [INTERNAL][FIX] MDC CHart Remake: Drill-Down popover fix when navigating between applications
    Change-Id: I712a716c81148ac3e4e10293e81b6fe03767a8f2

--- src/sap.ui.mdc/src/sap/ui/mdc/chartNew/DrillStackHandlerNew.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/chartNew/DrillStackHandlerNew.js
@@ -74,6 +74,14 @@ sap.ui.define([
 	 */
 	DrillStackHandler.createDrillDownPopover = function(oMDCChart) {
 
+		var oSubHeader = new Bar();
+		var oPopover = new ResponsivePopover({
+			id: oMDCChart.getId() + "-drilldownPopover",
+			contentWidth: "25rem",
+			contentHeight: "20rem",
+			placement: PlacementType.Bottom,
+			subHeader: oSubHeader
+		});
 		var oList = new List({
 			mode: ListMode.SingleSelectMaster,
 			selectionChange: function(oControlEvent) {
@@ -96,7 +104,9 @@ sap.ui.define([
 			}
 		});
 
-		var oSubHeader = new Bar();
+		oPopover.attachAfterClose(function(){
+			oPopover.destroy();
+		});
 
 		//TODO add search field
 		//var oSearchField = new SearchField({
@@ -105,14 +115,6 @@ sap.ui.define([
 		//oSearchField.attachLiveChange(function(oEvent) {
 		//this._triggerSearchInDrillDownPopover(oEvent, oList);
 		//}.bind(this));
-
-		var oPopover = new ResponsivePopover({
-			id: oMDCChart.getId() + "-drilldownPopover",
-			contentWidth: "25rem",
-			contentHeight: "20rem",
-			placement: PlacementType.Bottom,
-			subHeader: oSubHeader
-		});
 
 
 		var oRb = Core.getLibraryResourceBundle("sap.ui.mdc");
