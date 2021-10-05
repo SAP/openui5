7e42af5e75e423f99cbddc166a990f269d9e3306
Knut Thies <knut.thies@sap.com>
2021-10-05 16:03:20 +0200

    [INTERNAL] sap.ui.core.sample.odata.v4.ListBinding: make OPA more robust
    OPA:
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/odata/v4/ListBinding/Opa.qunit.html
    
    PS1: done
    PS2: adjusted commit message
    
    Change-Id: I5566fb55ff2a09e737e39297812245a505b64525
    CR-Id: 002075125800002940812021
    BCP: 2170241221

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ListBinding/pages/Main.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ListBinding/pages/Main.js
@@ -37,49 +37,50 @@ sap.ui.define([
 				checkEmployeeEquipmentInRow : function (iRow, sEquipmentName) {
 					var that = this;
 					return that.waitFor({
-						controlType : "sap.m.Table",
-						id : "EmployeeEquipments",
-						success : function (oEmployeeEquipments) {
-							var oRow = oEmployeeEquipments.getItems()[iRow];
-							Opa5.assert.strictEqual(
-									oRow.getCells()[2].getValue(),
-									sEquipmentName,
-									"Equipment name of row " + iRow + " as expected \""
-									+ sEquipmentName + "\"");
+						controlType : "sap.m.Input",
+						id : /employeeEquipment/,
+						matchers : function (oControl) {
+							return oControl.getBindingContext().getIndex() === iRow;
+						},
+						success : function (aControls) {
+							var oInput = aControls[0];
+							Opa5.assert.strictEqual(oInput.getValue(), sEquipmentName, "Equipment "
+							+ "name of row " + iRow + " as expected \"" + sEquipmentName + "\"");
 						},
 						viewName : sViewName
 					});
 				},
 				checkEmployeeNameInRow : function (iRow, sEmployeeName) {
 					return this.waitFor({
-						controlType : "sap.m.Table",
-						id : "Employees",
-						success : function (oEmployees) {
-							var oRow = oEmployees.getItems()[iRow];
-							Opa5.assert.strictEqual(
-								oRow && oRow.getCells()[0].getValue(),
-								sEmployeeName,
-								"Name of row " + iRow + " as expected \""
-									+ sEmployeeName + "\"");
+						controlType : "sap.m.Input",
+						id : /employeeName/,
+						matchers : function (oControl) {
+							return oControl.getBindingContext().getIndex() === iRow;
+						},
+						success : function (aControls) {
+							var oInput = aControls[0];
+							Opa5.assert.strictEqual(oInput.getValue(), sEmployeeName, "Name of row "
+								+ iRow + " as expected \"" + sEmployeeName + "\"");
 						},
 						viewName : sViewName
 					});
 				},
 				checkProductImageInRow : function (iRow, sUrl) {
-					var that = this;
-					return that.waitFor({
-						controlType : "sap.m.Table",
-						id : "EmployeeEquipments",
-						success : function (oEmployeeEquipments) {
-							var oRow = oEmployeeEquipments.getItems()[iRow],
-								oImage = oRow.getCells()[1];
-							Opa5.assert.strictEqual(
-								oImage.getSrc(),
-								oImage.getBinding("src").getModel().sServiceUrl + sUrl,
-								"URL of equipment image in row " + iRow + " as expected \""
-								+ sUrl + "\"");
+					return this.waitFor({
+						controlType : "sap.m.Image",
+						matchers : function (oControl) {
+							return oControl.getBindingContext().getIndex() === iRow;
 						},
-						viewName : sViewName
+						success : function (aControls) {
+							var oImage = aControls[0];
+
+							Opa5.assert.strictEqual(oImage.getSrc(),
+								oImage.getBinding("src").getModel().sServiceUrl + sUrl,
+								"URL of equipment image in row " + iRow + " as expected \"" + sUrl
+								+ "\"");
+						},
+						viewName : sViewName,
+						visible : false // only visible with realOData=true
 					});
 				},
 				checkTeamIDInForm : function (sTeamID) {
