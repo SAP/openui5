4e45b2593e76973866ad0331016fa1af38cc74bb
Alexandar Mitsev <alexandar.mitsev@sap.com>
2021-10-11 12:08:47 +0300

    [INTERNAL] Support Assistant: Remove inline styles for elements tree
    Change-Id: I2392c68ef027217ca2da8c459b4e983172763726
    BCP: 2170246268

--- src/sap.ui.support/src/sap/ui/support/supportRules/ui/external/ElementTree.js
+++ src/sap.ui.support/src/sap/ui/support/supportRules/ui/external/ElementTree.js
@@ -58,7 +58,7 @@ sap.ui.define(["jquery.sap.global"],
 		 * @private
 		 */
 		function _getElementTreeLeftColumnOfListItem(controls, paddingLeft) {
-			var html = "<offset style=\"padding-left:" + paddingLeft + "px\" >";
+			var html = "<offset data-indent=\"" + paddingLeft + "\" >";
 
 			if (controls.content.length > 0) {
 				html += "<arrow down=\"true\"></arrow>";
@@ -332,6 +332,25 @@ sap.ui.define(["jquery.sap.global"],
 			var controls = this.getData().controls;
 
 			this._treeContainer.innerHTML = this._createTreeHTML(controls);
+
+			this._provideIndentation();
+		};
+
+		/**
+		 * Sets the padding for each row in the element tree.
+		 * @private
+		 */
+		ElementTree.prototype._provideIndentation = function () {
+			var aOffsets = this._treeContainer.getElementsByTagName("offset"),
+				oOffset,
+				iIndex;
+
+			for (iIndex = 0; iIndex < aOffsets.length; iIndex++) {
+				oOffset = aOffsets[iIndex];
+				if (oOffset.dataset && oOffset.dataset.indent) {
+					oOffset.style.paddingLeft = oOffset.dataset.indent + "px";
+				}
+			}
 		};
 
 		/**
