46d0467aa3e47bb72b6aa6faf3bea71f5f16b44e
Christopher Pielka <c.pielka@sap.com>
2021-10-14 09:36:48 +0200

    [INTERNAL] MDC Chart Remake: Added designtime file
    Change-Id: I9dcd1986bb754641e3b8e8f2783a42fa34d50974

---
+++ src/sap.ui.mdc/src/sap/ui/mdc/designtime/chart/ChartNew.designtime.js
@@ -0,0 +1,70 @@
+/*
+ * ! ${copyright}
+ */
+sap.ui.define([
+
+], function () {
+	"use strict";
+
+	return {
+		properties: {
+			width: {
+				ignore: false
+			},
+			height: {
+				ignore: false
+			},
+			delegate: {
+				ignore: true
+			},
+			header: {
+				ignore: false
+			},
+			noDataText: {
+				ignore: false
+			},
+			p13nMode: {
+				ignore: false
+			},
+			legendVisible: {
+				ignore: false
+			},
+			ignoreToolbarActions: {
+				ignore: false
+			},
+			minWidth: {
+				ignore: false
+			},
+			minHeight: {
+				ignore: false
+			},
+			sortConditions: {
+				ignore: true
+			},
+			showChartTooltip: {
+				ignore: false
+			},
+			autoBindOnInit: {
+				ignore: false
+			},
+			chartType: {
+				ignore: false
+			},
+			showSelectionDetails: {
+				ignore: false
+			}
+		},
+		aggregations: {
+			items: {
+				ignore: false
+			},
+			actions: {
+				ignore: false
+			},
+			selectionDetailsActions: {
+				ignore: false
+			}
+		}
+	};
+
+});
