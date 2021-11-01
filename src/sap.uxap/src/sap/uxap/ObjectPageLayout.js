e417e21e66d01fcca5cff707cce3242e89fce0dc
Diana Pazheva <diana.pazheva@sap.com>
2021-11-01 23:56:10 +0200

    [FIX] sap.uxap.ObjectPageLayout: Corrected media update
    Problem:
    Incorrect value of the internal flag bMobileScenario
    after resize, because the value of the flag was updated
    after an unnecessary delay
    => this led to incorrect calculations of section positions
       (as logic reserved to mobile scenario was applied on desctop as well))
    => incorrect positionBottom added in the section positions cache
    => falure to identify the currently scrolled section during scroll on desctop
    
    Solution:
    Removed the delay before updating the value of the bMobileScenario flag
    on resize
    
    BCP: 2170272703
    Change-Id: Ibfdb477f7aee9676781cf29b0c2b3ad8f4377cd3
    (cherry picked from commit d3a99c18202aa6fb24bdd59d42e1c17c9be26050)
    CR-Id: 002075125800004038642021

--- src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -2740,6 +2740,9 @@ sap.ui.define([
 			return;
 		}
 
+		this._bMobileScenario = library.Utilities.isPhoneScenario(this._getCurrentMediaContainerRange());
+		this._bTabletScenario = library.Utilities.isTabletScenario(this._getCurrentMediaContainerRange());
+
 		if (!this._bDomReady) {
 			Log.info("ObjectPageLayout :: cannot _onUpdateScreenSize before dom is ready");
 			return;
@@ -2748,9 +2751,6 @@ sap.ui.define([
 		this._oLazyLoading.setLazyLoadingParameters();
 
 		setTimeout(function () {
-			this._bMobileScenario = library.Utilities.isPhoneScenario(this._getCurrentMediaContainerRange());
-			this._bTabletScenario = library.Utilities.isTabletScenario(this._getCurrentMediaContainerRange());
-
 			if (bIsAlwaysShowContentHeaderEnabled && (this._bHeaderInTitleArea != this._checkAlwaysShowContentHeader())) {
 				this.invalidate();
 			}
