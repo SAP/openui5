350936c9fcbc9b25f2381455a7148dfefaeb4619
Frank Weigel <frank.weigel@sap.com>
2021-11-14 10:03:26 +0100

    [INTERNAL] core/uxap: cleanup SDK samples
     - core: convert XMLComposite samples from component.json to
       manifest.json
     - uxap: remove entries for non-existing samples from docuindex.json
    
    Change-Id: I647a6331ee1515759c92dbfa6ab9ad3e961898b4

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/XMLComposite/01/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/XMLComposite/01/Component.js
@@ -3,25 +3,26 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel"
 ], function (UIComponent, JSONModel, ResourceModel) {
-
 	"use strict";
-	return UIComponent.extend("sap.ui.core.sample.XMLComposite.01.Component",
-		{
-			metadata : "json",
 
-			init: function () {
+	return UIComponent.extend("sap.ui.core.sample.XMLComposite.01.Component", {
+		metadata : {
+			manifest: "json"
+		},
 
-				// call the init function of the parent
-				UIComponent.prototype.init.apply(this, arguments);
+		init: function () {
 
-				// set data model
-				var oData = {
-					placeholder: "my placeholder ...",
-					searchText: "my button text ..."
-				};
-				var oModel = new JSONModel(oData);
-				this.setModel(oModel);
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
 
-			}
-		});
+			// set data model
+			var oData = {
+				placeholder: "my placeholder ...",
+				searchText: "my button text ..."
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+		}
+	});
 });
