8ec77f3e0129332e274978212dd5fa123457bbfc
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2021-11-19 11:33:31 +0100

    [INTERNAL] Theming: Remove CSS-Variable based features
    The CSS variables based theming parameter support was canceled and the already delivered code is now unused and can be removed again.
    
    PS01: Removal of tests & easy parts
    PS02: Removal of productive coding
    
    Change-Id: I2d079e153ce9549ec764e372af342a9d1f4b337e
    CR-Id: 002075125900005024982021
    BCP: n/a
    JIRA: CPOUI5FRAMEWORK-276

--- src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/themeParameters/lib1variables/library.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define(['sap/ui/core/Core', 'sap/ui/core/library'], function(Core, coreLib) {
-	"use strict";
-	sap.ui.getCore().initLibrary({
-		name: 'testlibs.themeParameters.lib1variables',
-		dependencies: [
-		]
-	});
-	return testlibs.themeParameters.lib1variables; // eslint-disable-line no-undef
-});
