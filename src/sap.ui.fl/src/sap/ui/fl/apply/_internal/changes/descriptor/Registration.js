94f912d935db64f3865a337076b16c84bc5fa566
d030990 <marcel.waechter@sap.com>
2021-12-01 10:48:58 +0100

    [INTERNAL] Add sap.fe changePageConfiguration to the sap.ui.fl Registration
    Change-Id: I50a29170f27c281d9291a753e9bf0df7d814485c

--- src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/Registration.js
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/Registration.js
@@ -27,7 +27,8 @@ sap.ui.define([
 		appdescr_ovp_addNewCard: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/ovp/AddNewCard"),
 		appdescr_ovp_removeCard: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/ovp/DeleteCard"),
 		appdescr_ui_generic_app_addNewObjectPage: requireAsync.bind(this, "sap/suite/ui/generic/template/manifestMerger/AddNewObjectPage"),
-		appdescr_ui_generic_app_changePageConfiguration: requireAsync.bind(this, "sap/suite/ui/generic/template/manifestMerger/ChangePageConfiguration")
+		appdescr_ui_generic_app_changePageConfiguration: requireAsync.bind(this, "sap/suite/ui/generic/template/manifestMerger/ChangePageConfiguration"),
+		appdescr_fe_changePageConfiguration: requireAsync.bind(this, "sap/fe/core/manifestMerger/ChangePageConfiguration")
 	};
 	return Registration;
 });
