c0e3eb000f214a468eca3fe4235e2096327b4d13
Sebastian Allmann <sebastian.allmann@sap.com>
2021-12-02 08:54:09 +0100

    [INTERNAL] ValueHelp: fix JSDoc
    Change-Id: I561b80cbf86710e648dddff2c30acc485bd755bd

--- src/sap.ui.mdc/src/sap/ui/mdc/ValueHelp.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/ValueHelp.js
@@ -73,7 +73,8 @@ sap.ui.define([
 	 * @private
 	 * @ui5-restricted sap.fe
 	 * @MDC_PUBLIC_CANDIDATE
-	 * @since 1.91.0
+	 * @since 1.95.0
+	 * @experimental As of version 1.95
 	 * @alias sap.ui.mdc.ValueHelp
 	 * @ui5-metamodel This control/element also will be described in the UI5 (legacy) designtime metamodel
 	 */
@@ -338,8 +339,6 @@ sap.ui.define([
 	 * @returns {this} Reference to <code>this</code> in order to allow method chaining
 	 * @private
 	 * @ui5-restricted sap.ui.mdc.field.FieldBase
-	 * @since 1.95.0
-	 * @experimental As of version 1.95
 	 * @ui5-metamodel This method also will be described in the UI5 (legacy) designtime metamodel
 	 */
 	ValueHelp.prototype.connect = function(oControl, oConfig) {
