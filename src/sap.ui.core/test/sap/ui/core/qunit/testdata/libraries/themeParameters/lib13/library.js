780f0d6d4750cd82f980c1bd7290638ac49d2270
Johannes Gluch <johannes.gluch@sap.com>
2021-12-02 13:40:54 +0100

    [FIX] theming.Parameters: Fix async usage after theme change
    - In case a theme change event is triggered it can possibly happen,
      that theme parameters for already loaded libraries are not correctly
      parsed/deleted in case parameters where retrieved using the async API
    
    BCP: 2180393575
    CR-Id: 002075125900005341062021
    Change-Id: I777d77d0ee6162f15422ca4da000fd84edba3fbc
    (cherry picked from commit be71b0750686d9779a03eec7281f36b957a6ea6f)

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/themeParameters/lib13/library.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/ui/core/Core', 'sap/ui/core/library'], function(Core, coreLib) {
+	"use strict";
+	sap.ui.getCore().initLibrary({
+		name: 'testlibs.themeParameters.lib13',
+		dependencies: [
+		]
+	});
+	return testlibs.themeParameters.lib13; // eslint-disable-line no-undef
+});
