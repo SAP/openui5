9f5a0e2c2f00d950282c1bbfcec366f70a46e4ee
Jiawei Cao <jiawei.cao@sap.com>
2022-02-22 09:38:15 +0100

    [FIX] core/m: fix viewport meta for iOS platform
    - maximum-scale=1.0 and user-scalable=no is still needed for iOS
      platform to disable the auto zoom when input is focused. Since iOS
      already ignores this setting and user can manually zoom although this
      setting is applied, we can set this to disable auto zoom while still
      enable the manual zoom for the end user
    
    BCP: 2280037152
    Change-Id: I838e333db98f0afb8da99a4e8285c6173ade0203
    CR-Id: 002075125800001536262022

--- src/sap.m/test/sap/m/qunit/App.qunit.js
+++ src/sap.m/test/sap/m/qunit/App.qunit.js
@@ -65,7 +65,11 @@ sap.ui.define([
 		// check viewport:  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		var $v = jQuery("meta").filter("[name=viewport]");
 		assert.equal($v.length, 1, "There should be a viewport meta tag");
-		assert.equal($v.attr("content"), "width=device-width, initial-scale=1.0", "The viewport meta tag content should be correct");
+		if (Device.os.ios) {
+			assert.equal($v.attr("content"), "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no", "The viewport meta tag content should be correct");
+		} else {
+			assert.equal($v.attr("content"), "width=device-width, initial-scale=1.0", "The viewport meta tag content should be correct");
+		}
 	});
 
 	QUnit.test("orientationChange event", function(assert) {
