843ab2e099dd22097ee4090548f2ae72e83adeb3
Sven Bender <sven.bender@sap.com>
2022-02-25 10:38:36 +0100

    [INTERNAL] sap/ui/thirdparty: bump URI.js to 1.19.8
    Change-Id: I44011c1ee1ca853972e13e33aef73b5c9da7a530
    BCP: 2280080962
    CR-Id: 002075125800001437882022

--- src/sap.ui.core/src/sap/ui/thirdparty/URI.js
+++ src/sap.ui.core/src/sap/ui/thirdparty/URI.js
@@ -1,7 +1,7 @@
 /*!
  * URI.js - Mutating URLs
  *
- * Version: 1.19.7
+ * Version: 1.19.8
  *
  * Author: Rodney Rehm
  * Web: http://medialize.github.io/URI.js/
@@ -90,7 +90,7 @@
     return /^[0-9]+$/.test(value);
   }
 
-  URI.version = '1.19.7';
+  URI.version = '1.19.8';
 
   var p = URI.prototype;
   var hasOwn = Object.prototype.hasOwnProperty;
@@ -522,7 +522,7 @@
     }
 
     // slashes and backslashes have lost all meaning for the web protocols (https, http, wss, ws)
-    string = string.replace(/^(https?|ftp|wss?)?:[/\\]*/, '$1://');
+    string = string.replace(/^(https?|ftp|wss?)?:[/\\]*/i, '$1://');
 
     // extract protocol
     if (string.substring(0, 2) === '//') {
