6655a6c85613fc175586e9932aec450cf2a8159a
Johannes Gluch <johannes.gluch@sap.com>
2022-03-15 13:14:59 +0100

    [INTERNAL] sap.ui.core: Migrate central testsuite
    - Improve unit tests to use modern teststarter and
      to become theme-independent
    
    JIRA: CPOUI5FRAMEWORK-354
    Change-Id: I1973a7e04e1a21d41544dd4f90932d8d9190d143

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testsuite.qunit.js
@@ -0,0 +1,98 @@
+sap.ui.define(function() {
+
+	"use strict";
+	return {
+		name: "TestSuite for sap.ui.core",
+		defaults: {
+			qunit: {
+				version: 2
+			},
+			sinon: {
+				version: 4
+			},
+			page: "test-resources/sap/ui/core/qunit/{name}/testsuite.{name}.qunit.html"
+		},
+		tests: {
+			"Test Starter": {
+				page: "test-resources/sap/ui/core/qunit/test/starter/testsuite.starter.qunit.html"
+			},
+			app: {},
+			bootstrap: {},
+			csp: {},
+			compat: {},
+			component: {},
+			composite: {},
+			"Messaging": {
+				page: "test-resources/sap/ui/core/qunit/messages/testsuite.messaging.base.qunit.html"
+			},
+			mvc: {},
+			routing: {},
+			"Core": {
+				page: "test-resources/sap/ui/core/qunit/testsuite.core.qunit.html"
+			},
+			"Core Framework": {
+				page: "test-resources/sap/ui/core/qunit/testsuite.core.framework.qunit.html"
+			},
+			"Browser Runtime": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.browser.runtime.qunit.html"
+			},
+			"Control Framework": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.control.framework.qunit.html"
+			},
+			"Controls": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.controls.qunit.html"
+			},
+			"Data Binding": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.databinding.qunit.html"
+			},
+			"Eventing": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.eventing.qunit.html"
+			},
+			"Foundation Enablement": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.foundation.enablement.qunit.html"
+			},
+			"Managed Object": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.managed.object.qunit.html"
+			},
+			"Modular Core": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.modular.core.qunit.html"
+			},
+			"Samples": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.samples.qunit.html"
+			},
+			"Security": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.security.qunit.html"
+			},
+			"Supportability": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.supportability.qunit.html"
+			},
+			"Theming": {
+				page: "test-resources/sap/ui/core/qunit/testsuites/testsuite.theming.qunit.html"
+			},
+			service: {},
+			tmpl: {},
+			designtime: {},
+			dnd: {},
+			generic: {},
+			"Legacy generic Testsuite": {
+				page: "test-resources/sap/ui/core/qunit/generic/legacy/testsuite.generic.qunit.html"
+			},
+			gherkin: {},
+			i18n: {},
+			json: {},
+			loader: {},
+			mockserver: {},
+			"OData Types": {
+				page: "test-resources/sap/ui/core/qunit/odata/type/testsuite.odata.types.qunit.html"},
+			"OData V2": {
+				page: "test-resources/sap/ui/core/qunit/odata/v2/testsuite.odatav2.qunit.html"},
+			"OData V4": {
+				page: "test-resources/sap/ui/core/qunit/odata/v4/testsuite.odatav4.qunit.html"},
+			opa: {},
+			types: {},
+			util: {},
+			rule: {},
+			xml: {}
+		}
+	};
+});
