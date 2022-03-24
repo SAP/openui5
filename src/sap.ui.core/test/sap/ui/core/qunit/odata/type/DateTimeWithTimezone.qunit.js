7a6539a90c3e675efa8839af273dadae8f1f55a8
Sven Bernhard <sven.bernhard@sap.com>
2022-03-24 12:44:54 +0100

    [INTERNAL] sap.ui.model.odata.type.DateTimeWithTimezone: support new format options
    The date formatter introduced new format options showDate and showTime
    and overloaded showTimezone with different meaning.
    DateTimeWithTimezone type supports the new format options and maps the
    old values for showTimezone to the new format options for temporary
    compatibility.
    
    PS1: PP LKI, SBE: use new format options and map old values accordingly
    PS2: PP LKI, SBE: simplified code and test, formatting
    PS3: PP LKI, SBE: review comments (exception message, defaulting, test
         improvement)
    
    Change-Id: I2d7ec4afbbb9ba4d02133a3b7355c17f9c778980
    JIRA: CPOUI5MODELS-895

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/type/DateTimeWithTimezone.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/type/DateTimeWithTimezone.qunit.js
@@ -4,7 +4,6 @@
 sap.ui.define([
 	"sap/base/Log",
 	"sap/ui/core/format/DateFormat",
-	"sap/ui/core/format/DateFormatTimezoneDisplay",
 	"sap/ui/model/_Helper",
 	"sap/ui/model/CompositeType",
 	"sap/ui/model/FormatException",
@@ -13,8 +12,8 @@ sap.ui.define([
 	"sap/ui/model/odata/type/Decimal",
 	"sap/ui/model/odata/type/String",
 	"sap/ui/test/TestUtils"
-], function (Log, DateFormat, DateFormatTimezoneDisplay, _Helper, CompositeType, FormatException,
-		ParseException, DateTimeWithTimezone, DecimalType, StringType, TestUtils) {
+], function (Log, DateFormat, _Helper, CompositeType, FormatException, ParseException,
+		DateTimeWithTimezone, DecimalType, StringType, TestUtils) {
 	/*global sinon, QUnit*/
 	/*eslint max-nested-callbacks: 0*/
 	"use strict";
@@ -62,6 +61,9 @@ sap.ui.define([
 		assert.strictEqual(oType.bParseWithValues, true);
 		assert.strictEqual(oType.bUseInternalValues, true);
 		assert.strictEqual(oType.vEmptyTimezoneValue, null);
+		assert.strictEqual(oType.bShowDate, true);
+		assert.strictEqual(oType.bShowTime, true);
+		assert.strictEqual(oType.bShowTimezone, true);
 
 		// cloning the type does not throw an error
 		// code under test
@@ -69,6 +71,55 @@ sap.ui.define([
 
 		assert.ok(oType instanceof DateTimeWithTimezone, "is a DateTimeWithTimezone");
 	});
+
+	//*********************************************************************************************
+[{
+	oFormatOptions : {showTimezone : "Show"},
+	oResult : {bShowDate : true, bShowTime : true, bShowTimezone : true}
+}, {
+	oFormatOptions : {showTimezone : "Hide"},
+	oResult : {bShowDate : true, bShowTime : true, bShowTimezone : false}
+}, {
+	oFormatOptions : {showTimezone : "Only"},
+	oResult : {bShowDate : false, bShowTime : false, bShowTimezone : true}
+}].forEach(function (oFixture, i) {
+	QUnit.test("constructor: legacy format options #" + i, function (assert) {
+		// code under test
+		var oType = new DateTimeWithTimezone(oFixture.oFormatOptions);
+
+		assert.strictEqual(oType.bShowDate, oFixture.oResult.bShowDate);
+		assert.strictEqual(oType.bShowTime, oFixture.oResult.bShowTime);
+		assert.strictEqual(oType.bShowTimezone, oFixture.oResult.bShowTimezone);
+	});
+});
+
+	//*********************************************************************************************
+[true, false, "foo", null].forEach(function (vValue, i) {
+	QUnit.test("constructor: new format options, #" + i, function (assert) {
+		var oType;
+
+		// code under test
+		oType = new DateTimeWithTimezone({showDate : vValue});
+
+		assert.strictEqual(oType.bShowDate, vValue);
+		assert.strictEqual(oType.bShowTime, true);
+		assert.strictEqual(oType.bShowTimezone, true);
+
+		// code under test
+		oType = new DateTimeWithTimezone({showTime : vValue});
+
+		assert.strictEqual(oType.bShowDate, true);
+		assert.strictEqual(oType.bShowTime, vValue);
+		assert.strictEqual(oType.bShowTimezone, true);
+
+		// code under test
+		oType = new DateTimeWithTimezone({showTimezone : vValue});
+
+		assert.strictEqual(oType.bShowDate, true);
+		assert.strictEqual(oType.bShowTime, true);
+		assert.strictEqual(oType.bShowTimezone, vValue);
+	});
+});
 
 	//*********************************************************************************************
 	QUnit.test("constructor: setFormatOptions/setConstraints called", function (assert) {
@@ -110,14 +161,13 @@ sap.ui.define([
 
 	//*********************************************************************************************
 	QUnit.test("constructor: format options are immutable", function (assert) {
-		var oFormatOptions = {showTimezone : DateFormatTimezoneDisplay.Only},
+		var oFormatOptions = {foo : "bar"},
 			oType = new DateTimeWithTimezone(oFormatOptions);
 
 		// code under test
-		oFormatOptions.showTimezone = DateFormatTimezoneDisplay.Show;
+		oFormatOptions.foo = "baz";
 
-		assert.deepEqual(oType.getFormatOptions(), {showTimezone : DateFormatTimezoneDisplay.Only},
-			"format option showTimezone is 'Only'");
+		assert.deepEqual(oType.getFormatOptions(), {foo : "bar"});
 
 		assert.throws(function () {
 			// code under test
@@ -153,8 +203,8 @@ sap.ui.define([
 });
 
 	//*********************************************************************************************
-	QUnit.test("formatValue: no Date and showTimezone = 'Hide' returns null", function (assert) {
-		var oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Hide});
+	QUnit.test("formatValue: no Date and showTimezone = false returns null", function (assert) {
+		var oType = new DateTimeWithTimezone({showTimezone : false});
 
 		// code under test
 		assert.strictEqual(oType.formatValue([null, "~timezone"], "string"), null);
@@ -177,9 +227,10 @@ sap.ui.define([
 
 	//*********************************************************************************************
 [
-	{},
-	{showTimezone : DateFormatTimezoneDisplay.Only},
-	{showTimezone : DateFormatTimezoneDisplay.Show}
+	{showDate : true, showTime : true, showTimezone : true},
+	{showDate : false, showTime : true, showTimezone : true},
+	{showDate : true, showTime : false, showTimezone : true},
+	{showDate : false, showTime : false, showTimezone : true}
 ].forEach(function (oFormatOptions) {
 	var sTitle = "formatValue: no Date given, sTargetType = 'string', oFormatOptions = "
 		+ JSON.stringify(oFormatOptions);
@@ -220,7 +271,7 @@ sap.ui.define([
 	//*********************************************************************************************
 	QUnit.test("formatValue: to string", function (assert) {
 		var oDate = new Date(),
-			oFormat = {format : function (){}},
+			oFormat = {format : function () {}},
 			oFormatMock = this.mock(oFormat),
 			oType = new DateTimeWithTimezone(),
 			oTypeMock = this.mock(oType);
@@ -251,23 +302,26 @@ sap.ui.define([
 	});
 
 	//*********************************************************************************************
-	QUnit.test("formatValue: to object, must not have showTimezone = 'Only'", function (assert) {
-		var oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Only});
+	QUnit.test("formatValue: to object, must not hide time zone", function (assert) {
+		var oType = new DateTimeWithTimezone({showDate : false, showTime : false});
 
 		this.mock(oType).expects("getPrimitiveType").withExactArgs("~targetType").returns("object");
 
 		assert.throws(function () {
 			// code under test
 			oType.formatValue([new Date(), "~timezone"], "~targetType");
-		}, new FormatException("For type 'object', the format option 'showTimezone' must not be "
-			+ "set to sap.ui.core.format.DateFormatTimezoneDisplay.Only"));
+		}, new FormatException("For type 'object', at least one of the format options 'showDate' or"
+			+ " 'showTime' must be enabled"));
 	});
 
 	//*********************************************************************************************
 [
-	{},
-	{showTimezone : DateFormatTimezoneDisplay.Hide},
-	{showTimezone : DateFormatTimezoneDisplay.Show}
+	{showDate : true, showTime : true, showTimezone : true},
+	{showDate : true, showTime : false, showTimezone : true},
+	{showDate : false, showTime : true, showTimezone : true},
+	{showDate : true, showTime : true, showTimezone : false},
+	{showDate : false, showTime : true, showTimezone : false},
+	{showDate : true, showTime : false, showTimezone : false}
 ].forEach(function (oFormatOptions, i) {
 	QUnit.test("formatValue: to object", function (assert) {
 		var oDate = new Date(),
@@ -282,33 +336,49 @@ sap.ui.define([
 
 	//*********************************************************************************************
 [{
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Only},
+	formatOptions : {showDate : false, showTime : false, showTimezone : true},
 	values : [new Date(), "Europe/Berlin"],
 	result : "Europe/Berlin"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "Europe/Berlin"],
 	result : "Dec 30, 2021, 5:00:00 AM Europe/Berlin"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Hide},
+	formatOptions : {showDate : true, showTime : true, showTimezone : false},
 	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "Europe/Berlin"],
 	result : "Dec 30, 2021, 5:00:00 AM"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
 	result : "Dec 29, 2021, 11:00:00 PM America/New_York"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Hide},
+	formatOptions : {showDate : true, showTime : true, showTimezone : false},
 	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
 	result : "Dec 29, 2021, 11:00:00 PM"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Only},
+	formatOptions : {showDate : false, showTime : false, showTimezone : true},
 	values : [null, "Europe/Berlin"],
 	result : "Europe/Berlin"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	values : [null, "Europe/Berlin"],
 	result : "Europe/Berlin"
+}, {
+	formatOptions : {showDate : true, showTime : false, showTimezone : true},
+	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
+	result : "Dec 29, 2021 America/New_York"
+}, {
+	formatOptions : {showDate : false, showTime : true, showTimezone : true},
+	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
+	result : "11:00:00 PM America/New_York"
+}, {
+	formatOptions : {showDate : true, showTime : false, showTimezone : false},
+	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
+	result : "Dec 29, 2021"
+}, {
+	formatOptions : {showDate : false, showTime : true, showTimezone : false},
+	values : [new Date(Date.UTC(2021, 11, 30, 4, 0, 0)), "America/New_York"],
+	result : "11:00:00 PM"
 }].forEach(function (oFixture, i) {
 	QUnit.test("formatValue: Integrative tests: " + oFixture.result + ", #" + i, function (assert) {
 		var oType = new DateTimeWithTimezone(oFixture.formatOptions);
@@ -325,9 +395,8 @@ sap.ui.define([
 	""
 ].forEach(function (vValue) {
 	[
-		{},
-		{showTimezone : DateFormatTimezoneDisplay.Hide},
-		{showTimezone : DateFormatTimezoneDisplay.Show}
+		{showDate : true, showTime : true, showTimezone : true},
+		{showDate : true, showTime : true, showTimezone : false}
 	].forEach(function (oFormatOptions, i) {
 	QUnit.test("parseValue: " + vValue + " to [null, undefined], #" + i, function (assert) {
 		var oType = new DateTimeWithTimezone(oFormatOptions);
@@ -346,11 +415,11 @@ sap.ui.define([
 	null,
 	""
 ].forEach(function (vValue, i) {
-	var sTitle = "parseValue: time zone is set to default if value is not set and if "
-		+ "showTimezone = 'Only'; #" + i;
+	var sTitle = "parseValue: time zone is set to default if value is not set and only timezone is "
+			+ "displayed; #" + i;
 
 	QUnit.test(sTitle, function (assert) {
-		var oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Only});
+		var oType = new DateTimeWithTimezone({showDate : false, showTime : false});
 
 		oType.vEmptyTimezoneValue = "~emptyTimezoneValue";
 
@@ -386,7 +455,7 @@ sap.ui.define([
 
 	//*********************************************************************************************
 	QUnit.test("parseValue: to string; sValue is not parsable", function (assert) {
-		var oFormat = {parse : function (){}},
+		var oFormat = {parse : function () {}},
 			oType = new DateTimeWithTimezone();
 
 		this.mock(oType).expects("getPrimitiveType").withExactArgs("~sourceType").returns("string");
@@ -413,7 +482,7 @@ sap.ui.define([
 
 	//*********************************************************************************************
 	QUnit.test("parseValue: to string", function (assert) {
-		var oFormat = {parse : function (){}},
+		var oFormat = {parse : function () {}},
 			oFormatMock = this.mock(oFormat),
 			aParsedDate = ["~timestamp", "~timezone"],
 			oType = new DateTimeWithTimezone(),
@@ -443,26 +512,25 @@ sap.ui.define([
 	});
 
 	//*********************************************************************************************
-	QUnit.test("parseValue: to object, must not have showTimezone = 'Only'", function (assert) {
-		var oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Only});
+	QUnit.test("parseValue: to object, a date or time value must be displayed", function (assert) {
+		var oType = new DateTimeWithTimezone({showDate : false, showTime : false});
 
 		this.mock(oType).expects("getPrimitiveType").withExactArgs("~sourceType").returns("object");
 
 		assert.throws(function () {
 			// code under test
 			oType.parseValue(new Date(), "~sourceType", [null, "Europe/Berlin"]);
-		}, new ParseException("For type 'object', the format option 'showTimezone' must not be set "
-			+ "to sap.ui.core.format.DateFormatTimezoneDisplay.Only"));
+		}, new ParseException("For type 'object', at least one of the format options 'showDate' or"
+			+ " 'showTime' must be enabled"));
 	});
 
 	//*********************************************************************************************
 [
-	{},
-	{showTimezone : DateFormatTimezoneDisplay.Show},
-	{showTimezone : DateFormatTimezoneDisplay.Hide}
+	{showDate : true, showTime : true, showTimezone : true},
+	{showDate : true, showTime : true, showTimezone : false}
 ].forEach(function (oFormatOptions) {
 	[undefined, null].forEach(function (vValue) {
-	var sTitle = "parseValue: to object, " + vValue + " with showTimezone = "
+	var sTitle = "parseValue: to object, " + vValue + " with format options = "
 		+ JSON.stringify(oFormatOptions) + " results in [null, undefined]";
 
 	QUnit.test(sTitle, function (assert) {
@@ -480,7 +548,7 @@ sap.ui.define([
 	//*********************************************************************************************
 [{}, "foo", 42].forEach(function (vValue) {
 	QUnit.test("parseValue: to object, not a Date object", function (assert) {
-		var oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Hide});
+		var oType = new DateTimeWithTimezone({showTimezone : false});
 
 		this.mock(oType).expects("getPrimitiveType").withExactArgs("~sourceType").returns("object");
 
@@ -495,7 +563,7 @@ sap.ui.define([
 	QUnit.test("parseValue: to object", function (assert) {
 		var oDate = new Date(),
 			aResult,
-			oType = new DateTimeWithTimezone({showTimezone : DateFormatTimezoneDisplay.Hide});
+			oType = new DateTimeWithTimezone({showTimezone : false});
 
 		this.mock(oType).expects("getPrimitiveType").withExactArgs("~sourceType").returns("object");
 
@@ -509,37 +577,37 @@ sap.ui.define([
 	//*********************************************************************************************
 [{
 	currentTimezone : "Europe/Berlin",
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Only},
+	formatOptions : {showDate : false, showTime : false, showTimezone : true},
 	value : "Europe/Berlin",
 	result : [undefined, "Europe/Berlin"]
 }, {
 	currentTimezone : null,
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Only},
+	formatOptions : {showDate : false, showTime : false, showTimezone : true},
 	value : "America/New_York",
 	result : [undefined, "America/New_York"]
 }, {
 	currentTimezone : "Europe/Berlin",
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Hide},
+	formatOptions : {showDate : true, showTime : true, showTimezone : false},
 	value : "Dec 30, 2021, 8:00:00 AM",
 	result : [new Date(Date.UTC(2021, 11, 30, 7, 0, 0)), undefined]
 }, {
 	currentTimezone : "Europe/Berlin",
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	value : "Dec 30, 2021, 8:00:00 AM Europe/Berlin",
 	result : [new Date(Date.UTC(2021, 11, 30, 7, 0, 0)), "Europe/Berlin"]
 }, {
 	currentTimezone : "Europe/Berlin",
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	value : "Dec 30, 2021, 8:00:00 AM",
 	result : [new Date(Date.UTC(2021, 11, 30, 7, 0, 0)), undefined]
 }, {
 	currentTimezone : null,
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	value : "Dec 30, 2021, 8:00:00 AM Europe/Berlin",
 	result : [new Date(Date.UTC(2021, 11, 30, 7, 0, 0)), "Europe/Berlin"]
 }, {
 	currentTimezone : null,
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	value : "Dec 30, 2021, 8:00:00 AM",
 	result : [new Date(Date.UTC(2021, 11, 30, 8, 0, 0)), undefined]
 }].forEach(function (oFixture, i) {
@@ -565,16 +633,25 @@ sap.ui.define([
 
 	//*********************************************************************************************
 [{
-	formatOptions : undefined,
+	formatOptions : {showDate : true, showTime : true, showTimezone : true},
 	messageKey : "EnterDateTime"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Hide},
+	formatOptions : {showDate : true, showTime : true, showTimezone : false},
 	messageKey : "EnterDateTime"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Only},
+	formatOptions : {showDate : false, showTime : true, showTimezone : false},
+	messageKey : "EnterDateTime"
+}, {
+	formatOptions : {showDate : true, showTime : false, showTimezone : false},
+	messageKey : "EnterDateTime"
+}, {
+	formatOptions : {showDate : false, showTime : false, showTimezone : true},
 	messageKey : "EnterDateTimeTimezone"
 }, {
-	formatOptions : {showTimezone : DateFormatTimezoneDisplay.Show},
+	formatOptions : {showDate : true, showTime : false, showTimezone : true},
+	messageKey : "EnterDateTime"
+}, {
+	formatOptions : {showDate : false, showTime : true, showTimezone : true},
 	messageKey : "EnterDateTime"
 }].forEach(function (oFixture, i) {
 	QUnit.test("_getErrorMessage: #" + i, function (assert) {
@@ -615,9 +692,11 @@ sap.ui.define([
 	//*********************************************************************************************
 [
 	{oFormatOptions : undefined, aResult : []},
-	{oFormatOptions : {showTimezone : DateFormatTimezoneDisplay.Show}, aResult : []},
-	{oFormatOptions : {showTimezone : DateFormatTimezoneDisplay.Only}, aResult : [0]},
-	{oFormatOptions : {showTimezone : DateFormatTimezoneDisplay.Hide}, aResult : [1]}
+	{oFormatOptions : {showDate : true, showTime : true, showTimezone : true}, aResult : []},
+	{oFormatOptions : {showDate : false, showTime : false, showTimezone : true}, aResult : [0]},
+	{oFormatOptions : {showDate : true, showTime : true, showTimezone : false}, aResult : [1]},
+	{oFormatOptions : {showDate : false, showTime : true, showTimezone : false}, aResult : [1]},
+	{oFormatOptions : {showDate : true, showTime : false, showTimezone : false}, aResult : [1]}
 ].forEach(function (oFixture, i) {
 	QUnit.test("getPartsIgnoringMessages: #" + i, function (assert) {
 		var oType = new DateTimeWithTimezone(oFixture.oFormatOptions);
