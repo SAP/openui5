a1a31e372ec887aef1db643ad551fb55182afb66
Kevin Edinger <kevin.edinger@sap.com>
2022-04-21 12:49:48 +0200

    [INTERNAL] sap.ui.rta: add check for flexEnabled in the start adaptation APIs
    previously the check was done only in the FLP plugin, which does not cover standalone scenarios.
    Now the API rejects the promise with a specific Error, so that any consumer can react properly.
    
    BCP: 2270073475
    Change-Id: I06df590d52118d9f0a4403656392793edb32bb79

--- src/sap.ui.rta/test/sap/ui/rta/qunit/api/startKeyUserAdaptation.qunit.js
+++ src/sap.ui.rta/test/sap/ui/rta/qunit/api/startKeyUserAdaptation.qunit.js
@@ -3,86 +3,49 @@
 sap.ui.define([
 	"sap/ui/rta/RuntimeAuthoring",
 	"sap/ui/rta/api/startKeyUserAdaptation",
-	"sap/ui/fl/registry/Settings",
 	"sap/ui/fl/write/api/FeaturesAPI",
-	"sap/base/Log",
+	"sap/ui/fl/write/api/PersistenceWriteAPI",
+	"sap/ui/fl/Layer",
 	"sap/ui/thirdparty/sinon-4",
 	"test-resources/sap/ui/rta/qunit/RtaQunitUtils"
 ], function(
 	RuntimeAuthoring,
 	startKeyUserAdaptation,
-	Settings,
 	FeaturesAPI,
-	Log,
+	PersistenceWriteAPI,
+	Layer,
 	sinon,
 	RtaQunitUtils
 ) {
 	"use strict";
 	var sandbox = sinon.createSandbox();
-
-	function setIsKeyUser(bIsKeyUser) {
-		sandbox.stub(FeaturesAPI, "isKeyUser").resolves(bIsKeyUser);
-	}
+	var oAppComponent = RtaQunitUtils.createAndStubAppComponent(sinon);
 
 	QUnit.module("Given startKeyUserAdaptation()", {
-		beforeEach: function () {
-			sandbox.spy(Log, "error");
-			this.oMockedAppComponent = RtaQunitUtils.createAndStubAppComponent(sandbox);
-			this.fnRtaStartStub = sandbox.stub(RuntimeAuthoring.prototype, "start").resolves();
-			this.aObjectsToBeDestroyed = [];
-		},
 		afterEach: function () {
-			Settings._instance = undefined;
-			this.oMockedAppComponent.destroy();
 			sandbox.restore();
 		}
 	}, function() {
-		QUnit.test("when called and restart is not needed", function(assert) {
-			var done = assert.async();
-			setIsKeyUser(true);
-			startKeyUserAdaptation({rootControl: this.oMockedAppComponent})
-				.then(function() {
-					var oRta = this.fnRtaStartStub.getCall(0).thisValue;
-					sandbox.stub(oRta, "destroy");
-					oRta.attachEventOnce("stop", function() {
-						assert.ok(oRta.destroy.calledOnce, "then destroy() was called when stop() was called");
-						done();
-					});
-					assert.strictEqual(oRta.getRootControl(), this.oMockedAppComponent.getId(), "then correct root control was set");
-					sandbox.stub(oRta, "_handleReloadOnExit").resolves(oRta._RELOAD.NOT_NEEDED);
-					oRta.stop(true);
-				}.bind(this));
-		});
-
-		QUnit.test("when called with an invalid root control", function(assert) {
-			setIsKeyUser(true);
-			return startKeyUserAdaptation({rootControl: {}})
-				.catch(function(oError) {
-					assert.ok(this.fnRtaStartStub.notCalled, "then RuntimeAuthoring.start() was not called");
-					assert.ok(oError instanceof Error, "then promise was rejected with an error");
-				}.bind(this));
-		});
-
-		QUnit.test("when called and the user is not a key user", function(assert) {
-			setIsKeyUser(false);
-			return startKeyUserAdaptation({rootControl: this.oMockedAppComponent})
-				.catch(function(oError) {
-					assert.ok(this.fnRtaStartStub.notCalled, "then RuntimeAuthoring.start() was not called");
-					assert.ok(oError instanceof Error, "then an error was thrown");
-				}.bind(this));
-		});
-
-		QUnit.test("when called and flex settings could not be loaded", function(assert) {
-			sandbox.stub(Settings, "getInstance").rejects();
-			return startKeyUserAdaptation({rootControl: this.oMockedAppComponent})
-				.catch(function(oError) {
-					assert.ok(this.fnRtaStartStub.notCalled, "then RuntimeAuthoring.start() was not called");
-					assert.ok(oError instanceof Error, "then an error was thrown");
-				}.bind(this));
+		QUnit.test("when called with an object", function(assert) {
+			sandbox.stub(FeaturesAPI, "isKeyUser").resolves(true);
+			sandbox.stub(RuntimeAuthoring.prototype, "start");
+			sandbox.stub(PersistenceWriteAPI, "getChangesWarning").resolves({});
+			return startKeyUserAdaptation({
+				rootControl: oAppComponent
+			})
+			.then(function(oRta) {
+				var oExpectedSettings = {
+					developerMode: false,
+					layer: Layer.CUSTOMER
+				};
+				assert.deepEqual(oRta.getFlexSettings(), oExpectedSettings, "the flex settings were properly set");
+				assert.strictEqual(oRta.getRootControl(), oAppComponent.getId(), "then correct root control was set");
+			});
 		});
 	});
 
 	QUnit.done(function() {
+		oAppComponent.destroy();
 		jQuery("#qunit-fixture").hide();
 	});
 });
--- src/sap.ui.rta/test/sap/ui/rta/qunit/util/adaptationStarter.qunit.js
+++ src/sap.ui.rta/test/sap/ui/rta/qunit/util/adaptationStarter.qunit.js
@@ -1,33 +1,37 @@
 /*global QUnit*/
 
 sap.ui.define([
-	"sap/ui/rta/util/adaptationStarter",
-	"sap/ui/fl/write/api/FeaturesAPI",
-	"sap/ui/thirdparty/sinon-4",
-	"sap/ui/fl/write/api/PersistenceWriteAPI",
-	"sap/ui/rta/RuntimeAuthoring",
+	"sap/base/Log",
 	"sap/m/MessageBox",
+	"sap/ui/core/Control",
+	"sap/ui/fl/write/api/FeaturesAPI",
+	"sap/ui/fl/write/api/PersistenceWriteAPI",
+	"sap/ui/rta/util/adaptationStarter",
+	"sap/ui/rta/RuntimeAuthoring",
+	"sap/ui/thirdparty/sinon-4",
 	"test-resources/sap/ui/rta/qunit/RtaQunitUtils"
 ], function(
-	adaptationStarter,
-	FeaturesAPI,
-	sinon,
-	PersistenceWriteAPI,
-	RuntimeAuthoring,
+	Log,
 	MessageBox,
+	Control,
+	FeaturesAPI,
+	PersistenceWriteAPI,
+	adaptationStarter,
+	RuntimeAuthoring,
+	sinon,
 	RtaQunitUtils
 ) {
 	"use strict";
+
 	var sandbox = sinon.createSandbox();
 	var oAppComponent = RtaQunitUtils.createAndStubAppComponent(sinon);
+	var oResourceBundle = sap.ui.getCore().getLibraryResourceBundle("sap.ui.rta");
 
 	function setIsKeyUser(bIsKeyUser) {
 		sandbox.stub(FeaturesAPI, "isKeyUser").resolves(bIsKeyUser);
 	}
 
-	var oResourceBundle = sap.ui.getCore().getLibraryResourceBundle("sap.ui.rta");
-
-	QUnit.module("Base tests", {
+	QUnit.module("When adaptationStarter is called... ", {
 		beforeEach: function() {
 			this.fnRtaStartStub = sandbox.stub(RuntimeAuthoring.prototype, "start").resolves();
 			this.fnMessageBoxStub = sandbox.stub(MessageBox, "warning");
@@ -37,7 +41,7 @@ sap.ui.define([
 			sandbox.restore();
 		}
 	}, function() {
-		QUnit.test("When adaptationStarter is called with changes from other system", function(assert) {
+		QUnit.test("with changes from other system", function(assert) {
 			sandbox.stub(PersistenceWriteAPI, "getChangesWarning").resolves({
 				showWarning: true, warningType: "mixedChangesWarning"
 			});
@@ -67,7 +71,7 @@ sap.ui.define([
 			}.bind(this));
 		});
 
-		QUnit.test("When adaptationStarter is called in a P System with no changes", function(assert) {
+		QUnit.test("in a P System with no changes", function(assert) {
 			sandbox.stub(PersistenceWriteAPI, "getChangesWarning").resolves({
 				showWarning: true, warningType: "noChangesAndPSystemWarning"
 			});
@@ -93,7 +97,7 @@ sap.ui.define([
 			}.bind(this));
 		});
 
-		QUnit.test("When adaptationStarter is called with changes in the same system", function(assert) {
+		QUnit.test("with changes in the same system", function(assert) {
 			sandbox.stub(PersistenceWriteAPI, "getChangesWarning").resolves({
 				showWarning: false
 			});
@@ -106,6 +110,161 @@ sap.ui.define([
 			})
 			.then(function () {
 				assert.strictEqual(this.fnMessageBoxStub.callCount, 0, "then the warning is not shown");
+			}.bind(this));
+		});
+
+		QUnit.test("with a layer other than CUSTOMER", function(assert) {
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "USER"
+				}
+			})
+			.then(function () {
+				assert.strictEqual(FeaturesAPI.isKeyUser.callCount, 0, "the key user check was not performed");
+				assert.strictEqual(this.fnRtaStartStub.callCount, 1, "RTA was started");
+			}.bind(this));
+		});
+
+		QUnit.test("without optional parameters", function(assert) {
+			var oRuntimeAuthoring;
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "USER"
+				}
+			})
+			.then(function(oRta) {
+				oRuntimeAuthoring = oRta;
+				return oRta.stop(true, true);
+			})
+			.then(function() {
+				assert.strictEqual(oRuntimeAuthoring.isDestroyStarted(), true, "the instance is getting destroyed via the default stop handler");
+			});
+		});
+
+		QUnit.test("with optional parameters", function(assert) {
+			var oLoadPluginsStub = sandbox.stub().resolves();
+			var oOnStartStub = sandbox.stub();
+			var oOnFailedStub = sandbox.stub();
+			var oOnStopStub = sandbox.stub();
+
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "USER"
+				}
+			}, oLoadPluginsStub, oOnStartStub, oOnFailedStub, oOnStopStub)
+			.then(function(oRta) {
+				assert.strictEqual(oRta.mEventRegistry.start.pop().fFunction, oOnStartStub, "then the passed on load handler is registered as event handler");
+				assert.strictEqual(oRta.mEventRegistry.failed.pop().fFunction, oOnFailedStub, "then the passed on failed handler is registered as event handler");
+				assert.strictEqual(oRta.mEventRegistry.stop.pop().fFunction, oOnStopStub, "then the passed on stop handler is registered as event handler");
+			});
+		});
+
+		QUnit.test("with a control instead of an UIComponent", function(assert) {
+			return adaptationStarter({
+				rootControl: new Control("foo"),
+				flexSettings: {
+					layer: "USER"
+				}
+			})
+			.then(function(oRta) {
+				assert.strictEqual(oRta.getRootControl(), oAppComponent.getId(), "the corresponding app component is set as root control");
+			});
+		});
+	});
+
+	QUnit.module("Negative Tests", {
+		beforeEach: function() {
+			this.oRtaStartStub = sandbox.stub(RuntimeAuthoring.prototype, "start");
+			this.oLogStub = sandbox.stub(Log, "error");
+		},
+		afterEach: function() {
+			sandbox.restore();
+		}
+	}, function() {
+		QUnit.test("When the user is not a key user", function(assert) {
+			setIsKeyUser(false);
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "CUSTOMER"
+				}
+			})
+			.then(function () {
+				assert.ok(false, "should not go here");
+			})
+			.catch(function(oError) {
+				assert.strictEqual(this.oRtaStartStub.callCount, 0, "RuntimeAuthoring is not started");
+				assert.strictEqual(this.oLogStub.callCount, 1, "an error was logged");
+				assert.strictEqual(this.oLogStub.lastCall.args[0], "UI Adaptation could not be started", "the generic part is correct");
+				assert.strictEqual(this.oLogStub.lastCall.args[1], "Key user rights have not been granted to the current user", "the specific part is correct");
+				assert.strictEqual(oError.reason, "isKeyUser", "the reason is properly set");
+			}.bind(this));
+		});
+
+		QUnit.test("When the flexEnabled flag is set to false", function(assert) {
+			setIsKeyUser(true);
+			sandbox.stub(oAppComponent, "getManifest").returns({
+				"sap.ui5": {
+					flexEnabled: false
+				}
+			});
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "CUSTOMER"
+				}
+			})
+			.then(function () {
+				assert.ok(false, "should not go here");
+			})
+			.catch(function(oError) {
+				assert.strictEqual(this.oRtaStartStub.callCount, 0, "RuntimeAuthoring is not started");
+				assert.strictEqual(this.oLogStub.callCount, 1, "an error was logged");
+				assert.strictEqual(this.oLogStub.lastCall.args[0], "UI Adaptation could not be started", "the generic part is correct");
+				assert.strictEqual(this.oLogStub.lastCall.args[1], "This app is not enabled for key user adaptation", "the specific part is correct");
+				assert.ok(oError instanceof Error, "then promise was rejected with an error");
+				assert.strictEqual(oError.reason, "flexEnabled", "the reason is properly set");
+			}.bind(this));
+		});
+
+		QUnit.test("When the passed root control does not meet expectations", function(assert) {
+			setIsKeyUser(true);
+			return adaptationStarter({
+				rootControl: {},
+				flexSettings: {
+					layer: "CUSTOMER"
+				}
+			})
+			.then(function () {
+				assert.ok(false, "should not go here");
+			})
+			.catch(function(oError) {
+				assert.strictEqual(this.oRtaStartStub.callCount, 0, "RuntimeAuthoring is not started");
+				assert.strictEqual(this.oLogStub.callCount, 1, "an error was logged");
+				assert.strictEqual(this.oLogStub.lastCall.args[0], "UI Adaptation could not be started", "the generic part is correct");
+				assert.strictEqual(this.oLogStub.lastCall.args[1], "An invalid root control was passed", "the specific part is correct");
+				assert.ok(oError instanceof Error, "then promise was rejected with an error");
+				assert.strictEqual(oError.reason, "rootControl", "the reason is properly set");
+			}.bind(this));
+		});
+
+		QUnit.test("When RuntimeAuthoring.start rejects with a reload", function(assert) {
+			this.oRtaStartStub.returns(Promise.reject("Reload triggered"));
+			return adaptationStarter({
+				rootControl: oAppComponent,
+				flexSettings: {
+					layer: "USER"
+				}
+			})
+			.then(function() {
+				assert.ok(false, "should not go here");
+			})
+			.catch(function(oError) {
+				assert.strictEqual(this.oLogStub.callCount, 0, "no error was logged");
+				assert.strictEqual(oError, "Reload triggered", "then promise was rejected with an error string");
 			}.bind(this));
 		});
 	});
