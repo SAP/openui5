3570796f7f6fb929d6b48e33cb2422184dbc99b6
Vinay K N <vinay.k.n@sap.com>
2022-05-18 11:38:46 +0530

    [INTERNAL] ToolbarPlaceholder and openFileDialog within Upload Set have been enhanced
    JIRA: FIORITECHP1-23928
    Change-Id: I397eee0295161e2688e232b8defaa5993a4137ae

---
+++ src/sap.m/src/sap/m/upload/UploadSetToolbarPlaceholder.js
@@ -0,0 +1,44 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define([
+	"sap/m/library",
+	"sap/ui/core/Control"
+], function(library, Control) {
+	"use strict";
+
+	/**
+	 * Constructor for a new UploadSetToolbarPlaceholder.
+	 *
+	 * @param {string} [sId] ID for the new control, generated automatically if no ID is given
+	 * @param {object} [mSettings] initial settings for the new control
+	 *
+	 * @class
+	 * Used to create a customizable toolbar for the UploadSet.
+	 * A FileUploader instance is required in the toolbar and it is placed by the application.
+	 * @extends sap.ui.core.Control
+	 *
+	 * @author SAP SE
+	 * @version ${version}
+	 *
+	 * @constructor
+	 * @public
+	 * @since 1.103.0
+	 * @alias sap.m.upload.UploadSetToolbarPlaceholder
+	 */
+
+	var UploadSetToolbarPlaceholder = Control.extend("sap.m.upload.UploadSetToolbarPlaceholder", {
+		metadata: {
+			library: "sap.m",
+			properties: {}
+		},
+		renderer: {
+			apiVersion: 2,
+			render: function(oRm, oControl) {}
+		}
+	});
+
+	return UploadSetToolbarPlaceholder;
+
+});
