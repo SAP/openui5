855aba2380b4590d2b109a624b441107f4698800
Iliana Bobeva <iliana.bobeva@sap.com>
2022-06-07 14:55:18 +0300

    [INTERNAL] sap.m: Update visual tests
    JIRA: BGSOFUIPIRIN-5718
    Change-Id: Icfdf3a3f713e47fd39dd95204a11ffda310463dd

--- src/sap.m/test/sap/m/visual/PullToRefresh.spec.js
+++ src/sap.m/test/sap/m/visual/PullToRefresh.spec.js
@@ -4,44 +4,44 @@ describe("sap.m.PullToRefresh", function() {
 	"use strict";
 
 	// load standard pull to refresh page
-	it('should load test page of standard pull to refresh',function(){
-		element(by.id('standard_pull_page')).click();
-		expect(takeScreenshot()).toLookAs('initial_standard_page');
+	it("should load test page of standard pull to refresh",function(){
+		element(by.id("standard_pull_page")).click();
+		expect(takeScreenshot(element(by.id("standardP2R-cont")))).toLookAs("initial_standard_page");
 	});
 
 	// verify loading indicator is displayed after activated
-	it('should activate the standard pull to refresh', function() {
-		expect(takeScreenshot(element(by.id('standard_pull_control')))).toLookAs('pull_to_refresh_standard_before_click');
-		element(by.id('standard_pull_control')).click();
-		expect(takeScreenshot(element(by.id('standard_pull_control')))).toLookAs('pull_to_refresh_standard_after_click');
+	it("should activate the standard pull to refresh", function() {
+		expect(takeScreenshot(element(by.id("standard_pull_control")))).toLookAs("pull_to_refresh_standard_before_click");
+		element(by.id("standard_pull_control")).click();
+		expect(takeScreenshot(element(by.id("standard_pull_control")))).toLookAs("pull_to_refresh_standard_after_click");
 	});
 
 	// load hide immediately pull to refresh page
-	it('should load test page of hide immediately pull to refresh', function() {
-		element(by.id('standardP2R-navButton')).click();
-		element(by.id('hide_pull_page')).click();
-		expect(takeScreenshot()).toLookAs('initial_hide_page');
+	it("should load test page of hide immediately pull to refresh", function() {
+		element(by.id("standardP2R-navButton")).click();
+		element(by.id("hide_pull_page")).click();
+		expect(takeScreenshot(element(by.id("simplePage-cont")))).toLookAs("initial_hide_page");
 	});
 
 	// verify pull to refresh description is hidden after activated
-	it('should activate the hide immediately pull to refresh', function() {
-		expect(takeScreenshot(element(by.id('hide_pull_control')))).toLookAs('pull_to_refresh_hide_before_click');
-		element(by.id('hide_pull_control')).click();
-		expect(takeScreenshot(element(by.id('hide_pull_control')))).toLookAs('pull_to_refresh_hide_after_click');
+	it("should activate the hide immediately pull to refresh", function() {
+		expect(takeScreenshot(element(by.id("hide_pull_control")))).toLookAs("pull_to_refresh_hide_before_click");
+		element(by.id("hide_pull_control")).click();
+		expect(takeScreenshot(element(by.id("hide_pull_control")))).toLookAs("pull_to_refresh_hide_after_click");
 	});
 
 	// load busy dialog pull to refresh page
-	it('should load test page of busy dialog pull to refresh', function() {
-		element(by.id('simplePage-navButton')).click();
-		element(by.id('busy_dialog_pull_page')).click();
-		expect(takeScreenshot()).toLookAs('initial_busy_dialog_page');
+	it("should load test page of busy dialog pull to refresh", function() {
+		element(by.id("simplePage-navButton")).click();
+		element(by.id("busy_dialog_pull_page")).click();
+		expect(takeScreenshot(element(by.id("busyPage-cont")))).toLookAs("initial_busy_dialog_page");
 	});
 
 	// verify pull to refresh opens busy dialog after activated
-	it('should activate the busy dialog pull to refresh', function() {
-		expect(takeScreenshot()).toLookAs('pull_to_refresh_busy_dialog_before_click');
-		element(by.id('busy_dialog_pull_control')).click();
-		expect(takeScreenshot()).toLookAs('pull_to_refresh_busy_dialog_after_click');
+	it("should activate the busy dialog pull to refresh", function() {
+		expect(takeScreenshot(element(by.id("busy_dialog_pull_control")))).toLookAs("pull_to_refresh_busy_dialog_before_click");
+		element(by.id("busy_dialog_pull_control")).click();
+		expect(takeScreenshot(element(by.id("busy_dialog_pull_control")))).toLookAs("pull_to_refresh_busy_dialog_after_click");
 	});
 
 });
