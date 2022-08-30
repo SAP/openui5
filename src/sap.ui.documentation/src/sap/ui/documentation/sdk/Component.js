533970109a4a47f1b9cd027b7be76389ca67d7bf
Ivanov <plamen.ivanov01@sap.com>
2022-08-30 16:15:55 +0300

    [INTERNAL] Demo Kit: Remove SWA integration
    Background: Sap Web Analytics is close to its end.
    
    Solution: SWA feature are now removed from the Demo Kit.
    
    JIRA: BGSOFUIPIRIN-5860
    Change-Id: I9faef5cca7d1f3865fc3b0edc9890fa7928be996
    (cherry picked from commit a5f39d9b51a1cfd7d510d02fdc9750c30ade6fde)
    CR-Id: 002075125800003283682022
    BCP: n/a

--- src/sap.ui.documentation/src/sap/ui/documentation/sdk/Component.js
+++ src/sap.ui.documentation/src/sap/ui/documentation/sdk/Component.js
@@ -172,7 +172,6 @@ sap.ui.define([
 					bOpenUI5,
 					sVersionSuffix,
 					bIsInternal,
-					bSupportsSWA,
 					sVersionPrefixPath = ResourcesUtil.getResourcesVersion();
 
 				this.aAllowedMembers = ["public", "protected"];
@@ -185,9 +184,6 @@ sap.ui.define([
 				sVersionSuffix = oVersion.getSuffix();
 				bSnapshot = /-SNAPSHOT$/i.test(sVersionSuffix);
 				bOpenUI5 = oVersionInfo.gav && /openui5/i.test(oVersionInfo.gav);
-				bSupportsSWA = oVersionInfo.libraries.some(function (lib) {
-					return lib.name === "sap.webanalytics.core";
-				});
 
 				// We show restricted members for internal versions
 				if (ResourcesUtil.isInternal(oVersionInfo)) {
@@ -196,6 +192,7 @@ sap.ui.define([
 				}
 
 				this.getModel("versionData").setData({
+					enableAdvancedCookieSettings: false, // this flag is left for eventual future advanced cookie settings
 					versionGav: oVersionInfo.gav,
 					versionName: oVersionInfo.name,
 					version: [oVersion.getMajor(), oVersion.getMinor(), oVersion.getPatch()].join("."),
@@ -206,7 +203,6 @@ sap.ui.define([
 					isDevVersion: bSnapshot,
 					isBetaVersion: !bOpenUI5 && !bSnapshot && /-beta$/i.test(sVersionSuffix),
 					isInternal: !!bIsInternal,
-					supportsSWA: bSupportsSWA,
 					libraries: oVersionInfo.libraries,
 					allowedMembers: this.aAllowedMembers
 				});
