5e8edb7e0b062e84e562d4c09c90b00e89b66780
Alexandar Mitsev <alexandar.mitsev@sap.com>
2022-09-13 17:58:45 +0300

    [INTERNAL] Integration Cards: Align paddings for multiline list items
    If item contains title combined with just one of description, chart or attributes line
    it should have the same height.
    Now paddings are adjusted and height is the same.
    
    Snippix: 917927
    BCP: 2270123926
    Change-Id: I4d35321e1031959289c955dbf74ea2447b7aadc3

--- src/sap.ui.integration/test/sap/ui/integration/visual/ListCardVisualTests.spec.js
+++ src/sap.ui.integration/test/sap/ui/integration/visual/ListCardVisualTests.spec.js
@@ -7,7 +7,7 @@ describe("sap.ui.integration.ListCardVisualTests", function () {
 	"use strict";
 	browser.testrunner.currentSuite.meta.controlName = "sap.ui.integration.widgets.Card";
 
-	var aCardIds = ["card1", "card2", "card3", "card4", "card5", "card6"];
+	var aCardIds = ["card1", "card2", "card3", "card4", "card5", "card6", "card7"];
 
 	it("List Card", function () {
 		utils.navigateTo("List Card");
