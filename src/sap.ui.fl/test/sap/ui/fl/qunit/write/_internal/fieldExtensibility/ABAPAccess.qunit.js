800f3f11f5c68f759f95c1d248a17a36175fec4f
Stefan Blamberg <stefan.blamberg@sap.com>
2022-09-20 17:46:52 +0200

    [INTERNAL][FIX] sap.ui.fl: Fix incorrect windowFeatures in ABAPAccess
    To properly prevent reverse tabnabbing attacks, the noreferrer and
    noopener window features must be separated by commas, not spaces.
    
    BCP: 2270142488
    CR-Id: 002075125800004166392022
    Change-Id: Ie364dbae732632d285ad061192aeccca6f5c85ec
    (cherry picked from commit ae99ea67e8ecec6b00b73ca6f6b61db76b5b91ec)

--- src/sap.ui.fl/test/sap/ui/fl/qunit/write/_internal/fieldExtensibility/ABAPAccess.qunit.js
+++ src/sap.ui.fl/test/sap/ui/fl/qunit/write/_internal/fieldExtensibility/ABAPAccess.qunit.js
@@ -176,7 +176,10 @@ sap.ui.define([
 		QUnit.test("onTriggerCreateExtensionData", function(assert) {
 			var done = assert.async();
 
-			var oOpenNewWindowStub = oSandbox.stub(ABAPAccess, "openNewWindow");
+			var oOpenNewWindowStub = oSandbox.stub(window, "open")
+				.onFirstCall()
+				.returns()
+				.callThrough();
 
 			var oExpectedParams = {
 				target: {
@@ -311,7 +314,10 @@ sap.ui.define([
 			var done = assert.async();
 
 			var oControl = this.oView.byId("EntityType01.Prop2");
-			var oOpenNewWindowStub = oSandbox.stub(ABAPAccess, "openNewWindow");
+			var oOpenNewWindowStub = oSandbox.stub(window, "open")
+				.onFirstCall()
+				.returns()
+				.callThrough();
 
 			this.oServer = sinon.fakeServer.create();
 			this.oServer.autoRespond = true;
