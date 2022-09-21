4810a7765390d6ae180f0c943a7d5ec9339f6ca1
Ashwin Kumar Bolar <ashwin.kumar.bolar@sap.com>
2022-09-21 08:33:53 +0530

    [INTERNAL] Handle qunit failure
    BCP: 2270129488
    CR-Id: 002075125800003820192022
    Change-Id: I5ad1d28afe172e07a0d6b3834d616fc7bc7eeabb

--- src/sap.m/test/sap/m/qunit/GenericTile.qunit.js
+++ src/sap.m/test/sap/m/qunit/GenericTile.qunit.js
@@ -3882,6 +3882,7 @@ QUnit.test("Check for visibilty of content in header mode in 2*1 tile ", functio
 
 	QUnit.module("Article Mode Tests", {
 		beforeEach: function() {
+			this._navigateEventHandlerStub = sinon.stub(GenericTile.prototype, "_navigateEventHandler");
 			this.oGenericTile = new GenericTile("generic-tile", {
 				mode: GenericTileMode.ArticleMode,
 				subheader: "Expenses By Region",
@@ -3912,6 +3913,7 @@ QUnit.test("Check for visibilty of content in header mode in 2*1 tile ", functio
 		afterEach: function() {
 			this.oGenericTile.destroy();
 			this.oGenericTile = null;
+			this._navigateEventHandlerStub.restore();
 		},
 
 		fnWithRenderAsserts: function(assert) {
