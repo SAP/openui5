c103c650ea3c08abf95d57fa491838a48068a23b
Petar Dimov <petar.dimov@sap.com>
2022-10-03 10:11:18 +0300

    [INTERNAL] sap.ui.integration: Rework Translator Editor sample
    Removed {{parameters.xxx}} from "data/json" section
    
    JIRA: BGSOFUIRODOPI-2830
    Change-Id: I6e28f8330622c8a37d834e64e809d8d0e761c112

--- src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/designtimeTranslation/dt/Configuration.js
+++ src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/designtimeTranslation/dt/Configuration.js
@@ -68,26 +68,6 @@ sap.ui.define(["sap/ui/integration/Designtime"], function (
 						"type": "string",
 						"label": "Untranslatable Text",
 						"translatable": false
-					},
-					"maxItems": {
-						"manifestpath": "/sap.card/content/maxItems",
-						"type": "integer",
-						"allowDynamicValues": false,
-						"allowSettings": false,
-						"translatable": false,
-						"visualization": {
-							"type": "Slider",
-							"settings": {
-								"value": "{currentSettings>value}",
-								"min": 0,
-								"max": 10,
-								"width": "100%",
-								"showAdvancedTooltip": true,
-								"showHandleTooltip": false,
-								"inputsAsTooltips": true,
-								"enabled": "{currentSettings>editable}"
-							}
-						}
 					}
 				}
 			},
