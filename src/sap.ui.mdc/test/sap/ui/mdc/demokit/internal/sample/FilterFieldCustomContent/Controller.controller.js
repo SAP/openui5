16bc65a6343407cafff646488c97975efa0f9fe9
Sebastian Allmann <sebastian.allmann@sap.com>
2022-10-11 08:27:50 +0200

    [INTERNAL] Field: improve sample for custom content
    As sample for custom controls with own update and event logic.
    
    Change-Id: I4d7df528d279487cf47e89f0e17296c7a20638be

--- src/sap.ui.mdc/test/sap/ui/mdc/demokit/internal/sample/FilterFieldCustomContent/Controller.controller.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/demokit/internal/sample/FilterFieldCustomContent/Controller.controller.js
@@ -1,17 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/Core",
-	"sap/ui/model/json/JSONModel",
 	"sap/ui/mdc/condition/ConditionModel",
 	"sap/ui/mdc/condition/Condition",
-	"sap/ui/mdc/enum/EditMode"
+	"sap/ui/mdc/enum/EditMode",
+	'sap/ui/mdc/enum/ConditionValidated',
+	"sap/m/MessageToast"
 ], function(
 	Controller,
 	oCore,
-	JSONModel,
 	ConditionModel,
 	Condition,
-	EditMode
+	EditMode,
+	ConditionValidated,
+	MessageToast
 	) {
 	"use strict";
 
@@ -22,20 +24,34 @@ sap.ui.define([
 			oCore.getMessageManager().registerObject(oView, true);
 
 			var oCM = new ConditionModel();
-			oCM.addCondition("title", Condition.createCondition("EQ", ["4711"]));
-			oCM.addCondition("metricsWords", Condition.createCondition("EQ", [4711]));
-			oCM.addCondition("descr", Condition.createCondition("GT", ["A"]));
-			oCM.addCondition("descr", Condition.createCondition("BT", ["B", "C"]));
+			oCM.addCondition("title", Condition.createCondition("EQ", ["4711"], undefined, undefined, ConditionValidated.NotValidated));
+			oCM.addCondition("metricsWords", Condition.createCondition("EQ", [4711], undefined, undefined, ConditionValidated.NotValidated));
+			oCM.addCondition("descr", Condition.createCondition("GT", ["A"], undefined, undefined, ConditionValidated.NotValidated));
+			oCM.addCondition("descr", Condition.createCondition("BT", ["B", "C"], undefined, undefined, ConditionValidated.NotValidated));
+			oCM.addCondition("number", Condition.createCondition("EQ", [1], undefined, undefined, ConditionValidated.NotValidated));
+			oCM.addCondition("number", Condition.createCondition("EQ", [3], undefined, undefined, ConditionValidated.NotValidated));
 
 			//set the model on Form just to have it somehow local
 			var oForm = this.byId("Form1");
 			oForm.setModel(oCM, "cm");
+		},
 
-			// var oViewModel = new JSONModel({
-			// 	editMode: EditMode.Editable
-			// });
-			// oView.setModel(oViewModel, "view");
+		handleChange: function(oEvent) {
+			var oFilterField = oEvent.getSource();
+			var oPromise = oEvent.getParameter("promise");
 
+			if (oPromise) {
+				oPromise.then(function(aConditions) {
+					var sText = "[";
+					for (var i = 0; i < aConditions.length; i++) {
+						sText = sText + JSON.stringify(aConditions[i]) + " ;";
+					}
+					sText = sText + "]";
+					MessageToast.show("Change " + oFilterField.getId() + "; conditions: " + sText);
+				}).catch(function(oError) {
+					MessageToast.show(oError.message);
+				});
+			}
 		}
 
 	});
