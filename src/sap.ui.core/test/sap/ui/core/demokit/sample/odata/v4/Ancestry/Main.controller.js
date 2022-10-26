6abc322f47ddbf61180bb49cf24be8436e630b7a
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2022-10-26 16:51:54 +0200

    [INTERNAL] sap.ui.core.sample.odata.v4.RealRecursiveHierarchy
    PS1: new test app for real service, incl. fixes to our code
    PS2: save copy of metadata and data, add local annotation file, add sort
      and filter, some cleanup
    PS3: minor cleanup
    PS4-5: rebased
    PS6: Ancestry
    PS7: minor cleanup
    PS8: rebased onto master
    
    Change-Id: I1c88eeb0bc892dcc232e66d05b70486a8cf6be4f
    JIRA: CPOUI5ODATAV4-1809

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/Ancestry/Main.controller.js
@@ -0,0 +1,69 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define([
+	"sap/ui/core/sample/common/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/Sorter",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, Filter, FilterOperator, Sorter, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui.core.sample.odata.v4.Ancestry.Main", {
+		onFilter : function () {
+			var sFilter = this.getView().getModel("ui").getProperty("/sFilter");
+
+			this.byId("table").getBinding("rows")
+				.filter(new Filter("hobby", FilterOperator.Contains, sFilter));
+		},
+
+		onInit : function () {
+			var oTable = this.byId("table"),
+				oRowsBinding = oTable.getBinding("rows");
+
+			this.getView().setModel(new JSONModel({
+				sFilter : "",
+				sIcon : ""
+			}), "ui");
+			this.bDescending = undefined;
+
+			// enable V4 tree table flag
+			oTable._oProxy._bEnableV4 = true;
+
+			this._oAggregation = {
+				expandTo : 2,
+				hierarchyQualifier : "SADL_V_RS_ANCESTRY_HIER"
+			};
+			oRowsBinding.setAggregation(this._oAggregation);
+			oRowsBinding.resume();
+		},
+
+		onSort : function () {
+			var sNewIcon,
+				oSorter;
+
+			// choose next sort order: no sort -> ascending -> descending -> no sort
+			switch (this.bDescending) {
+				case false:
+					this.bDescending = true;
+					sNewIcon = "sap-icon://sort-descending";
+					oSorter = new Sorter("date_of_birth", /*bDescending*/true);
+					break;
+
+				case true:
+					this.bDescending = undefined;
+					sNewIcon = "";
+					break;
+
+				default: // undefined
+					this.bDescending = false;
+					sNewIcon = "sap-icon://sort-ascending";
+					oSorter = new Sorter("date_of_birth", /*bDescending*/false);
+			}
+
+			this.getView().getModel("ui").setProperty("/sIcon", sNewIcon);
+			this.byId("table").getBinding("rows").sort(oSorter);
+		}
+	});
+});
