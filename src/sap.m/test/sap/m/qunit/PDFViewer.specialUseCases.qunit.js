e9ece73edce355a76a78818ba4327154e8954978
I527383 <bhumika.bhandari@sap.com>
2022-10-28 12:01:09 +0530

    [INTERNAL] Qunit fix of PDFViewer
    Change-Id: I3f6f3639037a2f5995dee1c44603bb7e5515e22c
    CR-Id: 002075125900003370662022
    JIRA: FIORITECHP1-24688
    BCP: 2270167909
    (cherry picked from commit 09a63ec292bbd85607046c7d060f8f6019e54f54)

--- src/sap.m/test/sap/m/qunit/PDFViewer.specialUseCases.qunit.js
+++ src/sap.m/test/sap/m/qunit/PDFViewer.specialUseCases.qunit.js
@@ -214,7 +214,9 @@ sap.ui.define( [
 
 	QUnit.test("Desktop View Emulated from Mobile", function (assert) {
 		this.stub(Device, "system").value({desktop: true});
-		this.stub(window.navigator, "pdfViewerEnabled").value(false);
+		if (window && window.navigator && window.navigator.pdfViewerEnabled) {
+            this.stub(navigator, "pdfViewerEnabled").value(false);
+        }
 
 		oPDFViewer = TestUtils.createPdfViewer({
 			source: "test-resources/sap/m/qunit/pdfviewer/sample-file.pdf"
