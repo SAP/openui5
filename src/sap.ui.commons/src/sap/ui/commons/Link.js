a860cfec1d75961db9f2aefab63fee7837d37487
Minkova <yana.minkova@sap.com>
2022-11-15 13:26:35 +0200

    [INTERNAL] sap.ui.commons.Link: Improve JSDoc
    The JSDoc annotations are improved for better TypeScript support.
    
    JIRA: BGSOFUIPIRIN-5939
    Change-Id: I661d213b7d94ae896f93c67d59fae11a434c7c12

--- src/sap.ui.commons/src/sap/ui/commons/Link.js
+++ src/sap.ui.commons/src/sap/ui/commons/Link.js
@@ -166,7 +166,8 @@ sap.ui.define([
 
 	/**
 	 * @see sap.ui.core.Control#getAccessibilityInfo
-	 * @returns {object} The accessibility info
+	 * @returns {{role: string, type: string, description: string, focusable: boolean, enabled: boolean}}
+	 * The object contains the accessibility information of <code>sap.ui.commons.Link</code>
 	 * @protected
 	 */
 	Link.prototype.getAccessibilityInfo = function() {
