9ad00e285b784428e5966787fddf2cf3a2d662eb
Johannes Gluch <johannes.gluch@sap.com>
2022-11-18 10:48:06 +0100

    [FIX] theming/ThemeManager: Fix preloadLibCss
    - Only reset ThemeManager in case library CSS was
      not preloaded
    - In case we update a (library) link tag also add
      the library to mAllLoadedLibraries to ensure
      themeChangedEvent waits for CSS of such an
      library (e.g. in preload use case)
    
    BCP: 2270189263
    BCP: 002075129500008388142022
    Change-Id: I6e9453604429405257a1a83c83ff34f1819fc190

--- src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/BootstrapCustomBootTaskPreloadCss.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/BootstrapCustomBootTaskPreloadCss.qunit.js
@@ -1,5 +1,9 @@
 /*global QUnit */
-sap.ui.define(["sap/ui/core/Configuration"], function(Configuration) {
+sap.ui.define([
+	"sap/ui/core/Core",
+	"sap/ui/core/Configuration",
+	"sap/ui/core/theming/Parameters"
+], function(Core, Configuration, Parameters) {
 	"use strict";
 
 	/**
@@ -22,6 +26,21 @@ sap.ui.define(["sap/ui/core/Configuration"], function(Configuration) {
 		assert.equal(sap.ui.require.toUrl("sap/ui/testlib/themes/" + sTheme), "foo/bar/sap/ui/testlib/themes/" + sTheme,
 			"Modulepath of 'sap/ui/testlib/themes/" + sTheme + "' should be set to themeroot.");
 
+	});
+	/**
+	 * Test that even though "preloadLibCss" prevents including the library.css, link tags added from other sources
+	 * with the correct id prefix "sap-ui-theme-" are tracked correctly. ThemeChanged event should wait for such
+	 * CSS files in case they were modified by ThemeManager as part of e.g. a themeChanged call.
+	 */
+	QUnit.test("ThemeChanged event should wait for CSS files loaded after changeTheme", function(assert) {
+		var done = assert.async();
+		Core.attachThemeChanged(function () {
+			assert.equal(Parameters.get({ name: "sapUiThemeParamForFantasyLib" }), "#fafafa",
+				"Preloaded library CSS was correctly recognized and theme changed event was fired.");
+				done();
+		});
+
+		Core.applyTheme("sap_hcb");
 	});
 
 });
