00b511b9b2f199a87d932555b50b5e74e5fbe8cf
Todor Stoyanov <t.stoyanov@sap.com>
2022-12-01 15:04:03 +0200

    [FIX] sap.m.SinglePlanningCalendar: added ability to create appointments in RTL mode with D&D
    BCP: 002075129400005237632022
    CR-Id: 002075125800005196542022
    Change-Id: I63fe4466ae02d5a60dcbeeecdad8c8443fbdc976

--- src/sap.m/src/sap/m/SinglePlanningCalendarGrid.js
+++ src/sap.m/src/sap/m/SinglePlanningCalendarGrid.js
@@ -739,6 +739,7 @@ sap.ui.define([
 					}
 
 					var oGrid = oEvent.getParameter("target"),
+						bIsRtl = sap.ui.getCore().getConfiguration().getRTL(),
 						aIntervalPlaceholders = oGrid.getAggregation("_intervalPlaceholders"),
 						oFirstIntervalRectangle = aIntervalPlaceholders[0].getDomRef().getBoundingClientRect(),
 						iIntervalHeight = oFirstIntervalRectangle.height,
@@ -751,12 +752,10 @@ sap.ui.define([
 					if (this._iColumns === 1) {
 						iIntervalIndex = iIndexInColumn;
 					} else {
-						var iHeaderSize = 64,
-							iBordersWidth = 2,
-							iIntervalWidth = Math.floor(aIntervalPlaceholders[0].getDomRef().getBoundingClientRect().width) - iBordersWidth,
-							iColumnsFromStart = Math.floor(Math.floor((oBrowserEvent.offsetX - iHeaderSize)) / iIntervalWidth),
+						var iHeaderWidthSize = bIsRtl ? 0 : this.getDomRef().querySelector(".sapMSinglePCRowHeaders").getClientRects()[0].width,
+							iIntervalWidth = oGrid._aGridCells[0].getClientRects()[0].width,
+							iColumnsFromStart = Math.floor(Math.floor((oBrowserEvent.offsetX - iHeaderWidthSize)) / iIntervalWidth),
 							iIntervalsInColumn = aIntervalPlaceholders.length / this._iColumns;
-
 						iIntervalIndex = iIndexInColumn + ((iColumnsFromStart) * iIntervalsInColumn);
 					}
 
@@ -2035,27 +2034,40 @@ sap.ui.define([
 			}
 		};
 
-		SinglePlanningCalendarGrid.prototype._createAppointmentsDndPlaceholders = function (oStartDate, iColumns) {
+		SinglePlanningCalendarGrid.prototype._createAppointmentsMatrix = function (oStartDate, index) {
+			var oColumnCalDate = new CalendarDate(oStartDate.getFullYear(), oStartDate.getMonth(), oStartDate.getDate() + index);
 			var iStartHour = this._getVisibleStartHour(),
-				iEndHour = this._getVisibleEndHour();
+			iEndHour = this._getVisibleEndHour();
+
+			if (!this._dndPlaceholdersMap[oColumnCalDate]) {
+				this._dndPlaceholdersMap[oColumnCalDate] = [];
+			}
+
+
+			for (var j = iStartHour; j <= iEndHour; j++) {
+				var aDndForTheDay = this._dndPlaceholdersMap[oColumnCalDate],
+					iYear = oColumnCalDate.getYear(),
+					iMonth = oColumnCalDate.getMonth(),
+					iDate = oColumnCalDate.getDate();
+				aDndForTheDay.push(this._createAppointmentsDndPlaceHolder(new UniversalDate(iYear, iMonth, iDate, j)));
+				aDndForTheDay.push(this._createAppointmentsDndPlaceHolder(new UniversalDate(iYear, iMonth, iDate, j, 30)));
+			}
+		};
+
+		SinglePlanningCalendarGrid.prototype._createAppointmentsDndPlaceholders = function (oStartDate, iColumns) {
+			var bIsRtl = sap.ui.getCore().getConfiguration().getRTL(),
+			i;
 
 			this._dndPlaceholdersMap = {};
 			this.destroyAggregation("_intervalPlaceholders");
 
-			for (var i = 0; i < iColumns; i++) {
-				var oColumnCalDate = new CalendarDate(oStartDate.getFullYear(), oStartDate.getMonth(), oStartDate.getDate() + i);
-
-				if (!this._dndPlaceholdersMap[oColumnCalDate]) {
-					this._dndPlaceholdersMap[oColumnCalDate] = [];
+			if (bIsRtl) {
+				for (i = iColumns - 1; i >= 0; i--) {
+					this._createAppointmentsMatrix(oStartDate, i);
 				}
-
-				for (var j = iStartHour; j <= iEndHour; j++) {
-					var aDndForTheDay = this._dndPlaceholdersMap[oColumnCalDate],
-						iYear = oColumnCalDate.getYear(),
-						iMonth = oColumnCalDate.getMonth(),
-						iDate = oColumnCalDate.getDate();
-					aDndForTheDay.push(this._createAppointmentsDndPlaceHolder(new UniversalDate(iYear, iMonth, iDate, j)));
-					aDndForTheDay.push(this._createAppointmentsDndPlaceHolder(new UniversalDate(iYear, iMonth, iDate, j, 30)));
+			} else {
+				for (i = 0; i <  iColumns; i++) {
+					this._createAppointmentsMatrix(oStartDate, i);
 				}
 			}
 		};
