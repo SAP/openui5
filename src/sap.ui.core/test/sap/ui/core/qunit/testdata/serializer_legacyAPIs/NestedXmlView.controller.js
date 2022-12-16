8dacf9c2e609f996bff127d4a892fc32581245ca
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2022-12-16 16:11:09 +0100

    [INTERNAL] Test cleanup: split ViewSerializer.qunit into modern and legacy
    JIRA: CPOUI5FRAMEWORK-422
    Change-Id: I9d379bec96ed7a6bacf439793aa7c1b4feb62011

---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/serializer_legacyAPIs/NestedXmlView.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("serializer.view.NestedXmlView", {
+	});
+});
---
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/serializer_legacyAPIs/TestXml.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("serializer.view.TestXml", {
+
+		onInit : function () {
+			var model = new JSONModel({
+				name : "Skoda Fabia",
+				price : "14000"
+			});
+			this.getView().setModel(model);
+		},
+
+		handleButtonPress : function () {
+			this.getView().byId("buttonPressLabel").setText("Button Pressed!");
+		},
+
+		formatPrice : function (sValue) {
+			return sValue + " EUR";
+		}
+	});
+
+});
