b07a4b8d2fa902bcf3481ceb1af419a704b135fb
Sven Bernhard <sven.bernhard@sap.com>
2022-12-22 14:06:53 +0100

    [INTERNAL] demokit.sample.odata.types.v4: Samples for OData V4 Edm.Date, Edm.DateTimeOffset and Edm.TimeOfDay
    Within demokit:
    http://localhost:8080/documentation.html?#/entity/sap.ui.model.odata.type/sample/sap.ui.core.sample.odata.types.v4.Date
    http://localhost:8080/documentation.html?#/entity/sap.ui.model.odata.type/sample/sap.ui.core.sample.odata.types.v4.DateTimeOffset
    http://localhost:8080/documentation.html?#/entity/sap.ui.model.odata.type/sample/sap.ui.core.sample.odata.types.v4.TimeOfDay
    Standalone without demokit:
    http://localhost:8080/resources/sap/ui/documentation/sdk/index.html?sap-timezone=Pacific/Fiji&sap-ui-xx-sample-id=sap.ui.core.sample.odata.types.v4.Date&sap-ui-xx-sample-lib=sap.ui.core&sap-ui-xx-sample-origin=.&sap-ui-xx-dk-origin=http%3A%2F%2Flocalhost%3A8080
    http://localhost:8080/resources/sap/ui/documentation/sdk/index.html?sap-timezone=Pacific/Fiji&sap-ui-xx-sample-id=sap.ui.core.sample.odata.types.v4.DateTimeOffset&sap-ui-xx-sample-lib=sap.ui.core&sap-ui-xx-sample-origin=.&sap-ui-xx-dk-origin=http%3A%2F%2Flocalhost%3A8080
    http://localhost:8080/resources/sap/ui/documentation/sdk/index.html?sap-timezone=Pacific/Fiji&sap-ui-xx-sample-id=sap.ui.core.sample.odata.types.v4.TimeOfDay&sap-ui-xx-sample-lib=sap.ui.core&sap-ui-xx-sample-origin=.&sap-ui-xx-dk-origin=http%3A%2F%2Flocalhost%3A8080
    
    PS1: Added applications
    PS2-4: Review comments
    PS5: Rebase
    
    Change-Id: Ibaee932fb96fd29366a21489327508df0005c11e
    JIRA: CPOUI5MODELS-1095

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/types/v4/DateTimeOffset/Component.js
@@ -0,0 +1,13 @@
+/*!
+ * ${copyright}
+ */
+sap.ui.define(["sap/ui/core/UIComponent"],
+	function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.core.sample.odata.types.v4.DateTimeOffset.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
