a69f2db5ddbd4793bd9bdc0330b3841ed3b5d599
I523040 <colin.otchere@sap.com>
2023-01-04 16:08:47 +0100

    [INTERNAL] sap.ui.rta - add qunit test for context-based adaptation
    JIRA: UITECHGRAVITY-2476
    Change-Id: Icd683dea026ad76bb28efc609a31e3d735a493f2

---
+++ src/sap.ui.rta/test/sap/ui/rta/qunit/toolbar/contextBased/ManageAdaptations.qunit.js
@@ -0,0 +1,416 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"../../RtaQunitUtils",
+	"sap/ui/core/Core",
+	"sap/ui/core/Control",
+	"sap/ui/core/Fragment",
+	"sap/ui/fl/Layer",
+	"sap/ui/fl/write/api/ContextBasedAdaptationsAPI",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/rta/toolbar/Adaptation",
+	"sap/ui/rta/toolbar/contextBased/ManageAdaptations",
+	"sap/ui/thirdparty/sinon-4"
+], function(
+	RtaQunitUtils,
+	Core,
+	Control,
+	Fragment,
+	Layer,
+	ContextBasedAdaptationsAPI,
+	JSONModel,
+	Adaptation,
+	ManageAdaptations,
+	sinon
+) {
+	"use strict";
+
+	var sandbox = sinon.createSandbox();
+
+	function getControl(oToolbar, sControlID) {
+		return oToolbar.getControl("manageAdaptationDialog--" + sControlID);
+	}
+
+	function getAdaptationTitle(oTableListItem) {
+		return oTableListItem.getCells()[1].getContent()[0].getItems()[0].getItems()[0].getText();
+	}
+
+	function initializeToolbar() {
+		var oToolbarControlsModel = RtaQunitUtils.createToolbarControlsModel();
+		var oToolbar = new Adaptation({
+			textResources: Core.getLibraryResourceBundle("sap.ui.rta"),
+			rtaInformation: {
+				flexSettings: {
+					layer: Layer.CUSTOMER
+				},
+				rootControl: new Control()
+			}
+		});
+		oToolbar.setModel(oToolbarControlsModel, "controls");
+
+		oToolbar.animation = false;
+		oToolbar.placeAt("qunit-fixture");
+		Core.applyChanges();
+		return oToolbar;
+	}
+
+	QUnit.module("Given a Toolbar with enabled context-based adaptations feature", {
+		beforeEach: function() {
+			this.oToolbar = initializeToolbar();
+			this.oManageAdaptations = new ManageAdaptations({ toolbar: this.oToolbar });
+			this.oEvent = {
+				getSource: function() {
+					return this.oToolbar.getControl("manageAdaptations");
+				}.bind(this)
+			};
+			return this.oToolbar.onFragmentLoaded().then(function() {
+				return this.oToolbar.show();
+			}.bind(this));
+		},
+		afterEach: function() {
+			this.oToolbar.destroy();
+			sandbox.restore();
+		}
+	}, function() {
+		QUnit.module("the manage adaptations dialog is created with empty ", {
+			beforeEach: function() {
+				sandbox.stub(ContextBasedAdaptationsAPI, "load").resolves(
+					new JSONModel({
+						adaptations: []
+					})
+				);
+				this.oFragmentLoadSpy = sandbox.spy(Fragment, "load");
+				return this.oManageAdaptations.openManageAdaptationDialog()
+					.then(function (oDialog) {
+						this.oDialog = oDialog;
+						return this.oToolbar._pFragmentLoaded;
+					}.bind(this));
+			}
+		}, function() {
+			QUnit.test("and context-based adaptations dialog is visible", function(assert) {
+				assert.strictEqual(this.oFragmentLoadSpy.callCount, 1, "the fragment was loaded");
+				assert.ok(this.oDialog.isOpen(), "the dialog is opened");
+			});
+
+			QUnit.test("and opend a second time", function(assert) {
+				// simulate user selection
+				this.oDialog.close();
+				return this.oManageAdaptations.openManageAdaptationDialog().then(function(oDialog) {
+					assert.strictEqual(this.oFragmentLoadSpy.callCount, 1, "the fragment was loaded not again");
+					assert.ok(oDialog.isOpen(), "the dialog is opened");
+					assert.deepEqual(oDialog.getModel("contextBased").getProperty("/adaptations"), [], "the data was reset");
+				}.bind(this));
+			});
+		});
+
+		QUnit.module("the manage adaptations dialog is opened containing two adaptations", {
+			beforeEach: function() {
+				this.sManageAdaptationsDialog = "manageAdaptationDialog";
+				this.oContextBasedAdaptatations = new JSONModel({
+					adaptations: [{
+						id: "id-1591275572834-1",
+						contexts: {
+							role: ["SALES"]
+						},
+						title: "German Admin",
+						description: "ACH Admin for Germany",
+						createdBy: "Test User 1",
+						createdAt: "May 25, 2022",
+						changedBy: "Test User 1",
+						changedAt: "May 27, 2022"
+					},
+					{
+						id: "id-1591275572835-1",
+						contexts: {
+							role: ["MARKETING_MANAGER"]
+						},
+						title: "DLM Copilot",
+						description: "DLM copilot contexts for Europe",
+						createdBy: "Test User 2",
+						createdAt: "May 17, 2022",
+						changedBy: "Test User 2",
+						changedAt: "SEPTEMBER 07, 2022"
+					}]
+				});
+				sandbox.stub(ContextBasedAdaptationsAPI, "load").resolves(this.oContextBasedAdaptatations);
+				this.oFragmentLoadSpy = sandbox.spy(Fragment, "load");
+				return this.oManageAdaptations.openManageAdaptationDialog()
+					.then(function (oDialog) {
+						this.oDialog = oDialog;
+						return this.oToolbar._pFragmentLoaded;
+					}.bind(this));
+			}
+		}, function() {
+			QUnit.test("and context-based adaptations are visible and correctly formatted", function(assert) {
+				assert.strictEqual(this.oFragmentLoadSpy.callCount, 1, "the fragment was loaded");
+				assert.ok(this.oDialog.isOpen(), "the dialog is opened");
+				assert.deepEqual(this.oDialog.getModel("contextBased").getProperty("/adaptations"), this.oContextBasedAdaptatations.getProperty("/adaptations"), "correct context-based adaptations are shown");
+			});
+
+			QUnit.test("and the priority of the context-based adaptations is changed using the up and down button", function(assert) {
+				var oMoveUpButton = getControl(this.oToolbar, "moveUpButton");
+				var oMoveDownButton = getControl(this.oToolbar, "moveDownButton");
+				var oAdaptationsTable = getControl(this.oToolbar, "manageAdaptationsTable");
+				var oSaveAsButtonEnabled = getControl(this.oToolbar, "manageAdaptations-saveButton");
+				var oFirstTableItem = oAdaptationsTable.getItems()[0];
+				var sFirstAdaptationText = oFirstTableItem.getCells()[1].mAggregations.content[0].mAggregations.items[0]
+					.mAggregations.items[0].getProperty("text");
+				oAdaptationsTable.getItems()[0].focus();
+				oAdaptationsTable.setSelectedItem(oAdaptationsTable.getItems()[0], true, true);
+
+				assert.notOk(oSaveAsButtonEnabled.getEnabled(), "Save Button is disabled");
+				assert.ok(oMoveUpButton.getEnabled(), "MoveUpButton is enabled");
+				assert.ok(oMoveDownButton.getEnabled(), "oMoveDownButton is enabled");
+
+				oMoveDownButton.firePress();
+				var sNewFirstAdaptationText = oFirstTableItem.getCells()[1].mAggregations.content[0].mAggregations.items[0]
+					.mAggregations.items[0].getProperty("text");
+				assert.notEqual(sFirstAdaptationText, sNewFirstAdaptationText, "priority of adaptations has changed");
+				assert.ok(oSaveAsButtonEnabled.getEnabled(), "Save Button is enabled");
+
+				oMoveUpButton.firePress();
+				sNewFirstAdaptationText = oFirstTableItem.getCells()[1].mAggregations.content[0].mAggregations.items[0]
+					.mAggregations.items[0].getProperty("text");
+				assert.strictEqual(sFirstAdaptationText, sNewFirstAdaptationText, "origianl priority is visible");
+			});
+
+			QUnit.test("and the priority of the context-based adaptations is changed using drag and drop", function(assert) {
+				return this.oManageAdaptations.openManageAdaptationDialog().then(function() {
+					var oAdaptationsTable = getControl(this.oToolbar, "manageAdaptationsTable");
+					var oSaveAsButtonEnabled = getControl(this.oToolbar, "manageAdaptations-saveButton");
+					assert.notOk(oSaveAsButtonEnabled.getEnabled(), "Save Button is disabled");
+
+					var oFirstTableItem = oAdaptationsTable.getItems()[0];
+					var sFirstAdaptationText = getAdaptationTitle(oFirstTableItem);
+					oAdaptationsTable.getItems()[0].focus();
+					oAdaptationsTable.setSelectedItem(oAdaptationsTable.getItems()[0], true, true);
+					var oFakeEvent = {
+						getParameter: function(sProperty) {
+							if (sProperty === "droppedControl") {
+								return oAdaptationsTable.getItems()[1];
+							} else if (sProperty === "draggedControl") {
+								return oFirstTableItem;
+							} else if (sProperty === "dropPosition") {
+								return "After";
+							}
+							return "";
+						},
+						getBindingContext: function() {
+							return oFirstTableItem.getBindingContext("contextBased");
+						}
+					};
+					oAdaptationsTable.getDragDropConfig()[0].mEventRegistry.drop[0].fFunction(oFakeEvent);
+					var sNewFirstAdaptationText = getAdaptationTitle(oFirstTableItem);
+					assert.notEqual(sFirstAdaptationText, sNewFirstAdaptationText, "priority of adaptations has changed");
+				}.bind(this));
+			});
+		});
+
+		QUnit.module("the manage adaptations dialog is opened containing four adaptations", {
+			beforeEach: function() {
+				this.sManageAdaptationsDialog = "manageAdaptationDialog";
+				this.oContextBasedAdaptatations = new JSONModel({
+					adaptations: [{
+						id: "id-1591275572834-1",
+						contexts: {
+							role: ["SALES"]
+						},
+						title: "German Admin",
+						description: "ACH Admin for Germany",
+						createdBy: "Test User 1",
+						createdAt: "May 25, 2022",
+						changedBy: "Test User 1",
+						changedAt: "May 27, 2022"
+					},
+					{
+						id: "id-1591275572835-1",
+						contexts: {
+							role: ["MARKETING_MANAGER"]
+						},
+						title: "DLM Copilot",
+						description: "DLM copilot contexts for Europe",
+						createdBy: "Test User 2",
+						createdAt: "May 17, 2022",
+						changedBy: "Test User 2",
+						changedAt: "SEPTEMBER 07, 2022"
+					},
+					{
+						id: "id-1591275572839-1",
+						contexts: {
+							role: ["INVENTORY_MANAGER"]
+						},
+						title: "England Admin",
+						description: "ACH Admin for England",
+						createdBy: "Test User 1",
+						createdAt: "May 17, 2022",
+						changedBy: "Test User 1",
+						changedAt: "May 28, 2022"
+					},
+					{
+						id: "id-1591275572899-1",
+						contexts: {
+							role: ["INVENTORY_MANAGER"]
+						},
+						title: "Spain Admin",
+						description: "ACH Admin for Spain",
+						createdBy: "Test User 1",
+						createdAt: "May 17, 2022",
+						changedBy: "Test User 1",
+						changedAt: "May 28, 2022"
+					}]
+				});
+				sandbox.stub(ContextBasedAdaptationsAPI, "load").resolves(this.oContextBasedAdaptatations);
+				this.oFragmentLoadSpy = sandbox.spy(Fragment, "load");
+				return this.oManageAdaptations.openManageAdaptationDialog()
+					.then(function (oDialog) {
+						this.oDialog = oDialog;
+						return this.oToolbar._pFragmentLoaded;
+					}.bind(this));
+			}
+		}, function() {
+			[{
+				testName: "and the search field of the context-based adaptations is filtering for 'somethingWhichDoesNotExits'",
+				input: "somethingWhichDoesNotExits",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 0
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: false
+					}
+				}
+			},
+			{
+				testName: "and the search field of the context-based adaptations is filtering for default context table 'DeFaUlT ApP'",
+				input: "DeFaUlT ApP",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 0
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: true
+					}
+				}
+			},
+			{
+				testName: "and the search field of the context-based adaptations is filtering for title 'AdMiN'",
+				input: "AdMiN",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 3
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: false
+					}
+				}
+			},
+			{
+				testName: "and the search field of the context-based adaptations is filtering for context id 'mAnAgER'",
+				input: "mAnAgER",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 3
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: false
+					}
+				}
+			},
+			{
+				testName: "and the search field of the context-based adaptations is filtering for createdBy 'TeST UsEr 2'",
+				input: "TeST UsEr 2",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 1
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: false
+					}
+				}
+			},
+			{
+				testName: "and the search field of the context-based adaptations is filtering for changedBy 'TeST USEr 2'",
+				input: "TeST USEr 2",
+				expectation: {
+					amountOfAdaptations: {
+						beforeSearch: 4,
+						afterSearch: 1
+					},
+					visibilityOfDefaultContextTable: {
+						beforeSearch: true,
+						afterSearch: false
+					}
+				}
+			}].forEach(function(mSetup) {
+				QUnit.test(mSetup.testName, function(assert) {
+					var oSearchField = getControl(this.oToolbar, "searchField");
+					var oAdaptationsTable = getControl(this.oToolbar, "manageAdaptationsTable");
+					var oMoveUpButton = getControl(this.oToolbar, "moveUpButton");
+					var oMoveDownButton = getControl(this.oToolbar, "moveDownButton");
+					var oDefaultContextTable = getControl(this.oToolbar, "defaultContext");
+					assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+					assert.equal(oAdaptationsTable.getItems().length, mSetup.expectation.amountOfAdaptations.beforeSearch, "correct amount of adaptations");
+					assert.ok(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is enabled");
+					assert.notOk(oMoveUpButton.getEnabled(), "MoveUpButton is disabled");
+					assert.notOk(oMoveDownButton.getEnabled(), "MoveDownButton is disabled");
+					assert.strictEqual(oDefaultContextTable.getVisible(), mSetup.expectation.visibilityOfDefaultContextTable.beforeSearch, "default context table is visible");
+					oSearchField.setValue(mSetup.input);
+					oSearchField.fireLiveChange();
+					assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+					assert.strictEqual(oAdaptationsTable.getItems().length, mSetup.expectation.amountOfAdaptations.afterSearch, "correct amount of adaptations");
+					assert.notOk(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is disabled");
+					assert.notOk(oMoveUpButton.getEnabled(), "MoveUpButton is disabled");
+					assert.notOk(oMoveDownButton.getEnabled(), "MoveDownButton is disabled");
+					assert.strictEqual(oDefaultContextTable.getVisible(), mSetup.expectation.visibilityOfDefaultContextTable.afterSearch, "default context table is visible");
+				});
+			});
+
+			QUnit.test("and the search field of the context-based adaptations is filtering for 'something' while an item is selected", function(assert) {
+				var oSearchField = getControl(this.oToolbar, "searchField");
+				var oAdaptationsTable = getControl(this.oToolbar, "manageAdaptationsTable");
+				var oMoveUpButton = getControl(this.oToolbar, "moveUpButton");
+				var oMoveDownButton = getControl(this.oToolbar, "moveDownButton");
+				var oDefaultContextTable = getControl(this.oToolbar, "defaultContext");
+				assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+				assert.strictEqual(oAdaptationsTable.getItems().length, 4, "correct amount of adaptations");
+				assert.ok(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is enabled");
+				assert.notEqual(oMoveUpButton.getEnabled(), "MoveUpButton is disabled");
+				assert.notEqual(oMoveDownButton.getEnabled(), "MoveDownButton is disabled");
+				assert.ok(oDefaultContextTable.getVisible(), "default context table is visible");
+				oAdaptationsTable.setSelectedItem(oAdaptationsTable.getItems()[0], true, true);
+				assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+				assert.strictEqual(oAdaptationsTable.getItems().length, 4, "correct amount of adaptations");
+				assert.ok(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is enabled");
+				assert.ok(oMoveUpButton.getEnabled(), "MoveUpButton is enabled");
+				assert.ok(oMoveDownButton.getEnabled(), "MoveDownButton is enabled");
+				assert.ok(oDefaultContextTable.getVisible(), "default context table is visible");
+				oSearchField.setValue("something");
+				oSearchField.fireLiveChange();
+				assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+				assert.strictEqual(oAdaptationsTable.getItems().length, 0, "correct amount of adaptations");
+				assert.notOk(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is disabled");
+				assert.notOk(oMoveUpButton.getEnabled(), "MoveUpButton is disabled");
+				assert.notOk(oMoveDownButton.getEnabled(), "MoveDownButton is disabled");
+				assert.notOk(oDefaultContextTable.getVisible(), "default context table is visible");
+				oSearchField.setValue("");
+				oSearchField.fireLiveChange();
+				assert.ok(oSearchField.getEnabled(), "search field is present and enabled");
+				assert.ok(oAdaptationsTable.getItems().length, 4, "correct amount of adaptations");
+				assert.ok(oAdaptationsTable.getDragDropConfig()[0].getEnabled(), "drag & drop is enabled");
+				assert.ok(oMoveUpButton.getEnabled(), "MoveUpButton is enabled");
+				assert.ok(oMoveDownButton.getEnabled(), "MoveDownButton is enabled");
+				assert.ok(oDefaultContextTable.getVisible(), "default context table is visible");
+			});
+		});
+	});
+});
