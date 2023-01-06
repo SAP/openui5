6b7d7bb7d8fc066bbd8a856693e77ec8d100b7cd
Lidiya Georgieva <lidiya.georgieva@sap.com>
2023-01-06 14:32:42 +0200

    [FIX] sap.m.Dialog: Where there is no vertical scrollbar, class 'sapMDialogVerticalScrollIncluded' is removed
    This is regarding the difference in width calculation on the browsers different than Chrome when
    there is vertical scrollbar in the dialog's content.
    
    BCP: 2270156416
    Change-Id: I597aa947b512997f813d409d55626fe8932ef984
    CR-Id: 002075125800000295632023

--- src/sap.m/src/sap/m/Dialog.js
+++ src/sap.m/src/sap/m/Dialog.js
@@ -1149,6 +1149,10 @@ function(
 						$dialogContent.css({"padding-right" : ""});
 						this._iLastWidthAndHeightWithScroll = null;
 					}
+				} else if (!this._hasVerticalScrollbar() || !bMinWidth) {
+					$dialog.removeClass("sapMDialogVerticalScrollIncluded");
+					$dialogContent.css({"padding-right" : ""});
+					this._iLastWidthAndHeightWithScroll = null;
 				}
 			}
 
