f2e9bbc79e4363b63a475032183571cae3104ebc
Uwe Reeder <uwe.reeder@sap.com>
2023-01-26 10:02:03 +0100

    [FIX] sap.ui.model.odata.v4.ODataListBinding#requestSideEffects: runs after delete in the same $batch
    PS1: done
    
    Change-Id: I3e9f72496a0ebba42092fcd91f2c27d152e1e873
    BCP: 2380019004

--- src/sap.ui.core/src/sap/ui/model/odata/v4/lib/_Cache.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/lib/_Cache.js
@@ -2878,7 +2878,7 @@ sap.ui.define([
 	 */
 	_CollectionCache.prototype.isDeletingInOtherGroup = function (sGroupId) {
 		return Object.values(this.aElements.$deleted || {}).some(function (oDeleted) {
-			return oDeleted.sGroupId !== sGroupId;
+			return oDeleted.groupId !== sGroupId;
 		});
 	};
 
