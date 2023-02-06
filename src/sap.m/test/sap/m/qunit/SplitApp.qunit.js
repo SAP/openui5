90569a41121497279db4ccb982b21de48101f121
Teodor Taushanov <teodor.taushanov@sap.com>
2023-02-06 17:12:20 +0200

    [FIX] sap.m.SplitApp: Fixed failing test on Safari
    The path to the image is not absolute on Safari anymore, so now there is no need for explicit browser check which was causing the test to fail as the url was not matching
    
    Change-Id: Icb8f1080ac1d85ad65b2dc01bec7fee23efd6eaa
    BCP: 2270154471
    CR-Id: 002075125800000845882023

--- src/sap.m/test/sap/m/qunit/SplitApp.qunit.js
+++ src/sap.m/test/sap/m/qunit/SplitApp.qunit.js
@@ -39,10 +39,6 @@ sap.ui.define([
 		return oApp.getDomRef("BG");
 	}
 
-	function getAbsoluteURL(sRelPath) {
-		return document.baseURI + sRelPath;
-	}
-
 	var sBackgroundImageSrc  = "test-resources/sap/m/images/SAPLogo.jpg";
 
 	QUnit.module("Initial Check");
@@ -862,7 +858,7 @@ sap.ui.define([
 		oCore.applyChanges();
 
 		// Check
-		assert.strictEqual(getBgDomElement(oApp).style.backgroundImage, 'url(\"' + (Device.browser.safari ? getAbsoluteURL(sBackgroundImageSrc) : sBackgroundImageSrc) + '\")',
+		assert.strictEqual(getBgDomElement(oApp).style.backgroundImage, 'url(\"' + sBackgroundImageSrc + '\")',
 			"correct property value");
 	});
 
@@ -889,7 +885,7 @@ sap.ui.define([
 		oApp.setBackgroundImage(sImgSrc2);
 		oCore.applyChanges();
 		// Check
-		assert.strictEqual(getBgDomElement(oApp).style.backgroundImage, 'url(\"' + (Device.browser.safari ? getAbsoluteURL(sImgSrc2) : sImgSrc2) + '\")',
+		assert.strictEqual(getBgDomElement(oApp).style.backgroundImage, 'url(\"' + sImgSrc2 + '\")',
 			"correct property value");
 	});
 
