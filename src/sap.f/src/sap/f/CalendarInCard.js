80fad2c877095ac474794af9e93715d8b7107df0
Todor Stoyanov <t.stoyanov@sap.com>
2023-02-14 22:27:41 +0200

    [INTERNAL] sap.f.CalendarCard: Use UI5Date instead of native Date
    PS1: Used UI5Date instead of native date in CalendarCard and CalendarCard.qunit
    PS2: Update commit message
    PS3: Used UI5Date instead of native date in CalendarContent
    
    JIRA: BGSOFUIBALKAN-6426
    
    Change-Id: Ide3fd42f8c8097cae6e18a0a8017c1f9cb29a3b9

--- src/sap.f/src/sap/f/CalendarInCard.js
+++ src/sap.f/src/sap/f/CalendarInCard.js
@@ -12,7 +12,8 @@ sap.ui.define([
 	'sap/ui/unified/Calendar',
 	'sap/ui/unified/calendar/CalendarDate',
 	'sap/ui/unified/calendar/CalendarUtils',
-	'./CalendarInCardRenderer'
+	'./CalendarInCardRenderer',
+	"sap/ui/core/date/UI5Date"
 ], function(
 	Button,
 	Toolbar,
@@ -23,7 +24,8 @@ sap.ui.define([
 	Calendar,
 	CalendarDate,
 	CalendarUtils,
-	CalendarRenderer
+	CalendarRenderer,
+	UI5Date
 ) {
 	"use strict";
 
@@ -176,7 +178,7 @@ sap.ui.define([
 	 * @private
 	 */
 	CalendarInCard.prototype._handleTodayPress = function () {
-		var oDate = new Date(),
+		var oDate = UI5Date.getInstance(),
 			oCalDate = CalendarDate.fromLocalJSDate(oDate);
 
 		this.getAggregation("month")[0].setDate(oDate);
@@ -421,7 +423,7 @@ sap.ui.define([
 	 * @private
 	 */
 	CalendarInCard.prototype._dateMatchesVisibleRange = function() {
-		var oCalNewDate = CalendarDate.fromLocalJSDate(new Date()),
+		var oCalNewDate = CalendarDate.fromLocalJSDate(UI5Date.getInstance()),
 			oSelectedDate,
 			bIsSameDate,
 			oYearRangePicker,
@@ -440,7 +442,7 @@ sap.ui.define([
 			case 3: // year range picker
 				oYearRangePicker = this._getYearRangePicker();
 				oYearRangePickerDate = oYearRangePicker.getDate();
-				oStartDate = new Date(oYearRangePickerDate.getFullYear() + (oYearRangePicker.getRangeSize() / 2),
+				oStartDate = UI5Date.getInstance(oYearRangePickerDate.getFullYear() + (oYearRangePicker.getRangeSize() / 2),
 					oYearRangePickerDate.getMonth(), oYearRangePickerDate.getDate());
 				return CalendarUtils._isSameMonthAndYear(CalendarDate.fromLocalJSDate(oStartDate),
 					oCalNewDate);
