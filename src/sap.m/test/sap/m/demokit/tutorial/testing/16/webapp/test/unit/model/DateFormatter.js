be9899ab07c51833266c2aa8904a670306325970
Peter Buchholz <peter.buchholz@sap.com>
2023-03-01 16:52:26 +0100

    [INTERNAL] sap.m.test.sap.m.demokit.tutorial.testing.*: use UI5Date
    The UI5Date class is meant to replace the JavaScript Date entirely
    within SAPUI. Therefore we only use the UI5Date in our samples unless
    Date.now() is sufficient.
    
    PS1: do it
    
    Change-Id: I66042691a5f2f01c7f290b4e6a88aff97dabbb96
    JIRA: CPOUI5MODELS-1140

--- src/sap.m/test/sap/m/demokit/tutorial/testing/16/webapp/test/unit/model/DateFormatter.js
+++ src/sap.m/test/sap/m/demokit/tutorial/testing/16/webapp/test/unit/model/DateFormatter.js
@@ -2,8 +2,9 @@
 
 sap.ui.define([
 	"sap/ui/demo/bulletinboard/model/DateFormatter",
-	"sap/ui/core/Locale"
-], function(DateFormatter, Locale) {
+	"sap/ui/core/Locale",
+	"sap/ui/core/date/UI5Date"
+], function(DateFormatter, Locale, UI5Date) {
 	"use strict";
 
 	var oFormatter = null;
@@ -12,7 +13,7 @@ sap.ui.define([
 		beforeEach: function() {
 			oFormatter = new DateFormatter({
 				now: function() {
-					return new Date(2015, 2, 14, 14, 0, 0, 0).getTime();
+					return UI5Date.getInstance(2015, 2, 14, 14, 0, 0, 0).getTime();
 				},
 				locale: new Locale("en-US")
 			});
@@ -25,25 +26,25 @@ sap.ui.define([
 	});
 
 	QUnit.test("Should return time if date from today", function(assert) {
-		var oDate = new Date(2015, 2, 14, 12, 5, 0, 0);
+		var oDate = UI5Date.getInstance(2015, 2, 14, 12, 5, 0, 0);
 		var sFormattedDate = oFormatter.format(oDate);
 		assert.strictEqual(sFormattedDate, "12:05 PM");
 	});
 
 	QUnit.test("Should return 'Yesterday' if date from yesterday", function(assert) {
-		var oDate = new Date(2015, 2, 13);
+		var oDate = UI5Date.getInstance(2015, 2, 13);
 		var sFormattedDate = oFormatter.format(oDate);
 		assert.strictEqual(sFormattedDate, "Yesterday");
 	});
 
 	QUnit.test("Should return day of the week if date < 7 days ago", function(assert) {
-		var oDate = new Date(2015, 2, 8);
+		var oDate = UI5Date.getInstance(2015, 2, 8);
 		var sFormattedDate = oFormatter.format(oDate);
 		assert.strictEqual(sFormattedDate, "Sunday");
 	});
 
 	QUnit.test("Should return date w/o time if date > 7 days ago", function(assert) {
-		var oDate = new Date(2015, 2, 7);
+		var oDate = UI5Date.getInstance(2015, 2, 7);
 		var sFormattedDate = oFormatter.format(oDate);
 		assert.strictEqual(sFormattedDate, "Mar 7, 2015");
 	});
