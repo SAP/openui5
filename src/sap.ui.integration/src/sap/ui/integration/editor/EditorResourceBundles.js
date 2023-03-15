b1e382ab957ca251ce0dd6a5a705e72c8b3df295
Alex Xiong <a.xiong@sap.com>
2023-03-15 19:21:39 +0800

    [INTERNAL][FIX] sap.ui.integration: Card Editor - load the resource bundles for multi language feature for each Card Editor
    Change-Id: I8489d3ecd27f78c519f851b5729de6b2b64fb1f8

--- src/sap.ui.integration/src/sap/ui/integration/editor/EditorResourceBundles.js
+++ src/sap.ui.integration/src/sap/ui/integration/editor/EditorResourceBundles.js
@@ -3,90 +3,82 @@
  */
 
 sap.ui.define([
+	"sap/ui/base/ManagedObject",
 	"sap/base/util/LoaderExtensions",
 	"sap/base/i18n/ResourceBundle"
-], function (LoaderExtensions, ResourceBundle) {
+], function (ManagedObject, LoaderExtensions, ResourceBundle) {
 	"use strict";
 
 	/**
-	 * @class
+	 * Constructor for a new <code>EditorResourceBundles</code>.
+	 *
+	 * @class Resource Bundles of Editor
 	 * @alias sap.ui.integration.editor.EditorResourceBundles
 	 * @author SAP SE
 	 * @since 1.94.0
 	 * @version ${version}
 	 * @private
-	 * @experimental since 1.94.0
-	 * @ui5-restricted
 	 */
-	var EditorResourceBundles = (function () {
-		var _aEditorResourceBundles,
-			_aLanguageList,
-			_sResourceBundleURL,
-            _aSupportedLocales;
-		LoaderExtensions.loadResource("sap/ui/integration/editor/languages.json", {
-			dataType: "json",
-			failOnError: false,
-			async: true
-		}).then(function (o) {
-			_aLanguageList = o;
-		});
-
-		function init() {
-			_aEditorResourceBundles = [];
-			//according to the language list, load each resource bundle
-			for (var p in _aLanguageList) {
-				var oResourceBundleTemp;
-				if (_sResourceBundleURL) {
-					var aFallbacks = [p];
-					if (p.indexOf("-") > -1) {
-						aFallbacks.push(p.substring(0, p.indexOf("-")));
-					}
-					//add en into fallbacks
-					if (!aFallbacks.includes("en")) {
-						aFallbacks.push("en");
-					}
-					oResourceBundleTemp = ResourceBundle.create({
-						url: _sResourceBundleURL,
-						async: false,
-						locale: p,
-						supportedLocales: aFallbacks
-					});
+	var EditorResourceBundles = ManagedObject.extend("sap.ui.integration.editor.EditorResourceBundles", {
+		metadata: {
+			library: "sap.ui.integration",
+			properties: {
+				url: {
+					type: "string",
+					defaultValue: ""
+				},
+				languages: {
+					type: "object"
+				},
+				supportedLocales: {
+					type: "array"
 				}
-                var oResourceBundleObject = {
-                    "language": _aLanguageList[p],
-                    "resourceBundle": oResourceBundleTemp,
-                    "isSupportedLocale": true
-                };
-                if (Array.isArray(_aSupportedLocales) && !_aSupportedLocales.includes(p) && !_aSupportedLocales.includes(p.replace('-', '_'))) {
-                    oResourceBundleObject.isSupportedLocale = false;
-                }
-				_aEditorResourceBundles[p] = oResourceBundleObject;
 			}
-			return _aEditorResourceBundles;
 		}
+	});
 
-		return {
-			getResourceBundleURL: function() {
-				return _sResourceBundleURL;
-			},
-			setResourceBundleURL: function(sResourceBundleURL) {
-				_sResourceBundleURL = sResourceBundleURL;
-			},
-            getSupportedLocales: function() {
-				return _aSupportedLocales;
-			},
-			setSupportedLocales: function(aSupportedLocales) {
-				_aSupportedLocales = aSupportedLocales;
-			},
-			getInstance: function () {
-				if (!_aEditorResourceBundles) {
-					_aEditorResourceBundles = init();
+	EditorResourceBundles.prototype.loadResourceBundles = function () {
+		var sUrl = this.getUrl();
+		var aSupportedLocales = this.getSupportedLocales();
+		var aLanguages = this.getLanguages();
+		this._aEditorResourceBundles = [];
+		//according to the language list, load each resource bundle
+		for (var p in aLanguages) {
+			var oResourceBundleTemp;
+			if (sUrl) {
+				var aFallbacks = [p];
+				if (p.indexOf("-") > -1) {
+					aFallbacks.push(p.substring(0, p.indexOf("-")));
 				}
-				return _aEditorResourceBundles;
+				//add en into fallbacks
+				if (!aFallbacks.includes("en")) {
+					aFallbacks.push("en");
+				}
+				oResourceBundleTemp = ResourceBundle.create({
+					url: sUrl,
+					async: false,
+					locale: p,
+					supportedLocales: aFallbacks
+				});
 			}
-		};
+			var oResourceBundleObject = {
+				"language": aLanguages[p],
+				"resourceBundle": oResourceBundleTemp,
+				"isSupportedLocale": true
+			};
+			if (Array.isArray(aSupportedLocales) && !aSupportedLocales.includes(p) && !aSupportedLocales.includes(p.replace('-', '_'))) {
+				oResourceBundleObject.isSupportedLocale = false;
+			}
+			this._aEditorResourceBundles[p] = oResourceBundleObject;
+		}
+	};
 
-	})();
+	EditorResourceBundles.prototype.getResourceBundles = function () {
+		if (!this._aEditorResourceBundles) {
+			this.loadResourceBundles();
+		}
+		return this._aEditorResourceBundles;
+	};
 
 	return EditorResourceBundles;
 });
