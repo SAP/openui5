9baa98e07c8d122ac5a85ebf47424b34c028b833
martinhaeuser <martin.haeuser@sap.com>
2023-03-16 13:27:29 +0100

    [INTERNAL] p13n.enum: set ui5-restricted as visibility
    Change-Id: Ieff27c51c66e2158d2a5270f53703ee4d32e89d6

--- src/sap.m/src/sap/m/p13n/enum/ProcessingStrategy.js
+++ src/sap.m/src/sap/m/p13n/enum/ProcessingStrategy.js
@@ -11,24 +11,33 @@ sap.ui.define(function() {
 	 *
 	 * @enum {string}
      * @private
-     * @ui5-restricted
+     * @ui5-restricted sap.m.p13n
 	 * @since 1.108
 	 * @alias sap.m.p13n.enum.ProcessingStrategy
 	 */
 	var ProcessingStrategy = {
 		/**
 		 * The delta will only calculate changes that will be added in addition to the existing state
+		 *
+		 * @private
+		 * @ui5-restricted sap.m.p13n
 		 */
 		Add: false,
 		/**
 		 * The delta will calculate changes and include state that has been added and removed
 		 * <b>Note</b>: For example for array based state, entries added/removed will be respected.
 		 * For example for object/path based state, the diff will not include paths that have not been explicitly provided.
+		 *
+ 		 * @private
+		 * @ui5-restricted sap.m.p13n
 		 */
 		PartialReplace: true,
 		/**
 		 * The delta will be calculated as complete/absolute state difference, in case the new state does not
 		 * provide a path to an existing state, this is going to be removed in addition to the PartialReplace processing strategy.
+		 *
+ 		 * @private
+		 * @ui5-restricted sap.m.p13n
 		 */
 		FullReplace: "FullReplace"
 	};
