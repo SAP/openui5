6eaa7955ce895c83ba2c170294ef9cf85ce1425f
Lars Kissel <lars.kissel@sap.com>
2023-04-17 18:11:48 +0200

    [INTERNAL] sap.ui.base.ExpressionParser: Improve scope of allowed expressions
    PS1: Downport
    
    BCP: 2370033311
    Change-Id: I91a3a38686841a8cc3f6a259c2560d74c669cb71
    CR-Id: 002075125900001359572023

--- src/sap.ui.core/src/sap/ui/base/ExpressionParser.js
+++ src/sap.ui.core/src/sap/ui/base/ExpressionParser.js
@@ -323,7 +323,7 @@ sap.ui.define([
 	 * @returns {any} the binding value
 	 */
 	function BINDING(i, aParts) {
-		return aParts[i];
+		return clean(aParts[i]);
 	}
 
 	/**
@@ -365,7 +365,7 @@ sap.ui.define([
 		if (oReference) {
 			oReference.base = oParent;
 		}
-		return vChild;
+		return clean(vChild);
 	}
 
 	/**
@@ -379,10 +379,10 @@ sap.ui.define([
 		var oReference = {};
 
 		// evaluate function expression and call it
-		return fnLeft(aParts, oReference).apply(oReference.base,
+		return clean(fnLeft(aParts, oReference).apply(oReference.base,
 			aArguments.map(function (fnArgument) {
 				return fnArgument(aParts); // evaluate argument
-			}));
+			})));
 	}
 
 	/**
@@ -434,7 +434,7 @@ sap.ui.define([
 		if (oReference) {
 			oReference.base = oParent;
 		}
-		return vChild;
+		return clean(vChild);
 	}
 
 	/**
@@ -474,6 +474,16 @@ sap.ui.define([
 			nud: unexpected
 		};
 		return mSymbols[sId];
+	}
+
+	/**
+	 * Cleans the given <code>vValue</code>.
+	 *
+	 * @param {any} vValue - the value to be cleaned
+	 * @returns {any} the cleaned value
+	 */
+	function clean(vValue) {
+		return vValue === Function ? undefined : vValue;
 	}
 
 	/**
