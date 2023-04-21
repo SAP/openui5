06da3cca4abdd10cb18467fed738e776ff0f3c05
Martin Haeuser <martin.haeuser@sap.com>
2023-04-21 11:14:18 +0200

    [INTERNAL] P13nOpa: use #requestFreshURL
    This reverts commit 1fe24a3ab3166d1aa0d415f3855f60a9de00c62c.
    
    Change-Id: I6dc8e823dabd0d846c02606d75eb2d0d3e5fbcbb

--- src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/appUnderTestTable/Component.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/qunit/p13n/OpaTests/appUnderTestTable/Component.js
@@ -2,30 +2,14 @@
  * ${copyright}
  */
 sap.ui.define([
-	'sap/ui/core/UIComponent',
-	'test-resources/sap/ui/mdc/qunit/util/V4ServerHelper',
-	'sap/ui/model/odata/v4/ODataModel'
-], function(UIComponent, V4ServerHelper, ODataModel) {
+	'sap/ui/core/sample/common/Component',
+	"sap/base/util/UriParameters"
+], function(CommonComponent, UriParameters) {
 	"use strict";
 
-	return UIComponent.extend("AppUnderTestTable.Component", {
+	return CommonComponent.extend("AppUnderTestTable.Component", {
 		metadata: {
 			manifest: "json"
-		},
-
-		init: function() {
-			UIComponent.prototype.init.apply(this, arguments);
-			V4ServerHelper.requestFreshServerURL(true).then(function(tenantBaseUrl) {
-
-				var oModel = new ODataModel({
-					serviceUrl: tenantBaseUrl + "music/",
-					groupId: "$direct",
-					autoExpandSelect: true,
-					operationMode: "Server"
-				 });
-
-				 this.setModel(oModel);
-			 }.bind(this));
 		}
 	});
 });
