64b6bf7e295e5dda04eb019965e30884f46b1394
Lars Kissel <lars.kissel@sap.com>
2023-04-27 10:17:37 +0200

    [INTERNAL] sap.ui.core.demokit.sample.*: Improve types and placeholders for date and time related apps
    PS1: do it
    PS2: rebase
    PS3: update commit message
    
    Change-Id: I2d337417e8843fbbf4cf780011e475a067b8ebd9
    JIRA: CPOUI5MODELS-1173

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeTimeAsTime/C.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeTimeAsTime/C.controller.js
@@ -1,25 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/LocaleData",
 	"sap/ui/core/date/UI5Date",
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/Configuration"
-], function (LocaleData, UI5Date, Controller, JSONModel, Configuration) {
+	"sap/ui/model/json/JSONModel"
+], function (UI5Date, Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.core.sample.TypeTimeAsTime.C", {
 
 		_data : {
-			"time" : UI5Date.getInstance()
+			time: UI5Date.getInstance()
 		},
 
 		onInit : function () {
-			var oLocale = Configuration.getLocale(),
-				oLocaleData = new LocaleData(oLocale),
-				oModel;
-
-			this._data["dtPattern"] = oLocaleData.getTimePattern("medium");
-			oModel = new JSONModel(this._data);
+			var oModel = new JSONModel(this._data);
 			this.getView().setModel(oModel);
 		}
 	});
