f74f1593db77695771f2a67dc0865e1860943cf7
Alexander Deutschmann <alexander.deutschmann@sap.com>
2023-05-03 18:12:30 +0200

    [INTERNAL][FIX] MDCTable: Cleanup of the ManagedObjectModel from type instance
    Change-Id: I35a4bda5707229f8e1c8a8e55bf0f5137803b8ef
    BCP: 2370057665

---
+++ src/sap.ui.mdc/test/sap/ui/mdc/qunit/table/TableTypeBase.qunit.js
@@ -0,0 +1,75 @@
+/* global QUnit */
+
+sap.ui.define([
+	"sap/ui/mdc/Table",
+	"sap/ui/mdc/table/TableTypeBase"
+], function(
+	Table,
+	TableTypeBase
+) {
+	"use strict";
+
+	var TestTableType = TableTypeBase.extend("sap.ui.mdc.test.TestTableType");
+
+	QUnit.module("Lifecycle of the ManagedObjectModel instance", {
+		beforeEach: function() {
+			this.createTable();
+		},
+		afterEach: function() {
+			this.destroyTable();
+		},
+		createTable: function() {
+			this.destroyTable();
+			this.oTable = new Table({
+				type: new TestTableType()
+			});
+			return this.oTable;
+		},
+		destroyTable: function() {
+			if (this.oTable) {
+				this.oTable.destroy();
+			}
+		},
+		sModelName: "$sap.ui.mdc.Table#type"
+	});
+
+	QUnit.test("After init", function(assert) {
+		var oModel = this.oTable.getModel(this.sModelName);
+
+		assert.ok(oModel, "The table has a model with the name '" + this.sModelName + "'");
+		assert.ok(oModel.isA("sap.ui.model.base.ManagedObjectModel"), "The model is of type sap.ui.model.base.ManagedObjectModel");
+		assert.ok(!oModel.bDestroyed, "The model is not destroyed");
+	});
+
+	QUnit.test("Remove the type from the table", function(assert) {
+		var oModel = this.oTable.getModel(this.sModelName);
+
+		this.oTable.setAggregation("type", null); // Table#setType immediately creates a new default type instance.
+
+		assert.equal(this.oTable.getModel(this.sModelName), null, "The table does not have a model with the name '" + this.sModelName + "'");
+		assert.ok(!oModel.bDestroyed, "The model is not destroyed");
+	});
+
+	QUnit.test("Destroy the type", function(assert) {
+		var oModel = this.oTable.getModel(this.sModelName);
+		var sModelName = this.sModelName;
+		var fnExit = this.oTable.getType().exit;
+
+		// Injecting into the exit hook is necessary, because the table immediately creates a new default type instance.
+		this.oTable.getType().exit = function() {
+			fnExit.apply(this, arguments);
+			assert.equal(this.getTable().getModel(sModelName), null, "The table does not have a model with the name '" + sModelName + "'");
+			assert.ok(oModel.bDestroyed, "The model is destroyed");
+		};
+
+		this.oTable.getType().destroy();
+	});
+
+	QUnit.test("Destroy the table", function(assert) {
+		this.oTable.destroy();
+
+		var oModel = this.oTable.getModel(this.sModelName);
+		assert.ok(oModel, "The table has a model with the name '" + this.sModelName + "'");
+		assert.ok(oModel.bDestroyed, "The model is destroyed");
+	});
+});
