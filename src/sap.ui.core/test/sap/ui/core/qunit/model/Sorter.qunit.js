c7087527dcf41d8d32752d9d0dd4e580c943a496
Stefan Scherer <stefan.scherer@sap.com>
2023-05-23 11:28:09 +0200

    [INTERNAL] sap.ui.core.qunit.model.Sorter.qunit.js: legacy-free UI5
    PS1: Tests adjusted
    
    Change-Id: I4858049178ecb5ff54ea053939f774f61c98b2a5
    JIRA: CPOUI5MODELS-1301

--- src/sap.ui.core/test/sap/ui/core/qunit/model/Sorter.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/model/Sorter.qunit.js
@@ -19,7 +19,9 @@ sap.ui.define([
 
 	//*********************************************************************************************
 	QUnit.test("defaultComparator: localeCompare with language tag", function (assert) {
-		this.mock(Configuration).expects("getLanguageTag").withExactArgs().returns("foo");
+		var oConfigurationMock = this.mock(Configuration);
+
+		oConfigurationMock.expects("getLanguageTag").withExactArgs().returns("foo");
 		this.mock(String.prototype).expects("localeCompare")
 			.withExactArgs("~b", "foo")
 			.on("~a")
@@ -27,6 +29,9 @@ sap.ui.define([
 
 		// code under test
 		assert.strictEqual(Sorter.defaultComparator("~a", "~b"), "bar");
+
+		// Otherwise, the call in "afterEach" leads to an error.
+		oConfigurationMock.verify();
 	});
 
 	//*********************************************************************************************
