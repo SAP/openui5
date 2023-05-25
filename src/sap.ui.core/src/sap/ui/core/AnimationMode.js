bd3d208749e6f5effbab21b4cf067004902bdaf8
Jan Mummenthaler <jan.mummenthaler@sap.com>
2023-05-25 16:07:17 +0200

    [INTERNAL] new control behavior Configuration
    Introduce ControlBehavior module for handling AnimationMode
    and Accessibility configuration options.
    
    Change-Id: I33ffc69f4bcf1884aea4128717eb533e36dd39e9

---
+++ src/sap.ui.core/src/sap/ui/core/AnimationMode.js
@@ -0,0 +1,47 @@
+/*!
+ * ${copyright}
+ */
+
+// Provides type sap.ui.core.AnimationMode.
+sap.ui.define([], function() {
+	"use strict";
+
+	/**
+	 * Enumerable list with available animation modes.
+	 *
+	 * This enumerable is used to validate the animation mode. Animation modes allow to specify
+	 * different animation scenarios or levels. The implementation of the Control (JavaScript or CSS)
+	 * has to be done differently for each animation mode.
+	 *
+	 * @enum {string}
+	 * @alias module:sap/ui/core/AnimationMode
+	 * @private
+	 * @ui5-restricted sap.ui.core.Configuration
+	 */
+	var AnimationMode = {
+		/**
+		 * <code>full</code> represents a mode with unrestricted animation capabilities.
+		 * @public
+		 */
+		full : "full",
+
+		/**
+		 * <code>basic</code> can be used for a reduced, more light-weight set of animations.
+		 * @public
+		 */
+		basic : "basic",
+
+		/**
+		 * <code>minimal</code> includes animations of fundamental functionality.
+		 * @public
+		 */
+		minimal : "minimal",
+
+		/**
+		 * <code>none</code> deactivates the animation completely.
+		 * @public
+		 */
+		none : "none"
+	};
+	return AnimationMode;
+});
