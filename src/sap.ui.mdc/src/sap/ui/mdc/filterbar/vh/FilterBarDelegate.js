20b093aa183f2a13b123cc1853146db85d1a02b5
Sebastian Allmann <sebastian.allmann@sap.com>
2023-06-06 13:11:48 +0200

    [INTERNAL] Field: fix sample page to use odata TypeMap
    Change-Id: I693bec81cece6197940b29d5eeb36314b3756424

--- src/sap.ui.mdc/src/sap/ui/mdc/filterbar/vh/FilterBarDelegate.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/filterbar/vh/FilterBarDelegate.js
@@ -6,15 +6,21 @@
 // Delegate class used to help create content in the filterbar and fill relevant metadata
 // ---------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------
-sap.ui.define(["sap/ui/mdc/FilterBarDelegate"], function(FilterBarDelegate) {
+sap.ui.define([
+	"sap/ui/mdc/FilterBarDelegate",
+	"sap/ui/core/Core"
+], function(FilterBarDelegate, Core) {
 	"use strict";
+
+	var mdcMessageBundle = Core.getLibraryResourceBundle("sap.ui.mdc");
 
 	var ValueHelpFilterBarDelegate = Object.assign({}, FilterBarDelegate);
 
 	ValueHelpFilterBarDelegate.fetchProperties = function(oFilterBar) {
 		return Promise.resolve([{
 			name: "$search",
-			typeConfig: FilterBarDelegate.getTypeMap().getTypeConfig("String", null, null)
+			label: mdcMessageBundle.getText("filterbar.SEARCH"),
+			dataType: "sap.ui.model.type.String"
 		}]);
 	};
 
