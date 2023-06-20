5826b4c7fa58362e975077ee23906ff8694e9227
Martin R. Hristov <m.hristov@sap.com>
2023-06-20 17:07:14 +0300

    [INTERNAL] sap.m.Input: Enhance late binding example
    JIRA: BGSOFUIRILA-3610
    Change-Id: I4792b145015bce5c65787d7f1d1cfaae81966eb9

--- src/sap.m/test/sap/m/demokit/sample/InputModelUpdate/Component.js
+++ src/sap.m/test/sap/m/demokit/sample/InputModelUpdate/Component.js
@@ -1,14 +1,12 @@
-sap.ui.define(['sap/ui/core/UIComponent'],
-	function(UIComponent) {
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
 	"use strict";
 
-	var Component = UIComponent.extend("sap.m.sample.InputModelUpdate.Component", {
-
-		metadata : {
+	return UIComponent.extend("sap.m.sample.InputModelUpdate.Component", {
+		metadata: {
 			manifest: "json"
 		}
 	});
-
-	return Component;
 
 });
