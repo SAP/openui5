07a02200c7fad484728b199b5090fada55800823
Petar Dimov <petar.dimov@sap.com>
2023-07-12 10:24:41 +0300

    [FIX] sap.ui.layout.BlockLayoutCell: Remove ARIA violation caused by aria-labelledby
    BlockLayoutCell is not focusable element.
    
    BCP: 2370061557
    Change-Id: Ib945a8303797f929588b997b37387ea650ee7c8b

--- src/sap.ui.layout/src/sap/ui/layout/BlockLayoutCellRenderer.js
+++ src/sap.ui.layout/src/sap/ui/layout/BlockLayoutCellRenderer.js
@@ -123,10 +123,6 @@ sap.ui.define(['./library', 'sap/ui/core/library', "sap/base/Log"],
 				rm.class("sapUiBlockCellCenteredContent");
 			}
 
-			if (bHasTitle) {
-				rm.attr("aria-labelledby", this.getTitleId(blockLayoutCell));
-			}
-
 			rm.openEnd();
 
 			if (bHasTitle) {
