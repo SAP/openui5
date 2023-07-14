d10751f3eecb30e46eac12a3f0f5e7a796b5ebbb
Petar Dimov <petar.dimov@sap.com>
2023-07-14 15:27:58 +0300

    [FIX] sap.f.GridContainer,GridList: Keyboard navigation is now correct for RTL languages
    BCP: 2380075806
    Change-Id: Idaa62ebb7e26accd893a323e85f3254c9cb5fad9

--- src/sap.f/src/sap/f/GridNavigationMatrix.js
+++ src/sap.f/src/sap/f/GridNavigationMatrix.js
@@ -6,7 +6,11 @@
  * Code other than the OpenUI5 libraries must not introduce dependencies to this module.
  */
 
-sap.ui.define([], function () {
+sap.ui.define([
+	"sap/ui/core/Configuration"
+], function (
+	Configuration
+) {
 	"use strict";
 
 	return {
@@ -19,18 +23,19 @@ sap.ui.define([], function () {
 		 * @returns {Array.<Array.<HTMLElement>>} The matrix
 		 */
 		create: function (oGridDomRef, aItemsDomRefs) {
-			var mGridStyles = window.getComputedStyle(oGridDomRef);
+			const mGridStyles = window.getComputedStyle(oGridDomRef);
 
-			var oLayoutSizes = {
+			const oLayoutSizes = {
 				columns: mGridStyles.gridTemplateColumns.split(/\s+/),
 				rows: mGridStyles.gridTemplateRows.split(/\s+/),
 				rowGap: parseFloat(mGridStyles.rowGap),
 				columnGap: parseFloat(mGridStyles.columnGap),
 				paddingTop: parseFloat(mGridStyles.paddingTop),
-				paddingLeft: parseFloat(mGridStyles.paddingLeft)
+				paddingLeft: parseFloat(mGridStyles.paddingLeft),
+				paddingRight: parseFloat(mGridStyles.paddingRight)
 			};
 
-			var aMatrix = Array.from(
+			const aMatrix = Array.from(
 				new Array(oLayoutSizes.rows.length),
 				function () {
 					return new Array(oLayoutSizes.columns.length).fill(this.EMPTY_CELL);
@@ -38,7 +43,7 @@ sap.ui.define([], function () {
 			);
 
 			aItemsDomRefs.forEach(function (oItemDomRef) {
-				var oPos = this._getPosition(oGridDomRef, oItemDomRef, oLayoutSizes);
+				const oPos = this._getPosition(oGridDomRef, oItemDomRef, oLayoutSizes);
 
 				this._addToMatrix(aMatrix, oPos, oItemDomRef);
 			}.bind(this));
@@ -47,10 +52,10 @@ sap.ui.define([], function () {
 		},
 
 		_getPosition: function (oGridDomRef, oItemDomRef, oLayoutSizes) {
-			var oGridRect = oGridDomRef.getBoundingClientRect(),
-				oItemRect = oItemDomRef.getBoundingClientRect(),
-				oGridRow = this._getGridRow(oGridRect, oItemRect, oLayoutSizes),
-				oGridCol = this._getGridCol(oGridRect, oItemRect, oLayoutSizes);
+			const oGridRect = oGridDomRef.getBoundingClientRect();
+			const oItemRect = oItemDomRef.getBoundingClientRect();
+			const oGridRow = this._getGridRow(oGridRect, oItemRect, oLayoutSizes);
+			const oGridCol = this._getGridCol(oGridRect, oItemRect, oLayoutSizes);
 
 			return {
 				xFrom: oGridRow.start,
@@ -61,14 +66,13 @@ sap.ui.define([], function () {
 		},
 
 		_getGridRow: function (oGridRect, oItemRect, oLayoutSizes) {
-			var iStartRow = -1,
+			let iStartRow = -1,
 				iEndRow = 0,
-				fSumRows = 0,
-				i,
-				fTopOffsetInGrid = oItemRect.top - oGridRect.top - oLayoutSizes.paddingTop,
-				fBottomOffsetInGrid = fTopOffsetInGrid + oItemRect.height;
+				fSumRows = 0;
+			const fTopOffsetInGrid = oItemRect.top - oGridRect.top - oLayoutSizes.paddingTop;
+			const fBottomOffsetInGrid = fTopOffsetInGrid + oItemRect.height;
 
-			for (i = 0; i < oLayoutSizes.rows.length; i++) {
+			for (let i = 0; i < oLayoutSizes.rows.length; i++) {
 				fSumRows += parseFloat(oLayoutSizes.rows[i]);
 
 				if (iStartRow === -1 && fTopOffsetInGrid < fSumRows) {
@@ -90,25 +94,51 @@ sap.ui.define([], function () {
 		},
 
 		_getGridCol: function (oGridRect, oItemRect, oLayoutSizes) {
-			var iStartCol = -1,
-				iEndCol = 0,
+			let iStartCol,
+				iEndCol,
 				fSumCols = 0,
-				i,
-				fLeftOffsetInGrid = oItemRect.left - oGridRect.left - oLayoutSizes.paddingLeft,
+				fLeftOffsetInGrid,
+				fRightOffsetInGrid;
+
+			if (Configuration.getRTL()) {
+				iEndCol = -1;
+				iStartCol = oLayoutSizes.columns.length - 1;
+				fRightOffsetInGrid = oGridRect.right - oLayoutSizes.paddingRight - oItemRect.right;
+				fLeftOffsetInGrid = fRightOffsetInGrid + oItemRect.width;
+
+				for (let i = oLayoutSizes.columns.length; i > 0; i--) {
+					fSumCols += parseFloat(oLayoutSizes.columns[i - 1]);
+
+					if (iEndCol === -1 && fRightOffsetInGrid < fSumCols) {
+						iEndCol = i;
+					}
+
+					fSumCols += oLayoutSizes.columnGap;
+
+					if (Math.round(fLeftOffsetInGrid) <= Math.round(fSumCols)) {
+						iStartCol = i - 1;
+						break;
+					}
+				}
+			} else {
+				iStartCol = -1;
+				iEndCol = 0;
+				fLeftOffsetInGrid = oItemRect.left - oGridRect.left - oLayoutSizes.paddingLeft;
 				fRightOffsetInGrid = fLeftOffsetInGrid + oItemRect.width;
 
-			for (i = 0; i < oLayoutSizes.columns.length; i++) {
-				fSumCols += parseFloat(oLayoutSizes.columns[i]);
+				for (let i = 0; i < oLayoutSizes.columns.length; i++) {
+					fSumCols += parseFloat(oLayoutSizes.columns[i]);
 
-				if (iStartCol === -1 && fLeftOffsetInGrid < fSumCols) {
-					iStartCol = i;
-				}
+					if (iStartCol === -1 && fLeftOffsetInGrid < fSumCols) {
+						iStartCol = i;
+					}
 
-				fSumCols += oLayoutSizes.columnGap;
+					fSumCols += oLayoutSizes.columnGap;
 
-				if (Math.round(fRightOffsetInGrid) <= Math.round(fSumCols)) {
-					iEndCol = i + 1;
-					break;
+					if (Math.round(fRightOffsetInGrid) <= Math.round(fSumCols)) {
+						iEndCol = i + 1;
+						break;
+					}
 				}
 			}
 
@@ -119,10 +149,8 @@ sap.ui.define([], function () {
 		},
 
 		_addToMatrix: function (aMatrix, oPosition, oDomRef) {
-			var iRow, iCol;
-
-			for (iRow = oPosition.xFrom; iRow < oPosition.xTo; iRow++) {
-				for (iCol = oPosition.yFrom; iCol < oPosition.yTo; iCol++) {
+			for (let iRow = oPosition.xFrom; iRow < oPosition.xTo; iRow++) {
+				for (let iCol = oPosition.yFrom; iCol < oPosition.yTo; iCol++) {
 					aMatrix[iRow][iCol] = oDomRef;
 				}
 			}
--- src/sap.f/test/sap/f/qunit/GridNavigationMatrix.qunit.js
+++ src/sap.f/test/sap/f/qunit/GridNavigationMatrix.qunit.js
@@ -3,187 +3,231 @@
 sap.ui.define([
 	"sap/f/GridNavigationMatrix",
 	"sap/ui/qunit/utils/createAndAppendDiv",
-	"sap/ui/thirdparty/jquery"
+	"sap/ui/core/Configuration"
 ],
 function (
 	GridNavigationMatrix,
 	createAndAppendDiv,
-	jQuery
+	Configuration
 ) {
 	"use strict";
 
-	var DOM_RENDER_LOCATION = "grid-area";
+	const DOM_RENDER_LOCATION = "grid-area";
 	createAndAppendDiv(DOM_RENDER_LOCATION);
+	const EMPTY_CELL = GridNavigationMatrix.EMPTY_CELL;
 
-	QUnit.module("Creating navigation matrix - different configurations", {
-		createGrid: function (sCont) {
-			this.$grid = jQuery(sCont).css({
-				display: "grid",
-				gridTemplateColumns: "repeat(3, 8rem)"
-			});
-			this.$grid.appendTo("#" + DOM_RENDER_LOCATION);
-			return this.$grid;
-		},
-		afterEach: function () {
-			this.$grid.detach();
+	function createGridItem(itemText, itemStyles = {}) {
+		const item = document.createElement("div");
+
+		item.appendChild(document.createTextNode(itemText));
+
+		for (const [name, value] of Object.entries(itemStyles)) {
+			item.style.setProperty(name, value);
 		}
-	});
+
+		return item;
+	}
+
+	function createGrid(gridConfig) {
+		const grid = document.createElement("div");
+		const gridStyles = {
+			display: "grid",
+			...gridConfig.styles
+		};
+
+		gridConfig.items.forEach(grid.appendChild.bind(grid));
+
+		for (const [name, value] of Object.entries(gridStyles)) {
+			grid.style.setProperty(name, value);
+		}
+
+		return grid;
+	}
+
+	function testMatrix(config, assert) {
+		document.getElementById(DOM_RENDER_LOCATION).appendChild(config.grid);
+
+		let matrix = GridNavigationMatrix.create(config.grid, Array.from(config.grid.children));
+
+		assert.deepEqual(matrix, config.expectedMatrix, "Navigation matrix should be correct");
+
+		Configuration.setRTL(true);
+		matrix = GridNavigationMatrix.create(config.grid, Array.from(config.grid.children));
+
+		assert.deepEqual(matrix, config.expectedRTLMatrix, "Navigation matrix should be correct in RTL");
+
+		Configuration.setRTL(false);
+		config.grid.remove();
+	}
+
+	QUnit.module("Creating navigation matrix - different configurations");
 
 	QUnit.test("1 row / 1 column per item", function (assert) {
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-				"<div>item 6</div>" +
-			"</div>"
-		).css({
-			gap: "8px"
-		});
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
+		const item1 = createGridItem("item 1");
+		const item2 = createGridItem("item 2");
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
+		const item6 = createGridItem("item 6");
 
-		// Assert
-		assert.strictEqual(aMatrix.length, 2, "There are 2 rows");
-		assert.strictEqual(aMatrix[0].length, 3, "There are 3 columns");
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[0][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[1][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[1][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(5), aMatrix[1][2], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5, item6],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "0px"
+				}
+			}),
+			expectedMatrix: [
+				[item1, item2, item3],
+				[item4, item5, item6]
+			],
+			expectedRTLMatrix: [
+				[item3, item2, item1],
+				[item6, item5, item4]
+			]
+		}, assert);
 	});
 
 	QUnit.test("Item with 2 rows / 2 cols", function (assert) {
-		// Arrange
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-			"</div>"
-		).css({
-			gap: "8px"
-		}).children()[1].style = "grid-row: span 2; grid-column: span 2;";
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
+		const item1 = createGridItem("item 1");
+		const item2 = createGridItem("item 2", {
+			"grid-row": "span 2",
+			"grid-column": "span 2"
+		});
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
 
-		// Assert - 2 rows / 2 columns item position
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[1][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[1][2], "Item position is correct");
-
-		// Assert - rest of the items
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[0][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[1][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[2][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[2][1], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "8px"
+				}
+			}),
+			expectedMatrix: [
+				[item1, item2, item2],
+				[item3, item2, item2],
+				[item4, item5, EMPTY_CELL]
+			],
+			expectedRTLMatrix: [
+				[item2, item2, item1],
+				[item2, item2, item3],
+				[EMPTY_CELL, item5, item4]
+			]
+		}, assert);
 	});
 
 	QUnit.test("1 row / 1 column per item, grid with padding", function (assert) {
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-				"<div>item 6</div>" +
-			"</div>"
-		).css({
-			gap: "8px",
-			padding: "1.5rem 1rem"
-		});
+		const item1 = createGridItem("item 1");
+		const item2 = createGridItem("item 2");
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
+		const item6 = createGridItem("item 6");
 
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
-
-		// Assert
-		assert.strictEqual(aMatrix.length, 2, "There are 2 rows");
-		assert.strictEqual(aMatrix[0].length, 3, "There are 3 columns");
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[0][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[1][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[1][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(5), aMatrix[1][2], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5, item6],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "8px",
+					padding: "1.5rem 1rem"
+				}
+			}),
+			expectedMatrix: [
+				[item1, item2, item3],
+				[item4, item5, item6]
+			],
+			expectedRTLMatrix: [
+				[item3, item2, item1],
+				[item6, item5, item4]
+			]
+		}, assert);
 	});
 
 	QUnit.test("Item starting and ending on specific row (no grid-auto-flow)", function (assert) {
-		// Arrange
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-			"</div>"
-		).css({
-			gap: "8px"
-		}).children()[0].style = "grid-row: 2 / 3";
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
+		const item1 = createGridItem("item 1", {
+			"grid-row": "2 / 3"
+		});
+		const item2 = createGridItem("item 2");
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
 
-		// Assert - specific row item
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[1][0], "Item position is correct");
-
-		// Assert - rest of the items
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[0][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[1][1], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "8px"
+				}
+			}),
+			expectedMatrix: [
+				[item2, item3, item4],
+				[item1, item5, EMPTY_CELL]
+			],
+			expectedRTLMatrix: [
+				[item4, item3, item2],
+				[EMPTY_CELL, item5, item1]
+			]
+		}, assert);
 	});
 
 	QUnit.test("Item starting and ending on specific column (no grid-auto-flow)", function (assert) {
-		// Arrange
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-			"</div>"
-		).css({
-			gap: "8px"
-		}).children()[0].style = "grid-column: 2 / 3;";
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
+		const item1 = createGridItem("item 1", {
+			"grid-column": "2 / 3"
+		});
+		const item2 = createGridItem("item 2");
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
 
-		// Assert - specific column item
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[0][1], "Item position is correct");
-
-		// Assert - rest of the items
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[1][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[1][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[1][2], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "8px"
+				}
+			}),
+			expectedMatrix: [
+				[EMPTY_CELL, item1, item2],
+				[item3, item4, item5]
+			],
+			expectedRTLMatrix: [
+				[item2, item1, EMPTY_CELL],
+				[item5, item4, item3]
+			]
+		}, assert);
 	});
 
 	QUnit.test("1 row / 1 column per item with 0px gap", function (assert) {
-		this.createGrid(
-			"<div>" +
-				"<div>item 1</div>" +
-				"<div>item 2</div>" +
-				"<div>item 3</div>" +
-				"<div>item 4</div>" +
-				"<div>item 5</div>" +
-				"<div>item 6</div>" +
-			"</div>"
-		).css({
-			gap: "0px"
-		});
-		var aMatrix = GridNavigationMatrix.create(this.$grid[0], this.$grid.children().get());
+		const item1 = createGridItem("item 1");
+		const item2 = createGridItem("item 2");
+		const item3 = createGridItem("item 3");
+		const item4 = createGridItem("item 4");
+		const item5 = createGridItem("item 5");
+		const item6 = createGridItem("item 6");
 
-		// Assert
-		assert.strictEqual(aMatrix.length, 2, "There are 2 rows");
-		assert.strictEqual(aMatrix[0].length, 3, "There are 3 columns");
-		assert.strictEqual(this.$grid.children().get(0), aMatrix[0][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(1), aMatrix[0][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(2), aMatrix[0][2], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(3), aMatrix[1][0], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(4), aMatrix[1][1], "Item position is correct");
-		assert.strictEqual(this.$grid.children().get(5), aMatrix[1][2], "Item position is correct");
+		testMatrix({
+			grid: createGrid({
+				items: [item1, item2, item3, item4, item5, item6],
+				styles: {
+					"grid-template-columns": "repeat(3, 8rem)",
+					gap: "0px"
+				}
+			}),
+			expectedMatrix: [
+				[item1, item2, item3],
+				[item4, item5, item6]
+			],
+			expectedRTLMatrix: [
+				[item3, item2, item1],
+				[item6, item5, item4]
+			]
+		}, assert);
 	});
 });
