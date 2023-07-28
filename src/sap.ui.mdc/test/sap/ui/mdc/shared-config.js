f0bf9d5be45a0b25d98bff211cc76735503d381c
Florian Vogt <florian.vogt@sap.com>
2023-07-28 16:02:43 +0200

    [INTERNAL] Infra: Update test and sample apps to use sap_horizon
    Change-Id: I7d326cc9a78b859a5d66161185a11e6725119e26

--- src/sap.ui.mdc/test/sap/ui/mdc/shared-config.js
+++ src/sap.ui.mdc/test/sap/ui/mdc/shared-config.js
@@ -1,3 +1,3 @@
 window['sap-ui-config'] = window['sap-ui-config'] || {};
-window['sap-ui-config'].theme = 'sap_fiori_3';
+window['sap-ui-config'].theme = 'sap_horizon';
 window['sap-ui-mdc-config'] = {disableStrictPropertyInfoValidation: true};
--- src/sap.ui.rta/test/sap/ui/rta/shared-config.js
+++ src/sap.ui.rta/test/sap/ui/rta/shared-config.js
@@ -1,5 +1,5 @@
 window["sap-ui-config"] = window["sap-ui-config"] || {};
-window["sap-ui-config"].theme = "sap_fiori_3";
+window["sap-ui-config"].theme = "sap_horizon";
 window["sap-ui-config"].noConflict = "true";
 window["sap-ui-config"].language = "en";
 window["sap-ui-config"].bindingSyntax = "complex";
