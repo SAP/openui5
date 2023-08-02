3e5088fe67db151bb36ae0bf32d7e4e7c96b76c6
Todor Stoyanov <t.stoyanov@sap.com>
2023-08-02 16:54:26 +0300

    [FIX] sap.m.MenuItem: addEventDelegate is handled correctly
    * the addEventDelegate method was not overridden and thus was not
      passing correct parameters to unified.MenuItem
    
    * Now addEventDelegate and removeEventDelegate methods are overwritten
      and pass correct parameters to unified.MenuItem
    
    BCP: 2380112907
    Change-Id: I3e44b3309592e2ec3769a19f01a3d223d8b56c97

---
+++ src/sap.m/test/sap/m/qunit/MenuItem.qunit.js
@@ -0,0 +1,84 @@
+/*global QUnit */
+sap.ui.define([
+	"sap/m/Menu",
+	"sap/m/MenuItem",
+	"sap/ui/core/Core",
+	"sap/m/Label"
+], function(
+	Menu,
+	MenuItem,
+	Core,
+	Label
+) {
+	"use strict";
+
+	QUnit.module('MenuItem(inside Menu)', {
+		beforeEach: function () {
+			this.sut = new MenuItem({
+				text: "mi",
+				items: [
+					new MenuItem({
+						text: "sub_mi_1"
+					}),
+					new MenuItem({
+						text: "sub_mi_2"
+					})
+				]
+			});
+			// Make sure sap.m.Menu code related to sap.m.MenuItem is considered
+			this.sutRootMenu = new Menu({items: this.sut});
+			this.oLabel = new Label(); //.openBy needs a reference
+			this.sutRootMenu.openBy(this.oLabel);
+			Core.applyChanges();
+		},
+		afterEach : function () {
+			this.sutRootMenu.close();
+			this.sut.destroy();
+			this.sutRootMenu.destroy();
+			this.oLabel.destroy();
+
+			this.sut = null;
+			this.sutRootMenu = null;
+			this.oLabel = null;
+
+		}
+	});
+
+	QUnit.test("addEventDelegate", function(assert) {
+		// prepare
+		var oDelegate = { onmouseover: function() { } };
+		var oSpyAddEventDelegate = this.spy(Core.byId(this.sut._getVisualControl()), "addEventDelegate");
+
+		// act
+		this.sut.addEventDelegate(oDelegate);
+
+		// assert
+		assert.ok(oSpyAddEventDelegate.calledWith(oDelegate), "eventDelegate is correctly passed to unified MenuItem");
+	});
+
+	QUnit.test("removeEventDelegate", function(assert) {
+		// prepare
+		var oDelegate = { onmouseover: function() { } };
+		var oSpyRemoveEventDelegate = this.spy(Core.byId(this.sut._getVisualControl()), "removeEventDelegate");
+		this.sut.addEventDelegate(oDelegate);
+
+		// act
+		this.sut.removeEventDelegate(oDelegate);
+
+		// assert
+		assert.ok(oSpyRemoveEventDelegate.calledWith(oDelegate),"eventDelegate is correctly removed from unified MenuItem");
+	});
+
+	QUnit.test("setProperty", function (assert) {
+		// prepare
+		var oSpySetProperty = this.spy(Core.byId(this.sut._getVisualControl()), "setProperty"),
+			sPropertyKey = "text",
+			sPropertyValue = "new_mi_text";
+
+		//act
+		this.sut.setProperty(sPropertyKey, sPropertyValue);
+
+		//assert
+		assert.ok(oSpySetProperty.calledWith(sPropertyKey, sPropertyValue), "property is correctly passed from unified MenuItem");
+	});
+});
