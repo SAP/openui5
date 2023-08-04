2ebd581471cd8c769d3f2ff25cbf2670dff480c6
Thorsten Hochreuter <thorsten.hochreuter@sap.com>
2023-08-04 18:37:24 +0200

    [INTERNAL] Theming: Support for default theme and fallback themes
    Adds support for a default theme and calculates a valid fallback theme.
    
    Two main scenarios are tackled:
    
    1. No "theme" configuration is set
    Previously, the default theme was set to "base".
    In case no theme configuration is given, this results in a broken UI
    with just the bare minimum of visual support.
    With this change we establish a predetermined default theme, set to the
    latest available supported theme.
    
    2. An invalid standard theme is set
    Invalid standard themes are:
      a) themes that have been deprecated and removed
      b) themes that are not yet supported in the current version
    
    For this "rel-1.84" branch change, the latest default theme is "sap_fiori_3".
    
    JIRA: CPOUI5FRAMEWORK-112
    JIRA: CPOUI5FRAMEWORK-500
    BCP: n/a
    CR-Id: 002075125800003562182023
    Change-Id: I301b79539221b4b0db7b9b1fd3f4bc9647ad1e9e

--- src/sap.ui.core/test/sap/ui/core/qunit/testsuites/testsuite.theming.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/testsuites/testsuite.theming.qunit.js
@@ -58,6 +58,15 @@ sap.ui.define(["sap/ui/Device"], function(Device) {
 						"sap/ui/legacy/testlib": "test-resources/sap/ui/core/qunit/testdata/legacy-uilib/"
 					}
 				}
+			},
+			"Theming_default_and_fallback": {
+				title: "Theming - defaulting and fallbacks",
+				ui5: {
+					theme: "sap_goldreflection" // Note: deprecated and deleted theme: used for fallback test!
+				},
+				qunit: {
+					reorder: false
+				}
 			}
 		}
 	};
