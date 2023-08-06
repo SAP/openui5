f54f0d507356f4271534a1497e789509a9dd39f8
Diana Petcheva <diana.petcheva@sap.com>
2023-08-06 20:56:42 +0300

    [FIX] sap.m.PlanningCalendar: Fix navigation skipping
    Issue: When there is a max date set, navigation with < and > arrow keys
    will sometimes skip a week to set the max day as the first day.
    
    Solution: No longer force the max day to be set to the first day in the
    row. Also update nav buttons after rendering since they would appear as
    disabled when they shouldn't be.
    
    BCP: 002075129400005568382023
    Change-Id: Ie952818aebf802099f74a123e7c0eaf18d895962
    (cherry picked from commit 199d51ecc8b9a3a8ad3d96474ee9f5cda490df31)
    CR-Id: 002075125900002356272023

--- src/sap.m/test/sap/m/qunit/PlanningCalendar.qunit.js
+++ src/sap.m/test/sap/m/qunit/PlanningCalendar.qunit.js
@@ -2860,9 +2860,6 @@ sap.ui.define([
 		this.oPC2.setStartDate(new Date(1999, 1, 1, 0, 0, 0));
 		this.oPC2._dateNav.setCurrent(new Date(1999, 1, 1, 0, 0, 0));
 
-		//Act
-		this.oPC2._applyArrowsLogic(true);
-
 		//Assert
 		assert.equal(this.oPC2._getHeader()._oPrevBtn.getEnabled(), false, "Back Arrow Is Disabled");
 
@@ -2870,9 +2867,6 @@ sap.ui.define([
 		this.oPC2.setMaxDate(new Date(2222,22,22,22,22,22));
 		this.oPC2.setStartDate(new Date(2222,22,22,22,22,22));
 		this.oPC2._dateNav.setCurrent(new Date(2222,22,22,22,22,22));
-
-		//Act
-		this.oPC2._applyArrowsLogic(false);
 
 		//Assert
 		assert.equal(this.oPC2._getHeader()._oNextBtn.getEnabled(), false, "Forward Arrow Is Disabled");
