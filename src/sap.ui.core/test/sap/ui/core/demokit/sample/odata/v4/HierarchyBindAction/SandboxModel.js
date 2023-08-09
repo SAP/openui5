dd338ae51f0926ac0e91c0da5be4d6eb73f1998f
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2023-08-09 12:29:56 +0200

    [INTERNAL] sap.ui.core.sample.odata.v4.HierarchyBindAction
    PS1: new demo app
    
    Change-Id: I264cbae7cd0814d4b35d1cf5e7d1509f38ec38be
    JIRA: CPOUI5ODATAV4-2226

---
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/HierarchyBindAction/SandboxModel.js
@@ -0,0 +1,10 @@
+/*!
+ * ${copyright}
+ */
+// The SandboxModel is used in the manifest instead of OData V4 model for the following purposes:
+// Certain constructor parameters are taken from URL parameters. For the "non-realOData" case, a
+// mock server for the back-end requests is set up.
+sap.ui.define([
+], function () {
+	"use strict";
+});
