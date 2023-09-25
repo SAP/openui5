10af4fdeefc3e82ceba6405f745f378e6afb8102
Franz Mueller <franz.mueller@sap.com>
2023-09-25 13:24:37 +0200

    [INTERNAL] m.VariantManagement: provide sample
    JIRA: CPOUIFDENVER-1950
    
    Change-Id: Ia549bc7bcec7aa11d3ef45a83aa5d17f95b78f7b

---
+++ src/sap.m/test/sap/m/demokit/sample/VariantManagement/Component.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	'sap/ui/core/UIComponent'
+], function(UIComponent) {
+	"use strict";
+
+	var Component = UIComponent.extend("sap.m.sample.VariantManagement.Component", {
+
+		metadata: {
+			manifest: "json"
+		}
+	});
+
+	return Component;
+
+});
