8fd446dcdb3317229dcafcf3e3c2e7aab484de0a
Jiawei Cao <jiawei.cao@sap.com>
2023-09-26 15:38:37 +0200

    [INTERNAL] Adapt bootstrap tests to remove the usage of sap.ui.requireSync
    JIRA: CPOUI5FRAMEWORK-531
    Change-Id: I4cb673f74eabe6b3c83816610a1b1acc74de1919

--- src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/_checkCoreAndLibraryBooted.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/bootstrap/_checkCoreAndLibraryBooted.qunit.js
@@ -3,41 +3,48 @@
 	"use strict";
 
 	QUnit.test("Check Existance of Core", function(assert) {
-		assert.expect(5);
-		var done = assert.async();
+		 return new Promise((resolve, reject) => {
+			sap.ui.require(["sap/ui/core/Core", "sap/ui/core/Lib", "sap/ui/Device"], async function(Core, Lib, Device) {
+				assert.expect(5);
+				var aExpectedLibraries = ["sap.m"];
 
-		var aExpectedLibraries = ["sap.m"];
-		var aConfigurationModules = sap.ui.getCore().getConfiguration().getValue("modules");
-		var oLoadedLibraries = sap.ui.getCore().getLoadedLibraries();
-		var sExpectedLibrary, sLoadedLibrary, aDependendLibraries = [];
+				/**
+				 * @deprecated As of version 1.120
+				 * check that SAPUI5 has been loaded
+				 */
+				assert.ok(sap.ui.getCore(), "sap.ui.getCore() returns a value");
 
-		/* check that SAPUI5 has been loaded */
-		assert.ok(sap.ui.getCore(), "sap.ui.getCore() returns a value");
+				await Core.ready();
 
-		var id = document.querySelector("html").getAttribute("data-sap-ui-browser");
-		sap.ui.require(["sap/ui/Device"], function (Device) {
-			if ( Device.browser.name ) {
-				assert.strictEqual(id, Device.browser.name + Math.floor(Device.browser.versionStr), "browser is known: data-sap-ui-browser should have been set and must not be empty");
-			} else {
-				assert.ok(!id, "browser is unknown: data-sap-ui-browser should not have been set (or empty)");
-			}
-			done();
+				var id = document.querySelector("html").getAttribute("data-sap-ui-browser");
+				if ( Device.browser.name ) {
+					assert.strictEqual(id, Device.browser.name + Math.floor(Device.browser.versionStr), "browser is known: data-sap-ui-browser should have been set and must not be empty");
+				} else {
+					assert.ok(!id, "browser is unknown: data-sap-ui-browser should not have been set (or empty)");
+				}
+
+				var aConfigurationModules = sap.ui.getCore().getConfiguration().getValue("modules");
+				var oLoadedLibraries = Lib.all();
+				var sExpectedLibrary, sLoadedLibrary, aDependendLibraries = [];
+
+				for (sLoadedLibrary in oLoadedLibraries) {
+					if (!aDependendLibraries.includes(sLoadedLibrary) && !aExpectedLibraries.includes(sLoadedLibrary)) {
+						// Don't collect duplicates and only collect dependend libraries which are not already expected
+						aDependendLibraries.push(sLoadedLibrary);
+					}
+				}
+
+				for (var i = 0; i < aExpectedLibraries.length; i++) {
+					sExpectedLibrary = aExpectedLibraries[i];
+					assert.strictEqual(sExpectedLibrary + ".library", aConfigurationModules[i], "'" +  sExpectedLibrary + "' is part of sap.ui.core.Configuration property 'module'");
+					assert.ok(oLoadedLibraries[sExpectedLibrary], "'" +  sExpectedLibrary + "' is registered as loadedLibrary within Core");
+				}
+
+				assert.strictEqual(aExpectedLibraries.concat(aDependendLibraries).length, Object.keys(oLoadedLibraries).length, "Only libraries declared in bootstrap including their dependencies are loaded");
+
+				resolve();
+			}, reject);
 		});
-
-		for (sLoadedLibrary in oLoadedLibraries) {
-			if (!aDependendLibraries.includes(sLoadedLibrary) && !aExpectedLibraries.includes(sLoadedLibrary)) {
-				// Don't collect duplicates and only collect dependend libraries which are not already expected
-				aDependendLibraries.push(sLoadedLibrary);
-			}
-		}
-
-		for (var i = 0; i < aExpectedLibraries.length; i++) {
-			sExpectedLibrary = aExpectedLibraries[i];
-			assert.strictEqual(sExpectedLibrary + ".library", aConfigurationModules[i], "'" +  sExpectedLibrary + "' is part of sap.ui.core.Configuration property 'module'");
-			assert.ok(oLoadedLibraries[sExpectedLibrary], "'" +  sExpectedLibrary + "' is registered as loadedLibrary within Core");
-		}
-
-		assert.strictEqual(aExpectedLibraries.concat(aDependendLibraries).length, Object.keys(oLoadedLibraries).length, "Only libraries declared in bootstrap including their dependencies are loaded");
 	});
 
 }());
