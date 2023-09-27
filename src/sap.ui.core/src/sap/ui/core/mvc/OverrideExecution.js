e97fd3335f982cf3a568420e4eed3ad8cd3f036d
Frank Weigel <frank.weigel@sap.com>
2023-09-27 07:55:09 +0200

    [INTERNAL] core.mvc.OverrideExecution: fix minor copy & paste error
    Change-Id: I8689d279c49f68af0da17d3e1f42840363c0a9b6

--- src/sap.ui.core/src/sap/ui/core/mvc/OverrideExecution.js
+++ src/sap.ui.core/src/sap/ui/core/mvc/OverrideExecution.js
@@ -2,7 +2,7 @@
  * ${copyright}
  */
 
-// Provides enumeration sap.ui.model.CountMode
+// Provides enumeration sap.ui.core.mvc.OverrideExecution
 sap.ui.define(function() {
 	"use strict";
 
@@ -31,12 +31,12 @@ sap.ui.define(function() {
 
 		/**
 		 * The override function is called instead of the original function
-         *
-         * This is the default option for ControllerExtension overrides
+		 *
+		 * This is the default option for ControllerExtension overrides
 		 * @public
 		 */
 		Instead: "Instead"
-    };
+	};
 
 	return OverrideExecution;
 
