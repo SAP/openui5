efb9c83afa87d0700e455779659b8a79bac901f8
Teodor Taushanov <teodor.taushanov@sap.com>
2023-10-03 14:04:02 +0300

    [INTERNAL] sap.tnt.SideNavigation: visual test is now fixed
    BCP: 2370107639
    Change-Id: If58ac36ae887e3db8482e1f109a6c3d470b94961

--- src/sap.tnt/test/sap/tnt/visual/ToolPageRootItemsOnly.spec.js
+++ src/sap.tnt/test/sap/tnt/visual/ToolPageRootItemsOnly.spec.js
@@ -22,28 +22,14 @@ describe('sap.tnt.ToolPageRootItemsOnly', function() {
 			element(by.id('menuToggleButton')).click();
 		}
 
-		element(by.css('#NList > li:first-of-type > .sapTntNavLIGroupItems > li:nth-of-type(3)')).click();
+		element(by.css('#NList > li:nth-of-type(4) .sapTntNavLIGroup')).click();
 		expect(takeScreenshot()).toLookAs('1_changed_page_content');
-	});
-
-	var sideNav = element(by.id('SNav'));
-
-	// collapse NavigationListItem
-	it('should visualize collapse NavigationListItem', function () {
-		element(by.css('#NList > li:first-of-type .sapUiIcon[aria-label=Collapse]')).click();
-		expect(takeScreenshot(sideNav)).toLookAs('2_collapsedNavigation_List_Item');
-	});
-
-	// long list of items
-	it('should visualize long list of items', function () {
-		element(by.css('#NList > li:nth-of-type(3) .sapUiIcon[aria-label=Expand]')).click();
-		expect(takeScreenshot(sideNav)).toLookAs('3_long_list_of_items');
 	});
 
 	// collapse Side Content
 	it('should visualize collapsed side content', function () {
 			element(by.id('menuToggleButton')).click();
-			expect(takeScreenshot()).toLookAs('4_collapsed_side_content');
+			expect(takeScreenshot()).toLookAs('2_collapsed_side_content');
 	});
 
 	// click on menu item
@@ -51,24 +37,7 @@ describe('sap.tnt.ToolPageRootItemsOnly', function() {
 	it('should click on first item (collapsed)', function () {
 		if (!bPhone) {
 			element(by.css('#NList > .sapTnTNavLIOverflow')).click();
-			expect(takeScreenshot()).toLookAs('5_click_on_menu_item');
+			expect(takeScreenshot()).toLookAs('3_click_on_menu_item');
 		}
-	});
-
-	// change the page content
-	// only for desktop
-	it('should change the page content (collapsed)', function () {
-		if (!bPhone) {
-			element(by.css('.sapTntNavLIPopup ul.sapTntNavLIGroupItems > li:first-child')).click();
-			expect(takeScreenshot()).toLookAs('6_change_page_content');
-		}
-	});
-
-	// no side navigation
-	it('should NOT display side navigation', function () {
-		element(by.id('menuToggleButton')).click();
-		element(by.css('#NList > li:first-of-type .sapUiIcon[aria-label=Expand]')).click();
-		element(by.css('#NList > li:first-of-type > .sapTntNavLIGroupItems > li:nth-of-type(4)')).click();
-		expect(takeScreenshot(element(by.id('page3')))).toLookAs('7_no_side_navigation');
 	});
 });
