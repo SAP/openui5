b8be3153d21153c48e9acc4668ce7722dbb9830b
Sven Bernhard <sven.bernhard@sap.com>
2023-10-04 13:04:00 +0200

    [FIX] sap.ui.model.odata.v2.ODataModel#resetChanges: do not remove persistent messages
    Persistent messages must not be deleted by the ODataModel. They have to
    be deleted by the application.
    
    PS1: downport to 1.108
    PS2: review comment
    
    Change-Id: I34ccd8cc983977630266f3b5dd9bd117db02fc5f
    BCP: 2370086388
    CR-Id: 002075125800004157502023
    (cherry picked from commit e5bafc5485106319bb48989e633bd403fde348f7)

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -6332,8 +6332,7 @@ sap.ui.define([
 		} else {
 			delete this.mChangedEntities[sKey];
 		}
-		sap.ui.getCore().getMessageManager().removeMessages(this.getMessagesByEntity(sKey,
-			/*bExcludePersistent*/!bDeleteEntity));
+		sap.ui.getCore().getMessageManager().removeMessages(this.getMessagesByEntity(sKey, /*bExcludePersistent*/true));
 
 		return pMetaDataLoaded;
 	};
