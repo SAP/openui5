440d1038357d483070f992cfa393dd356216c20f
Florian Vogt <florian.vogt@sap.com>
2023-10-09 16:57:51 +0200

    [INTERNAL] sap.f/sap.m: align samples on latest best practices
    JIRA: CPOUI5FOUNDATION-713
    Change-Id: Ia862485fafd66011c811a0adc3d7d35d6709c540

--- src/sap.ui.core/test/sap/ui/core/demokit/tutorial/databinding/14/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/tutorial/databinding/14/Component.js
@@ -1,31 +1,10 @@
-sap.ui.define(['sap/ui/core/UIComponent'],
-	function (UIComponent) {
-		"use strict";
-
-		var Component = UIComponent.extend("sap.ui.core.tutorial.databinding.14.Component", {
-
-			metadata: {
-				config: {
-					sample: {
-						iframe: "webapp/index.html",
-						stretch: true,
-						files: [
-							"webapp/controller/App.controller.js",
-							"webapp/i18n/i18n.properties",
-							"webapp/i18n/i18n_de.properties",
-							"webapp/model/Products.json",
-							"webapp/view/App.view.xml",
-							"webapp/index.html",
-							"ui5.yaml",
-							"webapp/index.js",
-							"package.json"
-						]
-					}
-				}
-			}
-
-		});
-
-		return Component;
-
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], (UIComponent) => {
+	"use strict";
+	return UIComponent.extend("sap.ui.core.tutorial.databinding.14.Component", {
+		metadata: {
+			manifest: "json"
+		}
 	});
+});
