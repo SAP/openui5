341a1914cd32bc57ab46138914c8d20d5e83169e
Martin R. Hristov <m.hristov@sap.com>
2023-10-11 13:38:22 +0300

    [FIX] sap.m.Input: Reopen Suggestion Popover when new data is bound
    When having a single item bound to the tabular suggestions and when
    typing new data is received with a single item again, mutation observer
    is not called  and only Item's properties are updated. Input does not get
    notified for that change, therefore we need to listen on aggregation update
    as well.
    
    BCP: 2370107738
    Change-Id: If04c932e940de58986c1b8d4472c656f6f920ebe

--- src/sap.m/test/sap/m/qunit/testsuite.mobile.qunit.js
+++ src/sap.m/test/sap/m/qunit/testsuite.mobile.qunit.js
@@ -204,6 +204,13 @@ sap.ui.define([
 					language: "en"
 				}
 			},
+			"opa/input/SuggestionRowsPopoverOPA": {
+				title: "OPA Test Page for sap.m.Input with tabular suggestions",
+				ui5: {
+					libs: "sap.m",
+					language: "en"
+				}
+			},
 			/**
 			 * @deprecated As of version 1.50
 			 */
