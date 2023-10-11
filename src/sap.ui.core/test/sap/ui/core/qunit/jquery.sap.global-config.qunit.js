9416fac200a25284badf1552f37c3b4d2e9de2eb
Jan Mummenthaler <jan.mummenthaler@sap.com>
2023-10-11 14:13:05 +0200

    [INTERNAL] fix jQuery.sap.global-config.qunit
    Change-Id: I0e44c41fc3949eaa4b83afd20fbf2ef08f527b2a

--- src/sap.ui.core/test/sap/ui/core/qunit/jquery.sap.global-config.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/jquery.sap.global-config.qunit.js
@@ -1,13 +1,19 @@
 /* global QUnit */
-sap.ui.require(["sap/ui/core/Theming"], function(Theming) {
+sap.ui.require([
+	"sap/ui/core/Core",
+	"sap/ui/core/Theming"
+], function(
+	Core,
+	Theming
+) {
 	"use strict";
 	QUnit.module("Basic");
 
 	QUnit.test("ExternalConfig", function(assert) {
-
-		assert.equal(Theming.getTheme(), "sap_bluecrystal", "Theme must be sap_blucrystal");
-		assert.ok(sap.ui.commons !== undefined, "sap.ui.commons must be preloaded");
-		assert.ok(sap.m === undefined, "sap.m must not be preloaded");
-
+		return Core.ready().then(() => {
+			assert.equal(Theming.getTheme(), "sap_bluecrystal", "Theme must be sap_blucrystal");
+			assert.ok(sap.ui.commons !== undefined, "sap.ui.commons must be preloaded");
+			assert.ok(sap.m === undefined, "sap.m must not be preloaded");
+		});
 	});
 });
