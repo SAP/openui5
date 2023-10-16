86f274f4fd2f1cdc7de97b0bb206da2eff392704
Jiawei Cao <jiawei.cao@sap.com>
2023-10-16 17:45:35 +0200

    [INTERNAL] Stabilize the terminology integration test
    - the model is loaded asynchronously and the check of the bound text
      should be done after the resource model is loaded
    
    Change-Id: I00ffff396566bc80c55720bf27c5fb8e557d97b3
    CR-Id: 002075125800004429192023
    BCP: n/a

--- src/sap.ui.core/test/sap/ui/core/qunit/Core_libraryTerminologies_integration.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/Core_libraryTerminologies_integration.qunit.js
@@ -1,7 +1,8 @@
 /*global QUnit*/
-sap.ui.require([
-	"sap/ui/core/ComponentContainer"
-], function (ComponentContainer) {
+sap.ui.define([
+	"sap/ui/core/ComponentContainer",
+	"sap/base/util/Deferred"
+], function (ComponentContainer, Deferred) {
 	"use strict";
 
 	QUnit.module("Integration Tests", {
@@ -18,7 +19,8 @@ sap.ui.require([
 	});
 
 	QUnit.test("Basic - with terminology 'transportation'", function (assert) {
-		var done = assert.async();
+		var oComponentCreatedDeferred = new Deferred();
+		var oResourceBundleLoadedDeferred = new Deferred();
 
 		var oContainer = new ComponentContainer({
 			async: true,
@@ -26,15 +28,20 @@ sap.ui.require([
 			name: "terminologies.sample.application",
 			componentCreated: function (oParams) {
 				var oComponent = oParams.getParameter("component");
+
 				assert.ok(oComponent, "Component should be available.");
 
 				// The first three pages are statically available
 				assert.equal(sap.ui.getCore().byId("__component0---mainView--appContentOnly_label").getText(), 'I am a Transportation text (application)', "The label text should be verticalized.");
 				assert.equal(sap.ui.getCore().byId("__component0---mainView--appContentOnly_button").getText(), 'Transportation Button (application)', "The button text should be verticalized.");
-				assert.equal(sap.ui.getCore().byId("__component0---mainView--reuse_fragment_label").getText(), 'I am a Transportation text (reuse-library)', "The label text inside the reuse fragment should be verticalized.");
-				assert.equal(sap.ui.getCore().byId("__component0---mainView--reuse_fragment_button").getText(), 'Transportation Button (reuse-library)', "The button text inside the reuse fragment should be verticalized.");
 				assert.equal(sap.ui.getCore().byId("__xmlview0--reuse_component_common_label").getText(), 'I am a Transportation text (reuse-library)', "The label text inside the reuse view should be verticalized.");
 				assert.equal(sap.ui.getCore().byId("__xmlview0--reuse_component_common_button").getText(), 'Transportation Button (reuse-library)', "The button text inside the reuse view should be verticalized.");
+
+				oComponent.getModel("i18n_reuse_lib_async").getResourceBundle().then(function() {
+					assert.equal(sap.ui.getCore().byId("__component0---mainView--reuse_fragment_label").getText(), 'I am a Transportation text (reuse-library)', "The label text inside the reuse fragment should be verticalized.");
+					assert.equal(sap.ui.getCore().byId("__component0---mainView--reuse_fragment_button").getText(), 'Transportation Button (reuse-library)', "The button text inside the reuse fragment should be verticalized.");
+					oResourceBundleLoadedDeferred.resolve();
+				});
 
 				// Render nested component
 				var oToolPage = oComponent.getRootControl().getContent()[0];
@@ -51,15 +58,18 @@ sap.ui.require([
 					assert.equal(sap.ui.getCore().byId("reuseComponent---reuseDefaultView--reuseCommonView--reuse_component_common_label").getText(), 'I am a Transportation text (reuse-library)', "The label text inside the reuse library of the reuse component should be verticalized.");
 					assert.equal(sap.ui.getCore().byId("reuseComponent---reuseDefaultView--reuseCommonView--reuse_component_common_button").getText(), 'Transportation Button (reuse-library)', "The button text inside the reuse library of the reuse component should be verticalized.");
 
-					// cleanup
-					oContainer.destroy();
-					done();
+					oComponentCreatedDeferred.resolve();
 				});
 				oNavContainer.to(oNestedComponentPage);
 			}
 		});
 
 		oContainer.placeAt("qunit-fixture");
+
+		return Promise.all([oComponentCreatedDeferred.promise, oResourceBundleLoadedDeferred.promise]).then(function() {
+			// cleanup
+			oContainer.destroy();
+		});
 	});
 
 });
