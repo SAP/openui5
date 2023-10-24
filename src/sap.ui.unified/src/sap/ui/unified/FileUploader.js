025b380f575d471c11b6a151b1941b470647ba02
Boyan Rakilovski <boyan.rakilovski@sap.com>
2023-10-24 12:04:46 +0300

    [FIX] sap.ui.unified.FileUploader: remove redundant browse button type
    Issue:
    - A type attribute was set to the browse button element,
    in order to avoid form submit when user presses over
    a referenced to the uploader element label.
    
    Solution:
    - The native semantics of a button element is sufficient.
    From submit would not get triggered on referenced label press
    as the corresponding event is prevented.
    
    BCP: 2370122194
    Change-Id: I5f9dc1eb505898ef38e1b6c674b0a26f587014d7

--- src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
+++ src/sap.ui.unified/src/sap/ui/unified/FileUploader.js
@@ -2074,7 +2074,6 @@ sap.ui.define([
 
 		if (this.oBrowse &&  this.oBrowse.$().length) {
 			$browse = this.oBrowse.$();
-			$browse.attr("type', 'button"); // The default type of button is submit that's why on click of label there are submit of the form. This way we are avoiding the submit of form.
 
 			$browse.off("click").on("click", (oEvent) => {
 				oEvent.preventDefault();
