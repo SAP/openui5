41a889685707129482a9fc63cf4929d985aced56
Boyan Rakilovski <boyan.rakilovski@sap.com>
2023-10-25 12:03:50 +0300

    [FIX] sap.m.PlanningCalendar: adjust rows accessibility semantics
    Issue:
    - The Axe accessibility tool reports that an element
    with role "row" doesn't have a parent element with a
    proper role attribute.
    
    Solution:
    - The "row" role is now removed is there is no need
    to provide additional accessibility semantics in this
    case.
    
    BCP: 2370087919
    Change-Id: I4180010d80fa9af4d677ff554c0c8b9d6f97ba52

--- src/sap.ui.unified/src/sap/ui/unified/CalendarRowRenderer.js
+++ src/sap.ui.unified/src/sap/ui/unified/CalendarRowRenderer.js
@@ -82,9 +82,6 @@ sap.ui.define([
 		}
 
 	//		var rb = sap.ui.getCore().getLibraryResourceBundle("sap.ui.unified");
-		oRm.accessibilityState(oRow, {
-			role: "row"
-		});
 		oRm.openEnd(); // div element
 
 		this.renderAppointmentsRow(oRm, oRow, aTypes);
