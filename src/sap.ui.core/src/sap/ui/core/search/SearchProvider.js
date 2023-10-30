42ad51dbdf02c768e3cf034720e82a4ce2377ee8
Florian Vogt <florian.vogt@sap.com>
2023-10-30 11:41:50 +0100

    [FEATURE] Deprecation of sap/ui/core/search classes
    - sap/ui/core/search/SearchProvider
    - sap/ui/core/search/OpenSearchProvider
    
    demo-kit-feedback: 4667
    Change-Id: I339f44748a4ca440702069fda9fb8b7425f958df

--- src/sap.ui.core/src/sap/ui/core/search/SearchProvider.js
+++ src/sap.ui.core/src/sap/ui/core/search/SearchProvider.js
@@ -22,6 +22,7 @@ sap.ui.define(['sap/ui/core/Element', "sap/base/Log", 'sap/ui/core/library'],
 	 * Do not create instances of this class, but use a concrete subclass instead.
 	 * @extends sap.ui.core.Element
 	 * @version ${version}
+	 * @deprecated since 1.120
 	 *
 	 * @public
 	 * @alias sap.ui.core.search.SearchProvider
