d7230f682f78307938bbd798d035447bf0462d63
Saurabh Patel <saurabh.patel@sap.com>
2023-10-30 14:39:12 +0530

    [INTERNAL] UploadSetwithTable test fix, when executing with Karma Runner
    BCP: 2370126082
    Change-Id: I2f044489aa4177a66685e0abc7cbca401ac90204

--- src/sap.m/test/sap/m/qunit/upload/FilePreviewDialog.qunit.js
+++ src/sap.m/test/sap/m/qunit/upload/FilePreviewDialog.qunit.js
@@ -1,13 +1,12 @@
 /*global QUnit*/
 sap.ui.define([
 	"sap/m/upload/UploadSetwithTable",
-	"sap/m/upload/UploadSetwithTableItem",
-	"sap/ui/core/Element",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/Core",
 	"sap/m/upload/FilePreviewDialog",
-	"./UploadSetwithTableTestUtils"
-], function(UploadSetwithTable, UploadSetwithTableItem, Element, JSONModel, oCore, FilePreviewDialog, TestUtils) {
+	"./UploadSetwithTableTestUtils",
+	"sap/ui/thirdparty/jquery"
+], function (UploadSetwithTable, JSONModel, oCore, FilePreviewDialog, TestUtils, jQuery) {
 	"use strict";
 
 
@@ -35,7 +34,7 @@ sap.ui.define([
 
 		this.oUploadSetwithTable.setPreviewDialog(associatedControl);
 
-		const oAssociatedControlRef = Element.getElementById(this.oUploadSetwithTable.getPreviewDialog());
+		const oAssociatedControlRef = oCore.byId(this.oUploadSetwithTable.getPreviewDialog());
 		assert.ok(oAssociatedControlRef, "FilePreviewDialog Instance created successfully");
 	});
 
@@ -44,7 +43,7 @@ sap.ui.define([
 
 		this.oUploadSetwithTable.setPreviewDialog(associatedControl);
 
-		const oAssociatedControlRef = Element.getElementById(this.oUploadSetwithTable.getPreviewDialog());
+		const oAssociatedControlRef = oCore.byId(this.oUploadSetwithTable.getPreviewDialog());
 		assert.equal(oAssociatedControlRef.getShowCarouselArrows(), true, "showCarouselArrows is true by default");
 		oAssociatedControlRef.setShowCarouselArrows(false);
 		assert.equal(oAssociatedControlRef.getShowCarouselArrows(), false, "showCarouselArrows is set to false");
@@ -79,74 +78,102 @@ sap.ui.define([
 		oCore.applyChanges();
 	});
 
-	QUnit.test("Test for supported media type and Illustrated Messages", function (assert) {
-		var fnDone = assert.async();
-
-		var oItem = this.oUploadSetwithTable.getItems()[0];
-		const oAssociatedPreviewDialog = new FilePreviewDialog();
-		oAssociatedPreviewDialog.attachEventOnce("beforePreviewDialogOpen", function (oEvent) {
-			var aItems = oEvent.getSource()._items;
-			var oPreviewDialog = oEvent.getSource()._oDialog;
-			var oCarousel = oPreviewDialog.getContent()[0];
-			var aPages = oCarousel.getPages();
-
-			oPreviewDialog.attachEventOnce("afterOpen",function(oEvent){
-
-			for (let i = 0; i < aPages.length; i++) {
-				assert.equal(oPreviewDialog.getTitle(), aItems[i].getFileName(), "First previewed document's title is correct on the dialog");
-				assert.equal(oCarousel.getActivePage(), aPages[i].getId(), "First previewed document's title is correct on the dialog");
-
-				switch (aItems[i].getMediaType()) {
-					case "application/msword":
-						assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
-						assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
-						assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
-						break;
-					case "application/vnd.ms-excel":
-						assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
-						assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
-						assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
-						break;
-					case "video/mp4":
-						assert.equal(aPages[i].getContent(), "<video controls width='100%' height='100%' src='uploadSetTableDemo/SampleFiles/Video.mp4'>", "IllustrationType for not supported file type is correct");
-						break;
-					case "text/plain":
-						assert.equal(aPages[i].getEditorType(), "TinyMCE6", "Editor type is correct for media type text/plain on page");
-						assert.ok(aPages[i].getButtonGroups(), "Group of buttons are present for the editor");
-						break;
-					case "image/png":
-						assert.equal(aPages[i].getSrc(), aItems[i].getUrl(), "Source of the media type image is same as expected");
-						break;
-					case  "image/gif":
-						assert.equal(aPages[i].getSrc(), aItems[i].getUrl(), "Source of the media type image is same as expected");
-						break;
-					case "application/zip":
-						assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
-						assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
-						assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
-						break;
-					case "model/vnd.sap.vds":
-						assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
-						assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
-						assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
-						break;
-					default:
-						break;
+	return oCore.loadLibrary("sap.suite.ui.commons", { async: true })
+		.then(function (assert) {
+			QUnit.module("Supported media type", {
+				beforeEach: function () {
+					this.$RootNode = jQuery(document.body);
+					this.oUploadSetwithTable = new UploadSetwithTable("UploadSetwithTable", {
+						items: {
+							path: "/items",
+							template: TestUtils.createItemTemplate(),
+							templateShareable: false
+						}
+					}).setModel(new JSONModel(TestUtils.getData()));
+					this.oUploadSetwithTable.placeAt("qunit-fixture");
+					oCore.applyChanges();
+				},
+				afterEach: function () {
+					this.oUploadSetwithTable.destroy();
+					this.oUploadSetwithTable = null;
 				}
-				if (i < aPages.length - 1) {
-					oCarousel._changeActivePage(i + 1);
-				}
-			}
+			});
+			QUnit.test("Test for supported media type and Illustrated Messages", function (assert) {
+				var fnDone = assert.async();
 
-			var oBeginButton = oPreviewDialog.getButtons()[1];
-			oBeginButton.firePress();
-			fnDone();
+				var oItem = this.oUploadSetwithTable.getItems()[0];
+				const oAssociatedPreviewDialog = new FilePreviewDialog();
+				oAssociatedPreviewDialog.attachEventOnce("beforePreviewDialogOpen", function (oEvent) {
+					var aItems = oEvent.getSource()._items;
+					var oPreviewDialog = oEvent.getSource()._oDialog;
+					var oCarousel = oPreviewDialog.getContent()[0];
+					var aPages = oCarousel.getPages();
+
+					oPreviewDialog.attachEventOnce("afterOpen", function (oEvent) {
+
+						for (let i = 0; i < aPages.length; i++) {
+							assert.equal(oPreviewDialog.getTitle(), aItems[i].getFileName(), "First previewed document's title is correct on the dialog");
+							assert.equal(oCarousel.getActivePage(), aPages[i].getId(), "First previewed document's title is correct on the dialog");
+
+							switch (aItems[i].getMediaType()) {
+								case "application/msword":
+									assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
+									assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
+									assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
+									break;
+								case "application/vnd.ms-excel":
+									assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
+									assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
+									assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
+									break;
+								case "video/mp4":
+									assert.equal(aPages[i].getContent(), "<video controls width='100%' height='100%' src='uploadSetTableDemo/SampleFiles/Video.mp4'>", "IllustrationType for not supported file type is correct");
+									break;
+								case "text/plain":
+									assert.equal(aPages[i].getEditorType(), "TinyMCE6", "Editor type is correct for media type text/plain on page");
+									assert.ok(aPages[i].getButtonGroups(), "Group of buttons are present for the editor");
+									break;
+								case "image/png":
+									assert.equal(aPages[i].getSrc(), aItems[i].getUrl(), "Source of the media type image is same as expected");
+									break;
+								case "image/gif":
+									assert.equal(aPages[i].getSrc(), aItems[i].getUrl(), "Source of the media type image is same as expected");
+									break;
+								case "application/zip":
+									assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
+									assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
+									assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
+									break;
+								case "model/vnd.sap.vds":
+									assert.equal(aPages[i].getTitle(), aItems[i].getFileName(), "Title is correct for file on page");
+									assert.equal(aPages[i].getDescription(), 'Preview not available for this file.', "Description for the page is correct on page");
+									assert.equal(aPages[i].getIllustrationType(), 'sapIllus-NoData', "IllustrationType for not supported file type is correct");
+									break;
+								default:
+									break;
+							}
+							if (i < aPages.length - 1) {
+								oCarousel._changeActivePage(i + 1);
+							}
+						}
+
+						var oBeginButton = oPreviewDialog.getButtons()[1];
+						oBeginButton.firePress();
+						fnDone();
+					});
+
+				});
+
+				this.oUploadSetwithTable.setPreviewDialog(oAssociatedPreviewDialog);
+				this.oUploadSetwithTable._openFilePreview(oItem);
+				oCore.applyChanges();
 			});
 
-		});
-
-		this.oUploadSetwithTable.setPreviewDialog(oAssociatedPreviewDialog);
-		this.oUploadSetwithTable._openFilePreview(oItem);
-		oCore.applyChanges();
+		})
+		.catch(function () {
+			QUnit.module("Supported media type");
+			QUnit.test("supported media type is not present", function (assert) {
+				assert.ok(true, "supported media type is not available for this file.");
+			});
 	});
 });
