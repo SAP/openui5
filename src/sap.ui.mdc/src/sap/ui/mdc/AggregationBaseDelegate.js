e44a0ba910f4126d3ab42e21d417333e940a4c61
Martin Haeuser <martin.haeuser@sap.com>
2023-11-14 13:42:48 +0100

    [INTERNAL] MDC: AggregationBaseDelegate docu refinement
    Change-Id: Ibcfa7e692160c53c3b930edd430bf00f69adf6d2

--- src/sap.ui.mdc/src/sap/ui/mdc/AggregationBaseDelegate.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/AggregationBaseDelegate.js
@@ -26,15 +26,16 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 	 * @since 1.82.0
 	 * @alias module:sap/ui/mdc/AggregationBaseDelegate
 	 * @extends module:sap/ui/mdc/BaseDelegate
+         * @abstract
 	 */
 	const AggregationBaseDelegate = Object.assign({}, BaseDelegate);
 
 	/**
 	 * Retrieves the relevant metadata for a given payload and returns the property info array.
+         * <br>By default, this method returns a <code>Promise</code> that resolves into <code>[]</code>.
 	 *
 	 * @param {sap.ui.mdc.Control} oControl Instance of an <code>sap.ui.mdc.Control</code>
 	 * @returns {Promise<object[]>} Once resolved, an array of property info objects is returned
-	 *
 	 * @public
 	 */
 	AggregationBaseDelegate.fetchProperties = function(oControl) {
@@ -42,21 +43,18 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 	};
 
 	/**
-	 * Creates an instance of the implementing MDC Control's default aggregation.
+	 * Central hook to add items to the provided control instance.
+	 * This method must return a promise that resolves with an instance of the implementing {@link sap.ui.mdc.Control Control} default aggregation.
 	 *
-	 * <b>Note:</b> The <code>addItem</code> hook can be used during the processing of an SAPUI5 flexibility change.
 	 * Consequently the parameter <code>mPropertyBag</code> is only passed during preprocessing. In runtime scenarios (such as opening a personalization dialog), this
 	 * method might be called without the parameter <code>mPropertyBag</code>.
 	 *
-	 * @param {sap.ui.mdc.Control} oControl Instance of an <code>sap.ui.mdc.Control</code>
-	 * @param {string} sPropertyName The name of the property info object/JSON
+	 * @param {sap.ui.mdc.Control|Element} oControl Instance of an <code>sap.ui.mdc.Control</code>.
+	 * <b>Note:</b> The <code>addItem</code> hook can be used during the processing of an SAPUI5 flexibility change.
+	 * @param {string} sPropertyName The name of the property info object
 	 * @param {Object} [mPropertyBag] Instance of property bag from SAPUI5 flexibility change API
 	 *
-	 * @returns {Promise} Promise that resolves with an instance of the implementing {@link sap.ui.mdc.Control Control} default aggregation.
-	 * <b>Note:</b>
-	 * This method always requires a return value once it has been called. If an item for a given property <code>sPropertyName</code>
-	 * has already been created, it is required to either return the existing instance or create a new instance.
-	 *
+	 * @abstract
 	 * @public
 	 */
 	AggregationBaseDelegate.addItem = function (oControl, sPropertyName, mPropertyBag) {
@@ -66,16 +64,18 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 	/**
 	 * Triggers any necessary follow-up steps that need to be taken after the removal of created items via <code>removeItem</code>.
 	 * The returned Boolean value inside the <code>Promise</code> can be used to prevent the default follow-up behavior of the SAPUI5 flexibility handling.
+	 * <br>By default, this method returns a <code>Promise</code> that resolves into <code>true</code>. This will ensure, that the item will be destroyed subsequentially after it has been removed
+	 * by the control.
 	 *
 	 * <b>Note:</b> The <code>removeItem</code> hook can be used during the processing of an SAPUI5 flexibility change.
 	 * Consequently the parameter <code>mPropertyBag</code> is only passed during preprocessing. In runtime scenarios (such as opening a personalization dialog), this
 	 * method might be called without the parameter <code>mPropertyBag</code>.
 	 *
-	 * @param {sap.ui.mdc.Control} oControl Instance of an <code>sap.ui.mdc.Control</code>
+	 * @param {sap.ui.mdc.Control|Element} oControl Instance of a <code>sap.ui.mdc.Control</code>
 	 * @param {sap.ui.core.Element} oItem The control instance that was removed
 	 * @param {Object} [mPropertyBag] Instance of property bag from SAPUI5 flexibility
 	 *
-	 * @returns {Promise} Promise that resolves with <code>true</code>, <code>false</code> to allow/prevent default behavior of the change
+	 * @returns {Promise<Boolean>} Promise resolving in a boolean, deciding whether the item should be destroyed (<code>true</code>) or kept (<code>false</code>) after removing it from the control aggregation.
 	 *
 	 * @public
 	 */
@@ -85,9 +85,10 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 
 	/**
 	 * A validator to evaluate the theoretical control state.
+	 * <br>By default, no state validation will be triggered.
 	 *
 	 * @param {sap.ui.mdc.Control} oControl Instance of an <code>sap.ui.mdc.Control</code>
-	 * @param {Object} oState The theoretical external state representation of an MDC control. The representation of this format is similar as processed by {@link sap.ui.mdc.p13n.StateUtil StateUtil}
+	 * @param {Object} oState The theoretical external state representation of a mdc control. The representation of this format is similar as processed by {@link sap.ui.mdc.p13n.StateUtil StateUtil}
 	 * @returns {Object} An object that must contain at least the <code>validation</code> attribute {@link sap.ui.core.MessageType MessageType}.
 	 * If <code>warning</code> or <code>error</code> state types have been provided, the <code>message</code> is shown in addition.
 	 *
@@ -107,9 +108,10 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 	/**
 	 * Hook that will be executed when changes are done applying to controls during the XML flexibility change appliance process.
 	 *
-	 * @param {Object<sap.ui.mdc.Control>} oControl Instance of an MDC control
+	 * @param {Element} oControl XML node of a mdc control
 	 * @param {Object} mPropertyBag Property bag from SAPUI5 flexibility
 	 *
+	 * @abstract
 	 * @public
 	 */
 	AggregationBaseDelegate.onAfterXMLChangeProcessing = function(oControl, mPropertyBag) {
@@ -117,23 +119,25 @@ sap.ui.define(['sap/ui/mdc/BaseDelegate', 'sap/ui/core/library'], function (Base
 	};
 
 	/**
-	 * A validator to evaluate the state of an MDC control.
+	 * A validator to evaluate the state of a mdc control.
 	 *
-	 * @param {sap.ui.mdc.Control} oControl Instance of a MDC control
-	 * @param {map} [mValidation] Object Describing the validation result
+	 * @param {sap.ui.mdc.Control} oControl Instance of a mdc control
+	 * //TODO: available validation states? Only FilterBar relevant?
 	 *
+	 * @abstract
 	 * @public
 	 */
 	AggregationBaseDelegate.determineValidationState = function(oControl) {
-		return oControl.checkValidationState ? oControl.checkValidationState() : -1;
+		return -1;
 	};
 
 	/**
-	 * Visualizes the validation state of an MDC control.
+	 * Visualizes the validation state of a mdc control.
 	 *
-	 * @param {Object<sap.ui.mdc.Control>} oControl Instance of a MDC control
-	 * @returns {map} mValidation Describes the validation result.
+	 * @param {sap.ui.mdc.Control} oControl Instance of a mdc control
+	 * @param {object} [mValidation] Object Describing the validation result
 	 *
+	 * @abstract
 	 * @public
 	 */
 	AggregationBaseDelegate.visualizeValidationState = function(oControl, mValidation) {
