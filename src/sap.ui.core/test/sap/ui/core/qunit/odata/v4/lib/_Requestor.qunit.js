34818aa9df6563c85219a87d28c51448e9037d13
Lars Kissel <lars.kissel@sap.com>
2023-11-20 19:00:32 +0100

    [INTERNAL] sap.ui.model.odata.v4.lib._Requestor#addChangeSet: do not auto-submit a created batch queue
    PS1: downport
    
    BCP: 2370151708
    CR-Id: 002075125900003393662023
    Change-Id: I5f93eac8bc9f44e0bdb0e5fdce86168957e45170
    (cherry picked from commit cd85233717e5d5ffb27da10f63d5abc262edd8a0)

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Requestor.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Requestor.qunit.js
@@ -4079,6 +4079,9 @@ sap.ui.define([
 			assert.strictEqual(oBatchQueue0[0].iSerialNumber, 0);
 		}
 
+		const oModelInterfaceMock = this.mock(oModelInterface);
+		oModelInterfaceMock.expects("onCreateGroup").withExactArgs("group");
+
 		// code under test
 		aBatchQueue = oRequestor.getOrCreateBatchQueue("group");
 
@@ -4087,10 +4090,15 @@ sap.ui.define([
 		// code under test
 		assert.strictEqual(oRequestor.getOrCreateBatchQueue("group"), aBatchQueue);
 
-		this.mock(oModelInterface).expects("onCreateGroup").withExactArgs("group2");
+		oModelInterfaceMock.expects("onCreateGroup").withExactArgs("group2");
 
 		// code under test
 		checkBatchQueue(oRequestor.getOrCreateBatchQueue("group2"), "group2");
+
+		oModelInterfaceMock.expects("onCreateGroup").withExactArgs("group3").never();
+
+		// code under test: create queue without informing the model
+		checkBatchQueue(oRequestor.getOrCreateBatchQueue("group3", true), "group3");
 	});
 
 	//*****************************************************************************************
@@ -4110,7 +4118,7 @@ sap.ui.define([
 			aRequests = [aChangeSet0, oGetRequest];
 
 		aRequests.iChangeSet = 0;
-		this.mock(oRequestor).expects("getOrCreateBatchQueue").withExactArgs("group")
+		this.mock(oRequestor).expects("getOrCreateBatchQueue").withExactArgs("group", true)
 			.returns(aRequests);
 		this.mock(oRequestor).expects("getSerialNumber").withExactArgs().returns(42);
 
