8c53f6e762c0f11bef03dc78e92b40b2654cfdeb
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2023-12-11 12:27:27 +0100

    [INTERNAL] sap.ui.model.odata.v4.lib._Cache#create: bKeepReportedMessagesPath
    (cherry picked from commit 5d7b96100114cdd82d16eca35f5e0c1bd075fe26)
    
    Change-Id: I99170517f99c6b21c70a8d8cbaef2125595fe251
    BCP: 2380140928
    CR-Id: 002075125900003946022023

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Cache.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/lib/_Cache.qunit.js
@@ -8585,7 +8585,7 @@ sap.ui.define([
 			this.mock(oCountChangeListener).expects("onChange");
 			this.mock(oCache).expects("visitResponse")
 				.withExactArgs(sinon.match.same(oPostResult), sinon.match.same(mTypeForMetaPath),
-					"/TEAMS/TEAM_2_EMPLOYEES", sPathInCache + sTransientPredicate);
+					"/TEAMS/TEAM_2_EMPLOYEES", sPathInCache + sTransientPredicate, undefined, true);
 			// simulate a lack of key predicate => the transient predicate is kept
 			oHelperMock.expects("getPrivateAnnotation")
 				.withExactArgs(sinon.match.same(oPostResult), "predicate")
@@ -9108,7 +9108,7 @@ sap.ui.define([
 			.returns({});
 		oCacheMock.expects("visitResponse")
 			.withExactArgs(sinon.match.same(oPostResult), sinon.match.same(mTypeForMetaPath),
-				"/Employees", sTransientPredicate);
+				"/Employees", sTransientPredicate, undefined, true);
 		oHelperMock.expects("updateSelected")
 			.withExactArgs(sinon.match.same(oCache.mChangeListeners), sTransientPredicate,
 				sinon.match.same(oCache.aElements[0]), sinon.match.same(oPostResult), undefined,
