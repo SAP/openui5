05562254388bd8ff27e65a96c95023aad50fd186
Jan Mummenthaler <jan.mummenthaler@sap.com>
2023-12-13 15:46:25 +0100

    [FIX] sap/ui/core/message/MessageMixin: Do not refresh MessageModel when control id already added to a Message
    BCP: 2370097718
    Change-Id: Id38e6db306a7ab077ede41ce43eb15ce2654b251

--- src/sap.ui.core/src/sap/ui/core/message/MessageMixin.js
+++ src/sap.ui.core/src/sap/ui/core/message/MessageMixin.js
@@ -56,7 +56,7 @@ sap.ui.define(["sap/ui/core/Element", "sap/ui/core/library", "sap/base/Log", "sa
 						);
 					}
 				}
-				if (oMessage.getControlId() !== this.getId()){
+				if (!oMessage.getControlIds().includes(this.getId())){
 					oMessage.addControlId(this.getId());
 					bForceUpdate = true;
 				}
