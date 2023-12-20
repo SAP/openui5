b3f7b285346f5b910d16d4620c5d213b9c4fd75d
Martin Haeuser <martin.haeuser@sap.com>
2023-12-20 11:42:33 +0100

    [INTERNAL] SortController, GroupController: fix position issues
    * added tests for SortController and GroupController
    * fix a bug in creating move changes for sorting/grouping
    
    SNOW: DINC0017980
    CR-Id: 002075125900004190522023
    Change-Id: Iebd20e79838a762c4b5a7bb09bb08164993b4197

--- src/sap.m/src/sap/m/p13n/GroupController.js
+++ src/sap.m/src/sap/m/p13n/GroupController.js
@@ -87,27 +87,7 @@ sap.ui.define([
         return oAddRemoveChange;
     };
 
-    GroupController.prototype._createMoveChange = function(sId, sPropertykey, iNewIndex, sMoveOperation, oControl, bPersistId){
-        var oMoveChange =  {
-            selectorElement: oControl,
-            changeSpecificData: {
-                changeType: sMoveOperation,
-                content: {
-                    id: sId,
-                    key: sPropertykey,
-                    index: iNewIndex
-                }
-            }
-        };
-
-        if (!bPersistId) {
-            delete oMoveChange.changeSpecificData.content.id;
-        }
-
-        return oMoveChange;
-    };
-
-    GroupController.prototype.mixInfoAndState = function(oPropertyHelper) {
+	GroupController.prototype.mixInfoAndState = function(oPropertyHelper) {
 
         var aItemState = this.getCurrentState();
         var mItemState = this.arrayToMap(aItemState);
--- src/sap.m/src/sap/m/p13n/SortController.js
+++ src/sap.m/src/sap/m/p13n/SortController.js
@@ -70,7 +70,8 @@ sap.ui.define([
     SortController.prototype.getChangeOperations = function() {
         return {
             add: "addSort",
-            remove: "removeSort"
+            remove: "removeSort",
+            move: "moveSort"
         };
     };
 
@@ -90,26 +91,6 @@ sap.ui.define([
             }
         };
         return oAddRemoveChange;
-    };
-
-    SortController.prototype._createMoveChange = function(sId, sPropertykey, iNewIndex, sMoveOperation, oControl, bPersistId){
-        var oMoveChange =  {
-            selectorElement: oControl,
-            changeSpecificData: {
-                changeType: sMoveOperation,
-                content: {
-                    id: sId,
-                    key: sPropertykey,
-                    index: iNewIndex
-                }
-            }
-        };
-
-        if (!bPersistId) {
-            delete oMoveChange.changeSpecificData.content.id;
-        }
-
-        return oMoveChange;
     };
 
     SortController.prototype._getPresenceAttribute = function(bexternalAppliance){
