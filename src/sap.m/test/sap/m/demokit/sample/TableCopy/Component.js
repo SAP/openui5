8f37f0f037d28390120a13425ba70ee6110de47f
Cahit Guerguec <cahit.guerguec@sap.com>
2023-12-28 12:01:22 +0100

    [INTERNAL] CopyProvider: MIME type support with ClipboardItem
     - Documentation for text/html and text/plain MIME types
     - Demokit example is added
    
    Jira: CPOUIFTEAMB-2274
    
    Change-Id: Id1067705fe59845e80430514d3c6ab8efcc76419

---
+++ src/sap.m/test/sap/m/demokit/sample/TableCopy/Component.js
@@ -0,0 +1,13 @@
+sap.ui.define(['sap/ui/core/UIComponent'],
+	function(UIComponent) {
+	"use strict";
+
+	const Component = UIComponent.extend("sap.m.sample.TableCopy.Component", {
+		metadata : {
+			manifest: "json"
+		}
+	});
+
+	return Component;
+
+});
