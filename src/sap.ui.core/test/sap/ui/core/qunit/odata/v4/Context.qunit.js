4ee113ac0e578aba23f974569ce374c5771162df
Alexander Rieß <alexander.mathias.riess@sap.com>
2024-01-04 19:55:12 +0100

    [INTERNAL] sap.ui.model.odata.v4.Context#setSelected: without key predicate
    PS1: cherry pick
    
    Change-Id: Ice97d2b02d4b5efdea86cc0253b70ab011ee049b
    SNOW: DINC0029552
    CR-Id: 002075125900000239222024
    (cherry picked from commit 867293bc1f9596775bc1261a641478ce2a3e8f35)

--- src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/Context.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/odata/v4/Context.qunit.js
@@ -4463,7 +4463,12 @@ sap.ui.define([
 	});
 
 	//*********************************************************************************************
-	QUnit.test("isEffectivelyKeptAlive: $$ownRequest", function (assert) {
+[
+	"/TEAMS('1')/TEAM_2_EMPLOYEES('2')",
+	"/TEAMS/0/TEAM_2_EMPLOYEES('2')"
+].forEach(function (sContextPath) {
+	const sTitle = "isEffectivelyKeptAlive: $$ownRequest, context path: " + sContextPath;
+	QUnit.test(sTitle, function (assert) {
 		var oBinding = {
 				checkKeepAlive : function () {},
 				getHeaderContext : function () {},
@@ -4471,7 +4476,7 @@ sap.ui.define([
 				isRelative : function () { return true; },
 				mParameters : {$$ownRequest : true}
 			},
-			oContext = Context.create({/*oModel*/}, oBinding, "/TEAMS('1')/TEAM_2_EMPLOYEES('2')");
+			oContext = Context.create({/*oModel*/}, oBinding, sContextPath);
 
 		oContext.setSelected(true);
 		this.mock(_Helper).expects("isDataAggregation")
@@ -4480,6 +4485,7 @@ sap.ui.define([
 		// code under test
 		assert.strictEqual(oContext.isEffectivelyKeptAlive(), true);
 	});
+});
 
 	//*********************************************************************************************
 	QUnit.test("isEffectivelyKeptAlive: data aggregation", function (assert) {
@@ -4494,6 +4500,24 @@ sap.ui.define([
 		oContext.setSelected(true);
 		this.mock(_Helper).expects("isDataAggregation")
 			.withExactArgs(sinon.match.same(oBinding.mParameters)).returns(true);
+
+		// code under test
+		assert.strictEqual(oContext.isEffectivelyKeptAlive(), false);
+	});
+
+	//*********************************************************************************************
+	QUnit.test("isEffectivelyKeptAlive: no key predicate", function (assert) {
+		const oBinding = {
+			getHeaderContext : function () {},
+			onKeepAliveChanged : function () {},
+			isRelative : function () { return false; },
+			mParameters : {}
+		};
+		const oContext = Context.create({/*oModel*/}, oBinding, "/SalesOrderList('0')/Messages/0");
+
+		oContext.setSelected(true);
+		this.mock(_Helper).expects("isDataAggregation")
+			.withExactArgs(sinon.match.same(oBinding.mParameters)).returns(false);
 
 		// code under test
 		assert.strictEqual(oContext.isEffectivelyKeptAlive(), false);
