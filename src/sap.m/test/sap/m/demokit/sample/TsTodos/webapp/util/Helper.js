86465767d7bf5f86f1871a7f14735fd91fe16779
Florian Vogt <florian.vogt@sap.com>
2024-01-11 09:26:00 +0100

    [INTERNAL] sap.m.sample.TsTodos: update deps
    Change-Id: I679a67c2a0e060815e066b859d7ef26987d5a4cc

--- src/sap.m/test/sap/m/demokit/sample/TsTodos/webapp/util/Helper.js
+++ src/sap.m/test/sap/m/demokit/sample/TsTodos/webapp/util/Helper.js
@@ -1,5 +1,6 @@
 sap.ui.define([], function () {
   "use strict";
+
   var Helper = {
     resolvePath: function resolvePath(imgPath) {
       var rootPath = sap.ui.require.toUrl("sap/m/sample/TsTodos/webapp");
