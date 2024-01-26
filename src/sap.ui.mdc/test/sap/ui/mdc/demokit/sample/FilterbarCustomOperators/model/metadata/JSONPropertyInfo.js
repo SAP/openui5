a47dc4b6e3ee03db8f5659f7514fe6ba85f9b5f3
Luca Müller <luca.mueller01@sap.com>
2024-01-26 10:06:36 +0100

    [INTERNAL] mdc.Filterbar: demokit sample with custom operatorst push
    PS 1: copy and adapt FilterbarTypes example
    PS 2: remove variant management, clean up imports
    PS 3: rebase
    PS 4: fix description, codeeditor model binding, PropertyInfo key instead of name
    PS 5: fix adapt filters, custom date operator, output for model filter
    PS 6: minor fixes
    PS 7: use Date datatype instead of integer
    PS 8: use type.Date instead of odata.type.Date
    PS 9: remove unused model
    
    Change-Id: I45740bc3145e6323640327033ab8a2178f1cc870
    JIRA: CPOUIFDENVER-1934

---
+++ src/sap.ui.mdc/test/sap/ui/mdc/demokit/sample/FilterbarCustomOperators/model/metadata/JSONPropertyInfo.js
@@ -0,0 +1,81 @@
+sap.ui.define([
+], function() {
+	"use strict";
+
+	/* Property Example:
+	{
+	  "rank": 1,
+	  "name": "Mount Everest",
+	  "height": 8848,
+	  "prominence": 8848,
+	  "range": "Mahalangur Himalaya",
+	  "coordinates": "27°59'17''N 86°55'31''E",
+	  "parent_mountain": true,
+	  "first_ascent": 1953-01-01,
+	  "countries": "Nepal, China"
+	} */
+
+	const aPropertyInfos = [{
+		key: "rank",
+		label: "Rank",
+		visible: true,
+		path: "rank",
+		dataType: "sap.ui.model.type.Integer"
+	},{
+		key: "name",
+		label: "Name",
+		visible: true,
+		path: "name",
+		dataType: "sap.ui.model.type.String"
+	},{
+		key: "height",
+		label: "Height",
+		visible: true,
+		path: "height",
+		dataType: "sap.ui.model.type.Integer"
+	},{
+		key: "prominence",
+		label: "Prominence",
+		visible: true,
+		path: "prominence",
+		dataType: "sap.ui.model.type.Float"
+	},{
+		key: "range",
+		label: "Range",
+		visible: true,
+		path: "range",
+		dataType: "sap.ui.model.type.String"
+	},{
+		key: "coordinates",
+		label: "Coordinates",
+		visible: true,
+		path: "coordinates",
+		dataType: "sap.ui.model.type.String"
+	},{
+		key: "parent_mountain",
+		label: "Parent Mountain",
+		visible: true,
+		path: "parent_mountain",
+		dataType: "sap.ui.model.type.Boolean"
+	},{
+		key: "first_ascent",
+		label: "First Ascent",
+		visible: true,
+		path: "first_ascent",
+		dataType: "sap.ui.model.type.Date"
+	},{
+		key: "countries",
+		label: "Countries",
+		visible: true,
+		path: "countries",
+		dataType: "sap.ui.model.type.String"
+	},{
+		key: "$search",
+		label: "Search",
+		visible: true,
+		maxConditions: 1,
+		dataType: "sap.ui.model.type.String"
+	}];
+
+	return aPropertyInfos;
+}, /* bExport= */false);
