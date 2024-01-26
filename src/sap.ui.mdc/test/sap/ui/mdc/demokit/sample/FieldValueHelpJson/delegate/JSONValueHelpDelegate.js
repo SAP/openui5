2ec2f132e6c40eb2454da16d495c3f68a950fc1c
Luca Müller <luca.mueller01@sap.com>
2024-01-26 13:48:33 +0100

    [INTERNAL] ValueHelp: update VH demokit sample to fit tutorial
    PS 1: update coding from tutorial
    PS 2: fix search using delegate
    PS 3: rebase
    PS 4: use current version of tutorial
    PS 5: fix check for table type
    PS 6: slight improvements
    PS 7: valueHelp as dependent of Filterbar
    
    Change-Id: I53a115186db2d5e9991d44ac4bcd09489a7b286a
    JIRA: CPOUIFDENVER-1934

---
+++ src/sap.ui.mdc/test/sap/ui/mdc/demokit/sample/FieldValueHelpJson/delegate/JSONValueHelpDelegate.js
@@ -0,0 +1,84 @@
+sap.ui.define([
+	"sap/ui/mdc/ValueHelpDelegate",
+	"sap/ui/mdc/p13n/StateUtil",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/mdc/condition/Condition",
+	"sap/ui/mdc/enums/ConditionValidated"
+], function (
+	ValueHelpDelegate,
+	StateUtil,
+	Filter,
+	FilterOperator,
+	Condition,
+	ConditionValidated
+) {
+	"use strict";
+
+	const JSONValueHelpDelegate = Object.assign({}, ValueHelpDelegate);
+
+	JSONValueHelpDelegate.createConditionPayload = function (oValueHelp, oContent, aValues, oContext) {
+		const sIdentifier = oContent.getId();
+		const oConditionPayload = {};
+		oConditionPayload[sIdentifier] = [];
+
+		if (oContent.sId.endsWith("locationTypeAhead")) {
+			if (oContext) {
+				oConditionPayload[sIdentifier].push({
+					countryId: oContext.getProperty("countryId")
+				});
+			}
+		}
+		return oConditionPayload;
+	};
+
+	// This delegate method sets the out-parameter for the "Country"
+	JSONValueHelpDelegate.onConditionPropagation = function (oValueHelp, sReason, oConfig) {
+		const oFilterBar = oValueHelp.getParent();
+
+		if (sReason !== "ControlChange") {
+			return;
+		}
+
+		// find all conditions carrying country information
+		const aAllConditionCountrysAndRegions = oFilterBar && oFilterBar.getConditions().reduce(function (aResult, oCondition) {
+			if (oCondition.payload) {
+				Object.values(oCondition.payload).forEach(function (aSegments) {
+					aSegments.forEach(function (oSegment) {
+						if (oSegment["countryId"] && aResult.indexOf(oSegment["countryId"]) === -1) {
+							aResult.push(oSegment["countryId"]);
+						}
+					});
+				});
+			}
+			return aResult;
+		}, []);
+
+
+		if (aAllConditionCountrysAndRegions?.length) {
+			StateUtil.retrieveExternalState(oFilterBar).then(function (oState) {
+				let bModify = false;
+				aAllConditionCountrysAndRegions.forEach(function (sCountry) {
+					const bExists = oState.filter && oState.filter['buildingCountry'] && oState.filter['buildingCountry'].find(function (oCondition) {
+						return oCondition.values[0] === sCountry;
+					});
+					if (!bExists) {
+						const oNewConditionCountry = Condition.createCondition("EQ", [sCountry], undefined, undefined, ConditionValidated.Validated);
+						oState.filter['buildingCountry'] = oState.filter && oState.filter['buildingCountry'] || [];
+						oState.filter['buildingCountry'].push(oNewConditionCountry);
+						bModify = true;
+					}
+				});
+
+				if (bModify) {
+					StateUtil.applyExternalState(oFilterBar, oState);
+				}
+			});
+		}
+	};
+
+	return JSONValueHelpDelegate;
+
+}
+
+);
