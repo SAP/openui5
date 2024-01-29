ed33ba066b0f84b33e5b802d8b96becace4afbe4
Diana Petcheva <diana.petcheva@sap.com>
2024-01-29 09:48:08 +0200

    [FEATURE] sap.m.SinglePlanningCalendar: improve anouncement info
    Issue: Announcement info for all-day appoitments anounces the start and
    end times which is unnecessary.
    
    All-day appointments now follow the following patterns:
    (Single Day) All-Day Appointment <Appointment Name> From <Full Date>
    (Multiple Days) All-Day Appointment <Appointment Name> From <Full Date>,
    	To <Full Date>
    
    JIRA: BGSOFUIBALKAN-7751
    Change-Id: I75ce69d5413e28cd78c5cdf1d272dbf92ae6dda3

--- src/sap.m/test/sap/m/qunit/SinglePlanningCalendarGrid.qunit.js
+++ src/sap.m/test/sap/m/qunit/SinglePlanningCalendarGrid.qunit.js
@@ -198,8 +198,8 @@ sap.ui.define([
 			oMockStardDate = UI5Date.getInstance(2019, 7, 5, 10),
 			oMockEndDate = UI5Date.getInstance(2019, 7, 5, 15),
 			// \u202f is a Narrow No-Break Space which has been introduced with CLDR version 43
-			sExpectedInfo = oGrid._oUnifiedRB.getText("CALENDAR_START_TIME") + ": Monday 05/08/2019 at 10:00:00\u202fAM; " +
-				oGrid._oUnifiedRB.getText("CALENDAR_END_TIME") + ": Monday 05/08/2019 at 3:00:00\u202fPM";
+			sExpectedInfo = oGrid._oUnifiedRB.getText("CALENDAR_START_TIME") + ": Monday, August 5, 2019 at 10:00:00\u202fAM; " +
+				oGrid._oUnifiedRB.getText("CALENDAR_END_TIME") + ": Monday, August 5, 2019 at 3:00:00\u202fPM";
 
 		oGrid.placeAt("qunit-fixture");
 		await nextUIUpdate(this.clock);
@@ -219,8 +219,8 @@ sap.ui.define([
 		var oGrid = new SinglePlanningCalendarGrid(),
 			oMockStardDate = UI5Date.getInstance(2019, 7, 5, 10),
 			oMockEndDate = UI5Date.getInstance(2019, 7, 5, 15),
-			sExpectedInfo = oGrid._oUnifiedRB.getText("CALENDAR_START_TIME") + ": Monday 05/08/2019 at 10:00:00; " +
-				oGrid._oUnifiedRB.getText("CALENDAR_END_TIME") + ": Monday 05/08/2019 at 15:00:00";
+			sExpectedInfo = oGrid._oUnifiedRB.getText("CALENDAR_START_TIME") + ": Monday, August 5, 2019 at 10:00:00; " +
+				oGrid._oUnifiedRB.getText("CALENDAR_END_TIME") + ": Monday, August 5, 2019 at 15:00:00";
 
 		oGrid.placeAt("qunit-fixture");
 		await nextUIUpdate(this.clock);
@@ -412,8 +412,8 @@ sap.ui.define([
 		// Prepare
 		var oGrid = new SinglePlanningCalendarGrid();
 
-		assert.strictEqual(oGrid._oFormatStartEndInfoAria.oFormatOptions.pattern.indexOf("EEEE dd/MM/yyyy"), 0, "Start End Aria info pattern is correct (contains 'yyyy' instead of 'YYYY'");
-		assert.strictEqual(oGrid._oFormatAriaFullDayCell.oFormatOptions.pattern.indexOf("EEEE dd/MM/yyyy"), 0, "Full day cell Aria info pattern is correct (contains 'yyyy' instead of 'YYYY'");
+		assert.strictEqual(oGrid._oFormatStartEndInfoAria.oFormatOptions.pattern.indexOf("EEEE, MMMM d, yyyy"), 0, "Start End Aria info pattern is correct (contains 'yyyy' instead of 'YYYY'");
+		assert.strictEqual(oGrid._oFormatAriaFullDayCell.oFormatOptions.pattern.indexOf("EEEE, MMMM d, yyyy"), 0, "Full day cell Aria info pattern is correct (contains 'yyyy' instead of 'YYYY'");
 		assert.strictEqual(oGrid._getDateFormatter().oFormatOptions.pattern, "yyyyMMdd-HHmm", "Grid cell pattern is correct (contains 'yyyy' instead of 'YYYY'");
 
 	});
