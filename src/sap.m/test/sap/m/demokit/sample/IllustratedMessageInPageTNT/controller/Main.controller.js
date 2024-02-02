7f71b8523f22d3dace43bd9f969d4b06faf3a83c
plamenivanov91 <plamen.ivanov01@sap.com>
2024-02-02 13:04:51 +0200

    [FEATURE] sap.m.IllustratedMessage: IllustratedMessageType enum for TNT
    New IllustratedMessageType enumeration introduced for the TNT library.
    
    BCP: 2370107020
    Fixes: https://github.com/SAP/openui5/issues/3823
    Change-Id: Iff98a7e835bdbaf72989dd1b9fe26d38a1bc1a56

--- src/sap.m/test/sap/m/demokit/sample/IllustratedMessageInPageTNT/controller/Main.controller.js
+++ src/sap.m/test/sap/m/demokit/sample/IllustratedMessageInPageTNT/controller/Main.controller.js
@@ -2,8 +2,8 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/Controller",
 	"sap/m/library",
-	"sap/ui/thirdparty/jquery"
-], function (JSONModel, Controller, library, jQuery) {
+	"sap/tnt/IllustratedMessageType"
+], function (JSONModel, Controller, library, IllustratedMessageType) {
 		"use strict";
 
 		var oIllustratedMessageSize = library.IllustratedMessageSize;
@@ -27,8 +27,7 @@ sap.ui.define([
 				this.oModel.setProperty("/sSelectedSize", aIMISizeData[0].key);
 				this.oModel.setProperty("/sSelectedType", aIMITypeDataTemp[0].key);
 
-				this._loadMetadata(sap.ui.require.toUrl('sap/tnt/themes/base/illustrations/metadata.json'),
-				this._populateIllustrationTypes.bind(this));
+				this._populateIllustrationTypes();
 
 				this.getView().setModel(this.oModel);
 			},
@@ -38,23 +37,10 @@ sap.ui.define([
 			onSelectType: function (oEvent) {
 				this.oModel.setProperty("/sSelectedType", oEvent.getParameter("selectedItem").getKey());
 			},
-			_loadMetadata: function(sMetadataPath, fCallBack) {
-
-				return new Promise(function (fnResolve) {
-					jQuery.ajax(sMetadataPath, {
-						type: "GET",
-						dataType: "json",
-						success: function (oMetadataJSON) {
-							fCallBack(oMetadataJSON);
-							fnResolve();
-						}
-					});
-				});
-			},
-			_populateIllustrationTypes: function (oMetadata) {
+			_populateIllustrationTypes: function () {
 				var aIMITypeData = [];
-				oMetadata.symbols.forEach(function (sType) {
-					aIMITypeData.push({key: "tnt-" + sType, text: sType});
+				Object.keys(IllustratedMessageType).forEach(function (sKey) {
+					aIMITypeData.push({key: IllustratedMessageType[sKey], text: sKey});
 				}, this);
 				this.oModel.setProperty("/typeTypes", aIMITypeData);
 				this.oModel.setProperty("/sSelectedType", aIMITypeData[0].key);
---
+++ src/sap.tnt/src/sap/tnt/IllustratedMessageType.js
@@ -0,0 +1,291 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define([], function () {
+	"use strict";
+
+	/**
+	 * Available TNT <code>Illustration</code> types for the {@link sap.m.IllustratedMessage} control.
+	 *
+	 * @enum {string}
+	 * @public
+	 * @alias sap.tnt.IllustratedMessageType
+	 * @since 1.121
+	 */
+	var IllustratedMessageType = {
+
+		/**
+		 * "Avatar" illustration type.
+		 * @public
+		 */
+		Avatar: "tnt-Avatar",
+
+		/**
+		 * "Calculator" illustration type.
+		 * @public
+		 */
+		Calculator: "tnt-Calculator",
+
+		/**
+		 * "ChartArea" illustration type.
+		 * @public
+		 */
+		ChartArea: "tnt-ChartArea",
+
+		/**
+		 * "ChartArea2" illustration type.
+		 * @public
+		 */
+		ChartArea2: "tnt-ChartArea2",
+
+		/**
+		 * "ChartBar" illustration type.
+		 * @public
+		 */
+		ChartBar: "tnt-ChartBar",
+
+		/**
+		 * "ChartBPMNFlow" illustration type.
+		 * @public
+		 */
+		ChartBPMNFlow: "tnt-ChartBPMNFlow",
+
+		/**
+		 * "ChartBullet" illustration type.
+		 * @public
+		 */
+		ChartBullet: "tnt-ChartBullet",
+
+		/**
+		 * "ChartDoughnut" illustration type.
+		 * @public
+		 */
+		ChartDoughnut: "tnt-ChartDoughnut",
+
+		/**
+		 * "ChartFlow" illustration type.
+		 * @public
+		 */
+		ChartFlow: "tnt-ChartFlow",
+
+		/**
+		 * "ChartGantt" illustration type.
+		 * @public
+		 */
+		ChartGantt: "tnt-ChartGantt",
+
+		/**
+		 * "ChartOrg" illustration type.
+		 * @public
+		 */
+		ChartOrg: "tnt-ChartOrg",
+
+		/**
+		 * "ChartPie" illustration type.
+		 * @public
+		 */
+		ChartPie: "tnt-ChartPie",
+
+		/**
+		 * "CodePlaceholder" illustration type.
+		 * @public
+		 */
+		CodePlaceholder: "tnt-CodePlaceholder",
+
+		/**
+		 * "Company" illustration type.
+		 * @public
+		 */
+		Company: "tnt-Company",
+
+		/**
+		 * "Compass" illustration type.
+		 * @public
+		 */
+		Compass: "tnt-Compass",
+
+		/**
+		 * "Components" illustration type.
+		 * @public
+		 */
+		Components: "tnt-Components",
+
+		/**
+		 * "Dialog" illustration type.
+		 * @public
+		 */
+		Dialog: "tnt-Dialog",
+
+		/**
+		 * "ExternalLink" illustration type.
+		 * @public
+		 */
+		ExternalLink: "tnt-ExternalLink",
+
+		/**
+		 * "FaceID" illustration type.
+		 * @public
+		 */
+		FaceID: "tnt-FaceID",
+
+		/**
+		 * "Fingerprint" illustration type.
+		 * @public
+		 */
+		Fingerprint: "tnt-Fingerprint",
+
+		/**
+		 * "Handshake" illustration type.
+		 * @public
+		 */
+		Handshake: "tnt-Handshake",
+
+		/**
+		 * "Help" illustration type.
+		 * @public
+		 */
+		Help: "tnt-Help",
+
+		/**
+		 * "Lock" illustration type.
+		 * @public
+		 */
+		Lock: "tnt-Lock",
+
+		/**
+		 * "Mission" illustration type.
+		 * @public
+		 */
+		Mission: "tnt-Mission",
+
+		/**
+		 * "MissionFailed" illustration type.
+		 * @public
+		 */
+		MissionFailed: "tnt-MissionFailed",
+
+		/**
+		 * "NoApplications" illustration type.
+		 * @public
+		 */
+		NoApplications: "tnt-NoApplications",
+
+		/**
+		 * "NoFlows" illustration type.
+		 * @public
+		 */
+		NoFlows: "tnt-NoFlows",
+
+		/**
+		 * "NoUsers" illustration type.
+		 * @public
+		 */
+		NoUsers: "tnt-NoUsers",
+
+		/**
+		 * "Radar" illustration type.
+		 * @public
+		 */
+		Radar: "tnt-Radar",
+
+		/**
+		 * "RoadMap" illustration type.
+		 * @public
+		 */
+		RoadMap: "tnt-RoadMap",
+
+		/**
+		 * "Secrets" illustration type.
+		 * @public
+		 */
+		Secrets: "tnt-Secrets",
+
+		/**
+		 * "Services" illustration type.
+		 * @public
+		 */
+		Services: "tnt-Services",
+
+		/**
+		 * "SessionExpired" illustration type.
+		 * @public
+		 */
+		SessionExpired: "tnt-SessionExpired",
+
+		/**
+		 * "SessionExpiring" illustration type.
+		 * @public
+		 */
+		SessionExpiring: "tnt-SessionExpiring",
+
+		/**
+		 * "Settings" illustration type.
+		 * @public
+		 */
+		Settings: "tnt-Settings",
+
+		/**
+		 * "Success" illustration type.
+		 * @public
+		 */
+		Success: "tnt-Success",
+
+		/**
+		 * "SuccessfulAuth" illustration type.
+		 * @public
+		 */
+		SuccessfulAuth: "tnt-SuccessfulAuth",
+
+		/**
+		 * "Systems" illustration type.
+		 * @public
+		 */
+		Systems: "tnt-Systems",
+
+		/**
+		 * "Teams" illustration type.
+		 * @public
+		 */
+		Teams: "tnt-Teams",
+
+		/**
+		 * "Tools" illustration type.
+		 * @public
+		 */
+		Tools: "tnt-Tools",
+
+		/**
+		 * "Tutorials" illustration type.
+		 * @public
+		 */
+		Tutorials: "tnt-Tutorials",
+
+		/**
+		 * "UnableToLoad" illustration type.
+		 * @public
+		 */
+		UnableToLoad: "tnt-UnableToLoad",
+
+		/**
+		 * "Unlock" illustration type.
+		 * @public
+		 */
+		Unlock: "tnt-Unlock",
+
+		/**
+		 * "UnsuccessfulAuth" illustration type.
+		 * @public
+		 */
+		UnsuccessfulAuth: "tnt-UnsuccessfulAuth",
+
+		/**
+		 * "User2" illustration type.
+		 * @public
+		 */
+		User2: "tnt-User2"
+	};
+
+	return IllustratedMessageType;
+});
+
