6d8a378a15ca581aea7744c60539b6fd416b675a
Deshev <nikolay.deshev@sap.com>
2024-02-16 13:39:09 +0200

    [FIX] sap.m.MultiInput: Avoid double value validation
    Avoid double value validation when there is both typed in value and selected item
    
    SNOW: CS20240007073299
    Change-Id: I7ba0f39a493343d425a009af55df2d432b0c2ccc
    CR-Id: 002075125900001262492024

--- src/sap.m/src/sap/m/MultiInput.js
+++ src/sap.m/src/sap/m/MultiInput.js
@@ -1017,7 +1017,7 @@ function(
 			bNewFocusIsInSuggestionPopup = false,
 			bNewFocusIsInTokenizer = false,
 			bNewFocusIsInMultiInput = this.getDomRef() && containsOrEquals(this.getDomRef(), document.activeElement),
-			bFocusedOut = !bNewFocusIsInSuggestionPopup && oEvent.relatedControlId !== this.getId() && !bNewFocusIsInTokenizer,
+			bFocusedOut,
 			oRelatedControlDomRef,
 			bFocusIsInSelectedItemPopup;
 
@@ -1041,6 +1041,8 @@ function(
 		if (this._bIsValidating || this._bValueHelpOpen) {
 			return;
 		}
+
+		bFocusedOut = !bNewFocusIsInSuggestionPopup && oEvent.relatedControlId !== this.getId() && !bNewFocusIsInTokenizer;
 
 		if (bFocusedOut && ((this.isMobileDevice() && !this.getShowSuggestion()) || !this.isMobileDevice())) {
 			this._validateCurrentText(true);
