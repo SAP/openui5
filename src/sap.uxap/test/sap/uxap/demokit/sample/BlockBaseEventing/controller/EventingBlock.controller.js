59ab70c5f03016c43d90e18a7adac844fd43d25e
PetyaMarkovaBogdanova <petya.markova@sap.com>
2024-02-20 12:07:37 +0200

    [INTERNAL] sap.uxap.ExploredSamples test fixed
    DINC: n/a
    Change-Id: I27b7ee53fc9061bd21104825d08016c3071b6a69

--- src/sap.uxap/test/sap/uxap/demokit/sample/BlockBaseEventing/controller/EventingBlock.controller.js
+++ src/sap.uxap/test/sap/uxap/demokit/sample/BlockBaseEventing/controller/EventingBlock.controller.js
@@ -2,6 +2,12 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 
 	return Controller.extend("sap.uxap.sample.BlockBaseEventing.controller.EventingBlock", {
+		onInit: function() {
+			var oStylesTag = document.createElement("link");
+			oStylesTag.setAttribute("rel", "stylesheet");
+			oStylesTag.setAttribute("href", "test-resources/sap/uxap/demokit/sample/BlockBaseEventing/styles.css");
+			document.head.appendChild(oStylesTag);
+		},
 		onInnerDummy: function (oEvent) {
 			/*
 			 Delegate the eventing to the parent block.
