1a66d8a83872f8897e5ad0ad5e5b080d92dd6029
Dr. Thomas Chadzelek <thomas.chadzelek@sap.com>
2024-02-29 08:22:50 +0100

    [INTERNAL] sap.ui.model.odata.v4.ODataPropertyBinding: use "super" #setType
    PS1: cherry picked from commit 5d17a2eadf5b071ca509483628be38ad058f9bb0
    PS2: commit message edited
    
    Change-Id: I16dc9ebe3099952c5834bbd67e395614615333e4
    JIRA: CPOUI5MODELS-1615
    CR-Id: 002075125800001206422024
    SNOW: CS20240007141719

--- src/sap.ui.core/src/sap/ui/model/odata/v4/ODataPropertyBinding.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v4/ODataPropertyBinding.js
@@ -329,7 +329,8 @@ sap.ui.define([
 			});
 			if (bForceUpdate && vValue.isFulfilled()) {
 				if (vType && vType.isFulfilled && vType.isFulfilled()) {
-					this.oType = vType.getResult();
+					PropertyBinding.prototype.setType
+						.call(this, vType.getResult(), this.sInternalType);
 				}
 				this.vValue = vValue.getResult();
 			}
@@ -343,7 +344,7 @@ sap.ui.define([
 
 			if (oCallToken === that.oCheckUpdateCallToken) { // latest call to checkUpdateInternal
 				that.oCheckUpdateCallToken = undefined;
-				that.oType = oType;
+				PropertyBinding.prototype.setType.call(that, oType, that.sInternalType);
 				if (oCallToken.forceUpdate || that.vValue !== vValue) {
 					that.bInitial = false;
 					that.vValue = vValue;
@@ -678,7 +679,7 @@ sap.ui.define([
 						// Note: this.oType => this.sReducedPath
 						&& _Helper.getMetaPath(this.oModel.resolve(this.sPath, oContext))
 							!== _Helper.getMetaPath(this.sReducedPath)) {
-						this.oType = undefined;
+						PropertyBinding.prototype.setType.call(this, undefined, this.sInternalType);
 					}
 					this.sReducedPath = undefined;
 				}
