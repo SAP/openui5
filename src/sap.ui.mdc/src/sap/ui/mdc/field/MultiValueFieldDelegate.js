445282ea63a8f3dcc2a1d4be9500f18735a20937
Sebastian Allmann <sebastian.allmann@sap.com>
2024-03-07 08:04:44 +0100

    [INTERNAL] MultiValueField: improve JSDoc
    Change-Id: I2e10b12f275ccab60ac3f1e1f0388da682f97c11

--- src/sap.ui.mdc/src/sap/ui/mdc/field/MultiValueFieldDelegate.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/field/MultiValueFieldDelegate.js
@@ -25,11 +25,11 @@ sap.ui.define([
 	 * Implements the model-specific logic to update items after conditions have been updated.
 	 *
 	 * Items can be removed, updated, or added.
-	 * Use the binding information of the <code>MultiValueField</code> control to update the data in the model.
+	 * Use the binding information of the <code>MultiValueField</code> control to update the data in the related model.
 	 *
 	 * @param {object} oPayload Payload for delegate
 	 * @param {sap.ui.mdc.condition.ConditionObject[]} aConditions Current conditions of the <code>MultiValueField</code> control
-	 * @param {sap.ui.mdc.MultiValueField} oMultiValueField Current <code>MultiValueField</code> control to determine binding information to update the values of the corresponding model
+	 * @param {sap.ui.mdc.MultiValueField} oMultiValueField Current <code>MultiValueField</code> control to determine binding information to update the values of the related model
 	 * @public
 	 */
 	MultiValueFieldDelegate.updateItems = function(oPayload, aConditions, oMultiValueField) {
