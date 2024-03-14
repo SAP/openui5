e42248f958038165e6ddc6d36946e32a8d67584c
yanaminkova <yana.minkova@sap.com>
2024-03-14 12:41:49 +0200

    [FIX] sap.f.ProductSwitchItem: Aria-checked attribute added
    SNOW: DINC0093892
    Change-Id: I40db61ddbcec1e55a9d19377f46f1c0369423116

--- src/sap.f/src/sap/f/ProductSwitchItemRenderer.js
+++ src/sap.f/src/sap/f/ProductSwitchItemRenderer.js
@@ -19,7 +19,7 @@ sap.ui.define([],
 			if (oProductSwith) {
 				oAccessibilityState.setsize = oProductSwith._getItemsCount();
 				oAccessibilityState.posinset = oProductSwith._getItemPosition(oControl);
-				oAccessibilityState.checked = oControl.getId() === oProductSwith.getSelectedItem() ? "true" : undefined;
+				oAccessibilityState.checked = oControl.getId() === oProductSwith.getSelectedItem() ? "true" : "false";
 			}
 
 			oRm.openStart("div", oControl);
