47b3ef6e128392756be24b901fc3d1bed586e560
Diana Pazheva <diana.pazheva@sap.com>
2024-03-22 00:30:22 +0200

    [INTERNAL] sap.ui.test: Autowaiter now ignores background animations
    Problem:
    The app may run a background animation [that never ends]. This
    causes the jsAutowaiter wait endlessly and the test cannot proceed.
    
    Solution:
    Define config parameter 'maxDuration' to the jsAnimationWaiter
    (similar to parameter 'maxDelay' of the timeoutWaiter)
    and ignore (i.e. do not wait) animations that exceed
    the maxDuration value.
    
    SNOW: DINC0093968
    Change-Id: I0d1b54a3d26dd6e217ecd07137859eedb0e1dfc6

--- src/sap.ui.core/src/sap/ui/test/autowaiter/_jsAnimationWaiter.js
+++ src/sap.ui.core/src/sap/ui/test/autowaiter/_jsAnimationWaiter.js
@@ -3,8 +3,9 @@
  */
 
 sap.ui.define([
+	"sap/base/util/extend",
 	"./WaiterBase"
-], function(WaiterBase) {
+], function(extend, WaiterBase) {
 	"use strict";
 
 	var oState = {
@@ -13,10 +14,12 @@ sap.ui.define([
 		CANCELLED: "CANCELLED"
 	};
 
+	var iInitiatorId = null;
+
 	var JSAnimationWaiter = WaiterBase.extend("sap.ui.test.autowaiter._jsAnimationWaiter", {
 		constructor: function() {
 			WaiterBase.apply(this, arguments);
-			this._oTrackedAnimations = new Set();
+			this._oTrackedAnimations = new Map();
 
 			if (window.requestAnimationFrame) {
 				var fnOriginalRequestAnimationFrame = window.requestAnimationFrame;
@@ -24,8 +27,13 @@ sap.ui.define([
 					var sId,
 						aCallbackArgs = Array.prototype.slice.call(arguments, 1),
 						wrappedCallback = function(iTimestamp) {
-							callback(iTimestamp);
-							this._deregister(sId, oState.EXECUTED);
+							iInitiatorId = sId;
+							try {
+								callback(iTimestamp);
+							} finally {
+								iInitiatorId = null;
+								this._deregister(sId, oState.EXECUTED);
+							}
 						}.bind(this);
 
 					sId = fnOriginalRequestAnimationFrame.apply(null, [wrappedCallback].concat(aCallbackArgs));
@@ -46,7 +54,8 @@ sap.ui.define([
 
 		_register: function(sId, sReason) {
 			this._oLogger.trace("register", "ID: " + sId + " Reason: " + sReason);
-			this._oTrackedAnimations.add(sId);
+			var iStartTime = this._oTrackedAnimations.get(iInitiatorId)?.startTime || Date.now();
+			this._oTrackedAnimations.set(sId, {startTime: iStartTime});
 		},
 
 		_deregister: function(sId, sReason) {
@@ -54,8 +63,32 @@ sap.ui.define([
 			this._oTrackedAnimations.delete(sId);
 		},
 
+		_getDefaultConfig: function () {
+			return extend({
+				// animations that exceed the maxDuration will be ignored (considered as background processes)
+				maxDuration: 1000 	// milliseconds
+			}, WaiterBase.prototype._getDefaultConfig.call(this));
+		},
+
+		_getValidationInfo: function () {
+			return extend({
+				maxDuration: "numeric"
+			}, WaiterBase.prototype._getValidationInfo.call(this));
+		},
+
 		hasPending: function () {
-			var bHasPending = this._oTrackedAnimations.size > 0;
+			var bHasPending = false,
+				iMaxDuration = this._getDefaultConfig().maxDuration,
+				iElapsedTime;
+			for (var oEntry of this._oTrackedAnimations.values()) {
+				iElapsedTime = Date.now() - oEntry.startTime;
+				// ignore animations that excede the <code>iMaxDuration</code>
+				// as these are considered background processes that never end
+				if (iElapsedTime < iMaxDuration) {
+					bHasPending = true;
+				}
+			}
+
 			this._oLogger.trace("hasPending", bHasPending);
 			if (bHasPending) {
 				this._oHasPendingLogger.debug("jsAnimation in progress");
--- src/sap.ui.core/test/sap/ui/core/qunit/opa/autowaiter/_jsAnimationWaiter.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/opa/autowaiter/_jsAnimationWaiter.js
@@ -76,4 +76,86 @@ sap.ui.define([
 		setTimeout(onStart, 0);
 	});
 
+	function getMockAnimation() {
+		var oAnimation = {
+			_id: null,
+			_animate: function() {
+				this._id = requestAnimationFrame(this._animate.bind(this));
+			},
+			start: function(iDuration) {
+				this._animate();
+				setTimeout(function() {
+					oAnimation.stop();
+				}, iDuration);
+			},
+			stop: function() {
+				cancelAnimationFrame(this._id);
+			}
+		};
+
+		return oAnimation;
+	}
+
+	QUnit.module("JSAnimationnWaiter - maxDuration", {
+		beforeEach: function () {
+			this.oTimeoutWaiterStub = sinon.stub(_timeoutWaiter, "hasPending");
+			this.oXHRWaiterStub = sinon.stub(_XHRWaiter, "hasPending");
+			this.oPromiseWaiterStub = sinon.stub(_promiseWaiter, "hasPending");
+			this.oTimeoutWaiterStub.returns(false);
+			this.oXHRWaiterStub.returns(false);
+			this.oPromiseWaiterStub.returns(false);
+			this.oAnimation = getMockAnimation();
+			return nextUIUpdate();
+		},
+
+		afterEach: function () {
+			this.oTimeoutWaiterStub.restore();
+			this.oXHRWaiterStub.restore();
+			this.oPromiseWaiterStub.restore();
+			this.oAnimation.stop();
+			this.oAnimation = null;
+			return nextUIUpdate();
+		}
+	});
+
+	QUnit.test("Autowait until animation exceeds maxDuration", function (assert) {
+		var fnDone = assert.async(),
+			oJsAnimationLogRegExp = new RegExp("jsAnimation in progress", "g"),
+			bFirstCheckBefore,
+			bSecondCheckBefore,
+			bFirstCheckAfter,
+			bSecondCheckAfter,
+			iMaxDuration = 1000,
+			iAnimationDuration = 2000;
+
+		_autoWaiter.extendConfig({
+			jsAnimationWaiter: {
+				// should be the default already
+				maxDuration: iMaxDuration
+			}
+		});
+		this.oAnimation.start(iAnimationDuration);
+
+		function onStart() {
+			bFirstCheckBefore = _autoWaiter.hasToWait();
+			bSecondCheckBefore = _autoWaiter.hasToWait();
+			assert.strictEqual(oLogCollector.getAndClearLog().match(oJsAnimationLogRegExp).length, 2);
+
+			function onEnd() {
+				bFirstCheckAfter = _autoWaiter.hasToWait();
+				bSecondCheckAfter = _autoWaiter.hasToWait();
+
+				assert.ok(bFirstCheckBefore, "animation is in progress");
+				assert.ok(bSecondCheckBefore, "animation is in progress");
+				assert.ok(!bFirstCheckAfter, "animation is done");
+				assert.ok(!bSecondCheckAfter, "animation is done");
+				assert.ok(!oLogCollector.getAndClearLog().match(oJsAnimationLogRegExp));
+				fnDone();
+			}
+
+			setTimeout(onEnd, iMaxDuration + 300 /* add tolerance*/);
+		}
+
+		setTimeout(onStart, 0);
+	});
 });
