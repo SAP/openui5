afca4d0d87321d9cc1f053751a14645e3185b9db
Silke Arians <silke.arians@sap.com>
2024-04-09 18:22:07 +0200

    [INTERNAL] sap.ui.fl: setMinUI5Version change merger enhancement
    - Allow base app to have minUI5Version specified as array
    - Ensure change applying is not breaking
    
    Rules:
    Compare major versions with each other and set always highest version.
    Major version which is only included in one (base app or change) will be
    dropped.
    If no major version remains throw exception.
    If result has more than one entry: Additionally check that for version
    major 1.x.x that minimum version is 1.120.0.
    If result has only one value then reduce to type string.
    
    Jira: UITECHGRAVITY-3821
    
    Change-Id: Idc3b6e838c8616c88713c42a852e382c2fb2dbfb

--- src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/ui5/SetMinUI5Version.js
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/ui5/SetMinUI5Version.js
@@ -38,11 +38,46 @@ sap.ui.define([
 		 * @ui5-restricted sap.ui.fl.apply._internal
 		 */
 		applyChange(oManifest, oChange) {
-			// TODO: Should existens of minui5version be checked in manifest and change or is the check during deployment enough and I can expect minui5version to be set in both?
-			var vCurrentVersion = new Version(oManifest["sap.ui5"].dependencies.minUI5Version);
-			if (vCurrentVersion.compareTo(oChange.getContent().minUI5Version) <= 0) {
-				oManifest["sap.ui5"].dependencies.minUI5Version = oChange.getContent().minUI5Version;
+			// Rules:
+			// Compare major versions with each other and set always highest version.
+			// Major version which is only included in one (base app or change) will be dropped.
+			// If no major version remains throw exception.
+			// If result has more than one entry: Additionally check that for version major 1.x.x that minimum version is 1.120.0.
+			// If result has only one value then reduce to type string.
+
+			const changeMinUI5Version = oChange.getContent().minUI5Version;
+			if (!oChange.getContent().hasOwnProperty("minUI5Version")) {
+				throw new Error("No minUI5Version in change content provided");
 			}
+
+			var {minUI5Version: aMinUI5Version} = oManifest["sap.ui5"].dependencies;
+			if (!aMinUI5Version) {
+				throw new Error("sap.ui5/dependencies/minUI5Version missing in base manifest");
+			}
+
+			if (typeof aMinUI5Version === "string") {
+				aMinUI5Version = [aMinUI5Version];
+			}
+
+			var vChangeVersion = new Version(changeMinUI5Version);
+			// drop major versions which are only included in base app and not in change
+			aMinUI5Version = aMinUI5Version.filter(function(item) {
+				var vItemVersion = new Version(item);
+				return vItemVersion.getMajor() === vChangeVersion.getMajor();
+			});
+			if (aMinUI5Version.length === 0) {
+				throw new Error("Upgrade/Downgrade for different major version not possible");
+			}
+
+			var aNewMinUI5Version = aMinUI5Version;
+			aNewMinUI5Version = aMinUI5Version.map(function(minUI5Version) {
+				var vCurrentVersion = new Version(minUI5Version);
+				if (vCurrentVersion.compareTo(vChangeVersion) <= 0 && vCurrentVersion.getMajor() === vChangeVersion.getMajor()) {
+					return changeMinUI5Version;
+				}
+				return minUI5Version;
+			});
+			oManifest["sap.ui5"].dependencies.minUI5Version = aNewMinUI5Version.length === 1 ? aNewMinUI5Version[0] : aNewMinUI5Version;
 			return oManifest;
 		}
 
