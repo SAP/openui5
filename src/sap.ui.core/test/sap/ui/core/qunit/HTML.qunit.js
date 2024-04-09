22b8a00429226a90e9f4a3064c0c5db92eda7312
Jiawei Cao <jiawei.cao@sap.com>
2024-04-09 17:54:00 +0200

    [INTERNAL] thirdparty/caja: correct the eflag for 'iframe' tag
    Change-Id: I876a0675b6a6df90d3b783fbacc6d8880ddaf8e6
    CR-Id: 002075125800001321072024
    SNOW: n/a

--- src/sap.ui.core/test/sap/ui/core/qunit/HTML.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/HTML.qunit.js
@@ -581,4 +581,17 @@ sap.ui.define([
 		assert.equal(html.getContent(), sanitizedContent, "HTML sanitized using constructor, wrong order");
 	});
 
+	QUnit.module("Fixes in caja-html-sanitizer");
+
+	QUnit.test("Tags wrapped in iframe tag", function(assert) {
+		var sOffensiveContent = "<select><iframe><select><img src=x onerror=alert(1)></select></iframe></select>";
+		var oHTML = new HTML({
+			sanitizeContent: true,
+			content: sOffensiveContent
+		});
+
+		assert.notOk(oHTML.getContent().includes("onerror"), "The error handler should be removed");
+		oHTML.destroy();
+	});
+
 });
