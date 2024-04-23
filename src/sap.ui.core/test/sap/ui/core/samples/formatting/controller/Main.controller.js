39e50fdcf120a76b38e42553a847e9df61f191d4
Stefan Scherer <stefan.scherer@sap.com>
2024-04-23 12:32:46 +0200

    [INTERNAL] sap.ui.core*: Remove outdated samples
    PS1: Do it!
    PS2: Rebased
    
    Change-Id: Ice690cdf4b5143fca08ffb16bab7ea274360ef88
    JIRA: CPOUI5MODELS-1627

--- src/sap.ui.core/test/sap/ui/core/samples/formatting/controller/Main.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
-	"use strict";
-	return Controller.extend("sap.ui.core.samples.formatting.controller.Main", {
-		onInit: function () {}
-	});
-});
