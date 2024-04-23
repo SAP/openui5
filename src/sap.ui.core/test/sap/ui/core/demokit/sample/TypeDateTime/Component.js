202c284c7d594e00c7f05fc23bbd1fbcacaebb5d
Knut Thies <knut.thies@sap.com>
2024-04-23 08:26:00 +0200

    [INTERNAL] sap.ui.core.demokit.sample.Type*: Implement sap.ui.core.IAsyncContentCreation marker interface
    PS1: do it
    PS2: remove redundant "async":true
    
    Change-Id: I2888c070e650b6980f92635fdc2a3f926723bebb
    JIRA: CPOUI5MODELS-1624

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeDateTime/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/TypeDateTime/Component.js
@@ -5,6 +5,7 @@ sap.ui.define(["sap/ui/core/UIComponent"],
 	return UIComponent.extend("sap.ui.core.sample.TypeDateTime.Component", {
 
 		metadata: {
+			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
 		}
 	});
