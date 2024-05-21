3b4d2088601e97368ea833d0531a2ce01f09f681
Knut Thies <knut.thies@sap.com>
2024-05-21 17:32:01 +0200

    [INTERNAL] sap.ui.core.demokit.sample.odata.v4.MultipleInlineCreationRowsGrid: make app future-proof
    Test:
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/common/index.html?component=odata.v4.MultipleInlineCreationRowsGrid&sap-ui-support=true
    Opa:
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/odata/v4/MultipleInlineCreationRowsGrid/Opa.qunit.html?supportAssistant=true
    
    PS1: impl. IAsyncContentCreation, adjust manifest.json, Main.controller.js and Opa.qunit.html
    
    Change-Id: I9134ca4e3333d532175a13984a575cdf308a74f7
    JIRA: CPOUI5MODELS-1680
    JIRA: CPOUI5MODELS-1712

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/MultipleInlineCreationRowsGrid/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/MultipleInlineCreationRowsGrid/Component.js
@@ -15,6 +15,7 @@ sap.ui.define([
 	return UIComponent.extend(
 		"sap.ui.core.sample.odata.v4.MultipleInlineCreationRowsGrid.Component", {
 		metadata : {
+			interfaces : ["sap.ui.core.IAsyncContentCreation"],
 			manifest : "json"
 		}
 	});
