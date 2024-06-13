5ae210be54421e4fcca9abe8634bc810febabcf4
Sebastian Allmann <sebastian.allmann@sap.com>
2024-06-13 10:18:05 +0200

    [FEATURE] ColumnLayout: render as <dl> in display mode
    If a Form contains only text it should be rendered as <dl> to allow a better screenreader experience.
    
    Therefore in display mode ColumLayout renders now a <dl> on container level. The older layouts (ResponsiveGridLayout, ResponsiveLayout and ColumnLayout) are not changed. (Here the DOM structure makes a change impossible.)
    
    JIRA: CPOUIFDENVER-2099
    SNOW: CS20250009272285
    SNOW: CS20250009429329
    
    Change-Id: I3c9d649ed9bf31ded87b10cd94db0d9bcd0b4ccd

--- src/sap.ui.layout/src/sap/ui/layout/form/FormRenderer.js
+++ src/sap.ui.layout/src/sap/ui/layout/form/FormRenderer.js
@@ -63,24 +63,9 @@ sap.ui.define([
 			rm.attr('title', oForm.getTooltip_AsString());
 		}
 
-		var oTitle = oForm.getTitle();
-		var oToolbar = oForm.getToolbar();
-		if (oToolbar) {
-			if (!oForm.getAriaLabelledBy() || oForm.getAriaLabelledBy().length == 0) {
-				// no aria-label -> use Title of Toolbar
-				var sToolbarTitleID = FormHelper.getToolbarTitle(oToolbar); // FormHelper must already be initialized by Form
-				mAriaProps["labelledby"] = sToolbarTitleID;
-			}
-		} else if (oTitle) {
-			var sId = "";
-			if (typeof oTitle == "string") {
-				sId = oForm.getId() + "--title";
-			} else {
-				sId = oTitle.getId();
-			}
-			mAriaProps["labelledby"] = {value: sId, append: true};
-		} else if (oForm._sSuggestedTitleId) {
-			mAriaProps["labelledby"] = {value: oForm._sSuggestedTitleId, append: true};
+		const sTitleID = oLayout?.getRenderer().getTitleId(oForm) || oForm._sSuggestedTitleId;
+		if (sTitleID) {
+			mAriaProps["labelledby"] = {value: sTitleID, append: true};
 		}
 
 		rm.accessibilityState(oForm, mAriaProps);
