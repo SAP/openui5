88c9e07a1ed2f2d1e591ef5238c936e9296bc500
I310817 <gergana.marcheva@sap.com>
2024-07-02 15:58:58 +0300

    [FIX] sap.ui.unified.CalendarRow: Appointments ARIA texts are correctly translated and localized now
    SNOW: CS20240007433019
    
    Change-Id: Ibb8a00e1514381c6fd401157a720470413514d0d

--- src/sap.ui.unified/test/sap/ui/unified/qunit/CalendarRow.qunit.js
+++ src/sap.ui.unified/test/sap/ui/unified/qunit/CalendarRow.qunit.js
@@ -1315,11 +1315,14 @@ QUnit.test("CalendarRow '_oFormatAria' formatter", function (assert) {
 QUnit.test("_oFormatAria start/end format for 12-hour clocks", function (assert) {
 	// Prepare
 	Formatting.setLanguageTag("en-US");
-
-	var oCalendarRow = new CalendarRow(),
+	var oLocale = new sap.ui.core.Locale("en-US"),
+		oLocaleData = sap.ui.core.LocaleData.getInstance(oLocale),
+		oCalendarRow = new CalendarRow(),
 		oMockedDate = UI5Date.getInstance(2019, 7, 5, 15),
-		// \u202f is a Narrow No-Break Space which has been introduced with CLDR version 43
-		sExpectedResult = "Monday 05/08/2019 at 3:00:00\u202fPM";
+		pattern = oCalendarRow._oRb.getText("APPOINTMENT_DATE_TIME_DESCRIPTION", [oLocaleData.getDatePattern("long"), oLocaleData.getTimePattern("medium")]),
+		sExpectedResult = DateFormat.getDateTimeInstance({
+			pattern: "EEEE " + pattern
+		}).format(oMockedDate);
 
 	// Assert
 	assert.strictEqual(oCalendarRow._oFormatAria.format(oMockedDate), sExpectedResult, "Cell's start/end info is properly formatted");
@@ -1331,10 +1334,33 @@ QUnit.test("_oFormatAria start/end format for 12-hour clocks", function (assert)
 QUnit.test("_oFormatAria start/end format for 24-hour clocks", function (assert) {
 	// Prepare
 	Formatting.setLanguageTag("en-GB");
-
-	var oCalendarRow = new CalendarRow(),
+	var oLocale = new sap.ui.core.Locale("en-GB"),
+		oLocaleData = sap.ui.core.LocaleData.getInstance(oLocale),
+		oCalendarRow = new CalendarRow(),
 		oMockedDate = UI5Date.getInstance(2019, 7, 5, 15),
-		sExpectedResult = "Monday 05/08/2019 at 15:00:00";
+		pattern = oCalendarRow._oRb.getText("APPOINTMENT_DATE_TIME_DESCRIPTION", [oLocaleData.getDatePattern("long"), oLocaleData.getTimePattern("medium")]),
+		sExpectedResult = DateFormat.getDateTimeInstance({
+			pattern: "EEEE " + pattern
+		}).format(oMockedDate);
+
+	// Assert
+	assert.strictEqual(oCalendarRow._oFormatAria.format(oMockedDate), sExpectedResult, "Cell's start/end info is properly formatted");
+
+	// Cleanup
+	oCalendarRow.destroy();
+});
+
+QUnit.test("_oFormatAria correctly localized and translated", function (assert) {
+	// Prepare
+	Formatting.setLanguageTag("de");
+	var oLocale = new sap.ui.core.Locale("de"),
+		oLocaleData = sap.ui.core.LocaleData.getInstance(oLocale),
+		oCalendarRow = new CalendarRow(),
+		oMockedDate = UI5Date.getInstance(2019, 7, 5, 15),
+		pattern = oCalendarRow._oRb.getText("APPOINTMENT_DATE_TIME_DESCRIPTION", [oLocaleData.getDatePattern("long"), oLocaleData.getTimePattern("medium")]),
+		sExpectedResult = DateFormat.getDateTimeInstance({
+			pattern: "EEEE " + pattern
+		}).format(oMockedDate);
 
 	// Assert
 	assert.strictEqual(oCalendarRow._oFormatAria.format(oMockedDate), sExpectedResult, "Cell's start/end info is properly formatted");
