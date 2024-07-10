71d19d49cd4e2b6508c29ec5db8813c082b939e6
Martin Haeuser <martin.haeuser@sap.com>
2024-07-10 13:15:58 +0200

    [INTERNAL] FilterController: add inbuilt filter as reset selector
    Ensure that the AdaptationFilterBar is cleared, too on resetting the view settings dialog.
    
    SNOW: CS20240007939711
    Change-Id: Ic3ccd41fa399de3d67f3f22c5dd58cd1f20568da

--- src/sap.ui.mdc/src/sap/ui/mdc/p13n/subcontroller/FilterController.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/p13n/subcontroller/FilterController.js
@@ -3,17 +3,17 @@
  */
 
 sap.ui.define([
-    "sap/m/p13n/FilterController",
-    "sap/ui/core/Lib",
-    'sap/ui/mdc/enums/ProcessingStrategy',
-    'sap/ui/mdc/condition/FilterOperatorUtil',
-    'sap/base/Log',
-    'sap/base/util/merge',
-    'sap/base/util/deepEqual'
+	"sap/m/p13n/FilterController",
+	"sap/ui/core/Lib",
+	'sap/ui/mdc/enums/ProcessingStrategy',
+	'sap/ui/mdc/condition/FilterOperatorUtil',
+	'sap/base/Log',
+	'sap/base/util/merge',
+	'sap/base/util/deepEqual'
 ], (BaseFilterController, Library, ProcessingStrategy, FilterOperatorUtil, Log, merge) => {
 	"use strict";
 
-    const FilterController = BaseFilterController.extend("sap.ui.mdc.p13n.subcontroller.FilterController");
+	const FilterController = BaseFilterController.extend("sap.ui.mdc.p13n.subcontroller.FilterController");
 
 	FilterController.prototype.getStateKey = function() {
 		return "filter";
@@ -43,11 +43,20 @@ sap.ui.define([
 		return this.getAdaptationControl().getCurrentState()[this.getStateKey()];
 	};
 
-    FilterController.prototype.getBeforeApply = function() {
-        const oAdaptationFilterBar = this.getAdaptationControl().getInbuiltFilter();
-        const pConditionPromise = oAdaptationFilterBar ? oAdaptationFilterBar.createConditionChanges() : Promise.resolve([]);
-        return pConditionPromise;
-    };
+	FilterController.prototype.getSelectorForReset = function () {
+		const oAdaptationControl = this.getAdaptationControl(), oInbuiltFilter = oAdaptationControl.getInbuiltFilter();
+		let selectors = [oAdaptationControl];
+		if (oInbuiltFilter) {
+			selectors = selectors.concat(oInbuiltFilter);
+		}
+		return selectors;
+	};
+
+	FilterController.prototype.getBeforeApply = function() {
+		const oAdaptationFilterBar = this.getAdaptationControl().getInbuiltFilter();
+		const pConditionPromise = oAdaptationFilterBar ? oAdaptationFilterBar.createConditionChanges() : Promise.resolve([]);
+		return pConditionPromise;
+	};
 
 	FilterController.prototype.getFilterControl = function() {
 		return this.getAdaptationControl().isA("sap.ui.mdc.IFilter") ? this.getAdaptationControl() : this.getAdaptationControl()._oP13nFilter;
@@ -73,27 +82,27 @@ sap.ui.define([
 	 */
 	FilterController.checkConditionOperatorSanity = function(mConditions) {
 		//TODO: consider to harmonize this sanity check with 'getCurrentState' cleanups
-        for (const sFieldPath in mConditions) {
-            const aConditions = mConditions[sFieldPath];
-            for (let i = 0; i < aConditions.length; i++) {
-                const oCondition = aConditions[i];
-                const sOperator = oCondition.operator;
-                if (!FilterOperatorUtil.getOperator(sOperator)){
-                    aConditions.splice(i, 1);
-                    /*
-                        * in case the unknown operator has been removed, we need to check
-                        * if this caused the object to be empty to not create unnecessary remove changes
-                        * this should only be done within this check, as empty objects have a special meaning in the 'filter'
-                        * object within the external state to reset the given conditions for a single property
-                        */
-                    if (mConditions[sFieldPath].length == 0) {
-                        delete mConditions[sFieldPath];
-                    }
-                    Log.warning("The provided conditions for field '" + sFieldPath + "' contain unsupported operators - these conditions will be neglected.");
-                }
-            }
-        }
-    };
+		for (const sFieldPath in mConditions) {
+			const aConditions = mConditions[sFieldPath];
+			for (let i = 0; i < aConditions.length; i++) {
+				const oCondition = aConditions[i];
+				const sOperator = oCondition.operator;
+				if (!FilterOperatorUtil.getOperator(sOperator)){
+					aConditions.splice(i, 1);
+					/*
+						* in case the unknown operator has been removed, we need to check
+						* if this caused the object to be empty to not create unnecessary remove changes
+						* this should only be done within this check, as empty objects have a special meaning in the 'filter'
+						* object within the external state to reset the given conditions for a single property
+						*/
+					if (mConditions[sFieldPath].length == 0) {
+						delete mConditions[sFieldPath];
+					}
+					Log.warning("The provided conditions for field '" + sFieldPath + "' contain unsupported operators - these conditions will be neglected.");
+				}
+			}
+		}
+	};
 
 	FilterController.prototype.initAdaptationUI = function(oPropertyHelper, oWrapper) {
 		const oAdaptationData = this.mixInfoAndState(oPropertyHelper);
@@ -125,18 +134,18 @@ sap.ui.define([
 		}
 	};
 
-    FilterController.prototype.getDelta = function(mPropertyBag) {
-        if (mPropertyBag.applyAbsolute === ProcessingStrategy.FullReplace) {
-            Object.keys(mPropertyBag.existingState).forEach(function(sKey){
-                if (!mPropertyBag.changedState.hasOwnProperty(sKey)) {
-                    mPropertyBag.changedState[sKey] = [];
-                }
-            });
-        }
-        return this.getConditionDeltaChanges(mPropertyBag);
-    };
+	FilterController.prototype.getDelta = function(mPropertyBag) {
+		if (mPropertyBag.applyAbsolute === ProcessingStrategy.FullReplace) {
+			Object.keys(mPropertyBag.existingState).forEach(function(sKey){
+				if (!mPropertyBag.changedState.hasOwnProperty(sKey)) {
+					mPropertyBag.changedState[sKey] = [];
+				}
+			});
+		}
+		return this.getConditionDeltaChanges(mPropertyBag);
+	};
 
-    FilterController.prototype._createConditionChangeContent = function(sFieldPath, oCondition) {
+	FilterController.prototype._createConditionChangeContent = function(sFieldPath, oCondition) {
 		return {
 			name: sFieldPath,
 			condition: oCondition
@@ -167,10 +176,10 @@ sap.ui.define([
 			return !(oProperty.filterable === false);
 		});
 
-        this.sortP13nData({
-            visible: "active",
-            position: undefined
-        }, oP13nData.items);
+		this.sortP13nData({
+			visible: "active",
+			position: undefined
+		}, oP13nData.items);
 
 		return oP13nData;
 	};
