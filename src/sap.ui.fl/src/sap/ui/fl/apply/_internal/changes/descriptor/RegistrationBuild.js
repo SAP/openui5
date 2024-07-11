b1d9e53a1d0090b491a738d0943475fc86db0998
Mehmet Bakan <mehmet.bakan@sap.com>
2024-07-11 15:03:38 +0200

    [INTERNAL] sap.ui.fl - add change merger setAch
    Change-Id: Ic21eb5c6108dc2b7f2149b2004242cf597666e03
    JIRA: UITECHGRAVITY-4148

--- src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/RegistrationBuild.js
+++ src/sap.ui.fl/src/sap/ui/fl/apply/_internal/changes/descriptor/RegistrationBuild.js
@@ -34,6 +34,7 @@ sap.ui.define([
 		appdescr_app_removeAllInboundsExceptOne: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/app/RemoveAllInboundsExceptOne"),
 		appdescr_app_changeInbound: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/app/ChangeInbound"),
 		appdescr_app_addNewInbound: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/app/AddNewInbound"),
+		appdescr_app_setAch: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/app/SetAch"),
 		appdescr_fiori_setAbstract: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/fiori/SetAbstract"),
 		appdescr_fiori_setCloudDevAdaptationStatus: requireAsync.bind(this, "sap/ui/fl/apply/_internal/changes/descriptor/fiori/SetCloudDevAdaptationStatus")
 	};
