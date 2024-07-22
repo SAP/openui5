03316b4da33c69f39a7f189f0ca585cdea59c1b5
Boyan Rakilovski <boyan.rakilovski@sap.com>
2024-07-22 16:19:49 +0300

    [FIX] sap.m.SinglePlanningCalendar: adjust more link tab order
    Issue 1:
    - There was a missing announcement for the grid cells.
    If a grid cell has a related appointments it should
    also have a "Contans Appointments" announcement.
    
    Issue 2:
    - Users couldn't navigate direclty over the first appointment
    in a row from a grid cell by using a TAB keyboard key.
    
    JIRA: BGSOFUIBALKAN-7340
    BCP: 2380128055
    BCP: 2380129002
    Change-Id: I224ae5260c0df493eec7ebeafe23319f56dc18f0

--- src/sap.m/src/sap/m/SinglePlanningCalendarGridRenderer.js
+++ src/sap.m/src/sap/m/SinglePlanningCalendarGridRenderer.js
@@ -137,6 +137,9 @@ sap.ui.define([
 				if (oControl._sLegendId && sLegendItemType) {
 					oRm.text(sLegendItemType);
 				}
+				if (oControl._doesContainBlockers(oColumnCalDate)) {
+					oRm.text(oControl._getCellDescription());
+				}
 				oRm.close("span");
 
 				oRm.close("div"); // END .sapMSinglePCColumn
@@ -459,6 +462,10 @@ sap.ui.define([
 				oRm.class("sapUiInvisibleText");
 				oRm.openEnd();
 				oRm.text(oControl._getCellStartEndInfo(oCellStartDate, oCellEndDate));
+
+				if (oControl._doesContainAppointments(oCellStartDate, oCellEndDate)) {
+					oRm.text(oControl._getCellDescription());
+				}
 				oRm.close("span");
 
 				oRm.close("div"); // END .sapMSinglePCRow
