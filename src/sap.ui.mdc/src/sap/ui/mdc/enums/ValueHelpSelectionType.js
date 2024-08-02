06da960aa82fbd9e678f7d101e3046031ceaab38
Sebastian Allmann <sebastian.allmann@sap.com>
2024-08-02 07:53:29 +0200

    [INTERNAL] ValueHelp: improve JSDoc
    Change-Id: Iec4c5c2a3e880869433992fdc224cb6b530988db

--- src/sap.ui.mdc/src/sap/ui/mdc/enums/ValueHelpSelectionType.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/enums/ValueHelpSelectionType.js
@@ -8,33 +8,26 @@ sap.ui.define(["sap/ui/base/DataType"], (DataType) => {
 
 
 	/**
-	 * Enumeration of the possible selection types
+	 * Enumeration of the possible selection types in {@link sap.ui.mdc.ValueHelp ValueHelp}
 	 *
 	 * @enum {string}
-	 * @private
-	 * @ui5-restricted sap.ui.mdc
+	 * @public
 	 * @since 1.115
 	 * @alias sap.ui.mdc.enums.ValueHelpSelectionType
 	 */
 	const ValueHelpSelectionType = {
 		/**
 		 * The given conditions are set and replace the existing ones.
-		 * @private
-		 * @ui5-restricted sap.ui.mdc
 		 */
 		Set: "Set",
 
 		/**
 		 * The given conditions are just added to the existing ones, if they don't already exist.
-		 * @private
-		 * @ui5-restricted sap.ui.mdc
 		 */
 		Add: "Add",
 
 		/**
 		 * The given conditions are removed.
-		 * @private
-		 * @ui5-restricted sap.ui.mdc
 		 */
 		Remove: "Remove"
 	};
