e8b39654b75a7784a604edd1d79cefa203cd9fea
I562693 <vinay.k.n@sap.com>
2024-08-23 12:33:33 +0530

    [FIX] We ensure that the UploadSetwithTable plugin now uses currentContexts for file preview
    plugin will now use getCurrentContexts API instead of getContexts for file Preview logic since getContexts will initiate data requests and leads to update issue with extended change detection enabled. getCurrentContexts resolve this by fetching the current loaded contexts (visible records)
    
    SNOW: DINC0242058
    Change-Id: Ie2be410da66ce85fc12a4cc0199e630338682eaa

--- src/sap.m/src/sap/m/plugins/UploadSetwithTable.js
+++ src/sap.m/src/sap/m/plugins/UploadSetwithTable.js
@@ -1709,9 +1709,9 @@ sap.ui.define([
 		},
 		getTableContexts: function(oTable) {
 			if (oTable?.isA("sap.m.Table")) {
-				return oTable.getBinding("items").getContexts();
+				return oTable.getBinding("items").getCurrentContexts();
 			} else if (oTable?.isA("sap.ui.table.Table")) {
-				return oTable?.getBinding("rows")?.getContexts();
+				return oTable?.getBinding("rows")?.getCurrentContexts();
 			}
 			return null;
 		}
@@ -1835,7 +1835,7 @@ sap.ui.define([
 			return false;
 		},
 		getTableContexts: function(oTable) {
-			return oTable?.getBinding("items")?.getContexts() || null;
+			return oTable?.getBinding("items")?.getCurrentContexts() || null;
 		}
 	 },
 	 "sap.ui.table.Table": {
@@ -1958,7 +1958,7 @@ sap.ui.define([
 			return false;
 		},
 		getTableContexts: function(oTable) {
-			return oTable?.getBinding("rows")?.getContexts() || null;
+			return oTable?.getBinding("rows")?.getCurrentContexts() || null;
 		}
 	 }
     }, UploadSetwithTable);
