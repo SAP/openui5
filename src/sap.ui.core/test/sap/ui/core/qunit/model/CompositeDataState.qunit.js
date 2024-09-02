3b089b4c3162f6767bcd6304072eed7d85a44cf2
d029019 <sven.bernhard@sap.com>
2024-09-02 13:49:44 +0200

    [FIX] sap/ui/model/DataState.js: consider also the control dirty state of the parent data state
    If a binding is used as a part of a CompositeBinding, it may happen
    that the control data state of the composite binding is dirty, but
    the control data states of all inner bindings are not dirty.
    The OData V4 model is not aware of composite bindings. It checks
    isControlDirty at the bindings that have been created by the model to
    reset the invalid data state after e.g. resetting all changes.
    Enable DataState#isControlDirty to consider also the control dirty state
    of the parent data state (CompositeDataState).
    
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/common/index.html?component=odata.v4.Products
    http://localhost:8080/test-resources/sap/ui/core/internal/samples/odata/v2/Products/index.html?sap-language=en
    Change e.g. the Weight to "foo" -> the input field is marked as invalid: "Enter a valid unit amount"
    Click on "Reset Changes" -> Value is reset to the original value and invalid marker is removed
    
    PS1: POC
    PS2: TDD
    PS3-4: Review comments
    
    Change-Id: I3b211f4d50b20657a3d03aa16327697706fb1461
    SNOW: CS20240008267927

--- src/sap.ui.core/test/sap/ui/core/qunit/model/CompositeDataState.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/model/CompositeDataState.qunit.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/base/Log",
-	"sap/ui/model/CompositeDataState"
-], function (Log, CompositeDataState) {
+	"sap/ui/model/CompositeDataState",
+	"sap/ui/model/DataState"
+], function (Log, CompositeDataState, DataState) {
 	/*global QUnit*/
 	"use strict";
 
@@ -18,10 +19,43 @@ sap.ui.define([
 	});
 
 	//*********************************************************************************************
+	QUnit.test("constructor", function(assert) {
+		const oDataState0 = {setParent() {}};
+		const oDataState0setParentExpectation = this.mock(oDataState0).expects("setParent");
+		const oDataState1 = {setParent() {}};
+		const oDataState1setParentExpectation = this.mock(oDataState1).expects("setParent");
+		const oDataState2 = {setParent() {}};
+		const oDataState2setParentExpectation = this.mock(oDataState2).expects("setParent");
+
+		// code under test
+		const oCompositeDataState = new CompositeDataState([oDataState0, oDataState1, oDataState2]);
+
+		const oExpectedProperties = {
+			controlMessages: [],
+			dirty: false,
+			internalValue: [],
+			invalidValue: undefined,
+			laundering: false,
+			messages: [],
+			modelMessages: [],
+			originalInternalValue: [],
+			originalValue: [],
+			value: []
+		};
+		assert.deepEqual(oCompositeDataState.mProperties, oExpectedProperties);
+		assert.deepEqual(oCompositeDataState.mChangedProperties, oExpectedProperties);
+		assert.notStrictEqual(oCompositeDataState.mChangedProperties, oCompositeDataState.mProperties);
+		assert.deepEqual(oCompositeDataState.aDataStates, [oDataState0, oDataState1, oDataState2]);
+		assert.ok(oDataState0setParentExpectation.calledWithExactly(oCompositeDataState));
+		assert.ok(oDataState1setParentExpectation.calledWithExactly(oCompositeDataState));
+		assert.ok(oDataState2setParentExpectation.calledWithExactly(oCompositeDataState));
+	});
+
+	//*********************************************************************************************
 	QUnit.test("getAllMessages", function(assert) {
-		var oDataState0 = {getAllMessages : function () {}},
-			oDataState1 = {getAllMessages : function () {}},
-			oDataState2 = {getAllMessages : function () {}},
+		var oDataState0 = {getAllMessages() {}, setParent() {}},
+			oDataState1 = {getAllMessages() {}, setParent() {}},
+			oDataState2 = {getAllMessages() {}, setParent() {}},
 			oCompositeDataState = new CompositeDataState([oDataState0, oDataState1, oDataState2]);
 
 		this.mock(oDataState0).expects("getAllMessages")
@@ -37,5 +71,45 @@ sap.ui.define([
 		// code under test
 		assert.deepEqual(oCompositeDataState.getAllMessages(),
 			["~msg0", "~msg1", "~msg2", "~msg3"]);
+	});
+
+	//*********************************************************************************************
+	QUnit.test("isControlDirty: composite data state is already dirty", function(assert) {
+		const oCompositeDataState = {};
+		this.mock(DataState.prototype).expects("isControlDirty").on(oCompositeDataState).withExactArgs()
+			.returns("~truthyValue");
+
+		// code under test
+		assert.strictEqual(CompositeDataState.prototype.isControlDirty.call(oCompositeDataState), "~truthyValue");
+	});
+
+	//*********************************************************************************************
+	QUnit.test("isControlDirty: delegate to aggregated data states", function(assert) {
+		const oDataState0 = {isControlDirtyInternal() {}};
+		const oDataState1 = {isControlDirtyInternal() {}};
+		const oDataState2 = {isControlDirtyInternal() {}};
+		const oCompositeDataState = {aDataStates: [oDataState0, oDataState1, oDataState2]};
+		this.mock(DataState.prototype).expects("isControlDirty").on(oCompositeDataState).withExactArgs().returns(false);
+		this.mock(oDataState0).expects("isControlDirtyInternal").withExactArgs().returns(false);
+		this.mock(oDataState1).expects("isControlDirtyInternal").withExactArgs().returns(true);
+		this.mock(oDataState2).expects("isControlDirtyInternal").never(); // stop iteration with first dirty state
+
+		// code under test
+		assert.strictEqual(CompositeDataState.prototype.isControlDirty.call(oCompositeDataState), true);
+	});
+
+	//*********************************************************************************************
+	QUnit.test("isControlDirty: delegate to aggregated data states; not dirty", function(assert) {
+		const oDataState0 = {isControlDirtyInternal() {}};
+		const oDataState1 = {isControlDirtyInternal() {}};
+		const oDataState2 = {isControlDirtyInternal() {}};
+		const oCompositeDataState = {aDataStates: [oDataState0, oDataState1, oDataState2]};
+		this.mock(DataState.prototype).expects("isControlDirty").on(oCompositeDataState).withExactArgs().returns(false);
+		this.mock(oDataState0).expects("isControlDirtyInternal").withExactArgs().returns(false);
+		this.mock(oDataState1).expects("isControlDirtyInternal").withExactArgs().returns(false);
+		this.mock(oDataState2).expects("isControlDirtyInternal").withExactArgs().returns(false);
+
+		// code under test
+		assert.strictEqual(CompositeDataState.prototype.isControlDirty.call(oCompositeDataState), false);
 	});
 });
