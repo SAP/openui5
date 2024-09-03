f98bb44c7317c1e8de6c5ba2e0ec25605af354a0
I310817 <gergana.marcheva@sap.com>
2024-09-03 16:45:07 +0300

    [FIX] sap.ui.unified.Calendar: Calendar with two months in one column is displayed correctly now
    Problem: When there is a limit space and a Calendar with two months displayed one over another, the arrow button for navigating to the next month wasn't visible. The users couldn't interact with the Calendar control.
    
    Root cause: The month button has a very big width and "pushed" the arrow button "outside of the view".
    
    Solution: Detect the "one column" calendar case and add special css class to adjust the button with.
    
    SNOW: CS20240007706463
    CR-Id: 002075125900002691122024
    Change-Id: I413e58d4bf133921a63401795d92706c57c3d036
    (cherry picked from commit 423f82f907005027be448f9fd1453e78c4ee3c93)

--- src/sap.ui.unified/src/sap/ui/unified/Calendar.js
+++ src/sap.ui.unified/src/sap/ui/unified/Calendar.js
@@ -685,7 +685,7 @@ sap.ui.define([
 			this._sLocale = sLocale;
 			this._oLocaleData = undefined;
 			this.invalidate();
-			this._toggleTwoMonthsInTwoColumnsCSS();
+			this._toggleTwoMonthsInColumnsCSS();
 		}
 
 		return this;
@@ -826,7 +826,7 @@ sap.ui.define([
 				oMonth.setSecondaryCalendarType(this._getSecondaryCalendarType());
 				this.addAggregation("month", oMonth);
 			}
-			this._toggleTwoMonthsInTwoColumnsCSS();
+			this._toggleTwoMonthsInColumnsCSS();
 		} else if (aMonths.length > iMonths){
 			for (i = aMonths.length; i > iMonths; i--) {
 				oMonth = this.removeAggregation("month", i - 1);
@@ -836,7 +836,7 @@ sap.ui.define([
 				// back to standard case -> initialize month width
 				this._bInitMonth = true;
 			}
-			this._toggleTwoMonthsInTwoColumnsCSS();
+			this._toggleTwoMonthsInColumnsCSS();
 		}
 
 		if (iMonths > 1 && aMonths[0].getDate()) {
@@ -1291,7 +1291,7 @@ sap.ui.define([
 		}
 		this._setHeaderText(oCalDate);
 		this._setPrimaryHeaderMonthButtonText();
-		this._toggleTwoMonthsInTwoColumnsCSS();
+		this._toggleTwoMonthsInColumnsCSS();
 	};
 
 	Calendar.prototype._updateLegendParent = function(){
@@ -1796,7 +1796,7 @@ sap.ui.define([
 		// change month and year
 		this._updateHeader(oFirstDate);
 		this._setPrimaryHeaderMonthButtonText();
-		this._toggleTwoMonthsInTwoColumnsCSS();
+		this._toggleTwoMonthsInColumnsCSS();
 
 		if (bFireStartDateChange) {
 			this.fireStartDateChange();
@@ -2392,7 +2392,7 @@ sap.ui.define([
 	 * Toggle On or Off CSS class for indicating if calendar is in two columns with two calendars mode
 	 * @private
 	 */
-	Calendar.prototype._toggleTwoMonthsInTwoColumnsCSS = function () {
+	Calendar.prototype._toggleTwoMonthsInColumnsCSS = function () {
 		if (this._isTwoMonthsInTwoColumns()) {
 			if (oCore.getConfiguration().getLocale().getLanguage().toLowerCase() === "ja" ||
 				oCore.getConfiguration().getLocale().getLanguage().toLowerCase() === "zh") {
@@ -2405,6 +2405,12 @@ sap.ui.define([
 		} else {
 			this.removeStyleClass("sapUiCalTwoMonthsTwoColumnsJaZh");
 			this.removeStyleClass("sapUiCalTwoMonthsTwoColumns");
+		}
+
+		if (this._isTwoMonthsInOneColumn()) {
+			this.addStyleClass("sapUiCalTwoMonthsInOneColumn");
+		} else {
+			this.removeStyleClass("sapUiCalTwoMonthsInOneColumn");
 		}
 	};
 
@@ -2649,7 +2655,7 @@ sap.ui.define([
 		}
 
 		this._setPrimaryHeaderMonthButtonText();
-		this._toggleTwoMonthsInTwoColumnsCSS();
+		this._toggleTwoMonthsInColumnsCSS();
 	}
 
 	/**
