84985fc87808308f62c72648e4f50e9b2e864c86
Uwe Reeder <uwe.reeder@sap.com>
2024-09-10 13:40:31 +0200

    [INTERNAL] sap.ui.core.demokit.sample.odata.v4.*: specific MessageBox methods
    PS1: done
    
    Change-Id: I5afd4e5c1eb2ade26079b23b80e0d70d917e2416
    CI: 112

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ConsumeV2Service/Main.controller.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/ConsumeV2Service/Main.controller.js
@@ -16,15 +16,9 @@ sap.ui.define([
 				.then(function () {
 					// Note: refresh is needed as long as there is no cache synchronization
 					oView.getModel().refresh();
-					MessageBox.alert("Data successfully reset", {
-						icon : MessageBox.Icon.SUCCESS,
-						title : "Success"
-					});
+					MessageBox.success("Data successfully reset");
 				}, function (oError) {
-					MessageBox.alert(oError.message, {
-						icon : MessageBox.Icon.ERROR,
-						title : "Error"
-					});
+					MessageBox.error(oError.message);
 				});
 		}
 	});
