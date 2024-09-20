f083088eb002edcb65063bb8b2cd8af2dec15c9d
Nikolay Hristov <n.hristov@sap.com>
2024-09-20 09:35:45 +0300

    [FIX] sap.m.TimePicker: fix cursor position when there is mask and no entry
    Problem: When there is mask set to the TimePicker, and there is no
    value entered, sometimes on focus of the input field, the cursor
    doesn't go to the beginning of the input field (and mask) because
    of unwanted mouse movement during the click of the button.
    
    There was previous fix but as we found lately, it doesn't cover
    all cases.
    
    Solution: If there is mask set and there is nothing entered in the
    TimePicker's input, the cursor is forced to move at the beginning
    of the input field in onmouseup event handler.
    
    Test snippet: #389598
    
    SNOW: CS20240007797996
    Change-Id: I1b9c7b2912ec99e7a8f4fce73482a94a411c6b47
    CR-Id: 002075125900003148402024

--- src/sap.m/test/sap/m/qunit/TimePicker.qunit.js
+++ src/sap.m/test/sap/m/qunit/TimePicker.qunit.js
@@ -3151,7 +3151,8 @@ sap.ui.define([
 
 	QUnit.test("When mask is enabled, focusing the input with mouse moves cursor to the start of the input", function(assert) {
 		// Prepare
-		var oTp = new TimePicker({});
+		var oTp = new TimePicker(),
+			oFakeClock = sinon.useFakeTimers();
 
 		oTp.placeAt("content");
 		// oTp.placeAt("qunit-fixture");
@@ -3166,11 +3167,11 @@ sap.ui.define([
 
 		// Act
 		oTp.onmouseup();
-
-		// Assert
+		oFakeClock.tick(100);
 		assert.equal(oTp._getCursorPosition(), 0, "The cursor position is moved to the start of the input");
 
-		// destroy
+		// Destroy
+		oFakeClock.restore();
 		oTp.destroy();
 		oTp = null;
 	});
