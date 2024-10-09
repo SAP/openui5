588a09cd8090d3b3ee5f2cc04e80f1f24ed244f2
I562693 <vinay.k.n@sap.com>
2024-10-09 15:36:27 +0530

    [FIX] We've now removed sap.m.upload.UploadSetwithTable deprecated control along with its files and folders from the repository, since it was introduced as an experimental control
    The control sap.m.upload.UploadSetwithTable is permanently removed from the repository, which was introduced as the experimental control. Tthe the replacement plugin sap.m.plugins.UploadSetwithTable is introduced from sapui5 version 1.124. The sap.m.upload.UploadSetwithTable control is now deprected from 1.124 and since there are no consumers, with collective decision the control and its related control files/folders are removed.
    
    Change-Id: Icd0773bca132c87aab94633e94fe7c372fed7eb8

--- src/sap.m/src/sap/m/library.js
+++ src/sap.m/src/sap/m/library.js
@@ -401,8 +401,6 @@ sap.ui.define([
 			"sap.m.UploadCollectionToolbarPlaceholder",
 			"sap.m.upload.UploadSet",
 			"sap.m.upload.UploadSetToolbarPlaceholder",
-			"sap.m.upload.UploadSetwithTable",
-			"sap.m.upload.UploadSetwithTableItem",
 			"sap.m.VariantManagement",
 			"sap.m.VBox",
 			"sap.m.ViewSettingsDialog",
@@ -650,8 +648,7 @@ sap.ui.define([
 				},
 				"sap.m.ObjectHeader": {
 					"moveControls": "default"
-				},
-				"sap.m.upload.UploadSetwithTable":"sap/m/upload/p13n/flexibility/UploadSetwithTable"
+				}
 			},
 			//Configuration used for rule loading of Support Assistant
 			"sap.ui.support": {
