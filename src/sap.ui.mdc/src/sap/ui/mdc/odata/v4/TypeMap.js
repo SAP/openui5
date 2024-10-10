5448624768dec12e7abc08d7d6a9329c18d03025
Thomas Buechler <thomas.buechler@sap.com>
2024-10-10 08:21:05 +0200

    [INTERNAL] MDC.TypeMap: Added documentation for Type preloading and ObjectPath.get deprecation
    As sync type loading will no longer work in UI5 2.0, the usage of ObjectPath.get is deprecated. An async alternative will be provided in a future change.
    
    CR-Id: 002075125900003725512024
    SNOW: DINC0264706
    Change-Id: I623bc7e3199e89c743f86139c38815c640faf73b

--- src/sap.ui.mdc/src/sap/ui/mdc/odata/v4/TypeMap.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/odata/v4/TypeMap.js
@@ -11,6 +11,7 @@ sap.ui.define([
 	/**
 	* @class ODataV4-specific {@link sap.ui.mdc.util.TypeMap TypeMap} configuration.
 	*
+	* <b>Note:</b> The modules of all data types registered in a <code>TypeMap</code> must be loaded in advance.<br>
 	* <b>Note:</b> This {@link sap.ui.mdc.util.TypeMap TypeMap} implementation contains the following types including ODataV4-specific configuration:
 	*
 	* <ul>
