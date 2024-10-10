fab751ec8113c0f23db8c0a33efd80d0879026cf
plamenivanov91 <plamen.ivanov01@sap.com>
2024-10-10 18:38:27 +0300

    [FIX] sap.m.Breadcrumbs: Fixed disabled Links tab order
    Problem: sap.m.Link controls with property enabled set to false are
    included in the tab chain.
    
    Solution: Disabled links are no longer included in the ItemNavigation
    object, therefore in the tab chain.
    
    SNOW: DINC0018999
    Change-Id: I833bd9608133459519a313109c6b0ac7ba6d2d2b

--- src/sap.m/src/sap/m/Breadcrumbs.js
+++ src/sap.m/src/sap/m/Breadcrumbs.js
@@ -752,18 +752,21 @@ sap.ui.define([
 			iSelectedDomIndex = -1,
 			aItemsToNavigate = this._getItemsToNavigate(),
 			aNavigationDomRefs = [],
+			bIsDisabledLink = false,
 			oItemDomRef;
 
 		if (aItemsToNavigate.length === 0) {
 			return;
 		}
 
-		aItemsToNavigate.forEach(function (oItem, iIndex) {
+		aItemsToNavigate.forEach(function (oItem) {
 			oItemDomRef = oItem.getFocusDomRef();
 			if (oItemDomRef) {
-				oItemDomRef.setAttribute("tabindex", iIndex === 0 ? "0" : "-1");
+				bIsDisabledLink = oItem.isA("sap.m.Link") && !oItem.getEnabled();
+				if (!bIsDisabledLink) {
+					aNavigationDomRefs.push(oItemDomRef);
+				}
 			}
-			aNavigationDomRefs.push(oItem.getFocusDomRef());
 		});
 
 		this.addDelegate(oItemNavigation);
