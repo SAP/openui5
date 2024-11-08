6fd9c5ec75d8409c388a19b8f33ec495de672c30
Knut Thies <knut.thies@sap.com>
2024-11-08 09:23:45 +0100

    [INTERNAL] sap.ui.model.odata.ODataMetaModel#fetchCodeList: handle 503 "Retry-After"
    Test:
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/common/index.html?component=ViewTemplate.valueList&realOData=true&sap-value-list=none
    new change based on 6306068 PS16
    
    PS1: Enhance existing integrationtest + first POC
    PS2: add manual integration test for currency codes
    PS3: rebase, resolve conflicts
    PS4 VSP: Review comment; Remove server URL from metadata document
    PS5: eliminate ODataMetaModel#clearCodeListsCache, use *withCodeLists.metadata.xml
    PS6+7: remove not needed clearCaches(), remove TODO
    PS8 VSP/KTH: TDD ODataMetaModel#fetchCodeList
    PS9 VSP/KTH: TDD ODataModel#_request
    PS10: review comments
    PS11: review comments SSC, fix missing UI changes
    PS12: review comments PKS
    PS13: revert UI changes
    PS14: remove not required define
    PS15: PP PKS/KTH: alternative way to test correct "bind"
    
    Change-Id: I40093c47b142b2acaf9b644eb4b5d0aa31d85b37
    JIRA: CPOUI5MODELS-1826
    JIRA: CPOUI5MODELS-1846
    JIRA: CPOUI5MODELS-1849
    JIRA: CPOUI5MODELS-1850

--- src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
+++ src/sap.ui.core/src/sap/ui/model/odata/v2/ODataModel.js
@@ -5101,11 +5101,14 @@ sap.ui.define([
 	 *   supports message scope.
 	 * @param {boolean} [bSideEffects]
 	 *   Whether the request is to read side effects
+	 * @param {function} [fnRequest]
+	 *   Request function which can handle 503 "Retry-After" responses,
+	 *   see {@link sap.ui.model.odata.v2.ODataModel#_request}
 	 * @return {object} Request object
 	 * @private
 	 */
 	ODataModel.prototype._createRequest = function(sUrl, sDeepPath, sMethod, mHeaders, oData, sETag,
-			bAsync, bUpdateAggregatedMessages, bSideEffects) {
+			bAsync, bUpdateAggregatedMessages, bSideEffects, fnRequest) {
 		var oRequest;
 
 		bAsync = bAsync !== false;
@@ -5165,6 +5168,9 @@ sap.ui.define([
 		};
 		if (bSideEffects) {
 			oRequest.sideEffects = true;
+		}
+		if (fnRequest) {
+			oRequest.fnRequest = fnRequest;
 		}
 		if (oData) {
 			oRequest.data = oData;
@@ -5947,12 +5953,14 @@ sap.ui.define([
 	 *   The parameters as specified in {@link #read}
 	 * @param {boolean} [bSideEffects]
 	 *   Whether to read data as side effects
+	 * @param {function} [fnRequest] Request function which can handle 503 "Retry-After" responses,
+	 *   see {@link sap.ui.model.odata.v2.ODataModel#_request}
 	 * @return {object}
 	 *   An object which has an <code>abort</code> function to abort the current request.
 	 *
 	 * @private
 	 */
-	 ODataModel.prototype._read = function(sPath, mParameters, bSideEffects) {
+	 ODataModel.prototype._read = function(sPath, mParameters, bSideEffects, fnRequest) {
 		var bCanonical, oContext, fnError, aFilters, sGroupId, mHeaders, sMethod, oRequest,
 			aSorters, fnSuccess, bUpdateAggregatedMessages, aUrlParams, mUrlParams,
 			that = this;
@@ -6016,7 +6024,7 @@ sap.ui.define([
 			sUrl = that._createRequestUrlWithNormalizedPath(sResourcePath, aUrlParams,
 				that.bUseBatch);
 			oRequest = that._createRequest(sUrl, sDeepPath, sMethod, mHeaders, null, /*sETag*/undefined,
-				undefined, bUpdateAggregatedMessages, bSideEffects);
+				undefined, bUpdateAggregatedMessages, bSideEffects, fnRequest);
 
 			mRequests = that.mRequests;
 			if (sGroupId in that.mDeferredGroups) {
@@ -7839,6 +7847,13 @@ sap.ui.define([
 	ODataModel.prototype._request = function(oRequest, fnSuccess, fnError, oHandler, oHttpClient, oMetadata,
 			bSkipHandleTracking) {
 		var oRequestHandle;
+
+		if (oRequest.fnRequest) {
+			const fnRequest = oRequest.fnRequest;
+			delete oRequest.fnRequest;
+			return fnRequest(oRequest, fnSuccess, fnError, oHandler, oHttpClient, oMetadata,
+				/*bSkipHandleTracking*/true);
+		}
 
 		if (this.bDestroyed) {
 			return {
