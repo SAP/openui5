73002ddfb721519416d46c707b8c1b24ae4a35bf
Matthias Osswald <mat.osswald@sap.com>
2024-11-08 12:07:57 +0100

    [INTERNAL] Walkthrough: Introduce test starter
    JIRA: CPOUI5FOUNDATION-819
    Change-Id: I9ea472651a873264c34578cbc9ebbd5ea46df95e

--- src/sap.m/test/sap/m/demokit/tutorial/walkthrough/29/webapp/test/integration/opaTests.qunit.js
+++ src/sap.m/test/sap/m/demokit/tutorial/walkthrough/29/webapp/test/integration/opaTests.qunit.js
@@ -1,18 +1,3 @@
-/*global QUnit*/
-
-QUnit.config.autostart = false;
-
-sap.ui.require(["sap/ui/core/Core"], async (Core) => {
-	"use strict";
-
-	await Core.ready();
-
-	sap.ui.require([
-		"ui5/walkthrough/localService/mockserver",
-		"ui5/walkthrough/test/integration/NavigationJourney"
-	], (mockserver) => {
-		// initialize the mock server
-		mockserver.init();
-		QUnit.start();
-	});
-});
+sap.ui.define([
+	"./NavigationJourney"
+]);
