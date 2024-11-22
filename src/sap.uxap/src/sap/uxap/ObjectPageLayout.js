74b1d54cc8a95c4e2c1881e6baa5de3fc3c3846c
PetyaMarkovaBogdanova <petya.markova@sap.com>
2024-11-22 19:48:42 +0200

    [FIX] sap.uxap.ObjectPageLayout: Calc FIX
    Problem: In certain situation, when the DOM element
    of ObjectPageLayout is still not rendered/ rendered
    in hidden parent element, onBefore rendering
    we are storing the scrolling offset based on its
    current scroll position (0) minus the stored
    position of the targeted subsection, so we end up
    with negative scroll position and wrong stored
    section.
    
    Solution: We double check for real DOM element
    painted on the document.
    
    SNOW: CS20240008278829
    Change-Id: I534f67475c89b8ac316e0c58cc8937dc8612d2fd

--- src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
+++ src/sap.uxap/src/sap/uxap/ObjectPageLayout.js
@@ -4484,9 +4484,10 @@ sap.ui.define([
 	 * @private
 	 */
 	ObjectPageLayout.prototype._storeScrollLocation = function () {
+		var domRef = this.getDomRef() && this.$().width() && this.$().height();
 
-		if (!this.getDomRef() || !this._bDomReady || !this._oScroller) {
-			return;
+		if (!domRef || !this._bDomReady || !this._oScroller) {
+			return false;
 		}
 
 		this.triggerPendingLayoutUpdates();
@@ -4507,6 +4508,7 @@ sap.ui.define([
 			iOffset: iScrollTopWithinScrolledSubSection
 		};
 		this._oCurrentTabSection = null;
+		return this._oStoredScrolledSubSectionInfo;
 	};
 
 	ObjectPageLayout.prototype.onkeyup = function (oEvent) {
