fb9da2018841fbdc363900eab6a7ddef9936c51d
Frank Weigel <frank.weigel@sap.com>
2024-12-01 10:06:35 +0100

    [INTERNAL] sap.m/sap.ui.core: fix linter / future fatal issues
     - components should declare in their metadata that they have a manifest
     - don't refer to non-existing event handlers in XMLViews
    
    Change-Id: I8e5cb66365b2038e2c177583b227d8e41bf53dc1

--- src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/scenario16/embeddingLib/embeddedComponent/Component.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/testdata/libraries/scenario16/embeddingLib/embeddedComponent/Component.js
@@ -2,5 +2,9 @@ sap.ui.define([
 	"sap/ui/core/UIComponent"
 ], function(UIComponent) {
 	"use strict";
-	return UIComponent.extend("testlibs.scenario16.embeddingLib.embeddedComponent.Component");
+	return UIComponent.extend("testlibs.scenario16.embeddingLib.embeddedComponent.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
 });
