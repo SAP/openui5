882d49a43fa410be00ccad2a6ad6c828127e5d67
yanaminkova <yana.minkova@sap.com>
2024-12-03 15:32:35 +0200

    [INTERNAL] DemoKit: Update FCL tutorial
    The namespace for the sap.f.tutorial.fiori2 has been updated
    to sap.f.tutorial.fcl
    Additionally, the file paths in index.html have been corrected,
    ensuring that the samples now load properly.
    
    JIRA: BGSOFUIPIRIN-6271
    Change-Id: I6e54114ad32c9a6a32e4905cc2d574aabb04caa6

---
+++ src/sap.f/test/sap/f/demokit/tutorial/fcl/07/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], (UIComponent) => {
+	"use strict";
+	return UIComponent.extend("sap.f.tutorial.fcl.07.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
