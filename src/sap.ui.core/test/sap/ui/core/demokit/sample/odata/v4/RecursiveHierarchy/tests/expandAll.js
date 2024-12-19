175ade730a3aa2ceefc46e6e123afba8f8b2589f
I749909 <joshua.albrecht@sap.com>
2024-12-19 14:25:49 +0100

    [INTERNAL] sap.ui.core.demokit.sample.odata.v4.RecursiveHierarchy: Added test expandCollapseAll
    PS1: init
    PS2-3: code review
    
    JIRA: CPOUI5ODATAV4-2807
    Change-Id: If1376aaf3830d1267dc9f54c7bee9e06573009eb

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/RecursiveHierarchy/tests/expandAll.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/v4/RecursiveHierarchy/tests/expandAll.js
@@ -9,7 +9,7 @@ sap.ui.define([
 	"use strict";
 
 	return function (Given, When, Then) {
-		function checkTable(vExpected, sComment) {
+		function checkTable(sComment, vExpected) {
 			Then.onTheMainPage.checkTable(vExpected, null, sComment);
 		}
 
@@ -36,81 +36,71 @@ sap.ui.define([
 
 		Then.onAnyPage.iTeardownMyUIComponentInTheEnd();
 
-		checkTable(`
-+ 0`,
-			"initial");
+		checkTable("initial", `
++ 0`);
 
 		toggleExpand("0", "expand 0 (Alpha)");
-		checkTable(`
+		checkTable("after expand 0 (Alpha)", `
 - 0
 	+ 1
 	* 2
 	* 3
-	+ 4`,
-			"after expand 0 (Alpha)");
+	+ 4`);
 
 		toggleExpand("0", "collapse 0 (Alpha)");
-		checkTable(`
-+ 0`,
-			"after collapse 0 (Alpha)");
+		checkTable("after collapse 0 (Alpha)", `
++ 0`);
 
 		expandAll("0", "expand all below 0 (Alpha)");
-		checkTable(`
+		checkTable("after expand all below 0 (Alpha)", `
 - 0
 	- 1
 		- 1.1
 			* 1.1.1
-			* 1.1.2`,
-			"after expand all below 0 (Alpha)");
+			* 1.1.2`);
 
 		scrollToRow(11, "scroll to 4 (Mu)");
-		checkTable(`
+		checkTable("after scroll to 4 (Mu)", `
 	- 4
 		* 4.1
 	- 5
 		- 5.1
-			* 5.1.1`,
-			"after scroll to 4 (Mu)");
+			* 5.1.1`);
 
 		scrollToRow(0, "scroll to 0 (Alpha)");
-		checkTable(`
+		checkTable("after scroll to 0 (Alpha)", `
 - 0
 	- 1
 		- 1.1
 			* 1.1.1
-			* 1.1.2`,
-			"after scroll to 0 (Alpha)");
+			* 1.1.2`);
 
 		toggleExpand("1.1", "collapse 1.1 (Gamma)");
-		checkTable(`
+		checkTable("after collapse 1.1 (Gamma)", `
 - 0
 	- 1
 		+ 1.1
 		- 1.2
-			* 1.2.1`,
-			"after collapse 1.1 (Gamma)");
+			* 1.2.1`);
 
 		toggleExpand("1", "collapse 1 (Beta)");
-		checkTable(`
+		checkTable("after collapse 1 (Beta)", `
 - 0
 	+ 1
 	* 2
 	* 3
-	- 4`,
-			"after collapse 1 (Beta)");
+	- 4`);
 
 		toggleExpand("0", "collapse 0 (Alpha)");
-		checkTable(`
-+ 0`,
-			"after collapse 0 (Alpha)");
+		checkTable("after collapse 0 (Alpha)", `
++ 0`);
 
 		expandAll("0", "expand all below 0 (Alpha)");
-		checkTable(`
+		checkTable("after expand all below 0 (Alpha)", `
 - 0
 	- 1
 		- 1.1
 			* 1.1.1
-			* 1.1.2`,
-			"after expand all below 0 (Alpha)");
+			* 1.1.2`);
 	};
 });
