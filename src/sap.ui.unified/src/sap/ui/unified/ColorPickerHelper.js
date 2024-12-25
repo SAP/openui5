8fc0c954e2260ca901422cffdc1a99c0387882a7
Todor Stoyanov <t.stoyanov@sap.com>
2024-12-25 11:22:47 +0100

    [INTERNAL] sap.ui.unified.ColorPickerHelper: use direct references to the sap/m controls
    * this change is directly related to - 5975683
      and will have to be added in version 2.0
    
    * Version 2.0 uses a hard dependency to sap/m controls.
      Since ColorPicker no longer exists in sap.ui.commons there is no need
    to check from which library to load internal controls
    
    JIRA: BGSOFUIBALKAN-7005
    Change-Id: Iae1bff5aed8ed91135207502830f02cf5ac4d973

--- src/sap.ui.unified/src/sap/ui/unified/ColorPickerHelper.js
+++ src/sap.ui.unified/src/sap/ui/unified/ColorPickerHelper.js
@@ -4,112 +4,50 @@
 
 // Provides helper sap.ui.unified.ColorPickerHelper.
 sap.ui.define([
-	'sap/ui/core/Lib',
-	"sap/m/Label",
-	"sap/m/InputBase",
-	"sap/m/Slider",
-	"sap/m/RadioButtonGroup",
-	"sap/m/RadioButton",
-	"sap/m/Button",
-	"sap/ui/commons/Label",
-	"sap/ui/commons/TextField",
-	"sap/ui/commons/Slider",
-	"sap/ui/commons/RadioButtonGroup",
-	"sap/ui/core/Item"
+	'sap/m/Button',
+	'sap/m/Label',
+	'sap/m/InputBase',
+	'sap/m/Slider',
+	'sap/m/RadioButton',
+	'sap/m/RadioButtonGroup'
 ], function(
-	Library,
+	Button,
 	Label,
 	InputBase,
 	Slider,
-	RadioButtonGroup,
 	RadioButton,
-	Button,
-	CommonsLabel,
-	TextField,
-	CommonsSlider,
-	CommonsRadioButtonGroup,
-	Item
+	RadioButtonGroup
 ) {
 	"use strict";
 	var ColorPickerHelper;
 
-	var oMLibraryLoad = {
-		isResponsive: function () {
-			return true;
-		},
-		factory: {
-			createLabel: function (mConfig) {
-				return new Label(mConfig);
-			},
-			createInput: function (sId, mConfig) {
-				return new InputBase(sId, mConfig);
-			},
-			createSlider: function (sId, mConfig) {
-				return new Slider(sId, mConfig);
-			},
-			createRadioButtonGroup: function (mConfig) {
-				return new RadioButtonGroup(mConfig);
-			},
-			createRadioButtonItem: function (mConfig) {
-				return new RadioButton(mConfig);
-			},
-			createButton: function (sId, mConfig) {
-				return new Button(sId, mConfig);
-			}
-		}
-	};
-
-	var oCommonsLibraryLoad = {
-		isResponsive: function () {
-			return false;
-		},
-		factory: {
-			createLabel: function (mConfig) {
-				return new CommonsLabel(mConfig);
-			},
-			createInput: function (sId, mConfig) {
-				return new TextField(sId, mConfig);
-			},
-			createSlider: function (sId, mConfig) {
-				if (mConfig && mConfig.step) {
-					mConfig.smallStepWidth = mConfig.step;
-					delete mConfig.step;
-				}
-				return new CommonsSlider(sId, mConfig);
-			},
-			createRadioButtonGroup: function (mConfig) {
-				if (mConfig && mConfig.buttons) {
-					mConfig.items = mConfig.buttons;
-					delete mConfig.buttons;
-				}
-				return new CommonsRadioButtonGroup(mConfig);
-			},
-			createRadioButtonItem: function (mConfig) {
-				return new Item(mConfig);
-			}
-		}
-	};
-
-	var oErrorLibraryLoad = {
-		isResponsive: function () { return false; },
-			factory: {
-				createLabel:  function () { throw new Error("no Label control available"); },
-				createInput:  function () { throw new Error("no Input control available"); },
-				createSlider: function () { throw new Error("no Slider control available"); },
-				createRadioButtonGroup: function () { throw new Error("no RadioButtonGroup control available"); },
-				createRadioButtonItem: function () { throw new Error("no RadioButtonItem control available"); }
-			}
-	};
-
 	ColorPickerHelper = {
 		getHelper: function () {
-			if (Library.isLoaded("sap.m")) {
-				return oMLibraryLoad;
-			} else if (Library.isLoaded("sap.ui.unified")) {
-				return  oCommonsLibraryLoad;
-			}
-
-			return oErrorLibraryLoad;
+			return {
+				isResponsive: function () {
+					return true;
+				},
+				factory: {
+					createLabel: function (mConfig) {
+						return new Label(mConfig);
+					},
+					createInput: function (sId, mConfig) {
+						return new InputBase(sId, mConfig);
+					},
+					createSlider: function (sId, mConfig) {
+						return new Slider(sId, mConfig);
+					},
+					createRadioButtonGroup: function (mConfig) {
+						return new RadioButtonGroup(mConfig);
+					},
+					createRadioButtonItem: function (mConfig) {
+						return new RadioButton(mConfig);
+					},
+					createButton: function (sId, mConfig) {
+						return new Button(sId, mConfig);
+					}
+				}
+			};
 		}
 	};
 
