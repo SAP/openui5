042728bdc6a2475f5998161a99809ced1fd3dab5
Jiawei Cao <jiawei.cao@sap.com>
2024-12-30 11:51:10 +0100

    [FIX] core/Element|Popup: get DOMRef after calling parent "destroy"
    - the call of "getDomRef" in Element.prototype.destroy was moved
      accidentally. In case the control is rendered synchronously within the
      "exit" method, the newly created DOM can't be removed anymore.
    
    - the unnecessary rendering is also avoided now in
      Popup.prototype._isFocusInsidePopup
    
    Change-Id: I08eb2451d45247d152ab85635fda1af88b794d1a
    SNOW: DINC0353296

--- src/sap.ui.core/src/sap/ui/core/Element.js
+++ src/sap.ui.core/src/sap/ui/core/Element.js
@@ -936,7 +936,6 @@ sap.ui.define([
 		// update the focus information (potentially) stored by the central UI5 focus handling
 		Element._updateFocusInfo(this);
 
-		var oDomRef = this.getDomRef();
 
 		ManagedObject.prototype.destroy.call(this, bSuppressInvalidate);
 
@@ -944,6 +943,7 @@ sap.ui.define([
 		this.data = noCustomDataAfterDestroy;
 
 		// exit early if there is no control DOM to remove
+		var oDomRef = this.getDomRef();
 		if (!oDomRef) {
 			return;
 		}
