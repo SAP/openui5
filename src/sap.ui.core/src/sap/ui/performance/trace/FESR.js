e282a803d39cfda7f55e570c3ee938847fcd9e5d
Johannes Gluch <johannes.gluch@sap.com>
2025-01-02 10:15:40 +0100

    [INTERNAL] sap.ui.Device: Add support for Client Hints
    Provide private API to retrieve the platform information from the client
    and use them within FESR to improve the reporting since the useragent
    does not provide platform version information for windows 11 and
    upcoming versions.
    
    JIRA: CPOUI5FRAMEWORK-729
    SNOW: CS20240008950088
    CR-Id: 002075125800000172922025
    Change-Id: I20d02c73b062ca9dbe637481ebc6a1e8511b2e32

--- src/sap.ui.core/src/sap/ui/performance/trace/FESR.js
+++ src/sap.ui.core/src/sap/ui/performance/trace/FESR.js
@@ -305,6 +305,9 @@ sap.ui.define([
 			Interaction.onInteractionStarted = onInteractionStarted;
 			Interaction.onInteractionFinished = onInteractionFinished;
 			Interaction.passportHeader = wmPassportHeader;
+			Device.os.getPlatformInfo().then(function(platform) {
+				CLIENT_OS = platform.name + "_" + platform.version;
+			});
 		} else if (!bActive && bFesrActive) {
 			bFesrActive = false;
 			Interaction.setActive(false);
