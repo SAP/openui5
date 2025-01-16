bfa6dd5a7b3359d2c69f803b1eac635d9c5feed0
Martin R. Hristov <m.hristov@sap.com>
2025-01-16 16:47:31 +0200

    [FIX] sap.m.Input: preserve value after rerendering of password field
    To avoid adding the value attribute directly to the input DOM reference, we should remove it from the renderer and manage its synchronization through JavaScript logic. However, since the input value is updated on focus or Enter key press, we require a "live value" property. This can be achieved under the assumption that the input field always uses renderer version >=2. When having the string rendering or a DOM flush is performed, the entire DOM is replaced, making detection within the control not technically possible.
    
    The solution retains the current operational logic but removes the attribute during the after rendering hook to maintain functionality seamlessly.
    
    SNOW: DINC0376300
    Change-Id: I4954e6e76da317d5bb77491bebc16783084ba161

--- src/sap.m/src/sap/m/Input.js
+++ src/sap.m/src/sap/m/Input.js
@@ -628,8 +628,6 @@ function(
 		this._setTypedInValue("");
 		this._bDoTypeAhead = false;
 		this._bBackspaceOrDelete = false;
-		this._isValueInitial = false;
-		this._previousInputType = this.getType();
 
 		// indicates whether input is clicked (on mobile) or the clear button
 		// used for identifying whether dialog should be open.
@@ -709,10 +707,6 @@ function(
 
 		InputBase.prototype.onBeforeRendering.call(this);
 
-		if (!this.getDomRef() && this.getValue()) {
-			this._isValueInitial = true;
-		}
-
 		if (this.getShowClearIcon()) {
 			this._getClearIcon().setProperty("visible", bShowClearIcon);
 		} else if (this._oClearButton) {
@@ -770,12 +764,14 @@ function(
 	Input.prototype.onAfterRendering = function() {
 		InputBase.prototype.onAfterRendering.call(this);
 
-		if ((this._isValueInitial || this.getType() !== this._previousInputType ) && this.getType() === InputType.Password ) {
-			this.getDomRef("inner").value = this.getProperty("value");
-			this._isValueInitial = false;
-		}
+		// workaround to remove value attribute when having input type password
+		if (this.getType() === InputType.Password) {
+			const innerRef = this.getDomRef("inner");
+			const value = innerRef.value;
 
-		this._previousInputType = this.getType();
+			innerRef.removeAttribute("value");
+			innerRef.value = value;
+		}
 	};
 
 	/**
