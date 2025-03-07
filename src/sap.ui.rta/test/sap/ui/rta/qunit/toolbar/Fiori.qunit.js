1e51f9f2bc26e778a6f35b6714047ef02e303e8f
Christoph Szabo <christoph.szabo@sap.com>
2025-03-07 14:11:14 +0100

    [INTERNAL][FIX] sap.ui.rta: fix SAP logo dimension of toolbar could be 0px
    Fixing of a rare case where the SAP logo dimension of the toolbar could be 0px.
    Added a qunit test to make sure 0 is supported and is correctly altered for the creation of the image.
    
    SNOW: CS20250009227086
    Change-Id: I5e8154a6d7334af4234cffa3ee61d8a2e830c137

--- src/sap.ui.rta/test/sap/ui/rta/qunit/toolbar/Fiori.qunit.js
+++ src/sap.ui.rta/test/sap/ui/rta/qunit/toolbar/Fiori.qunit.js
@@ -14,6 +14,7 @@ sap.ui.define([
 	"sap/ui/rta/Utils",
 	"sap/m/Image",
 	"sap/base/Log",
+	"sap/ui/core/Core",
 	"sap/ui/thirdparty/sinon-4",
 	"sap/ui/qunit/utils/nextUIUpdate"
 ], function(
@@ -30,6 +31,7 @@ sap.ui.define([
 	RtaUtils,
 	Image,
 	Log,
+	Core,
 	sinon,
 	nextUIUpdate
 ) {
@@ -154,6 +156,30 @@ sap.ui.define([
 				});
 				this.oToolbar.destroy();
 			}.bind(this));
+		});
+		QUnit.test("when buildControls is called and image dimensions are 0", async function(assert) {
+			this.oToolbar = new Fiori({
+				textResources: Lib.getResourceBundleFor("sap.ui.rta")
+			});
+			this.oToolbar.setModel(this.oToolbarControlsModel, "controls");
+			sinon.stub(this.oToolbar._oFioriHeader.getDomRef(), "querySelector")
+			.callThrough()
+			.withArgs("#shell-header-icon")
+			.returns(document.createElement("div"));
+
+			await this.oToolbar.onFragmentLoaded();
+			this.oToolbar.show();
+			assert.strictEqual(
+				Core.byId(`${this.oToolbar.getId()}_fragment--sapUiRta_icon`).getWidth(),
+				"0px",
+				"Image should have a width of 0px"
+			);
+			assert.strictEqual(
+				Core.byId(`${this.oToolbar.getId()}_fragment--sapUiRta_icon`).getHeight(),
+				"0px",
+				"Image should have a height of 0px"
+			);
+			this.oToolbar.destroy();
 		});
 	});
 
