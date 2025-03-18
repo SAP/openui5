29a3a915d6b0da11caa944c9e2741cb3e9d790b9
Petar Dimov <petar.dimov@sap.com>
2025-03-18 10:38:37 +0200

    [INTERNAL] sap.ui.support.CoreFace: Deprecate tests testing deprecating methods
    JIRA: BGSOFUIRODOPI-3432
    Change-Id: I6f43ff94bacc9b9e968317a8e95c98fcec591f3f

--- src/sap.ui.support/test/sap/ui/support/qunit/CoreFacade.qunit.js
+++ src/sap.ui.support/test/sap/ui/support/qunit/CoreFacade.qunit.js
@@ -8,16 +8,23 @@ sap.ui.define([
 		QUnit.module("CoreFacade API test");
 
 		QUnit.test("Fixed public methods", function (assert) {
+			var cf = CoreFacade(),
+				getUIAreasIsAMethod = cf.getUIAreas && typeof cf.getUIAreas === "function",
+				getComponentsIsAMethod = cf.getComponents && typeof cf.getComponents === "function";
 
+			assert.ok(getUIAreasIsAMethod, " should not be changed");
+			assert.ok(getComponentsIsAMethod, " should not be changed");
+		});
+
+		/**
+		 * @deprecated As of version 1.118.0
+		 */
+		QUnit.test("Deprecated public methods", function (assert) {
 			var cf = CoreFacade(),
 				getMetadataIsAMethod = cf.getMetadata && typeof cf.getMetadata === "function",
-				getUIAreasIsAMethod = cf.getUIAreas && typeof cf.getUIAreas === "function",
-				getComponentsIsAMethod = cf.getComponents && typeof cf.getComponents === "function",
 				getModelsIsAMethod = cf.getModels && typeof cf.getModels === "function";
 
 			assert.ok(getMetadataIsAMethod, " should not be changed");
-			assert.ok(getUIAreasIsAMethod, " should not be changed");
-			assert.ok(getComponentsIsAMethod, " should not be changed");
 			assert.ok(getModelsIsAMethod, " should not be changed");
 		});
 	});
