1b3c8149fa0fe16ad297c49013868edb7ee4a8ee
Alex Xiong <a.xiong@sap.com>
2025-04-18 09:27:50 +0800

    [INTERNAL][FIX] sap.ui.integration: Card editor - csrf lost in data provider
    SNOW: CS20250009560772
    Change-Id: Ib02bbc64c486777c4073ee7a116d04314304a0f3

--- src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/data/csrf/dt/Configuration.js
+++ src/sap.ui.integration/test/sap/ui/integration/demokit/cardExplorer/webapp/samples/data/csrf/dt/Configuration.js
@@ -9,7 +9,32 @@ sap.ui.define(["sap/ui/integration/Designtime"], function (
 					"title": {
 						"label": "Title",
 						"manifestpath": "/sap.card/header/title"
-					}
+					},
+					"products": {
+                        "manifestpath": "/sap.card/configuration/parameters/products/value",
+                        "label": "String Array (MultiInput)",
+                        "type": "string[]",
+                        "values": {
+                           "data": {
+								"request": {
+									"url": "{{destinations.ProductsMockServerWithCSRF}}/Products",
+									"parameters": {
+										"$format": "json",
+										"$top": 4
+									},
+									"method": "GET",
+									"headers": {
+										"X-CSRF-Token": "{csrfTokens>/token1/value}"
+									}
+								},
+								"path": "/data"
+							},
+							"item": {
+								"text":"{Name}",
+								"key":"{Name}"
+							}
+						}
+                    }
 				}
 			},
 			"preview": {
