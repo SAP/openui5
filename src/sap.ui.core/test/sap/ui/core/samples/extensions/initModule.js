fb8468048595e596c9e32899ee280ee27366ff20
Jan Mummenthaler <jan.mummenthaler@sap.com>
2025-05-09 10:34:52 +0200

    [FIX] Controller: remove static functions
    registerExtensionProvider already moved to ControllerExtensionProvider.
    It never should have been made public as the only allowed usage is for
    sap.ui.fl. Only one Provider is supported so every other usage will
    break flexibility.
    Therefore we make it private and restricted and change the existing
    usages to the ControllerExtensionProvider#registerExtensionProvider.
    
    extendByMember is only used internally and must no be exposed.
    
    JIRA: CPOUI5FRAMEWORK-768
    CR-Id: 002075125900002027852025
    SNOW: n/a
    Change-Id: I053c0f5629e785688948118205921c553fe96ca1

--- src/sap.ui.core/test/sap/ui/core/samples/extensions/initModule.js
+++ src/sap.ui.core/test/sap/ui/core/samples/extensions/initModule.js
@@ -1,8 +1,8 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/core/mvc/XMLView"], function(Controller, XMLView) {
+sap.ui.define(["sap/ui/core/mvc/ControllerExtensionProvider", "sap/ui/core/mvc/XMLView"], function(ControllerExtensionProvider, XMLView) {
     "use strict";
 
     //register an extension provider to simulate a real extension coming from flex
-    Controller.registerExtensionProvider("sample.ExtensionProvider");
+    ControllerExtensionProvider.registerExtensionProvider("sample.ExtensionProvider");
 
     //add an async view
     XMLView.create({
