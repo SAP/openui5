698c5a892c8e928ea1942b7c0922011013264b40
Plamen Ivanov <plamen.ivanov01@sap.com>
2025-05-19 12:38:01 +0300

    [INTERNAL] sap.m.IllustratedMessage: Updated default set enum types
    The default IllustratedMessageType enum file has been updated with the
    latest deprecated APIs and their respective recommended substitutions.
    
    Also some of the redundant Demo Kit samples have been removed.
    
    The IllustratedMessageInPage Demo Kit sample has been updated with the
    all of the default illustration set types (excluding the deprecated ones).
    
    CR-Id: 002075125900002298172025
    SNOW: DINC0509264
    SNOW: DINC0510121
    Change-Id: Ie1d351d4919889d0ed8d301e2f6ad34c10c39764

--- src/sap.m/src/sap/m/IllustratedMessageType.js
+++ src/sap.m/src/sap/m/IllustratedMessageType.js
@@ -150,12 +150,14 @@ sap.ui.define(["sap/ui/base/DataType"], function(DataType) {
 		/**
 		 * "Balloon Sky" illustration type.
 		 * @public
+		 * @deprecated As of version 1.136, replaced by {@link sap.m.IllustratedMessageType.ReceiveAppreciation}
 		 */
 		BalloonSky: "sapIllus-BalloonSky",
 
 		/**
 		 * "Connection" illustration type.
 		 * @public
+		 * @deprecated As of version 1.136, replaced by {@link sap.m.IllustratedMessageType.UnableToLoad}
 		 */
 		Connection: "sapIllus-Connection",
 
@@ -283,6 +285,7 @@ sap.ui.define(["sap/ui/base/DataType"], function(DataType) {
 		/**
 		 * "Simple Connection" illustration type.
 		 * @public
+		 * @deprecated As of version 1.136, replaced by {@link sap.m.IllustratedMessageType.UnableToLoad}
 		 */
 		SimpleConnection: "sapIllus-SimpleConnection",
 
