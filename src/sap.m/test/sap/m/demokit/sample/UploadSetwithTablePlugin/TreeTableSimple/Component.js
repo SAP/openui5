a69e4f8cd702013130fc9fc0cc5d99c2398738ae
Vinay K N <vinay.k.n@sap.com>
2025-06-03 13:58:02 +0530

    [INTERNAL] We've now added a demokit sample for UploadSetwithTable plugin with the Tree Table control
    Change-Id: I2b75f91ef628aa7dbf509a8413ad0a3edbf3a795

---
+++ src/sap.m/test/sap/m/demokit/sample/UploadSetwithTablePlugin/TreeTableSimple/Component.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/core/UIComponent"],
+	function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("tree.table.sample", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
