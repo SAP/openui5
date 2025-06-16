2836997c203c61f0bcd822efeb4a3357df0806dc
Patric Ksinsik <p.ksinsik@sap.com>
2025-06-16 15:58:14 +0200

    [INTERNAL] sap/ui/core/demokit/sample/odata/types/v2/DateTime: Use JSONModel
    ... instead of SandboxModel/ODataModel
    
    App link:
    http://localhost:8080/test-resources/sap/ui/core/demokit/sample/common/index.html?component=odata.types.v2.DateTime
    
    PS1: Implement
    PS2: Remove Component.exit
    PS3: Review comments
    PS4: Remove superfluous title
    PS5: Add missing sap.ui.core lib to manifest.json
    
    Change-Id: Id4d6a25e25c24133757be555f4fc59d30174bb82
    JIRA: CPOUI5MODELS-1907

--- src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/types/v2/DateTime/Component.js
+++ src/sap.ui.core/test/sap/ui/core/demokit/sample/odata/types/v2/DateTime/Component.js
@@ -1,8 +1,10 @@
 /*!
  * ${copyright}
  */
-sap.ui.define(["sap/ui/core/UIComponent"],
-	function (UIComponent) {
+sap.ui.define([
+	"sap/base/i18n/Localization",
+	"sap/ui/core/UIComponent"
+], function (Localization, UIComponent) {
 	"use strict";
 
 	return UIComponent.extend("sap.ui.core.sample.odata.types.v2.DateTime.Component", {
@@ -11,8 +13,13 @@ sap.ui.define(["sap/ui/core/UIComponent"],
 			manifest: "json"
 		},
 
-		exit: function () {
-			this.getModel().restoreSandbox();
+		init() {
+			UIComponent.prototype.init.apply(this, arguments);
+			this.getModel().setData({
+				Date: new Date("2022-12-31T00:00:00.000Z"),
+				EndDate: new Date("2023-03-31T00:00:00.000Z"),
+				Timezone: Localization.getTimezone()
+			});
 		}
 	});
 });
