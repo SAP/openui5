479385d1c92e46d8535d8939d0d5d1a2f2698a8e
Thomas Buechler <thomas.buechler@sap.com>
2025-06-30 09:27:22 +0200

    [FIX] Mdc.FieldBase: Mark keydown event on escape-induced valuehelp close
    Change-Id: If119ce97973dde9d733fb25c51a60094bb3a6a5c
    SNOW: DINC0552412

--- src/sap.ui.mdc/src/sap/ui/mdc/field/FieldBase.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/field/FieldBase.js
@@ -925,6 +925,7 @@ sap.ui.define([
 		const oValueHelp = _getValueHelp.call(this);
 		if (oValueHelp && oValueHelp.isOpen()) {
 			oValueHelp.close();
+			oEvent.setMarked();
 		}
 		this._sFilterValue = "";
 		if (this._oNavigateCondition || this._oNavigateCondition === null) {
