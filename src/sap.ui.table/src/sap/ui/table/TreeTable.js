b3b234868af823761afa338d545beaf4f167c8c1
Alexander Deutschmann <alexander.deutschmann@sap.com>
2025-07-01 17:46:59 +0200

    [INTERNAL][FIX] sap.ui.table.*: Invisible table can request contexts if binding is not suspended
    With I8179aedfad8cbbfc18ae379919422956a595da82, the table is prevented
    from requesting contests from the binding if the table is invisible.
    The use-case was to prevent this for an invisible table with a suspended
    binding. Preventing is in case of a non-suspended binding can lead to
    issues if the (active) binding expects to receive a getContext call.
    
    Change-Id: I783fbe965624604f86f809b5757829b299447f4f
    SNOW: DINC0550995

--- src/sap.ui.table/src/sap/ui/table/TreeTable.js
+++ src/sap.ui.table/src/sap/ui/table/TreeTable.js
@@ -263,7 +263,7 @@ sap.ui.define([
 	};
 
 	TreeTable.prototype._getContexts = function(iStartIndex, iLength, iThreshold, bKeepCurrent) {
-		if (!this.getVisible()) {
+		if (!this.getVisible() && this.getBinding()?.isSuspended()) {
 			return [];
 		}
 
--- src/sap.ui.table/test/sap/ui/table/qunit/TreeTable.ODataV4.qunit.js
+++ src/sap.ui.table/test/sap/ui/table/qunit/TreeTable.ODataV4.qunit.js
@@ -49,8 +49,7 @@ sap.ui.define([
 			}, function(oTable) {
 				oTable._oProxy._bEnableV4 = true;
 			});
-
-			this.fnBindingContextSpy = sinon.spy(TreeBindingProxy.prototype, "getContexts");
+			this.fnBindingContextSpy = sinon.spy(this.oTable.getBinding(), "getContexts");
 		},
 		afterEach: function() {
 			this.oTable?.destroy();
@@ -60,49 +59,24 @@ sap.ui.define([
 
 	QUnit.test("Initialized hidden table with suspended binding", async function(assert) {
 		await TableQUnitUtils.wait(100);
-		assert.ok(this.fnBindingContextSpy.notCalled, "TreeBindingProxy#getContexts() was not called");
+		assert.ok(this.fnBindingContextSpy.notCalled, "Binding#getContexts was not called");
 		assert.notOk(this.oTable.getRows()[0].getBindingContext(), "Table has no rows with bindingContext");
-
-		this.fnBindingContextSpy.resetHistory();
-		this.oTable.setVisible(true);
-		this.oTable.getBinding("rows").resume();
-		await this.oTable.qunit.whenBindingChange();
-		await this.oTable.qunit.whenRenderingFinished();
-
-		assert.ok(this.fnBindingContextSpy.called, "Table._getContexts() was called");
-		assert.ok(this.oTable.getRows()[0].getBindingContext(), "Table has rows with bindingContext");
 	});
 
-	QUnit.test("Hide/show and suspend/resume table during runtime", async function(assert) {
-		await TableQUnitUtils.wait(100);
-		assert.ok(this.fnBindingContextSpy.notCalled, "TreeBindingProxy#getContexts() was not called");
-		assert.notOk(this.oTable.getRows()[0].getBindingContext(), "Table has no rows with bindingContext");
-
+	QUnit.test("Change visibility of table and suspension state of binding", async function(assert) {
 		this.fnBindingContextSpy.resetHistory();
 		this.oTable.setVisible(true);
-		this.oTable.getBinding("rows").resume();
+		this.oTable.getBinding().resume();
+		await this.oTable.qunit.whenBindingChange();
 		await this.oTable.qunit.whenRenderingFinished();
-
-		assert.ok(this.fnBindingContextSpy.called, "TreeBindingProxy#getContexts() was called");
-		assert.equal(this.oTable.getVisible(), true, "Table is visible");
-		assert.equal(this.oTable.getBinding("rows").isSuspended(), false, "Binding isn't suspended");
-		assert.ok(this.oTable.getRows()[0].getBindingContext(), "Table has rows with bindingContext");
+		assert.ok(this.fnBindingContextSpy.called, "Show table and resume binding: Binding#getContexts called");
+		assert.ok(this.oTable.getRows()[0].getBindingContext(), "Show table and resume binding: Table has rows with bindingContext");
 
 		this.fnBindingContextSpy.resetHistory();
 		this.oTable.setVisible(false);
-		this.oTable.getBinding("rows").suspend();
+		this.oTable.getBinding().suspend();
 		await TableQUnitUtils.wait(100);
-
-		assert.ok(this.fnBindingContextSpy.notCalled, "TreeBindingProxy#getContexts() was not called");
-		assert.notOk(this.oTable.getRows()[0].getBindingContext(), "Table has no rows with bindingContext");
+		assert.ok(this.fnBindingContextSpy.notCalled, "Hide table and suspend binding: Binding#getContexts not called");
+		assert.notOk(this.oTable.getRows()[0].getBindingContext(), "Hide table and suspend binding: Table doesn't have rows with bindingcontext");
 	});
-
-	QUnit.test("#_getContexts", function(assert) {
-		const oGetContexts = sinon.stub(this.oTable.getBinding(), "getContexts");
-		assert.deepEqual(this.oTable._getContexts(), [], "Called without arguments on invisible and suspended table: [ ]");
-		assert.equal(oGetContexts.callCount, 0, "Called without arguments: Binding#getContexts not called");
-
-		oGetContexts.restore();
-	});
-
 });
