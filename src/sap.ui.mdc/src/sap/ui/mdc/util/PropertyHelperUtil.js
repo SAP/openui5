dabb065989d66eefa23a0973a94a872f6f58c234
Luca Mueller <luca.mueller01@sap.com>
2025-07-23 08:10:53 +0200

    [FIX] PropertyHelperUtil: check UI5 2.0 availability by deprecations
    ISSUE: Previously, we enabled propertyInfo validation in Ui5 2.0. For this, we loaded the VersionInfo and checked whether it is 2.0. This does not always work. In some cases, VersionInfo.load to to errors, resulting in missing bindingContexts for MDC Table.
    
    FIX: Instead of checking the version, we now check for the existence of deprecated functions. Specifically Core.attachInit. If available, Ui5 version is < 2.0.
    
    SNOW: DINC0578994
    CR-Id: 002075125800002983572025
    Change-Id: Ib35bf8659fb3e2642c44050f6c89d5055b831418
    (cherry picked from commit 2b3690e7e91869af60d9314a305580202510eb03)

--- src/sap.ui.mdc/src/sap/ui/mdc/util/PropertyHelperUtil.js
+++ src/sap.ui.mdc/src/sap/ui/mdc/util/PropertyHelperUtil.js
@@ -5,10 +5,12 @@
 sap.ui.define([
 	"sap/ui/core/Lib",
 	"sap/ui/VersionInfo",
+	"sap/ui/core/Core",
 	"sap/ui/base/Object"
 ], (
 	Library,
 	VersionInfo,
+	Core,
 	BaseObject
 ) => {
 	"use strict";
@@ -32,25 +34,24 @@ sap.ui.define([
 
 	const PropertyHelperUtil = {};
 
-    PropertyHelperUtil.checkValidationExceptions = async function () {
+    PropertyHelperUtil.checkValidationExceptions = function () {
 		if (this.bValidationException === null || this.bValidationException === undefined){
-			this.bValidationException = await this._checkValidationExceptions();
+			this.bValidationException = this._checkValidationExceptions();
 		}
 
-		return Promise.resolve(this.bValidationException);
+		return this.bValidationException;
 	};
 
-    PropertyHelperUtil._checkValidationExceptions = async function () {
+    PropertyHelperUtil._checkValidationExceptions = function () {
 		const affectedLibaries = ["sap.fe.core", "sap.fe.macros", "sap.sac.df"];
 		const aLoadedLibraries = affectedLibaries.filter((sLibrary) => Library.isLoaded(sLibrary));
 
-		const oVersionInfo = await VersionInfo.load();
 		const bDisabledViaConfig = window['sap-ui-mdc-config'] && window['sap-ui-mdc-config'].disableStrictPropertyInfoValidation;
 		const bDisabledViaURLParam = new URLSearchParams(window.location.search).get("sap-ui-xx-disableStrictPropertyValidation") == "true";
 		const bExceptionForFE = aLoadedLibraries.includes("sap.fe.core") || aLoadedLibraries.includes("sap.fe.macros");
 		const bDisabledForDF = aLoadedLibraries.includes("sap.sac.df");
 		const bExplicitlyEnabled = (new URLSearchParams(window.location.search).get("sap-ui-xx-enableStrictPropertyValidation") == "true");
-		const bUI5Version2 = oVersionInfo.version.indexOf("2.") === 0;
+		const bHasDeprecations = "attachInit" in Core;
 
 		// Disable strict validation if
 		// 1. it is disabled explicitly via config
@@ -58,8 +59,8 @@ sap.ui.define([
 		// 3. a library with an exception is loaded in the app (FE)
 		// 4. a library with an exception is loaded in the app (DF)
 		// 5. it has not explicitly been enabled via url param
-		// 6. UI5 version < 2.0
-		return (bDisabledViaConfig || bDisabledViaURLParam || bExceptionForFE || bDisabledForDF) && !bExplicitlyEnabled && !bUI5Version2;
+		// 6. whether deprecated functions prior to v2.0 are available -> if true, UI5 version is <2.0
+		return (bDisabledViaConfig || bDisabledViaURLParam || bExceptionForFE || bDisabledForDF) && !bExplicitlyEnabled && bHasDeprecations;
 	};
 
     return PropertyHelperUtil;
