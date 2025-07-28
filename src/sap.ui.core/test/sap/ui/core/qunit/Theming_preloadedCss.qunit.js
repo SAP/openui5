a152557176a25e48e96e39c4d3f6c1782c02bce7
Johannes Gluch <johannes.gluch@sap.com>
2025-07-28 13:35:38 +0200

    [INTERNAL] Theming_preloadedCss: Stabilize QUnit test
    Change-Id: Ic38a51572926543794e26424fdee6dc99e9006a9

--- src/sap.ui.core/test/sap/ui/core/qunit/Theming_preloadedCss.qunit.js
+++ src/sap.ui.core/test/sap/ui/core/qunit/Theming_preloadedCss.qunit.js
@@ -6,7 +6,7 @@ sap.ui.require([], () => {
 	QUnit.module("Theming - Preloaded CSS");
 
 	QUnit.test("Add preloaded CSS to theming lifecycle", (assert) => {
-		assert.expect(4);
+		assert.expect(5);
 		// Simulate a preloaded CSS file here, to ensure it did not
 		// finish loading before ThemeManager is loaded
 		// Note: It's not supported to dynamically adding library CSS with JS coding
@@ -24,14 +24,12 @@ sap.ui.require([], () => {
 			], (Theming) => {
 				const themeApplied = () => {
 					const aLinkElements = document.querySelectorAll("link[id^=sap-ui-theme-]");
-					const filterResourceTimings = (resourceTiming) => resourceTiming.initiatorType === "link" && (resourceTiming.name === aLinkElements[0].href || resourceTiming.name === aLinkElements[1].href);
-					const checkResourceTimings = (resourceTiming) => resourceTiming.responseStatus === 0 && resourceTiming.name === aLinkElements[0].href || resourceTiming.responseStatus === 200 && resourceTiming.name === aLinkElements[1].href;
-					const aResourceTimings = performance.getEntriesByType("resource").filter(filterResourceTimings);
 
 					assert.strictEqual(aLinkElements.length, 2, "There should be two link elements for library 'sap.failing.lib' and 'sap.m'");
 					assert.strictEqual(aLinkElements[0].getAttribute("id"), "sap-ui-theme-sap.failing.lib", "First link element should be for 'sap.failing.lib' since it was added first as part of the HTML page.");
+					assert.ok((aLinkElements[0].sheet.href === aLinkElements[0].href), "There should be at least a 'sheet'property available for HTMLLinkElement of 'sap.failing.lib' to indicate, that the CSS request finished loading (should be 'null' in case the request is still pending).");
 					assert.strictEqual(aLinkElements[1].getAttribute("id"), "sap-ui-theme-sap.m", "Second link element should be for 'sap.m' since it was added later as part of the test setup.");
-					assert.ok(aResourceTimings.every(checkResourceTimings), "There should be requests for each library CSS. The requests for 'sap.failing.lib' should all fail, the request for 'sap.m' should succeed.");
+					assert.ok((aLinkElements[1].sheet.href === aLinkElements[1].href && aLinkElements[1].sheet.cssRules.length > 0), "CSS for 'sap.m' should finished loading with CSS rules since the request was successful.");
 					res();
 				};
 				Theming.attachAppliedOnce(themeApplied);
